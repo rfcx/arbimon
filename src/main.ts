775b2f106b8e2a6ea1892f710d4159b229179ac4
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-08-20 01:24:59 +0700

    CE-1261 Add element plus framework and update config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import ElementPlus from 'element-plus'
 import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
 
+import 'element-plus/lib/theme-chalk/index.css'
+
 createApp(App)
+  .use(ElementPlus)
   .use(router)
   .mount('#app')
