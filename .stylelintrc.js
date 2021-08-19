23ba07e4a67f74d8b44295353bfc6dd132d94a82
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-08-19 20:43:09 +0700

    CE-1261 Setup project lint

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [
+    'stylelint-config-recommended-scss'
+  ],
+  rules: {
+    'color-no-hex': null
+  }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    port: 8080
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, './src')
+      }
+    ]
+  }
 })
