db5c111de51c6e4933392f8a9773992e53c64994
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-08-20 15:50:04 +0700

    CE-1261 Update global style template

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,12 @@
 import path from 'path'
 import { defineConfig } from 'vite'
-import pluginStyle from 'vite-plugin-style-import'
 
 import pluginVue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    pluginVue(),
-    pluginStyle({
-      libs: [
-        {
-          libraryName: 'element-plus',
-          esModule: true,
-          ensureStyleFile: true,
-          resolveStyle: (name) => {
-            name = name.slice(3)
-            return `element-plus/packages/theme-chalk/src/${name}.scss`
-          },
-          resolveComponent: (name) => {
-            return `element-plus/lib/${name}`
-          }
-        }
-      ]
-    })
+    pluginVue()
   ],
   resolve: {
     alias: [
