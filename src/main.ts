f9512889965b3e65255380917634fce7d4c4461b
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-08-26 19:57:49 +0700

    CE-1262 Add Auth0 functionality

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { createApp } from 'vue'
 
+import { Auth0 } from '@/services/auth/auth.service'
 import App from './App.vue'
 import router from './router'
 
 import 'virtual:windi.css'
 import './styles/global.scss'
 
-createApp(App)
-  .use(router)
-  .mount('#app')
+async function init (): Promise<void> {
+  const AuthPlugin = await Auth0.init({
+    redirectUri: window.location.origin,
+    onRedirectCallback: async (appState) => {
+      await router.push(appState?.targetUrl ? appState.targetUrl : window.location.pathname)
+    }
+  })
+
+  createApp(App)
+    .use(AuthPlugin)
+    .use(router)
+    .mount('#app')
+}
+
+void init()
