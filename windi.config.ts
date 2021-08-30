a7c3508fc5d01fc242905124e8ffac244c77b5ad
Somjintana Korbut <nuisomjin@gmail.com>
2021-08-30 12:35:08 +0100

    CE-1329 setup color theme and shortcut for buttons

--- windi.config.ts
+++ windi.config.ts
@@ -9,49 +9,42 @@ export default defineConfig({
   darkMode: false,
   theme: {
     extend: {
+      textColor: {
+        primary: '#fff',
+        secondary: '#999'
+      },
       colors: {
-        // Generator: http://mcg.mbitson.com
-        'brand-green': {
+        'brand-primary': {
+          light: '#E6F3EA',
           DEFAULT: '#31984F',
-          50: '#E6F3EA',
-          100: '#C1E0CA',
-          200: '#98CCA7',
-          300: '#6FB784',
-          400: '#50A769',
-          500: '#31984F',
-          600: '#2C9048',
-          700: '#25853F',
-          800: '#1F7B36',
-          900: '#136A26'
+          dark: '#25713b'
         },
-        'brand-gray': {
+        'blue-grey': {
           DEFAULT: '#232436',
-          50: '#E5E5E7',
-          100: '#BDBDC3',
-          200: '#91929B',
-          300: '#656672',
-          400: '#444554',
-          500: '#232436',
-          600: '#1F2030',
-          700: '#1A1B29',
-          800: '#151622',
-          900: '#0C0D16'
+          light: '#E5E5E7',
+          dark: '#0C0D16'
         },
-        'brand-dark-gray': {
+        'dark-grey': {
           DEFAULT: '#141525',
-          50: '#E3E3E5',
-          100: '#B9B9BE',
-          200: '#8A8A92',
-          300: '#5B5B66',
-          400: '#373846',
-          500: '#141525',
-          600: '#121221',
-          700: '#0E0F1B',
-          800: '#0B0C16',
-          900: '#06060D'
+          light: '#E3E3E5',
+          dark: '#06060D'
+        },
+        danger: {
+          DEFAULT: '#D46767',
+          light: '',
+          dark: ''
+        },
+        warning: {
+          DEFAULT: '#FFCD00',
+          light: '',
+          dark: ''
         }
       }
     }
+  },
+  shortcuts: {
+    btn: 'py-2 px-4 text-primary font-semibold rounded-lg shadow-md cursor-pointer',
+    'btn-primary': 'bg-brand-primary hover:bg-brand-primary-dark'
   },
   plugins: [
     pluginAspectRatio,
