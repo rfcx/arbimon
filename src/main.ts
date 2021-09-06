47e6d395234b60f78cabd5c94ee9c53032f4ba44
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-09-06 13:27:18 +0700

    CE-1262 Implement another style of auth0

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,38 @@
 import { createApp } from 'vue'
 
 import App from './App.vue'
-import { Auth0Plugin } from './auth/auth'
+import { Auth0 } from './auth2'
+// import { Auth0Plugin } from './auth/auth'
 import router from './router'
 import stores from './stores'
 
 import 'virtual:windi.css'
 import './styles/global.scss'
 
-createApp(App)
-  .use(Auth0Plugin, {
+// createApp(App)
+//   .use(Auth0Plugin, {
+//     redirectUri: window.location.origin,
+//     onRedirectCallback: async (appState: { targetUrl: string } | undefined) => {
+//       await router.push(appState?.targetUrl ? appState.targetUrl : window.location.pathname)
+//     }
+//   })
+//   .use(stores)
+//   .use(router)
+//   .mount('#app')
+
+async function init (): Promise<void> {
+  const Auth0Plugin = await Auth0.init({
     redirectUri: window.location.origin,
-    onRedirectCallback: async (appState: { targetUrl: string } | undefined) => {
-      await router.push(appState?.targetUrl ? appState.targetUrl : window.location.pathname)
+    onRedirectCallback: (appState) => {
+      void router.push(appState?.targetUrl ?? window.location.pathname)
     }
   })
-  .use(stores)
-  .use(router)
-  .mount('#app')
+
+  const app = createApp(App)
+  app.use(Auth0Plugin)
+    .use(stores)
+    .use(router)
+    .mount('#app')
+}
+
+void init()
