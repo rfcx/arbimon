54da2918397665c2ffa72094a6baf8798d6111ad
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-09-07 19:23:13 +0700

    CE-1327 Move comparison box into component dir

---
+++ src/components/comparison-box/comparison-box.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="overflow-x-auto">
+    <div class="flex flex-row flex-wrap">
+      <div
+        v-for="(filters, idx) in filters"
+        :key="'stream-card' + idx"
+        class="flex flex-col justify-center w-48 max-w-48 h-24 mr-4 mt-6 bg-clip-padding mirage-grey border-2 hover:bg-steel-grey cursor-pointer rounded-xl px-4 text-white text-sm"
+      >
+        <div class="flex flex-row">
+          <div
+            class="flex flex-row flex-1"
+            :title="filters.displayTitle"
+          >
+            <i class="icon-pin mr-2" /> <div class="truncate max-w-24">
+              {{ filters.displayTitle }}
+            </div>
+          </div>
+          <div
+            class="flex flex-col self-end"
+            :class="{ 'invisible': isDefaultFilter }"
+            @click="removeFilterConfig(idx)"
+          >
+            <i class="icon-close cursor-pointer" />
+          </div>
+        </div>
+        <div class="flex flex-row items-center mt-2">
+          <i class="icon-time mr-2" /> {{ filters.displayDate }}
+        </div>
+      </div>
+      <div
+        v-if="showAddButton"
+        class="flex flex-col justify-center items-center w-48 max-w-48 h-24 mt-6 bg-clip-padding mirage-grey hover:bg-steel-grey cursor-pointer border-2 border-dashed rounded-xl p-4 text-white text-sm"
+        @click="addFilterConfig"
+      >
+        <div class="uppercase">
+          + Add
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script src="./comparison-box.ts" lang="ts" />
--- src/pages/species-richness/species-richness.ts
+++ src/pages/species-richness/species-richness.ts
@@ -1,31 +1,15 @@
-import dayjs from 'dayjs'
-import { Vue } from 'vue-class-component'
+import { Options, Vue } from 'vue-class-component'
 
-import { SpeciesRichnessFilter, Stream } from '@/models'
+import ComparisonBoxComponent from '@/components/comparison-box/comparison-box.vue'
+import { SpeciesRichnessFilter } from '@/models'
 
-const defaultAllStreams: Stream = { id: 'all', name: 'All sites' }
-const defaultFilter = new SpeciesRichnessFilter(dayjs().subtract(7, 'days'), dayjs(), [defaultAllStreams])
-
+@Options({
+  components: {
+    'comparison-box': ComparisonBoxComponent
+  }
+})
 export default class SpeciesRichnessPage extends Vue {
-  public filters: SpeciesRichnessFilter[] = [defaultFilter]
-
-  public addFilterConfig (): void {
-    this.filters.push(defaultFilter)
-  }
-
-  //  TODO: HAve to improve this logic to check what is `all` meaning
-  public get isDefaultFilter (): boolean {
-    return this.filters.length === 1 && this.filters[0].streams[0].id === 'all'
-  }
-
-  public removeFilterConfig (idx: number): void {
-    this.filters.splice(idx, 1)
-    if (this.filters.length === 0) {
-      this.filters.push(defaultFilter)
-    }
-  }
-
-  public get showAddButton (): boolean {
-    return this.filters.length < 5
+  onFilterChange (filters: SpeciesRichnessFilter[]): void {
+    //
   }
 }
