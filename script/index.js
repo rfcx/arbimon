4ac8516bf817cc1989d7c035f468fc5bba4398e0
Somjintana Korbut <nuisomjin@gmail.com>
2021-09-19 23:03:35 +0100

    [40] write script to export data from Arbimon to JSON

---
+++ script/index.js
@@ -0,0 +1,41 @@
+const mysql = require('mysql')
+const fs = require('fs')
+
+require('dotenv').config()
+
+const config = {
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  host: process.env.HOST,
+  database: process.env.DATABASE
+}
+
+var connection = mysql.createConnection(config)
+connection.connect()
+ 
+// Getting validation data
+
+connection.query(`select a.site_id arbimon_site_id, s.external_id stream_id, s.name, s.lat, s.lon, a.date, a.hour, a.species_id, sp.scientific_name, sp.taxon_id, st.taxon, num_of_recordings from
+(select r.site_id, date(r.datetime) date, hour(r.datetime) hour, rv.species_id, rv.songtype_id, count(1) num_of_recordings
+from recordings r
+    left join recording_validations rv on r.recording_id = rv.recording_id and (rv.present = 1 or rv.present_review > 0)
+where r.site_id in (select site_id from sites where project_id = 1989)
+group by r.site_id, date(r.datetime), hour(r.datetime), rv.species_id, rv.songtype_id) a
+join sites s on a.site_id = s.site_id
+join species sp on a.species_id = sp.species_id
+join species_taxons st on sp.taxon_id = st.taxon_id 
+limit 5000;`, function (error, results, fields) {
+  if (error) throw error;
+  const json = JSON.stringify(results)
+  writeFile(json, 'raw-species-richness-data.json')
+})
+ 
+connection.end()
+
+// Helper functions
+
+function writeFile (json, filePath) {
+  fs.writeFile(filePath, json, 'utf8', (err) => {
+    console.error(err)
+  })
+}
