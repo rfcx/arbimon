c3fc7c55c9d4da66e7c85f660dd26ebdd11703bc
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-09-19 17:26:30 +0700

    CE-1279 Update prop logic for bar chart data and move bar chart interface to global model

--- src/pages/species-richness/species-richness.ts
+++ src/pages/species-richness/species-richness.ts
@@ -1,18 +1,33 @@
 import { Options, Vue } from 'vue-class-component'
 
-import ComparisonBoxComponent from '@/components/comparison-list/comparison-list.vue'
+import ComparisonListComponent from '@/components/comparison-list/comparison-list.vue'
 import HorizontalBarChartComponent from '@/components/horizontal-bar-chart/horizontal-bar-chart.vue'
 import { SpeciesRichnessFilter, StreamModels } from '@/models'
 import { StreamServices } from '@/services'
 
 @Options({
   components: {
-    'comparison-list': ComparisonBoxComponent,
+    ComparisonListComponent,
     HorizontalBarChartComponent
   }
 })
 export default class SpeciesRichnessPage extends Vue {
   public streams: StreamModels.Stream[] = []
+
+  public chartData = [
+    {
+      label: 'birds',
+      population: 1000
+    },
+    {
+      label: 'mammal',
+      population: 1500
+    },
+    {
+      label: 'reptile',
+      population: 800
+    }
+  ]
 
   async mounted (): Promise<void> {
     await this.getStreams()
