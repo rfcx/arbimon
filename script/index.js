05790d12ad1af415d92f8747cf48b7d5e0b4c77a
Somjintana Korbut <nuisomjin@gmail.com>
2021-09-20 15:14:18 +0100

    HOTFIX lint error

--- script/index.js
+++ script/index.js
@@ -11,15 +11,15 @@ const config = {
   timezone: 'UTC'
 }
 
-var connection = mysql.createConnection(config)
+const connection = mysql.createConnection(config)
 connection.connect()
- 
+
 // Getting validation data
 const speciesRichnessSQLQuery = fs.readFileSync('species-richness.sql').toString()
 connection.query(speciesRichnessSQLQuery, function (error, results, fields) {
-  if (error) throw error;
+  if (error) throw error
   const json = JSON.stringify(results, null, 2)
   fs.writeFileSync('raw-species-richness-data-01-07-apr-2021.json', json, 'utf8')
 })
- 
+
 connection.end()
