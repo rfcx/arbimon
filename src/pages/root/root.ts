a20f0760fe3ea947ed2ca3da19b1f9c20d20d2cc
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-09-20 13:09:35 +0700

    [44] Show "invalid project" if bad projectId

--- src/pages/root/root.ts
+++ src/pages/root/root.ts
@@ -1,11 +1,12 @@
 import { Options, Vue } from 'vue-class-component'
 
+import InvalidProjectComponent from '@/components/invalid-project/invalid-project.vue'
 import NavBarComponent from '@/components/navbar/navbar.vue'
-import { Auth0Option, Auth0User } from '@/models'
+import { Auth0Option, Auth0User, ProjectModels } from '@/models'
 import { VXServices } from '@/services'
 
 @Options({
-  components: { NavBarComponent }
+  components: { NavBarComponent, InvalidProjectComponent }
 })
 export default class RootPage extends Vue {
   @VXServices.Auth.auth.VX()
@@ -13,4 +14,7 @@ export default class RootPage extends Vue {
 
   @VXServices.Auth.user.VX()
   protected user!: Auth0User | undefined
+
+  @VXServices.Project.selectedProject.VX()
+  selectedProject!: ProjectModels.ProjectListItem | undefined
 }
