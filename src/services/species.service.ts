cfb26c60b76a1f1fbf36abcbca0d4067b75040d2
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-09-22 16:40:28 +0700

    [42] Update lodash import style

--- src/services/species.service.ts
+++ src/services/species.service.ts
@@ -1,4 +1,4 @@
-import _ from 'lodash'
+import { groupBy, includes, mapValues } from 'lodash'
 
 import rawDetections from '@/api/raw-species-richness-data-01-07-apr-2021.json'
 import { SpeciesModels, StreamModels } from '@/models'
@@ -11,9 +11,9 @@ interface SpeciesRichnessRequestParams {
 
 export function getMockupSpecies (options: SpeciesRichnessRequestParams): SpeciesModels.SpeciesRichnessBarChartItem[] {
   const { start, end, streams } = options
-  const filteredDetections = rawDetections.filter(r => r.date >= start && r.date < end && (streams.length === 0 || _.includes(streams.map(s => s.id), r.stream_id)))
-  const groupedDetections = _.groupBy(filteredDetections, 'taxon')
-  const data = _.mapValues(groupedDetections, (value, key) => {
+  const filteredDetections = rawDetections.filter(r => r.date >= start && r.date < end && (streams.length === 0 || includes(streams.map(s => s.id), r.stream_id)))
+  const groupedDetections = groupBy(filteredDetections, 'taxon')
+  const data = mapValues(groupedDetections, (value, key) => {
     return {
       label: key,
       population: new Set(value.map(d => d.species_id)).size
