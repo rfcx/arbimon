5a9dad0a8066c143b2f165f84a7956d3f6f1f3eb
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-09-23 12:01:54 +0700

    CE-1279 Update vuex service name

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,4 @@
 export * as ProjectServices from './projects.service'
 export * as SpeciesService from './species.service'
 export * as StreamServices from './streams.service'
-export * as VXServices from './vuex.service'
+export * as VuexService from './vuex.service'
