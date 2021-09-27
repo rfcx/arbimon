c9e65496b67b7bbbfac812595fd87a40a34f2f70
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-09-27 18:58:47 +0700

    [58] Get data from real mockup, transform it into group data, and update chart trigger

--- src/components/horizontal-bar-chart/horizontal-bar-chart.ts
+++ src/components/horizontal-bar-chart/horizontal-bar-chart.ts
@@ -8,14 +8,10 @@ const MARGIN = { top: 20, right: 20, bottom: 30, left: 80 }
 
 export default class HorizontalBarChartComponent extends Vue {
   @Prop({ default: [] })
-  public chartData!: ChartModels.BarChartItem[]
+  public chartData!: ChartModels.GroupedBarChartItem[]
 
   public get hasData (): boolean {
     return this.chartData.length > 0
-  }
-
-  public mounted (): void {
-    // this.generateGroupedChart()
   }
 
   @Watch('chartData', { deep: true })
@@ -23,139 +19,17 @@ export default class HorizontalBarChartComponent extends Vue {
     this.generateGroupedChart()
   }
 
-  public generateChart (): void {
-    const data = this.chartData.sort((a, b) => a.category.localeCompare(b.category))
-    const maximumFrequency = Math.max(...data.map(datum => datum.frequency))
-
-    const barHeight = 40
-    const fullWidth = document.getElementById('horizontal-bar-chart-component')?.clientWidth ?? 0 - MARGIN.left
-    const chartWidth = fullWidth - MARGIN.left - MARGIN.right
-    const fullHeight = (data.length + 1) * barHeight
-    const chartHeight = fullHeight - MARGIN.top - MARGIN.bottom
-
-    const chart: d3.Selection<SVGGElement, unknown, HTMLElement, unknown> = d3.select('#multi-bar-chart')
-    chart.selectAll('*').remove()
-
-    const svg = chart
-      .append('svg')
-      .attr('width', fullWidth)
-      .attr('height', fullHeight)
-      .append('g')
-      .attr('transform', `translate(${MARGIN.left},${MARGIN.top})`)
-
-    const xScale = d3.scaleLinear()
-      .domain([0, maximumFrequency])
-      .range([0, chartWidth])
-      .nice()
-
-    const xAxis = d3.axisBottom(xScale)
-      .tickFormat((d, _) => {
-        return d.valueOf() % 1 !== 0 ? '' : d3.format('d')(d)
-      })
-      .tickSize(0)
-      .tickPadding(10)
-
-    svg.append('g')
-      .attr('transform', `translate(0,${chartHeight})`)
-      .transition()
-      .call(xAxis)
-
-    const yScale = d3.scaleBand()
-      .domain(d3.map(data, (d) => d.category))
-      .range([0, chartHeight])
-      .paddingInner(0.2)
-
-    const yAxis = d3.axisLeft(yScale)
-      .tickSize(0)
-      .tickPadding(5)
-
-    svg.append('g')
-      .transition()
-      .call(yAxis)
-
-    // scale color line
-    svg.selectAll('.domain')
-      .style('stroke', 'none')
-
-    svg.selectAll('text')
-      .style('color', 'white')
-      .style('font-size', '14px')
-
-    svg.selectAll('line')
-      .style('color', 'none')
-
-    // bar label group
-    svg.append('g')
-      .selectAll('myRect')
-      .data(data)
-      .enter()
-      .append('rect')
-      .style('margin-top', '10px')
-      .attr('x', xScale(0))
-      .attr('y', (d) => yScale(d.category) ?? 0)
-      .attr('width', (d) => xScale(d.frequency))
-      .attr('height', yScale.bandwidth())
-      .attr('fill', '#31984F')
-
-    // frequency label group
-    const textSize = { width: 50, height: 10 }
-    svg.append('g')
-      .selectAll('myRect')
-      .data(data)
-      .enter()
-      .append('text')
-      .text((d) => d.frequency)
-      .attr('fill', 'white')
-      .attr('width', textSize.width)
-      .attr('height', textSize.height)
-      .attr('x', (d) => xScale(d.frequency) - textSize.width / 2)
-      .attr('y', (d) => (yScale(d.category) ?? 0) + yScale.bandwidth() / 2 + textSize.height / 2)
-  }
-
   public generateGroupedChart (): void {
-    const data = [
-      {
-        category: 'Birds',
-        values: [
-          { location: 'sites group 1', value: 110 },
-          { location: 'sites group 1', value: 200 },
-          { location: 'sites group 1', value: 450 }
-        ]
-      },
-      {
-        category: 'Amphebians',
-        values: [
-          { location: 'sites group 1', value: 150 },
-          { location: 'sites group 1', value: 120 },
-          { location: 'sites group 1', value: 200 }
-        ]
-      },
-      {
-        category: 'Mammals',
-        values: [
-          { location: 'sites group 1', value: 100 },
-          { location: 'sites group 1', value: 140 },
-          { location: 'sites group 1', value: 200 }
-        ]
-      },
-      {
-        category: 'Other',
-        values: [
-          { location: 'sites group 1', value: 100 },
-          { location: 'sites group 1', value: 500 },
-          { location: 'sites group 1', value: 200 }
-        ]
-      }
-    ]
+    const data = this.chartData
 
-    const maximumFrequency = Math.max(...data.map(d => Math.max(...d.values.map(v => v.value))))
+    const maximumFrequency = Math.max(...data.map(d => Math.max(...d.series.map(v => v.frequency))))
 
-    const colors = ['#FEED59', '#6CE49C', '#36D8CE', '#75BDFF', '#8000FF']
+    const colors = ['#31984F', '#FEED59', '#FF89D0', '#FFFFFF', '#75BDFF']
     const barHeight = 30
     const barMargin = 0
-    const groupHeight = Array.isArray(data) && data.length > 0 ? data[0].values.length * barHeight : 0 /** bar chart group y axis height */
+    const groupHeight = Array.isArray(data) && data.length > 0 ? data[0].series.length * barHeight : 0 /** bar chart group y axis height */
     const groupMargin = 30
-    const fullWidth = document.getElementById('horizontal-bar-chart-component')?.clientWidth ?? 0 - MARGIN.left
+    const fullWidth = (document.getElementById('horizontal-bar-chart-component')?.clientWidth ?? 0) - MARGIN.left
     const chartWidth = fullWidth - MARGIN.left - MARGIN.right
     const chartHeight = (data.length * groupHeight) + (data.length * barMargin) + groupMargin
     const fullHeight = chartHeight + MARGIN.top + MARGIN.bottom
@@ -170,6 +44,8 @@ export default class HorizontalBarChartComponent extends Vue {
       .append('g')
       .attr('transform', `translate(${MARGIN.left},${MARGIN.top})`)
 
+    // =================== Scale setting =================
+    // xScale configuration: d3 calculate the x number
     const xScale = d3.scaleLinear()
       .domain([0, maximumFrequency])
       .range([0, chartWidth])
@@ -210,6 +86,7 @@ export default class HorizontalBarChartComponent extends Vue {
     svg.selectAll('line')
       .style('color', 'none')
 
+    // =================== Generate bar group =================
     svg.selectAll('g.category')
       .data(data)
       .enter()
@@ -217,15 +94,15 @@ export default class HorizontalBarChartComponent extends Vue {
       .classed('category', true)
       .attr('transform', (d, i) => {
         // Center the group bar chart to label
-        const y = (yScale(d.category) ?? 0) + (barHeight * (-0.5 + (0.5 * (d.values.length - 1))))
+        const y = (yScale(d.category) ?? 0) + (barHeight * (-0.5 + (0.5 * (d.series.length - 1))))
         return `translate(0,${y})`
       })
       .each(function (d) {
         const category = d3.select(this)
-        for (let idx = 0; idx < d.values.length; idx++) {
-          const x = xScale(d.values[idx].value)
+        for (let idx = 0; idx < d.series.length; idx++) {
+          const x = xScale(d.series[idx].frequency)
           const width = x - xScale(0)
-          const y = (d.values.length / 2 - idx) * (barHeight + barMargin)
+          const y = (d.series.length / 2 - idx) * (barHeight + barMargin)
           category.append('rect')
             .attr('x', xScale(0))
             .attr('y', y)
@@ -235,8 +112,7 @@ export default class HorizontalBarChartComponent extends Vue {
 
           const textSize = { width: 30, height: 10 }
           category.append('text')
-            .text(d.values[idx].value)
-            // .attr('fill', 'white')
+            .text(d.series[idx].frequency)
             .attr('width', textSize.width)
             .attr('height', textSize.height)
             .attr('x', width - textSize.width)
