6868f9acd68bdecad78eb0c09ef3c3cf1b672beb
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-09-28 13:33:41 +0700

    [41] Basic taxonomy filter
    - Don't change focus/zoom when changing filter
    - Limit maxZoom (ex: when selecting single site)
    - Fix bug: default-size circles appearing if no data

--- src/components/map-bubble/map-bubble.ts
+++ src/components/map-bubble/map-bubble.ts
@@ -1,21 +1,26 @@
-import mapboxgl from 'mapbox-gl'
+import mapboxgl, { GeoJSONSource } from 'mapbox-gl'
 import { Vue } from 'vue-class-component'
 import { Prop, Watch } from 'vue-property-decorator'
 
 import { ChartModels } from '@/models'
 
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoicmZjeCIsImEiOiJoMEptMnlJIn0.LPKrjG_3AeYB5cqsyLpcrg'
-export const MAPBOX_STYLE = 'mapbox://styles/rfcx/ckapdhmby26zo1io3nqd84dsd'
-export const MAPBOX_STYLE_WITH_PLACE_LABELS = 'mapbox://styles/rfcx/ck9g6dci83g3x1io8dl27r7aq'
+// const MAPBOX_STYLE = 'mapbox://styles/rfcx/ckapdhmby26zo1io3nqd84dsd'
+// const MAPBOX_STYLE_WITH_PLACE_LABELS = 'mapbox://styles/rfcx/ck9g6dci83g3x1io8dl27r7aq'
 
 mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN
+
+// TODO 41 - Extract this
+const TAXONOMY_ALL = 'All'
+const TAXONOMIES = ['Amphibians', 'Birds']
 
 export default class MapBubbleComponent extends Vue {
   @Prop({ default: [] }) public datasets!: ChartModels.MapDataSet[]
 
   map!: mapboxgl.Map
   mapIsReady = false
-  taxon = 'Birds'
+  taxons = [TAXONOMY_ALL, ...TAXONOMIES]
+  taxon = this.taxons[0]
 
   get noData (): boolean { return this.datasets.length === 0 }
 
@@ -36,51 +41,62 @@ export default class MapBubbleComponent extends Vue {
     this.generateChart()
   }
 
-  getRadius (datum: ChartModels.MapSiteData): number {
-    if (!this.taxon) return Math.sqrt(Object.values(datum.distinctSpecies).reduce((sum, val) => sum + val, 0))
-    return Math.sqrt(datum.distinctSpecies[this.taxon])
+  @Watch('taxon') onMapConfigChange (): void {
+    this.generateChart(false)
   }
 
-  generateChart (): void {
+  getRadius (datum: ChartModels.MapSiteData): number {
+    if (this.taxon === TAXONOMY_ALL) return Math.sqrt(Object.values(datum.distinctSpecies).reduce((sum, val) => sum + val, 0))
+    return Math.sqrt(datum.distinctSpecies[this.taxon] ?? 0)
+  }
+
+  generateChart (rezoom = true): void {
     if (!this.mapIsReady) return
     if (this.noData) {
       // TODO 41 - Clear the map
       return
     }
     const map = this.map
-
+    console.log(this.datasets)
     this.datasets.forEach((dataset, idx) => {
-      map.addSource(`species-richness-${idx}`, {
-        type: 'geojson',
-        data: {
-          type: 'FeatureCollection',
-          features: dataset.data.map(datum => ({
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [datum.longitude, datum.latitude]
-            },
-            properties: { title: datum.siteId, radius: this.getRadius(datum) }
-          })
-          )
-        }
-      })
+      const data: GeoJSON.FeatureCollection<GeoJSON.Geometry> = {
+        type: 'FeatureCollection',
+        features: dataset.data.map(datum => ({
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [datum.longitude, datum.latitude]
+          },
+          properties: { title: datum.siteId, radius: this.getRadius(datum) }
+        })
+        )
+      }
 
-      map.addLayer({
-        id: `species-richness-${idx}`,
-        type: 'circle',
-        source: `species-richness-${idx}`,
-        paint: {
-          'circle-radius': ['*', ['get', 'radius'], 4],
-          'circle-color': '#B42222',
-          'circle-opacity': 0.3
-        }
-      })
+      const id = `species-richness-${idx}`
+
+      const source = map.getSource(id) as GeoJSONSource | undefined
+      if (source === undefined) map.addSource(id, { type: 'geojson', data })
+      else source.setData(data)
+
+      if (map.getLayer(id) === undefined) {
+        map.addLayer({
+          id,
+          type: 'circle',
+          source: id,
+          paint: {
+            'circle-radius': ['*', ['get', 'radius'], 4],
+            'circle-color': '#B42222',
+            'circle-opacity': 0.3
+          }
+        })
+      }
     })
 
     // TODO 41 - Merge this aggregation with the above loop
-    const coordinates: Array<[number, number]> = this.datasets.flatMap(dataset => dataset.data.map(datum => [datum.longitude, datum.latitude] as [number, number]))
-    const bounds = coordinates.reduce((bounds, coord) => bounds.extend(coord), new mapboxgl.LngLatBounds(coordinates[0], coordinates[0]))
-    map.fitBounds(bounds, { padding: 20 })
+    if (rezoom) {
+      const coordinates: Array<[number, number]> = this.datasets.flatMap(dataset => dataset.data.map(datum => [datum.longitude, datum.latitude] as [number, number]))
+      const bounds = coordinates.reduce((bounds, coord) => bounds.extend(coord), new mapboxgl.LngLatBounds(coordinates[0], coordinates[0]))
+      map.fitBounds(bounds, { padding: 20, maxZoom: 10 })
+    }
   }
 }
--- src/components/map-bubble/map-bubble.vue
+++ src/components/map-bubble/map-bubble.vue
@@ -11,9 +11,24 @@
     </div>
     <div
       v-show="!noData"
-      id="map-bubble"
-      class="w-full h-96 bg-white"
-    />
+    >
+      <select
+        v-model="taxon"
+        class="bg-dark-50 text-white"
+      >
+        <option
+          v-for="taxon in taxons"
+          :key="taxon"
+          :value="taxon"
+        >
+          {{ taxon }}
+        </option>
+      </select>
+      <div
+        id="map-bubble"
+        class="w-full h-96 bg-white"
+      />
+    </div>
   </div>
 </template>
 <script src="./map-bubble.ts" lang="ts" />
