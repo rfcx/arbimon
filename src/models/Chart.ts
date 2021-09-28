930b8505e73780edd7bc3c470c87716676fdabfb
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-09-28 12:54:06 +0700

    [41] Create data source for the map

--- src/models/Chart.ts
+++ src/models/Chart.ts
@@ -3,3 +3,15 @@ export interface BarChartItem {
   category: string
   frequency: number
 }
+
+export interface MapDataSet {
+  color: string
+  data: MapSiteData[]
+}
+
+export interface MapSiteData {
+  siteId: string
+  longitude: number
+  latitude: number
+  distinctSpecies: { [taxonName: string]: number }
+}
