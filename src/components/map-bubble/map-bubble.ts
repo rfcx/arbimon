a76423aa519de3f2268dc5d62bcf24fca41768ab
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-09-28 12:59:44 +0700

    [41] Plot data as circles
    - Render map immediately
    - Add data "source" & style "layer"
    - Zoom to data (WIP)

--- src/components/map-bubble/map-bubble.ts
+++ src/components/map-bubble/map-bubble.ts
@@ -13,22 +13,74 @@ mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN
 export default class MapBubbleComponent extends Vue {
   @Prop({ default: [] }) public datasets!: ChartModels.MapDataSet[]
 
-  public get noData (): boolean {
-    return this.chartData.length === 0
+  map!: mapboxgl.Map
+  mapIsReady = false
+  taxon = 'Birds'
+
+  get noData (): boolean { return this.datasets.length === 0 }
+
+  mounted (): void {
+    this.map = new mapboxgl.Map({
+      container: 'map-bubble',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-74.5, 40], // lng, lat // TODO 41 - Pick a nice default
+      zoom: 9
+    })
+      .on('load', () => {
+        this.mapIsReady = true
+        this.generateChart()
+      })
   }
 
-  @Watch('chartData')
-  onChartDataChange (): void {
+  @Watch('datasets') onDataChange (): void {
     this.generateChart()
   }
 
-  public generateChart (): void {
-    const map = new mapboxgl.Map({
-      container: 'map-bubble',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-74.5, 40], // lng, lat
-      zoom: 9
+  getRadius (datum: ChartModels.MapSiteData): number {
+    if (!this.taxon) return Math.sqrt(Object.values(datum.distinctSpecies).reduce((sum, val) => sum + val, 0))
+    return Math.sqrt(datum.distinctSpecies[this.taxon])
+  }
+
+  generateChart (): void {
+    if (!this.mapIsReady) return
+    if (this.noData) {
+      // TODO 41 - Clear the map
+      return
+    }
+    const map = this.map
+
+    this.datasets.forEach((dataset, idx) => {
+      map.addSource(`species-richness-${idx}`, {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: dataset.data.map(datum => ({
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [datum.longitude, datum.latitude]
+            },
+            properties: { title: datum.siteId, radius: this.getRadius(datum) }
+          })
+          )
+        }
+      })
+
+      map.addLayer({
+        id: `species-richness-${idx}`,
+        type: 'circle',
+        source: `species-richness-${idx}`,
+        paint: {
+          'circle-radius': ['*', ['get', 'radius'], 4],
+          'circle-color': '#B42222',
+          'circle-opacity': 0.3
+        }
+      })
     })
-    console.log(map)
+
+    // TODO 41 - Merge this aggregation with the above loop
+    const coordinates: Array<[number, number]> = this.datasets.flatMap(dataset => dataset.data.map(datum => [datum.longitude, datum.latitude] as [number, number]))
+    const bounds = coordinates.reduce((bounds, coord) => bounds.extend(coord), new mapboxgl.LngLatBounds(coordinates[0], coordinates[0]))
+    map.fitBounds(bounds, { padding: 20 })
   }
 }
