21f4fba484d5e8d37dd05f892605ae4bf2e8c278
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-10-05 16:32:55 +0700

    [20] Extract `generateChart`; Make period a var

--- src/components/line-chart/index.ts
+++ src/components/line-chart/index.ts
@@ -1,4 +1,74 @@
+import * as d3 from 'd3'
+
+export interface LineChartConfig {
+  height: number
+  width: number
+  margins: {
+    top: number
+    bottom: number
+    left: number
+    right: number
+  }
+}
+
 export interface LineChartSeries {
   color: string
   data: { [x: number]: number }
+}
+
+export const generateChart = (xSeries: number[], datasets: LineChartSeries[], config: LineChartConfig): SVGSVGElement | null => {
+  // Prepare data
+  const maxY = datasets.reduce((acc, cur) => Math.max(acc, Math.max(...Object.values(cur.data))), 0)
+  // TODO 20: Loop all datasets
+  const dataset = datasets[0]
+  const data = dataset.data
+
+  // Setup axes
+  const xScale = d3.scaleLinear()
+    .domain([0, xSeries.length - 1])
+    .range([config.margins.left, config.width - config.margins.right])
+
+  const xAxis = (g: any): unknown => g
+    .attr('transform', `translate(0, ${config.height - config.margins.bottom})`)
+    .call(d3.axisBottom(xScale).ticks(xSeries.length).tickSizeOuter(0))
+
+  const yScale = d3.scaleLinear()
+    .domain([0, maxY]).nice()
+    .range([config.height - config.margins.bottom, config.margins.top])
+
+  const yAxis = (g: any): unknown => g
+    .attr('transform', `translate(${config.margins.left}, 0)`)
+    .call(d3.axisLeft(yScale))
+
+  // Render chart
+  const svg = d3.create('svg')
+    .attr('viewBox', [0, 0, config.width, config.height].join(' '))
+    .attr('fill', 'none')
+    .attr('stroke-linejoin', 'round')
+    .attr('stroke-linecap', 'round')
+
+  svg.append('g').call(xAxis)
+  svg.append('g').call(yAxis)
+
+  // Render lines
+  const line = d3.line<number>()
+    .defined(d => d in data)
+    .x(x => xScale(x))
+    .y(x => yScale(data[x] ?? NaN))
+
+  svg.append('path')
+    .datum(xSeries.filter(line.defined()))
+    .attr('d', line)
+    .attr('stroke', '#999')
+    .attr('stroke-width', 1)
+    .style('stroke-dasharray', ('3, 3'))
+    .style('opacity', 50)
+
+  svg.append('path')
+    .datum(xSeries)
+    .attr('d', line)
+    .attr('stroke', dataset.color)
+    .attr('stroke-width', 3)
+
+  return svg.node()
 }
--- src/components/line-chart/line-chart.ts
+++ src/components/line-chart/line-chart.ts
@@ -1,12 +1,14 @@
-import * as d3 from 'd3'
 import { Vue } from 'vue-class-component'
 import { Prop, Watch } from 'vue-property-decorator'
 
-import { LineChartSeries } from '.'
+import { generateChart, LineChartConfig, LineChartSeries } from '.'
 
-const HEIGHT = 500
-const WIDTH = 1000
-const MARGINS = { top: 20, right: 30, bottom: 30, left: 40 }
+const PERIOD_TO_FORCED_X: Record<string, number> = {
+  hour: 24,
+  day: 28,
+  month: 12,
+  quarter: 4
+}
 
 export default class LineChart extends Vue {
   @Prop() domId!: string
@@ -25,6 +27,15 @@ export default class LineChart extends Vue {
     }
   ]
 
+  allPeriods = ['hour', 'day', 'month', 'year', 'quarter']
+  period = 'hour'
+
+  config: LineChartConfig = {
+    height: 500,
+    width: 1000,
+    margins: { top: 20, right: 30, bottom: 30, left: 40 }
+  }
+
   get hasData (): boolean { return this.datasets.length > 0 }
 
   mounted (): void {
@@ -36,67 +47,24 @@ export default class LineChart extends Vue {
   }
 
   updateChart (): void {
-    const chart = this.generateChart()
+    const data = this.groupDataByPeriod()
+    const xSeries = this.getXSeries(data)
+
+    const chart = generateChart(xSeries, data, this.config)
     if (!chart) return
+
     document.getElementById(this.domId)?.appendChild(chart)
   }
 
-  // TODO 20 - Move this out of the component
-  generateChart (): SVGSVGElement | null {
-    // Prepare data
-    const maxY = this.datasets.reduce((acc, cur) => Math.max(acc, Math.max(...Object.values(cur.data))), 0)
-    // TODO 20: Loop all datasets
-    const dataset = this.datasets[0]
-    const data = dataset.data
-    const xSeries = Array.from({ length: 24 }, (_, i) => i) // TODO 20: Also support days, months, etc (currently only hours)
+  groupDataByPeriod (): LineChartSeries[] {
+    return this.datasets // TODO 20: Implement this once we inject ungrouped data
+  }
 
-    // Setup axes
-    const xScale = d3.scaleLinear()
-      .domain([0, 23])
-      .range([MARGINS.left, WIDTH - MARGINS.right])
-
-    const xAxis = (g: any): unknown => g
-      .attr('transform', `translate(0, ${HEIGHT - MARGINS.bottom})`)
-      .call(d3.axisBottom(xScale).ticks(xSeries.length).tickSizeOuter(0))
-
-    const yScale = d3.scaleLinear()
-      .domain([0, maxY]).nice()
-      .range([HEIGHT - MARGINS.bottom, MARGINS.top])
-
-    const yAxis = (g: any): unknown => g
-      .attr('transform', `translate(${MARGINS.left}, 0)`)
-      .call(d3.axisLeft(yScale))
-
-    // Render chart
-    const svg = d3.create('svg')
-      .attr('viewBox', [0, 0, WIDTH, HEIGHT].join(' '))
-      .attr('fill', 'none')
-      .attr('stroke-linejoin', 'round')
-      .attr('stroke-linecap', 'round')
-
-    svg.append('g').call(xAxis)
-    svg.append('g').call(yAxis)
-
-    // Render lines
-    const line = d3.line<number>()
-      .defined(d => d in data)
-      .x(x => xScale(x))
-      .y(x => yScale(data[x] ?? NaN))
-
-    svg.append('path')
-      .datum(xSeries.filter(line.defined()))
-      .attr('d', line)
-      .attr('stroke', '#999')
-      .attr('stroke-width', 1)
-      .style('stroke-dasharray', ('3, 3'))
-      .style('opacity', 50)
-
-    svg.append('path')
-      .datum(xSeries)
-      .attr('d', line)
-      .attr('stroke', dataset.color)
-      .attr('stroke-width', 3)
-
-    return svg.node()
+  getXSeries (data: LineChartSeries[]): number[] {
+    // Force 0-24 hour, 0-12 months, etc
+    const xForced = Array.from({ length: PERIOD_TO_FORCED_X[this.period] ?? 0 }, (_, i) => i)
+    // Add real values (ex: 2020, 2021)
+    const xActual = data.flatMap(s => Object.keys(s.data).map(Number))
+    return Array.from(new Set([...xForced, ...xActual])).sort((a, b) => a - b)
   }
 }
