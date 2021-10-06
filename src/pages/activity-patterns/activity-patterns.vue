fc8c1aa7b5343915cec418def1727a91a06bf483
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-10-07 01:48:07 +0700

    [89] Remove form

--- src/pages/activity-patterns/activity-patterns.vue
+++ src/pages/activity-patterns/activity-patterns.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="activity-patterns">
-    <form>
-      <select
-        v-model="selectedSpecies"
-        class="bg-steel-grey rounded p-2 focus:(border-box-grey ring-0 outline-none) min-w-64"
+    <select
+      v-model="selectedSpecies"
+      class="bg-steel-grey rounded p-2 focus:(border-box-grey ring-0 outline-none) min-w-64"
+    >
+      <option
+        v-for="(item, idx) in species"
+        :key="'species-name-selector-' + idx"
+        :value="item.speciesId"
       >
-        <option
-          v-for="(item, idx) in species"
-          :key="'species-name-selector-' + idx"
-          :value="item.speciesId"
-        >
-          {{ item.speciesName }}
-        </option>
-      </select>
-    </form>
+        {{ item.speciesName }}
+      </option>
+    </select>
   </div>
 </template>
 <script src="./activity-patterns.ts" lang="ts"></script>
