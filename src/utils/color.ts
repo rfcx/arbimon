b87820795972793e1731042d52b47c4e20058e5d
Somjintana Korbut <nuisomjin@gmail.com>
2021-10-06 16:35:40 +0100

    [102] swap order of color / improve filter box style

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,9 +1,9 @@
 // Designed for contrast & color-blind support:
 // https://davidmathlogic.com/colorblind/#%23000000-%232C4A1A-%23499FE6-%23EAAE39-%239A258A-%23B0B9BF-%2370E8A8
 export const datasetColors = [
+  '#70E8A8',
   '#499FE6',
-  '#EAAE39',
   '#9A258A',
   '#B0B9BF',
-  '#70E8A8'
+  '#EAAE39'
 ]
