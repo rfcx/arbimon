f92f4b7589a4aada46f1a2df99c4a689d2aa8a1b
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-10-07 08:10:14 +0700

    [113] Simplify `speciesByTaxon` data transforms

--- src/services/species.api.service.ts
+++ src/services/species.api.service.ts
@@ -1,7 +1,7 @@
 import { groupBy, mapValues } from 'lodash'
 
 import rawDetections from '@/api/raw-species-richness-data-01-07-apr-2021.json'
-import { ChartModels, DetectionModels, SiteModels, TaxonomyModels } from '@/models'
+import { DetectionModels, SiteModels, TaxonomyModels } from '@/models'
 import { MapSiteData } from '@/models/Chart'
 
 interface SpeciesRichnessRequestParams {
@@ -30,17 +30,12 @@ function filterAPIData (options: SpeciesRichnessRequestParams): DetectionRespons
   return rawDetections.filter(r => r.date >= start && r.date < end && (sites.length === 0 || sites.map(s => s.id).includes(r.stream_id)))
 }
 
-export function getMockupSpecies (options: SpeciesRichnessRequestParams): Array<Omit<ChartModels.BarChartItem, 'color'>> {
+export function getMockupSpecies (options: SpeciesRichnessRequestParams): { [taxon: string]: number } {
   const filteredDetections = filterAPIData(options)
-  const groupedDetections = groupBy(filteredDetections, 'taxon')
-  const data = mapValues(groupedDetections, (value, key) => {
-    return {
-      category: key,
-      frequency: new Set(value.map(d => d.species_id)).size
-    }
-  })
-
-  return Object.values(data)
+  return mapValues(
+    groupBy(filteredDetections, 'taxon'),
+    (value) => new Set(value.map(d => d.species_id)).size
+  )
 }
 
 export function getSpeciesMapData (options: SpeciesRichnessRequestParams): MapSiteData[] {
