0a7a6ef266b7aa87fba1b31dbf4a1173f83e4256
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-10-07 17:52:24 +0700

    [112] Add td padding and opacity

--- src/pages/species-richness/components/species-richness-table/species-richness-table.vue
+++ src/pages/species-richness/components/species-richness-table/species-richness-table.vue
@@ -3,14 +3,14 @@
     <h2 class="text-white text-xl mb-1.5">
       Detected Species
     </h2>
-    <div class="max-h-100 overflow-y-auto">
+    <div class="mt-2 max-h-100 overflow-y-auto">
       <table class="w-full">
         <thead class="h-10">
           <tr class="sticky top-0">
             <th
               v-for="(item, idx) in tableHeader"
               :key="'species-table-header-' + item.title"
-              class="font-bold capitalize"
+              class="font-bold capitalize p-2"
               :class="{ 'text-left': idx < 2, 'text-mirage-grey': idx > 1 && idx < tableHeader.length - 1 }"
               :style="{ backgroundColor: item.color }"
             >
@@ -24,18 +24,22 @@
             :key="'species-table-row-' + row.speciesName"
             class="capitalize"
           >
-            <td>{{ row.speciesName }}</td>
-            <td>{{ row.className }}</td>
+            <td class="p-2">
+              {{ row.speciesName }}
+            </td>
+            <td class="p-2">
+              {{ row.className }}
+            </td>
             <td
               v-for="(dataset, idx ) in datasetCount"
               :key="'species-table-column-' + dataset"
-              class="py-1 text-center text-mirage-grey"
-              :style="{ backgroundColor: colors[idx] }"
+              class="p-2 text-center text-mirage-grey"
+              :style="{ backgroundColor: colors[idx] + 'f0' }"
             >
               <!-- v-for is 1-based -->
               {{ row.data[dataset - 1] ? 1 : 0 }}
             </td>
-            <td class="text-center">
+            <td class="p-2 text-center">
               {{ row.total }}
             </td>
           </tr>
