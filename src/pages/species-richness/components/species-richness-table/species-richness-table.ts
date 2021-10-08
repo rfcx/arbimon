50faf4b72b92e764bf0b08a2fe2269cb7d03275b
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-10-08 19:50:01 +0700

    [112] Add logic to display total column

--- src/pages/species-richness/components/species-richness-table/species-richness-table.ts
+++ src/pages/species-richness/components/species-richness-table/species-richness-table.ts
@@ -21,21 +21,17 @@ export default class SpeciesRichnessTable extends Vue {
   @Prop({ default: [] }) colors!: string[]
 
   get tableHeader (): Header[] {
-    return [
-      {
-        title: 'Species',
-        color: DEFAULT_COLOR
-      },
-      {
-        title: 'Class',
-        color: DEFAULT_COLOR
-      },
-      ...Array.from({ length: this.datasetCount }, (v, i) => ({ title: `Dataset ${i + 1}`, color: this.colors[i] })),
-      {
-        title: 'Total',
-        color: DEFAULT_COLOR
-      }
+    const headers = [
+      { title: 'Species', color: DEFAULT_COLOR },
+      { title: 'Class', color: DEFAULT_COLOR },
+      ...Array.from({ length: this.datasetCount }, (v, i) => ({ title: `Dataset ${i + 1}`, color: this.colors[i] }))
     ]
+
+    if (this.datasetCount > 1) {
+      headers.push({ title: 'Total', color: DEFAULT_COLOR })
+    }
+
+    return headers
   }
 
   get hasTableData (): boolean {
