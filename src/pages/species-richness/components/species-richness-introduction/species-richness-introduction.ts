b24b87c31916fa1e7c8c0af18c0bb50f07ef2697
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-10-10 21:37:41 +0700

    Fix merge (move logic into intro component)

---
+++ src/pages/species-richness/components/species-richness-introduction/species-richness-introduction.ts
@@ -0,0 +1,29 @@
+import { Options, Vue } from 'vue-class-component'
+import { Prop } from 'vue-property-decorator'
+
+import { SpeciesRichnessFilter } from '@/models'
+import { FileUtils } from '@/utils'
+import ExportButtonView from '@/views/export-button.vue'
+import { getReportRawData } from '../../csv'
+
+@Options({
+  components: {
+    ExportButtonView
+  }
+})
+export default class SpeciesRichnessTable extends Vue {
+  @Prop() filters!: SpeciesRichnessFilter[]
+  @Prop() haveData!: boolean
+
+  async exportCSVReport (): Promise<void> {
+    const csvs = await Promise.all(this.filters.map(async ({ startDate, endDate, sites, color }) => {
+      const start = startDate.toISOString()
+      const end = endDate.add(1, 'days').toISOString()
+      return await getReportRawData({ start, end, sites })
+    }))
+
+    const filename = 'report.csv'
+    // TODO 106 - Support multiple datasets
+    await FileUtils.exportCSVFile(filename, csvs[0], 'Species Report')
+  }
+}
