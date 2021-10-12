c882d89cef4f8e1db16ba3459fe974a193532222
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-10-12 15:38:34 +0700

    [107] Update name logic for zip file

--- src/services/dayjs-service.ts
+++ src/services/dayjs-service.ts
@@ -1,6 +1,7 @@
 // This is the one place we import & configure dayjs
 // eslint-disable-next-line unicorn/import-style
 import dayjsBase from 'dayjs'
+import minMax from 'dayjs/plugin/minMax'
 import pluralGetSet from 'dayjs/plugin/pluralGetSet'
 import quarterOfYear from 'dayjs/plugin/quarterOfYear'
 import utc from 'dayjs/plugin/utc'
@@ -8,5 +9,6 @@ import utc from 'dayjs/plugin/utc'
 dayjsBase.extend(utc)
 dayjsBase.extend(quarterOfYear)
 dayjsBase.extend(pluralGetSet)
+dayjsBase.extend(minMax)
 
 export const dayjs = dayjsBase
--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -2,41 +2,63 @@ import { Dayjs } from 'dayjs'
 
 import { SiteModels, SpeciesRichnessFilter } from '@/models'
 import { VuexService } from '@/services'
+import { dayjs } from '@/services/dayjs-service'
 
-function getDateFormatted (startDate: Dayjs, endDate: Dayjs, dateFormat: string): string {
-  const start = startDate.format(dateFormat)
-  const end = endDate.format(dateFormat)
-  return startDate.isSame(endDate, 'date') ? `${start} - ${end}` : start
+export const EXPORT_DATE_FORMAT = 'YYMMDD'
+
+export function formatDate (date: Dayjs, dateFormat: string = EXPORT_DATE_FORMAT): string {
+  return date.format(dateFormat)
 }
 
-export function getFilterExportName (startDate: Dayjs, endDate: Dayjs, sites: SiteModels.Site[], prefix: string): string {
+function getDateFormatted (startDate: Dayjs, endDate: Dayjs, dateFormat: string): string {
+  const start = formatDate(startDate, dateFormat)
+  const end = formatDate(endDate, dateFormat)
+  return startDate.isSame(endDate, 'date') ? start : `${start} - ${end}`
+}
+
+function getSiteName (sites: SiteModels.Site[]): string {
+  const siteLength = sites.length
+  switch (siteLength) {
+    case 0: return 'All sites'
+    case 1: return sites[0].name
+    default: return `${sites[0].name} + ${siteLength - 1} other sites`
+  }
+}
+
+export function getFilterExportGroupName (startDate: Dayjs, endDate: Dayjs, prefix: string): string {
+  const project = VuexService.Project.selectedProject.get()
+  const projectName = project?.name?.replaceAll(' ', '-') ?? 'None'
+
+  return `${projectName}--${prefix.replaceAll(' ', '-')}--${getDateGroup(startDate, endDate)}`
+}
+
+export function getFilterExportName (startDate: Dayjs, endDate: Dayjs, prefix: string, dateGroup?: string, sites?: SiteModels.Site[]): string {
   const project = VuexService.Project.selectedProject.get()
 
-  let siteName = 'All_Sites'
-  const siteLength = sites.length
-  if (siteLength === 1) {
-    siteName = sites[0].name
-  } else if (siteLength > 1) {
-    siteName = `${sites[0].name}+${siteLength - 1}-other-sites`
-  }
+  const projectName = project?.name?.replaceAll(' ', '-') ?? 'None'
+  const siteName = sites ? `--${getSiteName(sites).replaceAll(' ', '_')}` : ''
+  const date = dateGroup ? getDateFormatted(startDate, endDate, 'YYMMDD').replaceAll(' ', '') : `${getDateGroup(startDate, endDate)}`
 
-  const date = getDateFormatted(startDate, endDate, 'YYMMDD')
-
-  return `${project?.name ?? 'None'}--${prefix}--${siteName}--${date}`
+  return `${projectName}--${prefix}${siteName}--${date}${dateGroup ? '--' + dateGroup : ''}`
 }
 
 export function getFilterFriendlyName (filter: SpeciesRichnessFilter): string {
   const { startDate, endDate, sites } = filter
 
-  let siteName = 'All sites'
-  const siteLength = sites.length
-  if (siteLength === 1) {
-    siteName = sites[0].name
-  } else if (siteLength > 1) {
-    siteName = `${sites[0].name} + ${siteLength - 1} other sites`
-  }
-
+  const siteName = getSiteName(sites)
   const date = getDateFormatted(startDate, endDate, 'DD MMM YY')
 
   return `${siteName} (${date})`
+}
+
+export function getDateGroup (startDate: Dayjs, endDate: Dayjs): string {
+  return `${formatDate(startDate)}${formatDate(endDate)}`
+}
+
+export function getMaxFilterDate (dates: Dayjs[]): Dayjs {
+  return dayjs.max(dates)
+}
+
+export function getMinFilterDate (dates: Dayjs[]): Dayjs {
+  return dayjs.min(dates)
 }
