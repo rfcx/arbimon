058a6ed641fa1469de567ae87b1388a6a65cb13c
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-10-12 14:55:45 +0700

    [106] Separate pure/impure; Rename funcs/params

--- src/pages/species-richness/components/species-richness-introduction/species-richness-introduction.ts
+++ src/pages/species-richness/components/species-richness-introduction/species-richness-introduction.ts
@@ -3,6 +3,7 @@ import { Prop } from 'vue-property-decorator'
 
 import { FileModels, SpeciesRichnessFilter } from '@/models'
 import { FileUtils } from '@/utils'
+import { downloadZip } from '@/utils/file'
 import { getReportRawData } from '../../csv'
 
 export default class SpeciesRichnessTable extends Vue {
@@ -10,17 +11,20 @@ export default class SpeciesRichnessTable extends Vue {
   @Prop() haveData!: boolean
 
   async exportCSVReports (): Promise<void> {
-    const csvs = await Promise.all(this.filters.map(async ({ startDate, endDate, sites, color }) => {
+    const csvData = await Promise.all(this.filters.map(async ({ startDate, endDate, sites, color }) => {
       const start = startDate.toISOString()
       const end = endDate.add(1, 'days').toISOString()
       return await getReportRawData({ start, end, sites })
     }))
 
     // TODO - 106: Update filename and folder name
-    const files: FileModels.File[] = await Promise.all(csvs.map(async (csv, idx) => ({
+    const files: FileModels.File[] = await Promise.all(csvData.map(async (csvDatum, idx) => ({
       filename: `report-${idx + 1}.csv`,
-      data: await FileUtils.getCsvString(csv)
+      data: await FileUtils.toCsv(csvDatum)
     })))
-    await FileUtils.zipFiles(files, 'reports')
+
+    const folderName = 'reports'
+    const zipUrl = await FileUtils.zipFiles(files, folderName)
+    downloadZip(zipUrl, folderName)
   }
 }
