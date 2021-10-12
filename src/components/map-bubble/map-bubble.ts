5e69166a8cfe2c746bce70fc1859cf6d00b59908
Somjintana Korbut <nuisomjin@gmail.com>
2021-10-12 13:24:11 +0100

    [102] change color scheme / adjust map bubble opacity

--- src/components/map-bubble/map-bubble.ts
+++ src/components/map-bubble/map-bubble.ts
@@ -155,7 +155,7 @@ export default class MapBubbleComponent extends Vue {
         paint: {
           'circle-radius': ['*', ['get', 'radius'], 4], // TODO 41 - Normalize circle size
           'circle-color': this.dataset.color || '#B42222',
-          'circle-opacity': 0.3
+          'circle-opacity': 0.6
         }
       })
     }
