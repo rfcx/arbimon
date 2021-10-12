b860aecc12bfb890985980524552680091a9b6bb
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-10-12 21:12:31 +0700

    [107] Order props in map component

--- src/components/map-bubble/map-bubble.ts
+++ src/components/map-bubble/map-bubble.ts
@@ -14,9 +14,9 @@ export default class MapBubbleComponent extends Vue {
   @Prop() dataset!: MapModels.MapDataSet
   @Prop() taxon!: string
   @Prop() mapConfig!: MapModels.MapConfig
+  @Prop() mapExportName!: string
   @Prop({ default: 'mapbox://styles/mapbox/streets-v11' }) mapStyle!: string
   @Prop({ default: true }) isShowLabels!: boolean
-  @Prop() mapExportName!: string
 
   @Emit() emitMapMoved (): MapModels.MapConfig {
     return { sourceMapId: this.mapId, center: this.map.getCenter(), zoom: this.map.getZoom() }
