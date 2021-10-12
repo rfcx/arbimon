f497f6544d39fab087690839f74f1bbedd6e9cac
Somjintana Korbut <nuisomjin@gmail.com>
2021-10-12 14:03:01 +0100

    [102] edit color

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,3 +1,3 @@
 // Designed for contrast & color-blind support:
 // https://davidmathlogic.com/colorblind/#%23000000-%232C4A1A-%23499FE6-%23EAAE39-%239A258A-%23B0B9BF-%2370E8A8
-export const datasetColors = ['#85EBBA', '#85C1F5', '#B578DB', '#E5D3D2', '#D6E68C']
+export const datasetColors = ['#85EBBA', '#6FC1F5', '#B578DB', '#EAC3E4', '#D6E68C']
