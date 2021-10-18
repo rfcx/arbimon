627e659be29b84960f659f8d2817bd80b287a55d
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-10-18 11:57:54 +0700

    [131] Update registered component name

--- src/_components/index.ts
+++ src/_components/index.ts
@@ -1,6 +1,6 @@
 // Add components here to register them globally
-export { default as DropdownItem } from './dropdown/dropdown-item.vue'
 export { default as DropdownMenu } from './dropdown/dropdown-menu.vue'
+export { default as DropdownMenuItem } from './dropdown/dropdown-menu-item.vue'
 export { default as ExportButtonView } from './export-button.vue'
 export { default as NoDataContainerView } from './no-data-panel.vue'
 export { default as PageTitleView } from './page-title.vue'
