874a48f51a2e56e977bceea8cb1d380a841504f6
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-10-20 16:54:27 +0700

    [163] Add redirect source to wikipedia logic

--- src/activity-patterns/components/species-information/species-information.vue
+++ src/activity-patterns/components/species-information/species-information.vue
@@ -1,9 +1,28 @@
 <template>
-  <div class="grid grid-cols-4">
-    <div class="col-span-3 flex items-center px-4 py-2">
-      &emsp;&emsp;{{ speciesInformation?.content }}
+  <no-data-container-view
+    v-if="!speciesInformation || isLoading"
+    class="h-32"
+  />
+  <div
+    v-else
+    class="grid grid-cols-4"
+  >
+    <div class="col-span-3 self-center px-4 py-2">
+      <div>
+        &emsp;&emsp;{{ speciesInformation?.content }}
+        (
+        <b class="opacity-80">Source: &nbsp;</b>
+        <a
+          :href="redirectToWiki()"
+          target="_blank"
+          class="opacity-80 hover:(underline opacity-70)"
+        >
+          Wikipedia
+        </a>
+        )
+      </div>
     </div>
-    <div class="col-span-1">
+    <div class="col-span-1 place-self-center">
       <img
         :src="speciesInformation?.thumbnail?.source"
         class="object-contain max-h-50"
