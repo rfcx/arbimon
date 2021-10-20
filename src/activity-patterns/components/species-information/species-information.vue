23a8be0c82968f6a997ef1b77e250c23318116b4
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-10-20 15:43:04 +0700

    [163] Add style for displaying species information

--- src/activity-patterns/components/species-information/species-information.vue
+++ src/activity-patterns/components/species-information/species-information.vue
@@ -1,4 +1,14 @@
 <template>
-  <div>{{ speciesInformation }}</div>
+  <div class="grid grid-cols-4">
+    <div class="col-span-3 flex items-center px-4 py-2">
+      &emsp;&emsp;{{ speciesInformation?.content }}
+    </div>
+    <div class="col-span-1">
+      <img
+        :src="speciesInformation?.thumbnail?.source"
+        class="object-contain max-h-50"
+      >
+    </div>
+  </div>
 </template>
 <script lang="ts" src="./species-information.ts"></script>
