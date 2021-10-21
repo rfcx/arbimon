ff3ba2f4f5bd32d749d2af53b3994567e4a09e2b
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-10-21 17:02:25 +0700

    [130] Add mockup datasets

--- src/activity-patterns/components/metrics/metrics.ts
+++ src/activity-patterns/components/metrics/metrics.ts
@@ -8,5 +8,36 @@ import SingleDatasetComponent from './single-dataset-component.vue'
   }
 })
 export default class ActivityPatternsMetrics extends Vue {
-
+  get metricsInformation (): any {
+    return [
+      {
+        title: 'Detection frequency',
+        information: 'Number of recordings as a proportion of total recordings',
+        datasets: [
+          {
+            value: 25,
+            description: 'Found in 25 recordings out of 100 recordings'
+          },
+          {
+            value: 35,
+            description: 'Found in 350 recordings out of 1000 recordings'
+          }
+        ]
+      },
+      {
+        title: 'Occupancy',
+        information: 'Number of sites with a detection',
+        datasets: [
+          {
+            value: 45,
+            description: 'Found in 45 sites out of 100 sites'
+          },
+          {
+            value: 50,
+            description: 'Found in 25 sites out of 50 sites'
+          }
+        ]
+      }
+    ]
+  }
 }
