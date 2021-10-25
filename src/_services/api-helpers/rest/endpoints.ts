248db9895a25c2aef73c3a11cf775d6717d59111
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-10-26 01:00:56 +0700

    [163] Guarantee our RequestMethod is also an Axios Method

--- src/_services/api-helpers/rest/endpoints.ts
+++ src/_services/api-helpers/rest/endpoints.ts
@@ -1,4 +1,6 @@
-export type RequestMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'
+import { Method } from 'axios'
+
+export type RequestMethod = Method & ('GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE')
 
 export interface Endpoint {
   method: RequestMethod
