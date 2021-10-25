d8a2702c6b6bc00ba0ee78e404d98872ae831158
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-10-26 00:51:46 +0700

    [163] Encapsulate endpoint & API types; Inject baseUrl

--- src/_services/api/wiki-service/index.ts
+++ src/_services/api/wiki-service/index.ts
@@ -1,1 +1,5 @@
-export * from './wiki-service-api'
+import { BASE_URL_WIKI } from '~/api/wiki-service/env'
+import { WikiService } from '~/api/wiki-service/wiki-service-api'
+
+export * from './types'
+export const wikiService = new WikiService(BASE_URL_WIKI)
--- src/_services/api/wiki-service/wiki-service-api.ts
+++ src/_services/api/wiki-service/wiki-service-api.ts
@@ -1,28 +1,79 @@
 import axios from 'axios'
 
-import { WikiEndpoints } from '../../api-helpers/rest'
-import { WikiSummary, WikiSummaryResponse } from './types'
+import { WikiSummary } from '~/api/wiki-service/types'
+import { Endpoint } from '~/api-helpers/rest'
 
-// =================== WIKI ====================
-export const getSpeciesSummary = async (speciesName: string): Promise<WikiSummary | undefined> => {
-  const { method, url } = WikiEndpoints.endpointWikiSummary
+export class WikiService {
+  constructor (private readonly baseUrl: string) {}
 
-  try {
-    const response = await axios.request<WikiSummaryResponse>({
-      method,
-      url: url(speciesName)
-    })
-    const data = response.data
+  async getSpeciesSummary (speciesName: string): Promise<WikiSummary | undefined> {
+    try {
+      const endpoint: Endpoint = ({
+        method: 'GET',
+        url: `${this.baseUrl}/api/rest_v1/page/summary/${speciesName}`
+      })
 
-    return {
-      content: data.extract,
-      contentUrls: {
-        desktop: data.content_urls?.desktop?.page,
-        mobile: data.content_urls?.mobile?.page
-      },
-      thumbnailImage: data.thumbnail?.source
-    }
-  } catch (e) {
+      const { data } = await axios.request<WikiSummaryResponse>(endpoint)
+      return {
+        content: data.extract,
+        contentUrls: {
+          desktop: data.content_urls?.desktop?.page,
+          mobile: data.content_urls?.mobile?.page
+        },
+        thumbnailImage: data.thumbnail?.source
+      }
+    } catch (e) {
     // TODO #191: API Handle
+    }
   }
+}
+
+interface WikiSummaryResponse {
+  type: string
+  title: string
+  displaytitle: string
+  namespace: {
+    id: number
+    text: string
+  }
+  wikibase_item: string
+  titles: {
+    canonical: string
+    normalized: string
+    display: string
+  }
+  pageid: number
+  thumbnail: {
+    source: string
+    width: number
+    height: number
+  }
+  originalimage: {
+    source: string
+    width: number
+    height: number
+  }
+  lang: string
+  dir: string
+  revision: string
+  tid: string
+  timestamp: string
+  description: string
+  description_source: string
+  content_urls: {
+    desktop: {
+      page: string
+      revisions: string
+      edit: string
+      talk: string
+    }
+    mobile: {
+      page: string
+      revisions: string
+      edit: string
+      talk: string
+    }
+  }
+  extract: string
+  extract_html: string
 }
