81e849efc90e5729cb466d42d899042d474e0a5c
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-10-26 20:36:05 +0700

    [130] Update the multi metrics percentage position

--- src/activity-patterns/components/metrics/multi-metrics.vue
+++ src/activity-patterns/components/metrics/multi-metrics.vue
@@ -2,22 +2,23 @@
   <div
     v-for="(item, idx) in datasets"
     :key="'metrics-dataset-' + idx"
-    class="relative mb-6 group"
   >
-    <div class="text-right m-1">
+    <div class="m-1">
       {{ item.percentage }}%
     </div>
-    <div
-      class="absolute w-full h-2 rounded-xl opacity-60"
-      :style="{ backgroundColor: item.color }"
-    />
-    <div
-      class="h-2 rounded-xl z-5 opacity-80 group-hover:(opacity-100)"
-      :style="{ width: item.percentage + '%', backgroundColor: item.color }"
-    />
-    <p class="absolute min-w-36 mx-auto bg-box-grey mt-2 py-2 px-4 invisible group-hover:visible rounded-md">
-      {{ item.description }}
-    </p>
+    <div class="relative mb-6 group">
+      <div
+        class="absolute w-full h-2 rounded-xl opacity-60"
+        :style="{ backgroundColor: item.color }"
+      />
+      <div
+        class="h-2 rounded-xl z-5 opacity-80 group-hover:(opacity-100)"
+        :style="{ width: item.percentage + '%', backgroundColor: item.color }"
+      />
+      <p class="absolute min-w-36 mx-auto bg-box-grey mt-2 py-2 px-4 invisible group-hover:visible rounded-md">
+        {{ item.description }}
+      </p>
+    </div>
   </div>
 </template>
 <script lang="ts">
