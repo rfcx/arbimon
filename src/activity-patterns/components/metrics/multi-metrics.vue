c480e63681f4d2dd23d942c3481c1631090c11c4
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-10-26 21:31:04 +0700

    [130] Update padding

--- src/activity-patterns/components/metrics/multi-metrics.vue
+++ src/activity-patterns/components/metrics/multi-metrics.vue
@@ -2,11 +2,12 @@
   <div
     v-for="(item, idx) in datasets"
     :key="'metrics-dataset-' + idx"
+    class="group"
   >
-    <div class="m-1">
+    <div class="first:mt-4 hover:cursor-default">
       {{ item.percentage }}%
     </div>
-    <div class="relative mb-6 group">
+    <div class="relative mb-6">
       <div
         class="absolute w-full h-2 rounded-xl opacity-60"
         :style="{ backgroundColor: item.color }"
@@ -15,7 +16,7 @@
         class="h-2 rounded-xl z-5 opacity-80 group-hover:(opacity-100)"
         :style="{ width: item.percentage + '%', backgroundColor: item.color }"
       />
-      <p class="absolute min-w-36 mx-auto bg-box-grey mt-2 py-2 px-4 invisible group-hover:visible rounded-md">
+      <p class="absolute min-w-36 z-10 mx-auto bg-box-grey mt-2 py-2 px-4 invisible group-hover:visible rounded-md">
         {{ item.description }}
       </p>
     </div>
