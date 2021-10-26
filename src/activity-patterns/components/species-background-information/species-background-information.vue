3166e73ebd954abac13e90d8365d8c8c2545c4fe
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-10-26 15:28:45 +0700

    [Refactor] Extract species-selector

--- src/activity-patterns/components/species-background-information/species-background-information.vue
+++ src/activity-patterns/components/species-background-information/species-background-information.vue
@@ -1,27 +1,27 @@
 <template>
   <!-- TODO #188 #189 Handle loading and error case -->
   <div
-    v-if="!isLoading && speciesInformation"
+    v-if="speciesInformation"
     class="grid grid-cols-4"
   >
-    <div class="col-span-3 px-4 py-2">
+    <div class="col-span-3 pr-6">
       <p>
         {{ speciesInformation?.content }}
-      </p>
-      <p class="mt-4 text-right">
-        Source:
-        <a
-          :href="speciesWikiUrl"
-          target="_blank"
-          class="opacity-80 hover:(underline opacity-70)"
-        >
-          Wikipedia
-        </a>
+        <span class="mt-2 text-right text-subtle">
+          &mdash; <a
+            :href="speciesWikiUrl"
+            target="_blank"
+            class="opacity-80 hover:(underline opacity-70)"
+          >
+            Source: Wikipedia
+          </a>
+        </span>
       </p>
     </div>
-    <div class="col-span-1 place-self-center">
+    <div class="col-span-1">
       <img
         :src="speciesImage(speciesInformation?.thumbnailImage)"
+        :alt="species.speciesName"
         class="object-contain max-h-50"
       >
     </div>
---
+++ src/activity-patterns/components/species-selector/species-selector.vue
@@ -0,0 +1,15 @@
+<template>
+  <select
+    v-model="selectedSpeciesSlug"
+    class="capitalize bg-steel-grey rounded p-2 my-6 focus:(border-box-grey ring-0 outline-none) min-w-64"
+  >
+    <option
+      v-for="species in allSpecies"
+      :key="'species-selector-' + species.speciesId"
+      :value="species.speciesSlug"
+    >
+      {{ species.speciesName }}
+    </option>
+  </select>
+</template>
+<script src="./species-selector.ts" lang="ts"></script>
