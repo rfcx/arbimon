4866d6512cbf8ada9136c83bb4bacfee6cb7a179
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-10-26 22:09:03 +0700

    [130] Test & fix siteCount, detectionCount, detectionFrequency

--- src/_services/api/activity-patterns-service/activity-patterns-service-mock.test.ts
+++ src/_services/api/activity-patterns-service/activity-patterns-service-mock.test.ts
@@ -4,32 +4,40 @@ import { ApiDetection } from '~/api-helpers/mock'
 import { dayjs } from '~/dayjs'
 
 const MOCK_RECORDINGS_PER_HOUR = 12
-const EXAMPLE_SITE: Site = { siteId: '123', name: '', longitude: 0, latitude: 0 }
+
+const EXAMPLE_SITE_IDS = ['123', '456', '789']
+const EXAMPLE_SITES: Site[] = EXAMPLE_SITE_IDS.map(siteId => ({ siteId, name: '', longitude: 0, latitude: 0 }))
 const EXAMPLE_DATE = '2021-04-02T00:00:00.000Z'
+
+const EXAMPLE_SPECIES_IDS = [123, 456, 789]
+const EXAMPLE_SPECIES_ID_NOT_INTERESTED = EXAMPLE_SPECIES_IDS[0]
+const EXAMPLE_SPECIES_ID_NOT_INTERESTED2 = EXAMPLE_SPECIES_IDS[1]
+const EXAMPLE_SPECIES_ID_INTERESTED = EXAMPLE_SPECIES_IDS[3]
+
 const EXAMPLE_DATASET: DatasetDefinition = {
   start: EXAMPLE_DATE,
   end: dayjs(EXAMPLE_DATE).add(1, 'day').toString(),
-  sites: [EXAMPLE_SITE]
+  sites: EXAMPLE_SITES
 }
-const EXAMPLE_SPECIES_ID = 123
 
 const EMPTY_DETECTION: ApiDetection = {
   arbimon_site_id: 0,
-  stream_id: EXAMPLE_SITE.siteId,
+  stream_id: EXAMPLE_SITES[0].siteId,
   name: '',
   lat: 0,
   lon: 0,
   date: EXAMPLE_DATE,
   hour: 0,
-  species_id: EXAMPLE_SPECIES_ID,
+  species_id: EXAMPLE_SPECIES_ID_NOT_INTERESTED,
   scientific_name: '',
   taxon_id: 0,
   taxon: '',
-  detection_frequency: 0
+  num_of_recordings: 1,
+  detection_frequency: 1 / 12
 }
 
 describe('AP Service Mock', () => {
-  const detectionsToRecordingCount: Array<[number, ApiDetection[]]> = [
+  const totalDetectionsToRecordingCount: Array<[number, ApiDetection[]]> = [
     [MOCK_RECORDINGS_PER_HOUR, [
       { ...EMPTY_DETECTION, hour: 0 },
       { ...EMPTY_DETECTION, hour: 0 },
@@ -40,18 +48,94 @@ describe('AP Service Mock', () => {
       { ...EMPTY_DETECTION, hour: 0 },
       { ...EMPTY_DETECTION, hour: 1 }
     ]],
-    [2 * MOCK_RECORDINGS_PER_HOUR, [
-      { ...EMPTY_DETECTION, hour: 0, species_id: EXAMPLE_SPECIES_ID },
-      { ...EMPTY_DETECTION, hour: 0, species_id: EXAMPLE_SPECIES_ID + 1 },
-      { ...EMPTY_DETECTION, hour: 1, species_id: EXAMPLE_SPECIES_ID + 1 }
+    [3 * MOCK_RECORDINGS_PER_HOUR, [
+      { ...EMPTY_DETECTION, hour: 0, species_id: EXAMPLE_SPECIES_ID_INTERESTED },
+      { ...EMPTY_DETECTION, hour: 0, species_id: EXAMPLE_SPECIES_ID_NOT_INTERESTED },
+      { ...EMPTY_DETECTION, hour: 1, species_id: EXAMPLE_SPECIES_ID_NOT_INTERESTED },
+      { ...EMPTY_DETECTION, hour: 2, species_id: EXAMPLE_SPECIES_ID_INTERESTED }
     ]]
   ]
-  test.each(detectionsToRecordingCount)('calculate totalRecordingCount: %s', async (expected, detections) => {
+  test.each(totalDetectionsToRecordingCount)('calculate totalRecordingCount: %s', async (expected, detections) => {
     // Act
-    const sut = new ActivityPatternsService(detections)
-    const result = await sut.getActivityPatternsData(EXAMPLE_DATASET, EXAMPLE_SPECIES_ID)
+    const sut = new ActivityPatternsService(detections, 0)
+    const result = await sut.getActivityPatternsData(EXAMPLE_DATASET, EXAMPLE_SPECIES_ID_INTERESTED)
 
     // Assert
     expect(result.totalRecordingCount).toEqual(expected)
+  })
+
+  const totalDetectionsToSiteCount: Array<[number, ApiDetection[]]> = [
+    [1, [
+      { ...EMPTY_DETECTION, stream_id: EXAMPLE_SITES[0].siteId },
+      { ...EMPTY_DETECTION, stream_id: EXAMPLE_SITES[0].siteId },
+      { ...EMPTY_DETECTION, stream_id: EXAMPLE_SITES[0].siteId }
+    ]],
+    [3, [
+      { ...EMPTY_DETECTION, stream_id: EXAMPLE_SITES[0].siteId },
+      { ...EMPTY_DETECTION, stream_id: EXAMPLE_SITES[1].siteId },
+      { ...EMPTY_DETECTION, stream_id: EXAMPLE_SITES[2].siteId }
+    ]]
+  ]
+  test.each(totalDetectionsToSiteCount)('calculate totalSiteCount: %s', async (expected, detections) => {
+    // Act
+    const sut = new ActivityPatternsService(detections, 0)
+    const result = await sut.getActivityPatternsData(EXAMPLE_DATASET, EXAMPLE_SPECIES_ID_INTERESTED)
+
+    // Assert
+    expect(result.totalSiteCount).toEqual(expected)
+  })
+
+  const totalDetectionsToDetectionCount: Array<[number, ApiDetection[]]> = [
+    [3, [
+      { ...EMPTY_DETECTION, species_id: EXAMPLE_SPECIES_ID_INTERESTED, hour: 0, num_of_recordings: 1 },
+      { ...EMPTY_DETECTION, species_id: EXAMPLE_SPECIES_ID_INTERESTED, hour: 1, num_of_recordings: 2 },
+      { ...EMPTY_DETECTION, species_id: EXAMPLE_SPECIES_ID_NOT_INTERESTED, hour: 1, num_of_recordings: 2 }
+    ]],
+    [10, [
+      { ...EMPTY_DETECTION, species_id: EXAMPLE_SPECIES_ID_INTERESTED, hour: 0, num_of_recordings: 1 },
+      { ...EMPTY_DETECTION, species_id: EXAMPLE_SPECIES_ID_INTERESTED, hour: 1, num_of_recordings: 2 },
+      { ...EMPTY_DETECTION, species_id: EXAMPLE_SPECIES_ID_INTERESTED, hour: 2, num_of_recordings: 3 },
+      { ...EMPTY_DETECTION, species_id: EXAMPLE_SPECIES_ID_INTERESTED, hour: 3, num_of_recordings: 4 },
+      { ...EMPTY_DETECTION, species_id: EXAMPLE_SPECIES_ID_NOT_INTERESTED, hour: 0, num_of_recordings: 1 },
+      { ...EMPTY_DETECTION, species_id: EXAMPLE_SPECIES_ID_NOT_INTERESTED, hour: 1, num_of_recordings: 2 },
+      { ...EMPTY_DETECTION, species_id: EXAMPLE_SPECIES_ID_NOT_INTERESTED, hour: 2, num_of_recordings: 3 },
+      { ...EMPTY_DETECTION, species_id: EXAMPLE_SPECIES_ID_NOT_INTERESTED2, hour: 0, num_of_recordings: 4 },
+      { ...EMPTY_DETECTION, species_id: EXAMPLE_SPECIES_ID_NOT_INTERESTED2, hour: 1, num_of_recordings: 5 }
+    ]]
+  ]
+  test.each(totalDetectionsToDetectionCount)('calculate detectionCount: %s', async (expected, detections) => {
+    // Act
+    const sut = new ActivityPatternsService(detections, 0)
+    const result = await sut.getActivityPatternsData(EXAMPLE_DATASET, EXAMPLE_SPECIES_ID_INTERESTED)
+
+    // Assert
+    expect(result.detectionCount).toEqual(expected)
+  })
+
+  const totalDetectionsToDetectionFrequency: Array<[number, ApiDetection[]]> = [
+    [3 / (2 * MOCK_RECORDINGS_PER_HOUR), [
+      { ...EMPTY_DETECTION, species_id: EXAMPLE_SPECIES_ID_INTERESTED, hour: 0, num_of_recordings: 1 },
+      { ...EMPTY_DETECTION, species_id: EXAMPLE_SPECIES_ID_INTERESTED, hour: 1, num_of_recordings: 2 },
+      { ...EMPTY_DETECTION, species_id: EXAMPLE_SPECIES_ID_NOT_INTERESTED, hour: 1, num_of_recordings: 2 }
+    ]],
+    [10 / (4 * MOCK_RECORDINGS_PER_HOUR), [
+      { ...EMPTY_DETECTION, species_id: EXAMPLE_SPECIES_ID_INTERESTED, hour: 0, num_of_recordings: 1 },
+      { ...EMPTY_DETECTION, species_id: EXAMPLE_SPECIES_ID_INTERESTED, hour: 1, num_of_recordings: 2 },
+      { ...EMPTY_DETECTION, species_id: EXAMPLE_SPECIES_ID_INTERESTED, hour: 2, num_of_recordings: 3 },
+      { ...EMPTY_DETECTION, species_id: EXAMPLE_SPECIES_ID_INTERESTED, hour: 3, num_of_recordings: 4 },
+      { ...EMPTY_DETECTION, species_id: EXAMPLE_SPECIES_ID_NOT_INTERESTED, hour: 0, num_of_recordings: 1 },
+      { ...EMPTY_DETECTION, species_id: EXAMPLE_SPECIES_ID_NOT_INTERESTED, hour: 1, num_of_recordings: 2 },
+      { ...EMPTY_DETECTION, species_id: EXAMPLE_SPECIES_ID_NOT_INTERESTED, hour: 2, num_of_recordings: 3 },
+      { ...EMPTY_DETECTION, species_id: EXAMPLE_SPECIES_ID_NOT_INTERESTED2, hour: 0, num_of_recordings: 4 },
+      { ...EMPTY_DETECTION, species_id: EXAMPLE_SPECIES_ID_NOT_INTERESTED2, hour: 1, num_of_recordings: 5 }
+    ]]
+  ]
+  test.each(totalDetectionsToDetectionFrequency)('calculate detectionFrequency: %s', async (expected, detections) => {
+    // Act
+    const sut = new ActivityPatternsService(detections, 0)
+    const result = await sut.getActivityPatternsData(EXAMPLE_DATASET, EXAMPLE_SPECIES_ID_INTERESTED)
+
+    // Assert
+    expect(result.detectionFrequency).toEqual(expected)
   })
 })
