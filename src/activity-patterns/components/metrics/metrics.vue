7a7620a22ccb7e12462e7765464c7038caabe3b4
Somjintana Korbut <nuisomjin@gmail.com>
2021-10-26 14:47:23 +0100

    [130] change border color

--- src/activity-patterns/components/metrics/metrics.vue
+++ src/activity-patterns/components/metrics/metrics.vue
@@ -4,7 +4,7 @@
       v-for="(item, idx) in metrics"
       :key="'metrics-info-' + idx"
     >
-      <div class="px-6 py-4 border-r-2 last:border-none">
+      <div class="px-6 py-4 border-r-1 border-subtle last:border-none">
         <metrics-title
           :title="item.title"
           :information="item.information"
