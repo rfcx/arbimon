fbf1b79d111d7c926f8a09ab7e99f2385faca19e
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-11-04 20:24:32 +0700

    [193] Update sp page all detection

--- packages/website/src/species-richness/components/species-richness-by-location/functions.ts
+++ packages/website/src/species-richness/components/species-richness-by-location/functions.ts
@@ -1,6 +1,7 @@
+import { TAXONOMY_CLASS_ALL } from '~/api/taxonomy-service'
 import { MapSiteData } from '~/maps/map-bubble'
 
 export const generateHtmlPopup = (datum: MapSiteData, _?: string): string => {
-  const speciesCounts = Object.keys(datum.distinctSpecies).sort().map(key => `${key}: ${datum.distinctSpecies[key].toString()}`)
+  const speciesCounts = Object.keys(datum.distinctSpecies).sort().filter(d => d !== TAXONOMY_CLASS_ALL.name).map(key => `${key}: ${datum?.distinctSpecies[key].toString()}`)
   return `<strong>${datum.siteName}</strong><p>${speciesCounts.join('<br />')}</p>`
 }
