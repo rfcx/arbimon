e46a9ccb2ca8a326869d6ac298c09675626669f4
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-11-05 19:31:23 +0700

    [194] Add div to avoid vue warning lint

--- packages/website/src/activity-patterns/activity-patterns.vue
+++ packages/website/src/activity-patterns/activity-patterns.vue
@@ -1,32 +1,34 @@
 <template>
-  <page-title
-    page-title="Activity Patterns"
-    page-subtitle="Trends in detections and occupancy"
-  >
-    <dropdown-menu>
-      <dropdown-menu-item>
-        <icon-fas-hammer class="mr-2" /> TODO
-      </dropdown-menu-item>
-    </dropdown-menu>
-  </page-title>
-  <comparison-list-component
-    class="mt-5"
-    @emit-select="onFilterChange"
-  />
-  <species-selector
-    :species-slug="$route.params.speciesSlug"
-    @emit-selected-species-changed="onSelectedSpeciesChange"
-  />
-  <species-background-information
-    :species="species"
-  />
-  <activity-patterns-metrics
-    :metrics="metrics"
-    class="mt-5"
-  />
-  <activity-patterns-by-location
-    :datasets="mapDatasets"
-    class="mt-5"
-  />
+  <div>
+    <page-title
+      page-title="Activity Patterns"
+      page-subtitle="Trends in detections and occupancy"
+    >
+      <dropdown-menu>
+        <dropdown-menu-item>
+          <icon-fas-hammer class="mr-2" /> TODO
+        </dropdown-menu-item>
+      </dropdown-menu>
+    </page-title>
+    <comparison-list-component
+      class="mt-5"
+      @emit-select="onFilterChange"
+    />
+    <species-selector
+      :species-slug="$route.params.speciesSlug"
+      @emit-selected-species-changed="onSelectedSpeciesChange"
+    />
+    <species-background-information
+      :species="species"
+    />
+    <activity-patterns-metrics
+      :metrics="metrics"
+      class="mt-5"
+    />
+    <activity-patterns-by-location
+      :datasets="mapDatasets"
+      class="mt-5"
+    />
+  </div>
 </template>
 <script src="./activity-patterns.ts" lang="ts"></script>
