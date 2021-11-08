5ca78a25ea789d3056f947cc8bc6b390725cc84d
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-11-08 12:51:34 +0700

    [194] Update the absent color to be darker and smaller radius

--- packages/website/src/_services/maps/map-bubble/map-bubble.ts
+++ packages/website/src/_services/maps/map-bubble/map-bubble.ts
@@ -11,8 +11,8 @@ import { MapDataSet, MapSiteData } from '.'
 const DATA_LAYER_ID = 'species-information'
 const LABEL_LAYER_IDS = ['tunnel-primary-secondary-tertiary-case', 'tunnel-major-link-case', 'tunnel-motorway-trunk-case', 'tunnel-path', 'tunnel-steps', 'tunnel-major-link', 'tunnel-pedestrian', 'tunnel-primary-secondary-tertiary', 'tunnel-oneway-arrow-blue', 'tunnel-motorway-trunk', 'tunnel-oneway-arrow-white', 'ferry', 'ferry-auto', 'road-pedestrian-case', 'road-street-low', 'road-street-case', 'road-secondary-tertiary-case', 'road-primary-case', 'road-major-link-case', 'road-motorway-trunk-case', 'road-path', 'road-steps', 'road-major-link', 'road-pedestrian', 'road-street', 'road-secondary-tertiary', 'road-primary', 'road-oneway-arrow-blue', 'road-motorway-trunk', 'road-oneway-arrow-white', 'bridge-pedestrian-case', 'bridge-primary-secondary-tertiary-case', 'bridge-major-link-case', 'bridge-motorway-trunk-case', 'bridge-path', 'bridge-steps', 'bridge-major-link', 'bridge-pedestrian', 'bridge-primary-secondary-tertiary', 'bridge-oneway-arrow-blue', 'bridge-motorway-trunk', 'bridge-major-link-2-case', 'bridge-motorway-trunk-2-case', 'bridge-major-link-2', 'bridge-motorway-trunk-2', 'bridge-oneway-arrow-white', 'aerialway', 'admin-1-boundary-bg', 'admin-0-boundary-bg', 'admin-1-boundary', 'admin-0-boundary', 'admin-0-boundary-disputed', 'road-label', 'road-number-shield', 'road-exit-shield', 'waterway-label', 'natural-line-label', 'natural-point-label', 'water-line-label', 'water-point-label', 'poi-label', 'transit-label', 'airport-label', 'settlement-subdivision-label', 'settlement-label', 'state-label', 'country-label']
 
-const DEFAULT_RADIUS = 5
 const DEFAULT_BOOLEAN_RADIUS = 2
+const DEFAULT_COLOR = '#9E9E9E'
 
 export default class MapBubbleComponent extends Vue {
   @Prop() mapId!: string
@@ -92,7 +92,11 @@ export default class MapBubbleComponent extends Vue {
       return (belongRange + 1) * ratio
     }
 
-    return (typeof value === 'boolean') ? DEFAULT_BOOLEAN_RADIUS : Math.sqrt(value)
+    if (typeof value === 'boolean') {
+      return value ? DEFAULT_BOOLEAN_RADIUS : Math.sqrt(DEFAULT_BOOLEAN_RADIUS)
+    }
+
+    return Math.sqrt(value)
   }
 
   getPopup (datum: MapSiteData): string {
@@ -105,7 +109,7 @@ export default class MapBubbleComponent extends Vue {
       return this.dataset.color
     }
 
-    return value ? this.dataset.color : '#CFCFCF'
+    return value ? this.dataset.color : DEFAULT_COLOR
   }
 
   setupMapPopup (): void {
@@ -181,7 +185,7 @@ export default class MapBubbleComponent extends Vue {
         source: DATA_LAYER_ID,
         paint: {
           'circle-radius': ['*', ['get', 'radius'], 4], // TODO 41 - Normalize circle size
-          'circle-color': ['string', ['get', 'color'], '#CFCFCF'],
+          'circle-color': ['string', ['get', 'color'], DEFAULT_COLOR],
           'circle-stroke-width': 0.1,
           'circle-opacity': 0.6
         }
