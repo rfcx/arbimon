d5c7314d7c7b80a64fa05957b1b9cb5710fb50cb
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-11-08 22:19:53 +0700

    [210] Run lint in parallel; Fix prettier/stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   extends: ['stylelint-config-recommended-scss'],
-  ignoreFiles: ['node_modules', 'build', 'dist', 'lib', '.*/**/*', 'cypress', 'coverage'],
   rules: {
     'color-no-hex': null
   }
