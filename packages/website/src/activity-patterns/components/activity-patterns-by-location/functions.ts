a03a3b45b37f6f74c5995a8dbbf727b5f4fd9b73
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-11-10 17:28:07 +0700

    [193] PR review updates
    - Circles radii scale continuously with value
    - Fix normalization
    - Improve HTML popup text
    - Make min/max circle radii props
    - Split "zero" & "non-zero" layers (so that we can put "non-zero" on top
    - Darker higher opacity "zero" circles; tweak stroke-widths
    - Color driven by dataset (not embedded in individual pins)

--- packages/website/src/activity-patterns/components/activity-patterns-by-location/functions.ts
+++ packages/website/src/activity-patterns/components/activity-patterns-by-location/functions.ts
@@ -1,8 +1,15 @@
 import { ACTIVITY_PATTERN_KEYS } from '@/activity-patterns/functions'
 import { MapSiteData } from '~/maps/map-bubble'
 
+const getFormattedValue = (value: number | boolean, dataKey: string): string => {
+  switch (dataKey) {
+    case ACTIVITY_PATTERN_KEYS.detection: return (value as number).toString()
+    case ACTIVITY_PATTERN_KEYS.detectionFrequency: return (value as number).toFixed(3)
+    default: return (value as boolean) ? 'Detected' : 'Not detected'
+  }
+}
+
 export const generateDetectionHtmlPopup = (datum: MapSiteData, dataKey: string): string => {
   const value = datum.distinctSpecies[dataKey]
-  const info = dataKey === ACTIVITY_PATTERN_KEYS.detectionFrequency && !(typeof value === 'boolean') ? `${value.toFixed(2)}%` : value.toString()
-  return `<strong>${datum.siteName}: </strong><span>${info}</span>`
+  return `<strong>${datum.siteName}: </strong><span>${getFormattedValue(value, dataKey)}</span>`
 }
