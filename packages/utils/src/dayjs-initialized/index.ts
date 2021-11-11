825c479e6931cd700904a239492c3dc45656b8fc
Antony Harfield <antonyharfield@gmail.com>
2021-11-11 12:02:29 +0700

    [210] Re-enable es module interop

--- packages/utils/src/dayjs-initialized/index.ts
+++ packages/utils/src/dayjs-initialized/index.ts
@@ -1,9 +1,9 @@
 // eslint-disable-next-line regex/invalid -- this is the one place we import & configure dayjs
-import * as dayjsBase from 'dayjs'
-import * as minMax from 'dayjs/plugin/minMax'
-import * as pluralGetSet from 'dayjs/plugin/pluralGetSet'
-import * as quarterOfYear from 'dayjs/plugin/quarterOfYear'
-import * as utc from 'dayjs/plugin/utc'
+import dayjsBase from 'dayjs'
+import minMax from 'dayjs/plugin/minMax'
+import pluralGetSet from 'dayjs/plugin/pluralGetSet'
+import quarterOfYear from 'dayjs/plugin/quarterOfYear'
+import utc from 'dayjs/plugin/utc'
 
 dayjsBase.extend(utc)
 dayjsBase.extend(quarterOfYear)
