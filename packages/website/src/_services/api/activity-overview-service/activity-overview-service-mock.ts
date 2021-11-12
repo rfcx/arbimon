5def86e3ef7eea1dfd1c40f1110be81bbb49472f
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-11-12 15:28:40 +0700

    [257] add logic for calculating detection frequency by site

--- packages/website/src/_services/api/activity-overview-service/activity-overview-service-mock.ts
+++ packages/website/src/_services/api/activity-overview-service/activity-overview-service-mock.ts
@@ -1,7 +1,8 @@
-import { groupBy } from 'lodash'
+import { groupBy, mapValues, sum } from 'lodash'
 
 import { DatasetDefinition } from '~/api'
-import { ApiHourlySpeciesSummary, filterByDataset, getRawDetections } from '~/api-helpers/mock'
+import { ActicvityOverviewDataBySite, ActivityOverviewData, DetectionGroupByTaxonClass, DetectionGroupedBySiteAndTaxon } from '~/api/activity-overview-service'
+import { ApiHourlySpeciesSummary, filterByDataset, getRawDetections, simulateDelay } from '~/api-helpers/mock'
 
 export class ActivityOverviewService {
   constructor (
@@ -9,9 +10,41 @@ export class ActivityOverviewService {
     private readonly delay: number | undefined = undefined
   ) {}
 
-  async getActivityOverviewData (dataset: DatasetDefinition): Promise<void> {
-    const totalDetections = filterByDataset(this.rawHourlySpeciesSummaries, dataset)
-    const detectionsByTaxon = groupBy(totalDetections, 'taxon')
+  async getActivityOverviewData (dataset: DatasetDefinition): Promise<ActivityOverviewData> {
+    const totalSummaries = filterByDataset(this.rawHourlySpeciesSummaries, dataset)
+    const detectionsByTaxon: DetectionGroupByTaxonClass = groupBy(totalSummaries, 'taxon')
+    const overviewBySite = await this.getOverviewDataBySite(detectionsByTaxon)
+
+    return await simulateDelay({ ...dataset, overviewBySite }, this.delay)
+  }
+
+  getRecordingCount (detections: ApiHourlySpeciesSummary[]): number {
+    return new Set(detections.map(d => `${d.date}-${d.hour}`)).size * 12
+  }
+
+  async getOverviewDataBySite (detection: DetectionGroupByTaxonClass): Promise<ActicvityOverviewDataBySite> {
+    const summariesEachTaxonBySite: DetectionGroupedBySiteAndTaxon = mapValues(detection, (detection) => {
+      const groupedSites = groupBy(detection, 'stream_id')
+      return groupedSites
+    })
+
+    return mapValues(summariesEachTaxonBySite, (siteWithDetections, siteId) => {
+      return mapValues(siteWithDetections, (detections) => {
+        const siteTotalRecordingCount = this.getRecordingCount(detections)
+
+        const siteDetectionCount = sum(detections.map(d => d.num_of_recordings))
+        const siteDetectionFrequency = siteTotalRecordingCount === 0 ? 0 : siteDetectionCount / siteTotalRecordingCount
+
+        return {
+          siteId,
+          siteName: detections[0].name,
+          latitude: detections[0].lat,
+          longitude: detections[0].lon,
+          detection: siteDetectionCount,
+          detectionFrequency: siteDetectionFrequency
+        }
+      })
+    })
   }
 }
 
--- packages/website/src/_services/api/activity-overview-service/types.ts
+++ packages/website/src/_services/api/activity-overview-service/types.ts
@@ -1,5 +1,29 @@
 import { DatasetDefinition } from '~/api'
+import { ApiHourlySpeciesSummary } from '~/api-helpers/mock'
+
+export interface DetectionGroupedBySiteAndTaxon {
+  [taxon: string]: {
+    [siteId: string]: ApiHourlySpeciesSummary[]
+  }
+}
+
+export interface DetectionGroupByTaxonClass {
+  [taxon: string]: ApiHourlySpeciesSummary[]
+}
 
 export interface ActivityOverviewData extends DatasetDefinition {
-  //
+  overviewBySite: ActicvityOverviewDataBySite
+}
+
+export interface ActicvityOverviewDataBySite {
+  [taxon: string]: {
+    [siteId: string]: {
+      siteId: string
+      siteName: string
+      latitude: number
+      longitude: number
+      detection: number
+      detectionFrequency: number
+    }
+  }
 }
