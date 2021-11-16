c22269d686136e2dc6babd550cb47e486e367a6f
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-11-16 11:00:32 +0700

    [285] Add API URL to ENV

--- packages/website/src/_services/vue/vite-env.d.ts
+++ packages/website/src/_services/vue/vite-env.d.ts
@@ -1,1 +1,9 @@
 import 'vite/client'
+
+interface ImportMetaEnv extends Readonly<Record<string, string>> {
+  readonly VITE_BIO_API_HOST: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
