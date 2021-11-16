d9aee8e4e0f69433ea606e0d1a363089ff0468d6
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-11-16 11:19:40 +0700

    [285] Fix vite ENV types

--- packages/website/src/_services/vue/vite-env.d.ts
+++ packages/website/src/_services/vue/vite-env.d.ts
@@ -1,9 +1,7 @@
 import 'vite/client'
 
-interface ImportMetaEnv extends Readonly<Record<string, string>> {
-  readonly VITE_BIO_API_HOST: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
+declare global {
+  interface ImportMetaEnv extends Readonly<Record<string, string>> {
+    readonly VITE_BIO_API_HOST: string
+  }
 }
