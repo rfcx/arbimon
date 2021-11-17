8af643a68ac91a5cd998e559bfc05f436ee58f2d
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-11-17 13:39:33 +0700

    Merged conflict

--- packages/website/src/activity-patterns/components/species-background-information/species-background-information.vue
+++ packages/website/src/activity-patterns/components/species-background-information/species-background-information.vue
@@ -1,7 +1,6 @@
 <template>
   <!-- TODO #188 #189 Handle loading and error case -->
   <div
-<<<<<<< HEAD
     v-if="iucnSpeciesInformation || wikiSpeciesInformation"
     class="grid grid-cols-6"
   >
@@ -39,31 +38,6 @@
           >{{ species?.speciesName }}</a>
         </div>
       </div>
-=======
-    v-if="speciesInformation"
-    class="flex gap-10"
-  >
-    <div>
-      <p>
-        {{ speciesInformation?.content }}
-        <span class="mt-2 text-subtle">
-          &mdash; <a
-            :href="speciesWikiUrl"
-            target="_blank"
-            class="opacity-80 hover:(underline opacity-70)"
-          >
-            Source: Wikipedia
-          </a>
-        </span>
-      </p>
-    </div>
-    <div>
-      <img
-        :src="speciesImage(speciesInformation?.thumbnailImage)"
-        :alt="species?.speciesName ?? ''"
-        class="object-contain min-w-40 md:min-w-50 lg:min-w-60"
-      >
->>>>>>> develop
     </div>
   </div>
 </template>
