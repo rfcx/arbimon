f592aa7ae9dea8ebd26fb2a06400802c4eb1567f
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-11-17 22:16:58 +0700

    [294] Clean up iucn code in website part

--- packages/website/src/_services/api/iucn-service/types.ts
+++ packages/website/src/_services/api/iucn-service/types.ts
@@ -2,21 +2,3 @@ export interface IUCNSummary {
   content: string
   redirectUrl: string
 }
-
-export interface IUCNNarrativeInfoResponse {
-  name: string
-  result: IUCNNarrativeInfoResultResponse[]
-}
-
-export interface IUCNNarrativeInfoResultResponse {
-  species_id: number
-  taxonomicnotes: string | null
-  rationale: string | null
-  geographicrange: string | null
-  population: string | null
-  populationtrend: string | null
-  habitat: string | null
-  threats: string | null
-  conservationmeasures: string | null
-  usetrade: string | null
-}
