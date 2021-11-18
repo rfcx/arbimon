f0b2aabd4f6fc589a7cd10cde321b65d8d829131
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-11-19 01:02:30 +0700

    [294] Specific error for missing param; Update PO-maps API to new style

--- packages/api/src/_services/errors/index.ts
+++ packages/api/src/_services/errors/index.ts
@@ -8,5 +8,8 @@ export const ApiServerError = (message = 'Server error'): FastifyError =>
 export const ApiClientError = (message = 'Invalid request'): FastifyError =>
   new FastifyError(message, 400)
 
+export const ApiMissingParam = (paramName: string): FastifyError =>
+  new FastifyError(`${paramName} is required`, 400)
+
 export const ApiNotFoundError = (message = 'Data not found'): FastifyError =>
   new FastifyError(message, 404)
--- packages/api/src/iucn/index.ts
+++ packages/api/src/iucn/index.ts
@@ -2,14 +2,14 @@ import { FastifyPluginAsync } from 'fastify'
 import { BioIucnSpeciesRequest, BioIucnSpeciesResponse } from 'iucn/types'
 
 import { env } from '../_services/env/index.js'
-import { ApiClientError, ApiNotFoundError, ApiServerError } from '../_services/errors/types.js'
+import { ApiMissingParam, ApiNotFoundError, ApiServerError } from '../_services/errors/index.js'
 import { getSpeciesInformation } from './iucn.js'
 
 export const routesIucn: FastifyPluginAsync = async (app, options): Promise<void> => {
   app.get<{ Params: Partial<BioIucnSpeciesRequest> } >('/iucn/info/:speciesName', async (req, res): Promise<BioIucnSpeciesResponse> => {
     // Input params
     const { speciesName } = req.params
-    if (!speciesName) throw ApiClientError('speciesName is required')
+    if (!speciesName) throw ApiMissingParam('speciesName')
 
     // Call IUCN API
     const iucnInformation = await getSpeciesInformation(speciesName)
