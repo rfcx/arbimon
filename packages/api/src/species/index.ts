2006d725aa3b02582b31bac0805457869d20c53a
naluinui <nuisomjin@gmail.com>
2021-11-18 14:42:36 +0700

    [198] add route GET `/species/:speciesName`

--- packages/api/src/species/index.ts
+++ packages/api/src/species/index.ts
@@ -16,16 +16,32 @@ export const routesSpecies: FastifyPluginAsync = async (app, options): Promise<v
     return JSON.parse(rawSpeciesData).sort((a: { scientific_name: string }, b: { scientific_name: string }) => a.scientific_name.localeCompare(b.scientific_name))
   })
 
-  interface SpeciesRoute {
+  interface SpeciesIdRoute {
     Params: {
       speciesId?: number
     }
   }
 
-  app.get<SpeciesRoute>('/species/:speciesId', async (req, res) => {
+  interface SpeciesRoute {
+    Params: {
+      speciesName?: string
+    }
+  }
+
+  app.get<SpeciesIdRoute>('/species/id/:speciesId', async (req, res) => {
     // Inputs & validation
     const { speciesId } = req.params
     if (speciesId == null) return { error: 'missing speciesId' }
     return JSON.parse(rawSpeciesData).filter((s: { species_id: any }) => s.species_id === speciesId)
+  })
+
+  app.get<SpeciesRoute>('/species/:speciesName', async (req, res) => {
+    // Inputs & validation
+    const { speciesName } = req.params
+    if (!speciesName) return { error: 'missing speciesName' }
+    const matchesSpecies = JSON.parse(rawSpeciesData).filter((s: { scientific_name: any }) => s.scientific_name.search(speciesName) !== -1)
+
+    if (matchesSpecies.length === 0) return { error: 'illegal filename' }
+    return matchesSpecies[0]
   })
 }
