543d94d0b435c2bd846c7a50a3c5abd8bec03097
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-11-19 01:02:44 +0700

    [294] Minor improvements to frontend code

--- packages/website/src/_services/api/iucn-service/index.ts
+++ packages/website/src/_services/api/iucn-service/index.ts
@@ -1,6 +1,4 @@
 import { IUCNService } from '~/api/iucn-service/iucn-service-api'
 
-const BIO_API_HOST: string = import.meta.env.VITE_BIO_API_HOST
-
 export * from './types'
-export const iucnService = new IUCNService(BIO_API_HOST)
+export const iucnService = new IUCNService(import.meta.env.VITE_BIO_API_HOST)
