75c50ea4a49e98965a4888c79762806975811721
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-11-18 22:33:32 +0700

    [294] Hide implementation of Errors (behind helper functions)

--- packages/api/src/_services/errors/types.ts
+++ packages/api/src/_services/errors/types.ts
@@ -1,8 +1,12 @@
-export class APIError extends Error {
-  code = 500
-
-  constructor (message: string, code: number) {
-    super(message)
-    this.code = code
-  }
+class FastifyError extends Error {
+  constructor (message: string, public statusCode = 500) { super(message) }
 }
+
+export const ApiServerError = (message = 'Server error'): FastifyError =>
+  new FastifyError(message, 500)
+
+export const ApiClientError = (message = 'Invalid request'): FastifyError =>
+  new FastifyError(message, 400)
+
+export const ApiNotFoundError = (message = 'Data not found'): FastifyError =>
+  new FastifyError(message, 404)
