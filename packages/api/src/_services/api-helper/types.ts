47bc031200e5c1d38a3b922fb193b175cce1394a
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-11-18 22:49:10 +0700

    [294] Add comment for improve endpoint

--- packages/api/src/_services/api-helper/types.ts
+++ packages/api/src/_services/api-helper/types.ts
@@ -2,6 +2,7 @@ import { Method } from 'axios'
 
 export type RequestMethod = Method & ('GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE')
 
+// TODO ??? - Extract to utils
 export interface Endpoint {
   method: RequestMethod
   url: string
