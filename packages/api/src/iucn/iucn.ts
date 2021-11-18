f5e987d4f94de4b57fa49ea231945c903ff20796
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-11-18 22:34:34 +0700

    [294] Encapsulate internal types; Convert to return/throw style

--- packages/api/src/iucn/iucn.ts
+++ packages/api/src/iucn/iucn.ts
@@ -1,32 +1,53 @@
 import axios from 'axios'
 import * as dotenv from 'dotenv'
-import { IUCNNarrativeInfoResponse, IUCNNarrativeInfoResultResponse, IUCNSummary } from 'iucn/types'
+import { BioIucnSpeciesResponse } from 'iucn/types'
 
 import { Endpoint } from '../_services/api-helper/types'
-import { APIError } from '../_services/errors/types.js'
+
+interface IucnSpeciesNarrativeResult {
+  species_id: number
+  taxonomicnotes: string | null
+  rationale: string | null
+  geographicrange: string | null
+  population: string | null
+  populationtrend: string | null
+  habitat: string | null
+  threats: string | null
+  conservationmeasures: string | null
+  usetrade: string | null
+}
+
+interface IucnSpeciesNarrativeResponse {
+  name: string
+  result: IucnSpeciesNarrativeResult[]
+}
 
 dotenv.config()
 
-const TOKEN = process.env.IUCN_TOKEN ?? ''
+const IUCN_TOKEN = process.env.IUCN_TOKEN ?? ''
 const IUCN_BASE_URL = process.env.IUCN_BASE_URL ?? ''
 
-export async function getSpeciesSummary (speciesName: string): Promise<IUCNSummary | undefined> {
+export async function getSpeciesSummary (speciesName: string): Promise<BioIucnSpeciesResponse | undefined> {
   const information = await getSpeciesInformation(speciesName)
+  if (!information) return undefined
 
-  if (!information) {
-    throw new APIError('Species not found', 404)
+  return {
+    content: information?.habitat ?? information?.rationale ?? '',
+    redirectUrl: `${IUCN_BASE_URL}/website/${speciesName}`
   }
-
-  const redirectUrl = `${IUCN_BASE_URL}/website/${speciesName}`
-  return { content: information?.habitat ?? information?.rationale ?? '', redirectUrl }
 }
 
-async function getSpeciesInformation (speciesName: string): Promise<IUCNNarrativeInfoResultResponse | undefined> {
+async function getSpeciesInformation (speciesName: string): Promise<IucnSpeciesNarrativeResult | undefined> {
   const endpoint: Endpoint = ({
     method: 'GET',
-    url: `${IUCN_BASE_URL}/species/narrative/${speciesName}?token=${TOKEN}`
+    url: `${IUCN_BASE_URL}/species/narrative/${speciesName}?token=${IUCN_TOKEN}`
   })
 
-  const { data } = await axios.request<IUCNNarrativeInfoResponse>(endpoint)
-  return data.result.length === 0 ? undefined : data.result[0]
+  try {
+    const { data } = await axios.request<IucnSpeciesNarrativeResponse>(endpoint)
+    return data.result.length === 0 ? undefined : data.result[0]
+  } catch (e) {
+    console.error(e)
+    return undefined
+  }
 }
