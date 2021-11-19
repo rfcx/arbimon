64f299a3128e0dbcdc09f2c3e7f8b3bcc1ba2818
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-11-19 17:38:30 +0700

    [overview] Add map component to the overview page

---
+++ packages/website/src/overview/overview-location/overview-location.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full h-96">
+    <map-bubble-component
+      key="overview-location"
+      :dataset="siteMapDataset"
+      data-key="site"
+      map-id="overview-by-location"
+      map-export-name="overview-by-location"
+      :get-popup-html="getPopupHtml"
+      :map-config="config"
+      class="w-full"
+      @emit-map-moved="mapMoved"
+    />
+  </div>
+</template>
+<script lang="ts" src="./overview-location.ts"></script>
--- packages/website/src/overview/overview.ts
+++ packages/website/src/overview/overview.ts
@@ -1,8 +1,14 @@
-import { Vue } from 'vue-class-component'
+import { Options, Vue } from 'vue-class-component'
 import { Inject } from 'vue-property-decorator'
 
 import { BiodiversityStore } from '~/store'
+import OverviewLocationComponent from './overview-location/overview-location.vue'
 
+@Options({
+  components: {
+    OverviewLocationComponent
+  }
+})
 export default class OverviewPage extends Vue {
   @Inject() readonly store!: BiodiversityStore
 }
