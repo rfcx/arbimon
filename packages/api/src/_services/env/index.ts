f4b2de1f8db8d64517a4490a641ce7d5ce9fa7bc
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-11-19 14:54:45 +0700

    [debug] remove console debug

--- packages/api/src/_services/env/index.ts
+++ packages/api/src/_services/env/index.ts
@@ -7,8 +7,6 @@ dotenv.config()
 export type Env = Record<typeof envKeysRequired[number], string> & Record<typeof envKeysOptional[number], string | undefined>
 export const env = process.env as Env
 
-console.debug('Check env debugging => ', env)
-
 envKeysRequired.forEach(key => {
   if (!env[key]) throw Error(`Missing required environment: ${key}`)
 })
