ccbce6c8ff67a24b78c6e0e2068a9e7237857a5d
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-11-23 18:19:23 +0700

    [219] Fix `maxResult` for "All" taxon class

--- packages/website/src/_services/api/taxonomy-service/taxonomy-service-mock.ts
+++ packages/website/src/_services/api/taxonomy-service/taxonomy-service-mock.ts
@@ -1,11 +1,10 @@
 import { TaxonomyClass } from '../types'
 
 export const TAXONOMY_CLASS_ALL: TaxonomyClass = { name: 'All', symbol: 'Σ' }
+export const TAXONOMY_UNKNOWN_CLASS: TaxonomyClass = { name: 'unknown', symbol: '❓' }
 
 export const TAXONOMY_CLASSES: TaxonomyClass[] = [
   { name: 'Amphibians', symbol: '🐸' },
   { name: 'Birds', symbol: '🐦' },
   { name: 'Mammals', symbol: '🐗' }
 ]
-
-export const TAXONOMY_UNKNOWN_CLASS: TaxonomyClass = { name: 'unknown', symbol: '❓' }
