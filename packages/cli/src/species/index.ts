48a98c855cff6490fe05aa62a1994ad243ec48df
naluinui <nuisomjin@gmail.com>
2021-11-23 13:18:10 +0700

    [198] move code for generating mock data of species to cli

---
+++ packages/cli/src/species/index.ts
@@ -0,0 +1,109 @@
+import * as dotenv from 'dotenv'
+import * as fs from 'fs'
+import { dirname, resolve } from 'path'
+
+import { getSpeciesInformation, getSpeciesRank } from './iucn/iucn.js'
+import { Species } from './types'
+import { getWikiSpeciesInformation } from './wiki/wiki.js'
+
+// Env
+dotenv.config()
+
+const currentDir = dirname(new URL(import.meta.url).pathname)
+const mockSpeciesPath = resolve(currentDir, './raw-species.json')
+const outputFilePath = resolve(currentDir, './species-with-information.json')
+
+// Ingest raw data
+const rawSpeciesStringOrBuffer = fs.readFileSync(mockSpeciesPath)
+const rawSpeciesData = Buffer.isBuffer(rawSpeciesStringOrBuffer)
+  ? rawSpeciesStringOrBuffer.toString()
+  : rawSpeciesStringOrBuffer
+
+const speciesData = JSON.parse(rawSpeciesData)
+const speciesWithWikiInfo = await getAllSpeciesDataWithWiki(speciesData)
+const speciesWithIUCNInfo = await getAllSpeciesDataWithIUCN(speciesWithWikiInfo)
+const speciesWithIUCNRank = await getAllSpeciesDataWithIUCNRank(speciesWithIUCNInfo)
+const json = JSON.stringify(speciesWithIUCNRank, null, 2)
+fs.writeFileSync(outputFilePath, json, 'utf8')
+
+export async function getAllSpeciesDataWithWiki (data: Species[]): Promise<Species[]> {
+  return await Promise.all(
+    data.map(async (datum: Species) => {
+      return await getSpeciesDataWithWiki(datum)
+    })
+  )
+}
+
+export async function getSpeciesDataWithWiki (species: Species): Promise<Species> {
+  const wikiInformation = await getWikiSpeciesInformation(species.scientific_name)
+    .catch(e => {
+      console.log('error Wiki', e, species.scientific_name)
+    })
+  const updatedData: Species = {
+    ...species,
+    information: {
+      description: wikiInformation?.content ?? '',
+      source_type: 'Wiki',
+      source_url: wikiInformation?.contentUrls.desktop
+    },
+    external_links: []
+  }
+  updatedData.external_links?.push({
+    title: 'Wiki',
+    source_type: 'Wiki',
+    source_url: wikiInformation?.contentUrls.desktop
+  })
+  return updatedData
+}
+
+export async function getAllSpeciesDataWithIUCN (data: Species[]): Promise<Species[]> {
+  return await Promise.all(
+    data.map(async (datum: Species) => {
+      return await getSpeciesDataWithIUCN(datum)
+    })
+  )
+}
+
+export async function getSpeciesDataWithIUCN (species: Species): Promise<Species> {
+  const iucnInformation = await getSpeciesInformation(species.scientific_name)
+    .catch(e => {
+      console.log('error IUCN', e, species.scientific_name)
+    })
+  const iucnDesc = (iucnInformation?.habitat ?? iucnInformation?.rationale ?? iucnInformation?.taxonomicnotes)
+  const updatedData = species
+  if (iucnDesc) {
+    updatedData.information = {
+      description: iucnDesc ?? '',
+      source_type: 'IUCN',
+      source_url: `${process.env.IUCN_BASE_URL ?? ''}/website/${species.scientific_name}`
+    }
+  }
+  updatedData.external_links?.push({
+    title: 'IUCN',
+    source_type: 'IUCN',
+    source_url: `${process.env.IUCN_BASE_URL ?? ''}/website/${species.scientific_name}`
+  })
+  return updatedData
+}
+
+export async function getAllSpeciesDataWithIUCNRank (data: Species[]): Promise<Species[]> {
+  return await Promise.all(
+    data.map(async (datum: Species) => {
+      return await getSpeciesDataWithIUCNRank(datum)
+    })
+  )
+}
+
+export async function getSpeciesDataWithIUCNRank (species: Species): Promise<Species> {
+  const iucnRank = await getSpeciesRank(species.scientific_name)
+    .catch(e => {
+      // Error: calling IUCN API
+      console.error('error IUCN rank:', e)
+    })
+  if (!iucnRank) return species
+  const updatedInfo = {
+    ...species,
+    iucn_rank: iucnRank
+  }
+  return updatedInfo
+}
---
+++ packages/cli/src/species/iucn/iucn.ts
@@ -0,0 +1,73 @@
+import axios from 'axios'
+import * as dotenv from 'dotenv'
+
+import { Endpoint } from '../../_services/api-helper/types'
+import { SpeciesCategory } from '../types'
+
+dotenv.config()
+
+const IUCN_BASE_URL = process.env.IUCN_BASE_URL ?? ''
+const IUCN_TOKEN = process.env.IUCN_TOKEN ?? ''
+
+export interface IucnSpeciesNarrativeResult {
+  species_id: number
+  taxonomicnotes: string | null
+  rationale: string | null
+  geographicrange: string | null
+  population: string | null
+  populationtrend: string | null
+  habitat: string | null
+  threats: string | null
+  conservationmeasures: string | null
+  usetrade: string | null
+}
+
+interface IucnSpeciesNarrativeResponse {
+  name?: string
+  result?: IucnSpeciesNarrativeResult[]
+}
+
+interface IucnSpeciesResponse {
+  name?: string
+  result?: IucnSpeciesResult[]
+}
+
+interface IucnSpeciesResult {
+  category: SpeciesCategory | undefined
+  criteria: string | null
+  marine_system: boolean | null
+  freshwater_system: boolean | null
+  terrestrial_system: boolean | null
+  aoo_km2: boolean | null
+  eoo_km2: boolean | null
+  elevation_upper: number | null
+  elevation_lower: number | null
+  depth_upper: number | null
+  depth_lower: number | null
+  assessor: string | null
+  reviewer: string | null
+  errata_flag: boolean | null
+  errata_reason: string | null
+  amended_flag: boolean | null
+  amended_reason: string | null
+}
+
+export async function getSpeciesInformation (speciesName: string): Promise<IucnSpeciesNarrativeResult | undefined> {
+  const endpoint: Endpoint = {
+    method: 'GET',
+    url: `${IUCN_BASE_URL}/species/narrative/${speciesName}?token=${IUCN_TOKEN}`
+  }
+
+  const { data } = await axios.request<IucnSpeciesNarrativeResponse>(endpoint)
+  return data?.result?.[0]
+}
+
+export async function getSpeciesRank (speciesName: string): Promise<SpeciesCategory | undefined> {
+  const endpoint: Endpoint = {
+    method: 'GET',
+    url: `${IUCN_BASE_URL}/species/${speciesName}?token=${IUCN_TOKEN}`
+  }
+
+  const { data } = await axios.request<IucnSpeciesResponse>(endpoint)
+  return data?.result?.[0].category
+}
