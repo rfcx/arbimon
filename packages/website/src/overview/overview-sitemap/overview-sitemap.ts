e41f8c9a0e9afea9e3b2e7080d5783065353cc4d
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-11-23 11:58:37 +0700

    [Maps] Fix import (fix build)

--- packages/website/src/overview/overview-sitemap/overview-sitemap.ts
+++ packages/website/src/overview/overview-sitemap/overview-sitemap.ts
@@ -1,8 +1,8 @@
 import { Options, Vue } from 'vue-class-component'
 import { Inject } from 'vue-property-decorator'
 
-import { DEFAULT_LATITUDE, DEFAULT_LONGITUDE, MapConfig } from '~/maps'
-import { MapBubbleComponent, MapDataSet } from '~/maps/map-bubble'
+import { DEFAULT_LATITUDE, DEFAULT_LONGITUDE } from '~/maps'
+import { MapBubbleComponent, MapConfig, MapDataSet } from '~/maps/map-bubble'
 import { BiodiversityStore } from '~/store'
 import { generatePopupHtml, transformToMapDataset } from './functions'
 
