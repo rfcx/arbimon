14a1fb6539902558246bc00d480146242647e639
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-11-24 18:46:21 +0700

    [Export] Function to calc legend entries

---
+++ packages/website/src/_services/maps/map-legend/index.test.ts
@@ -0,0 +1,29 @@
+import { getLegendSizes as sut } from './index'
+
+describe('getLegendSizes', () => {
+  test('Returns requested number of legend entries', () => {
+    const legendSizes = sut(10.0, 100, 5)
+    expect(legendSizes.length).toBe(5)
+  })
+
+  test('Returns empty array if maxValue is zero', () => {
+    const legendSizes = sut(10.0, 0, 5)
+    expect(legendSizes.length).toBe(0)
+  })
+
+  test('Largest legend entry is greater or equal to input values', () => {
+    // Arrange
+    const maxPixels = 100
+    const inputMaxValues = [0.1398, 1, 1.7, 5, 10, 37.1415, 123.123, 1923]
+
+    // Act
+    const results = inputMaxValues.map(inputMaxValue => sut(maxPixels, inputMaxValue, 5))
+
+    // Assert
+    results.forEach((result, i) => {
+      const { value: actualMaxValue, pixels: actualMaxPixels } = result[result.length - 1]
+      expect(actualMaxValue).toBeGreaterThanOrEqual(inputMaxValues[i])
+      expect(actualMaxPixels).toBeGreaterThanOrEqual(maxPixels)
+    })
+  })
+})
