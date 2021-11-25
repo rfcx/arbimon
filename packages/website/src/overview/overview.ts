207911356760548ce1e0036c58f0f508787b422a
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-11-25 15:46:24 +0700

    - Fix import error

--- packages/website/src/overview/overview.ts
+++ packages/website/src/overview/overview.ts
@@ -2,11 +2,13 @@ import { Options, Vue } from 'vue-class-component'
 import { Inject } from 'vue-property-decorator'
 
 import { BiodiversityStore } from '~/store'
+import OverviewMetrics from './components/overview-metrics/overview-metrics.vue'
 import OverviewSitemap from './components/overview-sitemap/overview-sitemap.vue'
 import ProjectInfo from './components/project-info/project-info.vue'
 
 @Options({
   components: {
+    OverviewMetrics,
     OverviewSitemap,
     ProjectInfo
   }
