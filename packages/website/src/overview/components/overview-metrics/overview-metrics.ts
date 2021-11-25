4e24bc842dac168db6274e0fe81966579779be86
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-11-25 18:32:54 +0700

    [303] Add dashboard page metrics mockup

--- packages/website/src/overview/components/overview-metrics/overview-metrics.ts
+++ packages/website/src/overview/components/overview-metrics/overview-metrics.ts
@@ -1,5 +1,13 @@
 import { Vue } from 'vue-class-component'
+import { Prop } from 'vue-property-decorator'
+
+interface ProjectMetrics {
+  detectionCount: number
+  siteCount: number
+  speciesCount: number
+  endangeredSpecies: number
+}
 
 export default class OverviewMetrics extends Vue {
-
+  @Prop() metrics!: ProjectMetrics
 }
--- packages/website/src/overview/components/overview-metrics/overview-metrics.vue
+++ packages/website/src/overview/components/overview-metrics/overview-metrics.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>
-    <numeric-metric
-      value="50000"
-      subtitle="nutto"
-    />
-  </div>
+  <numeric-metric
+    value="50000"
+    subtitle="detections"
+  />
+  <numeric-metric
+    class="ml-4"
+    value="200"
+    subtitle="sites"
+  />
+  <numeric-metric
+    class="ml-4"
+    value="10"
+    subtitle="endangered"
+    total-value="97"
+  />
 </template>
 <script lang="ts" src="./overview-metrics.ts"></script>
