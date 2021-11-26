83884cd0d9ab5d94cc40f36890ccf94768e3b15c
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-11-26 16:44:57 +0700

    [308] Update dashboard api to return project profile and also add mock up profile data

--- packages/api/src/dashboard/dashboard-generated-controller.ts
+++ packages/api/src/dashboard/dashboard-generated-controller.ts
@@ -1,15 +1,17 @@
 import { Controller } from '../_services/api-helper/types.js'
 import { assertParamsExist } from '../_services/validation/index.js'
 import { DashboardGeneratedParams, DashboardGeneratedResponse } from '../TEMP/api-bio-types/dashboard-generated.js'
-import { getMetrics } from './dao.js'
+import { getMetrics, getProfile } from './dao.js'
 
 export const dashboardGeneratedController: Controller<DashboardGeneratedParams, DashboardGeneratedResponse> = async (req) => {
   const { projectId } = req.params
   assertParamsExist({ projectId })
 
   const metrics = await getMetrics()
+  const profile = await getProfile()
 
   return {
-    metrics
+    metrics,
+    profile
   }
 }
