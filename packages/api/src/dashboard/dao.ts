ffd7080b2cb56677d5ed138e5cc64d4f6009f46b
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-11-26 13:33:38 +0700

    - Fix dashboard metrics type

--- packages/api/src/dashboard/dao.ts
+++ packages/api/src/dashboard/dao.ts
@@ -1,7 +1,7 @@
-import { DashboardMetrics } from '../TEMP/api-bio-types/dashboard-generated.js'
+import { DashboardGeneratedResponseMetrics } from '../TEMP/api-bio-types/dashboard-generated.js'
 
 // TODO: Update to query from DB
-export async function getMetrics (): Promise<DashboardMetrics> {
+export async function getMetrics (): Promise<DashboardGeneratedResponseMetrics> {
   return {
     detectionCount: 50000,
     siteCount: 200,
