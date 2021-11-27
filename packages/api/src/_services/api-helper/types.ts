6f934146b47701eb9600e6f86a73a1e083fb572f
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-11-27 09:39:49 +0700

    [API] Missed one...

--- packages/api/src/_services/api-helper/types.ts
+++ packages/api/src/_services/api-helper/types.ts
@@ -1,14 +1,5 @@
-import { AxiosRequestConfig, Method } from 'axios'
 import { RouteHandlerMethod } from 'fastify'
 import { NoExtraProperties } from 'TEMP/utility-types'
-
-export type RequestMethod = Method & ('GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE')
-
-// TODO ??? - Extract to utils
-export interface Endpoint extends AxiosRequestConfig<any> {
-  method: RequestMethod
-  url: string
-}
 
 type FastifyController<Params, Response> = RouteHandlerMethod<any, any, any, {
   Params: Partial<Params>
