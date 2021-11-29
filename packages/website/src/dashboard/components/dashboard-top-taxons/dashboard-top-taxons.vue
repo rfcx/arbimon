cf7f0923b0ef7b207e6a7d731b5467f9f667bd9f
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-11-29 21:56:29 +0700

    [315] Add  richness bar and badge. Also, no data logic handler.

--- packages/website/src/dashboard/components/dashboard-top-taxons/dashboard-top-taxons.vue
+++ packages/website/src/dashboard/components/dashboard-top-taxons/dashboard-top-taxons.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full rounded-2xl bg-steel-grey p-4 flex justify-between">
+  <div class="w-full rounded-2xl bg-steel-grey mb-2 p-4 flex justify-between">
     <div class="font-semibold">
       Richness
     </div>
@@ -7,23 +7,40 @@
       class="flex hover:(underline)"
       :to="{ name: richnessRoutename, params: { projectId } }"
     >
-      <div>
+      <div v-if="hasData">
         {{ totalSpecies }} species
       </div>
       <icon-fas-angle-right class="font-semibold self-center" />
     </router-link>
   </div>
-  <ul class="mt-2 list-none">
-    <li
-      v-for="item in richnessPercentage"
-      :key="'dashboard-richness-' + item.taxonClass"
-      class="inline-flex"
-    >
+  <no-data-panel
+    v-if="!hasData"
+    class="h-20"
+  />
+  <div v-else>
+    <div class="relative">
+      <div class="absolute w-full h-2 rounded-xl bg-steel-grey-light" />
       <div
-        class="rounded-full w-2 h-2 self-center mx-2"
-        :style="{ backgroundColor: item.color }"
-      /> {{ item.taxonClass }} ({{ displayPercentage(item.percentage) }} %)
-    </li>
-  </ul>
+        v-for="(item, idx) in richnessPercentage"
+        :key="'dashboard-richness-percentage-' + item.taxonClass"
+        class="absolute h-2 rounded-xl"
+        :style="{ width: calculateBarWidth(idx) + '%', backgroundColor: item.color , zIndex: richnessPercentage.length - idx }"
+      />
+    </div>
+    <div class="pt-4">
+      <ul class="list-none">
+        <li
+          v-for="item in richnessPercentage"
+          :key="'dashboard-richness-class-' + item.taxonClass"
+          class="inline-flex"
+        >
+          <div
+            class="rounded-full w-2 h-2 self-center mx-2"
+            :style="{ backgroundColor: item.color }"
+          /> {{ item.taxonClass }} ({{ displayPercentage(item.percentage) }} %)
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 <script lang="ts" src="./dashboard-top-taxons.ts"></script>
