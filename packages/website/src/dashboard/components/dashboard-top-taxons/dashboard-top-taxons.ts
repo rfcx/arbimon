6337ff069403dd93fcd35be89896870166829be0
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-11-29 17:03:03 +0700

    [315] Add top taxon example component

---
+++ packages/website/src/dashboard/components/dashboard-top-taxons/dashboard-top-taxons.ts
@@ -0,0 +1,19 @@
+import { Vue } from 'vue-class-component'
+import { Inject, Prop } from 'vue-property-decorator'
+
+import { ROUTE_NAMES } from '~/router'
+import { BiodiversityStore } from '~/store'
+
+export default class DashboardTopTaxons extends Vue {
+  @Inject() readonly store!: BiodiversityStore
+  @Prop() totalSpecies!: number
+
+  get richnessRoutename (): string {
+    console.log(this.totalSpecies)
+    return ROUTE_NAMES.species_richness
+  }
+
+  get projectId (): string | undefined {
+    return this.store.selectedProject?.id
+  }
+}
---
+++ packages/website/src/dashboard/components/dashboard-top-taxons/dashboard-top-taxons.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="w-full rounded-2xl bg-steel-grey p-4 flex justify-between">
+    <div class="font-semibold">
+      Richness
+    </div>
+    <router-link
+      class="flex hover:(underline)"
+      :to="{ name: richnessRoutename, params: { projectId } }"
+    >
+      <div>
+        {{ totalSpecies }} species
+      </div>
+      <icon-fas-angle-right class="font-semibold self-center" />
+    </router-link>
+  </div>
+  <ul class="mt-2 list-none">
+    <li class="inline-flex">
+      <div class="bg-fuchsia-400 rounded-full w-2 h-2 self-center mx-2" /> bird
+    </li>
+    <li class="inline-flex">
+      <div class="bg-fuchsia-400 rounded-full w-2 h-2 self-center mx-2" /> bird
+    </li>
+    <li class="inline-flex">
+      <div class="bg-fuchsia-400 rounded-full w-2 h-2 self-center mx-2" /> bird
+    </li>
+    <li class="inline-flex">
+      <div class="bg-fuchsia-400 rounded-full w-2 h-2 self-center mx-2" /> bird
+    </li>
+    <li class="inline-flex">
+      <div class="bg-fuchsia-400 rounded-full w-2 h-2 self-center mx-2" /> bird
+    </li>
+    <li class="inline-flex">
+      <div class="bg-fuchsia-400 rounded-full w-2 h-2 self-center mx-2" /> bird
+    </li>
+  </ul>
+</template>
+<script lang="ts" src="./dashboard-top-taxons.ts"></script>
