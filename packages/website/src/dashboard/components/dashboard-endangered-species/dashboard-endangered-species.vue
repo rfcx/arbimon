de1c783fb2f47382dc7607d8d42089a58c454f36
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-11-30 14:43:41 +0700

    [316] Add endangered species component and it template

---
+++ packages/website/src/dashboard/components/dashboard-endangered-species/dashboard-endangered-species.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="w-full rounded-2xl bg-steel-grey my-4 p-4 flex justify-between">
+    <div class="font-semibold">
+      Highlighted species
+    </div>
+    <router-link
+      class="flex hover:(underline opacity-70)"
+      :to="{ name: activityOverview, params: { projectId } }"
+    >
+      <div>
+        Compare
+      </div>
+      <icon-fas-angle-right class="font-semibold self-center" />
+    </router-link>
+  </div>
+  <div class="flex items-center ml-2">
+    <img
+      src="../../assets/default-species-image.jpg"
+      class="max-h-20 mr-4"
+    >
+    <div
+      class="hover:(underline opacity-70 cursor-pointer)"
+    >
+      <p class="text-lg">
+        Cat
+      </p>
+      <p>Endangered</p>
+    </div>
+  </div>
+</template>
+<script lang="ts" src="./dashboard-endangered-species.ts"></script>
