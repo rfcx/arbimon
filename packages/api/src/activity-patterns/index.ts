bec1c73a6f0250b155138170fe440261a153ab9e
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-12-01 21:48:10 +0700

    [Debt] Rename API packages; Fix import sort

--- packages/api/src/activity-patterns/index.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyPluginAsync } from 'fastify'
-
-export const routesActivityPatterns: FastifyPluginAsync = async (app, options): Promise<void> => {
-  app.get('/activity-patterns', async (req, res) => {
-    return { 'todo:': 'build this' }
-  })
-}
