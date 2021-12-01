24fbc73dcb7d77847d0dad1b8c0d921dbb2e5e08
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-12-01 21:16:07 +0700

    [313 & 314] Add species richness and detection frequency line chart

--- packages/website/src/dashboard/components/dashboard-line-chart/dashboard-line-chart.ts
+++ packages/website/src/dashboard/components/dashboard-line-chart/dashboard-line-chart.ts
@@ -1,5 +1,35 @@
-import { Vue } from 'vue-class-component'
+import { Options, Vue } from 'vue-class-component'
+import { Inject, Prop } from 'vue-property-decorator'
 
+import { LineChartComponent, LineChartConfig, LineChartSeries } from '~/charts/line-chart'
+import { BiodiversityStore } from '~/store'
+
+@Options({
+  components: {
+    LineChartComponent
+  }
+})
 export default class DashboardLineChart extends Vue {
+  @Inject() readonly store!: BiodiversityStore
+  @Prop() timeData!: Record<number, number> | null
 
+  get hasData (): boolean {
+    return this.timeData !== null
+  }
+
+  get config (): Omit<LineChartConfig, 'width'> {
+    return {
+      height: 576,
+      margins: { top: 20, right: 30, bottom: 30, left: 40 },
+      xBounds: [0, 23]
+    }
+  }
+
+  get chartData (): LineChartSeries[] {
+    // Format time data into chart style
+    return [{
+      color: this.store.datasetColors[0],
+      data: this.timeData ?? []
+    }]
+  }
 }
