fe171d49d6405b4260a69c722f2c7b26f59507a7
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-12-01 21:23:12 +0700

    [Debt] Move temporary hacks together

--- packages/api/src/dashboard/index.ts
+++ packages/api/src/dashboard/index.ts
@@ -1,7 +1,7 @@
 import { FastifyPluginAsync } from 'fastify'
 
-import { dashboardGeneratedRoute } from '../TEMP/api-bio-types/dashboard-generated.js'
-import { dashboardProfileRoute } from '../TEMP/api-bio-types/dashboard-profile.js'
+import { dashboardGeneratedRoute } from '../Z_COMMON/api-bio-types/dashboard-generated.js'
+import { dashboardProfileRoute } from '../Z_COMMON/api-bio-types/dashboard-profile.js'
 import { dashboardGeneratedController } from './dashboard-generated-controller.js'
 import { dashboardProfileController } from './dashboard-profile-controller.js'
 
