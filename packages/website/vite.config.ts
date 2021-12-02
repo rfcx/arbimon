3fed0e47e38bac16d36f17a76019ee1183a7f0b3
Chanachai Yaemban <75108999+Crave61341416@users.noreply.github.com>
2021-12-02 16:49:46 +0700

    [318] Integrate Element+ Css Component

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -1,6 +1,8 @@
 import pluginVue from '@vitejs/plugin-vue'
+import pluginAutoImport from 'unplugin-auto-import/vite'
 import pluginIconsResolver from 'unplugin-icons/resolver'
 import pluginIcons from 'unplugin-icons/vite'
+import { ElementPlusResolver as pluginElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import pluginComponents from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 import pluginWindiCSS from 'vite-plugin-windicss'
@@ -9,12 +11,16 @@ import pluginTsConfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    pluginAutoImport({
+      resolvers: [pluginElementPlusResolver()]
+    }),
     pluginComponents({
       resolvers: [
         pluginIconsResolver({
           prefix: 'icon',
           alias: { fas: 'fa-solid', far: 'fa-regular' }
-        })
+        }),
+        pluginElementPlusResolver()
       ]
     }),
     pluginIcons({ compiler: 'vue3' }),
