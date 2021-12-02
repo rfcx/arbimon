af53770410c03c8fced1d99ca874bbe8be83dab4
Chanachai Yaemban <75108999+Crave61341416@users.noreply.github.com>
2021-12-02 16:55:01 +0700

    - Improve Dashboard UI

--- packages/website/src/dashboard/components/dashboard-highlighted-species/dashboard-highlighted-species.vue
+++ packages/website/src/dashboard/components/dashboard-highlighted-species/dashboard-highlighted-species.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="w-full rounded-2xl bg-steel-grey my-4 p-4 flex justify-between">
+  <router-link
+    class="w-full rounded-2xl bg-steel-grey my-4 p-4 flex justify-between"
+    :to="{ name: ROUTE_NAMES.activityOverview, params: { projectId: store.selectedProject?.id } }"
+  >
     <div class="font-semibold">
       Highlighted species
     </div>
-    <router-link
-      class="flex hover:(underline opacity-70)"
-      :to="{ name: ROUTE_NAMES.activityOverview, params: { projectId: store.selectedProject?.id } }"
-    >
-      <div>
-        Compare
-      </div>
-      <icon-fas-angle-right class="font-semibold self-center" />
-    </router-link>
-  </div>
+    <icon-fas-angle-right class="font-semibold self-center" />
+  </router-link>
+
   <no-data-panel
     v-if="!hasData"
     class="h-20"
@@ -27,17 +23,17 @@
       class="flex content-center mb-2"
     >
       <img
-        class="h-16 w-16 object-cover mr-2"
+        class="h-12 w-12 object-cover mr-2"
         :src="item.imageUrl"
       >
       <router-link
         class="self-center hover:(underline opacity-70)"
         :to="{ name: ROUTE_NAMES.activityPatterns, params: { projectId: store.selectedProject?.id, speciesSlug: item.speciesSlug } }"
       >
-        <p class="text-lg italic">
+        <p class="italic">
           {{ item.speciesName }}
         </p>
-        <p>
+        <p class="text-xs">
           {{ item.extinctionRisk.label }}
         </p>
       </router-link>
