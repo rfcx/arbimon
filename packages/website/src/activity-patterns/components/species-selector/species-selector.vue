80e18a2d39c2f5ce1f7f27edbe62c41a233b5eae
Chanachai Yaemban <75108999+Crave61341416@users.noreply.github.com>
2021-12-02 16:52:10 +0700

    [228] Use Element+ selector for species name in Spotlight page

--- packages/website/src/activity-patterns/components/species-selector/species-selector.vue
+++ packages/website/src/activity-patterns/components/species-selector/species-selector.vue
@@ -1,16 +1,23 @@
 <template>
-  <select
+  <el-select
     v-model="selectedSpeciesSlug"
+    filterable
+    :filter-method="onFilterType"
+    :loading="loadingSpecies"
     class="bg-steel-grey rounded p-2 my-6 focus:(border-box-grey ring-0 outline-none) min-w-64"
   >
-    <option
-      v-for="species in allSpecies"
+    <el-option
+      v-for="species in filteredSpecies"
       :key="'species-selector-' + species.speciesId"
       :value="species.speciesSlug"
       class="italic"
-    >
-      {{ species.speciesName }}
-    </option>
-  </select>
+      :label="species.speciesName"
+    />
+  </el-select>
 </template>
 <script src="./species-selector.ts" lang="ts"></script>
+<style lang="scss">
+.el-input__inner {
+  font-style: italic;
+}
+</style>
