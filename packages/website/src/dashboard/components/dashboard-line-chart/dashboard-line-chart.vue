470f9d04d70ed0ad449f943083695162ad15340e
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-12-02 18:01:31 +0700

    [313] Fix line chart bug and update line chart no data display height

--- packages/website/src/dashboard/components/dashboard-line-chart/dashboard-line-chart.vue
+++ packages/website/src/dashboard/components/dashboard-line-chart/dashboard-line-chart.vue
@@ -1,16 +1,8 @@
 <template>
-  <div>
-    <no-data-panel
-      v-if="!hasData"
-      class="<sm:h-34 lg:h-144"
-    />
-    <div v-else>
-      <line-chart-component
-        dom-id="dashboard-line-chart"
-        :config="config"
-        :datasets="chartData"
-      />
-    </div>
-  </div>
+  <line-chart-component
+    dom-id="dashboard-line-chart"
+    :config="config"
+    :datasets="chartData"
+  />
 </template>
 <script lang="ts" src="./dashboard-line-chart.ts"></script>
