9575e28ea16de2685a3ce428b95361e18fa1333e
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-12-03 22:09:14 +0700

    [Build] Enable ESLint caching

--- tools/configs/.eslintrc-base.cjs
+++ tools/configs/.eslintrc-base.cjs
@@ -3,8 +3,8 @@ module.exports = {
   env: { es2021: true, node: true },
   ignorePatterns: [
     'node_modules/',
-    'dist/',
     'build/',
+    'dist/',
     'lib/',
     'cypress/',
     'coverage/',
