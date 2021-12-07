dac701b5f36c4d187ad5d26481396dab00e666cc
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-12-07 17:14:23 +0700

    [Build] I think this will be faster

--- apps/api/loader.js
+++ apps/api/loader.js
@@ -3,51 +3,27 @@
  * This is a custom loader from https://github.com/TypeStrong/ts-node/discussions/1450
  */
 
-import { lstatSync } from 'fs'
 import { resolve as resolveTs } from 'ts-node/esm'
-import { createMatchPath, loadConfig } from 'tsconfig-paths'
+import * as tsConfigPaths from 'tsconfig-paths'
 
-/**
-  * @file Helpers - Custom ESM Loader
-  * @module tools/loaders/esm
-  * @see https://github.com/TypeStrong/ts-node/issues/1007
-  */
+const { absoluteBaseUrl, paths } = tsConfigPaths.loadConfig()
+const matchPath = tsConfigPaths.createMatchPath(absoluteBaseUrl, paths)
 
-/** @typedef {'builtin'|'commonjs'|'dynamic'|'json'|'module'|'wasm'} Format */
+export function resolve (specifier, ctx, defaultResolve) {
+  // Pass-through lib imports
+  if (!specifier.startsWith('@rfcx-bio')) return resolveTs(specifier, ctx, defaultResolve)
 
-/**
-  * Custom resolver that handles TypeScript path mappings.
-  *
-  * @see https://github.com/TypeStrong/ts-node/discussions/1450
-  * @see https://github.com/dividab/tsconfig-paths
-  *
-  * @async
-  * @param {string} specifier - Name of file to resolve
-  * @param {{ parentURL: string }} ctx - Resolver context
-  * @param {typeof resolveTs} defaultResolve - Default resolver function
-  * @return {Promise<{ url: string }>} Promise containing object with file path
-  */
-export const resolve = async (specifier, ctx, defaultResolve) => {
-  // Get base URL and path aliases
-  const { absoluteBaseUrl, paths } = loadConfig(process.cwd())
-
-  // CHAZ: Allow /index.js
-  // TODO: Use convention and bypass the lstatSync call
   const lastIndexOfIndex = specifier.lastIndexOf('/index.js')
-  const trimmed = lastIndexOfIndex === -1 ? specifier : specifier.substring(0, lastIndexOfIndex)
-
-  // Attempt to resolve path based on path aliases
-  const match = createMatchPath(absoluteBaseUrl, paths)(trimmed)
-
-  // Update specifier if match was found
-  if (match) {
-    try {
-      const directory = lstatSync(match).isDirectory()
-      console.log(specifier, `${match}${directory ? '/index.js' : '.js'}`)
-      specifier = `${match}${directory ? '/index.js' : '.js'}`
-    } catch {
-      specifier = `${match}.js`
-    }
+  if (lastIndexOfIndex !== -1) {
+    // Handle index.js
+    const trimmed = specifier.substring(0, lastIndexOfIndex)
+    const match = matchPath(trimmed)
+    if (match) return resolveTs(`${match}/index.js`, ctx, defaultResolve)
+  } else if (specifier.endsWith('.js')) {
+    // Handle *.js
+    const trimmed = specifier.substring(0, specifier.length - 3)
+    const match = matchPath(trimmed)
+    if (match) return resolveTs(`${match}.js`, ctx, defaultResolve)
   }
 
   return resolveTs(specifier, ctx, defaultResolve)
