5485724df839199bb968942cf34683752c23e048
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-12-08 13:09:56 +0700

    [CD] Test that we can deploy with API deps on common/utils

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -3,6 +3,8 @@ import fastifyCors from 'fastify-cors'
 import fastifyStatic from 'fastify-static'
 import { resolve } from 'path'
 
+import { urlify } from '@rfcx-bio/utils/url-helpers/index.js'
+
 import { env } from './_services/env/index.js'
 import { routesDashboard } from './dashboard/index.js'
 import { routesProjectSite } from './projects-and-sites/index.js'
@@ -15,6 +17,8 @@ import { routesStatus } from './status/index.js'
 export const app = fastify({
   logger: env.NODE_ENV !== 'production'
 })
+
+console.log(urlify('potato'))
 
 // Register plugins
 await app.register(fastifyCors)
