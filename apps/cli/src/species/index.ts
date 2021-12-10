570c615beec2fb744c61fc1cac764668dbab6989
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-12-10 16:03:39 +0700

    [Species] Refactor Species data CLI script
    - API functions handle errors
    - Merge IUCN & Wiki data in 1-step
    - Query APIs sequentially to avoid spamming
    - Encode IUCN URL
    - Store both Wiki & IUCN info
    - Remove undefined from props that are always defined
    - Fix types of SpeciesSource constants
    - Re-run script to update mock data

--- apps/cli/src/species/index.ts
+++ apps/cli/src/species/index.ts
@@ -2,108 +2,73 @@ import * as dotenv from 'dotenv'
 import * as fs from 'fs'
 import { dirname, resolve } from 'path'
 
-import { Species, SPECIES_SOURCE_IUCN, SPECIES_SOURCE_WIKI, SpeciesExternalLink, SpeciesInformation } from '@rfcx-bio/common/api-bio-types/species'
+import { Species, SPECIES_SOURCE_IUCN, SPECIES_SOURCE_WIKI } from '@rfcx-bio/common/api-bio-types/species.js'
 import { EXTINCTION_RISK_NOT_EVALUATED } from '@rfcx-bio/common/iucn/index.js'
 
 import { getSpeciesCommonInformation, getSpeciesInformation, getSpeciesRedirectLink } from './iucn/iucn.js'
 import { getWikiSpeciesInformation } from './wiki/wiki.js'
 
-// Env
+// TODO - Wrap CLI env
 dotenv.config()
 
+// TODO - Extract this
+const MODE = process.argv.find(arg => arg.startsWith('--mode='))?.split('=')[1].toLowerCase() ?? 'dev'
+const MODE_PRODUCTION = 'production'
+console.info(`Running species data lookup in ${MODE} mode`)
+
+// Script config
 const currentDir = dirname(new URL(import.meta.url).pathname)
 const mockSpeciesPath = resolve(currentDir, './raw-species.json')
 const outputFilePath = resolve(currentDir, './species-with-information.json')
 
-// Ingest raw data
-const rawSpeciesStringOrBuffer = fs.readFileSync(mockSpeciesPath)
-const rawSpeciesData = Buffer.isBuffer(rawSpeciesStringOrBuffer)
-  ? rawSpeciesStringOrBuffer.toString()
-  : rawSpeciesStringOrBuffer
+const MIN_DELAY = 500
+// TODO - Extract this
+const delay = async (ms: number): Promise<void> => await new Promise(resolve => setTimeout(resolve, ms))
 
-const speciesData = JSON.parse(rawSpeciesData).map((s: Species) => {
-  return { ...s, externalLinks: [] }
-})
-const speciesWithWikiInfo = await getAllSpeciesDataWithWiki(speciesData)
-const speciesWithIUCNInfo = await getAllSpeciesDataWithIUCN(speciesWithWikiInfo)
-const json = JSON.stringify(speciesWithIUCNInfo, null, 2)
-fs.writeFileSync(outputFilePath, json, 'utf8')
+async function main (): Promise<void> {
+  // Read existing data
+  const rawSpeciesString = fs.readFileSync(mockSpeciesPath, { encoding: 'utf8' })
+  const speciesInput = (JSON.parse(rawSpeciesString) as Species[])
+    .sort((s1, s2) => s1.scientificName.localeCompare(s2.scientificName))
 
-export async function getAllSpeciesDataWithWiki (data: Species[]): Promise<Species[]> {
-  return await Promise.all(
-    data.map(async (datum: Species) => {
-      return await getSpeciesDataWithWiki(datum)
-    })
-  )
-}
-
-export async function getSpeciesDataWithWiki (species: Species): Promise<Species> {
-  const wikiInformation = await getWikiSpeciesInformation(species.scientificName)
-    .catch(e => {
-      console.log('error Wiki', e.code, species.scientificName)
-    })
-  const updatedData = species
-  if (wikiInformation?.content) {
-    updatedData.information = {
-      description: wikiInformation?.content ?? '',
-      sourceType: SPECIES_SOURCE_WIKI,
-      sourceUrl: wikiInformation?.contentUrls.desktop
-    }
-
-    updatedData.externalLinks?.push({
-      title: 'Wiki',
-      sourceType: SPECIES_SOURCE_WIKI,
-      sourceUrl: wikiInformation?.contentUrls.desktop
-    })
+  // Call APIs sequentially (to avoid spamming APIs)
+  const speciesOutput: Species[] = []
+  const speciesToProcess = MODE === MODE_PRODUCTION ? speciesInput : speciesInput.slice(0, 2)
+  for (const species of speciesToProcess) {
+    console.info(`- Requesting data for ${species.scientificName}`)
+    speciesOutput.push(await updateSpeciesData(species))
   }
-  updatedData.thumbnailImageUrl = wikiInformation?.thumbnailImage
 
-  return updatedData
+  // Write output
+  const json = JSON.stringify(speciesOutput, null, 2)
+  fs.writeFileSync(outputFilePath, json, 'utf8')
+  console.log(`Finished writing to ${outputFilePath}`)
 }
 
-export async function getAllSpeciesDataWithIUCN (data: Species[]): Promise<Species[]> {
-  return await Promise.all(
-    data.map(async (datum: Species) => {
-      return await getSpeciesDataWithIUCN(datum)
-    })
-  )
-}
+async function updateSpeciesData (species: Species): Promise<Species> {
+  // Call APIs
+  const [wikiData, iucnCommonData, iucnData] = await Promise.all([
+    getWikiSpeciesInformation(species.scientificName),
+    getSpeciesCommonInformation(species.scientificName),
+    getSpeciesInformation(species.scientificName),
+    delay(MIN_DELAY)
+  ])
 
-export async function getSpeciesDataWithIUCN (species: Species): Promise<Species> {
-  const iucnCommonInformation = await getSpeciesCommonInformation(species.scientificName)
-    .catch(e => {
-      console.error('error IUCN common information:', e)
-      return undefined
-    })
-  const iucnInformation = await getSpeciesInformation(species.scientificName)
-    .catch(e => {
-      console.error('error IUCN information:', e, species.scientificName)
-      return undefined
-    })
-  if (!iucnCommonInformation && !iucnInformation) return species
-
-  const iucnDesc = (iucnInformation?.habitat ?? iucnInformation?.rationale ?? iucnInformation?.taxonomicnotes)
-
-  const iucnInfo: SpeciesInformation | undefined = iucnDesc
-    ? {
-        description: iucnDesc ?? '',
-        sourceType: SPECIES_SOURCE_IUCN,
-        sourceUrl: getSpeciesRedirectLink(species.scientificName),
-        sourceCite: 'IUCN 2021. IUCN Red List of Threatened Species. Version 2021-2 <www.iucnredlist.org>'
-      }
-    : undefined
-
-  const iucnLink: SpeciesExternalLink = {
-    title: 'IUCN',
-    sourceType: SPECIES_SOURCE_IUCN,
-    sourceUrl: `${process.env.IUCN_BASE_URL ?? ''}/website/${species.scientificName}`
-  }
+  // Merge data
+  const informationIucn = iucnData?.habitat ?? iucnData?.rationale ?? iucnData?.taxonomicnotes
+  const information = [
+    ...(informationIucn ? [{ description: informationIucn ?? '', sourceType: SPECIES_SOURCE_IUCN, sourceUrl: getSpeciesRedirectLink(species.scientificName), sourceCite: 'IUCN 2021. IUCN Red List of Threatened Species. Version 2021-2 <www.iucnredlist.org>' }] : []),
+    ...(wikiData?.content ? [{ description: wikiData.content, sourceType: SPECIES_SOURCE_WIKI, sourceUrl: wikiData.contentUrls.desktop }] : [])
+  ]
 
   return {
     ...species,
-    commonName: iucnCommonInformation?.main_common_name ?? '',
-    externalLinks: [...species.externalLinks ?? [], iucnLink],
-    extinctionRisk: iucnCommonInformation?.category ?? EXTINCTION_RISK_NOT_EVALUATED.code,
-    information: iucnInfo
+    commonName: iucnCommonData?.main_common_name ?? '',
+    extinctionRisk: iucnCommonData?.category ?? EXTINCTION_RISK_NOT_EVALUATED.code,
+    externalLinks: information.map(({ sourceType, sourceUrl }) => ({ sourceType, sourceUrl, title: sourceType })),
+    thumbnailImageUrl: wikiData?.thumbnailImage ?? '',
+    information
   }
 }
+
+await main()
--- apps/cli/src/species/wiki/wiki.ts
+++ apps/cli/src/species/wiki/wiki.ts
@@ -4,18 +4,23 @@ import { WikiSummary, WikiSummaryResponse } from 'species/wiki/types'
 const WIKI_BASE_URL = 'https://en.wikipedia.org'
 
 export async function getWikiSpeciesInformation (speciesName: string): Promise<WikiSummary | undefined> {
-  const endpoint: AxiosRequestConfig = {
-    method: 'GET',
-    url: `${WIKI_BASE_URL}/api/rest_v1/page/summary/${speciesName}`
-  }
+  try {
+    const endpoint: AxiosRequestConfig = {
+      method: 'GET',
+      url: `${WIKI_BASE_URL}/api/rest_v1/page/summary/${speciesName}`
+    }
 
-  const { data } = await axios.request<WikiSummaryResponse>(endpoint)
-  return {
-    content: data.extract,
-    contentUrls: {
-      desktop: data.content_urls?.desktop?.page,
-      mobile: data.content_urls?.mobile?.page
-    },
-    thumbnailImage: data.thumbnail?.source
+    const { data } = await axios.request<WikiSummaryResponse>(endpoint)
+    return {
+      content: data.extract,
+      contentUrls: {
+        desktop: data.content_urls?.desktop?.page,
+        mobile: data.content_urls?.mobile?.page
+      },
+      thumbnailImage: data.thumbnail?.source
+    }
+  } catch (error) {
+    console.error('wiki/getWikiSpeciesInformation', error)
+    return undefined
   }
 }
