a56397c9ab59d5378ef7afc5b3f179d4c422c069
naluinui <nuisomjin@gmail.com>
2021-12-11 15:27:24 +0700

    [CLI][81] convert species call raw data to media api url

--- packages/utils/src/dayjs-initialized/index.ts
+++ packages/utils/src/dayjs-initialized/index.ts
@@ -1,9 +1,9 @@
 // eslint-disable-next-line regex/invalid -- this is the one place we import & configure dayjs
 import dayjsBase from 'dayjs'
-import minMax from 'dayjs/plugin/minMax'
-import pluralGetSet from 'dayjs/plugin/pluralGetSet'
-import quarterOfYear from 'dayjs/plugin/quarterOfYear'
-import utc from 'dayjs/plugin/utc'
+import minMax from 'dayjs/plugin/minMax.js'
+import pluralGetSet from 'dayjs/plugin/pluralGetSet.js'
+import quarterOfYear from 'dayjs/plugin/quarterOfYear.js'
+import utc from 'dayjs/plugin/utc.js'
 
 dayjsBase.extend(utc)
 dayjsBase.extend(quarterOfYear)
