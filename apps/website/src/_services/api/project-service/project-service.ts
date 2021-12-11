e0410dcacde66f3ffec2dd47b7913589ca958be9
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-12-11 15:49:35 +0700

    [Clean] Project/Site constants & types => packages
    ...and extract Core API URL to env

--- apps/website/src/_services/api/project-service/project-service.ts
+++ apps/website/src/_services/api/project-service/project-service.ts
@@ -1,10 +1,19 @@
+import { AxiosRequestConfig } from 'axios'
+
 import { Project } from '@rfcx-bio/common/api-bio-types/projects'
 
-import { apiClient } from '~/api-helpers/rest/api-service'
-import { endpointProjects } from '~/api-helpers/rest/endpoints'
-import { RawProjectListItem } from '~/api-helpers/rest/types'
+import { apiClient } from '..'
 
-const toProject = (data: RawProjectListItem): Project => {
+/* eslint-disable camelcase */
+interface CoreApiProject {
+  id: string
+  name: string
+  is_public: boolean
+  external_id: number
+}
+/* eslint-enable camelcase */
+
+const toProject = (data: CoreApiProject): Project => {
   return {
     id: data.id,
     name: data.name,
@@ -13,11 +22,14 @@ const toProject = (data: RawProjectListItem): Project => {
   }
 }
 
+const endpointProjects: AxiosRequestConfig = {
+  method: 'GET',
+  url: `${import.meta.env.VITE_CORE_API_HOST}/projects`
+}
+
 export const getProjects = async (): Promise<Project[]> => {
   try {
-    const resp = await apiClient.request<RawProjectListItem[]>({
-      ...endpointProjects
-    })
+    const resp = await apiClient.request<CoreApiProject[]>(endpointProjects)
     return Array.isArray(resp) ? resp.map(toProject) : []
   } catch (error) {
     return await Promise.reject(error)
