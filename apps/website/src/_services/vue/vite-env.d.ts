1fde8846f696e8b909265b7d713a445d43877d2c
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-12-12 11:10:24 +0700

    [Env] Fix CoreApiHost refs

--- apps/website/src/_services/vue/vite-env.d.ts
+++ apps/website/src/_services/vue/vite-env.d.ts
@@ -2,6 +2,7 @@ import 'vite/client'
 
 declare global {
   interface ImportMetaEnv extends Readonly<Record<string, string>> {
+    readonly VITE_CORE_API_HOST: string
     readonly VITE_BIO_API_HOST: string
   }
 }
