3e6bd38cd7eca161d5bfa8b193bfd90069edfbc7
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-12-13 15:55:46 +0700

    [321] Fix wrong mapping content

--- apps/website/src/activity-patterns/components/species-background-information/species-background-information.ts
+++ apps/website/src/activity-patterns/components/species-background-information/species-background-information.ts
@@ -1,46 +1,38 @@
 import { Options, Vue } from 'vue-class-component'
 import { Prop, Watch } from 'vue-property-decorator'
 
-import { SpeciesLight } from '@rfcx-bio/common/api-bio-types/species'
+import { Species, SPECIES_SOURCE_IUCN, SPECIES_SOURCE_WIKI, SpeciesInformation, SpeciesLight } from '@rfcx-bio/common/api-bio-types/species'
 
-import { iucnService, IUCNSummary } from '~/api/iucn-service'
-import { wikiService, WikiSummary } from '~/api/wiki-service'
+import { getSpecies } from '~/api/species-service'
 import SpeciesInformationContentComponent from './species-information-content.vue'
-
-const WIKIPEDIA_MOBILE_MAX_WIDTH = 760
 
 @Options({
   components: {
     SpeciesInformationContentComponent
   }
 })
-export default class SpeciesInformation extends Vue {
+export default class SpeciesBackgroundInformation extends Vue {
   @Prop() species!: SpeciesLight | null
 
   isLoading = true
-  iucnSpeciesInformation: IUCNSummary | null = null
-  wikiSpeciesInformation: WikiSummary | null = null
-
-  get speciesIUCNUrl (): string {
-    return this.iucnSpeciesInformation?.redirectUrl ?? ''
-  }
-
-  get speciesWikiUrl (): string {
-    if (!this.wikiSpeciesInformation) return ''
-
-    return screen.width <= WIKIPEDIA_MOBILE_MAX_WIDTH
-      ? this.wikiSpeciesInformation.contentUrls.mobile
-      : this.wikiSpeciesInformation.contentUrls.desktop
-  }
+  speciesInformation: Species | null = null
 
   /**
    * Clean up html tag from raw content from iucn api
    */
   get speciesIUCNCleanContent (): string {
-    const rawContent = this.iucnSpeciesInformation?.content ?? ''
+    const rawContent = this.iucnSpeciesInformation?.description ?? ''
     const div = document.createElement('div')
     div.innerHTML = rawContent
     return div.innerText
+  }
+
+  get iucnSpeciesInformation (): SpeciesInformation | null {
+    return this.speciesInformation?.information.find(({ sourceType }) => sourceType === SPECIES_SOURCE_IUCN) ?? null
+  }
+
+  get wikiSpeciesInformation (): SpeciesInformation | null {
+    return this.speciesInformation?.information.find(({ sourceType }) => sourceType === SPECIES_SOURCE_WIKI) ?? null
   }
 
   override async created (): Promise<void> {
@@ -54,23 +46,17 @@ export default class SpeciesInformation extends Vue {
 
   // TODO 190: Improve image handler
   speciesImage (): string {
-    const url = this.wikiSpeciesInformation?.thumbnailImage
-    return url ?? new URL('../../../_assets/default-species-image.jpg', import.meta.url).toString()
+    const url = this.speciesInformation?.thumbnailImageUrl
+    return url && url.length > 0 ? url : new URL('../../../_assets/default-species-image.jpg', import.meta.url).toString()
   }
 
   async getSpeciesInformation (): Promise<void> {
     const scientificName = this.species?.scientificName
     if (!scientificName) return
-
-    this.isLoading = true
     try {
-      const [iucnInformation, wikiInformation] = await Promise.all([
-        iucnService.getSpeciesSummary(scientificName),
-        wikiService.getSpeciesSummary(scientificName)
-      ])
+      const speciesInformation = await getSpecies(scientificName)
       if (this.species?.scientificName === scientificName) {
-        this.iucnSpeciesInformation = iucnInformation ?? null
-        this.wikiSpeciesInformation = wikiInformation ?? null
+        this.speciesInformation = speciesInformation ?? null
         this.isLoading = false
       }
     } catch (e) {
