db22746446c5c994c67052c10b78d6ba5cf710c9
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-12-13 15:56:50 +0700

    [321] Remove wikipedia and iucn api service calling from website

--- apps/website/src/_services/api/iucn-service/index.ts
+++
@@ -1,4 +0,0 @@
-import { IUCNService } from '~/api/iucn-service/iucn-service-api'
-
-export * from './types'
-export const iucnService = new IUCNService(import.meta.env.VITE_BIO_API_HOST)
--- apps/website/src/_services/api/iucn-service/types.ts
+++
@@ -1,4 +0,0 @@
-export interface IUCNSummary {
-  content: string
-  redirectUrl: string
-}
