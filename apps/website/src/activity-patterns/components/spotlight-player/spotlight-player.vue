91de0df2a79fa820f910823d89ea86ba760bdf14
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-12-17 13:58:51 +0700

    [326] Update play icon and add title to the audio icon controls

--- apps/website/src/activity-patterns/components/spotlight-player/spotlight-player.vue
+++ apps/website/src/activity-patterns/components/spotlight-player/spotlight-player.vue
@@ -2,13 +2,15 @@
   <div
     v-if="!playing"
     class="hover:(opacity-80 cursor-pointer)"
+    title="Species call"
     @click="play()"
   >
-    <icon-fa-play />
+    <icon-fa-volume-up />
   </div>
   <div
     v-else
     class="hover:(opacity-80 cursor-pointer)"
+    title="Pause"
     @click="pause()"
   >
     <icon-fa-pause />
