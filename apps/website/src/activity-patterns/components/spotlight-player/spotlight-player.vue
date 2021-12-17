153324a2be88abe054dd6e4ba43a168dd7d0e2f3
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-12-17 13:26:58 +0700

    [326] Add howler library and add simple logic to play and pause the audio

--- apps/website/src/activity-patterns/components/spotlight-player/spotlight-player.vue
+++ apps/website/src/activity-patterns/components/spotlight-player/spotlight-player.vue
@@ -1,10 +1,17 @@
 <template>
-  <audio controls>
-    <source
-      :src="speciesCall.mediaWavUrl"
-      type="audio/mpeg"
-    >
-    Your browser does not support the audio element.
-  </audio>
+  <div
+    v-if="!playing"
+    class="hover:(opacity-80 cursor-pointer)"
+    @click="play()"
+  >
+    <icon-fa-play />
+  </div>
+  <div
+    v-else
+    class="hover:(opacity-80 cursor-pointer)"
+    @click="pause()"
+  >
+    <icon-fa-pause />
+  </div>
 </template>
 <script lang="ts" src="./spotlight-player.ts"></script>
