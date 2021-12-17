7a7a1823cc528be2a59d423aa20a0b2777418dfb
naluinui <nuisomjin@gmail.com>
2021-12-17 15:34:46 +0700

    [Cli] Resolves 361 add dimension value

--- apps/cli/src/arbimon-export/extract-species.ts
+++ apps/cli/src/arbimon-export/extract-species.ts
@@ -60,7 +60,7 @@ const transformToMediaURL = (data: ArbimonSpeciesCallRow[]): SpeciesCall[] =>
       recordedAt,
       timezone,
       mediaWavUrl: `https://media-api.rfcx.org/internal/assets/streams/${streamId}_t${dateQueryParamify(recordedAt)}.${dateQueryParamify(end)}_fwav.wav`,
-      mediaSpecUrl: `https://media-api.rfcx.org/internal/assets/streams/${streamId}_t${dateQueryParamify(recordedAt)}.${dateQueryParamify(end)}_fspec.png`
+      mediaSpecUrl: `https://media-api.rfcx.org/internal/assets/streams/${streamId}_t${dateQueryParamify(recordedAt)}.${dateQueryParamify(end)}.d512.512_fspec.png`
   }))
 
 await main()
