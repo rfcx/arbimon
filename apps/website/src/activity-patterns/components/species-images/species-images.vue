edaa837e8bc63a1eb409d51de9dbff7dc99bc492
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-12-20 16:29:10 +0700

    [326] Update image style alignment

--- apps/website/src/activity-patterns/components/species-images/species-images.vue
+++ apps/website/src/activity-patterns/components/species-images/species-images.vue
@@ -2,21 +2,23 @@
   <div class="text-lg">
     Photo
   </div>
-  <div class="relative max-h-48 max-w-full my-4 flex justify-center">
-    <img
-      :src="speciesImage()"
-      :alt="species?.scientificName ?? ''"
-      class="max-h-48"
-    >
-    <div
-      v-if="species?.scientificName"
-      class="absolute px-2 py-1 bottom-0 w-full text-center bg-dark-300 bg-opacity-70"
-    >
-      <a
-        :href="speciesImage()"
-        target="_blank"
-        class="italic hover:underline"
-      >{{ species?.scientificName }}</a>
+  <div class="max-h-60 max-w-full my-4 flex justify-left">
+    <div class="relative">
+      <img
+        :src="speciesImage()"
+        :alt="species?.scientificName ?? ''"
+        class="max-h-60"
+      >
+      <div
+        v-if="species?.scientificName"
+        class="absolute px-2 py-1 bottom-0 w-full text-right bg-dark-300 bg-opacity-70"
+      >
+        <a
+          :href="speciesImage()"
+          target="_blank"
+          class="italic hover:underline"
+        >{{ species?.scientificName }}</a>
+      </div>
     </div>
   </div>
 </template>
