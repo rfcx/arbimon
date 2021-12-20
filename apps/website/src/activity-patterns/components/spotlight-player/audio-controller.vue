260d600579faacf24d766ff6461afd049382593b
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-12-20 19:04:41 +0700

    [326] Add spotlight audio player

---
+++ apps/website/src/activity-patterns/components/spotlight-player/audio-controller.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    v-if="!playing"
+    class="hover:(opacity-80 cursor-pointer)"
+    title="Play"
+  >
+    <icon-fa-play />
+  </div>
+  <div
+    v-else
+    class="hover:(opacity-80 cursor-pointer)"
+    title="Pause"
+  >
+    <icon-fa-pause />
+  </div>
+</template>
+<script lang="ts">
+import { Vue } from 'vue-class-component'
+import { Prop } from 'vue-property-decorator'
+
+export default class AudioController extends Vue {
+  @Prop() playing!: boolean
+}
+
+</script>
--- apps/website/src/activity-patterns/components/spotlight-player/spotlight-player.ts
+++ apps/website/src/activity-patterns/components/spotlight-player/spotlight-player.ts
@@ -1,11 +1,18 @@
 import { Howl } from 'howler'
-import { Vue } from 'vue-class-component'
+import { Options, Vue } from 'vue-class-component'
 import { Prop } from 'vue-property-decorator'
 
 import { SpeciesCall } from '@rfcx-bio/common/api-bio-types/species'
+import { dayjs } from '@rfcx-bio/utils/dayjs-initialized'
 
 import { assetsService } from '@/activity-patterns/services'
+import AudioController from './audio-controller.vue'
 
+@Options({
+  components: {
+    AudioController
+  }
+})
 export default class SpotlightPlayer extends Vue {
   @Prop() speciesCall!: SpeciesCall
 
@@ -14,6 +21,12 @@ export default class SpotlightPlayer extends Vue {
   spectrogram = ''
   audio: Howl | null = null
   playing = false
+  playedTime = 0
+  playedProgress = 0
+
+  get displayPlayedTime (): string {
+    return `${dayjs.duration(this.playedTime, 'seconds').format('m:ss')}`
+  }
 
   override async created (): Promise<void> {
     this.loading = true
@@ -47,8 +60,19 @@ export default class SpotlightPlayer extends Vue {
       },
       onplay: () => {
         this.playing = true
+        requestAnimationFrame(this.step)
       }
     })
+  }
+
+  step (): void {
+    const audio = this.audio
+    const seek = audio?.seek() ?? 0
+    if (audio?.playing() ?? false) {
+      this.playedTime = seek
+      this.playedProgress = (seek / (audio?.duration() ?? 0)) * 100
+      requestAnimationFrame(this.step)
+    }
   }
 
   async play (): Promise<void> {
