bbd93f24bd878f907f56ac642887b78608cfd03e
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-12-20 15:00:10 +0700

    [326] Add spectrogram to the species call

--- apps/website/src/activity-patterns/components/spotlight-player/spotlight-player.vue
+++ apps/website/src/activity-patterns/components/spotlight-player/spotlight-player.vue
@@ -1,19 +1,35 @@
 <template>
-  <div
-    v-if="!playing"
-    class="hover:(opacity-80 cursor-pointer)"
-    title="Species call"
-    @click="play()"
-  >
-    <icon-fa-volume-up />
+  <div class="text-lg capitalize">
+    Species call
   </div>
   <div
-    v-else
-    class="hover:(opacity-80 cursor-pointer)"
-    title="Pause"
-    @click="pause()"
+    v-if="!loading"
+    class="max-w-full my-4 flex justify-center"
   >
-    <icon-fa-pause />
+    <div class="relative">
+      <img
+        :src="spectrogram"
+        class="max-w-60"
+      >
+      <div class="absolute bottom-2 right-2">
+        <div
+          v-if="!playing"
+          class="hover:(opacity-80 cursor-pointer)"
+          title="Species call"
+          @click="play()"
+        >
+          <icon-fa-volume-up />
+        </div>
+        <div
+          v-else
+          class="hover:(opacity-80 cursor-pointer)"
+          title="Pause"
+          @click="pause()"
+        >
+          <icon-fa-pause />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts" src="./spotlight-player.ts"></script>
