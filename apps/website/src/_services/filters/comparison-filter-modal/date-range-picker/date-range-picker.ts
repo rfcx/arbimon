351137971565969656e245161ed41495e78f5d49
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-12-21 18:57:55 +0700

    [55] Add date and month picker toggle options

--- apps/website/src/_services/filters/comparison-filter-modal/date-range-picker/date-range-picker.ts
+++ apps/website/src/_services/filters/comparison-filter-modal/date-range-picker/date-range-picker.ts
@@ -1,3 +1,4 @@
+import { IDatePickerType } from 'element-plus/es/components/date-picker/src/date-picker.type'
 import { Vue } from 'vue-class-component'
 import { Emit, Prop, Watch } from 'vue-property-decorator'
 
@@ -6,6 +7,16 @@ import { dayjs } from '@rfcx-bio/utils/dayjs-initialized'
 export interface DateRangeShortcut {
   text: string
   value: () => [Date, Date]
+}
+
+const DATE_PICKER_TYPE: { [key: string]: IDatePickerType } = {
+  date: 'daterange',
+  month: 'monthrange'
+}
+
+interface DateRangeTypeOption {
+  label: string
+  value: IDatePickerType
 }
 
 // TODO: Remove this! This is for assume as the date range for the whole project
@@ -23,6 +34,7 @@ export default class DateRangePicker extends Vue {
   }
 
   dateValues: Date[] = []
+  selectedType: IDatePickerType = DATE_PICKER_TYPE.date
 
   get dateShortcuts (): DateRangeShortcut[] {
     return [
@@ -45,6 +57,19 @@ export default class DateRangePicker extends Vue {
           const end = dayjs().subtract(1, 'years').endOf('year').toDate()
           return [start, end]
         }
+      }
+    ]
+  }
+
+  get datePickerTypeOptions (): DateRangeTypeOption[] {
+    return [
+      {
+        label: 'Day',
+        value: DATE_PICKER_TYPE.date
+      },
+      {
+        label: 'Month',
+        value: DATE_PICKER_TYPE.month
       }
     ]
   }
--- apps/website/src/_services/filters/comparison-filter-modal/date-range-picker/date-range-picker.vue
+++ apps/website/src/_services/filters/comparison-filter-modal/date-range-picker/date-range-picker.vue
@@ -1,10 +1,26 @@
 <template>
-  <el-date-picker
-    v-model="dateValues"
-    type="daterange"
-    unlink-panels
-    :shortcuts="dateShortcuts"
-    range-separator="to"
-  />
+  <div>
+    <div class="flex items-center">
+      <el-button-group class="mr-2">
+        <el-button
+          v-for="item in datePickerTypeOptions"
+          :key="'filter-date-picker-' + item.value"
+          size="small"
+          :color="selectedType === item.value ? '#31984F' : '#45485D'"
+          style="color:  #fff"
+          @click="selectedType = item.value"
+        >
+          {{ item.label }}
+        </el-button>
+      </el-button-group>
+      <el-date-picker
+        v-model="dateValues"
+        :type="selectedType"
+        unlink-panels
+        :shortcuts="dateShortcuts"
+        range-separator="to"
+      />
+    </div>
+  </div>
 </template>
 <script lang="ts" src="./date-range-picker.ts"></script>
