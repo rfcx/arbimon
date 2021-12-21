378ee2ca519f3899264d2de19ecb616186422ed4
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-12-21 15:41:47 +0700

    [55] Add date shortcut and update date range component style

--- apps/website/src/_services/filters/comparison-filter-modal/date-range-picker/date-range-picker.ts
+++ apps/website/src/_services/filters/comparison-filter-modal/date-range-picker/date-range-picker.ts
@@ -1,5 +1,41 @@
 import { Vue } from 'vue-class-component'
 
+import { dayjs } from '@rfcx-bio/utils/dayjs-initialized'
+
+export interface DateRangeShortcut {
+  text: string
+  value: () => [Date, Date]
+}
+
 export default class DateRangePicker extends Vue {
   dateValues: Date[] = []
+
+  get dateShortcuts (): DateRangeShortcut[] {
+    return [
+      {
+        text: 'All detections dates',
+        value: () => {
+          const start = dayjs().subtract(20, 'years').toDate()
+          const end = dayjs().toDate()
+          return [start, end]
+        }
+      },
+      {
+        text: 'This year',
+        value: () => {
+          const start = dayjs().startOf('year').toDate()
+          const end = dayjs().toDate()
+          return [start, end]
+        }
+      },
+      {
+        text: 'Last year',
+        value: () => {
+          const start = dayjs().subtract(1, 'years').toDate()
+          const end = dayjs().subtract(1, 'years').endOf('year').toDate()
+          return [start, end]
+        }
+      }
+    ]
+  }
 }
--- apps/website/src/_services/filters/comparison-filter-modal/date-range-picker/date-range-picker.vue
+++ apps/website/src/_services/filters/comparison-filter-modal/date-range-picker/date-range-picker.vue
@@ -3,6 +3,7 @@
     v-model="dateValues"
     type="daterange"
     unlink-panels
+    :shortcuts="dateShortcuts"
   />
 </template>
 <script lang="ts" src="./date-range-picker.ts"></script>
