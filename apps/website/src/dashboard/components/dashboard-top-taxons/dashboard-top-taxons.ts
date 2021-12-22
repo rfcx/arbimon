1c65f8a1644eabfcb6297f82827e693a6f12e962
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-12-22 17:19:06 +0700

    [332] Extract generic "horizontal stacked distribution"

--- apps/website/src/dashboard/components/dashboard-top-taxons/dashboard-top-taxons.ts
+++ apps/website/src/dashboard/components/dashboard-top-taxons/dashboard-top-taxons.ts
@@ -1,61 +1,20 @@
-import { sum } from 'lodash-es'
-import { Vue } from 'vue-class-component'
+import { Options, Vue } from 'vue-class-component'
 import { Inject, Prop } from 'vue-property-decorator'
 
+import HorizontalStackedDistribution from '~/charts/horizontal-stacked-distribution/horizontal-stacked-distribution.vue'
 import { RouteNames } from '~/router'
 import { BiodiversityStore } from '~/store'
 
-interface Bar {
-  name: string
-  percentage: number
-  width: number
-  color: string
-}
-
-const DEFAULT_COLOR = '#FFFFFF'
-
+@Options({
+  components: {
+    HorizontalStackedDistribution
+  }
+})
 export default class DashboardTopTaxons extends Vue {
   @Inject() readonly ROUTE_NAMES!: RouteNames
   @Inject() readonly store!: BiodiversityStore
 
   @Prop() dataset!: Record<string, number>
   @Prop() colors!: Record<string, string>
-  @Prop({ default: undefined }) knownTotalCount!: number | undefined
-
-  get hasData (): boolean {
-    return this.totalCount > 0
-  }
-
-  get totalCount (): number {
-    return this.knownTotalCount ?? sum(Object.values(this.dataset))
-  }
-
-  get bars (): Bar[] {
-    // Avoid divByZero
-    const totalCount = this.totalCount
-    if (totalCount === 0) return []
-
-    // Remove empty bars & sort
-    const inputs = Object.entries(this.dataset)
-      .filter(([, count]) => count > 0)
-      .sort((a, b) => b[1] - a[1] || a[0].localeCompare(b[0]))
-
-    // Calculate percentages & bar-widths (width is cumulative percentage)
-    let width = 0
-    const outputs: Bar[] = []
-
-    inputs.forEach(([name, count]) => {
-      const percentage = count / totalCount * 100
-      width += percentage
-
-      outputs.push({
-        name,
-        percentage,
-        width: Math.round(width),
-        color: this.colors[name] ?? DEFAULT_COLOR
-      })
-    })
-
-    return outputs
-  }
+  @Prop() speciesCount!: number
 }
