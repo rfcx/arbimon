26a9a1b0a88dcf133d77a6520afac1ad4596cd5a
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-12-22 15:56:47 +0700

    - Update grid gap for metrics

--- apps/website/src/activity-patterns/components/spotlight-metrics/spotlight-metrics.vue
+++ apps/website/src/activity-patterns/components/spotlight-metrics/spotlight-metrics.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="grid grid-cols-2 <sm:grid-cols-1">
+  <div class="grid grid-cols-2 gap-x-6 <sm:grid-cols-1">
     <template
       v-for="(item, idx) in metrics"
       :key="'metrics-info-' + idx"
     >
-      <div class="<sm:(first:mb-4) mr-8 px-8 py-6 bg-steel-grey rounded-4xl border-subtle last:border-none">
+      <div class="<sm:(first:mb-4) px-8 py-6 bg-steel-grey rounded-4xl">
         <metrics-title
           :title="item.title"
           :information="item.information"
