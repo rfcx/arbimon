cdf16b3ef264dec24a058d55620686e1f49f3f32
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-12-23 13:54:40 +0700

    [314] Dashboard maps for SR & DF

--- apps/website/src/dashboard/dashboard-page.ts
+++ apps/website/src/dashboard/dashboard-page.ts
@@ -1,12 +1,15 @@
+import { max } from 'lodash-es'
 import { Options, Vue } from 'vue-class-component'
 import { Inject } from 'vue-property-decorator'
 
 import { DashboardGeneratedResponse } from '@rfcx-bio/common/api-bio/dashboard/dashboard-generated'
 import { DashboardProfileResponse } from '@rfcx-bio/common/api-bio/dashboard/dashboard-profile'
 import { EXTINCTION_LABELS_AND_COLORS, getExtinctionRisk } from '@rfcx-bio/common/iucn'
+import { dayjs } from '@rfcx-bio/utils/dayjs-initialized'
 
 import { TAXONOMY_COLORS } from '~/api/taxonomy-service'
 import HorizontalStackedDistribution from '~/charts/horizontal-stacked-distribution/horizontal-stacked-distribution.vue'
+import { MapBubbleComponent, MapDataSet, MapSiteData } from '~/maps/map-bubble'
 import { RouteNames } from '~/router'
 import { BiodiversityStore } from '~/store'
 import { HighlightedSpeciesRow } from './components/dashboard-highlighted-species/dashboard-highlighted-species'
@@ -15,12 +18,11 @@ import DashboardLineChart from './components/dashboard-line-chart/dashboard-line
 import DashboardMetrics from './components/dashboard-metrics/dashboard-metrics.vue'
 import DashboardProjectProfile from './components/dashboard-project-profile/dashboard-project-profile.vue'
 import DashboardSidebarTitle from './components/dashboard-sidebar-title/dashboard-sidebar-title.vue'
-import DashboardSitemap from './components/dashboard-sitemap/dashboard-sitemap.vue'
 import { ThreatenedSpeciesRow } from './components/dashboard-threatened-species/dashboard-threatened-species'
 import DashboardThreatenedSpecies from './components/dashboard-threatened-species/dashboard-threatened-species.vue'
 import { dashboardService } from './services'
 
-export interface Tab {
+interface Tab {
   label: string
   value: string
 }
@@ -30,6 +32,11 @@ const TAB_VALUES = {
   frequency: 'detectionFrequency'
 }
 
+const tabs: Tab[] = [
+  { label: 'Species Richness', value: TAB_VALUES.richness },
+  { label: 'Detection Frequency', value: TAB_VALUES.frequency }
+]
+
 @Options({
   components: {
     DashboardHighlightedSpecies,
@@ -37,9 +44,9 @@ const TAB_VALUES = {
     DashboardMetrics,
     DashboardProjectProfile,
     DashboardSidebarTitle,
-    DashboardSitemap,
     DashboardThreatenedSpecies,
-    HorizontalStackedDistribution
+    HorizontalStackedDistribution,
+    MapBubbleComponent
   }
 })
 export default class DashboardPage extends Vue {
@@ -49,16 +56,40 @@ export default class DashboardPage extends Vue {
   generated: DashboardGeneratedResponse | null = null
   profile: DashboardProfileResponse | null = null
 
-  selectedTab = TAB_VALUES.richness
-  tabs = [
-    { label: 'Species Richness', value: TAB_VALUES.richness },
-    { label: 'Detection Frequency', value: TAB_VALUES.frequency }
-  ]
+  tabs = tabs
+  selectedTab = tabs[0].value
+
+  getPopupHtml = (data: MapSiteData, dataKey: string) => this.selectedTab === TAB_VALUES.richness
+    ? data.distinctSpecies[dataKey]
+    : (data.distinctSpecies[dataKey] as number).toFixed(3)
 
   get lineChartData (): Record<number, number> | null {
     return this.selectedTab === TAB_VALUES.richness
       ? this.generated?.richnessByHour ?? null
       : this.generated?.detectionFrequencyByHour ?? null
+  }
+
+  get mapDataset (): MapDataSet {
+    return {
+      startDate: dayjs(),
+      endDate: dayjs(),
+      sites: this.store.sites,
+      data: ((this.selectedTab === TAB_VALUES.richness ? this.generated?.richnessBySite : this.generated?.detectionFrequencyBySite) ?? [])
+        .map(({ name: siteName, latitude, longitude, value }) => ({
+          siteName,
+          latitude,
+          longitude,
+          distinctSpecies: {
+            refactorThis: value
+          }
+        })),
+      maxValues: {
+        refactorThis: max(this.selectedTab === TAB_VALUES.richness
+          ? this.generated?.richnessBySite.map(d => d.value)
+          : this.generated?.detectionFrequencyBySite.map(d => d.value)
+        ) ?? 0
+      }
+    }
   }
 
   get speciesHighlighted (): HighlightedSpeciesRow[] {
