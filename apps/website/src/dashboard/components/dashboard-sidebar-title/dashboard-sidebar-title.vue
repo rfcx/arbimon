cec091586282f85a7b13f3ff697c69557f912f06
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2021-12-23 12:54:19 +0700

    [CI] Fix Vue ESLint warning

--- apps/website/src/dashboard/components/dashboard-sidebar-title/dashboard-sidebar-title.vue
+++ apps/website/src/dashboard/components/dashboard-sidebar-title/dashboard-sidebar-title.vue
@@ -3,9 +3,13 @@
     class="w-full rounded-2xl bg-steel-grey my-4 p-4 flex justify-between"
     :to="route"
   >
-    <div class="font-semibold">{{ title }}</div>
+    <div class="font-semibold">
+      {{ title }}
+    </div>
     <div class="flex">
-      <template v-if="subtitle">{{ subtitle }}</template>
+      <template v-if="subtitle">
+        {{ subtitle }}
+      </template>
       <icon-fas-angle-right class="font-semibold self-center" />
     </div>
   </router-link>
