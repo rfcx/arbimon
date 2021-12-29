7381549bec27d8a0376dc95ce0ca92748b0f3edd
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2021-12-29 17:25:52 +0700

    [388] Update IUCN ranking style on threatened section

--- apps/website/src/dashboard/components/dashboard-threatened-species/dashboard-threatened-species.vue
+++ apps/website/src/dashboard/components/dashboard-threatened-species/dashboard-threatened-species.vue
@@ -17,18 +17,30 @@
         :src="item.imageUrl"
       >
       <router-link
-        class="self-center hover:(underline opacity-70)"
+        class="self-center hover:(text-subtle)"
         :to="{ name: ROUTE_NAMES.activityPatterns, params: { projectId: store.selectedProject?.id, speciesSlug: item.speciesSlug } }"
       >
-        <p class="italic">
-          {{ item.scientificName }}
-        </p>
+        <div class="flex items-center">
+          <el-tag
+            v-if="item.extinctionRisk"
+            class="border-none text-sm"
+            effect="dark"
+            size="mini"
+            :color="item.extinctionRisk.color"
+            :title="item.extinctionRisk.label"
+          >
+            {{ item.extinctionRisk.code }}
+          </el-tag>
+          <p class="ml-1 italic">
+            {{ item.scientificName }}
+          </p>
+        </div>
         <p class="text-xs text-subtle">
           {{ item.commonName }}
         </p>
-        <p class="text-xs">
+        <!-- <p class="text-xs">
           {{ item.extinctionRisk.label }}
-        </p>
+        </p> -->
       </router-link>
     </div>
   </div>
