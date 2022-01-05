8b7bacbece079229b34c05af5bc3a50bc1c35d70
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-01-05 16:50:14 +0700

    - Improve the information page UI and page title

--- apps/website/src/info/info-page.vue
+++ apps/website/src/info/info-page.vue
@@ -1,25 +1,20 @@
 <template>
-  <div class="grid gap-4 grid-cols-8">
-    <div class="col-span-1">
-      <div
-        v-for="item in information"
-        :key="'info-ref-' + item.slug"
-        class="py-2 px-1 border-b hover:(cursor-pointer bg-steel-grey)"
-        @click="onSelectedInfo(item.slug)"
-      >
-        <div :class="{ 'text-brand-primary': selectedInfo === item.slug }">
-          {{ item.title }}
-        </div>
-      </div>
+  <div class="text-2xl">
+    Supporting information
+  </div>
+  <div
+    v-for="(info, idx) in information"
+    :key="'info-' + info.slug"
+  >
+    <div
+      class="text-xl"
+      :class="idx > 0 ? 'mt-16' : 'mt-5' "
+    >
+      {{ info.title }}
     </div>
-    <div class="col-span-7">
-      <div class="text-2xl">
-        {{ currentContent?.title }}
-      </div>
-      <p class="mt-2 whitespace-pre-line">
-        {{ currentContent?.content }}
-      </p>
-    </div>
+    <p class="mt-2 whitespace-pre-line">
+      {{ info.content }}
+    </p>
   </div>
 </template>
 <script lang="ts" src="./info-page.ts"></script>
