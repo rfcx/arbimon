5218adda6cfe9f7c762859891cffd6cf4292d2f7
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-01-07 10:35:38 +0700

    [434] Refactor horizontal bar chart and fix font size changed when screen change

--- apps/website/src/_services/charts/horizontal-bar-chart/horizontal-bar-chart.ts
+++ apps/website/src/_services/charts/horizontal-bar-chart/horizontal-bar-chart.ts
@@ -1,57 +1,46 @@
 import { Vue } from 'vue-class-component'
 import { Prop, Watch } from 'vue-property-decorator'
 
-import { downloadSvgAsPng } from '~/charts'
 import { BarChartConfig } from '~/charts/horizontal-bar-chart'
 import { clearChart } from '..'
-import { generateChartExport, generateChartInternal } from './functions'
+import { generateChartInternal } from './functions'
 import { GroupedBarChartItem } from './types'
 
-// TODO: Refactor by extract richness page information to have pure bar chart logic
 export default class HorizontalBarChartComponent extends Vue {
-  @Prop({ default: [] }) chartData!: GroupedBarChartItem[]
   @Prop() domId!: string
-  @Prop() chartTitle!: string
-  @Prop() chartExportName!: string
+  @Prop() config!: Omit<BarChartConfig, 'width'>
+  @Prop() chartData!: GroupedBarChartItem[]
 
   get hasData (): boolean {
     return this.chartData.length > 0
   }
 
-  get config (): BarChartConfig {
-    return {
-      width: document.getElementById(`wrapper-${this.domId}`)?.clientWidth ?? 0,
-      margins: { top: 20, right: 20, bottom: 30, left: 80 },
-      xTitle: 'Number of species',
-      displayXAxisTick: false,
-      fontColor: 'white'
-    }
-  }
-
   override mounted (): void {
-    window.addEventListener('resize', this.renderChart)
+    window.addEventListener('resize', this.updateChart)
   }
 
   @Watch('chartData', { deep: true })
   onChartDataChange (): void {
-    this.renderChart()
+    this.updateChart()
   }
 
-  renderChart (): void {
+  @Watch('config')
+  onConfigChange (): void {
+    this.updateChart()
+  }
+
+  updateChart (): void {
     const id = this.domId
 
-    const svg = generateChartInternal(this.chartData, this.config)
-    if (!svg) return
+    const parent = document.getElementById(id)
+    if (!parent) return
+
+    const width = (document.getElementById(`wrapper-${id}`)?.clientWidth ?? 500)
+    const config = { ...this.config, width }
+    const chart = generateChartInternal(this.chartData, config)
+    if (!chart) return
 
     clearChart(id)
-    document.getElementById(id)?.appendChild(svg)
-  }
-
-  async downloadChart (): Promise<void> {
-    const config = { ...this.config, width: 1024, displayXAxisTick: true, fontColor: 'black' }
-    const svg = generateChartExport(this.chartData, config)
-    if (!svg) return
-
-    await downloadSvgAsPng(svg, this.chartExportName)
+    document.getElementById(id)?.appendChild(chart)
   }
 }
---
+++ apps/website/src/species-richness/components/species-richness-by-class/species-richness-by-class.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="w-full">
+    <div class="flex justify-between items-end">
+      <h2 class="text-white text-xl">
+        Distinct species by taxonomic class
+      </h2>
+      <export-button
+        v-if="hasData"
+        @click="downloadChart()"
+      />
+    </div>
+    <horizontal-bar-chart-component
+      dom-id="species-by-class"
+      :config="config"
+      :chart-data="chartData"
+    />
+  </div>
+</template>
+<script lang="ts" src="./species-richness-by-class.ts"></script>
