cca2931037546bd0577155c2ca2dbd29d545f330
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-01-07 11:08:19 +0700

    [431] Update activity overview components to use section title component

--- apps/website/src/activity-overview/components/activity-overview-by-time/activity-overview-by-time.vue
+++ apps/website/src/activity-overview/components/activity-overview-by-time/activity-overview-by-time.vue
@@ -1,40 +1,44 @@
 <template>
   <div class="w-full">
-    <div class="flex flex-row justify-between items-center">
-      <div class="flex flex-row items-center">
-        <select
-          v-model="selectedType"
-          class="text-xl py-1 bg-mirage-grey border-t-0 border-l-0 border-r-0 border-b-2 focus:(border-box-grey border-t-0 border-l-0 border-r-0 border-b-2 ring-0 outline-none)"
-        >
-          <option
-            v-for="item in datasetType"
-            :key="'detection-time-selector' + item.value"
-            :value="item.value"
+    <section-title>
+      <template #title>
+        <div class="flex flex-row items-center">
+          <select
+            v-model="selectedType"
+            class="text-xl py-1 bg-mirage-grey border-t-0 border-l-0 border-r-0 border-b-2 focus:(border-box-grey border-t-0 border-l-0 border-r-0 border-b-2 ring-0 outline-none)"
           >
-            {{ item.label }}
-          </option>
-        </select>
-        <h2 class="text-white text-xl ml-2">
-          by
-        </h2>
-        <select
-          v-model="selectedBucket"
-          class="text-xl lowercase ml-2 py-1 bg-mirage-grey border-t-0 border-l-0 border-r-0 border-b-2 focus:(border-box-grey border-t-0 border-l-0 border-r-0 border-b-2 ring-0 outline-none)"
-        >
-          <option
-            v-for="bucket in Object.entries(buckets)"
-            :key="bucket[0]"
-            :value="bucket[0]"
+            <option
+              v-for="item in datasetType"
+              :key="'detection-time-selector' + item.value"
+              :value="item.value"
+            >
+              {{ item.label }}
+            </option>
+          </select>
+          <h2 class="text-white text-xl ml-2">
+            by
+          </h2>
+          <select
+            v-model="selectedBucket"
+            class="text-xl lowercase ml-2 py-1 bg-mirage-grey border-t-0 border-l-0 border-r-0 border-b-2 focus:(border-box-grey border-t-0 border-l-0 border-r-0 border-b-2 ring-0 outline-none)"
           >
-            {{ bucket[1] }}
-          </option>
-        </select>
-      </div>
-      <export-button
-        v-if="hasData"
-        @click="downloadChart()"
-      />
-    </div>
+            <option
+              v-for="bucket in Object.entries(buckets)"
+              :key="bucket[0]"
+              :value="bucket[0]"
+            >
+              {{ bucket[1] }}
+            </option>
+          </select>
+        </div>
+      </template>
+      <template #controls>
+        <export-button
+          v-if="hasData"
+          @click="downloadChart()"
+        />
+      </template>
+    </section-title>
     <line-chart-component
       :dom-id="domId"
       :config="config"
