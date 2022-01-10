6fa4a21133ccf0d47f7c87a63aa533fb98fc4d0a
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-01-10 09:05:43 +0700

    [443] Move project-species API under species package

--- apps/api/src/projects-and-sites/index.ts
+++ apps/api/src/projects-and-sites/index.ts
@@ -1,14 +1,4 @@
-import { FastifyPluginAsync, FastifyReply } from 'fastify'
-import { readdir } from 'fs/promises'
-import { resolve } from 'path'
-
-import { PredictedOccupancyMap, ProjectSpeciesRouteResponse } from '@rfcx-bio/common/api-bio/species/project-species'
-
-import { ApiClientError, ApiMissingParam } from '../_services/errors'
-
-// TODO ??? - Move files to S3 & index them in the database
-const mockPredictionsFolderName = 'predicted-occupancy/puerto-rico'
-const mockPredictionsFolderPath = resolve('./public', mockPredictionsFolderName)
+import { FastifyPluginAsync } from 'fastify'
 
 // TODO ??? Authenticate the user & filter their results!!
 export const routesProjectSite: FastifyPluginAsync = async (app, options): Promise<void> => {
@@ -18,50 +8,5 @@ export const routesProjectSite: FastifyPluginAsync = async (app, options): Promi
 
   app.get('/sites', async (req, res) => {
     return ['site1', 'site2']
-  })
-
-  interface ProjectSpeciesRoute {
-    Params: {
-      projectId?: string
-      speciesId?: string
-    }
-  }
-  app.get<ProjectSpeciesRoute>('/projects/:projectId/species/:speciesId', async (req, res) => {
-    // Inputs & validation
-    const { projectId, speciesId } = req.params
-    if (!projectId) throw ApiMissingParam('projectId')
-    if (!speciesId) throw ApiMissingParam('speciesId')
-
-    // Queries
-    const predictedOccupancyMaps: PredictedOccupancyMap[] = (await readdir(mockPredictionsFolderPath))
-      .filter(filename => filename.startsWith(speciesId))
-      .map(filename => filename.substr(0, filename.lastIndexOf('.')) || filename)
-      .sort()
-      .map(filename => ({
-        title: filename,
-        url: `/projects/${projectId}/predicted-occupancy/${filename}`
-      }))
-
-    // Respond
-    const response: ProjectSpeciesRouteResponse = { predictedOccupancyMaps } // TODO ??? - return the rest of the data
-    return response
-  })
-
-  interface ProjectSpeciesPredictedOccupancyRoute {
-    Params: {
-      filename: string
-    }
-  }
-  app.get<ProjectSpeciesPredictedOccupancyRoute>('/projects/:projectId/predicted-occupancy/:filename', async (req, res): Promise<FastifyReply> => {
-    // Inputs & validation
-    const { filename } = req.params
-    if (!filename) throw ApiMissingParam('filename')
-
-    // Queries
-    const resolvedFilename = resolve(mockPredictionsFolderPath, filename)
-    if (!resolvedFilename.startsWith(mockPredictionsFolderPath)) throw ApiClientError()
-
-    // Respond
-    return await res.sendFile(mockPredictionsFolderName + '/' + filename + '.png')
   })
 }
--- apps/api/src/species/index.ts
+++ apps/api/src/species/index.ts
@@ -1,8 +1,15 @@
-import { FastifyPluginAsync } from 'fastify'
+import { FastifyPluginAsync, FastifyReply } from 'fastify'
+import { readdir } from 'fs/promises'
+import { resolve } from 'path'
 
+import { PredictedOccupancyMap, ProjectSpeciesRouteResponse } from '@rfcx-bio/common/api-bio/species/project-species'
 import { rawSpecies } from '@rfcx-bio/common/mock-data'
 
-import { ApiMissingParam, ApiNotFoundError } from '../_services/errors'
+import { ApiClientError, ApiMissingParam, ApiNotFoundError } from '../_services/errors'
+
+// TODO ??? - Move files to S3 & index them in the database
+const mockPredictionsFolderName = 'predicted-occupancy/puerto-rico'
+const mockPredictionsFolderPath = resolve('./public', mockPredictionsFolderName)
 
 export const routesSpecies: FastifyPluginAsync = async (app, options): Promise<void> => {
   app.get('/species', async (req, res) => rawSpecies)
@@ -12,7 +19,6 @@ export const routesSpecies: FastifyPluginAsync = async (app, options): Promise<v
       speciesSlug?: string
     }
   }
-
   app.get<SpeciesRoute>('/species/:speciesSlug', async (req, res) => {
     // Inputs & validation
     const { speciesSlug } = req.params
@@ -24,5 +30,52 @@ export const routesSpecies: FastifyPluginAsync = async (app, options): Promise<v
     // Response
     if (!matchesSpecies) throw ApiNotFoundError()
     return matchesSpecies
+  })
+
+  interface ProjectSpeciesRoute {
+    Params: {
+      projectId?: string
+      speciesSlug?: string
+    }
+  }
+  app.get<ProjectSpeciesRoute>('/projects/:projectId/species/:speciesSlug', async (req, res) => {
+    // Inputs & validation
+    const { projectId, speciesSlug } = req.params
+    if (!projectId) throw ApiMissingParam('projectId')
+    if (!speciesSlug) throw ApiMissingParam('speciesSlug')
+
+    // Queries
+    const predictedOccupancyMaps: PredictedOccupancyMap[] = (await readdir(mockPredictionsFolderPath))
+      .filter(filename => filename.startsWith(speciesSlug))
+      .map(filename => filename.substr(0, filename.lastIndexOf('.')) || filename)
+      .sort()
+      .map(filename => ({
+        title: filename,
+        url: `/projects/${projectId}/predicted-occupancy/${filename}`
+      }))
+
+    // Respond
+    const response: ProjectSpeciesRouteResponse = {
+      predictedOccupancyMaps
+    }
+    return response
+  })
+
+  interface ProjectSpeciesPredictedOccupancyRoute {
+    Params: {
+      filenameWithoutExtension: string
+    }
+  }
+  app.get<ProjectSpeciesPredictedOccupancyRoute>('/projects/:projectId/predicted-occupancy/:filenameWithoutExtension', async (req, res): Promise<FastifyReply> => {
+    // Inputs & validation
+    const { filenameWithoutExtension } = req.params
+    if (!filenameWithoutExtension) throw ApiMissingParam('filename')
+
+    // Queries
+    const resolvedFilename = resolve(mockPredictionsFolderPath, filenameWithoutExtension)
+    if (!resolvedFilename.startsWith(mockPredictionsFolderPath)) throw ApiClientError()
+
+    // Respond
+    return await res.sendFile(mockPredictionsFolderName + '/' + filenameWithoutExtension + '.png')
   })
 }
