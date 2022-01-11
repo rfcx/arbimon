c045ea4002856257bb5be8a53ffe73634ceb7965
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-01-11 16:18:09 +0700

    [443] Add generate url to common and update base url initial style in spotlight service

--- apps/website/src/activity-patterns/services/index.ts
+++ apps/website/src/activity-patterns/services/index.ts
@@ -2,4 +2,4 @@ import { AssetsService } from './assets-service-api'
 import { SpotlightService } from './spotlight-services-api'
 
 export const assetsService = new AssetsService()
-export const spotlightService = new SpotlightService()
+export const spotlightService = new SpotlightService(import.meta.env.VITE_BIO_API_BASE_URL)
--- apps/website/src/activity-patterns/services/spotlight-services-api.ts
+++ apps/website/src/activity-patterns/services/spotlight-services-api.ts
@@ -1,10 +1,9 @@
 import { PredictedOccupancyMap, ProjectSpeciesRouteResponse } from '@rfcx-bio/common/api-bio/species/project-species'
 import { Species } from '@rfcx-bio/common/api-bio/species/species'
+import { speciesInformationGeneratedUrl } from '@rfcx-bio/common/api-bio/spotlight/species-information'
 
 import { apiClient } from '~/api'
 import { useStore } from '~/store'
-
-const BIO_API_BASE_URL: string = import.meta.env.VITE_BIO_API_BASE_URL // TODO ??? - Fix @typescript/eslint so it picks up vite-env.d.ts
 
 export interface ProjectSpecies {
   speciesInformation: Species | undefined
@@ -12,6 +11,8 @@ export interface ProjectSpecies {
 }
 
 export class SpotlightService {
+  constructor (private readonly baseUrl: string) {}
+
   async getSpeciesInformation (speciesSlug: string): Promise<ProjectSpecies | undefined> {
     if (!speciesSlug) return undefined
 
@@ -19,13 +20,13 @@ export class SpotlightService {
     const projectId = store.selectedProject?.id
     if (!projectId) return undefined
 
-    const url = `${BIO_API_BASE_URL}/projects/${projectId}/species/${speciesSlug}`
+    const url = `${this.baseUrl}${speciesInformationGeneratedUrl(projectId, speciesSlug)}`
     const data = await apiClient.getOrUndefined<ProjectSpeciesRouteResponse>(url)
     return {
       speciesInformation: data?.speciesInformation,
       predictedOccupancyMaps: data?.predictedOccupancyMaps.map(({ title, url }) => ({
         title,
-        url: `${BIO_API_BASE_URL}${url}`
+        url: `${this.baseUrl}${url}`
       })) ?? []
     }
   }
