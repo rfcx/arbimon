a5e42ab0f132c1476f0879ed8346bf96900e102c
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-01-11 15:45:56 +0700

    [443] Fix spotlight response type

--- apps/website/src/activity-patterns/services/spotlight-services-api.ts
+++ apps/website/src/activity-patterns/services/spotlight-services-api.ts
@@ -1,12 +1,18 @@
-import { ProjectSpeciesRouteResponse } from '@rfcx-bio/common/api-bio/species/project-species'
+import { PredictedOccupancyMap, ProjectSpeciesRouteResponse } from '@rfcx-bio/common/api-bio/species/project-species'
+import { Species } from '@rfcx-bio/common/api-bio/species/species'
 
 import { apiClient } from '~/api'
 import { useStore } from '~/store'
 
 const BIO_API_BASE_URL: string = import.meta.env.VITE_BIO_API_BASE_URL // TODO ??? - Fix @typescript/eslint so it picks up vite-env.d.ts
 
+export interface ProjectSpecies {
+  speciesInformation: Species | undefined
+  predictedOccupancyMaps: PredictedOccupancyMap[]
+}
+
 export class SpotlightService {
-  async getSpeciesInformation (speciesSlug: string): Promise<ProjectSpeciesRouteResponse | undefined> {
+  async getSpeciesInformation (speciesSlug: string): Promise<ProjectSpecies | undefined> {
     if (!speciesSlug) return undefined
 
     const store = useStore()
