34cb70a73b24294b5ffb9c7a2589553b584818b6
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-01-13 21:10:37 +0700

    [362] Enable screen view

--- apps/website/src/_services/analytics/index.ts
+++ apps/website/src/_services/analytics/index.ts
@@ -2,6 +2,7 @@ import { RouteLocationNormalized } from 'vue-router'
 
 export const ANALYTICS_CONFIGS = {
   appName: import.meta.env.VITE_APP_NAME,
+  pageTrackerScreenviewEnabled: true,
   config: {
     id: import.meta.env.VITE_GA_MEASUREMENT_ID
   },
