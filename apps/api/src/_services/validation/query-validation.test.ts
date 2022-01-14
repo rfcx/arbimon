787c8cd7c597f33c0d22dbcd3b6dffecd5a96bd5
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-01-14 09:08:20 +0700

    - Update the test case

--- apps/api/src/_services/validation/query-validation.test.ts
+++ apps/api/src/_services/validation/query-validation.test.ts
@@ -15,10 +15,32 @@ import { isValidDate } from './query-validation'
 describe('ISO date format', () => {
   test('Empty date', () => {
     // Arrange
-    const invalid = undefined
+    const invalidDate = undefined
 
     // Act
-    const date = isValidDate(invalid)
+    const date = isValidDate(invalidDate)
+
+    // Assert
+    expect(date).toBe(false)
+  })
+
+  test('Invalid string', () => {
+    // Arrange
+    const invalidDate = 'abcd'
+
+    // Act
+    const date = isValidDate(invalidDate)
+
+    // Assert
+    expect(date).toBe(false)
+  })
+
+  test('Valid date', () => {
+    // Arrange
+    const validDate = '2021-01-01T00:00:00.000Z'
+
+    // Act
+    const date = isValidDate(validDate)
 
     // Assert
     expect(date).toBe(false)
