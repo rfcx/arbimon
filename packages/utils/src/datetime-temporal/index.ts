d2241603ed6c53815e37de32829f6655c6e9ee64
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-01-17 10:30:25 +0700

    [CI] Avoid dayjs in API package

--- packages/utils/src/datetime-temporal/index.ts
+++
@@ -1,4 +0,0 @@
-import { Temporal } from '@js-temporal/polyfill'
-
-// slice to remove "Z" which PlainDate doesn't support
-export const plainDate = (utcString: string): Temporal.PlainDate => Temporal.PlainDate.from(utcString.slice(0, -1))
