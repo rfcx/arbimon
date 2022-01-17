b9ce8492fd077bdf3d7f4d16e841826269d30ba0
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-01-17 17:29:20 +0700

    [463] Fix objectToTs (& support type declaration)

--- packages/utils/src/file/json-to-ts.ts
+++ packages/utils/src/file/json-to-ts.ts
@@ -1,10 +1,14 @@
-export const objectToTs = (data: any, constName: string): string =>
-  jsonToTs(JSON.stringify(data, undefined, 2), constName)
+const LINE_BREAK = '\r\n'
 
-export const jsonToTs = (data: string, constName: string): string =>
-  'export const ' +
-  constName +
-  ' = ' +
-  data.replaceAll('": "', '\': \'')
-    .replaceAll('  "', '  \'')
-    .replaceAll('",', '\',')
+export const objectToTs = (data: any, constName: string, type?: string, ...importLines: string[]): string =>
+  jsonToTs(JSON.stringify(data, undefined, 2), constName, type, ...importLines)
+
+const jsonToTs = (data: string, constName: string, type?: string, ...importLines: string[]): string => {
+  const imports = importLines.length > 0 ? `${importLines.join(LINE_BREAK)}${LINE_BREAK}${LINE_BREAK}` : ''
+  const declaration = `export const ${constName}${type ? ': ' + type : ''} = `
+  const object = data
+    .replace(/^(\s*)"(\w*)":/gm, '$1$2:')
+    .replace(/(:\s*)"(\w*)"/gm, '$1\'$2\'')
+
+  return `${imports}${declaration}${object}${LINE_BREAK}`
+}
