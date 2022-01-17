6caa759d458604364be30bfdc572649ddb423109
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-01-17 19:42:35 +0700

    [463] Make mode message generic

--- apps/cli/src/_services/modes/index.ts
+++ apps/cli/src/_services/modes/index.ts
@@ -1,7 +1,7 @@
 const MODE = process.argv.find(arg => arg.startsWith('--mode='))?.split('=')[1].toLowerCase() ?? 'dev'
 const MODE_PRODUCTION = 'production'
 
-console.info(`Running species data lookup in ${MODE} mode`)
+console.info(`*** Biodiversity CLI ***\nRunning in ${MODE.toUpperCase()} mode`)
 
 export const limitUnlessProduction = <T> (input: T[]): T[] =>
   MODE === MODE_PRODUCTION
