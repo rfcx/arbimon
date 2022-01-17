92cafa52a6c38523b04623241ea72b6836845520
Chanachai Yaemban <75108999+Crave61341416@users.noreply.github.com>
2022-01-17 09:44:10 +0700

    Fix activity api import

--- apps/api/src/activity/index.ts
+++ apps/api/src/activity/index.ts
@@ -1,6 +1,6 @@
 import { activityDatasetRoute } from '@rfcx-bio/common/api-bio/activity/activity-dataset'
 
-import { GET, RouteRegistration } from '~/api-helper/types'
+import { GET, RouteRegistration } from '../_services/api-helper/types'
 import { activityDatasetController } from './controller-activity-dataset'
 
 export const routesActivity: RouteRegistration[] = [
