3a60a8ae61c077e881fd0f5f8a147a561eb2513c
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-01-17 10:30:25 +0700

    Revert "[CI] Avoid dayjs in API package"
    This reverts commit 7bfcd1e71c3fe6c5a6f2ade927bd3969ca5a0878.

--- apps/api/src/_services/mock-helper/index.ts
+++ apps/api/src/_services/mock-helper/index.ts
@@ -1,6 +1,5 @@
-import { Temporal } from '@js-temporal/polyfill'
-
 import { MockHourlyDetectionSummary } from '@rfcx-bio/common/mock-data'
+import { plainDate } from '@rfcx-bio/utils/datetime-temporal'
 
 export interface FilterDataset {
   startDateUtc: string
@@ -12,23 +11,11 @@ export interface FilterDataset {
 export const filterMocksByParameters = (detections: MockHourlyDetectionSummary[], datasetParams: FilterDataset, speciesIds: number[] = []): MockHourlyDetectionSummary[] => {
   const { startDateUtc, endDateUtc, siteIds, taxons } = datasetParams
 
-  // Strip time
-  const startDateUtcInclusive = Temporal.Instant.from(startDateUtc)
-    .toZonedDateTimeISO('UTC')
-    .with({ hour: 0, minute: 0, second: 0, millisecond: 0 })
-    .toInstant()
-    .toString()
-
-  // Strip time & add 1 day to make it exclusive
-  const endDateUtcExclusive = Temporal.Instant.from(endDateUtc)
-    .toZonedDateTimeISO('UTC')
-    .with({ hour: 0, minute: 0, second: 0, millisecond: 0 })
-    .add({ days: 1 })
-    .toInstant()
-    .toString()
+  // Add 1 day to make it exclusive
+  const endDateUtcExclusive = plainDate(endDateUtc).add({ days: 1 }).toString()
 
   return detections.filter(r =>
-    r.date >= startDateUtcInclusive &&
+    r.date >= startDateUtc &&
     r.date < endDateUtcExclusive &&
     (siteIds.length === 0 || siteIds.includes(r.stream_id)) &&
     (taxons.length === 0 || taxons.includes(r.taxon)) &&
