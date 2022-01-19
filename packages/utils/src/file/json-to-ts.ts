e7275fefbbbf0173fbfff241ac50f443296d0344
Somjintana Korbut <nuisomjin@gmail.com>
2022-01-19 13:39:49 +0700

    [CLI] combine to-json with to-mock file

--- packages/utils/src/file/json-to-ts.ts
+++ packages/utils/src/file/json-to-ts.ts
@@ -3,7 +3,7 @@ const LINE_BREAK = '\n'
 export const objectToTs = (data: any, constName: string, type?: string, ...importLines: string[]): string =>
   jsonToTs(JSON.stringify(data, undefined, 2), constName, type, ...importLines)
 
-const jsonToTs = (data: string, constName: string, type?: string, ...importLines: string[]): string => {
+export const jsonToTs = (data: string, constName: string, type?: string, ...importLines: string[]): string => {
   const imports = importLines.length > 0 ? `${importLines.join(LINE_BREAK)}${LINE_BREAK}${LINE_BREAK}` : ''
   const declaration = `export const ${constName}${type ? ': ' + type : ''} = `
   const object = data
