c79613d09ff1f4492ccf26027d4ead7404254660
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-01-19 17:23:28 +0700

    [458] Add custom download icon and update ux ui for export logic

--- apps/website/src/_components/export-button.vue
+++ apps/website/src/_components/export-button.vue
@@ -3,20 +3,7 @@
     :disabled="disabled"
     class="btn btn-icon flex flex-row"
   >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      class="h-4 w-4 self-center"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
-      />
-    </svg>
+    <icon-custom-download />
     <slot name="label" />
   </button>
 </template>
