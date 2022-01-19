0c32ac06bca7f939276806c15ab7ff3e0d8d1457
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-01-19 17:46:42 +0700

    [463] Fix JSON to TS parsing (again)

--- packages/utils/src/file/json-to-ts.ts
+++ packages/utils/src/file/json-to-ts.ts
@@ -7,9 +7,10 @@ export const jsonToTs = (data: string, constName: string, type?: string, ...impo
   const imports = importLines.length > 0 ? `${importLines.join(LINE_BREAK)}${LINE_BREAK}${LINE_BREAK}` : ''
   const declaration = `export const ${constName}${type ? ': ' + type : ''} = `
   const object = data
-    .replace(/'/gm, "\\'")
-    .replace(/^(\s*)"(\w*)":/gm, '$1$2:')
-    .replace(/(:\s*)"([^"]*)"/gm, '$1\'$2\'')
+    .replace(/'/gm, "\\'") // escape single quotes
+    .replace(/^(\s*)"(\w*)":/gm, '$1$2:') // remove quotes around keys
+    .replace(/(:\s*)"(.*)"(,)?$/gm, '$1\'$2\'$3') // convert double quotes to single around values
+    .replace(/\\"/gm, '"') // unescape double quotes
 
   return `${imports}${declaration}${object}${LINE_BREAK}`
 }
