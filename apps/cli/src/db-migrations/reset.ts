c3bb04101aa5e913a08064f69cf5c1b07f3847ba
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-01-24 12:47:49 +0700

    [489] Add a script to reset databases

---
+++ apps/cli/src/db-migrations/reset.ts
@@ -0,0 +1,24 @@
+import { QueryTypes } from 'sequelize'
+
+import { getSequelize } from './helpers'
+
+const verbose = process.argv.some(arg => arg === '--verbose')
+
+const main = async (): Promise<void> => {
+  const sequelize = getSequelize(verbose)
+
+  // Drop all public tables
+  const tables = await sequelize.query<{ tablename: string }>('SELECT tablename FROM pg_tables WHERE schemaname = \'public\'', { type: QueryTypes.SELECT })
+  console.log(tables.map(t => t.tablename))
+  for (const table of tables) {
+    await sequelize.query(`DROP TABLE IF EXISTS public.${table.tablename} CASCADE`)
+  }
+
+  // Drop sequelize_meta
+  await sequelize.query('DROP TABLE IF EXISTS sequelize.sequelize_meta CASCADE')
+
+  await sequelize.close()
+}
+
+await main()
+  .catch(e => console.error(e))
