78d77d6ffd6c990ea6c1f6baaf51c546f65a0e12
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-01-26 00:38:22 +0700

    [489] Clean up logging

--- apps/cli/src/db-migrations/reset.ts
+++ apps/cli/src/db-migrations/reset.ts
@@ -9,12 +9,13 @@ const main = async (): Promise<void> => {
 
   // Drop all public tables
   const tables = await sequelize.query<{ tablename: string }>('SELECT tablename FROM pg_tables WHERE schemaname = \'public\'', { type: QueryTypes.SELECT })
-  console.log(tables.map(t => t.tablename))
   for (const table of tables) {
+    console.info(`Drop if exists: public.${table.tablename}`)
     await sequelize.query(`DROP TABLE IF EXISTS public.${table.tablename} CASCADE`)
   }
 
   // Drop sequelize_meta
+  console.info('Drop if exists: sequelize.sequelize_meta')
   await sequelize.query('DROP TABLE IF EXISTS sequelize.sequelize_meta CASCADE')
 
   await sequelize.close()
