75269d52b4d518a5cfa3d8cf0dd17dcd44b83209
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-01-28 13:17:33 +0700

    [497] Move species richness logic to the api

--- apps/api/src/richness/controller-richness-dataset.ts
+++ apps/api/src/richness/controller-richness-dataset.ts
@@ -1,8 +1,12 @@
 import { filterMocksByParameters } from '_services/mock-helper'
-import { groupBy, mapValues } from 'lodash-es'
+import dayjs from 'dayjs'
+import { groupBy, keyBy, mapValues } from 'lodash-es'
 
+import { MapSiteData, TimeBucket } from '@rfcx-bio/common/api-bio/richness/common'
 import { RichnessDatasetParams, RichnessDatasetQuery, RichnessDatasetResponse } from '@rfcx-bio/common/api-bio/richness/richness-dataset'
-import { MockHourlyDetectionSummary, rawDetections } from '@rfcx-bio/common/mock-data'
+import { SpeciesLight } from '@rfcx-bio/common/api-bio/species/common'
+import { MockHourlyDetectionSummary, rawDetections, rawSpecies } from '@rfcx-bio/common/mock-data'
+import { groupByNumber } from '@rfcx-bio/utils/lodash-ext'
 
 import { Handler } from '../_services/api-helper/types'
 import { assertInvalidQuery, assertParamsExist } from '../_services/validation'
@@ -33,7 +37,10 @@ export const RichnessHandler: Handler<RichnessDatasetResponse, RichnessDatasetPa
 async function getRichnessDatasetInformation (detections: MockHourlyDetectionSummary[]): Promise<RichnessDatasetResponse> {
   return {
     detectionCount: detections.length,
-    speciesByTaxon: getSpeciesByTaxon(detections)
+    speciesByTaxon: getSpeciesByTaxon(detections),
+    speciesBySite: getSpeciesBySite(detections),
+    speciesByTime: getSpeciesByTime(detections),
+    speciesPresence: getSpeciesPresence(detections)
   }
 }
 
@@ -42,4 +49,37 @@ const calculateSpeciesRichness = (detections: MockHourlyDetectionSummary[]): num
 const getSpeciesByTaxon = (detections: MockHourlyDetectionSummary[]): { [taxon: string]: number } => {
   const detectionsByTaxon = groupBy(detections, 'taxon')
   return mapValues(detectionsByTaxon, calculateSpeciesRichness)
+}
+
+const getSpeciesBySite = (detections: MockHourlyDetectionSummary[]): MapSiteData[] => {
+  const detectionsBySite = groupBy(detections, 'name') // TODO ?? - Extract field names
+  const mapDataBySite = mapValues(detectionsBySite, (detections, siteName) => ({
+    siteName,
+    longitude: detections[0].lon,
+    latitude: detections[0].lat,
+    distinctSpecies: mapValues(groupBy(detections, 'taxon'), calculateSpeciesRichness)
+  }))
+
+  return Object.values(mapDataBySite)
+}
+
+const getSpeciesByTime = (detections: MockHourlyDetectionSummary[]): Record<TimeBucket, Record<number, number>> => {
+  const SECONDS_PER_DAY = 86400 // 24 * 60 * 60
+
+  return {
+    hourOfDay: mapValues(groupByNumber(detections, d => d.hour), calculateSpeciesRichness),
+    dayOfWeek: mapValues(groupByNumber(detections, d => dayjs.utc(d.date).isoWeekday() - 1), calculateSpeciesRichness),
+    monthOfYear: mapValues(groupByNumber(detections, d => dayjs.utc(d.date).month()), calculateSpeciesRichness),
+    dateSeries: mapValues(groupByNumber(detections, d => dayjs.utc(d.date).startOf('day').unix() / SECONDS_PER_DAY), calculateSpeciesRichness)
+  }
+}
+
+const getSpeciesPresence = (detections: MockHourlyDetectionSummary[]): { [speciesId: string]: SpeciesLight } => {
+  const speciesIds = new Set(detections.map(d => d.species_id))
+
+  const species = rawSpecies
+    .filter(s => speciesIds.has(s.speciesId))
+    .map(({ speciesId, speciesSlug, scientificName, commonName, taxon }) =>
+      ({ speciesId, speciesSlug, scientificName, commonName, taxon }))
+    return keyBy(species, 'speciesId')
 }
