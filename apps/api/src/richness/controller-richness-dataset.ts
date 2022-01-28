7fa29cf3ba545105b29bf53839bf727ed09415eb
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-01-28 13:10:19 +0700

    [497] Add richness response logic

--- apps/api/src/richness/controller-richness-dataset.ts
+++ apps/api/src/richness/controller-richness-dataset.ts
@@ -1,17 +1,45 @@
+import { filterMocksByParameters } from '_services/mock-helper'
+import { groupBy, mapValues } from 'lodash-es'
+
 import { RichnessDatasetParams, RichnessDatasetQuery, RichnessDatasetResponse } from '@rfcx-bio/common/api-bio/richness/richness-dataset'
+import { MockHourlyDetectionSummary, rawDetections } from '@rfcx-bio/common/mock-data'
 
 import { Handler } from '../_services/api-helper/types'
-import { assertParamsExist } from '../_services/validation'
+import { assertInvalidQuery, assertParamsExist } from '../_services/validation'
+import { isValidDate } from '../_services/validation/query-validation'
 
 export const RichnessHandler: Handler<RichnessDatasetResponse, RichnessDatasetParams, RichnessDatasetQuery> = async (req) => {
   // Inputs & validation
   const { projectId } = req.params
   assertParamsExist({ projectId })
 
-  // Query
-  const filteredDetections = ''
+  const { startDate: startDateUtcInclusive, endDate: endDateUtcInclusive, siteIds, taxons } = req.query
+  if (!isValidDate(startDateUtcInclusive)) assertInvalidQuery({ startDateUtcInclusive })
+  if (!isValidDate(endDateUtcInclusive)) assertInvalidQuery({ endDateUtcInclusive })
 
-  return {
-    detectionCount: 0
+  // Query
+  const convertedQuery = {
+    startDateUtcInclusive,
+    endDateUtcInclusive,
+    siteIds: siteIds ?? [],
+    taxons: taxons ?? []
   }
+
+  const detections = filterMocksByParameters(rawDetections, { ...convertedQuery })
+
+  return await getRichnessDatasetInformation(detections)
+}
+
+async function getRichnessDatasetInformation (detections: MockHourlyDetectionSummary[]): Promise<RichnessDatasetResponse> {
+  return {
+    detectionCount: detections.length,
+    speciesByTaxon: getSpeciesByTaxon(detections)
+  }
+}
+
+const calculateSpeciesRichness = (detections: MockHourlyDetectionSummary[]): number => new Set(detections.map(d => d.species_id)).size
+
+const getSpeciesByTaxon = (detections: MockHourlyDetectionSummary[]): { [taxon: string]: number } => {
+  const detectionsByTaxon = groupBy(detections, 'taxon')
+  return mapValues(detectionsByTaxon, calculateSpeciesRichness)
 }
