1c27035439c39aa0c1320227ac1005cd3afb6e69
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-01-31 17:16:08 +0700

    [497] Fix the handler name

--- apps/api/src/richness/index.ts
+++ apps/api/src/richness/index.ts
@@ -3,14 +3,14 @@ import { richnessByExportRoute } from '@rfcx-bio/common/api-bio/richness/richnes
 
 import { GET, RouteRegistration } from '../_services/api-helpers/types'
 import { verifyProjectUserPermission } from '../_services/decorators'
-import { RichnessDatasetHandler } from './controller-richness-dataset'
-import { RichnessExportHandler } from './controller-richness-export'
+import { richnessDatasetHandler } from './controller-richness-dataset'
+import { richnessExportHandler } from './controller-richness-export'
 
 export const routesRichness: RouteRegistration[] = [
   {
     method: GET,
     url: richnessDatasetRoute,
-    handler: RichnessDatasetHandler,
+    handler: richnessDatasetHandler,
     preHandler: [
       verifyProjectUserPermission
     ]
@@ -18,7 +18,7 @@ export const routesRichness: RouteRegistration[] = [
   {
     method: GET,
     url: richnessByExportRoute,
-    handler: RichnessExportHandler,
+    handler: richnessExportHandler,
     preHandler: [
       verifyProjectUserPermission
     ]
