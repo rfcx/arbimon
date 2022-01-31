cbbc08c581ca0a310f189888ba802fc07cbb3428
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-01-31 17:14:29 +0700

    [468] Fix the error message

--- apps/api/src/_services/errors/data-access-errors.ts
+++ apps/api/src/_services/errors/data-access-errors.ts
@@ -1,20 +1,20 @@
 export class Unauthorized extends Error {
-  constructor (message: string = '') {
-    super(message)
+  constructor () {
+    super('unauthorized')
     this.name = 'UnauthorizedError'
   }
 }
 
 export class Forbidden extends Error {
-  constructor (message: string = '') {
-    super(message)
+  constructor () {
+    super('forbidden')
     this.name = 'ForbiddenError'
   }
 }
 
 export class NotFound extends Error {
-  constructor (message: string = '') {
-    super(message)
+  constructor () {
+    super('notfound')
     this.name = 'NotFoundError'
   }
 }
