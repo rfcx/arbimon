7475d81ce88f2e004b3b171c3bb60b63fc7e359a
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-01-31 14:57:44 +0700

    [497] Remove the confilct type

--- apps/api/src/richness/bll-richness-export.ts
+++ apps/api/src/richness/bll-richness-export.ts
@@ -4,17 +4,10 @@ import { RichnessByExportResponse } from '@rfcx-bio/common/api-bio/richness/rich
 import { EXTINCTION_RISK_PROTECTED_CODES } from '@rfcx-bio/common/iucn'
 import { rawDetections, rawSpecies } from '@rfcx-bio/common/mock-data'
 
-import { filterMocksByParameters } from '../_services/mock-helper'
+import { FilterDataset, filterMocksByParameters } from '../_services/mock-helper'
 import { isProjectMember } from '../_services/permission-helper/permission-helper'
 
-interface RichnessQuery {
-  startDateUtcInclusive: string
-  endDateUtcInclusive: string
-  siteIds: string[]
-  taxons: string[]
-}
-
-export async function getRichnessDatasetInformation (req: FastifyRequest, query: RichnessQuery): Promise<RichnessByExportResponse> {
+export async function getRichnessDatasetInformation (req: FastifyRequest, query: FilterDataset): Promise<RichnessByExportResponse> {
   const noPermission = !isProjectMember(req)
   const detections = filterMocksByParameters(rawDetections, { ...query })
 
