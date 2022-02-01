eb6173ae43cdae9dcf1a34689b2f5e012e8ecbd1
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-02-01 21:58:34 +0700

    [490] Seeders for taxon class & species

---
+++ apps/cli/src/db/seeders/auto/04-taxon-species.ts
@@ -0,0 +1,43 @@
+import { Optional, QueryInterface } from 'sequelize'
+import { MigrationFn } from 'umzug'
+
+import { TaxonSpeciesModel } from '@rfcx-bio/common/dao/models/taxon-species-model'
+import { TaxonSpecies } from '@rfcx-bio/common/domain'
+import { rawSpecies, TAXONOMY_CLASSES } from '@rfcx-bio/common/mock-data'
+
+export const up: MigrationFn<QueryInterface> = async (params): Promise<void> => {
+  const model = TaxonSpeciesModel(params.context.sequelize)
+
+  const taxonClassArbimonToBio = Object.fromEntries(
+    TAXONOMY_CLASSES.map(t => [t.idArbimon, t.id])
+  )
+
+  // TODO: Fix source data
+  const data: Array<Optional<TaxonSpecies, 'id'>> =
+    rawSpecies.map(s => ({
+      idArbimon: s.speciesId,
+      slug: s.speciesSlug,
+      taxonClassId: taxonClassArbimonToBio[s.taxonId],
+      scientificName: s.scientificName,
+      commonName: s.commonName,
+      extinctionRiskRating: s.extinctionRisk,
+      extinctionRiskRatingSource: '', // WRONG
+      description: s.information[0]?.description ?? '', // WRONG
+      descriptionSource: s.information[0]?.sourceType ?? '', // WRONG
+      descriptionSourceUrl: s.information[0]?.sourceUrl ?? '', // WRONG
+      callProjectId: 1,
+      callSiteId: 1, // WRONG
+      callType: s.speciesCall?.songType ?? '', // WRONG
+      callRecordedAt: new Date(), // WRONG
+      callTimezone: s.speciesCall?.timezone ?? '', // WRONG
+      callMediaWavUrl: s.speciesCall?.mediaWavUrl ?? '', // WRONG
+      callMediaSpecUrl: s.speciesCall?.mediaSpecUrl ?? '', // WRONG
+      photoUrl: s.thumbnailImageUrl ?? '', // WRONG
+      photoCaption: s.imageCaption,
+      photoAuthor: '', // Doesn't exist yet
+      photoLicense: '', // Doesn't exist yet
+      photoLicenseUrl: '' // Doesn't exist yet
+    }))
+
+  await model.bulkCreate(data)
+}
