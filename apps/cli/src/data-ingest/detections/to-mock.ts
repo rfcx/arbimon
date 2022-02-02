9217cc1abadbfef10c7af2ead2ad779eb12144de
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-02-02 20:41:57 +0700

    [Data] Merge JSON/TS export

--- apps/cli/src/data-ingest/detections/to-mock.ts
+++ apps/cli/src/data-ingest/detections/to-mock.ts
@@ -1,19 +1,22 @@
 import * as fs from 'fs'
 import { resolve } from 'path'
 
-import { objectToTs } from '@rfcx-bio/utils/file/json-to-ts'
+import { jsonToTs } from '@rfcx-bio/utils/file/json-to-ts'
 
-import { getMockDataDirectory } from '../../_services/output'
+import { getJsonOutputDirectory, getMockDataDirectory } from '../../_services/output'
 import { getArbimonDetectionSummaries } from './input-arbimon'
 
-const outputPath = resolve(getMockDataDirectory(), './raw-detections-by-hour.ts')
+const outputJsonPath = resolve(getJsonOutputDirectory(), './raw-detections-by-hour.json')
+const outputTsPath = resolve(getMockDataDirectory(), './raw-detections-by-hour.ts')
 const outputTsConstName = 'rawDetections'
 
 const main = async (): Promise<void> => {
   const detections = await getArbimonDetectionSummaries()
-  const outputTs = objectToTs(detections, outputTsConstName, 'MockHourlyDetectionSummary[]', 'import { MockHourlyDetectionSummary } from \'./types\'')
-  fs.writeFileSync(outputPath, outputTs, 'utf8')
-  console.info(`Finished writing to ${outputPath}`)
+  const outputJson = JSON.stringify(detections, undefined, 2)
+  const outputTs = jsonToTs(outputJson, outputTsConstName, 'MockHourlyDetectionSummary[]', 'import { MockHourlyDetectionSummary } from \'./types\'')
+  fs.writeFileSync(outputJsonPath, outputJson, 'utf8')
+  fs.writeFileSync(outputTsPath, outputTs, 'utf8')
+  console.info(`Finished writing to\n- ${outputJsonPath}\n- ${outputTsPath}`)
   process.exit(0)
 }
 
