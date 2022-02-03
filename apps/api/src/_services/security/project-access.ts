7b529f59c6a656c6b1004f5d8886b6981e0890c6
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-02-03 12:30:21 +0700

    [Auth] 'Bearer ' is not a valid token; Improve readability

--- apps/api/src/_services/security/project-access.ts
+++ apps/api/src/_services/security/project-access.ts
@@ -1,14 +1,19 @@
 import { getProjectPermission } from '../api-core/api-core'
 import { Forbidden } from '../errors/data-access-errors'
 
+export const isValidToken = (token: string): boolean => /^Bearer ./i.test(token) // at least 1 character after space
+
 export async function hasAccessToProject (id: string, token: string): Promise<boolean> {
-  if (!token.startsWith('Bearer ')) return false
+  if (!isValidToken(token)) return false
 
   return await getProjectPermission(id, token)
     .then(() => true)
     .catch(err => {
       // Forbidden is expected (it means the user is not a project member)
-      if (!(err instanceof Forbidden)) console.error(err)
+      if (err instanceof Forbidden) return false
+
+      // Log unexpected errors
+      console.error(err)
       return false
     })
 }
