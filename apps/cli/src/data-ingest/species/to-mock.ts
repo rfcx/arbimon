102970fc6dc3e2844675b4c59f3b4e5eedd5d055
Chanachai Yaemban <75108999+Crave61341416@users.noreply.github.com>
2022-02-08 14:40:56 +0700

    CLI refactor output function to take input as parameters

--- apps/cli/src/data-ingest/species/to-mock.ts
+++ apps/cli/src/data-ingest/species/to-mock.ts
@@ -1,11 +1,16 @@
 import * as fs from 'fs'
 import { resolve } from 'path'
 
+import { getSequentially } from '@rfcx-bio/utils/async'
 import { jsonToTs } from '@rfcx-bio/utils/file/json-to-ts'
 
-import { limitUnlessProtected } from '../../_services/env'
-import { getJsonOutputDirectory, getMockDataDirectory } from '../../_services/output'
-import { getScientificNamesFromMock } from './input-from-mock-detections'
+import { limitUnlessProtected } from '~/env'
+import { getJsonOutputDirectory, getMockDataDirectory } from '~/output'
+import { getArbimonSpeciesCalls } from './input-arbimon-species-call'
+import { getArbimonSpeciesFromMock, getScientificNamesFromMock } from './input-from-mock-detections'
+import { getIucnSpecies, getIucnSpeciesNarrative } from './input-iucn'
+import { getRfcxSpecies } from './input-rfcx'
+import { getWikiSpecies } from './input-wiki'
 import { getMergedSpecies } from './output-mock'
 
 const outputJsonPath = resolve(getJsonOutputDirectory(), './raw-species.json')
@@ -13,8 +18,17 @@ const outputTsPath = resolve(getMockDataDirectory(), './raw-species.ts')
 const outputTsConstName = 'rawSpecies'
 
 const main = async (): Promise<void> => {
+    // Get data from other sources
   const scientificNames = limitUnlessProtected(getScientificNamesFromMock())
-  const species = await getMergedSpecies(scientificNames)
+  const [arbimonSpeciesKeyed, arbimonSpeciesCallsKeyed, iucnSpeciesKeyed, iucnSpeciesNarrativesKeyed, rfcxSpeciesKeyed, wikiSpeciesKeyed] = await Promise.all([
+    getArbimonSpeciesFromMock(),
+    getArbimonSpeciesCalls(),
+    getSequentially(scientificNames, getIucnSpecies),
+    getSequentially(scientificNames, getIucnSpeciesNarrative),
+    getRfcxSpecies(),
+    getSequentially(scientificNames, getWikiSpecies)
+  ])
+  const species = await getMergedSpecies(scientificNames, arbimonSpeciesKeyed, arbimonSpeciesCallsKeyed, iucnSpeciesKeyed, iucnSpeciesNarrativesKeyed, rfcxSpeciesKeyed, wikiSpeciesKeyed)
 
   const outputJson = JSON.stringify(species, undefined, 2)
   const outputTs = jsonToTs(outputJson, outputTsConstName, 'Species[]', 'import { Species } from \'../api-bio/species/common\'')
