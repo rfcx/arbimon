d7d75cc27aee8d816fe6d8d0a0ea7fa1b9c4f4b9
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-02-08 18:46:01 +0700

    [425] Update logic to protect predicted occupancy image

--- apps/api/src/species/controller-species-prediction-occupancy.ts
+++ apps/api/src/species/controller-species-prediction-occupancy.ts
@@ -2,22 +2,26 @@ import { FastifyReply } from 'fastify'
 import { resolve } from 'path'
 
 import { SpeciesPredictionOccupancyParams } from '@rfcx-bio/common/api-bio/species/species-prediction-occupancy'
+import { EXTINCTION_RISK_PROTECTED_CODES } from '@rfcx-bio/common/iucn'
+import { rawSpecies } from '@rfcx-bio/common/mock-data'
 
 import { Handler } from '../_services/api-helpers/types'
-import { ApiPermissionDenied } from '../_services/errors'
+import { ApiClientError, ApiPermissionDenied } from '../_services/errors'
 import { isProjectMember } from '../_services/permission-helper/permission-helper'
 import { assertInvalidQuery, assertParamsExist } from '../_services/validation'
 import { mockPredictionsFolderName, mockPredictionsFolderPath } from './index'
 
 export const speciesPredictionOccupancyHandler: Handler<FastifyReply, SpeciesPredictionOccupancyParams> = async (req, res) => {
   // Inputs & validation
-  const { projectId } = req.params
-  assertParamsExist({ projectId })
+  const { projectId, speciesSlug, filenameWithoutExtension } = req.params
+  console.log({ params: req.params })
+  assertParamsExist({ projectId, speciesSlug, filenameWithoutExtension })
 
-  const { filenameWithoutExtension } = req.params
-  assertParamsExist({ filenameWithoutExtension })
+  const species = rawSpecies.find(({ speciesSlug: sp }) => sp === speciesSlug)
+  if (!species) throw ApiClientError('speciesSlug', speciesSlug)
 
-  const isLocationRedacted = !isProjectMember(req)
+  const protectedSpecies = EXTINCTION_RISK_PROTECTED_CODES.includes(species.extinctionRisk)
+  const isLocationRedacted = !isProjectMember(req) && protectedSpecies
   if (isLocationRedacted) throw ApiPermissionDenied()
 
   // Query
