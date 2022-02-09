ab08dd1df3328716d2a016291a21288e63841c94
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-02-09 12:25:21 +0700

    [557] Move the project attribute to be constant

--- apps/api/src/projects/controller-projects-all.ts
+++ apps/api/src/projects/controller-projects-all.ts
@@ -1,12 +1,12 @@
 import { ProjectsResponse } from '@rfcx-bio/common/api-bio/common/projects'
-import { ProjectModel } from '@rfcx-bio/common/dao/models/location-project-model'
+import { PROJECT_MODEL_ATTRIBUTES, ProjectModel } from '@rfcx-bio/common/dao/models/location-project-model'
 
 import { Handler } from '../_services/api-helpers/types'
 import { getSequelize } from '../_services/db'
 
 export const projectsAllHandler: Handler<ProjectsResponse> = async () => {
   const projects = await ProjectModel(getSequelize()).findAll({
-    attributes: ['id', 'slug', 'name', 'latitudeNorth', 'latitudeSouth', 'longitudeEast', 'longitudeWest']
+    attributes: PROJECT_MODEL_ATTRIBUTES.light
   })
   return projects
 }
