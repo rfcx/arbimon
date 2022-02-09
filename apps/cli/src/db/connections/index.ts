b847c44b9e9e212036487bdd6c024da92c40c30a
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-02-09 11:47:27 +0700

    [560] Extract sequelize to common; Connect from API

--- apps/cli/src/db/connections/index.ts
+++ apps/cli/src/db/connections/index.ts
@@ -1,8 +1,10 @@
 import { dirname } from 'path'
 import { resolve } from 'path/posix'
-import { Options, QueryInterface, Sequelize } from 'sequelize'
+import { QueryInterface, Sequelize } from 'sequelize'
 import { RunnableMigration, SequelizeStorage, Umzug } from 'umzug'
 import { fileURLToPath } from 'url'
+
+import { getSequelize as getSequelizeBase } from '@rfcx-bio/common/dao/connections'
 
 import { requireEnv } from '~/env'
 
@@ -11,42 +13,14 @@ const currentDir = dirname(fileURLToPath(import.meta.url))
 const migrationsDir = resolve(currentDir, '../../db/migrations')
 
 // TODO: This should be injected by the script controller
-const { BIO_DB_HOSTNAME, BIO_DB_PORT, BIO_DB_SSL_ENABLED, BIO_DB_DBNAME, BIO_DB_USER, BIO_DB_PASSWORD } =
+const { BIO_DB_HOSTNAME: host, BIO_DB_PORT: port, BIO_DB_SSL_ENABLED: isSsl, BIO_DB_DBNAME: databaseName, BIO_DB_USER: user, BIO_DB_PASSWORD: password } =
   requireEnv('BIO_DB_HOSTNAME', 'BIO_DB_PORT', 'BIO_DB_SSL_ENABLED', 'BIO_DB_DBNAME', 'BIO_DB_USER', 'BIO_DB_PASSWORD')
+
+export const getSequelize = (verbose = false): Sequelize =>
+  getSequelizeBase({ host, port, databaseName, user, password, isSsl, verbose })
 
 const importMigration = async (path?: string): Promise<Pick<RunnableMigration<QueryInterface>, 'up' | 'down'>> =>
   (await import(`file:///${(path ?? '').replace(/\\/g, '/')}`))
-
-export const getSequelize = (verbose = false): Sequelize => {
-  // Setup sequelize (ORM)
-  const sequelizeOptions: Options = {
-    host: BIO_DB_HOSTNAME,
-    port: BIO_DB_PORT,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: BIO_DB_SSL_ENABLED
-        ? {
-            require: true,
-            rejectUnauthorized: false // https://github.com/brianc/node-postgres/issues/2009
-          }
-        : false
-    },
-    define: {
-      charset: 'utf8',
-      collate: 'utf8_general_ci',
-      timestamps: true,
-      underscored: true
-    },
-    logging: verbose
-  }
-
-  return new Sequelize(
-    BIO_DB_DBNAME,
-    BIO_DB_USER,
-    BIO_DB_PASSWORD,
-    sequelizeOptions
-  )
-}
 
 export const getUmzug = (sequelize: Sequelize, verbose = false, cwd = migrationsDir, filename?: string): Umzug<QueryInterface> =>
   new Umzug({
---
+++ packages/common/src/dao/connections/index.ts
@@ -0,0 +1,42 @@
+import { Options, Sequelize } from 'sequelize'
+
+interface BioDbOptions {
+  host: string
+  port: number
+  databaseName: string
+  user: string
+  password: string
+  isSsl: boolean
+  verbose: boolean
+}
+
+export const getSequelize = ({ host, port, databaseName, user, password, isSsl, verbose }: BioDbOptions): Sequelize => {
+  // Setup sequelize (ORM)
+  const sequelizeOptions: Options = {
+    host,
+    port,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: isSsl
+        ? {
+            require: true,
+            rejectUnauthorized: false // https://github.com/brianc/node-postgres/issues/2009
+          }
+        : false
+    },
+    define: {
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+      timestamps: true,
+      underscored: true
+    },
+    logging: verbose
+  }
+
+  return new Sequelize(
+    databaseName,
+    user,
+    password,
+    sequelizeOptions
+  )
+}
