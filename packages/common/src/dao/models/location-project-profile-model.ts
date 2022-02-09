ff8a090e853ac704c8cfc0a86cf79598ec55807d
Chanachai Yaemban <75108999+Crave61341416@users.noreply.github.com>
2022-02-09 13:52:54 +0700

    [441] Project profile type, model and seeder

---
+++ packages/common/src/dao/models/location-project-profile-model.ts
@@ -0,0 +1,23 @@
+import { DataTypes } from 'sequelize'
+
+import { ProjectProfile } from '@/dao/types/location-project-profile'
+import { defineWithDefaults } from '../helpers/defaults'
+import { TABLE_LOCATION_PROJECTS_PROFILE } from '../table-names'
+
+export const ProjectProfileModel = defineWithDefaults<ProjectProfile>(
+  'LocationProjectProfile',
+  {
+    // PK
+    locationProjectId: { // 1
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+
+    // Facts
+    summary: DataTypes.STRING(1023),
+    readme: DataTypes.TEXT
+  },
+  {
+    tableName: TABLE_LOCATION_PROJECTS_PROFILE
+  }
+)
---
+++ packages/common/src/dao/types/location-project-profile.ts
@@ -0,0 +1,5 @@
+export interface ProjectProfile {
+  locationProjectId: number
+  summary: string
+  readme: string
+}
