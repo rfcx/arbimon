e1da1c0a2fa91f19addd031edc36ba6188bb1ec0
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-02-09 08:24:17 +0700

    [551] Add indexes for materialized views

--- apps/cli/src/db/migrations/220209-20-species-in-project-mview.ts
+++ apps/cli/src/db/migrations/220209-20-species-in-project-mview.ts
@@ -7,8 +7,9 @@ import { QueryInterface } from 'sequelize'
 import { MigrationFn } from 'umzug'
 
 const VIEW_NAME = 'species_in_project'
+const INDEX_COLS = ['location_project_id', 'taxon_class_id', 'taxon_species_id']
 
-export const up: MigrationFn<QueryInterface> = async (params): Promise<unknown> =>
+export const up: MigrationFn<QueryInterface> = async (params): Promise<void> => {
   await params.context.sequelize.query(
     `
     create materialized view ${VIEW_NAME} as
@@ -18,6 +19,13 @@ export const up: MigrationFn<QueryInterface> = async (params): Promise<unknown> 
     GROUP BY d.location_project_id, ts.id;
     `
   )
+
+  for (const indexCol of INDEX_COLS) {
+    await params.context.sequelize.query(
+      `CREATE INDEX ${VIEW_NAME}_${indexCol}_idx ON ${VIEW_NAME}(${indexCol});`
+    )
+  }
+}
 
 export const down: MigrationFn<QueryInterface> = async (params) =>
   await params.context.sequelize.query(`DROP VIEW IF EXISTS ${VIEW_NAME};`)
