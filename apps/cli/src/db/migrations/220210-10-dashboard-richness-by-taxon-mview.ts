75c04c0f3d155ede805af09f2a1a85bbccf2f0ea
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-02-10 09:55:22 +0700

    [559] Create m-views for dashboard-generated queries

--- apps/cli/src/db/migrations/220210-10-dashboard-richness-by-taxon-mview.ts
+++ apps/cli/src/db/migrations/220210-10-dashboard-richness-by-taxon-mview.ts
@@ -6,23 +6,17 @@
 import { QueryInterface } from 'sequelize'
 import { MigrationFn } from 'umzug'
 
-const VIEW_NAME = 'location_project_metric'
-const INDEX_COLS = ['location_project_id']
+const VIEW_NAME = 'dashboard_richness_by_taxon'
+const INDEX_COLS = ['location_project_id', 'count']
 
 export const up: MigrationFn<QueryInterface> = async (params): Promise<void> => {
   await params.context.sequelize.query(
     `
     create materialized view ${VIEW_NAME} as
-    SELECT d.location_project_id,
-          Count(1) AS detection_count,
-          Count(distinct d.location_site_id) as site_count,
-          Count(distinct d.taxon_species_id) as species_count,
-          Count(distinct
-              CASE WHEN s.extinction_risk_rating IN ('CR', 'EN', 'VU') THEN d.taxon_species_id END
-          ) as species_threatened_count
-    FROM detection_by_site_species_hour d
-    INNER JOIN taxon_species s ON d.taxon_species_id = s.id
-    GROUP BY d.location_project_id
+    SELECT ps.location_project_id, tc.common_name, COUNT(1) AS count
+    FROM species_in_project ps
+    INNER JOIN taxon_class tc on ps.taxon_class_id = tc.id
+    GROUP BY ps.location_project_id, tc.id
     ;
     `
   )
