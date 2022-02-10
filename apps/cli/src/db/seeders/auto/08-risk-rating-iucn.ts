ba8fc11f69223d9e3a26209e27956168be7484ce
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-02-10 09:57:54 +0700

    [559] Sneakier PKs for IUCN risk ratings

--- apps/cli/src/db/seeders/auto/08-risk-rating-iucn.ts
+++ apps/cli/src/db/seeders/auto/08-risk-rating-iucn.ts
@@ -8,17 +8,17 @@ export const up: MigrationFn<QueryInterface> = async (params): Promise<void> => 
   const sequelize = params.context.sequelize
 
   const data: RiskRatingIucn[] = [
-    { id: 100, code: 'NE', isThreatened: false },
-    { id: 200, code: 'NA', isThreatened: false },
-    { id: 300, code: 'DD', isThreatened: false },
-    { id: 400, code: 'LC', isThreatened: false },
-    { id: 500, code: 'NT', isThreatened: false },
-    { id: 600, code: 'VU', isThreatened: true },
-    { id: 700, code: 'EN', isThreatened: true },
-    { id: 800, code: 'CR', isThreatened: true },
-    { id: 900, code: 'RE', isThreatened: false },
-    { id: 1000, code: 'EW', isThreatened: false },
-    { id: 1100, code: 'EX', isThreatened: false }
+    { id: -1, code: 'NE', isThreatened: false },
+    { id: 0, code: 'NA', isThreatened: false },
+    { id: 100, code: 'DD', isThreatened: false },
+    { id: 200, code: 'LC', isThreatened: false },
+    { id: 300, code: 'NT', isThreatened: false },
+    { id: 400, code: 'VU', isThreatened: true },
+    { id: 500, code: 'EN', isThreatened: true },
+    { id: 600, code: 'CR', isThreatened: true },
+    { id: 700, code: 'RE', isThreatened: false },
+    { id: 800, code: 'EW', isThreatened: false },
+    { id: 900, code: 'EX', isThreatened: false }
   ]
 
   await RiskRatingIucnModel(sequelize).bulkCreate(data)
