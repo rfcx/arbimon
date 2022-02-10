2b2da5f15da467f621d996482590d68335c44292
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-02-10 09:55:52 +0700

    [559] Seed IUCN risk ratings

---
+++ packages/common/src/dao/models/risk-rating-iucn-model.ts
@@ -0,0 +1,29 @@
+import { DataTypes } from 'sequelize'
+
+import { RiskRatingIucn } from '@/dao/types/risk-rating-iucn'
+import { defineWithDefaultsAutoPk } from '../helpers/defaults'
+import { TABLE_RISK_RATING_IUCN } from '../table-names'
+
+export const RiskRatingIucnModel = defineWithDefaultsAutoPk<RiskRatingIucn>(
+  'RiskRatingIucn',
+  {
+    // PK
+    id: { // 1
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    // SKs
+    code: { // CR
+      type: DataTypes.STRING(2),
+      unique: true
+    },
+
+    // Facts
+    isThreatened: DataTypes.BOOLEAN // true
+  },
+  {
+    tableName: TABLE_RISK_RATING_IUCN
+  }
+)
