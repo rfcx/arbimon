65eaa9d1e2f94a13c5261a9f3c2e29e363f491c0
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-02-14 16:12:19 +0700

    [570] Update metrics style and display info when hover logic

--- apps/website/src/activity-patterns/components/spotlight-metrics/metrics-title.vue
+++ apps/website/src/activity-patterns/components/spotlight-metrics/metrics-title.vue
@@ -1,19 +1,16 @@
 <template>
-  <div class="flex flex-row items-center">
-    <div class="capitalize text-xl mr-2">
+  <div class="flex flex-row items-center group relative">
+    <div class="capitalize text-xl text-subtle mr-2">
       {{ title }}
     </div>
-    <div
-      v-if="information"
-      class="relative hover:cursor-pointer focus:cursor-pointer group"
-    >
+    <div class="text-subtle hover:cursor-pointer focus:cursor-pointer">
       <icon-fa-info-circle />
-      <p
-        class="absolute min-w-70 -left-24 -top-20 z-10 mx-auto bg-box-grey mt-2 py-2 px-4 invisible whitespace-pre-wrap group-hover:visible rounded-md"
-      >
-        {{ information }}
-      </p>
     </div>
+    <p
+      class="absolute min-w-70 -top-20 z-10 mx-auto bg-box-grey mt-2 py-2 px-4 invisible whitespace-pre-wrap group-hover:visible rounded-md"
+    >
+      {{ information }}
+    </p>
   </div>
 </template>
 <script lang="ts">
