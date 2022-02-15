71eb7181991e3e5d3737dc990a08f4f4fbadbf3a
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-02-15 15:30:20 +0700

    [564] Add image gallery to spotlight page

--- apps/website/src/activity-patterns/components/species-images/species-images.vue
+++ apps/website/src/activity-patterns/components/species-images/species-images.vue
@@ -1,23 +1,40 @@
 <template>
   <div>
-    <div class="max-h-60 max-w-full my-4 flex justify-left">
-      <div class="relative">
-        <img
-          :src="speciesImage()"
-          :alt="species?.scientificName ?? ''"
-          class="max-h-60"
+    <div class="my-4 flex justify-left">
+      <el-carousel
+        class="w-full h-75"
+        indicator-position="none"
+        autoplay="false"
+      >
+        <el-carousel-item
+          v-for="(image, idx) in images"
+          :key="'species-images-' + idx"
+          class="flex justify-center items-center"
         >
-        <div
-          v-if="species.thumbnailImageUrl"
-          class="absolute px-2 py-1 bottom-0 w-full text-center bg-dark-300 bg-opacity-70"
-        >
-          <a
-            :href="imageRef"
-            target="_blank"
-            class="italic hover:underline"
-          >Wiki: {{ species?.imageCaption }}</a>
-        </div>
-      </div>
+          <div class="relative overflow-hidden rounded-md">
+            <div
+              class="flex justify-center bg-cover bg-center h-60 w-90"
+              style="filter: blur(2px)"
+              :style="{ backgroundImage: 'url(' + image + ')'}"
+            />
+            <img
+              :src="image"
+              :alt="species?.scientificName ?? ''"
+              class="absolute top-0 left-0 right-0 mx-auto h-60 max-w-90"
+            >
+            <div
+              v-if="image"
+              class="absolute px-2 py-1 bottom-0 w-full text-center bg-dark-500 bg-opacity-70"
+            >
+              <a
+                :href="imageRef"
+                target="_blank"
+                class="italic hover:underline"
+              >Wiki: {{ species?.imageCaption }}</a>
+            </div>
+          </div>
+        </el-carousel-item>
+      </el-carousel>
     </div>
   </div>
 </template>
