ec27d85035db599f75743566dea3c5e88750bbe5
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-02-15 09:33:09 +0700

    [559] Add Puerto Rico local risk ratings to DB

---
+++ apps/cli/src/db/seeders/_data/location-project-species-puerto-rico.ts
@@ -0,0 +1,9 @@
+import { ProjectSpecies } from '@rfcx-bio/common/dao/types/location-project-species'
+
+export const projectSpeciesPuertoRico: Array<Partial<ProjectSpecies> & { slug: string }> = [
+  { slug: 'amazona-vittata', highlightedOrder: 0 },
+  { slug: 'accipiter-striatus-venator', highlightedOrder: 1, riskRatingLocalCode: 'CR', riskRatingLocalLevel: 600, riskRatingLocalSource: 'PRDNER 2016' },
+  { slug: 'agelaius-xanthomus', highlightedOrder: 2 },
+  { slug: 'setophaga-angelae', highlightedOrder: 3 },
+  { slug: 'antrostomus-noctitherus', highlightedOrder: 4 }
+]
