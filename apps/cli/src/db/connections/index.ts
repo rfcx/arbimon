12f9911eb7c0c01cb38833199c149ec06691a1d1
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-02-16 22:33:49 +0700

    [CD] Fix async import() on Unix
    ...possibly break it on Windows... :/

--- apps/cli/src/db/connections/index.ts
+++ apps/cli/src/db/connections/index.ts
@@ -15,12 +15,11 @@ const migrationsDir = resolve(currentDir, '../../db/migrations')
 const { BIO_DB_HOSTNAME: host, BIO_DB_PORT: port, BIO_DB_SSL_ENABLED: isSsl, BIO_DB_DBNAME: databaseName, BIO_DB_USER: user, BIO_DB_PASSWORD: password } =
 requireEnv('BIO_DB_HOSTNAME', 'BIO_DB_PORT', 'BIO_DB_SSL_ENABLED', 'BIO_DB_DBNAME', 'BIO_DB_USER', 'BIO_DB_PASSWORD')
 
-
 export const getSequelize = (verbose = false): Sequelize =>
   getSequelizeBase({ host, port, databaseName, user, password, isSsl, verbose })
 
 const importMigration = async (path?: string): Promise<Pick<RunnableMigration<QueryInterface>, 'up' | 'down'>> =>
-  (await import(`file:///${(path ?? '').replace(/\\/g, '/')}`))
+  await import(path ?? '')
 
 export const getUmzug = (sequelize: Sequelize, verbose = false, cwd = migrationsDir, filename?: string): Umzug<QueryInterface> =>
   new Umzug({
