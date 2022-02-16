67f4e47284486c33371af26baf59c9abc94bca3d
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-02-16 22:52:17 +0700

    [API] Don't expose internal errors; Use ModelRepository

--- apps/api/src/projects/controller-projects-all.ts
+++ apps/api/src/projects/controller-projects-all.ts
@@ -1,12 +1,22 @@
 import { ProjectsResponse } from '@rfcx-bio/common/api-bio/common/projects'
-import { LocationProjectModel, PROJECT_MODEL_ATTRIBUTES } from '@rfcx-bio/common/dao/models/location-project-model'
+import { ModelRepositoryFactory } from '@rfcx-bio/common/dao/model-repository'
+import { PROJECT_MODEL_ATTRIBUTES } from '@rfcx-bio/common/dao/models/location-project-model'
 
+import { ApiServerError } from '~/errors'
 import { Handler } from '../_services/api-helpers/types'
 import { getSequelize } from '../_services/db'
 
 export const projectsAllHandler: Handler<ProjectsResponse> = async () => {
-  const projects = await LocationProjectModel(getSequelize()).findAll({
-    attributes: PROJECT_MODEL_ATTRIBUTES.light
-  })
+  const models = ModelRepositoryFactory.getInstance(getSequelize())
+
+  const projects = await models.LocationProject
+    .findAll({
+      attributes: PROJECT_MODEL_ATTRIBUTES.light
+    })
+    .catch(err => {
+      console.error(err)
+      throw ApiServerError()
+    })
+
   return projects
 }
--- apps/api/src/projects/controller-sites-all.ts
+++ apps/api/src/projects/controller-sites-all.ts
@@ -1,6 +1,8 @@
 import { SitesParams, SitesResponse } from '@rfcx-bio/common/api-bio/common/sites'
-import { LocationSiteModel, SITE_MODEL_ATTRIBUTES } from '@rfcx-bio/common/dao/models/location-site-model'
+import { ModelRepositoryFactory } from '@rfcx-bio/common/dao/model-repository'
+import { SITE_MODEL_ATTRIBUTES } from '@rfcx-bio/common/dao/models/location-site-model'
 
+import { ApiServerError } from '~/errors'
 import { Handler } from '../_services/api-helpers/types'
 import { getSequelize } from '../_services/db'
 import { assertParamsExist } from '../_services/validation'
@@ -11,13 +13,20 @@ export const sitesAllHandler: Handler<SitesResponse, SitesParams> = async (req) 
   assertParamsExist({ projectId })
 
   // Query
-  const sites = await LocationSiteModel(getSequelize()).findAll({
-    where: { locationProjectId: projectId },
-    attributes: SITE_MODEL_ATTRIBUTES.light,
-    order: [
-      ['name', 'ASC']
-    ]
-  })
+  const models = ModelRepositoryFactory.getInstance(getSequelize())
+
+  const sites = await models.LocationSite
+    .findAll({
+      where: { locationProjectId: projectId },
+      attributes: SITE_MODEL_ATTRIBUTES.light,
+      order: [
+        ['name', 'ASC']
+      ]
+    })
+    .catch(err => {
+      console.error(err)
+      throw ApiServerError()
+    })
 
   return sites
 }
