c1b71307c2624e98ba3f47d7740898c50cea1941
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-02-18 10:34:16 +0700

    [586] Deprecate old risk helpers

--- packages/common/src/iucn/index.ts
+++ packages/common/src/iucn/index.ts
@@ -20,22 +20,49 @@ const EXTINCTION_RISKS_RAW = <const>[
 ]
 
 // Types & list
+/**
+ * @deprecated Use apps/website/src/_services/risk-ratings
+ */
 export const EXTINCTION_RISKS = EXTINCTION_RISKS_RAW
   .map(({ code, label, color }, level) => ({ code, label, color, level }))
 
+/**
+ * @deprecated Use apps/website/src/_services/risk-ratings
+ */
 export type ExtinctionRisk = typeof EXTINCTION_RISKS[number]
+/**
+ * @deprecated Use apps/website/src/_services/risk-ratings
+ */
 export type ExtinctionRiskCode = typeof EXTINCTION_RISKS[number]['code']
 
 // Lookups
 const EXTINCTION_RISKS_KEYED = keyBy(EXTINCTION_RISKS, 'code') as Record<ExtinctionRiskCode, ExtinctionRisk>
+/**
+ * @deprecated Use apps/website/src/_services/risk-ratings
+ */
 export const getExtinctionRisk = (code: ExtinctionRiskCode): ExtinctionRisk =>
   EXTINCTION_RISKS_KEYED[code] ??
   EXTINCTION_RISKS_KEYED.NE
 
 // Constants
+/**
+ * @deprecated Use apps/website/src/_services/risk-ratings
+ */
 export const EXTINCTION_RISK_NOT_EVALUATED = EXTINCTION_RISKS_KEYED.NE
+/**
+ * @deprecated Use apps/website/src/_services/risk-ratings
+ */
 export const EXTINCTION_RISK_THREATENED_CODES: ExtinctionRiskCode[] = ['VU', 'EN', 'CR']
+/**
+ * @deprecated Use apps/website/src/_services/risk-ratings
+ */
 export const EXTINCTION_RISK_PROTECTED_CODES: ExtinctionRiskCode[] = ['CR']
 
+/**
+ * @deprecated Use apps/website/src/_services/risk-ratings
+ */
 export const EXTINCTION_CODES_AND_COLORS = mapValues(keyBy(EXTINCTION_RISKS, 'code'), 'color')
+/**
+ * @deprecated Use apps/website/src/_services/risk-ratings
+ */
 export const EXTINCTION_LABELS_AND_COLORS = mapValues(keyBy(EXTINCTION_RISKS, 'label'), 'color')
