63b969b00fb4738c2ec4144448b71234a60ec8cd
Somjintana Korbut <nuisomjin@gmail.com>
2022-02-20 22:56:54 +0700

    [495] create job to sync projects

--- apps/cli/src/data-ingest/projects/input-arbimon/index.ts
+++ apps/cli/src/data-ingest/projects/input-arbimon/index.ts
@@ -1,7 +1,3 @@
-import * as fs from 'fs'
-import { dirname, resolve } from 'path'
-import { fileURLToPath } from 'url'
-
 import { Project } from '@rfcx-bio/common/dao/types'
 
 import { mysqlSelect } from '../../../_services/mysql'
@@ -9,10 +5,18 @@ import { ARBIMON_CONFIG } from '../../_connections/arbimon'
 import { ArbimonProject } from './type'
 
 export const getArbimonProjects = async (): Promise<Array<Omit<Project, 'id'>>> => {
-  // Read SQL
-  const currentDir = dirname(fileURLToPath(import.meta.url))
-  const sqlPath = resolve(currentDir, './get-projects.sql')
-  const sql = fs.readFileSync(sqlPath, 'utf8')
+  const sql =
+    `
+    select p.project_id, p.name, p.url slug, p.description, p.is_private, p.is_enabled, p.external_id core_project_id, p.reports_enabled, s.north, s.east, s.south, s.west
+    from projects p right join
+    (
+      select Max(s.lon) west, Min(s.lon) east, Min(s.lat) north, Max(s.lat) south, project_id 
+      from sites s
+      group by project_id 
+    ) s
+      on p.project_id = s.project_id
+    where p.external_id is not null and p.external_id != "undefined";
+    `
 
   // Query Arbimon
   const results = await mysqlSelect<ArbimonProject>(ARBIMON_CONFIG, sql)
