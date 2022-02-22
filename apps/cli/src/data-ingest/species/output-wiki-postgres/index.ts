5a99cc082f6de04420d62f22f6669cee624690c1
Chanachai Yaemban <75108999+Crave61341416@users.noreply.github.com>
2022-02-22 18:17:29 +0700

    [493] Temporary solution to avoid wrong pk

--- apps/cli/src/data-ingest/species/output-wiki-postgres/index.ts
+++ apps/cli/src/data-ingest/species/output-wiki-postgres/index.ts
@@ -17,9 +17,16 @@ export const writeWikiSpeciesDataToPostgres = async (sequelize: Sequelize, newDa
 
 export const writeWikiSpeciesPhotoDataToPostgres = async (sequelize: Sequelize, newData: Array<Omit<TaxonSpeciesPhoto, 'id'>>): Promise<void> => {
   const model = TaxonSpeciesPhotoModel(sequelize)
+  // we have to update existing data & create new data
   await model.destroy({
-      truncate: true
+      truncate: true,
+      restartIdentity: true // Temporary solution: by destroy a whole table (restartIdentity avoid create new PK)
     })
   const updateSpeciesWikiPhotoRows = await model.bulkCreate(newData)
+  // const updateSpeciesWikiPhotoRows = newData.map(async newData => await Promise.all(await model.update(newData, {
+  //   where: {
+  //     taxonSpeciesId: newData.taxonSpeciesId
+  //   }
+  // })))
   console.info(`- writeWikiSpeciesPhotoDataToPostgres: bulk upsert ${updateSpeciesWikiPhotoRows.length} photo info`)
 }
