444c3da054cbddd6905f2aa5397690b1e6814c1b
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-02-22 16:16:31 +0700

    [DX] Finally worked out an obvious way to avoid duplicating rules!

--- tools/configs/.eslintrc-ts.cjs
+++ tools/configs/.eslintrc-ts.cjs
@@ -1,3 +1,5 @@
+const regexRules = require('./eslint-regex-rules.cjs')
+
 module.exports = {
   extends: ['./.eslintrc-js.cjs'],
   plugins: ['sort-class-members'],
@@ -22,37 +24,7 @@ module.exports = {
     ],
 
     // Customize rules
-    'regex/invalid': [
-      'error', [
-        // TODO ??? - Extract this as an ESLint rule: `import-initialized-library`
-        {
-          regex: 'from \'lodash\'',
-          message: 'Use lodash-es',
-          replacement: 'from \'lodash-es\''
-        },
-        {
-          // eslint-disable-next-line no-useless-escape
-          regex: 'import \\* as [a-zA-Z0-9]* from \'dayjs\'',
-          message: 'Import the initialized dayjs object from \'@rfcx-bio/utils/dayjs-initialized\'',
-          replacement: 'import { dayjs } from \'@rfcx-bio/utils/dayjs-initialized\''
-        },
-        {
-          regex: '^import dayjs, ({[^}]*}) from \'dayjs\'$',
-          message: 'Import the initialized dayjs object from \'@rfcx-bio/utils/dayjs-initialized\'',
-          replacement: { function: 'return "import { dayjs } from \'@rfcx-bio/utils/dayjs-initialized\'; import " + captured[0] + " from \'dayjs\'"' }
-        },
-        {
-          regex: 'new mapboxgl.Map\\(([^\\)]*)\\)',
-          message: 'Use the createMap(...) function',
-          replacement: { function: 'return "createMap(" + captured[0] + ")"' }
-        },
-        {
-          regex: 'sequalize',
-          message: 'Spelling',
-          replacement: 'sequelize'
-        }
-      ]
-    ],
+    'regex/invalid': ['error', regexRules],
     'sort-class-members/sort-class-members': [
       'error',
       {
