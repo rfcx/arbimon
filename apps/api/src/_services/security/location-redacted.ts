19963df947716607f4337d9adc0a78535e820864
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-02-22 13:55:29 +0700

    - Extract location redacted logic into security folder

---
+++ apps/api/src/_services/security/location-redacted.ts
@@ -0,0 +1,17 @@
+import { ModelRepositoryFactory } from '@rfcx-bio/common/dao/model-repository'
+
+import { getSequelize } from '../db'
+
+export async function isProtectedSpecies (riskRatingIucnId: number): Promise<boolean> {
+  const sequelize = getSequelize()
+  const models = ModelRepositoryFactory.getInstance(sequelize)
+
+  const protectedCodes = await models.RiskRatingIucn.findAll({
+    where: {
+      isThreatened: true
+    },
+    raw: true
+  })
+
+  return protectedCodes.map(({ idOrdered }) => idOrdered).includes(riskRatingIucnId)
+}
