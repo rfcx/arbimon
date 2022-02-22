e072e5fffc12950b6b9854642bc00e277b3c4386
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-02-22 16:16:31 +0700

    [DX] Finally worked out an obvious way to avoid duplicating rules!

--- tools/configs/.eslintrc-vue.cjs
+++ tools/configs/.eslintrc-vue.cjs
@@ -1,3 +1,5 @@
+const regexRules = require('./eslint-regex-rules.cjs')
+
 module.exports = {
   extends: [
     'plugin:vue/vue3-recommended',
@@ -21,6 +23,7 @@ module.exports = {
     // Customize Rules
     'regex/invalid': [
       'error', [
+        ...regexRules,
         {
           regex: ' public ',
           message: 'Accessibility modifiers are meaningless to vue-class-component',
@@ -35,33 +38,6 @@ module.exports = {
           regex: ' private ',
           message: 'Accessibility modifiers are meaningless to vue-class-component',
           replacement: ' '
-        },
-        // TODO ??? - Find a way to avoid repeating this
-        {
-          regex: 'from \'lodash\'',
-          message: 'Use lodash-es',
-          replacement: 'from \'lodash-es\''
-        },
-        {
-          // eslint-disable-next-line no-useless-escape
-          regex: 'import \\* as [a-zA-Z0-9]* from \'dayjs\'',
-          message: 'Import the initialized dayjs object from \'@rfcx-bio/utils/dayjs-initialized\'',
-          replacement: 'import { dayjs } from \'@rfcx-bio/utils/dayjs-initialized\''
-        },
-        {
-          regex: '^import dayjs, ({[^}]*}) from \'dayjs\'$',
-          message: 'Import the initialized dayjs object from \'@rfcx-bio/utils/dayjs-initialized\'',
-          replacement: { function: 'return "import { dayjs } from \'@rfcx-bio/utils/dayjs-initialized\'; import " + captured[0] + " from \'dayjs\'"' }
-        },
-        {
-          regex: 'new mapboxgl.Map\\(([^\\)]*)\\)',
-          message: 'Use the createMap(...) function',
-          replacement: { function: 'return "createMap(" + captured[0] + ")"' }
-        },
-        {
-          regex: 'sequalize',
-          message: 'Spelling',
-          replacement: 'sequelize'
         }
       ]
     ]
