caf84aa3b6260dd31b7561102996bfbd0a37ea18
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-02-24 12:10:05 +0700

    [577] Change predicted occupancy map table to be more general and add the project file seeder

---
+++ packages/common/src/dao/models/location-project-species-file-model.ts
@@ -0,0 +1,35 @@
+import { DataTypes } from 'sequelize'
+
+import { defineWithDefaults } from '../helpers/defaults'
+import { LocationProjectSpeciesFile } from '../types'
+
+export const MODEL_LOCATION_PROJECT_SPECIES_FILE = 'LocationProjectSpeciesFile'
+export const TABLE_LOCATION_PROJECT_SPECIES_FILE = 'location_project_species_file'
+
+export const LocationProjectSpeciesFileModel = defineWithDefaults<LocationProjectSpeciesFile>(
+  MODEL_LOCATION_PROJECT_SPECIES_FILE,
+  {
+    // PK
+    locationProjectId: { // 1
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    taxonSpeciesId: { // 1
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    order: { // 1
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+
+    // Facts
+    description: DataTypes.STRING(255),
+    filename: DataTypes.STRING(255),
+    mimeType: DataTypes.STRING(32),
+    url: DataTypes.STRING(511)
+  },
+  {
+    tableName: TABLE_LOCATION_PROJECT_SPECIES_FILE
+  }
+)
