9aee5f48f4a12a7f5cd988a35409f3bf814a5477
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-02-25 13:43:37 +0700

    - Clean up spotlight api code

--- packages/common/src/api-bio/spotlight/common.ts
+++ packages/common/src/api-bio/spotlight/common.ts
@@ -1,9 +1,7 @@
-export type TimeBucket = 'hourOfDay' | 'dayOfWeek' | 'monthOfYear' | 'dateSeries'
-
-export type SpotlightDataByTime = Record<TimeBucket, SpotlightDetectionDataByTime>
+import { DataByTime } from '../common/time-bucket'
 
 export interface SpotlightDetectionDataBySite {
-  [siteId: string]: {
+  [siteId: number]: {
     siteId: number
     siteName: string
     latitude: number
@@ -19,9 +17,14 @@ export interface SpotlightDetectionDataByTime<T extends string | number = number
   detectionFrequency: Record<T, number>
 }
 
+// ----- Not related to api return item ----
+// ? Moving to somewhere
 export interface SpotlightExportData {
   hour: SpotlightDetectionDataByTime
   month: SpotlightDetectionDataByTime<string>
   year: SpotlightDetectionDataByTime
   sites: SpotlightDetectionDataBySite
 }
+
+// ? Moving to somewhere
+export type SpotlightDataByTime = DataByTime<SpotlightDetectionDataByTime>
