b0dbc74ac63938759c779354032cb7f451ff5a26
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-03-01 20:28:02 +0700

    - Fix redact logic to be only CR status

--- apps/api/src/_services/security/location-redacted.ts
+++ apps/api/src/_services/security/location-redacted.ts
@@ -2,12 +2,14 @@ import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
 
 import { getSequelize } from '../db'
 
+const PROTECTED_CODES = ['CR']
+
 export async function isProtectedSpecies (riskRatingIucnId: number): Promise<boolean> {
   const sequelize = getSequelize()
   const models = ModelRepository.getInstance(sequelize)
 
   const protectedCodes = await models.RiskRatingIucn.findAll({
-    where: { isThreatened: true },
+    where: { code: PROTECTED_CODES },
     raw: true
   })
 
