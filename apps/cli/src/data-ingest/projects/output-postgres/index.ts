24fa45d613be3f9dd4c85b01e5f1d0a89a24029e
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-03-02 18:39:01 +0700

    - Remove isPublish from writing project to postgres

--- apps/cli/src/data-ingest/projects/output-postgres/index.ts
+++ apps/cli/src/data-ingest/projects/output-postgres/index.ts
@@ -3,30 +3,17 @@ import pkg from 'sequelize'
 import { LocationProjectModel } from '@rfcx-bio/common/dao/models/location-project-model'
 import { Project } from '@rfcx-bio/common/dao/types'
 
-import { requireEnv } from '~/env'
 import { getSequelize } from '../../../db/connections'
-import { rawEnvToProjectAndProfile } from '../../../db/seeders/_data/location-project-and-profile'
 
 const { Op } = pkg
 
-const { BIO_ENVIRONMENT } = requireEnv('BIO_ENVIRONMENT')
-
 export const writeProjectsToPostgres = async (projects: Array<Omit<Project, 'id'>>): Promise<void> => {
   const model = LocationProjectModel(getSequelize())
-  const mockProjectCoreIds = rawEnvToProjectAndProfile[BIO_ENVIRONMENT].map(mp => mp.idCore)
-
-  const updatedProjects = projects.map(p => {
-    return {
-      ...p,
-      isPublished: mockProjectCoreIds.includes(p.idCore) // force publishing projects in the mock project list
-    }
-  })
 
   // update items
-  const updatedRows = await model.bulkCreate(updatedProjects, {
+  const updatedRows = await model.bulkCreate(projects, {
     updateOnDuplicate: [
       'name',
-      'isPublished',
       'latitudeNorth',
       'latitudeSouth',
       'longitudeEast',
@@ -39,7 +26,7 @@ export const writeProjectsToPostgres = async (projects: Array<Omit<Project, 'id'
     where: {
       idCore: {
         [Op.ne]: null,
-        [Op.notIn]: updatedProjects.map(p => p.idCore)
+        [Op.notIn]: projects.map(p => p.idCore)
       }
     }
   })
