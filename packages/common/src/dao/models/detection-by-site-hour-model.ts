fa1010d2156d1a6cfc837fe7df3c932490e7523e
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-03-02 15:25:52 +0700

    [576] Add detection-by-site-hour (to get total duration for detection frequency calc)

---
+++ packages/common/src/dao/models/detection-by-site-hour-model.ts
@@ -0,0 +1,32 @@
+import { DataTypes } from 'sequelize'
+
+import { defineWithDefaults } from '../helpers/defaults'
+import { DetectionBySiteHour } from '../types/detection-by-site-hour'
+
+export const MODEL_DETECTION_BY_SITE_HOUR = <const>'DetectionBySiteHour'
+export const TABLE_DETECTION_BY_SITE_HOUR = <const>'detection_by_site_hour'
+
+export const DetectionBySiteSpeciesHourModel = defineWithDefaults<DetectionBySiteHour>(
+  MODEL_DETECTION_BY_SITE_HOUR,
+  {
+    // PK
+    timePrecisionHourLocal: { // '2021-03-18T11:00:00.000Z' (as date)
+      type: DataTypes.DATE(3),
+      primaryKey: true
+    },
+    locationSiteId: { // 123
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+
+    // Dimensions
+    locationProjectId: DataTypes.INTEGER,
+
+    // Facts
+    count: DataTypes.INTEGER, // 1
+    durationMinutes: DataTypes.INTEGER // 12
+  },
+  {
+    tableName: TABLE_DETECTION_BY_SITE_HOUR
+  }
+)
