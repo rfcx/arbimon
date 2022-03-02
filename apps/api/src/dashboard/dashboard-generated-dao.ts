afecfc2ee805edb33b37b9b658f85c7fc0584a83
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-03-02 17:03:17 +0700

    [559] Convert detectionsBySite to DB; Move data re-organization into DAO

--- apps/api/src/dashboard/dashboard-generated-dao.ts
+++ apps/api/src/dashboard/dashboard-generated-dao.ts
@@ -1,47 +1,65 @@
 import { groupBy, mapValues, sum } from 'lodash-es'
 
-import { ApiMap } from '@rfcx-bio/common/api-bio/_helpers'
+import { ApiMap, ApiStack } from '@rfcx-bio/common/api-bio/_helpers'
+import { DashboardSpecies } from '@rfcx-bio/common/api-bio/dashboard/common'
 import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
-import { DashboardRichnessByRisk } from '@rfcx-bio/common/dao/types/dashboard-richness-by-risk'
-import { DashboardRichnessByTaxon } from '@rfcx-bio/common/dao/types/dashboard-richness-by-taxon'
-import { DashboardSpeciesThreatened } from '@rfcx-bio/common/dao/types/dashboard-species-threatened'
-import { LocationProjectMetric } from '@rfcx-bio/common/dao/types/location-project-metric'
+import { LocationProjectMetricLight } from '@rfcx-bio/common/dao/types/location-project-metric'
 import { rawDetections } from '@rfcx-bio/common/mock-data'
 import { groupByNumber } from '@rfcx-bio/utils/lodash-ext'
 
 import { getSequelize } from '../_services/db'
 
-export const getProjectMetrics = async (locationProjectId: number): Promise<LocationProjectMetric | undefined> =>
+export const getProjectMetrics = async (locationProjectId: number): Promise<LocationProjectMetricLight> =>
   await ModelRepository.getInstance(getSequelize())
     .LocationProjectMetric
     .findOne({
       where: { locationProjectId },
       raw: true
-     }) ?? undefined
+     })
+     .then(res => res ?? { detectionCount: 0, siteCount: 0, speciesCount: 0 })
 
-export const getSpeciesThreatened = async (locationProjectId: number): Promise<DashboardSpeciesThreatened[]> =>
+export const getSpeciesThreatened = async (locationProjectId: number): Promise<DashboardSpecies[]> =>
   await ModelRepository.getInstance(getSequelize())
     .DashboardSpeciesThreatened
     .findAll({
       where: { locationProjectId },
       raw: true
     })
+    .then(res => res.map(({ taxonSpeciesSlug, taxonClassSlug, scientificName, commonName, riskRatingIucnId, photoUrl }) => ({
+      slug: taxonSpeciesSlug,
+      taxonSlug: taxonClassSlug,
+      scientificName,
+      commonName,
+      riskId: riskRatingIucnId,
+      photoUrl
+    })))
 
-export const getRichnessByTaxon = async (locationProjectId: number): Promise<DashboardRichnessByTaxon[]> =>
+export const getRichnessByTaxon = async (locationProjectId: number): Promise<ApiStack> =>
   await ModelRepository.getInstance(getSequelize())
     .DashboardRichnessByTaxon
     .findAll({
       where: { locationProjectId },
       raw: true
     })
+    .then(res => res.map(r => [r.taxonClassId, r.count]))
 
-export const getRichnessByRisk = async (locationProjectId: number): Promise<DashboardRichnessByRisk[]> =>
+export const getRichnessByRisk = async (locationProjectId: number): Promise<ApiStack> =>
   await ModelRepository.getInstance(getSequelize())
     .DashboardRichnessByRisk
     .findAll({
       where: { locationProjectId },
       raw: true
     })
+    .then(res => res.map(r => [r.riskRatingIucnId, r.count]))
+
+export const getDetectionBySite = async (locationProjectId: number): Promise<ApiMap> =>
+  await ModelRepository.getInstance(getSequelize())
+    .DashboardDetectionsBySite
+    .findAll({
+      where: { locationProjectId },
+      attributes: ['name', 'latitude', 'longitude', ['count', 'value']],
+      raw: true
+    }) as unknown as ApiMap
 
 // OLD, not GOLD (gonna delete this)
 export const getRichnessByHour = async (): Promise<Record<number, number>> =>
@@ -65,19 +83,3 @@ export const getDetectionByHour = async (): Promise<Record<number, number>> => {
     return sum(detections.map(d => d.num_of_recordings))
   })
 }
-
-export const getDetectionBySite = async (): Promise<ApiMap> =>
-  Object.values(
-    mapValues(
-      groupBy(rawDetections, 'stream_id'),
-      (detections) => {
-        const detectionCount = sum(detections.map(d => d.num_of_recordings))
-        return {
-          name: detections[0].name,
-          longitude: detections[0].lon,
-          latitude: detections[0].lat,
-          value: detectionCount
-        }
-      }
-    )
-  )
