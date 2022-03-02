2ddeabd700a4b1691c9acf48d906eaebae3b7550
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-03-02 11:59:03 +0700

    - Change logic to check protected species by risk rating id

--- apps/api/src/_services/security/location-redacted.ts
+++ apps/api/src/_services/security/location-redacted.ts
@@ -1,17 +1,5 @@
-import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
-
-import { getSequelize } from '../db'
-
-const PROTECTED_CODES = ['CR']
+export const PROTECTED_RISK_RATING_IDS = [600]
 
 export async function isProtectedSpecies (riskRatingIucnId: number): Promise<boolean> {
-  const sequelize = getSequelize()
-  const models = ModelRepository.getInstance(sequelize)
-
-  const protectedCodes = await models.RiskRatingIucn.findAll({
-    where: { code: PROTECTED_CODES },
-    raw: true
-  })
-
-  return protectedCodes.map(({ idOrdered }) => idOrdered).includes(riskRatingIucnId)
+  return PROTECTED_RISK_RATING_IDS.includes(riskRatingIucnId)
 }
