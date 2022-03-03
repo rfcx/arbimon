d4dcd559ae3fb9aa4e8aea3a295fe2ff347e56ba
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-03-03 19:54:49 +0700

    [498] Add db query for db query species by taxon and species by taxon type as a record

--- apps/api/src/richness/controller-richness-dataset.ts
+++ apps/api/src/richness/controller-richness-dataset.ts
@@ -1,16 +1,21 @@
 import dayjs from 'dayjs'
 import { groupBy, keyBy, mapValues } from 'lodash-es'
+import { Op, Sequelize } from 'sequelize'
 
-import { MapSiteData, TimeBucket } from '@rfcx-bio/common/api-bio/richness/common'
+import { MapSiteData, SpeciesCountByTaxonName, TimeBucket } from '@rfcx-bio/common/api-bio/richness/common'
 import { RichnessDatasetParams, RichnessDatasetQuery, RichnessDatasetResponse } from '@rfcx-bio/common/api-bio/richness/richness-dataset'
 import { SpeciesLight } from '@rfcx-bio/common/api-bio/species/types'
+import { Where } from '@rfcx-bio/common/dao/helpers/query'
+import { AllModels, ModelRepository } from '@rfcx-bio/common/dao/model-repository'
+import { DetectionBySiteSpeciesHour, TaxonClass } from '@rfcx-bio/common/dao/types'
 import { EXTINCTION_RISK_PROTECTED_CODES } from '@rfcx-bio/common/iucn'
-import { MockHourlyDetectionSummary, rawDetections, rawSpecies } from '@rfcx-bio/common/mock-data'
+import { MockHourlyDetectionSummary, rawSpecies } from '@rfcx-bio/common/mock-data'
 import { groupByNumber } from '@rfcx-bio/utils/lodash-ext'
 
 import { BioInvalidQueryParamError } from '~/errors'
 import { Handler } from '../_services/api-helpers/types'
-import { filterMocksByParameters } from '../_services/mock-helper'
+import { getSequelize } from '../_services/db'
+import { FilterDataset } from '../_services/mock-helper'
 import { isProjectMember } from '../_services/permission-helper/permission-helper'
 import { assertPathParamsExist } from '../_services/validation'
 import { isValidDate } from '../_services/validation/query-validation'
@@ -33,29 +38,104 @@ export const richnessDatasetHandler: Handler<RichnessDatasetResponse, RichnessDa
     taxons: Array.isArray(taxons) ? taxons : typeof taxons === 'string' ? [taxons] : []
   }
 
-  const detections = filterMocksByParameters(rawDetections, { ...convertedQuery })
   const isLocationRedacted = !isProjectMember(req)
 
-  return await getRichnessDatasetInformation(detections, isLocationRedacted)
+  return await getRichnessDatasetInformation(Number(projectId), { ...convertedQuery }, isLocationRedacted)
 }
 
-async function getRichnessDatasetInformation (detections: MockHourlyDetectionSummary[], isLocationRedacted: boolean): Promise<RichnessDatasetResponse> {
+const getRichnessDatasetInformation = async (projectId: number, filter: FilterDataset, isLocationRedacted: boolean): Promise<RichnessDatasetResponse> => {
+  const sequelize = getSequelize()
+  const models = ModelRepository.getInstance(sequelize)
+
+  // Filter data
+  const detections = await getDetections(models, projectId, filter)
+  const taxonClasses = await getTaxonClasses(models)
+
   return {
     isLocationRedacted,
     detectionCount: detections.length,
-    speciesByTaxon: getSpeciesByTaxon(detections),
+    speciesByTaxon: await getSpeciesByTaxon(models, sequelize, projectId, filter, taxonClasses),
     speciesBySite: getSpeciesBySite(detections),
     speciesByTime: getSpeciesByTime(detections),
     speciesPresence: getSpeciesPresence(detections, isLocationRedacted)
   }
 }
 
-const calculateSpeciesRichness = (detections: MockHourlyDetectionSummary[]): number => new Set(detections.map(d => d.species_id)).size
+const getDetections = async (models: AllModels, projectId: number, filter: FilterDataset): Promise<DetectionBySiteSpeciesHour[]> => {
+  const { startDateUtcInclusive, endDateUtcInclusive, siteIds, taxons } = filter
 
-const getSpeciesByTaxon = (detections: MockHourlyDetectionSummary[]): { [taxon: string]: number } => {
-  const detectionsByTaxon = groupBy(detections, 'taxon')
-  return mapValues(detectionsByTaxon, calculateSpeciesRichness)
+  const where: Where<DetectionBySiteSpeciesHour> = {
+    timePrecisionHourLocal: {
+      [Op.and]: {
+        [Op.gte]: startDateUtcInclusive,
+        [Op.lt]: endDateUtcInclusive
+      }
+    },
+    locationProjectId: projectId
+  }
+
+  if (siteIds.length > 0) {
+    where.locationSiteId = siteIds
+  }
+
+  if (taxons.length > 0) {
+    where.taxonClassId = taxons
+  }
+
+  return await models.DetectionBySiteSpeciesHour.findAll({ where, raw: true })
 }
+
+async function getTaxonClasses (models: AllModels): Promise<TaxonClass[]> {
+  return await models.TaxonClass.findAll({ raw: true })
+}
+
+// TODO 640: Move with `getSpeciesByTaxon()`
+export interface DetectionsGroupByTaxonClass {
+  taxonClassId: number
+  speciesCount: number
+}
+
+const getSpeciesByTaxon = async (models: AllModels, sequelize: Sequelize, projectId: number, filter: FilterDataset, taxonClasses: TaxonClass[]): Promise<SpeciesCountByTaxonName> => {
+  // TODO 640: Extract where cause logic to utils
+  const { startDateUtcInclusive, endDateUtcInclusive, siteIds, taxons } = filter
+
+  const where: Where<DetectionBySiteSpeciesHour> = {
+    timePrecisionHourLocal: {
+      [Op.and]: {
+        [Op.gte]: startDateUtcInclusive,
+        [Op.lt]: endDateUtcInclusive
+      }
+    },
+    locationProjectId: projectId
+  }
+
+  if (siteIds.length > 0) {
+    where.locationSiteId = siteIds
+  }
+
+  if (taxons.length > 0) {
+    where.taxonClassId = taxons
+  }
+
+  const detectionsGroupByTaxonClass = await models.DetectionBySiteSpeciesHour.findAll({
+    attributes: {
+      include: ['taxon_class_id', [sequelize.literal('COUNT(DISTINCT(taxon_species_id))'), 'species_count']]
+    },
+    where,
+    group: 'taxon_class_id',
+    raw: true
+  }) as unknown as DetectionsGroupByTaxonClass[]
+
+  const speciesCountByTaxonName: { [taxon: string]: number } = {}
+
+  for (const { taxonClassId, speciesCount } of detectionsGroupByTaxonClass) {
+    const taxonCommonName = taxonClasses.find(({ id }) => id === taxonClassId)?.commonName ?? ''
+    speciesCountByTaxonName[taxonCommonName] = speciesCount
+  }
+  return speciesCountByTaxonName
+}
+
+const calculateSpeciesRichness = (detections: MockHourlyDetectionSummary[]): number => new Set(detections.map(d => d.species_id)).size
 
 const getSpeciesBySite = (detections: MockHourlyDetectionSummary[]): MapSiteData[] => {
   const detectionsBySite = groupBy(detections, 'name') // TODO ?? - Extract field names
--- packages/common/src/api-bio/richness/richness-dataset.ts
+++ packages/common/src/api-bio/richness/richness-dataset.ts
@@ -1,6 +1,6 @@
 import { FilterDatasetQuery } from '../common/filter'
 import { SpeciesLight } from '../species/types'
-import { MapSiteData, TimeBucket } from './common'
+import { MapSiteData, SpeciesCountByTaxonName, TimeBucket } from './common'
 
 // Request
 export interface RichnessDatasetParams {
@@ -18,7 +18,7 @@ export const richnessDatasetUrl = (params: RichnessDatasetParams): string =>
 export interface RichnessDatasetResponse {
   isLocationRedacted: boolean
   detectionCount: number
-  speciesByTaxon: { [taxon: string]: number }
+  speciesByTaxon: SpeciesCountByTaxonName
   speciesBySite: MapSiteData[]
   speciesByTime: Record<TimeBucket, Record<number, number>>
   speciesPresence: { [speciesId: string]: SpeciesLight }
