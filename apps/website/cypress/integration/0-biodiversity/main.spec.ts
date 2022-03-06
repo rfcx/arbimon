1084dcd1b40750c2da172b03b74c9ceb5fa760e2
Somjintana Korbut <nuisomjin@gmail.com>
2022-03-06 14:28:51 +0700

    [20] Add cypress to Github Actions

--- apps/website/cypress/integration/0-biodiversity/main.spec.ts
+++ apps/website/cypress/integration/0-biodiversity/main.spec.ts
@@ -3,13 +3,6 @@
 
 import 'cypress-wait-until'
 
-describe('load bio api', () => {
-  it ('API is healthy - with 200 status',() => {
-    const apiUrl = Cypress.env('api') + '/'
-    cy.request(apiUrl).its('status').should('eq', 200)
-  })
-})
-
 describe('load website', () => {
   beforeEach(() => {
     cy.visit('/')
