4c7ae656f45f5845a47100bbd22eb762375f3dbe
Chanachai Yaemban <75108999+Crave61341416@users.noreply.github.com>
2022-03-08 12:56:15 +0700

    [646] Fix props to not lose reactivity

--- apps/website/src/_services/hooks/useDateformat.ts
+++ apps/website/src/_services/hooks/useDateformat.ts
@@ -1,19 +1,23 @@
-export default function useDateFormate (): {
+import { ref } from 'vue'
+
+import dayjs from '@rfcx-bio/common/node_modules/dayjs'
+
+export default function useDateFormat (): {
   formateDate: (date: string | Date | undefined) => string
-  startToEndDate: (start: string | Date | undefined, end: string | Date | undefined) => string} {
+  startToEndDate: (start: string | Date | undefined, end: string | Date | undefined) => string } {
   const formateDate = (dateInput: string | Date | undefined): string => {
-    if (dateInput === undefined) return ''
-    if (typeof dateInput === 'string') {
-      if (dateInput.length === 0) return ''
-      const date = new Date(dateInput)
-      const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-      const monthString = month[date.getMonth()]
-      return `${monthString} ${date.getDate()}, ${date.getFullYear()} `
+    const dateInputRef = ref(dateInput)
+    if (dateInputRef.value === undefined) return ''
+    if (typeof dateInputRef.value === 'string') {
+      if (dateInputRef.value?.length === 0) return ''
+      const date = new Date(dateInputRef.value)
+
+      return dayjs(date).format('MMMM D, YYYY')
     }
-    return formateDate(dateInput.toISOString())
+    return formateDate(dateInputRef.value.toISOString())
   }
 
-  const startToEndDate = (start: string | Date | undefined = '', end: string | Date | undefined = ''): string => {
+  const startToEndDate = (start: string | Date | undefined, end: string | Date | undefined): string => {
     if (start === undefined || end === undefined) return ''
     if (typeof start === 'string' && typeof end === 'string') {
       if (start.length === 0 || end.length === 0) return ''
