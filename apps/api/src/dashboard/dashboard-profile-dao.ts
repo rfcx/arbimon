65a649d0a9e1c83d3f57d98f7e52a2b7e121d364
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-03-08 12:58:36 +0700

    [Mob] Extract response construction to DAO

--- apps/api/src/dashboard/dashboard-profile-dao.ts
+++ apps/api/src/dashboard/dashboard-profile-dao.ts
@@ -1,18 +1,39 @@
+import { DashboardProfileResponse } from '@rfcx-bio/common/api-bio/dashboard/dashboard-profile'
 import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
 import { LocationProjectProfile } from '@rfcx-bio/common/dao/types'
 import { DashboardSpeciesHighlighted } from '@rfcx-bio/common/dao/types/dashboard-species-highlighted'
 
 import { getSequelize } from '../_services/db'
 
-export const getProjectProfile = async (projectId: number): Promise<LocationProjectProfile | undefined> =>
+export const getDashboardProfile = async (projectId: number): Promise<DashboardProfileResponse | undefined> => {
+  const projectProfile = await getProjectProfile(projectId)
+  if (!projectProfile) return undefined
+
+  const speciesHighlightedRaw = await getHighlightedSpecies(projectId)
+
+  return {
+    ...projectProfile,
+    speciesHighlighted: speciesHighlightedRaw.map(({ taxonClassSlug, taxonSpeciesSlug, riskRatingIucnId, ...rest }) =>
+      ({
+        ...rest,
+        taxonSlug: taxonClassSlug,
+        slug: taxonSpeciesSlug,
+        riskId: riskRatingIucnId
+      })
+    )
+  }
+}
+
+const getProjectProfile = async (projectId: number): Promise<LocationProjectProfile | undefined> =>
   await ModelRepository.getInstance(getSequelize())
     .LocationProjectProfile
     .findOne({
       where: { locationProjectId: projectId },
+      attributes: ['summary', 'readme'],
       raw: true
     }) ?? undefined
 
-export const getHighlightedSpecies = async (projectId: number): Promise<DashboardSpeciesHighlighted[]> =>
+const getHighlightedSpecies = async (projectId: number): Promise<DashboardSpeciesHighlighted[]> =>
   await ModelRepository.getInstance(getSequelize())
     .DashboardSpeciesHighlighted
     .findAll({
