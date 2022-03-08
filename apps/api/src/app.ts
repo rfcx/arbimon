e7261969f5bbb90f3c3da70c43f7f53736163baa
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-03-08 09:42:49 +0700

    [639] Refactor app to export a function

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,4 +1,4 @@
-import fastify from 'fastify'
+import fastify, { FastifyInstance } from 'fastify'
 import fastifyCors from 'fastify-cors'
 import { fastifyRequestContextPlugin } from 'fastify-request-context'
 import fastifyStatic from 'fastify-static'
@@ -14,28 +14,32 @@ import { routesSpecies } from './species'
 import { routesSpotlight } from './spotlight'
 import { routesStatus } from './status'
 
-// Logging
-export const app = fastify({
-  logger: env.NODE_ENV === 'production' ? true : { prettyPrint: true }
-})
+export const createApp = async (): Promise<FastifyInstance> => {
+  // Create app
+  const app = await fastify({
+    logger: env.NODE_ENV === 'production' ? true : { prettyPrint: true }
+  })
 
-// Register plugins
-await app.register(fastifyCors)
-await app.register(fastifyStatic, { root: resolve('./public') })
-await app.register(fastifyRequestContextPlugin)
+  // Register plugins
+  await app.register(fastifyCors)
+  await app.register(fastifyStatic, { root: resolve('./public') })
+  await app.register(fastifyRequestContextPlugin)
 
-// Register routes
-const routesRegistrations = [
-  routesDashboard,
-  routesMedia,
-  routesProject,
-  routesSpecies,
-  routesRichness,
-  routesSpotlight,
-  routesActivity,
-  routesStatus
-]
+  // Register routes
+  const routesRegistrations = [
+    routesDashboard,
+    routesMedia,
+    routesProject,
+    routesSpecies,
+    routesRichness,
+    routesSpotlight,
+    routesActivity,
+    routesStatus
+  ]
 
-routesRegistrations
-  .flat()
-  .forEach(route => app.route({ ...route }))
+  routesRegistrations
+    .flat()
+    .forEach(route => app.route({ ...route }))
+
+  return app
+}
