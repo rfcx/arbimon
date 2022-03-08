02b08c19dc601281a2d5ce3a7a6b6a5df75046db
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-03-08 12:47:36 +0700

    [Mob] Extract more ATTRIBUTES_XYZ constants

--- apps/cli/src/data-ingest/species/output-wiki-postgres/index.ts
+++ apps/cli/src/data-ingest/species/output-wiki-postgres/index.ts
@@ -1,24 +1,23 @@
 import { Sequelize } from 'sequelize'
 
-import { ATTRIBUTES_TAXON_SPECIES_PHOTO, TaxonSpeciesPhotoModel } from '@rfcx-bio/common/dao/models/taxon-species-photo-model'
+import { TaxonSpeciesPhotoModel } from '@rfcx-bio/common/dao/models/taxon-species-photo-model'
 import { TaxonSpeciesWikiModel } from '@rfcx-bio/common/dao/models/taxon-species-wiki-model'
-import { TaxonSpeciesPhoto, TaxonSpeciesWiki } from '@rfcx-bio/common/dao/types'
+import { ATTRIBUTES_TAXON_SPECIES_PHOTO, ATTRIBUTES_TAXON_SPECIES_WIKI, TaxonSpeciesPhoto, TaxonSpeciesWiki } from '@rfcx-bio/common/dao/types'
 
 export const writeWikiSpeciesDataToPostgres = async (sequelize: Sequelize, newData: TaxonSpeciesWiki[]): Promise<void> => {
-  const model = TaxonSpeciesWikiModel(sequelize)
-  const updateSpeciesWikiRows = await model.bulkCreate(newData, {
-    updateOnDuplicate: [
-      'description',
-      'descriptionSourceUrl'
-    ]
-  })
+  const updateSpeciesWikiRows = await TaxonSpeciesWikiModel(sequelize)
+    .bulkCreate(newData, {
+      updateOnDuplicate: ATTRIBUTES_TAXON_SPECIES_WIKI.updateOnDuplicate
+    })
+
   console.info(`- writeWikiSpeciesDataToPostgres: bulk upsert ${updateSpeciesWikiRows.length} species`)
 }
 
 export const writeWikiSpeciesPhotoDataToPostgres = async (sequelize: Sequelize, newData: TaxonSpeciesPhoto[]): Promise<void> => {
-  const model = TaxonSpeciesPhotoModel(sequelize)
+  const updateSpeciesWikiPhotoRows = await TaxonSpeciesPhotoModel(sequelize)
+    .bulkCreate(newData, {
+      updateOnDuplicate: ATTRIBUTES_TAXON_SPECIES_PHOTO.updateOnDuplicate
+    })
 
-  const updateOnDuplicate = ATTRIBUTES_TAXON_SPECIES_PHOTO.light
-  const updateSpeciesWikiPhotoRows = await model.bulkCreate(newData, { updateOnDuplicate })
   console.info(`- writeWikiSpeciesPhotoDataToPostgres: bulk upsert ${updateSpeciesWikiPhotoRows.length} photo info`)
 }
