3470ce3a37439658142638ecbb7cf1f3336460c5
Chanachai Yaemban <75108999+Crave61341416@users.noreply.github.com>
2022-03-08 12:30:29 +0700

    [646] Change minDate & maxDate type

--- packages/common/src/api-bio/dashboard/dashboard-generated.ts
+++ packages/common/src/api-bio/dashboard/dashboard-generated.ts
@@ -18,8 +18,8 @@ export interface DashboardGeneratedResponse {
   siteCount: number
   speciesCount: number
   speciesThreatenedCount: number
-  maxDate: Date | string
-  minDate: Date | string
+  maxDate?: Date
+  minDate?: Date
 
   // Species
   speciesThreatened: DashboardSpecies[]
