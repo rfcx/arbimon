0c92c5db3ccad2c44ff7a809250bde5887098f5a
Chanachai Yaemban <75108999+Crave61341416@users.noreply.github.com>
2022-03-09 15:32:02 +0700

    [Mob] Start hourly cron to sync data from Arbimon
    Errors

--- apps/cli/src/_services/mysql/index.ts
+++ apps/cli/src/_services/mysql/index.ts
@@ -1,8 +1,8 @@
 import { ConnectionOptions, createConnection } from 'mysql2/promise'
 
-export const mysqlSelect = async <T>(config: ConnectionOptions, sql: string): Promise<T[]> => {
+export const mysqlSelect = async <T>(config: ConnectionOptions, sql: string, values: any): Promise<T[]> => {
   const connection = await createConnection(config)
-  const [rows] = await connection.query(sql)
+  const [rows] = await connection.query(sql, values)
   void connection.end()
 
   return rows as T[]
