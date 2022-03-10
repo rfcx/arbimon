e1727a2f8514dffe9778a5db995781545f8f564b
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-03-10 14:42:13 +0700

    [653] Rename RiskRating `idOrdered` to `id` (simpler)

--- apps/cli/src/db/seeders/01-master-data/001-risk-rating-iucn.ts
+++ apps/cli/src/db/seeders/01-master-data/001-risk-rating-iucn.ts
@@ -2,24 +2,11 @@ import { QueryInterface } from 'sequelize'
 import { MigrationFn } from 'umzug'
 
 import { RiskRatingIucnModel } from '@rfcx-bio/common/dao/models/risk-rating-iucn-model'
-import { RiskRatingIucn } from '@rfcx-bio/common/dao/types'
+
+import { rawRiskRatings } from '../_data/risk-rating'
 
 export const up: MigrationFn<QueryInterface> = async (params): Promise<void> => {
   const sequelize = params.context.sequelize
-
-  const data: RiskRatingIucn[] = [
-    { idOrdered: -1, code: 'NE', isThreatened: false },
-    { idOrdered: 0, code: 'NA', isThreatened: false },
-    { idOrdered: 100, code: 'DD', isThreatened: false },
-    { idOrdered: 200, code: 'LC', isThreatened: false },
-    { idOrdered: 300, code: 'NT', isThreatened: false },
-    { idOrdered: 400, code: 'VU', isThreatened: true },
-    { idOrdered: 500, code: 'EN', isThreatened: true },
-    { idOrdered: 600, code: 'CR', isThreatened: true },
-    { idOrdered: 700, code: 'RE', isThreatened: false },
-    { idOrdered: 800, code: 'EW', isThreatened: false },
-    { idOrdered: 900, code: 'EX', isThreatened: false }
-  ]
-
-  await RiskRatingIucnModel(sequelize).bulkCreate(data)
+  await RiskRatingIucnModel(sequelize)
+    .bulkCreate(rawRiskRatings)
 }
