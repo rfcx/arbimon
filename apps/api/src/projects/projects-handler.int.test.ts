60032912dafbe373c4f52b9c3d6308c4ff7de908
Antony Harfield <antonyharfield@hotmail.com>
2022-03-14 12:48:06 +0700

    [CI] Update example projects int test

--- apps/api/src/projects/projects-handler.int.test.ts
+++ apps/api/src/projects/projects-handler.int.test.ts
@@ -4,10 +4,9 @@ import { expect, test } from 'vitest'
 import { projectsRoute } from '@rfcx-bio/common/api-bio/common/projects'
 
 import { GET } from '~/api-helpers/types'
-// eslint-disable-next-line no-restricted-imports -- Allowed in tests
-import { routesProject } from '.'
+import { routesProject } from './index'
 
-test('Example', async () => {
+test('GET /projects contains valid project', async () => {
   const app = await fastify({ logger: true })
   const route = routesProject.find(r => r.url === projectsRoute)
   if (!route) { assert(false); return }
@@ -21,4 +20,8 @@ test('Example', async () => {
 
   // Assert
   expect(response.statusCode).toBe(200)
+  const result = JSON.parse(response.body)
+  const bciProject = result.find((p: any) => p.slug === 'bci-panama-2018')
+  expect(bciProject).toBeDefined()
+  expect(bciProject.name).toBe('BCI-Panama_2018')
 })
