1c257ef3f72eaeeab0c7a5547fccd29a402b29ec
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-03-14 18:44:42 +0700

    [649] Fix map data for export detections dataset

--- apps/website/src/species-richness/csv.ts
+++ apps/website/src/species-richness/csv.ts
@@ -1,11 +1,11 @@
-import { SpeciesByExportReportRow } from '@rfcx-bio/common/api-bio/richness/common'
+import { RichnessByExportReportRow } from '@rfcx-bio/common/api-bio/richness/richness-dataset'
 import { TaxonClass } from '@rfcx-bio/common/dao/types'
 import { JsZipFile, toCsv, zipAndDownload } from '@rfcx-bio/utils/file'
 
 import { getCSVDatasetMetadata } from '~/export'
 import { ColoredFilter, getExportDateTime, getExportFilterName, getExportGroupName } from '~/filters'
 
-export const downloadCsvReports = async (filters: ColoredFilter[], datasets: SpeciesByExportReportRow[][], reportPrefix: string, taxonClasses: TaxonClass[]): Promise<void> => {
+export const downloadCsvReports = async (filters: ColoredFilter[], datasets: RichnessByExportReportRow[][], reportPrefix: string, taxonClasses: TaxonClass[]): Promise<void> => {
   const exportDateTime = getExportDateTime()
 
   const files = await Promise.all(
@@ -18,7 +18,7 @@ export const downloadCsvReports = async (filters: ColoredFilter[], datasets: Spe
   await zipAndDownload(files, groupName)
 }
 
-const getCsvFile = async ({ startDate, endDate, sites: siteGroups, otherFilters }: ColoredFilter, dataAsJson: SpeciesByExportReportRow[], reportPrefix: string, exportTime: string, datasetIndex: number, taxonClasses: TaxonClass[]): Promise<JsZipFile> => {
+const getCsvFile = async ({ startDate, endDate, sites: siteGroups, otherFilters }: ColoredFilter, dataAsJson: RichnessByExportReportRow[], reportPrefix: string, exportTime: string, datasetIndex: number, taxonClasses: TaxonClass[]): Promise<JsZipFile> => {
   // const sites = siteGroups.flatMap(sg => sg.value)
   const taxonFilterIds = otherFilters.filter(({ propertyName }) => propertyName === 'taxon').map(({ value }) => value) as number[]
   const taxonFilter = taxonClasses.filter(tc => taxonFilterIds.includes(tc.id)).map(tc => tc.commonName)
