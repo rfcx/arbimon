595c430ed326f65e8d3a403878201c373ac44e4a
Chanachai Yaemban <75108999+Crave61341416@users.noreply.github.com>
2022-03-15 15:00:03 +0700

    [WIP] Attempt to as updatedAt, CreatedAt

--- packages/common/src/dao/types/datasource.ts
+++ packages/common/src/dao/types/datasource.ts
@@ -2,11 +2,8 @@ export interface Datasource {
   id: string
   locationProjectId: number
   summaryText?: string
-  updatedAt: Date
 }
 
-export type DatasourceLight = Omit<Datasource, 'updatedAt'>
-
 export const ATTRIBUTES_DATASOURCE: Record<string, Array<keyof Datasource>> = {
-  light: ['id', 'locationProjectId', 'summaryText', 'updatedAt']
+  light: ['id', 'locationProjectId', 'summaryText']
 }
