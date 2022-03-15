24604f69dcc39986b34ebc723f48e371bc009ffd
Chanachai Yaemban <75108999+Crave61341416@users.noreply.github.com>
2022-03-15 15:45:48 +0700

    [633] Dateformat hook with full pattern date

--- apps/website/src/_components/last-sync.vue
+++ apps/website/src/_components/last-sync.vue
@@ -1,18 +1,18 @@
 <template>
   <p class="text-center opacity-50">
-    Last generated/synced at: {{ latestUpdateDate }}
+    Last generated based on validated analysis data from Arbimon at: {{ latestUpdateDate }}
   </p>
 </template>
 
 <script setup lang="ts">
 import { computed, defineProps, withDefaults } from 'vue'
 
-import useDateFormat from '../_services/hooks/useDateformat'
+import useDateFormat from '../_services/hooks/useDateFormat'
 
 const props = withDefaults(defineProps<{syncUpdated: Date | string}>(), {
   syncUpdated: () => 'December 28, 2021 03:00PM (UTC)'
 })
 
-const { formateDate } = useDateFormat()
-const latestUpdateDate = computed(() => formateDate(props.syncUpdated))
+const { formatFullDate } = useDateFormat()
+const latestUpdateDate = computed(() => formatFullDate(props.syncUpdated))
 </script>
