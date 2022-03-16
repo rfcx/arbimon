640d3d4e9c6699d248a7294fa655b464012b59ca
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-03-16 12:17:45 +0700

    [637] Lookup project PKs instead of assuming (sites, profiles)

--- apps/cli/src/db/seeders/auto/212-location-sites.ts
+++ apps/cli/src/db/seeders/auto/212-location-sites.ts
@@ -5,16 +5,20 @@ import { LocationSiteModel } from '@rfcx-bio/common/dao/models/location-site-mod
 import { Site } from '@rfcx-bio/common/dao/types'
 import { rawSites } from '@rfcx-bio/common/mock-data'
 
+import { getPuertoRicoProjectId } from '@/db/_helpers/get-puerto-rico-id'
+
 export const up: MigrationFn<QueryInterface> = async (params): Promise<void> => {
-  const siteDao = LocationSiteModel(params.context.sequelize)
+  const sequelize = params.context.sequelize
 
-  const mockProjectId = 1
+  const prId = await getPuertoRicoProjectId(sequelize)
+  if (Number.isNaN(prId)) return
 
-  const sites: Array<Optional<Site, 'id'>> =
-    rawSites.map(({ id, locationProjectId, ...rest }) => ({ ...rest, locationProjectId: mockProjectId }))
+  // Save mock sites under PR project
+  const sites: Array<Optional<Site, 'id'>> = rawSites
+    .map(({ id, locationProjectId, ...rest }) => ({
+      ...rest,
+      locationProjectId: prId
+    }))
 
-  await siteDao.bulkCreate(sites).then(async () => {
-    // fix auto increment key break - https://github.com/sequelize/sequelize/issues/9295
-    await params.context.sequelize.query('select setval(\'location_site_id_seq\', (select max(id) from location_site), true);')
-  })
+  await LocationSiteModel(sequelize).bulkCreate(sites)
 }
