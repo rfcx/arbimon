cc2005aa755f44c42281bb07a09a4bfcc19877d5
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-03-16 21:50:30 +0700

    [653] Only sync Arbimon data during migrate job

--- apps/cli/src/db/seeders/04-sync/002-incremental-sync.ts
+++ apps/cli/src/db/seeders/04-sync/002-incremental-sync.ts
@@ -1,8 +1,17 @@
 import { QueryInterface } from 'sequelize'
 import { MigrationFn } from 'umzug'
 
-import { incrementalSync } from '@/sync/all'
+import { syncAllForProject } from '@/sync/all'
+import { getNeedSyncingProjects } from '@/sync/data-source'
 
 export const up: MigrationFn<QueryInterface> = async (params): Promise<void> => {
-  await incrementalSync(params.context.sequelize)
+  const sequelize = params.context.sequelize
+
+  console.info('STEP: Get project lookups')
+  const syncingProjects = await getNeedSyncingProjects(sequelize)
+
+  console.info('STEP: Sync site, species, and detections')
+  for (const project of syncingProjects) {
+    await syncAllForProject(sequelize, project)
+  }
 }
