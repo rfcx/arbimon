ef7cb6e6750f99ccae6906a0353266b9de922d56
Chanachai Yaemban <75108999+Crave61341416@users.noreply.github.com>
2022-03-16 16:57:51 +0700

    [633] Handle date display

--- apps/website/src/_components/last-sync.vue
+++ apps/website/src/_components/last-sync.vue
@@ -1,6 +1,6 @@
 <template>
   <p class="text-xs text-left py-2 mt-5 border-t-1 border-l-0 border-r-0 border-b-0 border-solid opacity-50">
-    Last generated based on validated analysis data from Arbimon at: {{ latestUpdateDate }}
+    {{ displayLastSynced }}
   </p>
 </template>
 
@@ -12,8 +12,11 @@ import useDateFormat from '../_services/hooks/use-date-format'
 const props = defineProps<{syncUpdated: Date | string | null}>()
 
 const { formatFullDate } = useDateFormat()
-const latestUpdateDate = computed(() => {
-  if (props.syncUpdated === null) return 'Detections data not synced'
-  return formatFullDate(props.syncUpdated)
+
+const displayLastSynced = computed(() => {
+  return props.syncUpdated === null
+    ? 'This project has been queued for detection data synchronization'
+    : `Last generated based on validated analysis data from Arbimon at: ${formatFullDate(props.syncUpdated)}`
 })
+
 </script>
