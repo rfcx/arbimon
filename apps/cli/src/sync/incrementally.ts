5e07aec3511d646ab3b81c51957df35abb684cb4
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-03-16 17:22:46 +0700

    [653] Run syncs as seeders (during CD)

--- apps/cli/src/sync/incrementally.ts
+++ apps/cli/src/sync/incrementally.ts
@@ -1,25 +1,13 @@
 import { refreshMviews } from '@/db/actions/refresh-mviews'
 import { getSequelize } from '@/db/connections'
-import { syncAllForProject } from '@/sync/all'
-import { getNeedSyncingProjects } from '@/sync/data-source'
-import { syncOnlyMissingIUCNSpeciesInfo } from '@/sync/species-info/iucn'
-import { syncOnlyMissingWikiSpeciesInfo } from '@/sync/species-info/wiki'
+import { incrementalSync } from '@/sync/all'
 
 const main = async (): Promise<void> => {
   console.info('Hourly sync start')
   try {
     const sequelize = getSequelize()
 
-    console.info('STEP: Get project lookups')
-    const syncingProjects = await getNeedSyncingProjects(sequelize)
-
-    console.info('STEP: Sync site, species, and detections')
-    for (const project of syncingProjects) {
-      await syncAllForProject(sequelize, project)
-    }
-
-    await syncOnlyMissingWikiSpeciesInfo(sequelize)
-    await syncOnlyMissingIUCNSpeciesInfo(sequelize)
+    await incrementalSync(sequelize)
 
     console.info('STEP: Refresh mviews')
     await refreshMviews(sequelize)
