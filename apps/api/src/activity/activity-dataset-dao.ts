a3e74f77fb108b58c00ca72fced62b21389243c1
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-03-16 19:43:19 +0700

    [651] Extract activity overview handler into handler, bll and dao

--- apps/api/src/activity/activity-dataset-dao.ts
+++ apps/api/src/activity/activity-dataset-dao.ts
@@ -1,82 +1,18 @@
 import { groupBy, mapValues, sum } from 'lodash-es'
 import { Op } from 'sequelize'
 
-import { ActivityDatasetParams, ActivityDatasetResponse } from '@rfcx-bio/common/api-bio/activity/activity-dataset'
 import { ActivityOverviewDataBySpecies, ActivityOverviewDetectionDataBySite, ActivityOverviewDetectionDataByTime } from '@rfcx-bio/common/api-bio/activity/common'
-import { FilterDatasetQuery } from '@rfcx-bio/common/api-bio/common/filter'
 import { Where } from '@rfcx-bio/common/dao/helpers/query'
-import { AllModels, ModelRepository } from '@rfcx-bio/common/dao/model-repository'
+import { AllModels } from '@rfcx-bio/common/dao/model-repository'
 import { DetectionBySiteSpeciesHour } from '@rfcx-bio/common/dao/types'
 import { groupByNumber } from '@rfcx-bio/utils/lodash-ext'
 
-import { BioInvalidPathParamError, BioInvalidQueryParamError } from '~/errors'
 import { RISK_RATING_PROTECTED_IDS } from '~/security/protected-species'
-import { Handler } from '../_services/api-helpers/types'
 import { FilterDataset } from '../_services/datasets/dataset-types'
 import { dayjs } from '../_services/dayjs-initialized'
-import { getSequelize } from '../_services/db'
-import { isProjectMember } from '../_services/permission-helper/permission-helper'
-import { assertPathParamsExist } from '../_services/validation'
-import { isValidDate } from '../_services/validation/query-validation'
-
-export const activityDatasetHandler: Handler<ActivityDatasetResponse, ActivityDatasetParams, FilterDatasetQuery> = async (req) => {
-  // Input & validation
-  const { projectId } = req.params
-  assertPathParamsExist({ projectId })
-
-  const projectIdInteger = parseInt(projectId)
-  if (Number.isNaN(projectIdInteger)) throw BioInvalidPathParamError({ projectId })
-
-  const { startDate, endDate, siteIds, taxons } = req.query
-  if (!isValidDate(startDate)) throw BioInvalidQueryParamError({ startDate })
-  if (!isValidDate(endDate)) throw BioInvalidQueryParamError({ endDate })
-
-  // Query
-  const datasetFilter: FilterDataset = {
-    locationProjectId: projectIdInteger,
-    startDateUtcInclusive: startDate,
-    endDateUtcInclusive: endDate,
-    // TODO ???: Better way to check query type!
-    siteIds: Array.isArray(siteIds) ? siteIds.map(Number) : typeof siteIds === 'string' ? [Number(siteIds)] : [],
-    taxons: Array.isArray(taxons) ? taxons.map(Number) : typeof taxons === 'string' ? [Number(taxons)] : []
-  }
-
-  const hasProjectPermission = isProjectMember(req)
-
-  // Response
-  return await getActivityOverviewData(datasetFilter, hasProjectPermission)
-}
-
-const getActivityOverviewData = async (filter: FilterDataset, hasProjectPermission: boolean): Promise<ActivityDatasetResponse> => {
-  const sequelize = getSequelize()
-  const models = ModelRepository.getInstance(sequelize)
-
-  const { locationProjectId } = filter
-
-  // Filtering
-  const totalDetections = await filterDetecions(models, locationProjectId, filter)
-  const totalRecordingCount = getRecordingDurationMinutes(totalDetections)
-
-  const detectionsBySite = await getDetectionDataBySite(models, totalDetections)
-  const detectionsBySpecies = await getDetectionDataBySpecies(models, totalDetections, hasProjectPermission, locationProjectId)
-  const detectionsByTimeHour = getDetectionsByTimeHour(totalDetections, totalRecordingCount)
-  const detectionsByTimeDay = getDetectionsByTimeDay(totalDetections, totalRecordingCount)
-  const detectionsByTimeMonth = getDetectionsByTimeMonth(totalDetections, totalRecordingCount)
-  const detectionsByTimeDate = getDetectionsByTimeDateUnix(totalDetections, totalRecordingCount)
-
-  return {
-    isLocationRedacted: !hasProjectPermission,
-    detectionsBySite,
-    detectionsBySpecies,
-    detectionsByTimeHour,
-    detectionsByTimeDay,
-    detectionsByTimeMonth,
-    detectionsByTimeDate
-  }
-}
 
 // Similar logic with filterDetection in `controller-spotlight-dataset.ts`. Moving to common?
-async function filterDetecions (models: AllModels, projectId: number, filter: FilterDataset): Promise<DetectionBySiteSpeciesHour[]> {
+export async function filterDetecions (models: AllModels, projectId: number, filter: FilterDataset): Promise<DetectionBySiteSpeciesHour[]> {
   const { startDateUtcInclusive, endDateUtcInclusive, siteIds, taxons } = filter
 
   const where: Where<DetectionBySiteSpeciesHour> = {
@@ -104,12 +40,12 @@ async function filterDetecions (models: AllModels, projectId: number, filter: Fi
   })
 }
 
-function getRecordingDurationMinutes (detections: DetectionBySiteSpeciesHour[]): number {
+export function getRecordingDurationMinutes (detections: DetectionBySiteSpeciesHour[]): number {
   const detectionBySiteHour = Object.values(groupBy(detections, d => `${d.timePrecisionHourLocal.getTime()}-${d.locationSiteId}`))
   return sum(detectionBySiteHour.map(speciesSummaries => speciesSummaries[0].durationMinutes))
 }
 
-function calculateDetectionCount (detections: DetectionBySiteSpeciesHour[]): number {
+export function calculateDetectionCount (detections: DetectionBySiteSpeciesHour[]): number {
   return sum(detections.map(({ count }) => count))
 }
 
@@ -118,11 +54,11 @@ function calculateDetectionFrequency (detections: DetectionBySiteSpeciesHour[], 
   return detectionCount === 0 ? 0 : detectionCount / totalRecordingCount
 }
 
-function calculateOccupancy (totalSiteCount: number, occupiedSites: number): number {
+export function calculateOccupancy (totalSiteCount: number, occupiedSites: number): number {
   return occupiedSites === 0 ? 0 : occupiedSites / totalSiteCount
 }
 
-const getDetectionDataBySite = async (models: AllModels, detections: DetectionBySiteSpeciesHour[]): Promise<ActivityOverviewDetectionDataBySite> => {
+export const getDetectionDataBySite = async (models: AllModels, detections: DetectionBySiteSpeciesHour[]): Promise<ActivityOverviewDetectionDataBySite> => {
   const detectionsBySites: { [siteId: number]: DetectionBySiteSpeciesHour[] } = groupBy(detections, 'locationSiteId')
   const siteIds = Object.keys(detectionsBySites)
 
@@ -155,7 +91,7 @@ const getDetectionDataBySite = async (models: AllModels, detections: DetectionBy
   return summariesBySites
 }
 
-async function getDetectionDataBySpecies (models: AllModels, detections: DetectionBySiteSpeciesHour[], hasProjectPermission: boolean, locationProjectId: number): Promise<ActivityOverviewDataBySpecies[]> {
+export async function getDetectionDataBySpecies (models: AllModels, detections: DetectionBySiteSpeciesHour[], hasProjectPermission: boolean, locationProjectId: number): Promise<ActivityOverviewDataBySpecies[]> {
   const totalRecordingCount = getRecordingDurationMinutes(detections)
   let filteredDetections = detections
 
@@ -206,7 +142,7 @@ async function getDetectionDataBySpecies (models: AllModels, detections: Detecti
   return activityOverviewDataBySpecies
 }
 
-function getDetectionsByTimeHour (specificSpeciesDetections: DetectionBySiteSpeciesHour[], totalRecordingCount: number): ActivityOverviewDetectionDataByTime {
+export function getDetectionsByTimeHour (specificSpeciesDetections: DetectionBySiteSpeciesHour[], totalRecordingCount: number): ActivityOverviewDetectionDataByTime {
   const byHour = groupByNumber(specificSpeciesDetections, d => dayjs.utc(d.timePrecisionHourLocal).hour())
   return {
     detection: mapValues(byHour, calculateDetectionCount),
@@ -214,7 +150,7 @@ function getDetectionsByTimeHour (specificSpeciesDetections: DetectionBySiteSpec
   }
 }
 
-function getDetectionsByTimeDay (specificSpeciesDetections: DetectionBySiteSpeciesHour[], totalRecordingCount: number): ActivityOverviewDetectionDataByTime {
+export function getDetectionsByTimeDay (specificSpeciesDetections: DetectionBySiteSpeciesHour[], totalRecordingCount: number): ActivityOverviewDetectionDataByTime {
   const byDay = groupByNumber(specificSpeciesDetections, d => dayjs.utc(d.timePrecisionHourLocal).isoWeekday() - 1)
   return {
     detection: mapValues(byDay, calculateDetectionCount),
@@ -222,7 +158,7 @@ function getDetectionsByTimeDay (specificSpeciesDetections: DetectionBySiteSpeci
   }
 }
 
-function getDetectionsByTimeMonth (specificSpeciesDetections: DetectionBySiteSpeciesHour[], totalRecordingCount: number): ActivityOverviewDetectionDataByTime {
+export function getDetectionsByTimeMonth (specificSpeciesDetections: DetectionBySiteSpeciesHour[], totalRecordingCount: number): ActivityOverviewDetectionDataByTime {
   const byMonth = groupByNumber(specificSpeciesDetections, d => dayjs.utc(d.timePrecisionHourLocal).month())
   return {
     detection: mapValues(byMonth, calculateDetectionCount),
@@ -230,7 +166,7 @@ function getDetectionsByTimeMonth (specificSpeciesDetections: DetectionBySiteSpe
   }
 }
 
-function getDetectionsByTimeDateUnix (specificSpeciesDetections: DetectionBySiteSpeciesHour[], totalRecordingCount: number): ActivityOverviewDetectionDataByTime {
+export function getDetectionsByTimeDateUnix (specificSpeciesDetections: DetectionBySiteSpeciesHour[], totalRecordingCount: number): ActivityOverviewDetectionDataByTime {
   const SECONDS_PER_DAY = 24 * 60 * 60
   const byDateUnix = groupByNumber(specificSpeciesDetections, d => dayjs.utc(d.timePrecisionHourLocal).startOf('day').unix() / SECONDS_PER_DAY)
   return {
