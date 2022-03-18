664a473c847d7ef016046ce108a0482442f3f426
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-03-18 10:00:48 +0700

    [Seeds] Sort by slug (to improve diffs); Simplify helper & move to common

--- apps/cli/src/db/seeders/_helpers/export-seed-taxon-species-iucn.ts
+++ apps/cli/src/db/seeders/_helpers/export-seed-taxon-species-iucn.ts
@@ -1,16 +1,20 @@
 import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
+import { attributesExceptIdAndDates } from '@rfcx-bio/common/dao/query-helpers/attributes'
 
-import { getDataFromBioDb } from '@/data-ingest/species/input-bio-db/from-bio-db'
 import { toTaxonSpeciesIucnSeed } from '@/data-ingest/species/output-seed-data/to-taxon-species-iucn-seed'
 import { getSequelize } from '@/db/connections'
 
 const main = async (): Promise<void> => {
   const models = ModelRepository.getInstance(getSequelize())
 
-  const data = await getDataFromBioDb(
-    models.TaxonSpeciesIucn,
-    [{ model: models.TaxonSpecies, attributes: ['slug'], required: true }]
-  )
+  const attributes = attributesExceptIdAndDates(models.TaxonSpeciesIucn)
+  const data = await models.TaxonSpeciesIucn
+    .findAll({
+      attributes,
+      include: [{ model: models.TaxonSpecies, attributes: ['slug'], required: true }],
+      order: [[models.TaxonSpecies, 'slug']],
+      raw: true
+    })
 
   toTaxonSpeciesIucnSeed(data)
 }
