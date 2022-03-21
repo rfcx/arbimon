c36becc11ca0ad577827538e58473ed8d78e83be
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-03-21 15:57:19 +0700

    [678] Add cache table for project core ids

---
+++ packages/common/src/dao/models/user-core-projects-model.ts
@@ -0,0 +1,30 @@
+import { DataTypes } from 'sequelize'
+
+import { defineWithDefaults } from '../model-factory-helpers/defaults'
+import { UserCoreProjects } from '../types'
+
+export const MODEL_USER_CORE_PROJECTS = 'userCoreProjects'
+export const TABLE_USER_CORE_PROJECTS = 'user_core_projects'
+
+export const UserCoreProjectsModel = defineWithDefaults<UserCoreProjects>(
+  MODEL_USER_CORE_PROJECTS,
+  {
+    // PK
+    userId: { // 1
+      type: DataTypes.STRING,
+      primaryKey: true
+    },
+    // Facts
+    projectCoreIds: {
+      type: DataTypes.ARRAY(DataTypes.STRING(255)),
+      allowNull: false
+    },
+    expiredAt: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  },
+  {
+    tableName: TABLE_USER_CORE_PROJECTS
+  }
+)
