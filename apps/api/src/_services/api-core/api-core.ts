c6ab084fbaaa7a088cee9c49d8495e6016b8b656
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-03-21 14:45:23 +0700

    [672] Add get project ids from core

--- apps/api/src/_services/api-core/api-core.ts
+++ apps/api/src/_services/api-core/api-core.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { FastifyLoggerInstance } from 'fastify'
 
-import { CoreProject } from '@rfcx-bio/common/api-bio/common/permission'
+import { CoreProject, CoreProjectLight } from '@rfcx-bio/common/api-bio/common/permission'
 
 import { ApiClient } from '../api-helpers/api-client'
 import { unpackAxiosError } from '../api-helpers/axios-errors'
@@ -23,4 +23,14 @@ export async function getProjectPermission (projectId: string, token: string): P
 export async function getMedia (logger: FastifyLoggerInstance, url: string): Promise<ArrayBuffer | undefined> {
   // ! `blob` is a "browser only" option. read more here: https://stackoverflow.com/a/60461828
   return await ApiClient.getInstance(logger).getOrUndefined<ArrayBuffer>(url, { responseType: 'arraybuffer' })
+}
+
+export async function getUserProjectIds (token: string): Promise<string[]> {
+  return await axios.request<CoreProjectLight[]>({
+    method: 'GET',
+    url: `${CORE_API_BASE_URL}/projects?fields=id`,
+    headers: { authorization: token }
+  })
+  .then(r => r.data.map(({ id }) => id))
+  .catch(unpackAxiosError)
 }
