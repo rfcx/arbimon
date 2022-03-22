1040edf191023f80ac5ada1f166c21f24195f385
Chanachai Yaemban <75108999+Crave61341416@users.noreply.github.com>
2022-03-22 11:15:59 +0700

    [634] Sync history list

--- apps/website/src/sync-history/sync-history.vue
+++ apps/website/src/sync-history/sync-history.vue
@@ -1,13 +1,31 @@
+import useDateFormat from '~/hooks/use-date-format';
 <template>
   <page-title
     page-title="Sync History"
     page-subtitle="TODO: Subtitle"
   />
   <div class="mt-5">
-    content
+    <ul
+      v-for="update of allUpdate"
+      :key="update.id"
+    >
+      <li>{{ formatDate(update.updatedAt) }}</li>
+      <li>{{ JSON.parse(update.summaryText) }}</li>
+    </ul>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue'
+
+import useDateFormat from '../_services/hooks/use-date-format'
+import { useStore } from '../_services/store'
+
+const store = useStore()
+const { formatDate } = useDateFormat()
+
+const allUpdate = computed(() => store.projectFilters?.updatedList)
+
+</script>
 
 <style lang="scss"></style>
