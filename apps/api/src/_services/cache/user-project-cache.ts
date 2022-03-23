fdfbea51174941cdf42e5d85cd5b9566824785d3
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-03-23 11:41:25 +0700

    [672] Refactor cache to apply date check in DB

--- apps/api/src/_services/cache/user-project-cache.ts
+++ apps/api/src/_services/cache/user-project-cache.ts
@@ -1,3 +1,5 @@
+import { Op } from 'sequelize'
+
 import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
 
 import { dayjs } from '~/dayjs-initialized'
@@ -8,11 +10,11 @@ export const EXPIRED_DURATION_HOURS = 1
 export const getMemberProjectCoreIdsFromCache = async (userIdAuth0: string): Promise<string[] | undefined> =>
   await ModelRepository.getInstance(getSequelize())
     .CacheUserProject
-    .findByPk(userIdAuth0, { raw: true })
-    .then(cache => {
-      if (!cache || dayjs(cache?.expiredAt).isAfter(dayjs())) return undefined
-      return cache?.projectCoreIds
+    .findOne({
+      where: { userIdAuth0, expiredAt: { [Op.gt]: dayjs().toISOString() } },
+      raw: true
     })
+    .then(cache => cache?.projectCoreIds)
 
 export const updateMemberProjectCoreIds = async (userIdAuth0: string, projectCoreIds: string[]): Promise<void> => {
   const expiredAt = dayjs().add(EXPIRED_DURATION_HOURS, 'hours').toDate()
