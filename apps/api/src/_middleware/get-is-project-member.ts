f3ed9c55e4b8784e43222da7567cb0632d895362
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-03-23 09:37:54 +0700

    [672] Colocate `isProjectMember`/`getMemberProjects` code

---
+++ apps/api/src/_middleware/get-is-project-member.ts
@@ -0,0 +1,51 @@
+import { FastifyRequest } from 'fastify'
+
+import { ProjectSpecificRouteParams } from '@rfcx-bio/common/api-bio/common/project-specific-route'
+import { LocationProjectModel } from '@rfcx-bio/common/dao/models/location-project-model'
+
+import { getProjectPermission } from '~/api-core/api-core'
+import { isValidToken } from '~/api-helpers/is-valid-token'
+import { Middleware } from '~/api-helpers/types'
+import { getSequelize } from '~/db'
+import { BioPublicError, ERROR_STATUS_CODE } from '~/errors'
+
+const IS_PROJECT_MEMBER = 'IS_PROJECT_MEMBER'
+
+export const getIsProjectMember = (req: FastifyRequest): boolean =>
+  req.requestContext.get(IS_PROJECT_MEMBER) === true
+
+export const loadIsProjectMember: Middleware<ProjectSpecificRouteParams> = async (req, res): Promise<void> => {
+  const token = req.headers.authorization
+  const projectIdBio = Number(req.params.projectId)
+
+  // No token => not project member
+  if (token === undefined || !isValidToken(token) || projectIdBio === undefined) {
+    req.requestContext.set(IS_PROJECT_MEMBER, false)
+    return
+  }
+
+  // Get idCore
+  const projectIdCore = await LocationProjectModel(getSequelize())
+    .findByPk(projectIdBio, { attributes: ['idCore'] })
+    .then(proj => proj?.idCore)
+
+  // No project => not project member
+  if (projectIdCore === undefined) {
+    req.requestContext.set(IS_PROJECT_MEMBER, false)
+    return
+  }
+
+  // Call Core API
+  const isMember = await getProjectPermission(projectIdCore, token)
+    .then(() => true)
+    .catch(err => {
+      // Forbidden is expected (it means the user is not a project member)
+      if (err instanceof BioPublicError && err.statusCode === ERROR_STATUS_CODE.forbidden) return false
+
+      // Log unexpected errors
+      req.log.error(err)
+      return false
+    })
+
+  req.requestContext.set(IS_PROJECT_MEMBER, isMember)
+}
--- apps/api/src/projects/projects-handler.ts
+++ apps/api/src/projects/projects-handler.ts
@@ -2,14 +2,14 @@ import { ProjectsResponse } from '@rfcx-bio/common/api-bio/common/projects'
 import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
 import { ATTRIBUTES_LOCATION_PROJECT } from '@rfcx-bio/common/dao/types'
 
+import { getMemberProjectCoreIds } from '@/_middleware/get-member-projects'
 import { ApiServerError } from '~/errors'
-import { getCoreProjectIds } from '~/permission-helper/permission-helper'
 import { Handler } from '../_services/api-helpers/types'
 import { getSequelize } from '../_services/db'
 
 export const projectsAllHandler: Handler<ProjectsResponse> = async (req) => {
   // Inputs & validation
-  const accessibleProjectCoreIds = getCoreProjectIds(req)
+  const accessibleProjectCoreIds = getMemberProjectCoreIds(req)
   if (accessibleProjectCoreIds === undefined || accessibleProjectCoreIds.length === 0) return []
 
   const models = ModelRepository.getInstance(getSequelize())
