824cfee4357bf355ae0e4d0a8ae97d5cbab547e2
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-03-23 09:48:41 +0700

    [672] Temporarily hard-code public projects; Extract DAO

---
+++ apps/api/src/projects/projects-dao.ts
@@ -0,0 +1,57 @@
+import { Op } from 'sequelize'
+
+import { LocationProjectForUser } from '@rfcx-bio/common/api-bio/common/projects'
+import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
+import { ATTRIBUTES_LOCATION_PROJECT } from '@rfcx-bio/common/dao/types'
+
+import { getSequelize } from '~/db'
+
+export const getUserProjects = async (memberProjectCoreIds: string[]): Promise<LocationProjectForUser[]> => {
+  const models = ModelRepository.getInstance(getSequelize())
+
+  const projects = memberProjectCoreIds.length === 0
+    ? []
+    : await models.LocationProject
+      .findAll({
+        order: ['name'],
+        where: { idCore: memberProjectCoreIds },
+        attributes: ATTRIBUTES_LOCATION_PROJECT.light,
+        raw: true
+      })
+
+  // Temporarily hard-code public projects
+  // TODO: Remove this hack & use ProjectVersion data
+  const publicSlugs = [
+    'bci-panama-2018',
+    'fake-arbimon-project-for-bio',
+    'puerto-rico',
+    'puerto-rico-island-wide',
+    'rfcx-guardians-madre-de-dios-peru'
+  ]
+
+  const myProjectIds = projects.map(p => p.id)
+  const publicProjects = await models.LocationProject
+    .findAll({
+      where: {
+        id: { [Op.notIn]: myProjectIds },
+        slug: publicSlugs
+      },
+      attributes: ATTRIBUTES_LOCATION_PROJECT.light,
+      raw: true
+    })
+
+  return [
+    ...projects.map(p => ({
+      ...p,
+      isMyProject: true,
+      hasPublishedVersions: publicSlugs.includes(p.slug),
+      hasPublicVersions: publicSlugs.includes(p.slug)
+    })),
+    ...publicProjects.map(p => ({
+      ...p,
+      isMyProject: false,
+      hasPublishedVersions: true,
+      hasPublicVersions: true
+    }))
+  ]
+}
--- apps/api/src/projects/projects-handler.ts
+++ apps/api/src/projects/projects-handler.ts
@@ -1,28 +1,19 @@
 import { ProjectsResponse } from '@rfcx-bio/common/api-bio/common/projects'
-import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
-import { ATTRIBUTES_LOCATION_PROJECT } from '@rfcx-bio/common/dao/types'
 
 import { getMemberProjectCoreIds } from '@/_middleware/get-member-projects'
+import { getProjects } from '@/projects/projects-bll'
 import { ApiServerError } from '~/errors'
 import { Handler } from '../_services/api-helpers/types'
-import { getSequelize } from '../_services/db'
 
 export const projectsAllHandler: Handler<ProjectsResponse> = async (req) => {
-  // Inputs & validation
-  const accessibleProjectCoreIds = getMemberProjectCoreIds(req)
-  if (accessibleProjectCoreIds === undefined || accessibleProjectCoreIds.length === 0) return []
+  try {
+    // Inputs & validation
+    const memberProjectCoreIds = getMemberProjectCoreIds(req)
 
-  const models = ModelRepository.getInstance(getSequelize())
-  const projects = await models.LocationProject
-    .findAll({
-      order: ['name'],
-      where: { idCore: accessibleProjectCoreIds },
-      attributes: ATTRIBUTES_LOCATION_PROJECT.light
-    })
-    .catch(err => {
-      req.log.error(err)
-      throw ApiServerError()
-    })
-
-  return projects
+    // Response
+    return await getProjects(memberProjectCoreIds)
+  } catch (err) {
+    req.log.error(err)
+    throw ApiServerError()
+  }
 }
