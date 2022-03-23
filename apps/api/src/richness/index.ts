ec213c0c71275d622c64704e1b8e259f1915234c
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-03-23 20:45:43 +0700

    [672] Add caching to `isProjectMember`

--- apps/api/src/richness/index.ts
+++ apps/api/src/richness/index.ts
@@ -1,6 +1,6 @@
 import { richnessDatasetRoute } from '@rfcx-bio/common/api-bio/richness/richness-dataset'
 
-import { loadIsProjectMember } from '@/_middleware/get-is-project-member'
+import { setIsProjectMember } from '@/_middleware/get-is-project-member'
 import { GET, RouteRegistration } from '../_services/api-helpers/types'
 import { richnessDatasetHandler } from './richness-dataset-handler'
 
@@ -8,7 +8,7 @@ export const routesRichness: RouteRegistration[] = [
   {
     method: GET,
     url: richnessDatasetRoute,
-    preHandler: [loadIsProjectMember],
+    preHandler: [setIsProjectMember],
     handler: richnessDatasetHandler
   }
 ]
