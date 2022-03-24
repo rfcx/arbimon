fa8c22e8b325a7876034674f1587f92d43dc7ced
Somjintana Korbut <nuisomjin@gmail.com>
2022-03-24 23:59:52 +0700

    [Test] edit some sql code for project

--- apps/cli/src/data-ingest/projects/arbimon/index.ts
+++ apps/cli/src/data-ingest/projects/arbimon/index.ts
@@ -1,4 +1,4 @@
-import { Sequelize } from 'sequelize'
+import { QueryTypes, Sequelize } from 'sequelize'
 
 import { Project } from '@rfcx-bio/common/dao/types'
 
@@ -18,21 +18,21 @@ export interface ArbimonProject {
 
 export const getArbimonProjects = async (arbimonSequelize: Sequelize): Promise<Array<Omit<Project, 'id'>>> => {
   const sql = `
-    select p.project_id, p.name, p.url slug, p.description, p.is_private, p.is_enabled, p.external_id core_project_id, p.reports_enabled, s.north, s.east, s.south, s.west
-    from projects p right join
+    SELECT p.project_id, p.name, p.url slug, p.description, p.is_private, p.is_enabled, p.external_id core_project_id, p.reports_enabled, s.north, s.east, s.south, s.west
+    FROM projects p LEFT JOIN
     (
-      select Max(s.lon) west, Min(s.lon) east, Min(s.lat) north, Max(s.lat) south, project_id 
-      from sites s
-      group by project_id 
+      SELECT Max(s.lon) west, Min(s.lon) east, Min(s.lat) north, Max(s.lat) south, project_id 
+      FROM sites s
+      GROUP BY project_id 
     ) s
-      on p.project_id = s.project_id
-    where p.external_id is not null and p.external_id != "undefined";
+      ON p.project_id = s.project_id
+    WHERE p.external_id is not null AND p.external_id != "undefined";
     `
 
-  const results = await arbimonSequelize.query(sql, { raw: true }) as ArbimonProject[]
+  const results: ArbimonProject[] = await arbimonSequelize.query(sql, { type: QueryTypes.SELECT, raw: true })
   return results.map(i => {
     return {
-      idCore: i.core_project_id,
+      idCore: i.core_project_id || '',
       idArbimon: i.project_id,
       slug: i.slug,
       slugArbimon: i.slug,
