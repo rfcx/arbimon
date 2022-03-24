5434594c3ded2698aa4b66d0ffc70479d255dc25
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-03-24 12:38:00 +0700

    [Sync] Prioritize key projects

--- apps/cli/src/sync/data-source/index.ts
+++ apps/cli/src/sync/data-source/index.ts
@@ -3,27 +3,42 @@ import { Op, QueryTypes, Sequelize } from 'sequelize'
 import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
 import { Project } from '@rfcx-bio/common/dao/types'
 
+import { rawEnvToProjectAndProfile } from '@/db/seeders/_data/location-project-and-profile'
+import { requireEnv } from '~/env'
+
 export const getNeedSyncingProjects = async (sequelize: Sequelize): Promise<Project[]> => {
+  // Temporarily prioritize some
+  const { BIO_ENVIRONMENT } = requireEnv('BIO_ENVIRONMENT')
+  const projectSlugs = [
+      ...rawEnvToProjectAndProfile[BIO_ENVIRONMENT].map(project => project.slugArbimon),
+      'destinos-awake'
+    ]
+    .map(s => `'${s}'`)
+    .join(',')
+
   const sql = `
-    SELECT DISTINCT lp.id, lp.id_arbimon, ds.updated_at 
+    SELECT DISTINCT 
+      CASE WHEN (lp.slug_arbimon IN (${projectSlugs})) THEN 0 ELSE 1 END,
+      lp.id,
+      lp.id_arbimon,
+      ds.updated_at
     FROM location_project lp
-    LEFT JOIN data_source ds ON lp.id = ds.location_project_id 
-    WHERE ds.location_project_id IS NULL
-      OR DATE_PART('hour',AGE(CURRENT_TIMESTAMP, lp.updated_at)) >= 1
-    ORDER BY ds.updated_at DESC
+    LEFT JOIN data_source ds ON lp.id = ds.location_project_id
+    WHERE ds.location_project_id IS NULL 
+      OR DATE_PART('hour', AGE(CURRENT_TIMESTAMP, lp.updated_at)) >= 1
+    ORDER BY 1, ds.updated_at DESC
     LIMIT 10
-    `
+  `
 
-    const projectIds = await sequelize
-      .query<{ id: number, id_Arbimon: number, updated_at: Date | null }>(sql, { type: QueryTypes.SELECT, raw: true })
-      .then(p => p.map(i => i.id))
+  const projectIds = await sequelize
+    .query<{ id: number, id_Arbimon: number, updated_at: Date | null }>(sql, { type: QueryTypes.SELECT, raw: true })
+    .then(p => p.map(i => i.id))
 
-    return await ModelRepository.getInstance(sequelize).LocationProject.findAll({
-      where: {
-        id: {
-          [Op.in]: projectIds
-        }
-      },
-      raw: true
-    })
+  // TODO: Why is this a separate query?!
+  return await ModelRepository.getInstance(sequelize).LocationProject.findAll({
+    where: {
+      id: { [Op.in]: projectIds }
+    },
+    raw: true
+  })
 }
