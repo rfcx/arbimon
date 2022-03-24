9337cb0198249c63412443ed4d427d7de95f1ff2
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-03-24 17:08:46 +0700

    [660] Add the UI for handling user project and public project and logic for searching project inside the project select modal

--- apps/website/src/_layout/components/project-selector/project-selector.ts
+++ apps/website/src/_layout/components/project-selector/project-selector.ts
@@ -1,4 +1,5 @@
 import { OnClickOutside } from '@vueuse/components'
+import { slice } from 'lodash-es'
 import { Options, Vue } from 'vue-class-component'
 import { Emit, Inject } from 'vue-property-decorator'
 import { RouteParamsRaw } from 'vue-router'
@@ -7,18 +8,35 @@ import { LocationProjectForUser } from '@rfcx-bio/common/api-bio/common/projects
 
 import { ROUTE_NAMES } from '~/router'
 import { BiodiversityStore } from '~/store'
+import ProjectList from './project-list.vue'
+
+const MAX_DISPLAY_ITEMS = 5
 
 @Options({
-  components: { OnClickOutside }
+  components: {
+    OnClickOutside,
+    ProjectList
+  }
 })
 export default class ProjectSelectorComponent extends Vue {
   @Inject() readonly store!: BiodiversityStore
   @Emit() emitCloseProjectSelector (): boolean { return false }
 
   newSelectedProject: LocationProjectForUser | null = null
+  searchKeyword: string | null = null
 
   get selectedProject (): LocationProjectForUser | null {
     return this.store.selectedProject ?? null
+  }
+
+  get userProjects (): LocationProjectForUser[] {
+    const allMyProjects = this.store.projects.filter(({ isMyProject }) => isMyProject)
+    return this.filterProject(allMyProjects)
+  }
+
+  get publicProjects (): LocationProjectForUser[] {
+    const allPublicProjects = this.store.projects.filter(({ isMyProject }) => !isMyProject)
+    return this.filterProject(allPublicProjects)
   }
 
   override created (): void {
@@ -33,6 +51,23 @@ export default class ProjectSelectorComponent extends Vue {
 
   setSelectedProject (project: LocationProjectForUser): void {
     this.newSelectedProject = project
+  }
+
+  filterProject (projects: LocationProjectForUser[]): LocationProjectForUser[] {
+    const keyword = this.searchKeyword
+    if (keyword !== null && keyword.length > 0) {
+      const filteredProjects = projects
+        .filter(({ name }) => name.toLowerCase().includes(keyword.toLowerCase()))
+        .sort((a, b) => a.name.localeCompare(b.name))
+      return slice(filteredProjects, 0, MAX_DISPLAY_ITEMS)
+    }
+    const matchedProjectIdx = projects.findIndex(({ id }) => id === this.selectedProject?.id)
+    if (this.selectedProject !== null && matchedProjectIdx !== -1) {
+      const matchedProject = projects[matchedProjectIdx]
+      projects.splice(matchedProjectIdx, 1)
+      return slice([matchedProject, ...projects], 0, MAX_DISPLAY_ITEMS)
+    }
+    return slice(projects, 0, MAX_DISPLAY_ITEMS)
   }
 
   async confirmedSelectedProject (): Promise<void> {
