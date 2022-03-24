b850dda07f209a0266a32024dc3030ace605b571
Somjintana Korbut <nuisomjin@gmail.com>
2022-03-25 00:28:52 +0700

    [Sync] move reusable code to beforeAll method

--- apps/cli/src/sync/arbimon/index.int.test.ts
+++ apps/cli/src/sync/arbimon/index.int.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from 'vitest'
+import { beforeAll, expect, test } from 'vitest'
 
 import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
 
@@ -6,15 +6,15 @@ import { getPopulatedArbimonInMemorySequelize } from '@/data-ingest/_testing/arb
 import { getSequelize } from '@/db/connections'
 import { syncProjects } from './index'
 
+// Arrange
 const testProjectSlug = 'rfcx-th'
-test('New project created', async () => {
-  // Arrange
-  const arbimonSequelize = await getPopulatedArbimonInMemorySequelize()
-  const biodiversitySequelize = getSequelize()
+const arbimonSequelize = await getPopulatedArbimonInMemorySequelize()
+const biodiversitySequelize = getSequelize()
 
-  // Act
+beforeAll(async () => {
   await syncProjects(arbimonSequelize, biodiversitySequelize)
-
+})
+test('New project created', async () => {
   // Assert
   const models = ModelRepository.getInstance(biodiversitySequelize)
   const project = await models.LocationProject.findOne({ where: { slugArbimon: testProjectSlug } })
@@ -22,17 +22,15 @@ test('New project created', async () => {
 })
 
 test('Project name updated', async () => {
-  // Arrange
-  const arbimonSequelize = await getPopulatedArbimonInMemorySequelize()
-  const biodiversitySequelize = getSequelize()
-  await syncProjects(arbimonSequelize, biodiversitySequelize)
   const projectName = 'Kitty at NU'
-  arbimonSequelize.query(`update projects set name = '${projectName}'`)
+  void arbimonSequelize.query(`update projects set name = '${projectName}'`)
 
   // Act
   await syncProjects(arbimonSequelize, biodiversitySequelize)
 
   // Assert
-  const project = await ModelRepository.getInstance(biodiversitySequelize).LocationProject.findOne({ where: { slugArbimon: testProjectSlug } })
-  expect(project!.name).toBe(projectName)
+  const project = await ModelRepository.getInstance(biodiversitySequelize)
+    .LocationProject
+    .findOne({ where: { slugArbimon: testProjectSlug } })
+    expect(project?.name).toBe(projectName)
 })
