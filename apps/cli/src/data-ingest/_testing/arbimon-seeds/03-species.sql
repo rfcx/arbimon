22d2ef55c9d6fdb541a506646fb9a275b44629c7
Somjintana Korbut <nuisomjin@gmail.com>
2022-03-24 23:02:55 +0700

    [Test] Fix the input

--- apps/cli/src/data-ingest/_testing/arbimon-seeds/03-species.sql
+++ apps/cli/src/data-ingest/_testing/arbimon-seeds/03-species.sql
@@ -1,1 +1,12 @@
-INSERT INTO species (scientific_name,code_name,taxon_id,family_id,image,description,biotab_id,defined_by) VALUES ('Crypturellus boucardi','CRYBOU',1,1,NULL,NULL,NULL,NULL), ('Nettapus coromandelianus','NETCOR',1,14,NULL,NULL,NULL,NULL), ('Falco amurensis','FALAMU',1,35,NULL,NULL,NULL,NULL), ('Eudynamys scolopaceus','EUDSCO',1,70,NULL,NULL,NULL,NULL), ('Merops orientalis','MERORI',1,89,NULL,NULL,NULL,NULL), ('Carpodacus erythrinus','CARERY',1,189,NULL,NULL,NULL,NULL), ('Duttaphrynus melanostictus','DUTMEL',2,207,NULL,NULL,NULL,NULL), ('Hemidactylium scutatum','HEMSCU',2,241,NULL,NULL,NULL,NULL), ('Chiropotes satanas','CHISAT',5,258,NULL,NULL,0,NULL), ('Aepyceros melampus',NULL,5,362,NULL,NULL,NULL,NULL);
+INSERT INTO species (species_id,scientific_name,code_name,taxon_id,family_id,image,description,biotab_id,defined_by) 
+VALUES 
+  (74,'Crypturellus boucardi','CRYBOU',1,1,NULL,NULL,NULL,NULL), 
+  (501,'Nettapus coromandelianus','NETCOR',1,14,NULL,NULL,NULL,NULL), 
+  (1050,'Falco amurensis','FALAMU',1,35,NULL,NULL,NULL,NULL), 
+  (2755,'Eudynamys scolopaceus','EUDSCO',1,70,NULL,NULL,NULL,NULL), 
+  (3842,'Merops orientalis','MERORI',1,89,NULL,NULL,NULL,NULL), 
+  (9620,'Carpodacus erythrinus','CARERY',1,189,NULL,NULL,NULL,NULL), 
+  (12204,'Duttaphrynus melanostictus','DUTMEL',2,207,NULL,NULL,NULL,NULL), 
+  (12675,'Hemidactylium scutatum','HEMSCU',2,241,NULL,NULL,NULL,NULL), 
+  (16729,'Chiropotes satanas','CHISAT',5,258,NULL,NULL,0,NULL), 
+  (42251,'Aepyceros melampus',NULL,5,362,NULL,NULL,NULL,NULL);
--- apps/cli/src/data-ingest/_testing/arbimon.ts
+++ apps/cli/src/data-ingest/_testing/arbimon.ts
@@ -1,16 +1,17 @@
-import { readFileSync, readdirSync } from 'fs'
-import { Sequelize } from 'sequelize'
+import { readdirSync, readFileSync } from 'fs'
 import path from 'path'
+import { Sequelize } from 'sequelize'
 
-function readAllFiles(folder: string, extension: string): string[] {
+function readAllFiles (folder: string, extension: string): string[] {
   const files = readdirSync(folder).filter(file => file.endsWith(`.${extension}`))
   return files.map(file => readFileSync(path.join(folder, file)).toString())
 }
 
-async function sequentialQueryAll(sqlFolder: string, sequelize: Sequelize) {
+async function sequentialQueryAll (sqlFolder: string, sequelize: Sequelize): Promise<void> {
   const sqlFiles = readAllFiles(sqlFolder, 'sql')
   for (const sql of sqlFiles) {
     try {
+      console.error('running SQL:\n' + sql)
       await sequelize.query(sql)
     } catch (err) {
       console.error('Failed running SQL:\n' + sql)
@@ -23,7 +24,7 @@ export const getPopulatedArbimonInMemorySequelize = async (): Promise<Sequelize>
   const testingFolder = path.join(__dirname, '../../../src/data-ingest/_testing') // TODO Find a better way of getting the path
   const ddlFolder = path.join(testingFolder, 'arbimon-ddl')
   const seedsFolder = path.join(testingFolder, 'arbimon-seeds')
-  
+
   const sequelize = new Sequelize('sqlite::memory:', { logging: false })
   await sequentialQueryAll(ddlFolder, sequelize)
   await sequentialQueryAll(seedsFolder, sequelize)
