ab9c8566cd3c870706308410e1d4bafdf1cc6e1d
Somjintana Korbut <nuisomjin@gmail.com>
2022-03-24 11:18:13 +0700

    Revert "[Test] create Sequelize for Arbimon DB"
    This reverts commit a70296cc31ae552162fea9d12b321800a86b0b8d.

--- apps/cli/src/data-ingest/_connections/arbimon.ts
+++ apps/cli/src/data-ingest/_connections/arbimon.ts
@@ -1,5 +1,4 @@
 import { ConnectionOptions } from 'mysql2/promise'
-import { Options, Sequelize } from 'sequelize'
 
 import { requireEnv } from '~/env'
 
@@ -7,39 +6,10 @@ import { requireEnv } from '~/env'
 const { ARBIMON_DB_USER, ARBIMON_DB_PASSWORD, ARBIMON_DB_HOSTNAME, ARBIMON_DB_DBNAME } =
   requireEnv('ARBIMON_DB_USER', 'ARBIMON_DB_PASSWORD', 'ARBIMON_DB_HOSTNAME', 'ARBIMON_DB_DBNAME')
 
-// TODO: remove Arbimon config and use Sequelize instead
 export const ARBIMON_CONFIG: ConnectionOptions = {
   user: ARBIMON_DB_USER,
   password: ARBIMON_DB_PASSWORD,
   host: ARBIMON_DB_HOSTNAME,
   database: ARBIMON_DB_DBNAME,
   timezone: '+00:00'
-}
-
-interface ArbimonConnectionOptions {
-  user: string
-  password: string
-  host: string
-  database: string
-  timezone: string
-}
-
-export const getArbimonSequelize = (co: ArbimonConnectionOptions): Sequelize => {
-  // Setup sequelize (ORM)
-  const sequelizeOptions: Options = {
-    host: co.host,
-    dialect: 'mysql',
-    define: {
-      charset: 'utf8',
-      collate: 'utf8_general_ci'
-    },
-    logging: true
-  }
-
-  return new Sequelize(
-    co.database,
-    co.user,
-    co.password,
-    sequelizeOptions
-  )
 }
