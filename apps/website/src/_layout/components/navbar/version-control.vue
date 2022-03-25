5c38c6e863f672eca3c35b4934b7d242ee41d82e
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-03-25 13:17:45 +0700

    [664] Add version control dropdown

---
+++ apps/website/src/_layout/components/navbar/version-control.vue
@@ -0,0 +1,24 @@
+<template>
+  <select
+    v-model="selectedVersion"
+    class="bg-subtle px-3 py-1 w-40"
+  >
+    <option
+      value="draft"
+      class="bg-steel-grey border-none"
+    >
+      Draft
+    </option>
+    <option
+      value="publish"
+      class="bg-subtle border-none"
+    >
+      Publish
+    </option>
+  </select>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const selectedVersion = ref('draft')
+</script>
