cefdbae2156fd0b7b4f535aa5dff23d389956a25
Somjintana Korbut <nuisomjin@gmail.com>
2022-03-25 10:13:39 +0700

    [643] fix the build

--- apps/cli/src/db/seeders/04-sync/001-project-sync.ts
+++ apps/cli/src/db/seeders/04-sync/001-project-sync.ts
@@ -1,8 +1,8 @@
 import { QueryInterface } from 'sequelize'
 import { MigrationFn } from 'umzug'
 
-import { syncProjects } from '@/sync/arbimon'
 import { getArbimonSequelize } from '@/data-ingest/_connections/arbimon'
+import { syncProjects } from '@/sync/arbimon'
 
 export const up: MigrationFn<QueryInterface> = async (params): Promise<void> => {
   await syncProjects(getArbimonSequelize(), params.context.sequelize)
