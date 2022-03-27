7b5ffb1a8bfc25c5c3bb2c6bba969282d183c20f
Somjintana Korbut <nuisomjin@gmail.com>
2022-03-28 00:30:03 +0700

    #683 add more test cases on the detections sync

--- apps/cli/src/sync/arbimon/detections.int.test.ts
+++ apps/cli/src/sync/arbimon/detections.int.test.ts
@@ -4,11 +4,11 @@ import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
 
 import { getPopulatedArbimonInMemorySequelize } from '@/data-ingest/_testing/arbimon'
 import { getSequelize } from '@/db/connections'
-import { syncAllForProject } from '@/sync/all'
-import { syncProjects } from '@/sync/arbimon'
+import { syncAllBasedOnDetectionsForProject, syncProjects } from '@/sync/arbimon'
 
 // Arrange
 const testProjectIdArbimon = 1920
+let testProjectId = 1
 const arbimonSequelize = await getPopulatedArbimonInMemorySequelize()
 const biodiversitySequelize = getSequelize()
 
@@ -18,30 +18,48 @@ beforeAll(async () => {
 
   // sync all data for a test project
   const project = await ModelRepository.getInstance(biodiversitySequelize)
-  .LocationProject
-  .findOne({
-    where: { idArbimon: testProjectIdArbimon },
-    raw: true
-  })
+    .LocationProject
+    .findOne({
+      where: { idArbimon: testProjectIdArbimon },
+      raw: true
+    })
 
   if (!project) { return }
-  await syncAllForProject(arbimonSequelize, biodiversitySequelize, project)
+  testProjectId = project.id
+  await syncAllBasedOnDetectionsForProject(arbimonSequelize, biodiversitySequelize, project)
 })
 
-test('Extract sites correctly', async () => {
-  const project = await ModelRepository.getInstance(biodiversitySequelize)
-  .LocationProject
-  .findOne({
-    where: { idArbimon: testProjectIdArbimon },
-    raw: true
-  })
+test('Site: Test project has 2 sites - based on validated data', async () => {
+  const numberOfSites = await ModelRepository.getInstance(biodiversitySequelize)
+    .LocationSite
+    .count({ where: { locationProjectId: testProjectId } })
 
-  const sites = await ModelRepository.getInstance(biodiversitySequelize)
-  .LocationSite
-  .findAll({
-    where: { locationProjectId: project?.id },
-    raw: true
-  })
-
-  expect(sites.length).toBe(2)
+  expect(numberOfSites).toBe(2)
 })
+
+test('Species: Test project has 10 species - based on validated data', async () => {
+  const numberOfSpecies = await ModelRepository.getInstance(biodiversitySequelize)
+    .SpeciesInProject
+    .count({ where: { locationProjectId: testProjectId } })
+
+  expect(numberOfSpecies).toBe(10)
+})
+
+test('Detections: detected hemidactylium-scutatum 4 times at NU - Eng between 10am', async () => {
+  const speciesId = await ModelRepository.getInstance(biodiversitySequelize)
+    .TaxonSpecies
+    .findOne({
+      where: { slug: 'hemidactylium-scutatum' },
+      raw: true
+    }).then(v => {
+      return v?.id
+    })
+
+  expect(speciesId).toBeDefined()
+
+  // TODO: continue on this
+})
+
+test('Create new datasource', async () => {})
+
+test('Update existing datasource', async () => {})
