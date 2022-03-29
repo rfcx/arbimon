c6188def85d8c49c473ef7cabe1ebb93c4fa5cb4
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-03-29 20:15:41 +0700

    #693 Add release date & commit ENV variables

--- apps/website/src/_services/vue/vite-env.d.ts
+++ apps/website/src/_services/vue/vite-env.d.ts
@@ -3,9 +3,11 @@ import 'vite/client'
 declare global {
   interface ImportMetaEnv extends Readonly<Record<string, string>> {
     readonly VITE_APP_NAME: string
+    readonly VITE_ARBIMON_BASE_URL: string
     readonly VITE_BIO_API_BASE_URL: string
     readonly VITE_CORE_API_BASE_URL: string
-    readonly VITE_ARBIMON_BASE_URL: string
     readonly VITE_GA_MEASUREMENT_ID: string
+    readonly VITE_RELEASE_COMMIT: string
+    readonly VITE_RELEASE_DATE: string
   }
 }
