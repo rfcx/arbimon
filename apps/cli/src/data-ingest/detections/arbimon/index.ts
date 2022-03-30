b6029407adc0a63ee76c17b46bba1be96b2c6ff5
Somjintana Korbut <nuisomjin@gmail.com>
2022-03-30 16:37:06 +0700

    #683 get duration mins for each projects

--- apps/cli/src/data-ingest/detections/arbimon/index.ts
+++ apps/cli/src/data-ingest/detections/arbimon/index.ts
@@ -26,18 +26,23 @@ export const getArbimonHourlyDetectionsForProjects = async (arbimonSequelize: Se
   // TODO: update duration_in_minutes
   `
   SELECT  s.project_id,
-          r.datetime,
           date(r.datetime) date,
           hour(r.datetime) hour,
           r.site_id,
           rv.species_id,
           count(1) detection_count,
-          12       duration_in_minutes
+          dc.duration_in_minutes
   FROM recordings r
-  JOIN recording_validations rv
+  LEFT JOIN recording_validations rv
     ON r.recording_id = rv.recording_id AND (rv.present = 1 or rv.present_review > 0)
   JOIN sites s ON r.site_id = s.site_id
-  WHERE s.project_id in (${idsString})
+  JOIN (
+    select r.site_id, hour(r.datetime) hour, date(r.datetime) date, sum(r.duration)/60 duration_in_minutes
+    from recordings r 
+    where r.site_id in (select site_id from sites where project_id in (${idsString}))
+    GROUP by hour(r.datetime), r.site_id
+  ) AS dc ON dc.site_id = s.site_id and dc.hour = hour and dc.date = date
+  WHERE s.project_id in (${idsString}) and rv.species_id is not NULL 
   GROUP BY s.project_id, r.site_id, date(r.datetime), hour(r.datetime), rv.species_id
   ;
   `
@@ -49,13 +54,19 @@ export const getArbimonHourlyDetectionsForProjects = async (arbimonSequelize: Se
           strftime('%H',r.datetime) hour,
           r.site_id,
           rv.species_id,
-          count(1) detection_count,
-          12       duration_in_minutes
+          count(1)            detection_count,
+          dc.duration_in_minutes
   FROM recordings r
-  JOIN recording_validations rv
+  LEFT JOIN recording_validations rv
     ON r.recording_id = rv.recording_id AND (rv.present = 1 or rv.present_review > 0)
   JOIN sites s ON r.site_id = s.site_id
-  WHERE s.project_id in (${idsString})
+  JOIN (
+    select r.site_id, hour(r.datetime) hour, date(r.datetime) date, sum(r.duration)/60 duration_in_minutes
+    from recordings r 
+    where r.site_id in (select site_id from sites where project_id in (${idsString}))
+    GROUP by hour(r.datetime), r.site_id
+  ) AS dc ON dc.site_id = s.site_id and dc.hour = hour and dc.date = date
+  WHERE s.project_id in (${idsString}) and rv.species_id is not NULL 
   GROUP BY s.project_id, r.site_id, date(r.datetime), strftime('%H',r.datetime), rv.species_id
   ;
   `
