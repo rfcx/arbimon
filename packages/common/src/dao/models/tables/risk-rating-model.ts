af4d5486ec17ca86b4c083f7ed1ced2533f07152
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-04-04 22:27:19 +0700

    #695 Fix imports/proxies in common

--- packages/common/src/dao/models/tables/risk-rating-model.ts
+++ packages/common/src/dao/models/tables/risk-rating-model.ts
@@ -1,12 +1,12 @@
 import { DataTypes } from 'sequelize'
 
 import { defineWithDefaults } from '../../model-helpers/defaults'
-import { RiskRatingIucn } from '../../types'
+import { RiskRating } from '../../types'
 
 export const MODEL_RISK_RATING = 'RiskRating'
 export const TABLE_RISK_RATING = 'risk_rating'
 
-export const RiskRatingModel = defineWithDefaults<RiskRatingIucn>(
+export const RiskRatingModel = defineWithDefaults<RiskRating>(
   MODEL_RISK_RATING,
   {
     // PK
