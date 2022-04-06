c2f4483c96b1877dc3633d0b8fed2a88ed8bd21d
Somjintana Korbut <nuisomjin@gmail.com>
2022-04-06 10:20:26 +0700

    #695 create seeder for source

---
+++ apps/cli/src/db/seeders/01-master-data/003-source.ts
@@ -0,0 +1,13 @@
+import { QueryInterface } from 'sequelize'
+import { MigrationFn } from 'umzug'
+
+import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
+
+import { rawSources } from '../_data/source'
+
+export const up: MigrationFn<QueryInterface> = async (params): Promise<void> => {
+  const sequelize = params.context.sequelize
+  const models = ModelRepository.getInstance(sequelize)
+
+  await models.Source.bulkCreate(rawSources)
+}
---
+++ apps/cli/src/db/seeders/_data/source.ts
@@ -0,0 +1,5 @@
+import { Source } from '@rfcx-bio/common/dao/types'
+
+export const rawSources: Source[] = [
+  { id: 100, name: 'Arbimon Validated' }
+]
