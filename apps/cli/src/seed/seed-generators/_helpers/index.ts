865fefe4fbec25c8ede9b318426fedcdd49cf61a
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-04-07 22:20:27 +0700

    #695 Rename file to match function

--- apps/cli/src/seed/seed-generators/_helpers/index.ts
+++ apps/cli/src/seed/seed-generators/_helpers/index.ts
@@ -1,7 +1,1 @@
-import { dirname, resolve } from 'path'
-import { fileURLToPath } from 'url'
-
-const currentDir = dirname(fileURLToPath(import.meta.url))
-const pathToSeederData = '../data/generated'
-
-export const getSeederDataDirectory = (): string => resolve(currentDir, pathToSeederData)
+export * from './get-generated-data-directory'
