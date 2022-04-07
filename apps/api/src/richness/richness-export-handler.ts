9623e6c774b89dd067e60fd1abca7ace306b2546
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-04-07 14:55:27 +0700

    #715 Extract richness export data into new endpoint and calling when user click export button

---
+++ apps/api/src/richness/richness-export-handler.ts
@@ -0,0 +1,34 @@
+import { RichnessExportParams, RichnessExportQuery, RichnessExportResponse } from '@rfcx-bio/common/api-bio/richness/richness-export'
+
+import { getIsProjectMember } from '@/_middleware/get-is-project-member'
+import { BioInvalidPathParamError, BioInvalidQueryParamError } from '~/errors'
+import { Handler } from '../_services/api-helpers/types'
+import { assertPathParamsExist } from '../_services/validation'
+import { isValidDate } from '../_services/validation/query-validation'
+import { getRichnessExport } from './richness-export-bll'
+
+export const richnessExportHandler: Handler<RichnessExportResponse, RichnessExportParams, RichnessExportQuery> = async (req) => {
+  // Inputs & validation
+  const { projectId } = req.params
+  assertPathParamsExist({ projectId })
+
+  const projectIdInteger = parseInt(projectId)
+  if (Number.isNaN(projectIdInteger)) throw BioInvalidPathParamError({ projectId })
+
+  const { startDate: startDateUtcInclusive, endDate: endDateUtcInclusive, siteIds, taxons } = req.query
+  if (!isValidDate(startDateUtcInclusive)) throw BioInvalidQueryParamError({ startDate: startDateUtcInclusive })
+  if (!isValidDate(endDateUtcInclusive)) throw BioInvalidQueryParamError({ endDate: endDateUtcInclusive })
+
+  const datasetFilter = {
+    locationProjectId: projectIdInteger,
+    startDateUtcInclusive,
+    endDateUtcInclusive,
+    // TODO ???: Better way to check query type!
+    siteIds: Array.isArray(siteIds) ? siteIds.map(Number) : typeof siteIds === 'string' ? [Number(siteIds)] : [],
+    taxons: Array.isArray(taxons) ? taxons.map(Number) : typeof taxons === 'string' ? [Number(taxons)] : []
+  }
+
+  const isProjectMember = getIsProjectMember(req)
+
+  return await getRichnessExport(datasetFilter, isProjectMember)
+}
