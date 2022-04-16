2659599660f1f4565d6653c869a3ec7725813f05
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-04-16 14:25:24 +0700

    #720 Avoid type assertion

--- apps/cli/src/ingest/_connections/arbimon.ts
+++ apps/cli/src/ingest/_connections/arbimon.ts
@@ -1,4 +1,3 @@
-import { ConnectionOptions } from 'mysql2/promise'
 import { Options, Sequelize } from 'sequelize'
 
 import { requireEnv } from '~/env'
@@ -7,26 +6,23 @@ import { requireEnv } from '~/env'
 const { ARBIMON_DB_USER, ARBIMON_DB_PASSWORD, ARBIMON_DB_HOSTNAME, ARBIMON_DB_DBNAME } =
   requireEnv('ARBIMON_DB_USER', 'ARBIMON_DB_PASSWORD', 'ARBIMON_DB_HOSTNAME', 'ARBIMON_DB_DBNAME')
 
-const ARBIMON_CONFIG: ConnectionOptions = {
-  user: ARBIMON_DB_USER,
-  password: ARBIMON_DB_PASSWORD,
-  host: ARBIMON_DB_HOSTNAME,
-  database: ARBIMON_DB_DBNAME,
-  timezone: '+00:00'
-}
-
 interface ArbimonConnectionOptions {
   user: string
   password: string
   host: string
   database: string
-  timezone: string
 }
 
-const getArbimonSequelizeBase = (co: ArbimonConnectionOptions): Sequelize => {
-  // Setup sequelize (ORM)
+const ARBIMON_CONFIG: ArbimonConnectionOptions = {
+  user: ARBIMON_DB_USER,
+  password: ARBIMON_DB_PASSWORD,
+  host: ARBIMON_DB_HOSTNAME,
+  database: ARBIMON_DB_DBNAME
+}
+
+const getArbimonSequelizeBase = ({ user, password, host, database }: ArbimonConnectionOptions): Sequelize => {
   const sequelizeOptions: Options = {
-    host: co.host,
+    host,
     dialect: 'mysql',
     define: {
       charset: 'utf8',
@@ -36,11 +32,11 @@ const getArbimonSequelizeBase = (co: ArbimonConnectionOptions): Sequelize => {
   }
 
   return new Sequelize(
-    co.database,
-    co.user,
-    co.password,
+    database,
+    user,
+    password,
     sequelizeOptions
   )
 }
 
-export const getArbimonSequelize = (): Sequelize => getArbimonSequelizeBase(ARBIMON_CONFIG as ArbimonConnectionOptions)
+export const getArbimonSequelize = (): Sequelize => getArbimonSequelizeBase(ARBIMON_CONFIG)
