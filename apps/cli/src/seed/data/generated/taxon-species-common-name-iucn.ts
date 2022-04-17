d9742a293e7157458ddeca2d52fff7b59ba96867
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-04-18 03:01:27 +0700

    #695 Generate TaxonSpeciesCommonName & TaxonSpeciesRiskRating seeds (from API)

---
+++ apps/cli/src/seed/data/generated/taxon-species-common-name-iucn.ts
@@ -0,0 +1,140 @@
+import { SeedTaxonSpeciesCommonName } from '../types'
+
+export const taxonSpeciesCommonNameIucn: SeedTaxonSpeciesCommonName[] = [
+  {
+    slug: 'agelaius-xanthomus',
+    commonName: 'Yellow-shouldered Blackbird'
+  },
+  {
+    slug: 'amazona-viridigenalis',
+    commonName: 'Red-crowned Amazon'
+  },
+  {
+    slug: 'amazona-vittata',
+    commonName: 'Puerto Rican Amazon'
+  },
+  {
+    slug: 'antrostomus-noctitherus',
+    commonName: 'Puerto Rican Nightjar'
+  },
+  {
+    slug: 'bubulcus-ibis',
+    commonName: 'Cattle Egret'
+  },
+  {
+    slug: 'capra-aegagrus',
+    commonName: 'Wild Goat'
+  },
+  {
+    slug: 'eleutherodactylus-antillensis',
+    commonName: 'Red-eyed Coqui'
+  },
+  {
+    slug: 'eleutherodactylus-brittoni',
+    commonName: 'Grass Coqui'
+  },
+  {
+    slug: 'eleutherodactylus-cochranae',
+    commonName: 'Whistling Coqui'
+  },
+  {
+    slug: 'eleutherodactylus-cooki',
+    commonName: 'Cave Coqui'
+  },
+  {
+    slug: 'eleutherodactylus-coqui',
+    commonName: 'Common Coqui'
+  },
+  {
+    slug: 'eleutherodactylus-gryllus',
+    commonName: 'Cricket Coqui'
+  },
+  {
+    slug: 'eleutherodactylus-hedricki',
+    commonName: 'Hedrick\'s Coqui'
+  },
+  {
+    slug: 'eleutherodactylus-juanariveroi',
+    commonName: 'Plains Coquí'
+  },
+  {
+    slug: 'eleutherodactylus-locustus',
+    commonName: 'Locust Coqui'
+  },
+  {
+    slug: 'eleutherodactylus-monensis',
+    commonName: 'Mona Coqui'
+  },
+  {
+    slug: 'eleutherodactylus-portoricensis',
+    commonName: 'Mountain Coqui'
+  },
+  {
+    slug: 'eleutherodactylus-richmondi',
+    commonName: 'Richmond\'s Coqui'
+  },
+  {
+    slug: 'eleutherodactylus-unicolor',
+    commonName: 'Dwarf Coqui'
+  },
+  {
+    slug: 'eleutherodactylus-wightmanae',
+    commonName: 'Melodius Coqui'
+  },
+  {
+    slug: 'gallinula-galeata',
+    commonName: 'Common Gallinule'
+  },
+  {
+    slug: 'larus-atricilla',
+    commonName: 'Laughing Gull'
+  },
+  {
+    slug: 'leptodactylus-albilabris-',
+    commonName: 'Ranita de Labio Blanco'
+  },
+  {
+    slug: 'lithobates-catesbeianus',
+    commonName: 'American Bullfrog'
+  },
+  {
+    slug: 'osteopilus-septentrionalis',
+    commonName: 'Cuban Treefrog'
+  },
+  {
+    slug: 'passer-domesticus',
+    commonName: 'House Sparrow'
+  },
+  {
+    slug: 'patagioenas-leucocephala',
+    commonName: 'White-crowned Pigeon'
+  },
+  {
+    slug: 'pluvialis-squatarola',
+    commonName: 'Grey Plover'
+  },
+  {
+    slug: 'rhinella-marina',
+    commonName: 'Cane Toad'
+  },
+  {
+    slug: 'setophaga-angelae',
+    commonName: 'Elfin Woods Warbler'
+  },
+  {
+    slug: 'streptopelia-decaocto',
+    commonName: 'Eurasian Collared-dove'
+  },
+  {
+    slug: 'sula-sula',
+    commonName: 'Red-footed Booby'
+  },
+  {
+    slug: 'thalasseus-maximus',
+    commonName: 'Royal Tern'
+  },
+  {
+    slug: 'vireo-latimeri',
+    commonName: 'Puerto Rican Vireo'
+  }
+]
---
+++ apps/cli/src/seed/data/generated/taxon-species-risk-rating-iucn.ts
@@ -0,0 +1,174 @@
+import { SeedTaxonSpeciesRiskRating } from '../types'
+
+export const taxonSpeciesRiskRatingIucn: SeedTaxonSpeciesRiskRating[] = [
+  {
+    slug: 'agelaius-xanthomus',
+    riskRatingId: 500,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Agelaius%20xanthomus'
+  },
+  {
+    slug: 'amazona-viridigenalis',
+    riskRatingId: 500,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Amazona%20viridigenalis'
+  },
+  {
+    slug: 'amazona-vittata',
+    riskRatingId: 600,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Amazona%20vittata'
+  },
+  {
+    slug: 'antrostomus-noctitherus',
+    riskRatingId: 500,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Antrostomus%20noctitherus'
+  },
+  {
+    slug: 'bubulcus-ibis',
+    riskRatingId: 200,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Bubulcus%20ibis'
+  },
+  {
+    slug: 'capra-aegagrus',
+    riskRatingId: 300,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Capra%20aegagrus'
+  },
+  {
+    slug: 'eleutherodactylus-antillensis',
+    riskRatingId: 200,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Eleutherodactylus%20antillensis'
+  },
+  {
+    slug: 'eleutherodactylus-brittoni',
+    riskRatingId: 200,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Eleutherodactylus%20brittoni'
+  },
+  {
+    slug: 'eleutherodactylus-cochranae',
+    riskRatingId: 200,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Eleutherodactylus%20cochranae'
+  },
+  {
+    slug: 'eleutherodactylus-cooki',
+    riskRatingId: 500,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Eleutherodactylus%20cooki'
+  },
+  {
+    slug: 'eleutherodactylus-coqui',
+    riskRatingId: 200,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Eleutherodactylus%20coqui'
+  },
+  {
+    slug: 'eleutherodactylus-gryllus',
+    riskRatingId: 600,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Eleutherodactylus%20gryllus'
+  },
+  {
+    slug: 'eleutherodactylus-hedricki',
+    riskRatingId: 500,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Eleutherodactylus%20hedricki'
+  },
+  {
+    slug: 'eleutherodactylus-juanariveroi',
+    riskRatingId: 600,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Eleutherodactylus%20juanariveroi'
+  },
+  {
+    slug: 'eleutherodactylus-locustus',
+    riskRatingId: 500,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Eleutherodactylus%20locustus'
+  },
+  {
+    slug: 'eleutherodactylus-monensis',
+    riskRatingId: 400,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Eleutherodactylus%20monensis'
+  },
+  {
+    slug: 'eleutherodactylus-portoricensis',
+    riskRatingId: 500,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Eleutherodactylus%20portoricensis'
+  },
+  {
+    slug: 'eleutherodactylus-richmondi',
+    riskRatingId: 500,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Eleutherodactylus%20richmondi'
+  },
+  {
+    slug: 'eleutherodactylus-unicolor',
+    riskRatingId: 600,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Eleutherodactylus%20unicolor'
+  },
+  {
+    slug: 'eleutherodactylus-wightmanae',
+    riskRatingId: 500,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Eleutherodactylus%20wightmanae'
+  },
+  {
+    slug: 'gallinula-galeata',
+    riskRatingId: 200,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Gallinula%20galeata'
+  },
+  {
+    slug: 'larus-atricilla',
+    riskRatingId: 200,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Larus%20atricilla'
+  },
+  {
+    slug: 'leptodactylus-albilabris-',
+    riskRatingId: 200,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/leptodactylus%20albilabris%20'
+  },
+  {
+    slug: 'lithobates-catesbeianus',
+    riskRatingId: 200,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Lithobates%20catesbeianus'
+  },
+  {
+    slug: 'osteopilus-septentrionalis',
+    riskRatingId: 200,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Osteopilus%20septentrionalis'
+  },
+  {
+    slug: 'passer-domesticus',
+    riskRatingId: 200,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Passer%20domesticus'
+  },
+  {
+    slug: 'patagioenas-leucocephala',
+    riskRatingId: 300,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Patagioenas%20leucocephala'
+  },
+  {
+    slug: 'pluvialis-squatarola',
+    riskRatingId: 200,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Pluvialis%20squatarola'
+  },
+  {
+    slug: 'rhinella-marina',
+    riskRatingId: 200,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Rhinella%20marina'
+  },
+  {
+    slug: 'setophaga-angelae',
+    riskRatingId: 500,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Setophaga%20angelae'
+  },
+  {
+    slug: 'streptopelia-decaocto',
+    riskRatingId: 200,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Streptopelia%20decaocto'
+  },
+  {
+    slug: 'sula-sula',
+    riskRatingId: 200,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Sula%20sula'
+  },
+  {
+    slug: 'thalasseus-maximus',
+    riskRatingId: 200,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Thalasseus%20maximus'
+  },
+  {
+    slug: 'vireo-latimeri',
+    riskRatingId: 200,
+    sourceUrl: 'https://apiv3.iucnredlist.org/api/v3/website/Vireo%20latimeri'
+  }
+]
