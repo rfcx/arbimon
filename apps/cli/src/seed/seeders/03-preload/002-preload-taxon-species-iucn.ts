5b36877dbd897fa59f2352daf9ebc44055789fc7
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-04-17 20:51:51 +0700

    #695 Restore IUCN species seeder (old seed; I will re-dump this)

--- apps/cli/src/seed/seeders/03-preload/002-preload-taxon-species-iucn.ts
+++ apps/cli/src/seed/seeders/03-preload/002-preload-taxon-species-iucn.ts
@@ -1,33 +1,58 @@
 import { QueryInterface } from 'sequelize'
 import { MigrationFn } from 'umzug'
 
-// import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
-// import { TaxonSpeciesRiskRating } from '@rfcx-bio/common/dao/types'
-// import { isDefined } from '@rfcx-bio/utils/predicates'
+import { masterTaxonSpeciesSources, riskRatings } from '@rfcx-bio/common/dao/master-data'
+import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
+import { TaxonSpeciesCommonName, TaxonSpeciesDescription, TaxonSpeciesRiskRating } from '@rfcx-bio/common/dao/types'
+import { isDefined } from '@rfcx-bio/utils/predicates'
 
-// import { rawTaxonSpeciesIucn } from '@/db/seeders/_data/taxon-species-iucn'
+import { rawTaxonSpeciesIucn } from '../../data/manual/taxon-species-iucn'
 
 export const up: MigrationFn<QueryInterface> = async (params): Promise<void> => {
-  // const sequelize = params.context.sequelize
-  // const models = ModelRepository.getInstance(sequelize)
+  const sequelize = params.context.sequelize
+  const models = ModelRepository.getInstance(sequelize)
 
-  // // Lookups
-  // const speciesSlugToId: Record<string, number> = await models.TaxonSpecies
-  //   .findAll({ attributes: ['id', 'slug'] })
-  //   .then(allSpecies => Object.fromEntries(allSpecies.map(s => [s.slug, s.id])))
+  // Lookups
+  const speciesSlugToId: Record<string, number> = await models.TaxonSpecies
+    .findAll({
+      attributes: ['id', 'slug'],
+      raw: true
+    })
+    .then(res => Object.fromEntries(res.map(s => [s.slug, s.id])))
 
-  // // Convert data
-  // const data: TaxonSpeciesRiskRating[] = rawTaxonSpeciesIucn
-  //   .map(({ taxonSpeciesId: _, 'TaxonSpecies.slug': slug, ...rest }) => {
-  //     const taxonSpeciesId = speciesSlugToId[slug]
-  //     if (!taxonSpeciesId) return undefined
+  const validRiskRatings = new Set(riskRatings.map(r => r.id))
 
-  //     return {
-  //       taxonSpeciesId,
-  //       ...rest
-  //     }
-  //   })
-  //   .filter(isDefined)
+  // Convert data
+  const validData = rawTaxonSpeciesIucn
+    .filter(s => s.slug in speciesSlugToId)
+    .map(({ slug, ...rest }) => ({ taxonSpeciesId: speciesSlugToId[slug], ...rest }))
 
-  // await models.TaxonSpeciesIucn.bulkCreate(data)
+  const commonNames: TaxonSpeciesCommonName[] = validData
+    .map(({ taxonSpeciesId, commonName }) => {
+      if (!commonName) return undefined
+      return { taxonSpeciesId, taxonSpeciesSourceId: masterTaxonSpeciesSources.IUCN.id, commonName }
+    })
+    .filter(isDefined)
+
+  const descriptions: TaxonSpeciesDescription[] = validData
+    .map(({ taxonSpeciesId, description, descriptionSourceUrl: sourceUrl }) => {
+      if (!description || !sourceUrl) return undefined
+      return { taxonSpeciesId, taxonSpeciesSourceId: masterTaxonSpeciesSources.IUCN.id, description, sourceUrl }
+    })
+    .filter(isDefined)
+
+  const risks: TaxonSpeciesRiskRating[] = validData
+    .map(({ taxonSpeciesId, riskRatingIucnId: riskRatingId, descriptionSourceUrl: sourceUrl }) => {
+      if (!riskRatingId || !sourceUrl) return undefined
+      if (!validRiskRatings.has(riskRatingId)) {
+        console.warn(`Invalid risk rating: ${riskRatingId}`)
+        return undefined
+      }
+      return { taxonSpeciesId, taxonSpeciesSourceId: masterTaxonSpeciesSources.IUCN.id, riskRatingId, sourceUrl }
+    })
+    .filter(isDefined)
+
+  await models.TaxonSpeciesCommonName.bulkCreate(commonNames)
+  await models.TaxonSpeciesDescription.bulkCreate(descriptions)
+  await models.TaxonSpeciesRiskRating.bulkCreate(risks)
 }
