dc226b614b16767b308daef2dc72a48f87095095
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-04-20 00:23:32 +0700

    #730 Add assert (API validator) that correctly chooses 401 vs 403

--- apps/api/src/_services/validation/index.ts
+++ apps/api/src/_services/validation/index.ts
@@ -1,4 +1,8 @@
-import { BioMissingPathParamError, BioMissingQueryParamError } from '../errors'
+import { FastifyRequest } from 'fastify'
+
+import { getIsProjectMember } from '@/_middleware/get-is-project-member'
+import { isValidToken } from '~/api-helpers/is-valid-token'
+import { BioForbiddenError, BioMissingPathParamError, BioMissingQueryParamError, BioUnauthorizedError } from '../errors'
 
 /**
  * Validates multiple parameters at once, throwing an error if any of them are undefined.
@@ -21,3 +25,14 @@ export const assertQueryParamsExist = (params: Record<string, string | undefined
   Object.entries(params).forEach(
     ([key, value]) => { if (!value) throw BioMissingQueryParamError(key) }
   )
+
+/**
+ * Validated both the presence of a valid token & the access it grants
+ */
+export const assertAuthorizedForProject = (req: FastifyRequest): void => {
+  // TODO: Move API query here instead of in the pre-handler
+  const token = req.headers.authorization
+  if (token === undefined || !isValidToken(token)) throw BioUnauthorizedError()
+
+  if (!(getIsProjectMember(req))) throw BioForbiddenError()
+}
