ec4308ff756287e6e0e9c0db769914359dfb40e8
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-04-19 21:11:21 +0700

    #696 Writing a test case for empty site in dashboard generated

--- apps/api/src/dashboard/dashboard-generated-handler.int.test.ts
+++ apps/api/src/dashboard/dashboard-generated-handler.int.test.ts
@@ -1,7 +1,9 @@
 import fastify, { FastifyInstance } from 'fastify'
 import { describe, expect, test } from 'vitest'
 
+import { ApiMap } from '@rfcx-bio/common/api-bio/_helpers'
 import { dashboardGeneratedUrl } from '@rfcx-bio/common/api-bio/dashboard/dashboard-generated'
+import { Site } from '@rfcx-bio/common/dao/types'
 
 import { GET } from '~/api-helpers/types'
 import { routesDashboard } from './index'
@@ -105,13 +107,34 @@ describe(`GET ${ROUTE}  (dashboard generated)`, () => {
       url: dashboardGeneratedUrl({ projectId: '1' })
     })
 
-    test.todo('species richness by site is correct', async () => {
+    test('species richness by site is correct', async () => {
       // Arrange
-      // const knownSiteId = 123
+      const knownSiteName = 'SA09'
+      const expectedProperties = ['name', 'latitude', 'longitude', 'value']
 
       // Act
-      const result = JSON.parse(response.body)?.richnessBySite
-      expect(result).toBeDefined()
+      const maybeResult = JSON.parse(response.body)?.richnessBySite
+
+      // Assert - property exists & correct type
+      expect(maybeResult).toBeDefined()
+      expect(Array.isArray(maybeResult)).toBe(true)
+
+      const result = maybeResult as ApiMap
+      expect(result.length).toBe(877)
+
+      // Assert - first result is object
+      const maybeKnownSite = result.find(bySite => bySite.name === knownSiteName)
+      expect(maybeKnownSite).toBeTypeOf('object')
+      const knownSite = maybeKnownSite as Pick<Site, 'name' | 'latitude' | 'longitude'> & { value: number }
+
+      // Assert - first result contains (only) expected props
+      expectedProperties.forEach(expectedProperty => expect(knownSite).toHaveProperty(expectedProperty))
+      Object.keys(knownSite).forEach(actualProperty => expect(expectedProperties).toContain(actualProperty))
+
+      // Assert - latitude, longitude, and value are correct
+      expect(knownSite.latitude).toBe(17.962779)
+      expect(knownSite.longitude).toBe(-66.201552)
+      expect(knownSite.value).toBe(14)
     })
 
     test.todo('species richness by site is empty', async () => {
