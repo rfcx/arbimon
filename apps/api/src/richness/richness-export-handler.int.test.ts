ac6225af0b693af8bb349fd4b896995e1ca6d22d
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-04-20 14:49:08 +0700

    - Add to do test case on richness export api

---
+++ apps/api/src/richness/richness-export-handler.int.test.ts
@@ -0,0 +1,11 @@
+const ROUTE = '/projects/:projectId/richness-export'
+
+describe.todo(`GET ${ROUTE} (richness export)`, () => {
+  describe('simple tests', () => {
+    //
+  })
+
+  describe('known data test', () => {
+    //
+  })
+})
