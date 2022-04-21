e1825c88b8171b93b5c711cc36f70b7290c9253a
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-04-21 16:12:09 +0700

    #732 Rewrite project root to be composition api and using last update from projectData

--- apps/website/src/_layout/project-root/project-root.vue
+++ apps/website/src/_layout/project-root/project-root.vue
@@ -5,15 +5,16 @@
     <navbar-component />
     <!-- ================== page content =============== -->
     <div class="max-w-screen-2xl mx-auto px-2 py-4 sm:px-6 lg:px-8">
+      <div v-if="isLoading" />
       <div
-        v-if="store.selectedProject"
+        v-else-if="store.selectedProject"
         class="page_content"
       >
         <router-view />
         <div class="mt-5 py-2 grid sm:grid-cols-2 text-xs text-subtle border-t-1 border-l-0 border-r-0 border-b-0 border-solid opacity-50">
           <last-sync
             class="text-center sm:text-left"
-            :sync-updated="store.projectFilters?.updatedList[0]?.updatedAt ?? null"
+            :sync-updated="lastUpdatedAt"
             :project-slug="store.selectedProject?.slug"
           />
           <app-version class="text-center mt-2 sm:(mt-0 text-right)" />
@@ -23,4 +24,15 @@
     </div>
   </div>
 </template>
-<script src="./project-root.ts" lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue'
+
+import { useStore } from '~/store'
+import InvalidProjectComponent from '../components/invalid-project/invalid-project.vue'
+import NavbarComponent from '../components/navbar/nav-bar.vue'
+
+const store = useStore()
+const { isLoading, isError, data } = store.projectData
+const lastUpdatedAt = computed(() => data?.value?.updatedList[0]?.updatedAt ?? null)
+
+</script>
