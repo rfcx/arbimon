50124875009d80205bd72823a13b9131f2cb8dda
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-05-11 12:37:54 +0700

    #735 Fix/clean useProjectData()

--- apps/website/src/_services/api/project-service/use-project-data.ts
+++ apps/website/src/_services/api/project-service/use-project-data.ts
@@ -1,7 +1,6 @@
 import { computed, ComputedRef } from 'vue'
 
 import { ProjectFiltersResponse } from '@rfcx-bio/common/api-bio/common/project-filters'
-import { mapIfDefined } from '@rfcx-bio/utils/fp'
 import { isDefined } from '@rfcx-bio/utils/predicates'
 
 import { useApiQuery } from '~/api/use-api-query'
@@ -9,13 +8,16 @@ import { Loadable, queryAsLoadable } from '~/loadable'
 import { useStore } from '~/store'
 import { projectService } from './project-service'
 
-const getLoadable = () => {
+const getLoadable = (): ComputedRef<Loadable<ProjectFiltersResponse, unknown>> => {
   const store = useStore()
   const projectId = computed(() => store.selectedProject?.id)
 
   const query = useApiQuery(
     ['fetch-project-filter', projectId],
-    async () => await mapIfDefined(projectId.value, projectService.getProjectFilters)
+    async () => {
+      if (projectId.value === undefined) return undefined
+      return await projectService.getProjectFilters(projectId.value)
+    }
   )
 
   return queryAsLoadable(query, isDefined)
@@ -23,4 +25,4 @@ const getLoadable = () => {
 
 // Export
 let loadable: ComputedRef<Loadable<ProjectFiltersResponse, unknown>> | undefined
-export const useProjectData = (): ComputedRef<Loadable<ProjectFiltersResponse>> => loadable ??= getLoadable()
+export const useProjectData = (): ComputedRef<Loadable<ProjectFiltersResponse>> => { loadable ??= getLoadable(); return loadable }
