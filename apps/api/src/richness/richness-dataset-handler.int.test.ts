f829271d8961c94619b7fc87ab1d050a919719ca
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-05-16 19:07:55 +0700

    #755 Refactor richness tests to use inject helpers; Wrap in top-level describe

--- apps/api/src/richness/richness-dataset-handler.int.test.ts
+++ apps/api/src/richness/richness-dataset-handler.int.test.ts
@@ -1,9 +1,8 @@
-import fastify, { FastifyInstance } from 'fastify'
-import { fastifyRequestContextPlugin } from 'fastify-request-context'
 import { describe, expect, test } from 'vitest'
 
 import { richnessDatasetUrl } from '@rfcx-bio/common/api-bio/richness/richness-dataset'
 
+import { getInjectAsLoggedInProjectMember, getMockedFastify } from '@/_testing/get-inject'
 import { GET } from '~/api-helpers/types'
 import { routesRichness } from './index'
 
@@ -20,155 +19,116 @@ const EXPECTED_PROPS = [
   'richnessPresence'
 ]
 
-const getMockedApp = async (): Promise<FastifyInstance> => {
-  const app = await fastify()
-  await app.register(fastifyRequestContextPlugin)
+describe(`GET ${ROUTE} (richness dataset)`, async () => {
+  const routes = routesRichness
+  const injectAsLoggedInProjectMember = await getInjectAsLoggedInProjectMember(routes)
 
-  routesRichness
-    .map(({ preHandler, ...rest }) => ({ ...rest })) // Remove preHandlers that call external APIs
-    .forEach(route => app.route(route))
-
-  return app
-}
-
-describe('happy path', () => {
-  test(`GET ${ROUTE} exists`, async () => {
-    // Arrange
-    const app = await getMockedApp()
-
-    // Act
-    const routes = app.printRoutes()
-
-    // Assert
-    expect(routes).toContain(ROUTE)
-   })
-
-  test(`GET ${ROUTE} returns successfully`, async () => {
-    // Arrange
-    const app = await getMockedApp()
-
-    // Act
-    const response = await app.inject({
-      method: GET,
-      url: '/projects/1/richness',
-      query: { startDate: '2001-01-01T00:00:00.000Z', endDate: '2021-01-01T00:00:00.000Z', siteIds: '', taxons: '' }
-    })
-
-    // Assert
-    expect(response.statusCode).toBe(200)
-
-    const result = JSON.parse(response.body)
-    expect(result).toBeDefined()
-    expect(result).toBeTypeOf('object')
-  })
-
-  test(`GET ${ROUTE} contains all expected props`, async () => {
-    // Arrange
-    const app = await getMockedApp()
-
-    // Act
-    const response = await app.inject({
-      method: GET,
-      url: richnessDatasetUrl({ projectId: '1' }),
-      query: { startDate: '2001-01-01T00:00:00.000Z', endDate: '2021-01-01T00:00:00.000Z', siteIds: '', taxons: '' }
-    })
-
-    // Assert
-    const result = JSON.parse(response.body)
-    EXPECTED_PROPS.forEach(expectedProp => expect(result).toHaveProperty(expectedProp))
-  })
-
-  test(`GET ${ROUTE} does not contain any additional props`, async () => {
-    // Arrange
-    const app = await getMockedApp()
-
-    // Act
-    const response = await app.inject({
-      method: GET,
-      url: richnessDatasetUrl({ projectId: '1' }),
-      query: { startDate: '2001-01-01T00:00:00.000Z', endDate: '2021-01-01T00:00:00.000Z' }
-    })
-
-    // Assert
-    const result = JSON.parse(response.body)
-    Object.keys(result).forEach(actualProp => expect(EXPECTED_PROPS).toContain(actualProp))
-  })
-})
-
-describe('validate known data', () => {
-  test(`GET ${ROUTE} does not have any data on given date`, async () => {
-    // Arrange
-    const app = await getMockedApp()
-
-    // Act
-    const response = await app.inject({
-      method: GET,
-      url: richnessDatasetUrl({ projectId: '1' }),
-      query: { startDate: '2001-01-01T00:00:00.000Z', endDate: '2002-01-01T00:00:00.000Z' }
-    })
-
-    // Assert
-    const result = JSON.parse(response.body)
-    Object.keys(result).forEach(actualProp => expect(EXPECTED_PROPS).toContain(actualProp))
-
-    expect(result.richnessBySite).toEqual([])
-    expect(result.richnessByTaxon).toEqual({})
-    expect(result.richnessByTimeDayOfWeek).toEqual({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 })
-    expect(result.richnessByTimeUnix).toEqual({})
-    expect(result.richnessPresence).toEqual([])
-  })
-})
-
-describe('client errors', () => {
-  test(`GET ${ROUTE} rejects missing query`, async () => {
+  describe('happy path', () => {
+    test('exists', async () => {
       // Arrange
-      const app = await getMockedApp()
+      const app = await getMockedFastify({ routes })
 
       // Act
-      const response = await app.inject({
-      method: GET,
-      url: '/projects/1/richness'
+      const routeList = app.printRoutes()
+
+      // Assert
+      expect(routeList).toContain(ROUTE)
     })
 
-    // Assert
-    expect(response.statusCode).toBe(400)
+    test('returns successfully', async () => {
+      // Act
+      const response = await injectAsLoggedInProjectMember({
+        method: GET,
+        url: '/projects/1/richness',
+        query: { startDate: '2001-01-01T00:00:00.000Z', endDate: '2021-01-01T00:00:00.000Z', siteIds: '', taxons: '' }
+      })
+
+      // Assert
+      expect(response.statusCode).toBe(200)
+
+      const result = JSON.parse(response.body)
+      expect(result).toBeDefined()
+      expect(result).toBeTypeOf('object')
+    })
+
+    test('contains all expected props & no more', async () => {
+      // Act
+      const response = await injectAsLoggedInProjectMember({
+        method: GET,
+        url: richnessDatasetUrl({ projectId: '1' }),
+        query: { startDate: '2001-01-01T00:00:00.000Z', endDate: '2021-01-01T00:00:00.000Z', siteIds: '', taxons: '' }
+      })
+
+      // Assert
+      const result = JSON.parse(response.body)
+      EXPECTED_PROPS.forEach(expectedProp => expect(result).toHaveProperty(expectedProp))
+      expect(Object.keys(result).length).toBe(EXPECTED_PROPS.length)
+    })
   })
 
-  test(`GET ${ROUTE} rejects invalid project id`, async () => {
-      // Arrange
-      const app = await getMockedApp()
-
+  describe('validate known data', () => {
+    test('does not have any data on given date', async () => {
       // Act
-      const response = await app.inject({
-      method: GET,
-      url: '/projects/x/richness'
+      const response = await injectAsLoggedInProjectMember({
+        method: GET,
+        url: richnessDatasetUrl({ projectId: '1' }),
+        query: { startDate: '2001-01-01T00:00:00.000Z', endDate: '2002-01-01T00:00:00.000Z' }
+      })
+
+      // Assert
+      const result = JSON.parse(response.body)
+      Object.keys(result).forEach(actualProp => expect(EXPECTED_PROPS).toContain(actualProp))
+
+      expect(result.richnessBySite).toEqual([])
+      expect(result.richnessByTaxon).toEqual({})
+      expect(result.richnessByTimeDayOfWeek).toEqual({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 })
+      expect(result.richnessByTimeUnix).toEqual({})
+      expect(result.richnessPresence).toEqual([])
     })
-
-    // Assert
-    expect(response.statusCode).toBe(400)
-
-    const result = JSON.parse(response.body)
-    const errorMessage = result.message
-    expect(errorMessage).toContain('Invalid path params: projectId')
   })
 
-  test(`GET ${ROUTE} rejects invalid date`, async () => {
-      // Arrange
-      const app = await getMockedApp()
+  describe('client errors', () => {
+    test('rejects missing query', async () => {
+        // Act
+        const response = await injectAsLoggedInProjectMember({
+        method: GET,
+        url: '/projects/1/richness'
+      })
 
-      // Act
-      const response = await app.inject({
-      method: GET,
-      url: '/projects/1/richness',
-      query: { startDate: 'abc', endDate: '2021-01-01T00:00:00.000Z' }
+      // Assert
+      expect(response.statusCode).toBe(400)
     })
 
-    // Assert
-    expect(response.statusCode).toBe(400)
+    test('rejects invalid project id', async () => {
+        // Act
+        const response = await injectAsLoggedInProjectMember({
+        method: GET,
+        url: '/projects/x/richness'
+      })
 
-    const result = JSON.parse(response.body)
-    const errorMessage = result.message
-    expect(errorMessage).toContain('Invalid query params')
-    expect(errorMessage).toContain('startDate with value')
+      // Assert
+      expect(response.statusCode).toBe(400)
+
+      const result = JSON.parse(response.body)
+      const errorMessage = result.message
+      expect(errorMessage).toContain('Invalid path params: projectId')
+    })
+
+    test('rejects invalid date', async () => {
+        // Act
+        const response = await injectAsLoggedInProjectMember({
+        method: GET,
+        url: '/projects/1/richness',
+        query: { startDate: 'abc', endDate: '2021-01-01T00:00:00.000Z' }
+      })
+
+      // Assert
+      expect(response.statusCode).toBe(400)
+
+      const result = JSON.parse(response.body)
+      const errorMessage = result.message
+      expect(errorMessage).toContain('Invalid query params')
+      expect(errorMessage).toContain('startDate with value')
+    })
   })
 })
