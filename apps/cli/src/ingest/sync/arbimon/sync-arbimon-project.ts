7fe54d6d3aa66e58921c28f8df2bcdb5b0133fc2
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-05-20 15:22:04 +0700

    #720 Fix call to getArbimonProjects

--- apps/cli/src/ingest/sync/arbimon/sync-arbimon-project.ts
+++ apps/cli/src/ingest/sync/arbimon/sync-arbimon-project.ts
@@ -3,8 +3,8 @@ import { Sequelize } from 'sequelize'
 import { getArbimonProjects, tranformArbimonToBioProjects } from '@/ingest/inputs/arbimon-projects'
 import { writeProjectsToBio } from '@/ingest/outputs/projects'
 
-export const syncArbimonProjects = async (arbimonSequelize: Sequelize, biodiversitySequelize: Sequelize, syncUntil: Date): Promise<void> => {
-  const arbimonProjects = await getArbimonProjects(arbimonSequelize, syncUntil, 2)
+export const syncArbimonProjects = async (arbimonSequelize: Sequelize, biodiversitySequelize: Sequelize, syncUntil: Date, lastSyncdId: number, batchLimit: number): Promise<void> => {
+  const arbimonProjects = await getArbimonProjects(arbimonSequelize, syncUntil, lastSyncdId, batchLimit)
   const projects = tranformArbimonToBioProjects(arbimonProjects)
   await writeProjectsToBio(biodiversitySequelize, projects)
 }
