a92d7e9813d329745d4ef14d77537f32b16e5cf5
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-05-21 08:01:51 +0700

    #720 Update syncArbimonProjects to use parser

--- apps/cli/src/ingest/sync/sync-arbimon-project.ts
+++ apps/cli/src/ingest/sync/sync-arbimon-project.ts
@@ -1,10 +1,16 @@
+import { partition } from 'lodash-es'
 import { Sequelize } from 'sequelize'
 
-import { getArbimonProjects, tranformArbimonToBioProjects } from '@/ingest/inputs/get-arbimon-projects'
+import { getArbimonProjects } from '@/ingest/inputs/get-arbimon-projects'
 import { writeProjectsToBio } from '@/ingest/outputs/projects'
+import { parseProjectArbimonToBio } from '../parsers/parse-project-arbimon-to-bio'
 
 export const syncArbimonProjects = async (arbimonSequelize: Sequelize, biodiversitySequelize: Sequelize, syncUntil: Date, lastSyncdId: number, batchLimit: number): Promise<void> => {
   const arbimonProjects = await getArbimonProjects(arbimonSequelize, syncUntil, lastSyncdId, batchLimit)
-  const projects = tranformArbimonToBioProjects(arbimonProjects)
+
+  const [projects, errors] = partition(arbimonProjects.map(parseProjectArbimonToBio), p => p.success)
+
   await writeProjectsToBio(biodiversitySequelize, projects)
+  // await writeErrorsToBio(projectsInvalid)
+  console.log(errors)
 }
