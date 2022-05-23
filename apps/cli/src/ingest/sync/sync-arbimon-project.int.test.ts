334d0d9a26e09f75acd4ee7810546f244d581e40
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-05-23 12:50:17 +0700

    #720 Wrap syncArbimonProjectsBatch in a loop; Scaffold other funcs & tests

--- apps/cli/src/ingest/sync/sync-arbimon-project.int.test.ts
+++ apps/cli/src/ingest/sync/sync-arbimon-project.int.test.ts
@@ -1,12 +1,52 @@
 import { describe, test } from 'vitest'
 
-describe('ingest > sync > syncArbimonProjects', () => {
-  test.todo('todo', async () => {
-    // Arrange
+describe('ingest > sync', () => {
+  describe('syncArbimonProjectsBatch', () => {
+    test.todo('can sync projects', async () => {
+      // Arrange
 
-    // Act
+      // Act
 
-    // Assert
+      // Assert
+      // TODO: Assert valid projects are in Bio projects table
+    })
 
+    test.todo('can sync projects when some invalid', async () => {
+      // Arrange
+
+      // Act
+
+      // Assert
+      // TODO: Assert valid projects are in Bio projects table
+      // TODO: Assert invalid projects are in Bio sync_error table
+    })
+
+    test.todo('sync is idempotent', async () => {
+      // Arrange
+
+      // Act
+      // TODO: Run the same batch twice
+
+      // Assert
+      // TODO: Assert result is the same as if we ran sync once
+    })
+
+    test.todo('updates sync status with last ID and updatedAt', async () => { })
+    test.todo('returns sync status (to support immediately looping)', async () => { })
+  })
+
+  describe('syncArbimonProjects', () => {
+    test.todo('can sync all projects', async () => { })
+    test.todo('can sync all projects when some invalid', async () => { })
+  })
+
+  describe('syncArbimonProjectsByIds', () => {
+    test.todo('can sync valid projects', async () => { })
+    test.todo('can sync invalid projects', async () => { })
+    test.todo('removes sync errors on successful sync', async () => { })
+  })
+
+  describe('syncArbimonProjectsThatFailed', () => {
+    test.todo('can sync valid projects', async () => { })
   })
 })
--- apps/cli/src/ingest/sync/sync-arbimon-project.ts
+++ apps/cli/src/ingest/sync/sync-arbimon-project.ts
@@ -1,16 +1,60 @@
 import { partition } from 'lodash-es'
 import { Sequelize } from 'sequelize'
 
+import { masterSources, masterSyncDataTypes } from '@rfcx-bio/common/dao/master-data'
+import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
+import { SyncStatus } from '@rfcx-bio/common/dao/types'
+
+import { getSequelize } from '@/db/connections'
 import { getArbimonProjects } from '@/ingest/inputs/get-arbimon-projects'
 import { writeProjectsToBio } from '@/ingest/outputs/projects'
 import { parseProjectArbimonToBio } from '../parsers/parse-project-arbimon-to-bio'
+import { getDefaultSyncStatus, SyncConfig } from './sync-config'
 
-export const syncArbimonProjects = async (arbimonSequelize: Sequelize, biodiversitySequelize: Sequelize, syncUntil: Date, lastSyncdId: number, batchLimit: number): Promise<void> => {
-  const arbimonProjects = await getArbimonProjects(arbimonSequelize, syncUntil, lastSyncdId, batchLimit)
+const SYNC_CONFIG: SyncConfig = {
+  sourceId: masterSources.ArbimonValidated.id,
+  syncDataTypeId: masterSyncDataTypes.Project.id,
+  syncBatchLimit: 1000
+}
 
-  const [projects, errors] = partition(arbimonProjects.map(parseProjectArbimonToBio), p => p.success)
+export const syncArbimonProjectsBatch = async (arbimonSequelize: Sequelize, biodiversitySequelize: Sequelize, syncStatus: SyncStatus): Promise<SyncStatus> => {
+  const arbimonProjects = await getArbimonProjects(arbimonSequelize, syncStatus)
+  if (arbimonProjects.length === 0) return syncStatus
 
+  const [projects, validationErrors] = partition(arbimonProjects.map(parseProjectArbimonToBio), p => p.success)
+
+  // TODO: Use a transaction
   await writeProjectsToBio(biodiversitySequelize, projects)
-  // await writeErrorsToBio(projectsInvalid)
-  console.log(errors)
+  // const insertErrors = await writeProjectsToBio(biodiversitySequelize, projects)
+  console.log(validationErrors)
+  // await writeErrorsToBio(validationErrors, insertErrors)
+
+  const updatedSyncStatus: SyncStatus = { ...syncStatus, syncUntilDate: arbimonProjects[arbimonProjects.length - 1].updatedAt, syncUntilId: arbimonProjects[arbimonProjects.length - 1].id }
+  // await writeSyncResult(updatedSyncStatus)
+
+  return updatedSyncStatus
+}
+
+export const syncArbimonProjects = async (arbimonSequelize: Sequelize, biodiversitySequelize: Sequelize): Promise<void> => {
+  let syncStatus = await ModelRepository.getInstance(getSequelize())
+    .SyncStatus
+    .findOne({
+      where: { sourceId: SYNC_CONFIG.sourceId, syncDataTypeId: SYNC_CONFIG.syncDataTypeId },
+      raw: true
+    }) ?? getDefaultSyncStatus(SYNC_CONFIG)
+
+  while (true) {
+    const updatedSyncStatus = await syncArbimonProjectsBatch(arbimonSequelize, biodiversitySequelize, syncStatus)
+    if (syncStatus.syncUntilDate === updatedSyncStatus.syncUntilDate && syncStatus.syncUntilId === updatedSyncStatus.syncUntilId) return
+    syncStatus = updatedSyncStatus
+  }
+}
+
+export const syncArbimonProjectsByIds = async (arbimonSequelize: Sequelize, biodiversitySequelize: Sequelize, arbimonProjectIds: number[]): Promise<void> => {
+  // TODO: Implement
+}
+
+export const syncArbimonProjectsThatFailed = async (arbimonSequelize: Sequelize, biodiversitySequelize: Sequelize): Promise<void> => {
+  // TODO: Lookup failures in sync_error table (in batches)
+  // TODO: Call syncArbimonProjectsByIds
 }
