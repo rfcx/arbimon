54c7be922ec3c80194e3e03e718b4e64312488cf
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-05-23 12:48:15 +0700

    #720 Expose default syncUntilDate & syncUntilId via helper function

---
+++ apps/cli/src/ingest/sync/sync-config.ts
@@ -0,0 +1,11 @@
+import { SyncStatus } from '@rfcx-bio/common/dao/types'
+import { dayjs } from '@rfcx-bio/utils/dayjs-initialized'
+
+export type SyncConfig = Pick<SyncStatus, 'sourceId' | 'syncDataTypeId' | 'syncBatchLimit'>
+
+export const getDefaultSyncStatus = (syncConfig: SyncConfig): SyncStatus =>
+  ({
+    ...syncConfig,
+    syncUntilDate: dayjs('1980-01-01T00:00:00.000Z').toDate(),
+    syncUntilId: 0
+  })
