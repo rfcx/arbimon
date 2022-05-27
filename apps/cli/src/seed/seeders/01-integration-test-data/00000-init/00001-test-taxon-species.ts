2da5f727b4f8b6f6b8b454b32e9ce7cffa1ddaad
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-05-27 15:46:45 +0700

    #755 Extract test species (to support looking up their classes)

--- apps/cli/src/seed/seeders/01-integration-test-data/00000-init/00001-test-taxon-species.ts
+++ apps/cli/src/seed/seeders/01-integration-test-data/00000-init/00001-test-taxon-species.ts
@@ -1,78 +1,12 @@
 import { QueryInterface } from 'sequelize'
 import { MigrationFn } from 'umzug'
 
-import { masterTaxonClasses } from '@rfcx-bio/common/dao/master-data'
 import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
-import { TaxonSpecies } from '@rfcx-bio/common/dao/types'
+
+import { testSpecies } from '@/seed/data/integration/test-taxon-species'
 
 export const up: MigrationFn<QueryInterface> = async ({ context: { sequelize } }): Promise<void> => {
   const models = ModelRepository.getInstance(sequelize)
 
   await models.TaxonSpecies.bulkCreate(testSpecies)
 }
-
-const testSpecies: TaxonSpecies[] = [
-  {
-    id: 1,
-    idArbimon: 1,
-    slug: 'cat',
-    taxonClassId: masterTaxonClasses.Mammals.id, // 600
-    scientificName: 'Felis catus'
-  },
-  {
-    id: 2,
-    idArbimon: 2,
-    slug: 'cobra',
-    taxonClassId: masterTaxonClasses.Amphibians.id, // 100
-    scientificName: 'Naja'
-  },
-  {
-    id: 3,
-    idArbimon: 3,
-    slug: 'eagle',
-    taxonClassId: masterTaxonClasses.Birds.id, // 300
-    scientificName: 'Accipitridae'
-  },
-  {
-    id: 4,
-    idArbimon: 4,
-    slug: 'sparrow',
-    taxonClassId: masterTaxonClasses.Birds.id, // 300
-    scientificName: 'Passeridae'
-  },
-  {
-    id: 5,
-    idArbimon: 5,
-    slug: 'dog',
-    taxonClassId: masterTaxonClasses.Mammals.id, // 600
-    scientificName: 'Canis lupus familiaris'
-  },
-  {
-    id: 6,
-    idArbimon: 6,
-    slug: 'watermonitor',
-    taxonClassId: masterTaxonClasses.Amphibians.id, // 100
-    scientificName: 'Varanus salvator'
-  },
-  {
-    id: 7,
-    idArbimon: 7,
-    slug: 'cricket',
-    taxonClassId: masterTaxonClasses.Insects.id, // 500
-    scientificName: 'Grylloidea'
-  },
-  {
-    id: 8,
-    idArbimon: 8,
-    slug: 'rooster',
-    taxonClassId: masterTaxonClasses.Birds.id, // 300
-    scientificName: 'Gallus gallus'
-  },
-  {
-    id: 9,
-    idArbimon: 9,
-    slug: 'poison-dart-frog',
-    taxonClassId: masterTaxonClasses.Amphibians.id, // 100
-    scientificName: 'Dendrobatidae'
-  }
-]
