ab17ea434876eeab80f6b04435e524c7d01420a0
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-06-07 01:07:46 +0700

    #771 Add token to requests using interceptor

---
+++ packages/utils/src/api/api-client.ts
@@ -0,0 +1,27 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
+
+export const getApiClient = (baseURL: string, getToken?: () => Promise<string>): AxiosInstance => {
+  const apiClient = axios.create({
+    baseURL,
+    timeout: 30 * 1000 // 30 secs
+  })
+
+  if (getToken !== undefined) {
+    apiClient.interceptors.request.use(async config => {
+      const token = await getToken()
+      config.headers = { ...config.headers, Authorization: `Bearer ${token}` }
+      return config
+    })
+
+    // TODO: Try again ONCE if the request fails with 401
+    // apiClient.interceptors.response.use()
+  }
+
+  return apiClient
+}
+
+export const apiGetOrUndefined = async <T> (apiClient: AxiosInstance, url: string, config?: AxiosRequestConfig): Promise<T | undefined> => {
+  return await apiClient.get<T>(url, config)
+    .then(res => res.data)
+    .catch(() => undefined)
+}
