d40d63869f6a3f3f7dd9f036c3a88e87990558ea
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-06-07 01:18:28 +0700

    #771 Inject API client (including baseURL & token)

--- apps/website/src/main.ts
+++ apps/website/src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import VueGtag from 'vue-gtag'
 import { VueQueryPlugin } from 'vue-query'
 
+import { getApiClient } from '@rfcx-bio/utils/api'
+
 import appComponent from '@/_layout'
 import { ANALYTICS_CONFIGS } from '~/analytics'
 import { getIdToken, useAuth0Client } from '~/auth-client'
@@ -10,6 +12,7 @@ import { FEATURE_TOGGLES } from '~/feature-toggles'
 import router, { ROUTE_NAMES } from '~/router'
 import { pinia, useStoreOutsideSetup } from '~/store'
 import { componentsFromGlob } from '~/vue/register-components'
+import { apiClientBioKey, authKey, gtagKey, routeNamesKey, storeKey, togglesKey } from './globals'
 
 import 'mapbox-gl/dist/mapbox-gl.css'
 import 'virtual:windi.css'
@@ -25,6 +28,8 @@ async function init (): Promise<void> {
   const store = useStoreOutsideSetup()
   await store.updateUser(user)
 
+  // Setup API token
+  const apiClientBio = getApiClient(import.meta.env.VITE_BIO_API_BASE_URL, user ? async () => await getIdToken(authClient) : undefined)
 
   // Setup app
   const app = createApp(appComponent)
@@ -33,12 +38,17 @@ async function init (): Promise<void> {
     .use(VueGtag, ANALYTICS_CONFIGS, router)
     .use(VueQueryPlugin)
     .use(componentsFromGlob, import.meta.globEager('/src/_components/**/*.vue'))
-    .provide('auth', authClient)
-    .provide('store', useStore())
-    .provide('TOGGLES', FEATURE_TOGGLES)
-    .provide('ROUTE_NAMES', ROUTE_NAMES)
 
-  app.provide('gtag', app.config.globalProperties.$gtag)
+  // Inject globals
+  app
+    .provide(authKey, authClient)
+    .provide(apiClientBioKey, apiClientBio)
+    .provide(storeKey, store) // TODO: Replace with useStore()
+    .provide(gtagKey, app.config.globalProperties.$gtag)
+    .provide(togglesKey, FEATURE_TOGGLES)
+    .provide(routeNamesKey, ROUTE_NAMES)
+
+  // Go go go!
   app.mount('#app')
 
   // Handle redirects
