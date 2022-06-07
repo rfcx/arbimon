089198b11c537967116fbe10f1cdba2e6390ee99
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-06-07 22:36:29 +0700

    #771 Delete unused species APIs

--- apps/api/src/species/index.ts
+++ apps/api/src/species/index.ts
@@ -2,32 +2,18 @@ import { resolve } from 'path'
 
 import { projectSpeciesAllRoute } from '@rfcx-bio/common/api-bio/species/project-species-all'
 import { projectSpeciesOneRoute } from '@rfcx-bio/common/api-bio/species/project-species-one'
-import { speciesAllRoute } from '@rfcx-bio/common/api-bio/species/species-all'
-import { speciesOneRoute } from '@rfcx-bio/common/api-bio/species/species-one'
 import { speciesPredictionOccupancyRoute } from '@rfcx-bio/common/api-bio/species/species-prediction-occupancy'
 
 import { setIsProjectMember } from '@/_middleware/get-is-project-member'
 import { GET, RouteRegistration } from '../_services/api-helpers/types'
 import { projectSpeciesAllHandler } from './controller-project-species-all'
 import { projectSpeciesOneHandler } from './controller-project-species-one'
-import { speciesAllHandler } from './controller-species-all'
-import { speciesOneHandler } from './controller-species-one'
 import { speciesPredictionOccupancyHandler } from './controller-species-prediction-occupancy'
 
 export const mockPredictionsFolderName = 'predicted-occupancy/puerto-rico'
 export const mockPredictionsFolderPath = resolve('./public', mockPredictionsFolderName)
 
 export const routesSpecies: RouteRegistration[] = [
-  {
-    method: GET,
-    url: speciesOneRoute,
-    handler: speciesOneHandler
-  },
-  {
-    method: GET,
-    url: speciesAllRoute,
-    handler: speciesAllHandler
-  },
   {
     method: GET,
     url: projectSpeciesOneRoute,
