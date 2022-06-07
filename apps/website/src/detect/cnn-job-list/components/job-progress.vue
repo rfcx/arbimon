c76b7023b8382701b789b7e17439d106615433ec
Somjintana Korbut <nuisomjin@gmail.com>
2022-06-07 10:02:50 +0700

    #771 add cnn job list page layout and components

---
+++ apps/website/src/detect/cnn-job-list/components/job-progress.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mb-1 text-xs text-subtle">
+    {{ props.jobProgress.status }}
+  </div>
+  <div class="w-14 bg-faded rounded-full h-2.5">
+    <div
+      class="bg-brand-primary h-2.5 rounded-full"
+      :style="{ 'width': `${props.jobProgress.value}%` }"
+    >
+      <!---->
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import { JobProgress } from '~/job'
+
+const props = defineProps<{
+  jobProgress: JobProgress
+}>()
+
+</script>
