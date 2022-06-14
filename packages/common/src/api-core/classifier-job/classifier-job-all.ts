0ae6f61f0ceeee11432cb30510a07060fdd630d2
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2022-06-14 14:40:16 +0300

    The service to get the list of classifier jobs is added

---
+++ packages/common/src/api-core/classifier-job/classifier-job-all.ts
@@ -0,0 +1,32 @@
+import { AxiosInstance } from 'axios'
+
+// Request types
+export interface ClassifierJobAllParams {
+  limit?: number
+  offset?: number
+}
+
+// Response types
+export type ClassifierJobAllResponse = ClassifierJob[]
+
+export interface ClassifierJob {
+  id: number,
+  classifier_id: number,
+  project_id: string,
+  query_streams: string,
+  query_start: string,
+  query_end: string,
+  query_hours: string,
+  minutes_total: number,
+  minutes_completed: number,
+  status: number,
+  created_by_id: number,
+  created_at: string,
+  updated_at: string,
+  started_at: string,
+  completed_at: string
+}
+
+// Service
+export const apiCoreGetClassifierJobAll = async (apiClient: AxiosInstance, params: ClassifierJobAllParams = {}): Promise<ClassifierJobAllResponse | undefined> =>
+  await apiClient.get('/classifier-jobs', { params })
