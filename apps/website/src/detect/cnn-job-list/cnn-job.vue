f36dec7cf7088baa401b1ccf88e118b10cb6983a
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-06-16 09:30:17 +0700

    #772 Map API type to UI type; Show API state (for debugging)

--- apps/website/src/detect/cnn-job-list/cnn-job.vue
+++ apps/website/src/detect/cnn-job-list/cnn-job.vue
@@ -7,7 +7,21 @@
       </button>
     </router-link>
   </div>
-  <table class="w-full text-sm text-left mt-5">
+  <!-- DEBUG START -->
+  {{ isLoadingClassifierJobs }}
+  {{ isErrorClassifierJobs }}
+  {{ classifierJobs }}
+  <!-- DEBUG END -->
+  <p v-if="isLoadingClassifierJobs">
+    Loading...
+  </p>
+  <p v-else-if="isErrorClassifierJobs">
+    Error :(
+  </p>
+  <table
+    v-else
+    class="w-full text-sm text-left mt-5"
+  >
     <thead class="text-xs text-subtle uppercase">
       <tr class="border-b border-box-grey">
         <th
@@ -55,7 +69,7 @@
 
 <script setup lang="ts">
 import { AxiosInstance } from 'axios'
-import { inject, watch } from 'vue'
+import { computed, inject } from 'vue'
 
 // import { ClassifierJob } from '@rfcx-bio/common/api-core/classifier-job/classifier-job-all'
 import { apiClientCoreKey } from '@/globals'
@@ -65,7 +79,38 @@ import { useClassifierJobs } from '../_composables/use-classifier-jobs'
 import JobItemRow from './components/job-item-row.vue'
 
 const apiClientCore = inject(apiClientCoreKey) as AxiosInstance
-const { data } = useClassifierJobs(apiClientCore)
+const { isLoading: isLoadingClassifierJobs, isError: isErrorClassifierJobs, data: classifierJobs } = useClassifierJobs(apiClientCore)
+
+// TODO: Extract
+const getStatus = (s: number): string => {
+  switch (s) {
+    case 0: return 'Queued'
+    case 20: return 'Processing'
+    case 30: return 'Done'
+    case 40: return 'Error'
+    case 50: return 'Cancelled'
+    default: return 'Unknown'
+  }
+}
+
+const getProgress = (minComplete: number, minTotal: number): number =>
+  0
+
+const jobs = computed(() => classifierJobs.value?.items?.map(cj => ({
+  id: cj.id,
+  modelName: cj.classifier.name,
+  input: {
+    sites: cj.queryStreams,
+    dateRange: `${cj.queryStart}-${cj.queryEnd}`,
+    timeOfDay: cj.queryHours
+  },
+  progress: {
+    status: getStatus(cj.status),
+    value: getProgress(cj.minutesCompleted, cj.minutesTotal)
+  },
+  numberOfRecordings: 0,
+  createdAt: new Date()
+})) ?? [])
 
 // const jobFormatted = (job: ClassifierJob): Job => {
 //   return {
@@ -85,64 +130,51 @@ const { data } = useClassifierJobs(apiClientCore)
 //   }
 // }
 
-watch(data, () => { console.info(data) })
-
-// const getStatus = (s: number): string => {
-//   switch (s) {
-//     case 0: return 'Queued'
-//     case 20: return 'Processing'
-//     case 30: return 'Done'
-//     case 40: return 'Error'
-//     case 50: return 'Cancelled'
-//     default: return '-'
+// const jobs = [
+//   {
+//     id: 1,
+//     modelName: 'PR Regional CNN',
+//     input: {
+//       sites: 'SR*',
+//       dateRange: '2022-05-11',
+//       timeOfDay: 'Diurnal'
+//     },
+//     progress: {
+//       status: 'Processing',
+//       value: 20
+//     },
+//     numberOfRecordings: 4000,
+//     createdAt: new Date()
+//   },
+//   {
+//     id: 2,
+//     modelName: 'PR Regional CNN',
+//     input: {
+//       sites: 'SR*',
+//       dateRange: '2022-05-12',
+//       timeOfDay: 'Diurnal'
+//     },
+//     progress: {
+//       status: 'Queued',
+//       value: 0
+//     },
+//     numberOfRecordings: 2000,
+//     createdAt: new Date()
+//   },
+//   {
+//     id: 3,
+//     modelName: 'PR Regional CNN',
+//     input: {
+//       sites: 'SR*',
+//       dateRange: '2022-05-11 - 2022-05-12',
+//       timeOfDay: 'Nocturnal'
+//     },
+//     progress: {
+//       status: 'Queued',
+//       value: 0
+//     },
+//     numberOfRecordings: 4000,
+//     createdAt: new Date()
 //   }
-// }
-
-const jobs = [
-  {
-    id: 1,
-    modelName: 'PR Regional CNN',
-    input: {
-      sites: 'SR*',
-      dateRange: '2022-05-11',
-      timeOfDay: 'Diurnal'
-    },
-    progress: {
-      status: 'Processing',
-      value: 20
-    },
-    numberOfRecordings: 4000,
-    createdAt: new Date()
-  },
-  {
-    id: 2,
-    modelName: 'PR Regional CNN',
-    input: {
-      sites: 'SR*',
-      dateRange: '2022-05-12',
-      timeOfDay: 'Diurnal'
-    },
-    progress: {
-      status: 'Queued',
-      value: 0
-    },
-    numberOfRecordings: 2000,
-    createdAt: new Date()
-  },
-  {
-    id: 3,
-    modelName: 'PR Regional CNN',
-    input: {
-      sites: 'SR*',
-      dateRange: '2022-05-11 - 2022-05-12',
-      timeOfDay: 'Nocturnal'
-    },
-    progress: {
-      status: 'Queued',
-      value: 0
-    },
-    numberOfRecordings: 4000,
-    createdAt: new Date()
-  }
-]
+// ]
 </script>
