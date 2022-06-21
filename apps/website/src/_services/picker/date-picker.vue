f8350af3d9a2a19bdf2aec32e070177f8944f290
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-06-21 11:23:17 +0700

    #792 Scaffold for testing emits

--- apps/website/src/_services/picker/date-picker.vue
+++ apps/website/src/_services/picker/date-picker.vue
@@ -6,4 +6,14 @@
 </template>
 
 <script setup lang="ts">
+import { Dayjs } from 'dayjs'
+import { onMounted } from 'vue'
+
+import { dayjs } from '@rfcx-bio/utils/dayjs-initialized'
+
+// Interface
+const emit = defineEmits<{(e: 'dateRangeSelected', value: [Dayjs, Dayjs]): void}>()
+
+// Mock emit
+onMounted(() => emit('dateRangeSelected', [dayjs(), dayjs()]))
 </script>
