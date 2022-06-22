a0ce29de39e1801a55171d06e32f2c4b6c3a2da6
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-06-22 10:44:52 +0700

    #793 Extract time-of-day-picker to shared components

--- apps/website/src/_services/picker/time-of-day-picker.vue
+++ apps/website/src/_services/picker/time-of-day-picker.vue
@@ -4,16 +4,15 @@
       v-for="time in timeRanges"
       :key="'time-' + time.value"
       class="btn text-sm"
-      @click="onTimeClick(time.value)"
       round
+      @click="onTimeClick(time.value)"
     >
-     {{ time.label }}
+      {{ time.label }}
     </el-button>
   </el-row>
 </template>
 
 <script setup lang="ts">
-
 const timeRanges = [
   { value: 'all', label: 'All', time: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24] },
   { value: 'diurnal', label: 'Diurnal', time: [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18] },
@@ -26,5 +25,4 @@ const onTimeClick = (time: string) => {
   const emitRange = timeRanges.find(item => item.value === time)
   if (emitRange) emit('emitSelectTime', emitRange?.time)
 }
-
 </script>
