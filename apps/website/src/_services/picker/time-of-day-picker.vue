557b58eb8956118d80ac458fbe834f1fae093227
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-06-22 11:57:06 +0700

    #793 Simplify time-of-day emit

--- apps/website/src/_services/picker/time-of-day-picker.vue
+++ apps/website/src/_services/picker/time-of-day-picker.vue
@@ -1,28 +1,25 @@
 <template>
   <el-row class="mb-4 flex flex-row items-center">
     <el-button
-      v-for="time in timeRanges"
-      :key="'time-' + time.value"
+      v-for="{ key, label, value } in timeRanges"
+      :key="'time-' + key"
       class="btn text-sm"
       round
-      @click="onTimeClick(time.value)"
+      @click="onTimeClick(value)"
     >
-      {{ time.label }}
+      {{ label }}
     </el-button>
   </el-row>
 </template>
 
 <script setup lang="ts">
 const timeRanges = [
-  { value: 'all', label: 'All', time: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23] },
-  { value: 'diurnal', label: 'Diurnal', time: [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17] },
-  { value: 'nocturnal', label: 'Nocturnal', time: [18, 19, 20, 21, 22, 23, 0, 1, 2, 3, 4, 5] }
+  { key: 'all', label: 'All', value: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23] },
+  { key: 'diurnal', label: 'Diurnal', value: [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17] },
+  { key: 'nocturnal', label: 'Nocturnal', value: [18, 19, 20, 21, 22, 23, 0, 1, 2, 3, 4, 5] }
 ]
 
 const emit = defineEmits<{(e: 'emitSelectTime', value: number[]): void}>()
 
-const onTimeClick = (time: string) => {
-  const emitRange = timeRanges.find(item => item.value === time)
-  if (emitRange) emit('emitSelectTime', emitRange?.time)
-}
+const onTimeClick = (value: number[]) => { emit('emitSelectTime', value) }
 </script>
