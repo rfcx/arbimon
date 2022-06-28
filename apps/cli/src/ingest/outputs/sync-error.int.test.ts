4a463b59d9f08e6ce61b5401de726f73ac30f81c
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2022-06-28 16:19:21 +0300

    Added few test cases for error and status logs

--- apps/cli/src/ingest/outputs/sync-error.int.test.ts
+++ apps/cli/src/ingest/outputs/sync-error.int.test.ts
@@ -1,10 +1,40 @@
-import { describe, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
+
+import { masterSources, masterSyncDataTypes } from '@rfcx-bio/common/dao/master-data'
+import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
+
+import { getSequelize } from '@/db/connections'
+import { writeSyncError } from './sync-error'
+
+const biodiversitySequelize = await getSequelize()
 
 describe('ingest > outputs > sync error', () => {
-  test.todo('can log a new error', () => {
-    // log new error when the sync fails
+  const ERROR_LOG = {
+    syncSourceId: masterSources.ArbimonValidated.id,
+    syncDataTypeId: masterSyncDataTypes.Project.id,
+    externalId: '807cuoi3cvw1',
+    error: 'Batch insert failed'
+  }
+
+  const where = { syncSourceId: ERROR_LOG.syncSourceId, syncDataTypeId: ERROR_LOG.syncDataTypeId, externalId: ERROR_LOG.externalId }
+
+  test('can log a new error when the sync fails', async () => {
+    // Act
+    await writeSyncError(ERROR_LOG, biodiversitySequelize)
+
+    // Assert
+    const result = await ModelRepository.getInstance(biodiversitySequelize).SyncError.findOne({ where })
+    expect(result).toBeDefined()
+    expect(result?.error).toBe(ERROR_LOG.error)
   })
-  test.todo('can update existing error in the database', () => {
-    // if you found the same error that is already in the database, the updated_at date should be updated
+  test('can update existing error in the database', async () => {
+    // Act
+    await writeSyncError(ERROR_LOG, biodiversitySequelize)
+
+    // Assert
+    const result = await ModelRepository.getInstance(biodiversitySequelize).SyncError.findAll({ where })
+
+    expect(result.length).toEqual(1)
+    expect(result[0].externalId).toBe(ERROR_LOG.externalId)
   })
 })
--- apps/cli/src/ingest/outputs/sync-status.int.test.ts
+++ apps/cli/src/ingest/outputs/sync-status.int.test.ts
@@ -1,5 +1,54 @@
-import { describe, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
+
+import { masterSources, masterSyncDataTypes } from '@rfcx-bio/common/dao/master-data'
+import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
+import { dayjs } from '@rfcx-bio/utils/dayjs-initialized'
+
+import { getSequelize } from '@/db/connections'
+import { writeSyncResult } from './sync-status'
+
+const biodiversitySequelize = await getSequelize()
 
 describe('ingest > outputs > sync status', () => {
-  test.todo('')
+  test('can add a new status', async () => {
+    // Arrange
+    const STATUS_LOG = {
+      syncSourceId: masterSources.ArbimonValidated.id,
+      syncDataTypeId: masterSyncDataTypes.Project.id,
+      syncUntilDate: dayjs('1980-01-01T00:00:00.000Z').toDate(),
+      syncUntilId: 0,
+      syncBatchLimit: 1
+    }
+
+    const where = { syncSourceId: STATUS_LOG.syncSourceId, syncDataTypeId: STATUS_LOG.syncDataTypeId }
+
+    // Act
+    await writeSyncResult(STATUS_LOG, biodiversitySequelize)
+
+    // Assert
+    const result = await ModelRepository.getInstance(biodiversitySequelize).SyncStatus.findOne({ where })
+    expect(result).toBeDefined()
+    expect(result?.syncUntilDate).toEqual(STATUS_LOG.syncUntilDate)
+  })
+  test('can update existing status in the database', async () => {
+     // Arrange
+     const STATUS_LOG = {
+      syncSourceId: masterSources.ArbimonValidated.id,
+      syncDataTypeId: masterSyncDataTypes.Project.id,
+      syncUntilDate: dayjs.utc('2021-03-19T11:00:00.000Z').toDate(),
+      syncUntilId: 1921,
+      syncBatchLimit: 2
+    }
+
+    const where = { syncSourceId: STATUS_LOG.syncSourceId, syncDataTypeId: STATUS_LOG.syncDataTypeId, syncUntilId: STATUS_LOG.syncUntilId }
+
+    // Act
+    await writeSyncResult(STATUS_LOG, biodiversitySequelize)
+
+    // Assert
+    const result = await ModelRepository.getInstance(biodiversitySequelize).SyncStatus.findAll({ where })
+
+    expect(result.length).toEqual(1)
+    expect(result[0].syncUntilId).toBe(STATUS_LOG.syncUntilId)
+  })
 })
