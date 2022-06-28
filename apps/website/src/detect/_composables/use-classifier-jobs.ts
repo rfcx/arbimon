e99e283e473d83e7dcf9a21b5c04c005839627c9
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-06-28 18:11:39 +0700

    #801 Remove emit and implement query client for re call classifier jobs

--- apps/website/src/detect/_composables/use-classifier-jobs.ts
+++ apps/website/src/detect/_composables/use-classifier-jobs.ts
@@ -3,9 +3,11 @@ import { useQuery, UseQueryReturnType } from 'vue-query'
 
 import { apiCoreGetClassifierJobAll, ClassifierJobAll, ClassifierJobAllParams } from '@rfcx-bio/common/api-core/classifier-job/classifier-job-all'
 
+export const FETCH_CLASSIFIER_JOBS_KEY = 'fetch-classifier-jobs'
+
 export const useClassifierJobs = (apiClient: AxiosInstance, params?: ClassifierJobAllParams): UseQueryReturnType<ClassifierJobAll | undefined, unknown > => {
   return useQuery(
-    ['fetch-classifier-jobs', params],
+    [FETCH_CLASSIFIER_JOBS_KEY, params],
     async () => await apiCoreGetClassifierJobAll(apiClient, params)
   )
 }
