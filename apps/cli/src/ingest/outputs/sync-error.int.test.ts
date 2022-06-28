a2afb984c046cbce20076c1e0977abc414d9bb2e
Somjintana Korbut <nuisomjin@gmail.com>
2022-06-28 16:59:01 +0700

    #802 add mock tests for sync log and status

---
+++ apps/cli/src/ingest/outputs/sync-error.int.test.ts
@@ -0,0 +1,10 @@
+import { describe, test } from 'vitest'
+
+describe('ingest > outputs > sync error', () => {
+  test.todo('can log a new error', () => {
+    // log new error when the sync fails
+  })
+  test.todo('can update existing error in the database', () => {
+    // if you found the same error that is already in the database, the updated_at date should be updated
+  })
+})
---
+++ apps/cli/src/ingest/outputs/sync-status.int.test.ts
@@ -0,0 +1,5 @@
+import { describe, test } from 'vitest'
+
+describe('ingest > outputs > sync status', () => {
+  test.todo('')
+})
