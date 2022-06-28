b8702fb72b44f7b9eb56607e8042d3bab1c1ca7a
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-06-28 10:48:43 +0700

    #805 Fix references to UPDATE_ON_DUPLICATE...

--- apps/cli/src/ingest/outputs/species.ts
+++ apps/cli/src/ingest/outputs/species.ts
@@ -1,12 +1,12 @@
 import { Sequelize, Transaction } from 'sequelize'
 
 import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
-import { UPDATE_ON_DUPLICATE_SPECIES } from '@rfcx-bio/common/dao/models/taxon-species-model'
+import { UPDATE_ON_DUPLICATE_TAXON_SPECIES } from '@rfcx-bio/common/dao/models/taxon-species-model'
 import { TaxonSpecies } from '@rfcx-bio/common/dao/types'
 
 export const writeSpeciesToBio = async (species: Array<Omit<TaxonSpecies, 'id'>>, sequelize: Sequelize, transaction: Transaction | null = null): Promise<void> => {
   const options = {
-    updateOnDuplicate: UPDATE_ON_DUPLICATE_SPECIES,
+    updateOnDuplicate: UPDATE_ON_DUPLICATE_TAXON_SPECIES,
     ...(transaction) && { transaction }
   }
 
