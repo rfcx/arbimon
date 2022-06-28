c07f014a7d74a3c5a60a73471b3b69d0a255a91b
Charles Allen <31621095+charles-allen@users.noreply.github.com>
2022-06-28 10:45:06 +0700

    #804 Fix typo

--- apps/cli/src/data-ingest/projects/db/index.ts
+++ apps/cli/src/data-ingest/projects/db/index.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize'
 
 import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
-import { UPDATED_ON_DUPLICATE_LOCATION_PROJECT } from '@rfcx-bio/common/dao/models/location-project-model'
+import { UPDATE_ON_DUPLICATE_LOCATION_PROJECT } from '@rfcx-bio/common/dao/models/location-project-model'
 import { Project } from '@rfcx-bio/common/dao/types'
 
 export const writeProjectsToPostgres = async (sequelize: Sequelize, projects: Array<Omit<Project, 'id'>>): Promise<void> => {
@@ -9,7 +9,7 @@ export const writeProjectsToPostgres = async (sequelize: Sequelize, projects: Ar
 
   // update items
   const updatedRows = await model.bulkCreate(projects, {
-    updateOnDuplicate: UPDATED_ON_DUPLICATE_LOCATION_PROJECT
+    updateOnDuplicate: UPDATE_ON_DUPLICATE_LOCATION_PROJECT
   })
 
   console.info(`- writeProjectsToPostgres: bulk upsert ${updatedRows.length} projects`)
