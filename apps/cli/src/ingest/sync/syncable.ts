4cfb7f38d4529ed1cd9c8aecc61a5ad1ce813445
Somjintana Korbut <nuisomjin@gmail.com>
2022-06-29 19:20:48 +0700

    #802 Add project coordinates back into project sync

--- apps/cli/src/ingest/sync/syncable.ts
+++ apps/cli/src/ingest/sync/syncable.ts
@@ -5,14 +5,15 @@ export interface Syncable {
 
 export const isSyncable = (candidate: unknown): candidate is Syncable => {
   const candidateAny = candidate as any
+  const updatedAtDate = Date.parse(candidateAny.updatedAt)
 
   // Reject invalid candidates
   if (
     candidateAny.idArbimon === undefined ||
     typeof candidateAny.idArbimon !== 'number' ||
     candidateAny.updatedAt === undefined ||
-    !(candidateAny.updatedAt instanceof Date)
-  ) throw new Error('Project does not contain needed sync status data')
+    isNaN(updatedAtDate)
+  ) throw new Error('Input does not contain needed sync status data')
 
   return true
 }
