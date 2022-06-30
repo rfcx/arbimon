93572cfd871285dd860b86efc4e40afe309a4ed1
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2022-06-30 21:42:10 +0300

    #802 Added regex to slug schema

--- apps/cli/src/ingest/parsers/parse-project-arbimon-to-bio.ts
+++ apps/cli/src/ingest/parsers/parse-project-arbimon-to-bio.ts
@@ -3,7 +3,7 @@ import { SafeParseReturnType, z } from 'zod'
 const ProjectArbimonSchema = z.object({
   idArbimon: z.number(),
   idCore: z.string(),
-  slug: z.string(),
+  slug: z.string().regex(/^[a-z0-9]+(?:-[a-z0-9]+)*$/),
   name: z.string(),
   latitudeNorth: z.number(),
   latitudeSouth: z.number(),
