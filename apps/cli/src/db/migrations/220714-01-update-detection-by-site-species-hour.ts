36ec1e957f010152e6b4d0c0d6ec8034d7501818
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2022-07-14 12:21:53 +0300

    #829 Added a new migration to change duration_minutes to detection_minutes

--- apps/cli/src/db/migrations/220714-01-update-detection-by-site-species-hour.ts
+++ apps/cli/src/db/migrations/220714-01-update-detection-by-site-species-hour.ts
@@ -10,10 +10,10 @@
 
  export const up: MigrationFn<QueryInterface> = async (params): Promise<unknown> => {
   return await Promise.all([
-    await params.context.renameColumn(TABLE_NAME, 'duration_minutes', 'detection_minutes'),
     await params.context.changeColumn(TABLE_NAME, 'duration_minutes', {
       type: DataTypes.ARRAY,
       defaultValue: null
-    })
+    }),
+    await params.context.renameColumn(TABLE_NAME, 'duration_minutes', 'detection_minutes')
   ])
  }
