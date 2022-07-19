b9413065db859c82c2a67d2f3b33a782203025bf
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-07-20 03:39:20 +0700

    #832 Add api to calculate totalDurationInMinutes, connect the new api to UI, and add some test cases

---
+++ apps/api/src/detect/detect-recording-bll.ts
@@ -0,0 +1,86 @@
+import { range } from 'lodash-es'
+import { Op } from 'sequelize'
+
+import { DetectRecordingQueryParams, DetectRecordingResponse } from '@rfcx-bio/common/api-bio/detect/detect-recording'
+import { AllModels, ModelRepository } from '@rfcx-bio/common/dao/model-repository'
+
+import { dayjs } from '~/dayjs-initialized'
+import { getSequelize } from '~/db'
+import { getDetectRecordingTotalDurationMinutes, getSitesByNameQuery } from './detect-recording-dao'
+
+export interface DetectRecordingQuery {
+  locationProjectId: number
+  dateStartLocal: string
+  dateEndLocal: string
+  siteIds?: number[]
+  hours?: number[]
+}
+
+const DATE_FORMAT = 'YYYY-MM-DD'
+
+export const getDetectRecording = async (locationProjectId: number, detectRecordingQueryParams: DetectRecordingQueryParams): Promise<DetectRecordingResponse> => {
+  const sequelize = getSequelize()
+  const models = ModelRepository.getInstance(sequelize)
+
+  const { dateStartLocal, dateEndLocal, querySites, queryHours } = detectRecordingQueryParams
+
+  const query: DetectRecordingQuery = {
+    locationProjectId,
+    dateStartLocal: dayjs(dateStartLocal).startOf('day').format(DATE_FORMAT),
+    dateEndLocal: dayjs(dateEndLocal).endOf('day').format(DATE_FORMAT)
+  }
+
+  if (querySites) {
+    query.siteIds = await getSiteIdsFromQuerySiteNames(models, locationProjectId, querySites)
+  }
+
+  if (queryHours) {
+    query.hours = getHoursFromQueryHours(queryHours)
+  }
+
+  const [totalDurationInMinutes] = await Promise.all([
+    getDetectRecordingTotalDurationMinutes(sequelize, query)
+  ])
+
+  return {
+    ...totalDurationInMinutes
+  }
+}
+
+export const getHoursFromQueryHours = (queryHour: string): number[] => {
+  const hourGroupString = queryHour.replaceAll(' ', '').split(',')
+  const hours: number[] = []
+  for (const hourString of hourGroupString) {
+    if (!Number.isNaN(hourString)) {
+      hours.push(Number(hourString))
+    } else {
+      const [number1, number2] = hourString.split('-').map(Number)
+      if (number1 < number2) {
+        hours.push(...range(number1, number2 + 1))
+      } else {
+        hours.push(...range(number1, 24), ...range(0, number2 + 1))
+      }
+    }
+  }
+
+  return hours
+}
+
+export const getSiteIdsFromQuerySiteNames = async (models: AllModels, locationProjectId: number, querySites: string): Promise<number[]> => {
+  const querySiteNames = querySites.split(',').map(n => n.replace(/^\*/, '%').replace(/\*$/, '%'))
+  const queryWithArray = querySiteNames.filter(n => !n.startsWith('%') && !n.endsWith('%'))
+  const queryWithLike = querySiteNames.filter(n => n.startsWith('%') || n.endsWith('%'))
+  const result: number[] = []
+
+  if (queryWithArray.length > 0) {
+    const queryWithArrayResult = await getSitesByNameQuery(models, locationProjectId, queryWithArray)
+    result.push(...queryWithArrayResult)
+  }
+
+  if (queryWithLike.length > 0) {
+    const queryWithLikeResults = await Promise.all(queryWithLike.map(async q => await getSitesByNameQuery(models, locationProjectId, { [Op.like]: q })))
+    result.push(...queryWithLikeResults.flat())
+  }
+
+  return result
+}
---
+++ apps/api/src/detect/detect-recording-handler.ts
@@ -0,0 +1,22 @@
+import { DetectRecordingParams, DetectRecordingQueryParams, DetectRecordingResponse } from '@rfcx-bio/common/api-bio/detect/detect-recording'
+import { isValidQueryHours } from '@rfcx-bio/utils/query-hour'
+
+import { Handler } from '~/api-helpers/types'
+import { BioInvalidPathParamError, BioInvalidQueryParamError } from '~/errors'
+import { assertPathParamsExist } from '~/validation'
+import { getDetectRecording } from './detect-recording-bll'
+
+export const detectRecordingHandler: Handler<DetectRecordingResponse, DetectRecordingParams, DetectRecordingQueryParams> = async (req) => {
+  // Inputs & validation
+  const { projectId } = req.params
+  assertPathParamsExist({ projectId })
+
+  const projectIdInteger = parseInt(projectId)
+  if (Number.isNaN(projectIdInteger)) throw BioInvalidPathParamError({ projectId })
+
+  const { queryHours } = req.query
+  if (queryHours && !isValidQueryHours(queryHours)) throw BioInvalidQueryParamError({ queryHours })
+
+  // Query & Response
+  return await getDetectRecording(projectIdInteger, req.query)
+}
