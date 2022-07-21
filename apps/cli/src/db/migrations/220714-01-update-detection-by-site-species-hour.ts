c56192436324afff1b3a3803d96193ba90f34b12
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2022-07-22 02:00:19 +0300

    Migration file formatting

--- apps/cli/src/db/migrations/220714-01-update-detection-by-site-species-hour.ts
+++ apps/cli/src/db/migrations/220714-01-update-detection-by-site-species-hour.ts
@@ -1,20 +1,19 @@
-/**
- * WARNING: MIGRATIONS ARE IMMUTABLE
- * Do not depend on imported code which may change
- */
+import { DataTypes, QueryInterface } from 'sequelize'
+import { MigrationFn } from 'umzug'
 
- import { DataTypes, QueryInterface } from 'sequelize'
- import { MigrationFn } from 'umzug'
+const TABLE_NAME = 'detection_by_site_species_hour'
 
- const TABLE_NAME = 'detection_by_site_species_hour'
+export const up: MigrationFn<QueryInterface> = async (params): Promise<unknown> => {
+  return await params.context.addColumn(
+    TABLE_NAME,
+    'detection_minutes',
+    {
+      type: DataTypes.STRING(255),
+      defaultValue: null
+    }
+  )
+}
 
- export const up: MigrationFn<QueryInterface> = async (params): Promise<unknown> => {
-  return await params.context.addColumn(TABLE_NAME, 'detection_minutes', {
-    type: DataTypes.STRING(255),
-    defaultValue: null
-  })
- }
-
- export const down: MigrationFn<QueryInterface> = async (params) => {
+export const down: MigrationFn<QueryInterface> = async (params) => {
   return await params.context.removeColumn(TABLE_NAME, 'detection_minutes')
 }
