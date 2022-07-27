10f7adf5a92e482f382e975f742350834c0b7e8e
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-07-27 23:33:49 +0700

    #830 Update richness presence query to include local protected species and add not to the species presence table to avoid user misunderstanding
    (cherry picked from commit 3d6af0455094577ee38071df11abca27585f4765)

--- apps/api/src/richness/richness-dataset-dao.ts
+++ apps/api/src/richness/richness-dataset-dao.ts
@@ -111,7 +111,7 @@ export const getRichnessByTimeUnix = async (sequelize: Sequelize, filter: Filter
 export const getRichnessPresence = async (sequelize: Sequelize, filter: FilterDatasetForSql, isProjectMember: boolean): Promise<RichnessPresence[]> => {
   const filterBase = datasetFilterWhereRaw(filter)
 
-  const conditions = !isProjectMember ? `${filterBase.conditions} AND NOT sip.risk_rating_global_id = ANY ($protectedRiskRating)` : filterBase.conditions
+  const conditions = !isProjectMember ? `${filterBase.conditions} AND NOT sip.risk_rating_global_id = ANY ($protectedRiskRating) AND NOT sip.risk_rating_local_id = ANY ($protectedRiskRating)` : filterBase.conditions
   const bind = !isProjectMember ? { ...filterBase.bind, protectedRiskRating: RISK_RATING_PROTECTED_IDS } : filterBase.bind
 
   const sql = `
--- apps/website/src/_components/section-title.vue
+++ apps/website/src/_components/section-title.vue
@@ -3,6 +3,12 @@
     <slot name="title">
       <h2 class="text-subtle text-xl">
         {{ title }}
+        <span
+          v-if="note"
+          class="text-danger text-xs ml-1"
+        >
+          {{ note }}
+        </span>
       </h2>
     </slot>
     <slot name="controls" />
@@ -15,5 +21,6 @@ import { Prop } from 'vue-property-decorator'
 
 export default class SectionTitle extends Vue {
   @Prop({ default: '' }) title!: string
+  @Prop({ default: '' }) note!: string
 }
 </script>
