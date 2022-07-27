6bcfc551a34bfff1627119f3a3cfeae85f3e9d91
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-07-27 15:07:23 +0700

    #830 Fix writeSyncLogByProject() to omit createdAt and updatedAt

--- apps/cli/src/ingest/outputs/sync-log-by-project.ts
+++ apps/cli/src/ingest/outputs/sync-log-by-project.ts
@@ -3,7 +3,7 @@ import { Sequelize, Transaction } from 'sequelize'
 import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
 import { SyncLogByProject } from '@rfcx-bio/common/dao/types'
 
-export const writeSyncLogByProject = async (log: Omit<SyncLogByProject, 'id'>, sequelize: Sequelize, transaction: Transaction | null = null): Promise<void> => {
+export const writeSyncLogByProject = async (log: Omit<SyncLogByProject, 'id' | 'createdAt' | 'updatedAt'>, sequelize: Sequelize, transaction: Transaction | null = null): Promise<void> => {
   const model = ModelRepository.getInstance(sequelize).SyncLogByProject
   await model.upsert(log, {
     ...transaction && { transaction }
