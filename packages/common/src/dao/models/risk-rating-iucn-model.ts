f6428eebccf382cd67ccb820d4ed4073e73d3ed2
Antony Harfield <antonyharfield@hotmail.com>
2022-08-11 19:52:01 +0100

    Fix master data seeder - with some help from Chaz

--- packages/common/src/dao/models/risk-rating-iucn-model.ts
+++ packages/common/src/dao/models/risk-rating-iucn-model.ts
@@ -5,6 +5,7 @@ import { RiskRatingIucn } from '../types/risk-rating-iucn'
 
 export const MODEL_RISK_RATING_IUCN = 'RiskRatingIucn'
 export const TABLE_RISK_RATING_IUCN = 'risk_rating_iucn'
+export const UPDATE_ON_DUPLICATE_RISK_RATING_IUCN: Array<(keyof RiskRatingIucn)> = ['code', 'isThreatened']
 
 export const RiskRatingIucnModel = defineWithDefaults<RiskRatingIucn>(
   MODEL_RISK_RATING_IUCN,
@@ -12,8 +13,7 @@ export const RiskRatingIucnModel = defineWithDefaults<RiskRatingIucn>(
     // PK
     id: { // 1
       type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
+      primaryKey: true
     },
 
     // SKs
