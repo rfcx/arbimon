2dbcccaf6a67a3f027d2e1c892eb32e27b07433c
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-08-29 17:49:20 +0700

    #856 Add job detection item template

---
+++ apps/website/src/_components/audio-controller.vue
@@ -0,0 +1,35 @@
+<template>
+  <action-controller
+    v-if="loading"
+    action-name="Loading"
+    class="hover:cursor-not-allowed"
+  >
+    <icon-fas-spinner
+      class="animate-spin"
+      :class="'text-' + size"
+    />
+  </action-controller>
+  <action-controller
+    v-else-if="!playing"
+    action-name="Play"
+  >
+    <icon-fa-play :class="'text-' + size" />
+  </action-controller>
+  <action-controller
+    v-else
+    action-name="Pause"
+  >
+    <icon-fa-pause :class="'text-' + size" />
+  </action-controller>
+</template>
+<script lang="ts">
+import { Vue } from 'vue-class-component'
+import { Prop } from 'vue-property-decorator'
+
+export default class AudioController extends Vue {
+  @Prop({ default: false }) playing!: boolean
+  @Prop({ default: false }) loading!: boolean
+  @Prop({ default: 'sm' }) size!: 'xs' | 'sm' | 'md' | 'lg'
+}
+
+</script>
--- apps/website/src/detect/cnn-job-detail/cnn-job-detail.vue
+++ apps/website/src/detect/cnn-job-detail/cnn-job-detail.vue
@@ -2,9 +2,11 @@
   <job-detail-header />
   <job-detail-information class="mt-4" />
   <job-detail-result class="mt-4" />
+  <job-detections class="mt-4" />
 </template>
 <script setup lang="ts">
 import JobDetailHeader from './components/job-detail-header.vue'
+import JobDetections from './components/job-detections.vue'
 import JobDetailInformation from './components/job-information.vue'
 import JobDetailResult from './components/job-result.vue'
 
