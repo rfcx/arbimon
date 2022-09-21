30d1586d6021f11fd7af979c62f7107356ab4bff
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-09-21 13:44:22 +0700

    #898 Add missing species photo in puerto rico project

---
+++ apps/cli/src/db/seeders/_data/missing-taxon-species-photo.ts
@@ -0,0 +1,124 @@
+import { TaxonSpeciesPhoto } from '@rfcx-bio/common/dao/types'
+
+export const rawMissingTaxonSpeciesPhoto: Array<TaxonSpeciesPhoto & { slug: string }> = [
+  {
+    taxonSpeciesId: 5,
+    source: 'AMPHIBIANWEB',
+    photoUrl: 'https://calphotos.berkeley.edu/imgs/512x768/1111_1111/1111/2879.jpeg',
+    photoCaption: 'Whistle Coqui',
+    photoAuthor: 'Luis J. Villanueva-Rivera',
+    photoLicense: 'CC BY-NC-SA 3.0',
+    photoLicenseUrl: 'https://calphotos.berkeley.edu/cgi/img_query?enlarge=1111+1111+1111+2879',
+    slug: 'eleutherodactylus-cochranae'
+  },
+  {
+    taxonSpeciesId: 152,
+    source: 'INATURALIST',
+    photoUrl: 'https://inaturalist-open-data.s3.amazonaws.com/photos/191409815/medium.jpeg',
+    photoCaption: 'Puerto Rican Vireo',
+    photoAuthor: 'Max Ramey',
+    photoLicense: 'CC BY-NC',
+    photoLicenseUrl: 'https://inaturalist.ca/photos/191409815',
+    slug: 'vireo-latimeri'
+  },
+  {
+    taxonSpeciesId: 155,
+    source: 'AMPHIBIANWEB',
+    photoUrl: 'https://calphotos.berkeley.edu/imgs/512x768/1111_1111/1111/2882.jpeg',
+    photoCaption: 'Coquí Martillito',
+    photoAuthor: 'Luis J. Villanueva-Rivera',
+    photoLicense: 'CC BY-NC-SA 3.0',
+    photoLicenseUrl: 'https://calphotos.berkeley.edu/cgi/img_query?enlarge=1111+1111+1111+2882',
+    slug: 'eleutherodactylus-locustus'
+  },
+  {
+    taxonSpeciesId: 156,
+    source: 'AMPHIBIANWEB',
+    photoUrl: 'https://calphotos.berkeley.edu/imgs/512x768/1111_1111/1111/2883.jpeg',
+    photoCaption: 'Coquí de La Montaña',
+    photoAuthor: 'Luis J. Villanueva-Rivera',
+    photoLicense: 'CC BY-NC-SA 3.0',
+    photoLicenseUrl: 'https://calphotos.berkeley.edu/cgi/img_query?enlarge=1111+1111+1111+2883',
+    slug: 'eleutherodactylus-portoricensis'
+  },
+  {
+    taxonSpeciesId: 157,
+    source: 'AMPHIBIANWEB',
+    photoUrl: 'https://calphotos.berkeley.edu/imgs/512x768/1111_1111/1111/2884.jpeg',
+    photoCaption: 'Coquí Caoba',
+    photoAuthor: 'Luis J. Villanueva-Rivera',
+    photoLicense: 'CC BY-NC-SA 3.0',
+    photoLicenseUrl: 'https://calphotos.berkeley.edu/cgi/img_query?enlarge=1111+1111+1111+2884',
+    slug: 'eleutherodactylus-richmondi'
+  },
+  {
+    taxonSpeciesId: 158,
+    source: 'AMPHIBIANWEB',
+    photoUrl: 'https://calphotos.berkeley.edu/imgs/512x768/1111_1111/1111/2885.jpeg',
+    photoCaption: 'Coquí Melodioso',
+    photoAuthor: 'Luis J. Villanueva-Rivera',
+    photoLicense: 'CC BY-NC-SA 3.0',
+    photoLicenseUrl: 'https://calphotos.berkeley.edu/cgi/img_query?enlarge=1111+1111+1111+2885',
+    slug: 'eleutherodactylus-wightmanae'
+  },
+  {
+    taxonSpeciesId: 524,
+    source: 'AMPHIBIANWEB',
+    photoUrl: 'https://calphotos.berkeley.edu/imgs/512x768/0000_0000/0502/0200.jpeg',
+    photoCaption: 'Cane Toad',
+    photoAuthor: 'William Flaxington',
+    photoLicense: 'CC BY-NC 3.0',
+    photoLicenseUrl: 'https://calphotos.berkeley.edu/cgi/img_query?enlarge=0000+0000+0502+0200',
+    slug: 'rhinella-marina'
+  },
+  {
+    taxonSpeciesId: 710,
+    source: 'AMPHIBIANWEB',
+    photoUrl: 'https://calphotos.berkeley.edu/imgs/512x768/1111_1111/1111/2881.jpeg',
+    photoCaption: 'Coquí Grillo',
+    photoAuthor: 'Luis J. Villanueva-Rivera',
+    photoLicense: 'CC BY-NC 3.0',
+    photoLicenseUrl: 'https://calphotos.berkeley.edu/cgi/img_query?enlarge=1111+1111+1111+2881',
+    slug: 'eleutherodactylus-gryllus'
+  },
+  // {
+  //   taxonSpeciesId: 711,
+  //   source: '',
+  //   photoUrl: '',
+  //   photoCaption: '',
+  //   photoAuthor: '',
+  //   photoLicense: '',
+  //   photoLicenseUrl: '',
+  //   slug: 'eleutherodactylus-hedricki'
+  // },
+  {
+    taxonSpeciesId: 713,
+    source: 'INATURALIST',
+    photoUrl: 'https://inaturalist-open-data.s3.amazonaws.com/photos/134497951/medium.jpeg',
+    photoCaption: 'Mona Coqui',
+    photoAuthor: 'archypielago08',
+    photoLicense: 'CC BY-NC',
+    photoLicenseUrl: 'https://inaturalist.ca/photos/134497951',
+    slug: 'eleutherodactylus-monensis'
+  },
+  {
+    taxonSpeciesId: 728,
+    source: 'INATURALIST',
+    photoUrl: 'https://inaturalist-open-data.s3.amazonaws.com/photos/174295170/medium.jpeg',
+    photoCaption: 'Puerto Rican Pewee',
+    photoAuthor: 'Ben Zerante',
+    photoLicense: 'CC BY-NC',
+    photoLicenseUrl: 'https://inaturalist.ca/photos/174295170',
+    slug: 'contopus-latirostris-blancoi'
+  },
+  {
+    taxonSpeciesId: 4454,
+    source: 'WIKI',
+    photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/77/Sarabi-dog.jpg',
+    photoCaption: 'Sarabi dog',
+    photoAuthor: 'Sina xiave',
+    photoLicense: 'CC BY-SA 4.0',
+    photoLicenseUrl: 'https://en.wikipedia.org/wiki/Dog#/media/File:Sarabi-dog.jpg',
+    slug: 'canis-lupus-familiaris'
+  }
+]
