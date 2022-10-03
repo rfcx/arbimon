27e9a43d68945492a145d762267d7e77469dc5a8
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-10-03 12:09:56 +0700

    #910 Update logic to calculate site count on dashboard

---
+++ apps/cli/src/db/migrations/221003-01-update-project-site-count.ts
@@ -0,0 +1,51 @@
+/**
+ * WARNING: MIGRATIONS ARE IMMUTABLE
+ * Do not depend on imported code which may change
+ */
+
+import { QueryInterface } from 'sequelize'
+import { MigrationFn } from 'umzug'
+
+const VIEW_NAME = 'location_project_metric'
+
+export const up: MigrationFn<QueryInterface> = async (params): Promise<void> => {
+  await params.context.sequelize.query(`DROP VIEW IF EXISTS ${VIEW_NAME}`)
+
+  await params.context.sequelize.query(
+    `
+      CREATE VIEW ${VIEW_NAME} AS
+      SELECT d.location_project_id,
+            SUM(d.count)                       AS detection_minutes_count,
+            (
+              SELECT COUNT(s.id)
+              FROM location_site s
+              WHERE s.location_project_id = d.location_project_id
+            ) AS site_count,
+            COUNT(distinct d.taxon_species_id) AS species_count,
+            MAX(d.time_precision_hour_local)   AS max_date,
+            MIN(d.time_precision_hour_local)   AS min_date
+      FROM detection_by_site_species_hour d
+      GROUP BY d.location_project_id
+    `
+  )
+}
+
+// Old migrate logic from `220210-91-location-project-metric-mview`
+export const down: MigrationFn<QueryInterface> = async (params) => {
+  await params.context.sequelize.query(`DROP VIEW IF EXISTS ${VIEW_NAME}`)
+
+  await params.context.sequelize.query(
+    `
+      CREATE VIEW ${VIEW_NAME} AS
+      SELECT d.location_project_id,
+            SUM(d.count)                       AS detection_minutes_count,
+            COUNT(distinct d.location_site_id) AS site_count,
+            COUNT(distinct d.taxon_species_id) AS species_count,
+            MAX(d.time_precision_hour_local)   AS max_date,
+            MIN(d.time_precision_hour_local)   AS min_date
+      FROM detection_by_site_species_hour d
+      GROUP BY d.location_project_id
+    ;
+    `
+  )
+}
