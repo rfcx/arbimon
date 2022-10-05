321b357b2e77a75a73a5df349330512ac3333933
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-10-06 00:20:47 +0700

    #910 Update and add mock data for recordings and detections on Puerto Rico Project

---
+++ apps/cli/src/db/seeders/02-arbimon-data-mock/601-recordings-by-site-hour.ts
@@ -0,0 +1,37 @@
+import { QueryInterface } from 'sequelize'
+import { MigrationFn } from 'umzug'
+
+import { LocationSiteModel } from '@rfcx-bio/common/dao/models/location-site-model'
+import { RecordingBySiteHourModel } from '@rfcx-bio/common/dao/models/recording-by-site-hour-model'
+import { rawRecordings } from '@rfcx-bio/common/mock-data/raw-recordings'
+import { isDefined } from '@rfcx-bio/utils/predicates'
+
+import { getPuertoRicoProjectId } from '@/db/_helpers/get-puerto-rico-id'
+
+export const up: MigrationFn<QueryInterface> = async (params): Promise<void> => {
+  const sequelize = params.context.sequelize
+
+  // Lookups
+  const [puertoRicoProjectId, sites] = await Promise.all([
+    getPuertoRicoProjectId(sequelize),
+    LocationSiteModel(sequelize).findAll()
+  ])
+
+  if (Number.isNaN(puertoRicoProjectId)) return
+
+  const siteArbimonToBio: Record<number, number> = Object.fromEntries(sites.map(s => [s.idArbimon, s.id]))
+
+  const recordingValues: string[] = rawRecordings
+    .map(d => {
+      const siteId = siteArbimonToBio[d.location_site_id] ?? -1
+      if (siteId === -1) return undefined
+
+      return `('${d.time_precision_hour_local}', ${puertoRicoProjectId}, ${siteId}, '${d.created_at}', '${d.updated_at}', ${d.count}, '${d.counts_by_minute}', ${d.total_duration_in_minutes})`
+    }).filter(isDefined)
+
+  await sequelize.query(`INSERT INTO recording_by_site_hour (time_precision_hour_local, location_project_id, location_site_id, created_at, updated_at, count, counts_by_minute, total_duration_in_minutes) VALUES ${recordingValues.join(',')}`)
+}
+
+export const down: MigrationFn<QueryInterface> = async (params): Promise<void> => {
+  await RecordingBySiteHourModel(params.context.sequelize).sync({ force: true })
+}
