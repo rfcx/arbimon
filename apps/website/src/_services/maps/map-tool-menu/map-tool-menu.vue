77bc4812dd23bc62215ab60df8afd42ce22a7290
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-10-09 00:11:16 +0700

    #913 Update map icon and align map options

--- apps/website/src/_services/maps/map-tool-menu/map-tool-menu.vue
+++ apps/website/src/_services/maps/map-tool-menu/map-tool-menu.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="flex">
-    <map-style-options
-      :map-options="mapStatisticsDisplayStyleOptions"
-      :map-style="props.mapStatisticsStyle"
-      @emit-map-style="emitMapStatisticsStyle"
-    />
-    <map-style-options
-      :map-options="mapGroundStyleOptions"
-      :map-style="props.mapGroundStyle"
-      class="ml-2"
-      @emit-map-style="emitMapGroundStyle"
-    />
+    <div class="sm:flex">
+      <map-style-options
+        :map-options="mapStatisticsDisplayStyleOptions"
+        :map-style="props.mapStatisticsStyle"
+        @emit-map-style="emitMapStatisticsStyle"
+      />
+      <map-style-options
+        :map-options="mapGroundStyleOptions"
+        :map-style="props.mapGroundStyle"
+        class="sm:ml-2 <sm:mt-2"
+        @emit-map-style="emitMapGroundStyle"
+      />
+    </div>
     <button
       class="btn ml-2"
-      :class="{ '<lg:(bg-brand-primary btn-icon)': isShowLabel }"
+      :class="{ '<2xl:(bg-brand-primary btn-icon)': isShowLabel }"
       @click="emitShowLabelsToggle"
     >
-      <div class="flex items-center <lg:hidden">
+      <div class="flex items-center <2xl:hidden">
         <input
           type="checkbox"
           class="mr-2 text-brand-primary focus:(ring-0 outline-none) rounded"
@@ -24,7 +26,7 @@
         >
         Labels
       </div>
-      <div class="lg:hidden">
+      <div class="2xl:hidden md:hidden">
         <icon-fas-tag class="text-xs" />
       </div>
     </button>
@@ -68,8 +70,8 @@ const mapGroundStyleOptions: MapOptions[] = [
 ]
 
 const mapStatisticsDisplayStyleOptions: MapOptions[] = [
-  { style: MAPBOX_STYLE_HEATMAP, name: 'Heatmap', icon: new URL('./icons/satellite.svg', import.meta.url).toString() },
-  { style: MAPBOX_STYLE_BUBBLE, name: 'Bubble', icon: new URL('./icons/global.svg', import.meta.url).toString() }
+  { style: MAPBOX_STYLE_HEATMAP, name: 'Heatmap', icon: new URL('./icons/heatmap.svg', import.meta.url).toString() },
+  { style: MAPBOX_STYLE_BUBBLE, name: 'Bubble', icon: new URL('./icons/bubble.svg', import.meta.url).toString() }
 ]
 
 </script>
