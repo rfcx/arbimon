d11608b5aeea81f2f76536036964907d797e5173
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-10-08 23:13:39 +0700

    #913 Clean up map options code

--- apps/website/src/_services/maps/map-tool-menu/map-style-options.vue
+++ apps/website/src/_services/maps/map-tool-menu/map-style-options.vue
@@ -1,82 +1,42 @@
 <template>
-  <template
-    v-for="(item, idx) in mapStatisticsDisplayStyleOptions"
-    :key="'map-statistics-style-' + item.name"
-  >
-    <button
-      class="btn <md:(btn-icon)"
-      :class="{
-        'bg-brand-primary': props.mapStatisticsStyle === item.style,
-        'rounded-r-none': idx !== mapStatisticsDisplayStyleOptions.length - 1,
-        'rounded-l-none': idx !== 0
-      }"
-      @click="$emit('emitMapStatisticsStyle', item.style)"
+  <div>
+    <template
+      v-for="(item, idx) in props.mapOptions"
+      :key="'map-statistics-style-' + item.name"
     >
-      <div class="<lg:hidden">
-        {{ item.name }}
-      </div>
-      <div>
-        <img
-          class="lg:hidden <lg:visible"
-          :src="item.icon"
-          width="16"
-          height="16"
-        >
-      </div>
-    </button>
-  </template>
-  <template
-    v-for="(item, idx) in mapGroundStyleOptions"
-    :key="'map-ground-style-' + item.name"
-  >
-    <button
-      class="btn <md:(btn-icon)"
-      :class="{
-        'bg-brand-primary': props.mapGroundStyle === item.style,
-        'rounded-r-none': idx !== mapGroundStyleOptions.length - 1,
-        'rounded-l-none': idx !== 0,
-        'ml-2': idx === 0
-      }"
-      @click="$emit('emitMapGroundStyle', item.style)"
-    >
-      <div class="<lg:hidden">
-        {{ item.name }}
-      </div>
-      <div>
-        <img
-          class="lg:hidden <lg:visible"
-          :src="item.icon"
-          width="16"
-          height="16"
-        >
-      </div>
-    </button>
-  </template>
+      <button
+        class="btn <md:(btn-icon)"
+        :class="{
+          'bg-brand-primary': props.mapStyle === item.style,
+          'rounded-r-none': idx !== props.mapOptions.length - 1,
+          'rounded-l-none': idx !== 0
+        }"
+        @click="$emit('emitMapStyle', item.style)"
+      >
+        <div class="<lg:hidden">
+          {{ item.name }}
+        </div>
+        <div>
+          <img
+            class="lg:hidden <lg:visible"
+            :src="item.icon"
+            width="16"
+            height="16"
+          >
+        </div>
+      </button>
+    </template>
+  </div>
 </template>
 <script setup lang="ts">
-import { MAPBOX_STYLE_BUBBLE, MAPBOX_STYLE_HEATMAP, MAPBOX_STYLE_RFCX_WITH_PLACE_LABELS, MAPBOX_STYLE_SATELLITE_STREETS, MapboxStyle } from '~/maps'
-
-interface MapOptions {
-  style: MapboxStyle
-  name: string
-  icon: string
-}
+import { MapboxStyle } from '~/maps'
+import { MapOptions } from './types'
 
 const props = defineProps<{
-  readonly mapGroundStyle: MapboxStyle
-  readonly mapStatisticsStyle: MapboxStyle
+  readonly mapOptions: MapOptions[]
+  readonly mapStyle: MapboxStyle
 }>()
 
-defineEmits<{(e: 'emitMapGroundStyle', style: MapboxStyle): void, (e: 'emitMapStatisticsStyle', style: MapboxStyle): void}>()
-
-const mapGroundStyleOptions: MapOptions[] = [
-  { style: MAPBOX_STYLE_SATELLITE_STREETS, name: 'Satellite', icon: new URL('./icons/satellite.svg', import.meta.url).toString() },
-  { style: MAPBOX_STYLE_RFCX_WITH_PLACE_LABELS, name: 'Simple', icon: new URL('./icons/global.svg', import.meta.url).toString() }
-]
-
-const mapStatisticsDisplayStyleOptions: MapOptions[] = [
-  { style: MAPBOX_STYLE_HEATMAP, name: 'Heatmap', icon: new URL('./icons/satellite.svg', import.meta.url).toString() },
-  { style: MAPBOX_STYLE_BUBBLE, name: 'Bubble', icon: new URL('./icons/global.svg', import.meta.url).toString() }
-]
+defineEmits<{(e: 'emitMapStyle', style: MapboxStyle): void}>()
 
 </script>
