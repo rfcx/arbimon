8feebaf5882181de46325b9c95d891be38afab5b
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-10-18 17:37:23 +0700

    #916 Add logic to not export legend if it is heatmap option

--- apps/website/src/_services/maps/functions.ts
+++ apps/website/src/_services/maps/functions.ts
@@ -1,22 +1,25 @@
 import { Map as MapboxMap } from 'mapbox-gl'
 
-import { withFileName, zipAndDownload } from '@rfcx-bio/utils/file'
+import { JsZipFile, withFileName, zipAndDownload } from '@rfcx-bio/utils/file'
 import { asPromise } from '@rfcx-bio/utils/fp'
+import { isDefined } from '@rfcx-bio/utils/predicates'
 
 import { canvasToPngBlob, svgToCanvas } from '~/charts'
 import { generateMapLegend } from './map-legend'
 import { MapBaseLegendEntry } from './types'
 
-export const downloadMapPng = async (map: MapboxMap, filename: string, legendEntry: MapBaseLegendEntry[]): Promise<void> => {
+export const downloadMapPng = async (map: MapboxMap, filename: string, legendEntry?: MapBaseLegendEntry[]): Promise<void> => {
   const mapBlobPromise = canvasToPngBlob(map.getCanvas())
     .then(withFileName(`${filename}.png`))
 
-  const legendBlobPromise = asPromise(legendEntry)
-    .then(generateMapLegend)
-    .then(svgToCanvas)
-    .then(canvasToPngBlob)
-    .then(withFileName(`${filename}-legend.png`))
+  const legendBlobPromise: Promise<JsZipFile> | undefined = legendEntry
+    ? asPromise(legendEntry)
+      .then(generateMapLegend)
+      .then(svgToCanvas)
+      .then(canvasToPngBlob)
+      .then(withFileName(`${filename}-legend.png`))
+    : undefined
 
-  await Promise.all([mapBlobPromise, legendBlobPromise])
+  await Promise.all([mapBlobPromise, legendBlobPromise].filter(isDefined))
     .then(async files => await zipAndDownload(files, filename))
 }
