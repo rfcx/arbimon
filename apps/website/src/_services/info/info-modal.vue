053001e3527e8fbffb47cd16a45c306238706a33
Antony Harfield <antonyharfield@hotmail.com>
2023-02-22 12:00:54 +0000

    #975 Use modal instead of separate route

---
+++ apps/website/src/_services/info/info-modal.vue
@@ -0,0 +1,22 @@
+<template>
+  <modal-popup
+    title="supporting information"
+    modal-body="sm:(my-8 align-middle max-w-5xl w-full)"
+    @emit-close="$emit('close')"
+  >
+    <button
+      class="mt-4 mr-4 px-2 float-right"
+      @click="$emit('close')"
+    >
+      <icon-fa-close class="cursor-pointer h-8 inline" />
+    </button>
+    <div class="p-4">
+      <InfoPage />
+    </div>
+  </modal-popup>
+</template>
+<script setup lang="ts">
+import InfoPage from './info-page.vue'
+
+const emit = defineEmits<{(e: 'close'): void}>()
+</script>
