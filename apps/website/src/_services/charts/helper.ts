75d194b6581a0f3657256a88b08a722af7d27c83
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2023-02-28 12:32:04 +0400

    rfcx/engineering-support#285 Return back the original order of percentage values

--- apps/website/src/_services/charts/helper.ts
+++ apps/website/src/_services/charts/helper.ts
@@ -2,15 +2,8 @@ export function roundedPercentage<T> (selector: (x: T) => number, xs: T[]): Arra
   // calculate the difference between the 100% and the sum of the rounded percentages
   const difference = 100 - xs.reduce((acc, curr) => acc + Math.abs(Math.floor(selector(curr))), 0)
 
-  // sort in descendant order by decimal part of the real percentage
-  const sortedByDecimalPart = [...xs].sort((a, b) => {
-    const aPercentage = selector(a)
-    const bPercentage = selector(b)
-    return bPercentage - Math.floor(bPercentage) - (aPercentage - Math.floor(aPercentage))
-  })
-
   // distribute the difference between the rounded values in decreasing order of their decimal parts
-  const { result } = sortedByDecimalPart.reduce(
+  const { result } = [...xs].reduce(
     (acc, curr) => {
       if (acc.difference > 0) {
         acc.difference -= 1
