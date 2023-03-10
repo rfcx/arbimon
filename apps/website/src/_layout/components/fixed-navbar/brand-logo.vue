e0c346b7233033af5d379b1a9d5fd0842b033eab
Antony Harfield <antonyharfield@hotmail.com>
2023-03-10 22:35:51 +0000

    #984 Sidebar for projects

---
+++ apps/website/src/_layout/components/fixed-navbar/brand-logo.vue
@@ -0,0 +1,18 @@
+<template>
+  <img
+    src="/src/_services/assets/rfcx-logo.svg"
+    class="mr-3 h-6 sm:h-9"
+    alt="Arbimon Logo"
+  >
+  <span
+    class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
+    :class="{ pride: toggles?.logoPride }"
+  >Arbimon</span>
+</template>
+<script setup lang="ts">
+import { inject } from 'vue'
+
+import { togglesKey } from '@/globals'
+
+const toggles = inject(togglesKey)
+</script>
