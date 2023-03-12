fc2812c71f7e7472ed9d888bdf8d3d021c797f8b
Antony Harfield <antonyharfield@hotmail.com>
2023-03-12 22:49:20 +0000

    #985 Home layout

---
+++ apps/website/src/landing/home/blocks/home-how-it-works.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="bg-white dark:bg-steel-gray">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+      <div class="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
+        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+          How it works
+        </h2>
+        <p class="text-gray-500 sm:text-xl dark:text-gray-400">
+          Get started as easy as 1, 2, 3
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
--- apps/website/src/landing/home/home-page.vue
+++ apps/website/src/landing/home/home-page.vue
@@ -1,10 +1,19 @@
 <template>
-  <Hero />
+  <hero />
+  <impact />
+  <what-we-do />
+  <hr class="h-px bg-gray-200 border-0 dark:bg-gray-700">
+  <features />
+  <how-it-works />
 </template>
 <script lang="ts" setup>
 import { useHead } from '@vueuse/head'
 
+import Features from './blocks/home-features.vue'
 import Hero from './blocks/home-hero.vue'
+import HowItWorks from './blocks/home-how-it-works.vue'
+import Impact from './blocks/home-impact.vue'
+import WhatWeDo from './blocks/home-what-we-do.vue'
 
 useHead({
   title: 'Arbimon - Empower your wildlife research',
