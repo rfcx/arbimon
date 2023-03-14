07b6de7a251134de67c0e4610f23af4ed6349a21
Antony Harfield <antonyharfield@hotmail.com>
2023-03-14 11:25:16 +0000

    #985 Testimonials

--- apps/website/src/landing/home/blocks/home-impact.vue
+++ apps/website/src/landing/home/blocks/home-impact.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="bg-white dark:bg-mirage-gray">
-    <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-24 lg:px-6">
       <div class="mx-auto max-w-screen-md text-center mb-8 lg:mb-16">
         <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
           Our Impact at a Glance
--- apps/website/src/landing/home/home-page.vue
+++ apps/website/src/landing/home/home-page.vue
@@ -5,6 +5,7 @@
   <hr class="h-px bg-gray-200 border-0 dark:bg-gray-700">
   <features />
   <how-it-works />
+  <testimonials-partners />
 </template>
 <script lang="ts" setup>
 import { useHead } from '@vueuse/head'
@@ -13,6 +14,7 @@ import Features from './blocks/home-features.vue'
 import Hero from './blocks/home-hero.vue'
 import HowItWorks from './blocks/home-how-it-works.vue'
 import Impact from './blocks/home-impact.vue'
+import TestimonialsPartners from './blocks/home-testimonials-partners.vue'
 import WhatWeDo from './blocks/home-what-we-do.vue'
 
 useHead({
