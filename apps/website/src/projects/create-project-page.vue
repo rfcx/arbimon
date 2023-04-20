1def6303664ba3ae1610d482186fea5e6bb1d570
Antony Harfield <antonyharfield@hotmail.com>
2023-04-20 14:38:54 +0100

    #1002 Create project or display an error

--- apps/website/src/projects/create-project-page.vue
+++ apps/website/src/projects/create-project-page.vue
@@ -1,15 +1,146 @@
 <template>
   <fixed-navbar />
-  <section class="w-full pt-20 px-8 pb-6 bg-white dark:bg-mirage-gray">
-    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-24 lg:px-6">
-      <div class="mx-auto max-w-screen-md mb-8 lg:mb-12">
-        <h2 class="text-2xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-          Create New Project
-        </h2>
+  <section class="pt-16 bg-white dark:bg-mirage-gray">
+    <div class="py-8 px-4 mx-auto max-w-screen-md lg:py-24">
+      <h2 class="text-2xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+        Create a new project
+      </h2>
+      <div class="mt-4 grid gap-4 sm:grid-cols-2 sm:gap-6">
+        <div class="sm:col-span-2">
+          <label
+            for="name"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >Project Name</label>
+          <input
+            id="name"
+            v-model="name"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+            placeholder="Brown bears in Eastern Finland"
+            required
+          >
+        </div>
+        <!-- <div>
+          <label
+            for="category"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >Category</label>
+          <select
+            id="category"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+          >
+            <option selected="">
+              Select category
+            </option>
+            <option value="TV">
+              TV/Monitors
+            </option>
+            <option value="PC">
+              PC
+            </option>
+            <option value="GA">
+              Gaming/Console
+            </option>
+            <option value="PH">
+              Phones
+            </option>
+          </select>
+        </div>
+        <div>
+          <label
+            for="item-weight"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >Item Weight (kg)</label>
+          <input
+            id="item-weight"
+            type="number"
+            name="item-weight"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+            placeholder="12"
+            required=""
+          >
+        </div>
+        <div class="sm:col-span-2">
+          <label
+            for="description"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >Description</label>
+          <textarea
+            id="description"
+            rows="8"
+            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+            placeholder="Your description here"
+          />
+        </div> -->
+      </div>
+      <div>
+        <button
+          class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
+          :disabled="isCreating"
+          @click.prevent="create"
+        >
+          Create project
+        </button>
+        <svg
+          v-if="isCreating"
+          aria-hidden="true"
+          role="status"
+          class="inline w-4 h-4 ml-4 text-white animate-spin"
+          viewBox="0 0 100 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="#E5E7EB"
+          />
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentColor"
+          />
+        </svg>
+        <span
+          v-if="hasFailed"
+          class="p-4 text-sm text-red-800 dark:text-red-400"
+          role="alert"
+        >
+          <span class="font-medium">Failed!</span> Give us a moment to get things straight and try submitting again.
+        </span>
       </div>
     </div>
   </section>
 </template>
 <script setup lang="ts">
+import type { AxiosInstance } from 'axios'
+import { inject, ref, watch } from 'vue'
+import { useRouter } from 'vue-router'
+
+import { apiBioPostProjectCreate } from '@rfcx-bio/common/api-bio/project/project-create'
+
 import FixedNavbar from '@/_layout/components/fixed-navbar/fixed-navbar.vue'
+import { apiClientBioKey } from '@/globals'
+import { ROUTE_NAMES } from '~/router'
+
+const router = useRouter()
+
+const apiClientBio = inject(apiClientBioKey) as AxiosInstance
+
+const name = ref<string>('')
+const isCreating = ref<boolean>(false)
+const hasFailed = ref<boolean>(false)
+
+watch(name, () => { hasFailed.value = false })
+
+async function create () {
+  hasFailed.value = false
+  isCreating.value = true
+  const project = { name: name.value }
+  try {
+    const response = await apiBioPostProjectCreate(apiClientBio, project)
+    router.push({ name: ROUTE_NAMES.overview, params: { slug: response?.slug } })
+  } catch (e) {
+    if (e instanceof Error) console.error(e.message)
+    hasFailed.value = true
+  }
+  isCreating.value = false
+}
 </script>
