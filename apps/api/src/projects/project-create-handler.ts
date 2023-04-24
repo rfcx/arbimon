0e894cf9f998fc189c28db6921eaec28ec9a559f
Antony Harfield <antonyharfield@hotmail.com>
2023-04-24 12:46:08 +0100

    Lint fix

--- apps/api/src/projects/project-create-handler.ts
+++ apps/api/src/projects/project-create-handler.ts
@@ -1,8 +1,8 @@
 import { type ProjectCreateRequest, type ProjectCreateResponse } from '@rfcx-bio/common/api-bio/project/project-create'
 
+import { type Auth0UserInfo } from '~/auth0'
 import { type Handler } from '../_services/api-helpers/types'
 import { createProject } from './project-create-bll'
-import { Auth0UserInfo } from '~/auth0'
 
 export const projectCreateHandler: Handler<ProjectCreateResponse, unknown, unknown, ProjectCreateRequest> = async (req, res) => {
   // Inputs & validation
