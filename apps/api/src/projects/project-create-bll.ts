10aaba7179b2ff14da7966b36be579a3747cb8e7
Antony Harfield <antonyharfield@hotmail.com>
2023-04-24 12:24:26 +0100

    #1002 Handle user project caching on create project

--- apps/api/src/projects/project-create-bll.ts
+++ apps/api/src/projects/project-create-bll.ts
@@ -1,12 +1,16 @@
 import { type ProjectCreateRequest } from '@rfcx-bio/common/api-bio/project/project-create'
 
 import { createProject as createProjectInCore, getProject as getProjectInCore } from '~/api-core/api-core'
+import { clearMemberProjectCoreIds } from '~/cache/user-project-cache'
 import { createProject as createProjectLocal } from './project-create-dao'
 
-export const createProject = async (request: ProjectCreateRequest, token: string): Promise<string> => {
+export const createProject = async (request: ProjectCreateRequest, auth0UserId: string, token: string): Promise<string> => {
   // Create in Core
   const idCore = await createProjectInCore({ name: request.name, is_public: false }, token)
   const { external_id: idArbimon } = await getProjectInCore(idCore, token)
+
+  // Cache user project
+  await clearMemberProjectCoreIds(auth0UserId)
 
   // Pre-populate insights table with the same data (will get updated again after sync)
   const project = { idCore, idArbimon, name: request.name }
--- apps/api/src/projects/project-create-handler.ts
+++ apps/api/src/projects/project-create-handler.ts
@@ -2,12 +2,15 @@ import { type ProjectCreateRequest, type ProjectCreateResponse } from '@rfcx-bio
 
 import { type Handler } from '../_services/api-helpers/types'
 import { createProject } from './project-create-bll'
+import { Auth0UserInfo } from '~/auth0'
 
 export const projectCreateHandler: Handler<ProjectCreateResponse, unknown, unknown, ProjectCreateRequest> = async (req, res) => {
   // Inputs & validation
   const project = req.body
 
-  const slug = await createProject(project, req.headers.authorization ?? '')
+  const auth0UserInfo = await req.jwtDecode<Auth0UserInfo>()
+
+  const slug = await createProject(project, auth0UserInfo.auth0_user_id, req.headers.authorization ?? '')
   res.statusCode = 201
   return { slug }
 }
