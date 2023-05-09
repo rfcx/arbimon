45d8fa4d8d102c4d39dc941829433da83526fa63
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-05-09 16:57:01 +0700

    #1022 finished the `export-csv` function in the code
    next up is to add it to the github workflows

---
+++ apps/cli/src/sync/export-csv/get-species.ts
@@ -0,0 +1,22 @@
+import { type Sequelize, QueryTypes } from 'sequelize'
+
+export interface ExportedSpecies {
+  id: number
+  scientific_name: string
+  taxon_name: string
+  iucn_status: string
+  is_threatened: boolean
+}
+
+export const getSpecies = async (sequelize: Sequelize): Promise<ExportedSpecies[]> => {
+  const sql = `
+    select s.id, s.scientific_name, c.common_name taxon_name, rri.code iucn_code, rri.is_threatened
+    from taxon_species s
+    join taxon_class c on s.taxon_class_id = c.id
+    left join taxon_species_iucn si on s.id = si.taxon_species_id
+    left join risk_rating_iucn rri on si.risk_rating_iucn_id = rri.id
+  `
+
+  const species = await sequelize.query<ExportedSpecies>(sql, { type: QueryTypes.SELECT, raw: true })
+  return species
+}
