545caa09944bb47b3e99b62510b6bf7d75574e64
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-05-09 18:09:23 +0700

    #1022 change folder name format back to the one with colon

--- apps/cli/src/sync/export-csv/get-filename.ts
+++ apps/cli/src/sync/export-csv/get-filename.ts
@@ -3,5 +3,5 @@ import { type Dayjs } from 'dayjs'
 import { type Filetype } from './create-command'
 
 export const getFilename = (startTime: Dayjs, filetype: Filetype): string => {
-  return `biodiversity-exports/${startTime.format('YYYY-MM-DD HH-mm-ss')}/${filetype}.csv`
+  return `biodiversity-exports/${startTime.format('YYYY-MM-DD HH:mm:ss')}/${filetype}.csv`
 }
