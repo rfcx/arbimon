5f926d2c7a5fc6c7eac35323652f7d4e48d35da1
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2023-05-10 18:17:19 +0400

    #1017 Support search site input in the query

--- apps/website/src/_services/filters/comparison-filter-modal/filter-taxon/filter-taxon.ts
+++ apps/website/src/_services/filters/comparison-filter-modal/filter-taxon/filter-taxon.ts
@@ -14,7 +14,7 @@ export default class FilterTaxon extends Vue {
 
   @Emit() emitSelectedTaxons (): FilterPropertyEquals[] {
     if (this.selectedTaxons.length === this.taxons.length) return [] // select all === no filter
-    void this.$router.replace({ query: { ...this.$route.query, 'taxon[]': this.selectedTaxons } })
+    void this.$router.replace({ query: { ...this.$route.query, taxons: this.selectedTaxons } })
     return this.selectedTaxons.map(i => { return { propertyName: 'taxon', value: i } })
   }
 
