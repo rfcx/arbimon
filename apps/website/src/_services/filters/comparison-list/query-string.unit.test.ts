6cbc3e48853a9787d34876802a07324898100de5
Antony Harfield <antonyharfield@hotmail.com>
2023-05-17 13:53:26 +0100

    #1017 Refactor and add more tests

--- apps/website/src/_services/filters/comparison-list/query-string.unit.test.ts
+++ apps/website/src/_services/filters/comparison-list/query-string.unit.test.ts
@@ -8,41 +8,22 @@ import { fromQuery, toQuery } from '@/_services/filters/comparison-list/query-st
 import { FilterImpl } from '../classes'
 import { type FilterPropertyEquals, type SiteGroup } from '../types'
 
-const DEFAULT_START = dayjs.utc('1990-01-01T00:00:00.000Z').startOf('day')
-const DEFAULT_END = dayjs().utc().startOf('day')
+function singleSiteGroup (id: number, name: string): SiteGroup {
+  return { label: name, value: [{ id, name }] as Site[] }
+}
+function wildcardSiteGroup (count: number, startingId: number, prefix: string): SiteGroup {
+  return { label: prefix + '*', value: Array.from(Array(count).keys()).map(i => ({ id: startingId + i, name: `${prefix}${i}` })) as Site[] }
+}
+function taxon (value: number): FilterPropertyEquals {
+  return { propertyName: 'taxon', value }
+}
+const exampleStartDate = dayjs.utc('2022-03-01T00:00Z')
+const exampleEndDate = dayjs.utc('2022-08-31T23:59Z')
 
 describe('comparison-list > query-string > toQuery', () => {
-  const SITE_1: Site = {
-    id: 111111,
-    idCore: 'qwerty',
-    idArbimon: 1234,
-    locationProjectId: 111,
-    name: 'SITE_1',
-    latitude: 49,
-    longitude: 25,
-    altitude: 0
-  }
-  const sites: SiteGroup[] = [{
-    label: 'SITE_1',
-    value: [SITE_1]
-  }]
-  const taxons: FilterPropertyEquals[] = [{
-    propertyName: 'taxon',
-    value: 200
-  }]
-  const filters: FilterImpl[] = [new FilterImpl(
-    DEFAULT_START,
-    DEFAULT_END,
-    sites,
-    taxons
-  )
-]
-  test('succeeds for empty filters', async () => {
-    // Arrange
-    const filters: FilterImpl[] = []
-
+  test('empty filters', async () => {
     // Act
-    const query = toQuery(filters)
+    const query = toQuery([])
 
     // Assert
     expect(query).toBeDefined()
@@ -50,93 +31,147 @@ describe('comparison-list > query-string > toQuery', () => {
     expect(query).toEqual({})
   })
 
-  test('succeeds for filled filters', async () => {
+  test('1 filter with site and taxon', async () => {
     // Arrange
-    const EXPECTED_PROPS = ['dss', 'ab', 'ae', 'as', 'at']
-    const EXPECTED_QUERY = {
-      dss: '1',
-      ab: '1990-01-01',
-      ae: '2023-05-16',
-      as: '111111',
-      at: '200'
-    }
+    const startDate = '2022-03-01'
+    const endDate = '2022-08-31'
+    const siteId = 5
+    const taxonId = 200
+    const filter: FilterImpl = new FilterImpl(
+      dayjs.utc(startDate + 'T00:00Z'),
+      dayjs.utc(endDate + 'T23:59Z'),
+      [singleSiteGroup(siteId, 'ABC')],
+      [taxon(taxonId)]
+    )
+
     // Act
-    const query = toQuery(filters)
+    const query = toQuery([filter])
 
     // Assert
-    expect(query).toBeDefined()
-    EXPECTED_PROPS.forEach(expectedProp => { expect(query).toHaveProperty(expectedProp) })
-    expect(Object.keys(query)).toHaveLength(5)
+    expect(Object.keys(query)).toHaveLength(5) // dss, ab, ae, as, at
     Object.values(query).forEach(value => { expect(value).toBeTypeOf('string') })
-    expect(query).toEqual(EXPECTED_QUERY)
+    expect(query.dss).toBe('1')
+    expect(query.ab).toBe(startDate)
+    expect(query.ae).toBe(endDate)
+    expect(query.as).toBe(siteId.toString())
+    expect(query.at).toBe(taxonId.toString())
   })
+
+  test('1 filter without taxon', async () => {
+    // Arrange
+    const filter = new FilterImpl(exampleStartDate, exampleEndDate, [singleSiteGroup(5, 'ABC')])
+
+    // Act
+    const query = toQuery([filter])
+
+    // Assert
+    expect(Object.keys(query)).toHaveLength(4) // dss, ab, ae, as
+    expect(query).not.toHaveProperty('at')
+  })
+
+  test('1 filter with a list of single sites', async () => {
+    // Arrange
+    const siteIds = [5, 6, 7]
+    const filter = new FilterImpl(exampleStartDate, exampleEndDate, siteIds.map(i => singleSiteGroup(i, `ABC_${i}`)))
+
+    // Act
+    const query = toQuery([filter])
+
+    // Assert
+    expect(query.as).toBe(siteIds.join(','))
+  })
+
+  test('1 filter with a multiple sites using wildcard', async () => {
+    // Arrange
+    const wildcardPrefix = 'AB_'
+    const filter = new FilterImpl(exampleStartDate, exampleEndDate, [wildcardSiteGroup(100, 1001, wildcardPrefix)])
+
+    // Act
+    const query = toQuery([filter])
+
+    // Assert
+    expect(query.as).toBe(wildcardPrefix + '*')
+  })
+
+  // TODO test more than 1 filter
 })
 
 describe('comparison-list > query-string > fromQuery', () => {
-  const SITE_1: Site = {
-    id: 111111,
-    idCore: 'qwerty',
-    idArbimon: 1234,
-    locationProjectId: 111,
-    name: 'SITE_1',
-    latitude: 49,
-    longitude: 25,
-    altitude: 0
-  }
-
-  const sites: SiteGroup[] = [{
-    label: 'SITE_1',
-    value: [SITE_1]
-  }]
-
-  const taxons: FilterPropertyEquals[] = [{
-    propertyName: 'taxon',
-    value: 200
-  }]
-
-  const EXPECTED_FILTERS: FilterImpl[] = [new FilterImpl(
-    dayjs.utc('2021-01-01T00:00:00.000Z').startOf('day'),
-    dayjs.utc('2023-01-01T00:00:00.000Z').startOf('day'),
-    sites,
-    taxons
-  )]
-
-  const taxon: TaxonClass = {
-    id: 200,
-    idArbimon: 123,
-    slug: 'bird',
-    commonName: 'Bird'
-  }
-
-  const projectFilter: ProjectFiltersResponse = {
-    locationSites: [SITE_1],
-    taxonClasses: [taxon],
+  const SITE_1 = { id: 201, name: 'GH_1' }
+  const TAXON_BIRD = { id: 200, slug: 'bird' }
+  const projectFilters: ProjectFiltersResponse = {
+    locationSites: [{ id: 101, name: 'DEF_1' }, { id: 102, name: 'DEF_2' }, { id: 103, name: 'DEF_3' }, SITE_1] as Site[],
+    taxonClasses: [TAXON_BIRD, { id: 300, slug: 'mammal' }] as TaxonClass[],
     dateStartInclusiveUtc: '2021-01-01T00:00:00.000Z',
     dateEndInclusiveUtc: '2023-01-01T00:00:00.000Z'
   }
-  test('succeeds for empty query', async () => {
+
+  test('empty query', async () => {
     // Act
-    const query = fromQuery({}, undefined)
+    const query = fromQuery({}, projectFilters)
 
     // Assert
     expect(query).toBeDefined()
     expect(query).toEqual([])
   })
 
-  test('succeeds for filled query', async () => {
+  test('1 filter with dates and site', async () => {
     // Arrange
-    const QUERY = {
+    const query = {
       dss: '1',
-      ab: '2021-01-01',
-      ae: '2023-01-01',
-      as: '111111',
-      at: '200'
+      ab: '2022-04-01',
+      ae: '2022-10-31',
+      as: SITE_1.id.toString()
     }
+
     // Act
-    const filters = fromQuery(QUERY, projectFilter)
+    const filters = fromQuery(query, projectFilters)
 
     // Assert
-    expect(filters).toBeDefined()
-    expect(filters).toEqual(EXPECTED_FILTERS)
+    expect(filters).toHaveLength(1)
+    const filter = filters[0]
+    expect(filter.startDate).toEqual(dayjs.utc(query.ab))
+    expect(filter.endDate).toEqual(dayjs.utc(query.ae))
+    expect(filter.sites).toHaveLength(1)
+    expect(filter.sites[0].label).toBe(SITE_1.name)
+    expect(filter.sites[0].value).toHaveLength(1)
+    expect(filter.sites[0].value[0].id).toBe(SITE_1.id)
   })
+
+  test('1 filter including taxon', async () => {
+    // Arrange
+    const query = {
+      dss: '1',
+      ab: '2022-04-01',
+      ae: '2022-10-31',
+      as: SITE_1.id.toString(),
+      at: TAXON_BIRD.id.toString()
+    }
+
+    // Act
+    const filters = fromQuery(query, projectFilters)
+
+    // Assert
+    expect(filters[0].otherFilters).toHaveLength(1)
+    expect(filters[0].otherFilters[0].propertyName).toBe('taxon')
+    expect(filters[0].otherFilters[0].value).toBe(TAXON_BIRD.id)
+  })
+
+  test('1 filter without dates (use default from project)', async () => {
+    // Arrange
+    const query = {
+      dss: '1',
+      as: SITE_1.id.toString()
+    }
+
+    // Act
+    const filters = fromQuery(query, projectFilters)
+
+    // Assert
+    const filter = filters[0]
+    expect(filter.startDate).toEqual(dayjs.utc(projectFilters.dateStartInclusiveUtc))
+    expect(filter.endDate).toEqual(dayjs.utc(projectFilters.dateEndInclusiveUtc))
+  })
+
+  // TODO test more than 1 filter
 })
