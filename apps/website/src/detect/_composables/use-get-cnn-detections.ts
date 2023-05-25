a313aba74dc2670470ed3189c705417f0df15458
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-05-25 17:51:42 +0700

    #1040 fix unknown calls

--- apps/website/src/detect/_composables/use-get-cnn-detections.ts
+++ apps/website/src/detect/_composables/use-get-cnn-detections.ts
@@ -1,13 +1,13 @@
 import { type AxiosInstance } from 'axios'
 import { type UseQueryReturnType, useQuery } from 'vue-query'
 
-import { type DetectSummaryResponse, apiBioGetDetectSummaryData } from '@rfcx-bio/common/api-bio/detect/detect-summary'
+import { type DetectCnnDetectionsQueryParams, type DetectCnnDetectionsResponse, apiBioGetDetectCnnDetections } from '@rfcx-bio/common/api-bio/detect/detect-cnn-detections'
 
 export const FETCH_DETECTIONS = 'fetch-detections'
 
-export const useGetJobDetections = (apiClient: AxiosInstance, jobId: number): UseQueryReturnType<DetectSummaryResponse | undefined, unknown> => {
+export const useGetJobDetections = (apiClient: AxiosInstance, params: DetectCnnDetectionsQueryParams): UseQueryReturnType<DetectCnnDetectionsResponse | undefined, unknown> => {
   return useQuery(
     [FETCH_DETECTIONS],
-    async () => await apiBioGetDetections(apiClient, jobId)
+    async () => await apiBioGetDetectCnnDetections(apiClient, params)
   )
 }
