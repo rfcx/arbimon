cf71fad30c11b6c5b064349a7cb5e74eef3b07a1
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-05-26 15:52:45 +0700

    #1040 connect bio backend to core API.

--- apps/api/src/detect/cnn/detect-cnn-detections-handler.ts
+++ apps/api/src/detect/cnn/detect-cnn-detections-handler.ts
@@ -2,19 +2,18 @@ import { type RequestParamsDefault } from 'fastify'
 
 import { type DetectCnnDetectionsQueryParams, type DetectCnnDetectionsResponse } from '@rfcx-bio/common/api-bio/detect/detect-cnn-detections'
 
+import { getDetections } from '~/api-core/api-core'
+import { isValidToken } from '~/api-helpers/is-valid-token'
 import { type Handler } from '~/api-helpers/types'
+import { BioUnauthorizedError } from '~/errors'
 
-export const detectCnnDetectionsHandler: Handler<DetectCnnDetectionsResponse, RequestParamsDefault, DetectCnnDetectionsQueryParams> = async (req): Promise<DetectCnnDetectionsResponse> => {
-  console.info(req.params)
+export const detectCnnDetectionsHandler: Handler<DetectCnnDetectionsResponse[], RequestParamsDefault, DetectCnnDetectionsQueryParams> = async (req): Promise<DetectCnnDetectionsResponse[]> => {
+  const token = req.headers.authorization
 
-  return [
-    {
-      id: '12345',
-      stream_id: 'lskdmfsfi',
-      start: '2023-02-20T23:22:43.445Z',
-      end: '2023-02-20T23:22:43.448Z',
-      confidence: 0.123,
-      review_status: null
-    }
-  ]
+  if (token === undefined || !isValidToken(token)) {
+    throw BioUnauthorizedError()
+  }
+
+  const detections = await getDetections(token, req.query)
+  return detections
 }
