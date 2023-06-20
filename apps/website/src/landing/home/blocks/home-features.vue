c44d45cb791979abb1e229c4b9c2fa5e0e35d02d
Somjintana Korbut <nuisomjin@gmail.com>
2023-06-20 23:11:11 +0100

    #1047 landing - styling feature section

--- apps/website/src/landing/home/blocks/home-features.vue
+++ apps/website/src/landing/home/blocks/home-features.vue
@@ -1,69 +1,73 @@
 <template>
-  <section class="bg-white dark:bg-steel-gray">
+  <section class="bg-white dark:bg-pitch">
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-24 lg:px-6">
       <div class="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
-        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-          Simple, yet powerful features
+        <span class="text-sm text-spoonbill mb-4 uppercase font-['Roboto Mono']">END-TO-END WORKFLOW</span>
+        <h2 class="my-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+          Intuitive & powerful features
         </h2>
       </div>
       <div class="text-center space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
-        <div class="flex flex-col items-center">
-          <div class="flex justify-center items-center mb-4 w-12 h-12 rounded-full bg-primary-100 lg:h-20 lg:w-20 dark:bg-primary">
-            <svg
-              class="w-6 h-6 text-primary-600 lg:w-12 lg:h-12 dark:text-primary-300"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            ><path
-              fill-rule="evenodd"
-              d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z"
-              clip-rule="evenodd"
-            /></svg>
+        <div
+          v-for="(item, index) in config"
+          :key="index"
+          class="flex flex-col items-left justify-start"
+        >
+          <div class="flex justify-start align-start mb-2 w-12 h-12 lg:h-18 lg:w-18">
+            <img :src="item.icon">
           </div>
-          <h3 class="mb-2 text-xl font-bold dark:text-white">
-            Powerful Algorithms
+          <h3 class="mb-2 text-md font-medium dark:text-insight text-left">
+            {{ item.title }}
           </h3>
-          <p class="max-w-72 text-gray-500 dark:text-gray-400">
-            Supercharge your biodiversity research with Arbimon's advanced algorithm.
-          </p>
-        </div>
-        <div class="flex flex-col items-center">
-          <div class="flex justify-center items-center mb-4 w-12 h-12 rounded-full bg-primary-100 lg:h-20 lg:w-20 dark:bg-primary">
-            <svg
-              class="w-6 h-6 text-primary-600 lg:w-12 lg:h-12 dark:text-primary-300"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            ><path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z" /></svg>
-          </div>
-          <h3 class="mb-2 text-xl font-bold dark:text-white">
-            Free Data Storage
-          </h3>
-          <p class="max-w-72 text-gray-500 dark:text-gray-400">
-            Save your recording data without worrying about storage costs with Arbimon's free storage.
-          </p>
-        </div>
-        <div class="flex flex-col items-center">
-          <div class="flex justify-center items-center mb-4 w-12 h-12 rounded-full bg-primary-100 lg:h-20 lg:w-20 dark:bg-primary">
-            <svg
-              class="w-6 h-6 text-primary-600 lg:w-12 lg:h-12 dark:text-primary-300"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            ><path
-              fill-rule="evenodd"
-              d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z"
-              clip-rule="evenodd"
-            /><path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z" /></svg>
-          </div>
-          <h3 class="mb-2 text-xl font-bold dark:text-white">
-            Multiple Users
-          </h3>
-          <p class="max-w-72 text-gray-500 dark:text-gray-400">
-            Collaborate with ease and boost your team's productivity with Arbimon's multiple user feature.
+          <p class="max-w-72 text-gray-500 dark:text-insight text-left">
+            {{ item.description }}
           </p>
         </div>
       </div>
     </div>
   </section>
 </template>
+
+<script setup lang="ts">
+
+import iconCloud from '@/_assets/landing/featured/fi_cloud.svg'
+import iconDollar from '@/_assets/landing/featured/fi_dollar-sign.svg'
+import iconHardDrive from '@/_assets/landing/featured/fi_hard-drive.svg'
+import iconInsight from '@/_assets/landing/featured/fi_insight.svg'
+import iconSettings from '@/_assets/landing/featured/fi_settings.svg'
+import iconUser from '@/_assets/landing/featured/fi_users.svg'
+
+const config = [
+  {
+    title: 'Powerful AI algorithms',
+    description: 'Supercharge your biodiversity research with Arbimon’s advanced AI algorithms',
+    icon: iconSettings
+  },
+  {
+    title: 'Unlimited data storage',
+    description: 'Upload, save, aand organize your recordings efficiently and without storage limits',
+    icon: iconHardDrive
+  },
+  {
+    title: 'Free',
+    description: 'We`re connecting research and action, and our platform is free to advance this mission',
+    icon: iconDollar
+  },
+  {
+    title: 'Unlimited users',
+    description: 'Collaborate with ease using Arbimon’s multi-user compatibility features',
+    icon: iconUser
+  },
+  {
+    title: 'Cloud computing',
+    description: 'Capitalize on the power of AI & cloud computing through our web-based platform',
+    icon: iconCloud
+  },
+  {
+    title: 'Automated insights',
+    description: 'Visualize, interpret, and share your data and biodiversity insights seamlessly',
+    icon: iconInsight
+  }
+]
+
+</script>
