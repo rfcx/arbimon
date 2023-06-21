34883a75dd9f869104a50db5d08adf3319fa8247
Somjintana Korbut <nuisomjin@gmail.com>
2023-06-21 15:15:08 +0100

    #1047 landing/testimonial - styling user feedback section

--- apps/website/src/landing/home/blocks/home-testimonials-partners.vue
+++ apps/website/src/landing/home/blocks/home-testimonials-partners.vue
@@ -1,44 +1,77 @@
 <template>
-  <section class="bg-white dark:bg-pitch">
-    <div class="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-24 lg:px-6">
-      <div class="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
-        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-frequency">
-          What our partners have to say
-        </h2>
-      </div>
-      <figure class="py-12 px-64 rounded-xl bg-gray-100 dark:bg-gray-300">
-        <svg
-          class="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600"
-          viewBox="0 0 24 27"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
+  <section class="bg-white dark:bg-pitch py-8 lg:py-24">
+    <div class="px-4 flex justify-between items-center lg:px-6">
+      <div>
+        <button
+          type="button"
+          @click="currentFeedbackIndex = (currentFeedbackIndex !== 0) ? currentFeedbackIndex - 1 : feedbacks.length - 1"
         >
-          <path
-            d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
-            fill="currentColor"
-          />
-        </svg>
-        <blockquote>
-          <p class="text-2xl text-gray-800 dark:text-gray-900">
-            "Arbimon has revolutionized the way we conduct acoustic research. With its user-friendly interface, we can now easily collect, analyze, and collaborate on biodiversity data to drive meaningful change and protect our planet's precious ecosystems."
+          <svg
+            aria-hidden="true"
+            class="w-8 h-8 text-frequency sm:w-6 sm:h-6 dark:text-frequency"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          ><path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15 19l-7-7 7-7"
+          /></svg>
+          <span class="sr-only">Previous</span>
+        </button>
+      </div>
+      <div class="flex gap-4">
+        <img
+          src="@/_assets/landing/testimonials/fi_quote.svg"
+          alt="Quote"
+          class="p-4 m-4"
+        >
+        <div class="max-w-screen-sm">
+          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-frequency">
+            What our partners have to say
+          </h2>
+          <p>
+            {{ feedbacks[currentFeedbackIndex].text }}
           </p>
-        </blockquote>
-        <figcaption class="flex items-center justify-center mt-6 space-x-3">
-          <img
-            class="w-12 h-12 rounded-full"
-            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png"
-            alt="profile picture"
-          >
-          <div class="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
-            <div class="pr-3 text-gray-800 dark:text-gray-900">
-              Andry Ford
-            </div>
-            <div class="pl-3 font-light text-gray-700 dark:text-gray-800">
-              Chief Scientist at Ikenfell
-            </div>
-          </div>
-        </figcaption>
-      </figure>
+        </div>
+      </div>
+      <div>
+        <button
+          type="button"
+          @click="currentFeedbackIndex = (currentFeedbackIndex + 1 !== feedbacks.length) ? currentFeedbackIndex + 1 : 0"
+        >
+          <svg
+            aria-hidden="true"
+            class="w-8 h-8 text-frequency sm:w-6 sm:h-6 dark:text-frequency"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          ><path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9 5l7 7-7 7"
+          /></svg>
+          <span class="sr-only">Next</span>
+        </button>
+      </div>
+    </div>
+    <!-- Slider indicators -->
+    <div class="flex space-x-3 -translate-x-1/2 bottom-5 left-1/2 p-10 m-auto justify-center">
+      <button
+        v-for="(feedback, index) in feedbacks"
+        :key="index"
+        type="button"
+        class="w-2 h-2 rounded-full border-frequency border-1"
+        :aria-current="currentFeedbackIndex === index"
+        :aria-label="'Feedback ' + (index + 1) + ' of ' + feedbacks.length"
+        :data-carousel-slide-to="index"
+        :class="{ 'bg-frequency': currentFeedbackIndex === index }"
+        @click="currentFeedbackIndex = index"
+      />
     </div>
   </section>
   <section class="bg-hero-testimonial bg-cover bg-no-repeat">
@@ -56,7 +89,6 @@
           <img
             :src="partner.imageUrl"
             :alt="partner.name"
-            class="hover:text-gray-900 dark:hover:text-white"
           >
         </a>
       </div>
@@ -66,6 +98,11 @@
 
 <script setup lang="ts">
 
+import { ref } from 'vue'
+
+import feedbacks from '@/_assets/landing/testimonials/feedbacks.json'
 import partners from '@/_assets/landing/testimonials/partners/data.json'
+
+const currentFeedbackIndex = ref<number>(0)
 
 </script>
