9829c840e0e17295140c44d84a91acb39e0c3870
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-06-21 16:08:11 +0700

    #1040 adapt api type to use correct method and same string case

--- apps/api/src/detect/cnn/detect-cnn-detections-handler.ts
+++ apps/api/src/detect/cnn/detect-cnn-detections-handler.ts
@@ -2,7 +2,7 @@ import { type RequestParamsDefault } from 'fastify'
 
 import { type DetectCnnDetectionsQueryParams, type DetectCnnDetectionsResponse } from '@rfcx-bio/common/api-bio/detect/detect-cnn-detections'
 
-import { getDetections } from '~/api-core/api-core'
+import { getDetectionsFromApi } from '~/api-core/api-core'
 import { isValidToken } from '~/api-helpers/is-valid-token'
 import { type Handler } from '~/api-helpers/types'
 import { BioUnauthorizedError } from '~/errors'
@@ -10,10 +10,11 @@ import { BioUnauthorizedError } from '~/errors'
 export const detectCnnDetectionsHandler: Handler<DetectCnnDetectionsResponse[], RequestParamsDefault, DetectCnnDetectionsQueryParams> = async (req): Promise<DetectCnnDetectionsResponse[]> => {
   const token = req.headers.authorization
 
+  // no token no data
   if (token === undefined || !isValidToken(token)) {
     throw BioUnauthorizedError()
   }
 
-  const detections = await getDetections(token, req.query)
+  const detections = await getDetectionsFromApi(token, req.query)
   return detections
 }
