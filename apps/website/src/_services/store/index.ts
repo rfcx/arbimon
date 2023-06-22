7e5876f1c8785f87a65b83361e0e69e2dc0527e5
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-06-22 11:40:35 +0700

    #1040 New store to sync values between 2 components, sync 2 components together.

--- apps/website/src/_services/store/index.ts
+++ apps/website/src/_services/store/index.ts
@@ -3,6 +3,7 @@ import { createPinia, defineStore } from 'pinia'
 
 import { type ProjectFiltersResponse, apiBioGetProjectFilters } from '@rfcx-bio/common/api-bio/project/project-filters'
 import { type LocationProjectForUser, apiBioGetProjects } from '@rfcx-bio/common/api-bio/project/projects'
+import { useCnnResultFilterStore } from './use-cnn-result-filter-store'
 import { getApiClient } from '@rfcx-bio/utils/api'
 
 import { getIdToken, useAuth0Client } from '~/auth-client'
@@ -60,3 +61,7 @@ export const useStore = defineStore('root', {
 export type BiodiversityStore = ReturnType<typeof useStore>
 export const pinia = createPinia()
 export const useStoreOutsideSetup = (): BiodiversityStore => useStore(pinia)
+
+export {
+  useCnnResultFilterStore
+}
--- apps/website/src/detect/cnn-job-detail/components/job-result-filter-modal.vue
+++ apps/website/src/detect/cnn-job-detail/components/job-result-filter-modal.vue
@@ -17,31 +17,69 @@
       <div class="text-md font-weight-bold mt-2">
         Threshold
         <el-slider
-          v-model="filterConfigs.threshold"
-          :format-tooltip="formatThreshold"
+          v-model="cnnResultFilterStore.filter.threshold"
+          :format-tooltip="cnnResultFilterStore.formatThreshold"
           size="small"
         />
       </div>
 
-      <div
-        v-for="filter in mobJobResultFilters"
-        :key="'job-result-filter-' + filter.label"
-        :label="filter.label"
-        class="mt-2"
-      >
+      <div class="job-result-filter-validation-status mt-2">
         <div class="text-md font-weight-bold">
-          {{ filter.label }}
+          Validation status
         </div>
-        <el-select
-          class="mt-2"
-          trigger="click"
-        >
+        <el-select class="mt-2" v-model="cnnResultFilterStore.filter.validationStatus">
           <el-option
-            v-for="(item, idx) in filter.items"
-            :key="'job-result-filter-item-' + filter.label + idx"
-            :label="item.label"
-            :value="item.value"
-          />
+            v-for="status in cnnResultFilterStore.validationStatusFilterOptions"
+            :key="status.value"
+            :label="status.label"
+            :value="status.value"
+          >
+          </el-option>
+        </el-select>
+      </div>
+
+      <div class="job-result-filter-taxon-class mt-2">
+        <div class="text-md font-weight-bold">
+          Class
+        </div>
+        <el-select class="mt-2" v-model="cnnResultFilterStore.filter.taxonClass">
+          <el-option
+            v-for="status in cnnResultFilterStore.classFilterOptions"
+            :key="status.value"
+            :label="status.label"
+            :value="status.value"
+          >
+          </el-option>
+        </el-select>
+      </div>
+
+      <div class="job-result-filter-sites mt-2">
+        <div class="text-md font-weight-bold">
+          Sites
+        </div>
+        <el-select class="mt-2" multiple collapse-tags v-model="cnnResultFilterStore.filter.siteIds">
+          <el-option
+            v-for="status in cnnResultFilterStore.sitesFilterOptions"
+            :key="status.value"
+            :label="status.label"
+            :value="status.value"
+          >
+          </el-option>
+        </el-select>
+      </div>
+
+      <div class="job-result-filter-sort-by mt-2">
+        <div class="text-md font-weight-bold">
+          Sort by
+        </div>
+        <el-select class="mt-2" v-model="cnnResultFilterStore.filter.sortBy">
+          <el-option
+            v-for="status in cnnResultFilterStore.sortByFilterOptions"
+            :key="status.value"
+            :label="status.label"
+            :value="status.value"
+          >
+          </el-option>
         </el-select>
       </div>
 
@@ -63,89 +101,12 @@
   </modal-popup>
 </template>
 <script setup lang="ts">
-import { reactive } from 'vue'
+import { useCnnResultFilterStore } from '~/store'
 
-import type { ValidationFilterConfig } from './types'
-
-const emit = defineEmits<{(e: 'emitClose'): void, (e: 'emitConfig', value: ValidationFilterConfig): void}>()
-const filterConfigs = reactive({
-  threshold: 50,
-  validationStatus: '',
-  taxonClass: '',
-  siteIds: [],
-  sortBy: ''
-})
+const cnnResultFilterStore = useCnnResultFilterStore()
+const emit = defineEmits<{(e: 'emitClose'): void}>()
 
 const confirmFilterConfig = () => {
-  emit('emitConfig', filterConfigs)
   emit('emitClose')
 }
-
-const formatThreshold = (val: number) => {
-  return val / 100
-}
-
-const mobJobResultFilters = [
-  {
-    label: 'Validation status',
-    items: [
-      {
-        label: 'All',
-        value: 3
-      },
-      {
-        label: 'Unvalidated',
-        value: 2
-      },
-      {
-        label: 'Present',
-        value: 1
-      },
-      {
-        label: 'Not present',
-        value: -1
-      },
-      {
-        label: 'Unknown',
-        value: 0
-      }
-    ]
-  },
-  {
-    label: 'Class',
-    items: [
-      {
-        label: 'Non-flying mammals',
-        value: 'mammals'
-      },
-      {
-        label: 'Birds',
-        value: 'birds'
-      }
-    ]
-  },
-  {
-    label: 'Site',
-    items: [
-      {
-        label: 'PPP',
-        value: 'ppp'
-      }
-    ]
-  },
-  {
-    label: 'Sort by score',
-    items: [
-      {
-        label: 'Low to high',
-        value: 'low to high'
-      },
-      {
-        label: 'High to low',
-        value: 'high to low'
-      }
-    ]
-  }
-]
-
 </script>
