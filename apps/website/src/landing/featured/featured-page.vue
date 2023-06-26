5f025e35af104bf8981f2fb068f9a03ae1239cd5
Somjintana Korbut <nuisomjin@gmail.com>
2023-06-26 12:42:21 +0100

    #1047 featured works - update theme & layout

--- apps/website/src/landing/featured/featured-page.vue
+++ apps/website/src/landing/featured/featured-page.vue
@@ -1,72 +1,89 @@
 <template>
-  <section class="bg-white dark:bg-mirage-gray">
-    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-24 lg:px-6">
-      <div class="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
-        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+  <section class="bg-white dark:bg-echo">
+    <div class="w-full h-64 lg:h-84 bg-hero-featured-works bg-cover bg-no-repeat bg-right border-b border-b-frequency">
+      <div class="max-w-screen-md p-8 md:p-20">
+        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-frequency">
           Featured Work
         </h2>
-        <p class="text-gray-500 sm:text-xl dark:text-gray-400">
+        <p class="text-gray-500 dark:text-insight">
           See how Arbimon have been applied in real-world scenarios to advance wildlife conservation and research efforts around the globe.
         </p>
       </div>
-      <div class="grid grid-cols-3 grid-flow-row gap-5">
-        <router-link
-          class="row-span-2"
-          :to="{ name: ROUTE_NAMES.landingFeaturedReforestation }"
-        >
-          <card
-            title="Reforestation"
-            subtitle="National Park of Malaysia"
-            background-image="https://images.unsplash.com/photo-1574068468668-a05a11f871da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80"
-          />
-        </router-link>
-        <router-link
-          class="col-span-2"
-          :to="{ name: ROUTE_NAMES.landingFeaturedEndangeredSpecies }"
-        >
-          <card
-            class="h-86"
-            title="Endangered Species Monitoring"
-            subtitle="Charles Darwin Foundation"
-            background-image="https://images.unsplash.com/photo-1542202229-7d93c33f5d07?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-          />
-        </router-link>
-        <div class="col-span-2">
-          <card
-            class="h-86"
-            title="Establish Biodiversity Baseline"
-            subtitle="WWF"
-            background-image="https://images.unsplash.com/photo-1440342359743-84fcb8c21f21?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-          />
-        </div>
-        <div class="col-span-2">
-          <card
-            class="h-72"
-            title="Wildlife Corridor Development"
-            subtitle="WeForest"
-            background-image="https://images.unsplash.com/photo-1567178395999-eaeb304311b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1600&q=80"
-          />
-        </div>
-        <div class="row-span-2">
-          <card
-            title="Verification Standard"
-            subtitle="XYZ Corporation"
-            background-image="https://images.unsplash.com/photo-1522598312049-70ccda16fe43?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-          />
-        </div>
-        <div class="col-span-2">
-          <card
-            class="h-72"
-            title="Climate Change Impact"
-            subtitle="Brazil National Park"
-            background-image="https://images.unsplash.com/photo-1533004597346-abf021591221?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1828&q=80"
-          />
-        </div>
+    </div>
+    <div class="py-8 px-2 lg:py-24 lg:px-4 flex flex-col md:flex-row">
+      <aside class="basis-1/4 sticky h-screen top-4">
+        <ul>
+          <li
+            v-for="item in contents"
+            :key="item.id"
+            class="p-2"
+          >
+            {{ item.category }}
+          </li>
+        </ul>
+      </aside>
+      <div class="md:basis-3/4 px-8 flex flex-col flex-nowrap flex-grow items-stretch gap-8 lg:px-32 overflow-y-auto">
+        <card
+          v-for="item in contents"
+          :key="item.id"
+          :title="item.title"
+          :subtitle="item.description"
+          :location="item.location"
+          :link="item.link"
+          :background-image="item.imageUrl"
+        />
       </div>
     </div>
   </section>
 </template>
 <script setup lang="ts">
-import { ROUTE_NAMES } from '~/router'
 import Card from './components/featured-card.vue'
+
+const contents = [
+  {
+    id: 1,
+    title: 'Establishing Biodiversity Baselines',
+    location: 'Gombe Stream National Park, Tanzania',
+    description: 'Developing baseline biodiversity metrics in partnership with the Jane Goodall Institute',
+    imageUrl: 'https://images.unsplash.com/photo-1440342359743-84fcb8c21f21?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+    category: 'Biodiversity Baselines',
+    link: '#'
+  },
+  {
+    id: 2,
+    title: 'Endangered Species Protection',
+    location: 'Isabela Island, Galapagos',
+    description: 'Monitoring the critically endangered mangrove finch',
+    imageUrl: 'https://images.unsplash.com/photo-1542202229-7d93c33f5d07?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+    category: 'Endangered Species',
+    link: '#'
+  },
+  {
+    id: 3,
+    title: 'Reforestation & Restoration',
+    location: 'Pontal do Paranapanema, Brazil',
+    description: 'Assessment of restoration sites in supporting native fauna',
+    imageUrl: 'https://images.unsplash.com/photo-1574068468668-a05a11f871da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80',
+    category: 'Restoration',
+    link: '#'
+  },
+  {
+    id: 4,
+    title: 'Climate Change Impact',
+    location: 'Puerto Rico island-wide',
+    description: 'Assessing animal communities’ responses to climate change',
+    imageUrl: 'https://images.unsplash.com/photo-1533004597346-abf021591221?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1828&q=80',
+    category: 'Climate Change Impact',
+    link: '#'
+  },
+  {
+    id: 5,
+    title: 'Sustainable Supply Chain Monitoring',
+    location: 'Borneo, Malaysia',
+    description: 'Species specific and soundscape approaches to map biodiversity levels across land use categories',
+    imageUrl: '',
+    category: 'Sustainable Supply Chain',
+    link: '#'
+  }
+]
 </script>
