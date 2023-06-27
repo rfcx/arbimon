67e29c9a44d0c2c1b7c0da26afcd70e1d99357c3
Somjintana Korbut <nuisomjin@gmail.com>
2023-06-27 14:00:23 +0100

    #1046 featured works - add detail page [WIP]

---
+++ apps/website/src/landing/featured-detail/blocks/content.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="bg-white dark:bg-moss">
+    <div class="py-8 px-4 mx-auto max-w-screen-lg xl:gap-16 sm:py-16 lg:px-6">
+      <p>{{ subheader }}</p>
+    </div>
+  </section>
+</template>
+
+<script setup type="ts">
+defineProps({
+  subheader: { type: String, default: '.....' }
+  })
+</script>
---
+++ apps/website/src/landing/featured-detail/blocks/feedback.vue
@@ -0,0 +1,105 @@
+<template>
+  <section class="bg-white dark:bg-moss py-8 lg:py-24">
+    <div class="px-4 flex justify-between items-center lg:px-6">
+      <div class="px-8 md:px-4">
+        <button
+          v-show="feedbacks.length > 1"
+          type="button"
+          @click="currentFeedbackIndex = (currentFeedbackIndex !== 0) ? currentFeedbackIndex - 1 : feedbacks.length - 1"
+        >
+          <svg
+            aria-hidden="true"
+            class="w-8 h-8 text-frequency sm:w-10 sm:h-10 dark:text-frequency"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          ><path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15 19l-7-7 7-7"
+          /></svg>
+          <span class="sr-only">Previous</span>
+        </button>
+      </div>
+      <div class="max-w-screen-sm">
+        <h2 class="text-bold text-frequency text-lg">
+          {{ feedbacks[currentFeedbackIndex].text }}
+        </h2>
+        <div class="mt-4 flex">
+          <div class="mr-4 mb-4 relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+            <span class="font-medium text-gray-600 dark:text-gray-300"> {{ feedbacks[currentFeedbackIndex].partner.name.charAt(0) }}</span>
+          </div>
+          <div class="flex flex-col">
+            <div class="mb-2">
+              <span class="mt-8 mb-4 mr-2 text-gray-900 dark:text-insight">
+                {{ feedbacks[currentFeedbackIndex].partner.name }}
+              </span>
+            </div>
+            <span class="text-gray-500 block text-sm dark:text-insight">
+              {{ feedbacks[currentFeedbackIndex].partner.title }}
+            </span>
+          </div>
+        </div>
+      </div>
+      <div class="px-8 md:px-4">
+        <button
+          v-show="feedbacks.length > 1"
+          type="button"
+          @click="currentFeedbackIndex = (currentFeedbackIndex + 1 !== feedbacks.length) ? currentFeedbackIndex + 1 : 0"
+        >
+          <svg
+            aria-hidden="true"
+            class="w-8 h-8 text-frequency sm:w-10 sm:h-10 dark:text-frequency"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          ><path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9 5l7 7-7 7"
+          /></svg>
+          <span class="sr-only">Next</span>
+        </button>
+      </div>
+    </div>
+    <!-- Slider indicators -->
+    <div
+      v-show="feedbacks.length > 1"
+      class="flex space-x-3 -translate-x-1/2 bottom-5 left-1/2 p-10 m-auto justify-center"
+    >
+      <button
+        v-for="(feedback, index) in feedbacks"
+        :key="index"
+        type="button"
+        class="w-2 h-2 rounded-full border-frequency border-1"
+        :aria-current="currentFeedbackIndex === index"
+        :aria-label="'Feedback ' + (index + 1) + ' of ' + feedbacks.length"
+        :data-carousel-slide-to="index"
+        :class="{ 'bg-frequency': currentFeedbackIndex === index }"
+        @click="currentFeedbackIndex = index"
+      />
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+
+import type { PropType } from 'vue'
+import { ref } from 'vue'
+
+import { type PartnerFeedback } from '../types'
+
+defineProps({
+  feedbacks: {
+    type: Array as PropType<PartnerFeedback[]>,
+    required: true
+  }
+})
+
+const currentFeedbackIndex = ref<number>(0)
+
+</script>
