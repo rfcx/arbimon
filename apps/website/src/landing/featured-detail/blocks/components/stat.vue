46ee986106d4f256cc779ba365fa2196b0169e8f
Somjintana Korbut <nuisomjin@gmail.com>
2023-06-28 10:37:12 +0100

    #1046 featured-work - support responsive, create reusable component for stat data

---
+++ apps/website/src/landing/featured-detail/blocks/components/stat.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="mb-2 text-xs uppercase text-spoonbill font-eyebrow">
+    {{ title }}
+  </div>
+  <p class="mb-4 text-gray-900 dark:text-insight">
+    <slot />
+  </p>
+</template>
+
+<script setup lang="ts">
+
+defineProps<{
+  readonly title: string
+}>()
+
+</script>
