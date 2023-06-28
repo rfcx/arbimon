42cf9bfbadad29f790ef49d0f31c8c06b7440984
Somjintana Korbut <nuisomjin@gmail.com>
2023-06-28 10:37:38 +0100

    #1046 featured-work - add more content

--- apps/website/src/landing/featured/data/index.ts
+++ apps/website/src/landing/featured/data/index.ts
@@ -91,31 +91,38 @@ export const projects: ProjectDetail[] = [
     },
     header: {
       title: 'Monitoring the critically endangered mangrove finch',
-      description: 'Arbimon makes it possible to establish holistic baselines of biodiversity presence in hotspots to improve our current and future understandings of ecosystem dynamics. ',
-      projectName: 'Develop baseline biodiversity metrics in Gombe Stream National Park',
+      description: 'Reforestation is occurring across global ecosystems to build corridors and restore habitats that have been lost. Ecoacoustics can track the return of native fauna, measure impact, and inform best practices.',
+      projectName: 'Assess restoration sites in supporting native fauna in Pontal do Paranapanema',
       applications: [
-        'High-accuracy CNN (>90%) is facilitating ongoing automated detection and monitoring of endangered species across the archipelago.',
-        'Results are being utilized to inform current finch distribution across all potential mangrove habitats to inform action.'
+        '178 species are being monitored across Atlantic Forest restoration sites to track change year-over-year',
+        'Results have already proven reforested sites are host to a similar number of bird species as protected sites and impact is shown in as little as five years'
       ],
-      timeline: '2022 - ongoing',
+      timeline: '2021 - ongoing',
       scope: '34 Sampling sites, 2 Species monitored',
-      partners: ['Charles Darwin Foundation'],
-      services: ['Survey design', 'Expert species validation', 'Ecological analyses', 'AI modeled development'],
-      sdgs: ['15']
+      partners: ['IPÊ & WeForest'],
+      services: [
+        'Survey design',
+        'Species manual validation and identification',
+        'Soundscape analysis',
+        'Species richness analyses',
+        'Species occupancy analyses',
+        'Species composition analyses'
+      ],
+      sdgs: ['13', '15']
     },
     content: {
-      subtitle: 'Partnering with the Charles Darwin Foundation, Arbimon is facilitating important work to understand where the Critically Endangered mangrove finch is present using automated detections of their calls.',
+      subtitle: 'Partnering with WeForest & IPÊ, Arbimon is facilitating the analysis of biodiversity across reforested regions in Pontal.',
       paragraphs: [
-        'The Mangrove Finch (Camarhynchus heliobates) is one of the 14 species of Darwin\'s finches that only live in the Galapagos Islands. It is the rarest bird of the archipelago, with an estimated population of 100 individuals that inhabit only 30 hectares in two areas on Isabela Island. In collaboration with Rainforest Connection, we supported this project by providing acoustic devices installed in 34 sites where the mangrove Finch persists.',
-        'Using Arbimon tools, we detected mangrove species in 26 sites. In addition, we have also detected the woodpecker finch (Vulnerable, IUCN Red List) in 34 sites. We have trained an AI model with precision of 0.78-0.98 to automatically detect both species in raw soundscape recordings to facilitate the detection, monitoring, protection, and recovery of these threatened birds.'
+        'The remaining 12% of the Atlantic Forest is still among the most biologically rich and diverse forests in the world and exhibits a high number of species that can be found nowhere else on Earth. WeForest and IPÊ have been collaborating to reconnect the forest patches in a fragmented landscape in São Paulo, Brazil, creating more habitat for animals such as the endangered black lion tamarin. The main goal of this study is to establish a comprehensive understanding of how restoration sites affect species distribution, composition, and richness.',
+        'Therefore, we aim to evaluate the performance of restoration sites to enhance and protect biodiversity. More specifically, we aim to test if restoration sites are functioning as wildlife corridors by assessing spatial patterns of biodiversity, at both the species- and community-level, in a fragmented landscape in the Atlantic Forest in Brazil.'
       ]
     },
     gallery: {
       images: []
     },
     feedback: [{
-      text: 'The Arbimon platform has enabled us to use finch vocalizations to track their presence at time scales previously inaccessible. We essentially have tens of “researchers” stationed across Galapagos mangroves permanently listening for the presence of one of the world’s most range-restricted birds.',
-      partnerName: 'XXX, Charles Darwin Foundation'
+      text: 'Arbimon has provided valuable insights into how WeForest reforestation is impacting the ecosystem. Additionally, it creates an easily accessible transparency dashboard that can be utilized by a variety of stakeholders.',
+      partnerName: 'XXX, WeForest'
     }]
   },
   {
@@ -129,32 +136,42 @@ export const projects: ProjectDetail[] = [
       name: 'Climate Change Impact'
     },
     header: {
-      title: 'Monitoring the critically endangered mangrove finch',
-      description: 'Arbimon makes it possible to establish holistic baselines of biodiversity presence in hotspots to improve our current and future understandings of ecosystem dynamics. ',
+      title: 'Climate Change Impact',
+      description: 'Climate change is having significant impacts on global biodiversity patterns and extinction rates. Ecoacoustic monitoring allows us to understand trends at both the species and ecosystem levels to create proactive conservation plans.',
       projectName: 'Develop baseline biodiversity metrics in Gombe Stream National Park',
       applications: [
-        'High-accuracy CNN (>90%) is facilitating ongoing automated detection and monitoring of endangered species across the archipelago.',
-        'Results are being utilized to inform current finch distribution across all potential mangrove habitats to inform action.'
+        'Island-wide maps of biodiversity presence across Puerto Rico over time',
+        'CNN for ~40 species to automate the detection of species with a focus on threatened and endemic species'
       ],
-      timeline: '2022 - ongoing',
-      scope: '34 Sampling sites, 2 Species monitored',
-      partners: ['Charles Darwin Foundation'],
-      services: ['Survey design', 'Expert species validation', 'Ecological analyses', 'AI modeled development'],
-      sdgs: ['15']
+      timeline: '2021 - ongoing',
+      scope: '944 Sites, 96 Species',
+      partners: [
+        'United State Fish & Wildlife Services',
+        'Departamento de Recursos Naturales y Ambientales de Puerto Rico, Para La Naturaleza'
+      ],
+      services: [
+        'Hardware provider',
+        'Species List',
+        'Expert species validations',
+        'Multi-species occupancy models',
+        'AI model development',
+        'Soundscape Analysis'
+      ],
+      sdgs: ['13', '15']
     },
     content: {
-      subtitle: 'Partnering with the Charles Darwin Foundation, Arbimon is facilitating important work to understand where the Critically Endangered mangrove finch is present using automated detections of their calls.',
+      subtitle: 'Puerto Rico\'s biodiversity is under constant threat from both natural (i.e., hurricanes) and human (i.e., urbanization, climate change) disturbances. Unfortunately, we historically have not had good information about the status of many species in Puerto Rico, nor how their population fluctuates and responds according to natural and human disturbances.',
       paragraphs: [
-        'The Mangrove Finch (Camarhynchus heliobates) is one of the 14 species of Darwin\'s finches that only live in the Galapagos Islands. It is the rarest bird of the archipelago, with an estimated population of 100 individuals that inhabit only 30 hectares in two areas on Isabela Island. In collaboration with Rainforest Connection, we supported this project by providing acoustic devices installed in 34 sites where the mangrove Finch persists.',
-        'Using Arbimon tools, we detected mangrove species in 26 sites. In addition, we have also detected the woodpecker finch (Vulnerable, IUCN Red List) in 34 sites. We have trained an AI model with precision of 0.78-0.98 to automatically detect both species in raw soundscape recordings to facilitate the detection, monitoring, protection, and recovery of these threatened birds.'
+        'To improve knowledge about the distribution of bird, amphibian, and mammal species that occur in Puerto Rico, Arbimon, in partnership with the US Fish and Wildlife Service, Departamento de Recursos Naturales y Ambientales de Puerto Rico, and the Para la Naturaleza Foundation conducted an island-wide survey using ecoacoustic monitoring. The survey focused on the lowlands and coastal areas on the main island of Puerto Rico, a selection of elevational gradients in the mountainous regions, and the major offshore islands of Culebra, Desecheo, Mona and Vieques. Acoustic recorders and the analytical tools in Arbimon have been used to collect data from over 900 sites across the archipelago yearly during the three month peak of bird breeding season.',
+        'This project aims to evaluate climate change\'s impact on species\' spatial distribution in a tropical island ecosystem using a decade of acoustic monitoring data. Our results indicate that the distributions of several bird and frog species are positively affected by precipitation, and the proportion of canopy cover and species are shifting toward high-elevation areas due to climate change. The study also found that drought has a stronger negative effect on bird communities than a hurricane of category 4. In addition, the results indicate that there may need to be more than the present amount of protected areas to safeguard bird species under climate change.'
       ]
     },
     gallery: {
       images: []
     },
     feedback: [{
-      text: 'The Arbimon platform has enabled us to use finch vocalizations to track their presence at time scales previously inaccessible. We essentially have tens of “researchers” stationed across Galapagos mangroves permanently listening for the presence of one of the world’s most range-restricted birds.',
-      partnerName: 'XXX, Charles Darwin Foundation'
+      text: 'With Arbimon, we have created the first-ever standardized island-wide maps of biodiversity presence using data from across the island. We are hopeful in how this will inform our ability to fight extinction across this important archipelago.',
+      partnerName: 'XXX, USFWS'
     }]
   },
   {
@@ -168,32 +185,43 @@ export const projects: ProjectDetail[] = [
       name: 'Sustainable Supply Chain'
     },
     header: {
-      title: 'Monitoring the critically endangered mangrove finch',
-      description: 'Arbimon makes it possible to establish holistic baselines of biodiversity presence in hotspots to improve our current and future understandings of ecosystem dynamics. ',
-      projectName: 'Develop baseline biodiversity metrics in Gombe Stream National Park',
+      title: 'Sustainable Supply Chain Monitoring',
+      description: 'As we transition away from monoculture plantations and improve agricultural practices, it is important to understand which approaches are most effective for creating positive change. Ecoacoustic monitoring makes it possible to track health progress across agricultural and agroforestry sites.',
+      projectName: 'Comparing biodiversity levels in forests, restoration sites, and oil palm plantations to inform sustainable agricultural systems',
       applications: [
-        'High-accuracy CNN (>90%) is facilitating ongoing automated detection and monitoring of endangered species across the archipelago.',
-        'Results are being utilized to inform current finch distribution across all potential mangrove habitats to inform action.'
+        'Comparing biodiversity levels in forests, restoration sites, and oil palm plantations to inform sustainable agricultural systems'
       ],
-      timeline: '2022 - ongoing',
-      scope: '34 Sampling sites, 2 Species monitored',
-      partners: ['Charles Darwin Foundation'],
-      services: ['Survey design', 'Expert species validation', 'Ecological analyses', 'AI modeled development'],
-      sdgs: ['15']
+      timeline: '2021 - ongoing',
+      scope: '52 Sampling sites, 103 Species monitored',
+      partners: ['Ecoculture'],
+      services: [
+        'Survey design',
+        'Expert manual species validation and labeling',
+        'Statistical and ecological analyses',
+        'Species Distribution Modeling',
+        // '- Bayesian Multi-species Occupancy Model/ Species distribution models',
+        // '- Dynamic Occupancy Models (temporal trend analyses)',
+        // '- Vocal Activity Patterns',
+        'Soundscape analyses',
+        // '- Acoustic Space Use',
+        // '- Soundscape composition analyses',
+        'Science outreach and communication'
+      ],
+      sdgs: []
     },
     content: {
-      subtitle: 'Partnering with the Charles Darwin Foundation, Arbimon is facilitating important work to understand where the Critically Endangered mangrove finch is present using automated detections of their calls.',
+      subtitle: '',
       paragraphs: [
-        'The Mangrove Finch (Camarhynchus heliobates) is one of the 14 species of Darwin\'s finches that only live in the Galapagos Islands. It is the rarest bird of the archipelago, with an estimated population of 100 individuals that inhabit only 30 hectares in two areas on Isabela Island. In collaboration with Rainforest Connection, we supported this project by providing acoustic devices installed in 34 sites where the mangrove Finch persists.',
-        'Using Arbimon tools, we detected mangrove species in 26 sites. In addition, we have also detected the woodpecker finch (Vulnerable, IUCN Red List) in 34 sites. We have trained an AI model with precision of 0.78-0.98 to automatically detect both species in raw soundscape recordings to facilitate the detection, monitoring, protection, and recovery of these threatened birds.'
+        'In collaboration with Ecoculture, this project aims to develop and implement community-led landscape restoration and management approaches to achieve meaningful environmental and social benefits.',
+        'This study will support sustainable agricultural systems by comparing biodiversity levels in primary forests, restoration sites, oil palm plantations, and invasive eucalyptus trees. Acoustic recorders were deployed at 52 sites in the Sabah region of Borneo, and recordings were analyzed using Arbimon. The study found that the number of bird species and soundscapes in primary forests and restoration sites is significantly higher than in oil palm and eucalyptus sites. In addition, restoration sites were found to sustain biodiversity levels comparable to pristine areas, suggesting that sustainable agricultural practices such as agroforestry and regenerative agriculture can help sustain biodiversity.'
       ]
     },
     gallery: {
       images: []
     },
     feedback: [{
-      text: 'The Arbimon platform has enabled us to use finch vocalizations to track their presence at time scales previously inaccessible. We essentially have tens of “researchers” stationed across Galapagos mangroves permanently listening for the presence of one of the world’s most range-restricted birds.',
-      partnerName: 'XXX, Charles Darwin Foundation'
+      text: 'Arbimon has made it possible to holistically track species presence in a way we have not been able to before. This data is extremely valuable to inform how we prioritize local initiatives to support biodiversity moving forward',
+      partnerName: 'XXX, Ecoculture'
     }]
   }
 ]
