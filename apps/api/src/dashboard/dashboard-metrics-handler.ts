c229f603386d9c72a77465726d696473268804e4
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-07-03 15:26:11 +0700

    parallelizing the metrics dao handler, fix return types

--- apps/api/src/dashboard/dashboard-metrics-handler.ts
+++ apps/api/src/dashboard/dashboard-metrics-handler.ts
@@ -1,12 +1,11 @@
-import { type DashboardMetricsParams } from '@rfcx-bio/common/api-bio/dashboard/dashboard-metrics'
-import { type LocationProjectMetric } from '@rfcx-bio/common/dao/types/location-project-metric'
+import { type DashboardMetricsParams, type DashboardMetricsResponse } from '@rfcx-bio/common/api-bio/dashboard/dashboard-metrics'
 
 import { type Handler } from '~/api-helpers/types'
 import { BioInvalidPathParamError } from '~/errors'
 import { assertPathParamsExist } from '~/validation'
 import { getProjectMetrics } from './dashboard-metrics-dao'
 
-export const dashboardMetricsHandler: Handler<Omit<LocationProjectMetric, 'locationProjectId'> & { siteCount: number }, DashboardMetricsParams> = async (req) => {
+export const dashboardMetricsHandler: Handler<DashboardMetricsResponse, DashboardMetricsParams> = async (req) => {
   // Inputs & validation
   const { projectId } = req.params
   assertPathParamsExist({ projectId })
