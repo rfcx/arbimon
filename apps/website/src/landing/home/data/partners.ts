e8ed6c66b57c6b8fac05519b8d6ae21439fe5add
Somjintana Korbut <nuisomjin@gmail.com>
2023-07-05 11:20:17 +0100

    #1047 fix images disappear

---
+++ apps/website/src/landing/home/data/partners.ts
@@ -0,0 +1,291 @@
+import aspirationIcon from '@/_assets/landing/testimonials/partners/Aspiration.png'
+import awakeEcotourismIcon from '@/_assets/landing/testimonials/partners/Awake Ecotourism.png'
+import biolowiezaNationalParkIcon from '@/_assets/landing/testimonials/partners/Biolowieza National Park.png'
+import birdLifeInternationalIcon from '@/_assets/landing/testimonials/partners/BirdLife International.png'
+import birdsCaribbeanIcon from '@/_assets/landing/testimonials/partners/Birds Caribbean.png'
+import burungIndonesiaIcon from '@/_assets/landing/testimonials/partners/Burung Indonesia.png'
+import cMindsIcon from '@/_assets/landing/testimonials/partners/C-Minds.png'
+import centerForWildlifeStudiesIcon from '@/_assets/landing/testimonials/partners/Center for Wildlife Studies.png'
+import charlesDarwinFoundationIcon from '@/_assets/landing/testimonials/partners/Charles Darwin Foundation.png'
+import danuGirangFieldCenterIcon from '@/_assets/landing/testimonials/partners/Danu Girang Field Center.png'
+import deepEnergiesIcon from '@/_assets/landing/testimonials/partners/Deep Energies.png'
+import drnaIcon from '@/_assets/landing/testimonials/partners/DRNA.png'
+import ecoCultureIcon from '@/_assets/landing/testimonials/partners/EcoCulture.png'
+import eticaEnLosBosquesIcon from '@/_assets/landing/testimonials/partners/Etica en los Bosques.png'
+import fazosIcon from '@/_assets/landing/testimonials/partners/FAZOS.png'
+import finnishGeospatialInstituteIcon from '@/_assets/landing/testimonials/partners/Finnish Geospatial Institute.png'
+import fundacionBiodiversaColombiaIcon from '@/_assets/landing/testimonials/partners/Fundacion Biodiversa Colombia.png'
+import googleOrgIcon from '@/_assets/landing/testimonials/partners/Google.org.png'
+import healthInHarmonyIcon from '@/_assets/landing/testimonials/partners/Health in Harmony.png'
+import huaweiIcon from '@/_assets/landing/testimonials/partners/Huawei.png'
+import hutanIcon from '@/_assets/landing/testimonials/partners/HUTAN.png'
+import institutoDePesquisasIcon from '@/_assets/landing/testimonials/partners/Instituto de Pesquisas Ecológicas.png'
+import institutoHumboldtIcon from '@/_assets/landing/testimonials/partners/Instituto Humboldt.png'
+import interAmeIcon from '@/_assets/landing/testimonials/partners/Interamerican University Puerto Rico.png'
+import janeGoodallInstituteIcon from '@/_assets/landing/testimonials/partners/Jane Goodall Institute.png'
+import jocotocoFoundationIcon from '@/_assets/landing/testimonials/partners/Jocotoco Foundation.png'
+import juruaIcon from '@/_assets/landing/testimonials/partners/Jurua Institute.png'
+import kkiWarsiIcon from '@/_assets/landing/testimonials/partners/KKI Warsi.png'
+import mammalSocietyIcon from '@/_assets/landing/testimonials/partners/Mammal Society.png'
+import mashpiIcon from '@/_assets/landing/testimonials/partners/Mashpi Reserve.png'
+import neusiedlerIcon from '@/_assets/landing/testimonials/partners/Neusiedler see Seewinkel National Park.png'
+import northCarolinaIcon from '@/_assets/landing/testimonials/partners/North Carolina State University.png'
+import o2Icon from '@/_assets/landing/testimonials/partners/O2 Reserve.png'
+import ojojaguarIcon from '@/_assets/landing/testimonials/partners/Ojojaguar.png'
+import paraLaIcon from '@/_assets/landing/testimonials/partners/Para la Naturaleza.png'
+import partnerForestIcon from '@/_assets/landing/testimonials/partners/Partnerships for Forests.png'
+import rspbIcon from '@/_assets/landing/testimonials/partners/RSPB.png'
+import rwandanDevIcon from '@/_assets/landing/testimonials/partners/Rwandan Development Board.png'
+import southAfricanIcon from '@/_assets/landing/testimonials/partners/South African Biodiversity Institute.png'
+import sumatraIcon from '@/_assets/landing/testimonials/partners/Sumatra Ranger Project.png'
+import thirdMilleniumIcon from '@/_assets/landing/testimonials/partners/Third Millenium Alliance.png'
+import treesForFutureIcon from '@/_assets/landing/testimonials/partners/Trees for the Future.png'
+import usFishIcon from '@/_assets/landing/testimonials/partners/U.S. Fish and Wildlife Service.png'
+import uniBritrolIcon from '@/_assets/landing/testimonials/partners/University of Bristol.png'
+import uniStAndrewsIcon from '@/_assets/landing/testimonials/partners/University of St Andrews.png'
+import weforestIcon from '@/_assets/landing/testimonials/partners/WeForest.png'
+import wildlifeConservationSocietyIcon from '@/_assets/landing/testimonials/partners/Wildlife Conservation Society.png'
+import worldWildlifeFundIcon from '@/_assets/landing/testimonials/partners/World Wildlife Fund.png'
+
+export const partners = [
+  {
+    name: 'Aspiration',
+    imageUrl: aspirationIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Awake Ecotourism',
+    imageUrl: awakeEcotourismIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Biolowieza National Park',
+    imageUrl: biolowiezaNationalParkIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'BirdLife International',
+    imageUrl: birdLifeInternationalIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Birds Caribbean',
+    imageUrl: birdsCaribbeanIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Burung Indonesia',
+    imageUrl: burungIndonesiaIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'C-Minds',
+    imageUrl: cMindsIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Center for Wildlife Studies',
+    imageUrl: centerForWildlifeStudiesIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Charles Darwin Foundation',
+    imageUrl: charlesDarwinFoundationIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'DRNA',
+    imageUrl: drnaIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Danu Girang Field Center',
+    imageUrl: danuGirangFieldCenterIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Deep Energies',
+    imageUrl: deepEnergiesIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'EcoCulture',
+    imageUrl: ecoCultureIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Etica en los Bosques',
+    imageUrl: eticaEnLosBosquesIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'FAZOS',
+    imageUrl: fazosIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Finnish Geospatial Institute',
+    imageUrl: finnishGeospatialInstituteIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Fundacion Biodiversa Colombia',
+    imageUrl: fundacionBiodiversaColombiaIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Google.org',
+    imageUrl: googleOrgIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'HUTAN',
+    imageUrl: hutanIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Health in Harmony',
+    imageUrl: healthInHarmonyIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Huawei',
+    imageUrl: huaweiIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Instituto Humboldt',
+    imageUrl: institutoHumboldtIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Instituto de Pesquisas Ecológicas',
+    imageUrl: institutoDePesquisasIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Interamerican University Puerto Rico',
+    imageUrl: interAmeIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Jane Goodall Institute',
+    imageUrl: janeGoodallInstituteIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Jocotoco Foundation',
+    imageUrl: jocotocoFoundationIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Jurua Institute',
+    imageUrl: juruaIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'KKI Warsi',
+    imageUrl: kkiWarsiIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Mammal Society',
+    imageUrl: mammalSocietyIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Mashpi Reserve',
+    imageUrl: mashpiIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Neusiedler see Seewinkel National Park',
+    imageUrl: neusiedlerIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'North Carolina State University',
+    imageUrl: northCarolinaIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'O2 Reserve',
+    imageUrl: o2Icon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Ojojaguar',
+    imageUrl: ojojaguarIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Para la Naturaleza',
+    imageUrl: paraLaIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Partnerships for Forests',
+    imageUrl: partnerForestIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'RSPB',
+    imageUrl: rspbIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Rwandan Development Board',
+    imageUrl: rwandanDevIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'South African Biodiversity Institute',
+    imageUrl: southAfricanIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Sumatra Ranger Project',
+    imageUrl: sumatraIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Third Millenium Alliance',
+    imageUrl: thirdMilleniumIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Trees for the Future',
+    imageUrl: treesForFutureIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'U.S. Fish and Wildlife Service',
+    imageUrl: usFishIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'University of Bristol',
+    imageUrl: uniBritrolIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'University of St Andrews',
+    imageUrl: uniStAndrewsIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'WeForest',
+    imageUrl: weforestIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'Wildlife Conservation Society',
+    imageUrl: wildlifeConservationSocietyIcon,
+    linkUrl: '#'
+  },
+  {
+    name: 'World Wildlife Fund',
+    imageUrl: worldWildlifeFundIcon,
+    linkUrl: '#'
+  }
+]
