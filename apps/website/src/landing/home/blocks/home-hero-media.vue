475ebf741188530d4629a2fb05436ca181c18d8d
Somjintana Korbut <nuisomjin@gmail.com>
2023-07-06 13:28:24 +0100

    #1063 landing/hero - add ability to play sound

--- apps/website/src/landing/home/blocks/home-hero-media.vue
+++ apps/website/src/landing/home/blocks/home-hero-media.vue
@@ -7,7 +7,7 @@
       :alt="items[currentItemIndex].title"
     >
     <div class="absolute bottom-4 w-100 h-48 flex flex-col items-center gap-2 border-1 px-2 py-4 border-chirp rounded-lg bg-black bg-opacity-75">
-      <media-player />
+      <media-player :src="items[currentItemIndex].audioUrl" />
       <div class="text-center shrink-0 h-20 px-2">
         <p>{{ items[currentItemIndex].title }}</p>
         <p class="text-sm">
@@ -51,7 +51,10 @@ import { ref } from 'vue'
 
 import birdImage from '@/_assets/default-species-image.jpg'
 import frogImage from '@/_assets/landing/media-player/ameerega-trivittata.png'
-import monkeyImage from '@/_assets/landing/media-player/psilopogon-pyrolophus.png'
+import frogSound from '@/_assets/landing/media-player/ameerega-trivittata.wav'
+import monkeyImage from '@/_assets/landing/media-player/hylobates-agilis.png'
+import monkeySound from '@/_assets/landing/media-player/hylobates-agilis.wav'
+import birdSound from '@/_assets/landing/media-player/psilopogon-pyrolophus.wav'
 import MediaPlayer from '../components/media-player.vue'
 import { type MediaHeroContent } from '../data/type'
 
@@ -62,21 +65,22 @@ const items = ref<Array<MediaHeroContent>>([
     title: 'Agile Gibbon (Hylobates agilis)',
     description: 'Endangered (IUCN RedList) Detected 2488 times in 21 sites in 1 country',
     imageUrl: monkeyImage,
-    audioUrl: ''
+    audioUrl: monkeySound
   },
   {
     id: 2,
     title: 'Three-striped Poison Frog (Ameerega trivittata)',
     description: 'Detected 34 times in 12 sites and 2 countries',
     imageUrl: frogImage,
-    audioUrl: ''
+    audioUrl: frogSound
   },
   {
     id: 3,
     title: 'Fire-tufted Barbet (Psilopogon pyrolophus)',
     description: 'Detected 123 times in 1 site and 1 country',
     imageUrl: birdImage,
-    audioUrl: ''
+    audioUrl: birdSound
   }
 ])
+
 </script>
--- apps/website/src/landing/home/components/media-player.vue
+++ apps/website/src/landing/home/components/media-player.vue
@@ -1,54 +1,117 @@
 <!-- eslint-disable vue/html-self-closing -->
 <template>
-  <div class="flex flex-row gap-4">
-    <div ref="soundbar"></div>
+  <div class="flex flex-row gap-4 items-center h-14">
     <button
       type="button"
+      class="w-8"
       @click="togglePlay"
     >
+      <span v-if="isLoading">.</span>
       <img
-        v-if="!isPlaying"
-        src="@/_assets/landing/media-player/fi_volume-play.svg"
+        v-else-if="!isPlaying"
+        src="@/_assets/landing/media-player/fi_play.svg"
         alt="Play"
       />
       <img
         v-else
-        src="@/_assets/landing/media-player/fi_volume-mute.svg"
+        src="@/_assets/landing/media-player/fi_pause.svg"
         alt="Mute"
       />
     </button>
+    <div class="w-48">
+      <div
+        ref="soundbar"
+        :class="isPlaying ? 'block' : 'hidden'"
+      ></div>
+      <div
+        class="w-48 flex h-14 items-center"
+        :class="!isPlaying ? 'block' : 'hidden'"
+      >
+        <hr
+          class="w-48 mb-3 h-0.3 border-0 bg-frequency"
+        >
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+
+import { Howl } from 'howler'
 import SiriWave from 'siriwave'
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
+
+const sound = ref<Howl | null>(null)
+
+const props = defineProps<{
+  src: string
+}>()
 
 const soundbar = ref<HTMLElement | null>(null)
 const siriWave = ref<SiriWave | null>(null)
 const isPlaying = ref(false)
+const isLoading = ref(false)
 
 onMounted(() => {
+  setupSoundBar()
+})
+
+const setupSoundBar = () => {
   siriWave.value = new SiriWave({
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     container: soundbar.value!,
-    width: 200,
-    height: 40,
+    width: 192,
+    height: 54.5,
     autostart: true,
     speed: 0.2,
     amplitude: 1,
-    color: '#ADFF2C'
+    color: '#ADFF2C',
+    curveDefinition: [
+      { attenuation: -2, lineWidth: 0, opacity: 0 },
+      { attenuation: -6, lineWidth: 0, opacity: 0 },
+      { attenuation: 4, lineWidth: 0, opacity: 0 },
+      { attenuation: 2, lineWidth: 0, opacity: 0 },
+      { attenuation: 1, lineWidth: 2, opacity: 1 }
+    ]
   })
-  togglePlay()
-})
+  siriWave.value?.stop() // stop the animation to match the audio behavior
+}
+
+const setupSoundPlayer = (src: string) => {
+  isLoading.value = true
+  siriWave.value?.stop()
+  if (sound.value) {
+    sound.value?.unload()
+    sound.value = null
+  }
+  sound.value = new Howl({
+    src,
+    loop: true,
+    preload: true,
+    onload: () => {
+      isLoading.value = false
+    }
+  })
+}
 
 const togglePlay = () => {
   isPlaying.value = !isPlaying.value
   if (isPlaying.value) {
+    if (!sound.value) {
+      setupSoundPlayer(props.src)
+    }
+    sound.value?.play()
     siriWave.value?.start()
   } else {
+    sound.value?.pause()
     siriWave.value?.stop()
   }
 }
+
+watch(() => props.src, () => {
+  isPlaying.value = false
+  siriWave.value?.stop()
+  setupSoundPlayer(props.src)
+ })
 
 </script>
