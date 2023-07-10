583ef078b54b9e7a95b2deaf63a750954cd54f82
Somjintana Korbut <nuisomjin@gmail.com>
2023-07-11 00:51:45 +0100

    #1063 Landing - add data stat section

---
+++ apps/website/src/landing/home/blocks/home-data-stat.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="bg-white dark:bg-pitch border-y-1 border-frequency">
+    <div class="grid grid-row-2 lg:(grid-cols-2 px-20 py-10) px-4 py-8 gap-10 max-w-screen-xl mx-auto">
+      <div>
+        <h6>Powered by data</h6>
+        <p>Arbimon’s archive is continuously growing!</p>
+      </div>
+      <div class="grid grid-cols-3">
+        <number-stat
+          :number="14000"
+          title="Recordings uploaded"
+        />
+        <number-stat
+          :number="6300"
+          title="Analyses Performed"
+        />
+        <number-stat
+          :number="134"
+          title="Projects Created"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import NumberStat from '../components/number-stat.vue'
+</script>
---
+++ apps/website/src/landing/home/components/number-stat.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="text-center">
+    <h1 class="text-flamingo">
+      {{ valueShortScale }}
+    </h1>
+    <p class="text-sm">
+      {{ props.title }}
+    </p>
+  </section>
+</template>
+
+<script setup lang="ts">
+
+import numeral from 'numeral'
+import { computed } from 'vue'
+
+const props = defineProps<{
+  title: string
+  number: number
+}>()
+
+const valueShortScale = computed(() => numeral(props.number).format('0a'))
+
+</script>
