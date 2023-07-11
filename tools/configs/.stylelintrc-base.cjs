5fe7197b06e39309dbde0b8ecaa14a9bcf1245b4
Somjintana Korbut <nuisomjin@gmail.com>
2023-07-11 10:13:07 +0100

    update stylelint rules

--- tools/configs/.stylelintrc-base.cjs
+++ tools/configs/.stylelintrc-base.cjs
@@ -13,7 +13,7 @@ module.exports = {
       }
     ],
     'at-rule-empty-line-before': ['always', {
-      except: ['after-same-name', 'inside-block'],
+      except: ['blockless-after-same-name-blockless', 'blockless-after-blockless', 'inside-block'],
       ignore: ['after-comment', 'first-nested']
     }]
   }
