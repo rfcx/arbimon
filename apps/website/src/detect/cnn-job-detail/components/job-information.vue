6f3e5c45da2c457dd54d4e4d17b28e298d3b64a1
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-07-14 17:59:25 +0700

    #998 prepare headings of validation part

--- apps/website/src/detect/cnn-job-detail/components/job-information.vue
+++ apps/website/src/detect/cnn-job-detail/components/job-information.vue
@@ -22,17 +22,47 @@
             2022/02/20 - 2022/02/21
           </h2>
           <icon-fas-clock class="block m-auto" />
-          <h1 class="text-lg">
+          <h2 class="text-lg">
             Diurnal
-          </h1>
+          </h2>
         </div>
       </div>
       <div>
         <span class="text-subtle">Status</span>
-        <jobInformationStatus :variant="0" />
+        <div
+          id="cnn-job-information-status"
+          class="grid grid-rows-3"
+        >
+          <div class="my-auto w-6">
+            <jobInformationStatus :variant="0" />
+          </div>
+          <h2 class="text-lg">
+            Queued
+          </h2>
+          <el-progress
+            class="col-span-2"
+            :stroke-width="6"
+            :precentage="50"
+          />
+          <div class="flex flex-row col-span-2">
+            <icon-fa-calendar class="block mr-2" />
+            <h3 class="text-md text-subtle">
+              Jun 29, 2022 13:24 - Tomaz
+            </h3>
+          </div>
+        </div>
       </div>
       <div>
         <span class="text-subtle">Validation Status</span>
+        <h2 class="text-lg">
+          1/1,413
+        </h2>
+        <div class="flex flex-row mt-2">
+          <icon-fa-calendar class="block mr-2" />
+          <h3 class="text-md text-subtle">
+            Rreviewed 3 hours ago by Gabriel
+          </h3>
+        </div>
       </div>
     </div>
   </div>
@@ -44,6 +74,10 @@ import jobInformationStatus from './job-information-status.vue'
 
 <style lang="scss">
 div#cnn-job-information-input {
+  grid-template-columns: 24px 1fr;
+}
+
+div#cnn-job-information-status {
   grid-template-columns: 24px 1fr;
 }
 </style>
