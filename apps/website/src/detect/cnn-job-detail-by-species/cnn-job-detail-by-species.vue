0dc651b2fcccbede5f9509ead4e9e963689b41fe
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-07-18 17:39:22 +0700

    #1065 add preparations for modal selector component

--- apps/website/src/detect/cnn-job-detail-by-species/cnn-job-detail-by-species.vue
+++ apps/website/src/detect/cnn-job-detail-by-species/cnn-job-detail-by-species.vue
@@ -15,14 +15,46 @@
           <span class="text-subtle">{{ speciesName }}</span>
         </el-breadcrumb-item>
       </el-breadcrumb>
+      <div class="job-btn-container">
+        <el-button
+          type="info"
+          size="large"
+          class="rounded-md uppercase font-semibold"
+        >
+          JOB DETAIL
+        </el-button>
+        <el-button
+          type="info"
+          size="large"
+          class="rounded-md"
+        >
+          <icon-fa-ellipsis-h />
+        </el-button>
+      </div>
+    </div>
+    <div class="job-detail-by-species-selector mt-4">
+      <section-title>
+        <template #title>
+          <div class="flex justify-between items-center">
+            <h2 class="text-lg font-semibold">
+              Name of the species (amount of detections)
+            </h2>
+          </div>
+        </template>
+        <template #controls>
+          <JobResultFilterBySpecies />
+        </template>
+      </section-title>
     </div>
   </div>
 </template>
+
 <script setup lang="ts">
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 
 import { ROUTE_NAMES } from '~/router'
+import JobResultFilterBySpecies from './components/job-result-filter-by-species.vue'
 
 const route = useRoute()
 
