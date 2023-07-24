fc55b9ffb6636da0d5192fa56d6d9f0bb26885b6
Somjintana Korbut <nuisomjin@gmail.com>
2023-07-24 12:22:47 +0100

    #1076 Featured - update spacing in the list view

--- apps/website/src/landing/featured/featured-page.vue
+++ apps/website/src/landing/featured/featured-page.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="bg-white dark:bg-echo">
     <div class="w-full h-64 lg:h-84 bg-hero-featured-works bg-cover bg-no-repeat bg-right border-b border-b-frequency">
-      <div class="flex flex-row max-w-screen-2xl mx-auto">
-        <div class="max-w-screen-md p-8 md:p-20">
+      <div class="grid lg:grid-cols-12 max-w-screen-xl mx-auto px-4">
+        <div class="place-self-center lg:col-span-7 py-8 lg:py-24">
           <h1 class="mb-4 text-gray-900 dark:text-frequency">
             Featured Work
           </h1>
@@ -15,8 +15,8 @@
         </div>
       </div>
     </div>
-    <div class="max-w-screen-2xl mx-auto py-8 px-2 lg:pt-24 lg:px-20 flex flex-col md:flex-row">
-      <aside class="w-full px-8 md:px-0 md:sticky md:h-screen md:top-4 md:basis-1/4">
+    <div class="grid md:grid-cols-12 max-w-screen-xl mx-auto py-8 lg:(pt-20)">
+      <aside class="md:col-span-4 px-8 md:px-4 md:sticky md:h-screen md:top-4">
         <button
           data-collapse-toggle="category-menu"
           type="button"
@@ -54,18 +54,18 @@
           <li
             v-for="item in projects"
             :key="item.id"
-            class="py-2 px-0 md:px-2"
+            class="mb-4"
           >
             <router-link
               :to="'#' + item.category.id"
-              :class="currenRoute.hash === '#' + item.category.id ? 'text-frequency font-bold border-b-2 border-b-frequency' : 'text-gray-500 dark:text-insight'"
+              :class="currenRoute.hash === '#' + item.category.id ? 'text-frequency font-medium border-b-2 border-b-frequency' : 'text-gray-500 dark:text-insight'"
             >
               {{ item.category.name }}
             </router-link>
           </li>
         </ul>
       </aside>
-      <div class="md:basis-3/4 flex flex-col flex-nowrap flex-grow items-stretch gap-8 overflow-y-auto px-8 lg:pb-64">
+      <div class="items-stretch gap-10 overflow-y-auto flex flex-col px-8 md:(pb-64 px-4 col-span-8)">
         <card
           v-for="item in projects"
           :id="item.category.id"
