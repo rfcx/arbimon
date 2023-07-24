efd57b9aaf0cc1c0b4c614c66615a798ea6fa196
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-07-24 19:06:12 +0700

    #998 add comments and enabled tag for detections job

--- apps/website/src/detect/_composables/use-get-detections.ts
+++ apps/website/src/detect/_composables/use-get-detections.ts
@@ -6,9 +6,14 @@ import { type DetectDetectionsQueryParams, type DetectDetectionsResponse, apiBio
 
 export const FETCH_DETECTIONS = 'fetch-detections'
 
-export const useGetJobDetections = (apiClient: AxiosInstance, jobId: number, params: ComputedRef<DetectDetectionsQueryParams>): UseQueryReturnType<DetectDetectionsResponse | undefined, unknown> => {
-  return useQuery(
-    [FETCH_DETECTIONS, params],
-    async () => await apiBioGetDetectDetections(apiClient, jobId, params.value)
-  )
+export const useGetJobDetections = (apiClient: AxiosInstance, jobId: number, params: ComputedRef<DetectDetectionsQueryParams>, enabled: ComputedRef<boolean>): UseQueryReturnType<DetectDetectionsResponse | undefined, unknown> => {
+  return useQuery({
+    queryKey: [FETCH_DETECTIONS, params],
+    queryFn: async () => await apiBioGetDetectDetections(apiClient, jobId, params.value),
+    enabled,
+    // the job detections will refetch intervally every 30 seconds all the time.
+    // because there could be new detections review data coming in from other users,
+    // not just the incoming detections data.
+    refetchInterval: 30_000
+  })
 }
