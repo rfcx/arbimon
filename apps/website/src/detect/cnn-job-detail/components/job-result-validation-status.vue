21e87b436b041d6670828fcff902c430343123d8
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-07-24 17:08:48 +0700

    #998 refactor UI of job validation status

--- apps/website/src/detect/cnn-job-detail/components/job-result-validation-status.vue
+++ apps/website/src/detect/cnn-job-detail/components/job-result-validation-status.vue
@@ -3,45 +3,47 @@
     <h3 class="job-result-validation-status-header text-subtle text-sm mb-2">
       Validation status
     </h3>
-    <div v-if="isLoadingValidationStatus" />
-    <div v-else-if="isErrorValidationStatus" />
-    <div v-else-if="!validationStatusData" />
+    <div
+      v-if="props.isLoading"
+      class="loading-shimmer mx-2 rounded-lg"
+    />
+    <ComponentError
+      v-else-if="props.isError"
+      class="py-8"
+    />
     <div
       v-else
       class="grid grid-cols-6 gap-2"
     >
       <template
-        v-for="key in Object.keys(validationStatusData).map(Number)"
-        :key="'validation-status-' + key"
+        v-for="key in Object.entries(props.data?.reviewStatus ?? {})"
+        :key="'validation-status-' + key[0]"
       >
-        <span class="font-semibold col-span-1 <md:col-span-1 <lg:col-span-2">{{ getValidationStatusValue(key) }}</span>
-        <span class="ml-2 col-span-5 <md:col-span-5 <lg:col-span-4">{{ getValidationStatusLabel(key) }}</span>
+        <span class="font-semibold col-span-2">{{ validationStatusMap[key[0]] }}</span>
+        <span class="ml-2 col-span-4">{{ key[1] }}</span>
       </template>
     </div>
   </div>
 </template>
+
 <script setup lang="ts">
-  import type { AxiosInstance } from 'axios'
-import { computed, inject } from 'vue'
-import { useRoute } from 'vue-router'
+import type { Ref } from 'vue'
+import { ref } from 'vue'
 
-import { displayValue } from '@rfcx-bio/utils/number'
+import { type DetectValidationResultsResponse } from '@rfcx-bio/common/api-bio/detect/detect-validation-results'
 
-import { useGetValidationStatus } from '@/detect/_composables/use-get-validation-status'
-import { apiClientBioKey } from '@/globals'
-import { VALIDATION_STATUS_BY_ID } from '~/validation'
+import ComponentError from './component-error.vue'
 
-const route = useRoute()
-const jobId = computed(() => typeof route.params.jobId === 'string' ? parseInt(route.params.jobId) : -1)
+const props = withDefaults(defineProps<{ isLoading: boolean, isError: boolean, data: DetectValidationResultsResponse | undefined }>(), {
+  isLoading: true,
+  isError: false,
+  data: undefined
+})
 
-// External data
-const apiBio = inject(apiClientBioKey) as AxiosInstance
-const { isLoading: isLoadingValidationStatus, isError: isErrorValidationStatus, data: validationStatusData } = useGetValidationStatus(apiBio, jobId.value)
-
-const getValidationStatusLabel = (key: number) => VALIDATION_STATUS_BY_ID[key].label
-
-const getValidationStatusValue = (key: number) => {
-  const validKey = key as unknown as 0 | 1 | 2 | 3
-  return validationStatusData.value ? displayValue(validationStatusData.value[validKey]) : 0
-}
+const validationStatusMap: Ref<Record<string, string>> = ref({
+  rejected: 'Not Present',
+  uncertain: 'Unknown',
+  confirmed: 'Present',
+  total: 'Total'
+})
 </script>
