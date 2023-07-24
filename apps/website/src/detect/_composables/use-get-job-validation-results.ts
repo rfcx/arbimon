887facec0db30d0cfec9bed10b6398d3da9d6477
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-07-24 17:06:41 +0700

    #998 add new composable to query job validation status by species

---
+++ apps/website/src/detect/_composables/use-get-job-validation-results.ts
@@ -0,0 +1,17 @@
+import { type AxiosInstance } from 'axios'
+import { type UseQueryReturnType, useQuery } from 'vue-query'
+
+import { type DetectValidationResultsQueryParams, type DetectValidationResultsResponse, apiBioGetValidationResults } from '@rfcx-bio/common/api-bio/detect/detect-validation-results'
+
+export const FETCH_VALIDATION_STATUS = 'fetch-validation-status'
+
+export const useGetJobValidationResults = (apiClient: AxiosInstance, jobId: number, query: DetectValidationResultsQueryParams): UseQueryReturnType<DetectValidationResultsResponse | undefined, unknown> => {
+  return useQuery(
+    [FETCH_VALIDATION_STATUS],
+    async () => await apiBioGetValidationResults(apiClient, jobId, query),
+    {
+      // refetch every 1 minute
+      refetchInterval: 60_000
+    }
+  )
+}
