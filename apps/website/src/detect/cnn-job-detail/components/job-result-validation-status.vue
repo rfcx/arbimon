9d46ba1adbfc083339159a528a5d7d23191998e7
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-07-24 19:05:24 +0700

    #998 compute unreviewed species from total value

--- apps/website/src/detect/cnn-job-detail/components/job-result-validation-status.vue
+++ apps/website/src/detect/cnn-job-detail/components/job-result-validation-status.vue
@@ -13,14 +13,15 @@
     />
     <div
       v-else
-      class="grid grid-cols-6 gap-2"
+      class="grid gap-x-4"
+      style="grid-template-columns: fit-content(100px) 1fr;"
     >
       <template
         v-for="key in Object.entries(props.data?.reviewStatus ?? {})"
         :key="'validation-status-' + key[0]"
       >
-        <span class="font-semibold col-span-2">{{ validationStatusMap[key[0]] }}</span>
-        <span class="ml-2 col-span-4">{{ key[1] }}</span>
+        <span class="font-semibold justify-self-start text-right text-lg">{{ validationStatusValue(key[0], key[1]) }}</span>
+        <span class="text-lg">{{ validationStatusName(key[0]) }}</span>
       </template>
     </div>
   </div>
@@ -39,6 +40,28 @@ const props = withDefaults(defineProps<{ isLoading: boolean, isError: boolean, d
   isError: false,
   data: undefined
 })
+
+/**
+ * Returns `unreviewed` amount instead of total from
+ *
+ * `total` - (`uncertain` + `confirmed` + `rejected`)
+ */
+const validationStatusValue = (key: string, value: number): number => {
+  if (key === 'total') {
+    const sum = (props.data?.reviewStatus.uncertain ?? 0) + (props.data?.reviewStatus.confirmed ?? 0) + (props.data?.reviewStatus.rejected ?? 0)
+    return (props.data?.reviewStatus.total ?? 0) - sum
+  }
+
+  return value
+}
+
+const validationStatusName = (key: string): string => {
+  if (key === 'total') {
+    return 'Unreviewed'
+  }
+
+  return validationStatusMap.value[key]
+}
 
 const validationStatusMap: Ref<Record<string, string>> = ref({
   rejected: 'Not Present',
