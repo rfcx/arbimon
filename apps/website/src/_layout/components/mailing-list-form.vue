4488609c0866026f8c62afbc5a96f0ee4d8336af
Somjintana Korbut <nuisomjin@gmail.com>
2023-07-25 14:45:20 +0100

    #1078 Contact us - add action when the user press subscribe

--- apps/website/src/_layout/components/mailing-list-form.vue
+++ apps/website/src/_layout/components/mailing-list-form.vue
@@ -1,11 +1,18 @@
 <template>
-  <form class="flex flex-col lg:flex-row gap-4 justify-end items-stretch flex-wrap">
+  <form
+    :action="formMainAudience"
+    method="post"
+    target="_blank"
+    class="flex flex-col lg:flex-row gap-4 justify-end items-stretch flex-wrap"
+  >
     <div class="flex-1">
       <div class="sr-only">
         First name
       </div>
       <input
+        required
         type="text"
+        name="FNAME"
         placeholder="First name"
         class="w-full border border-cloud rounded-md dark:(bg-pitch text-insight placeholder:text-insight) focus:(border-frequency ring-frequency)"
       >
@@ -15,15 +22,26 @@
         Email address
       </div>
       <input
+        required
         type="text"
+        name="EMAIL"
         placeholder="Email Address"
         class="w-full border border-cloud rounded-md dark:(bg-pitch text-insight placeholder:text-insight) focus:(border-frequency ring-frequency)"
       >
     </div>
+    <input
+      type="hidden"
+      name="tags"
+      :value="tagWebsiteSubscriber"
+    >
     <button
+      type="submit"
       class="btn btn-primary min-w-30 h-10 mr-0 px-2 grow-0"
     >
       Subscribe
     </button>
   </form>
 </template>
+<script setup lang="ts">
+import { formMainAudience, tagWebsiteSubscriber } from '@/_services/mailchimp'
+</script>
