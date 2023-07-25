e2b0efcb6e32e30b09f007c51d0d2bb96983043e
Somjintana Korbut <nuisomjin@gmail.com>
2023-07-25 16:45:43 +0100

    #1068 How it works - change the structure of the page

---
+++ apps/website/src/_layout/components/hero-content.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="bg-white dark:bg-echo">
+    <div class="w-full h-64 lg:h-84 bg-hero-featured-works bg-cover bg-no-repeat bg-right border-b border-b-frequency">
+      <div class="grid lg:grid-cols-12 max-w-screen-xl mx-auto px-4 h-full">
+        <div class="lg:col-span-7 py-8 lg:py-24 my-auto">
+          <slot />
+        </div>
+        <div class="hidden not-sr-only">
+          .
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ apps/website/src/landing/how-it-works/blocks/how-it-works-contact-card.vue
@@ -0,0 +1,15 @@
+<template>
+  <contact-card>
+    <div class="flex flex-col">
+      <h1>
+        Have a question about the process?
+      </h1>
+      <p class="my-8">
+        Our team of experts can answer any of your questions and help guide you along the way.
+      </p>
+    </div>
+  </contact-card>
+</template>
+<script setup lang="ts">
+import ContactCard from '../../home/components/contact-card.vue'
+</script>
