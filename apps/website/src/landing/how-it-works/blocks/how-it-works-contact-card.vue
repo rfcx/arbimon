258467e7c3167f31f72bf9f458bee9a3aa8a6ee3
Somjintana Korbut <nuisomjin@gmail.com>
2023-07-26 12:43:21 +0100

    #1068 How it work - update content

--- apps/website/src/landing/how-it-works/blocks/how-it-works-contact-card.vue
+++ apps/website/src/landing/how-it-works/blocks/how-it-works-contact-card.vue
@@ -7,9 +7,16 @@
       <p class="my-8">
         Our team of experts can answer any of your questions and help guide you along the way.
       </p>
+      <router-link
+        :to="{ name: ROUTE_NAMES.landingContact }"
+        class="btn btn-primary"
+      >
+        Contact us
+      </router-link>
     </div>
   </contact-card>
 </template>
 <script setup lang="ts">
+import { ROUTE_NAMES } from '~/router'
 import ContactCard from '../../home/components/contact-card.vue'
 </script>
--- apps/website/src/landing/how-it-works/blocks/how-it-works-cta.vue
+++ apps/website/src/landing/how-it-works/blocks/how-it-works-cta.vue
@@ -2,19 +2,16 @@
   <section class="bg-gray-800 dark:bg-pitch">
     <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6 flex flex-row items-center">
       <div class="basis-2/3">
-        <h3 class="mb-2 tracking-tight text-gray-700 dark:text-insight">
-          Need assistance with part of the process?
-        </h3>
-        <p class="font-light text-gray-500 dark:text-insight">
-          Find out how our team of scientists can support your next project
-        </p>
+        <h2 class="mb-2 text-gray-700 dark:text-insight">
+          Ready to level up your research?
+        </h2>
       </div>
       <div class="basis-1/3 text-right">
         <router-link
-          :to="{ name: ROUTE_NAMES.landingContact }"
+          :to="{ name: ROUTE_NAMES.myProjects }"
           class="btn btn-primary"
         >
-          Contact us
+          Get started
         </router-link>
       </div>
     </div>
