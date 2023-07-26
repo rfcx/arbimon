7c2ec940bbeb7f54a227dbb6b9fde2f2c6209296
Somjintana Korbut <nuisomjin@gmail.com>
2023-07-26 12:43:21 +0100

    #1068 How it work - update content

--- apps/website/src/landing/how-it-works/blocks/how-it-works-steps.vue
+++ apps/website/src/landing/how-it-works/blocks/how-it-works-steps.vue
@@ -2,7 +2,10 @@
   <section class="bg-white dark:bg-echo">
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-24 lg:px-6">
       <div class="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
-        <h1 class="mb-4 text-gray-900 dark:text-white">
+        <h6 class="font-eyebrow text-sm uppercase text-spoonbill mb-6">
+          Arbimon step-by-step
+        </h6>
+        <h1 class="mb-6 text-gray-900 dark:text-white">
           How Arbimon works
         </h1>
         <p class="text-gray-500 sm:text-xl dark:text-insight">
@@ -10,119 +13,65 @@
         </p>
       </div>
       <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
-        <div class="rounded-xl px-8 py-12">
-          <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12">
-            <svg
-              class="w-5 h-5 text-frequency lg:w-6 lg:h-6 dark:text-primary-300"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            ><path
-              fill-rule="evenodd"
-              d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z"
-              clip-rule="evenodd"
-            /></svg>
-          </div>
-          <h3 class="mb-2 text-xl font-bold dark:text-insight">
-            Define
-          </h3>
-          <p class="text-gray-700 dark:text-insight">
-            Define the fundamental research and conservation questions to be addressed: Where? Why? What? How?
-          </p>
-        </div>
-        <div class="rounded-xl px-8 py-12">
-          <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12">
-            <svg
-              class="w-5 h-5 text-frequency lg:w-6 lg:h-6 dark:text-primary-300"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            ><path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z" /></svg>
-          </div>
-          <h3 class="mb-2 text-xl font-bold dark:text-insight">
-            Collect
-          </h3>
-          <p class="text-gray-700 dark:text-insight">
-            Conduct fieldwork to collect data by deploying acoustic recorders based on study goals and sampling design.
-          </p>
-        </div>
-        <div class="rounded-xl px-8 py-12">
-          <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12">
-            <svg
-              class="w-5 h-5 text-frequency lg:w-6 lg:h-6 dark:text-primary-300"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            ><path
-              fill-rule="evenodd"
-              d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z"
-              clip-rule="evenodd"
-            /><path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z" /></svg>
-          </div>
-          <h3 class="mb-2 text-xl font-bold dark:text-insight">
-            Upload
-          </h3>
-          <p class="text-gray-700 dark:text-insight">
-            Upload an unlimited volume of audio recordings to Arbimon for free storage, file management, and ecoacoustic analysis.
-          </p>
-        </div>
-        <div class="rounded-xl px-8 py-12">
-          <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12">
-            <svg
-              class="w-5 h-5 text-frequency lg:w-6 lg:h-6 dark:text-primary-300"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            ><path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z" /><path
-              fill-rule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z"
-              clip-rule="evenodd"
-            /></svg>
-          </div>
-          <h3 class="mb-2 text-xl font-bold dark:text-insight">
-            Inspect
-          </h3>
-          <p class="text-gray-700 dark:text-insight">
-            View spectrograms to annotate target acoustic signals, create call templates, and build training datasets for ecoacoustic analyses.
-          </p>
-        </div>
-        <div class="rounded-xl px-8 py-12">
-          <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12">
-            <svg
-              class="w-5 h-5 text-frequency lg:w-6 lg:h-6 dark:text-primary-300"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            ><path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" /></svg>
-          </div>
-          <h3 class="mb-2 text-xl font-bold dark:text-insight">
-            Analyze
-          </h3>
-          <p class="text-gray-700 dark:text-insight">
-            Analyze recordings by combining state-of-the-art AI tools, ecoacoustic analyses, and environmental variables to generate ecological insights.
-          </p>
-        </div>
-        <div class="rounded-xl px-8 py-12">
-          <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12">
-            <svg
-              class="w-5 h-5 text-frequency lg:w-6 lg:h-6 dark:text-primary-300"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            ><path
-              fill-rule="evenodd"
-              d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
-              clip-rule="evenodd"
-            /></svg>
-          </div>
-          <h3 class="mb-2 text-xl font-bold dark:text-insight">
-            Share
-          </h3>
-          <p class="text-gray-700 dark:text-insight">
-            Generate and publish figures and visualizations that highlight actionable results from your acoustic analyses, bringing transparency to your biodiversity reporting.
-          </p>
-        </div>
+        <StepCard
+          v-for="step in data"
+          :key="step.id"
+          :title="'Step ' + step.id + ': ' + step.title"
+          :description="step.description"
+          :icon-url="step.iconUrl"
+        />
       </div>
     </div>
   </section>
 </template>
+
+<script setup lang="ts">
+
+import activityIcon from '@/_assets/how-it-works/fi_activity.svg'
+import inspectIcon from '@/_assets/how-it-works/fi_inspect.svg'
+import chartIcon from '@/_assets/how-it-works/fi_pie-chart.svg'
+import searchIcon from '@/_assets/how-it-works/fi_search.svg'
+import treeIcon from '@/_assets/how-it-works/fi_tree.svg'
+import uploadIcon from '@/_assets/how-it-works/fi_upload-cloud.svg'
+import StepCard from '../components/step-card.vue'
+
+const data = [
+  {
+    id: 1,
+    title: 'Define',
+    description: 'Define the fundamental research and conservation questions to be addressed: Where? Why? What? How?',
+    iconUrl: searchIcon
+  },
+  {
+    id: 2,
+    title: 'Collect',
+    description: 'Conduct fieldwork to collect data by deploying acoustic recorders based on study goals and sampling design.',
+    iconUrl: treeIcon
+  },
+  {
+    id: 3,
+    title: 'Upload',
+    description: 'Upload an unlimited volume of audio recordings to Arbimon for free storage, file management, and ecoacoustic analysis.',
+    iconUrl: uploadIcon
+  },
+  {
+    id: 4,
+    title: 'Inspect',
+    description: 'View spectrograms to annotate target acoustic signals, create call templates, and build training datasets for ecoacoustic analyses.',
+    iconUrl: inspectIcon
+  },
+  {
+    id: 5,
+    title: 'Analyze',
+    description: 'Analyze recordings by combining state-of-the-art AI tools, ecoacoustic analyses, and environmental variables to generate ecological insights.',
+    iconUrl: activityIcon
+  },
+  {
+    id: 6,
+    title: 'Share',
+    description: 'Generate and publish figures and visualizations that highlight actionable results from your acoustic analyses, bringing transparency to your biodiversity reporting.',
+    iconUrl: chartIcon
+  }
+]
+
+</script>
---
+++ apps/website/src/landing/how-it-works/components/step-card.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="rounded-xl px-8 py-12">
+    <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12">
+      <img
+        :src="iconUrl"
+        alt="icon"
+      >
+    </div>
+    <h4 class="mb-2 dark:text-insight">
+      {{ title }}
+    </h4>
+    <p class="text-gray-700 dark:text-insight">
+      {{ description }}
+    </p>
+  </div>
+</template>
+<script setup lang="ts">
+defineProps<{ iconUrl: string, title: string, description: string }>()
+</script>
