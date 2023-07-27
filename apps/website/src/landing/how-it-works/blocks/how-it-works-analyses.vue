e9e962715fea3071c847d514cabf98baf78857ec
Somjintana Korbut <nuisomjin@gmail.com>
2023-07-27 08:21:35 +0100

    #1068 How it work - implement analyse layout

--- apps/website/src/landing/how-it-works/blocks/how-it-works-analyses.vue
+++ apps/website/src/landing/how-it-works/blocks/how-it-works-analyses.vue
@@ -1,80 +1,69 @@
 <template>
   <section class="bg-white dark:bg-echo">
-    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-24 lg:px-6">
+    <div class="py-10 px-6 mx-auto max-w-screen-xl lg:py-24 lg:px-6">
       <div class="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
-        <h1 class="mb-4 text-gray-900 dark:text-insight">
+        <h1 class="mb-6 text-gray-900 dark:text-insight">
           Types of analyses available on Arbimon
         </h1>
-        <p class="text-gray-500 sm:text-xl dark:text-insight">
+        <p class="text-gray-500 dark:text-insight">
           We offer a range of analyses to fit a wide variety of research needs.
         </p>
       </div>
-      <div class="space-y-4 md:grid md:grid-cols-3 lg:grid-cols-4 md:gap-4 md:space-y-0">
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:(bg-pitch border-frequency)">
-          <img
-            class="rounded-t-lg"
-            src="https://flowbite.com/docs/images/blog/image-1.jpg"
-            alt=""
-          >
-          <div class="p-5 border-t border-frequency">
-            <span class="text-insight text-xs font-display uppercase mr-2 px-2.5 py-0.5 rounded-full dark:(border-1 border-spoonbill text-insight)">Supervised</span>
-            <h5 class="my-2 text-2xl tracking-tight text-gray-900 dark:text-insight">
-              Regional CNN
-            </h5>
-            <p class="mb-3 font-normal text-gray-700 dark:text-insight">
-              RFCx trains a CNN classifier model using positive and negative examples for each class (usually species) collected during PM.
-            </p>
-          </div>
-        </div>
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:(bg-pitch border-frequency)">
-          <img
-            class="rounded-t-lg"
-            src="https://flowbite.com/docs/images/blog/image-1.jpg"
-            alt=""
-          >
-          <div class="p-5 border-t border-frequency ">
-            <span class="text-insight text-xs font-display uppercase mr-2 px-2.5 py-0.5 rounded-full dark:(border-1 border-spoonbill text-insight)">Supervised</span>
-            <h5 class="my-2 text-2xl tracking-tight text-gray-900 dark:text-insight">
-              Pattern Matching (PM)
-            </h5>
-            <p class="mb-3 font-normal text-gray-700 dark:text-insight">
-              PM is an analytical method where example of a species-specific call is fed into a statistical model that detects similar matches in other recordings.
-            </p>
-          </div>
-        </div>
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:(bg-pitch border-frequency)">
-          <img
-            class="rounded-t-lg"
-            src="https://flowbite.com/docs/images/blog/image-1.jpg"
-            alt=""
-          >
-          <div class="p-5 border-t border-frequency ">
-            <span class="text-insight text-xs font-display uppercase mr-2 px-2.5 py-0.5 rounded-full dark:(border-1 border-spoonbill text-insight)">Supervised</span>
-            <h5 class="my-2 text-2xl tracking-tight text-gray-900 dark:text-insight">
-              Random Forest Model
-            </h5>
-            <p class="mb-3 font-normal text-gray-700 dark:text-insight">
-              RFM is a type of machine learning model consisting of multiple decision trees. The RFM module is designed for single-species detection.
-            </p>
-          </div>
-        </div>
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:(bg-pitch border-frequency)">
-          <img
-            class="rounded-t-lg"
-            src="https://flowbite.com/docs/images/blog/image-1.jpg"
-            alt=""
-          >
-          <div class="p-5 border-t border-frequency ">
-            <span class="text-insight text-xs font-display uppercase mr-2 px-2.5 py-0.5 rounded-full  dark:(border-1 border-flamingo text-insight)">Unsupervised</span>
-            <h5 class="my-2 text-2xl tracking-tight text-gray-900 dark:text-insight">
-              Clustering
-            </h5>
-            <p class="mb-3 font-normal text-gray-700 dark:text-insight">
-              The Audio Event Detection (AED) and Clustering analyses aim to automatically detect and categorize sounds in large audio datasets without supervision.
-            </p>
-          </div>
-        </div>
+      <div class="space-y-4 md:grid md:grid-cols-2 md:gap-4 md:space-y-0">
+        <analyse-card
+          v-for="analyse in data"
+          :key="analyse.id"
+          :title="analyse.title"
+          :description="analyse.description"
+          :icon-url="analyse.iconUrl"
+          :tag="analyse.tag"
+        />
       </div>
     </div>
   </section>
 </template>
+<script setup lang="ts">
+import aedIcon from '@/_assets/how-it-works/AED+C.svg'
+import pmIcon from '@/_assets/how-it-works/PM.svg'
+import rfmIcon from '@/_assets/how-it-works/RFM.svg'
+import ssIcon from '@/_assets/how-it-works/Soundscapes.svg'
+import AnalyseCard from '../components/analyse-card.vue'
+
+const data = [
+  {
+    id: 1,
+    title: 'Pattern Matching (PM)',
+    description: 'Our Pattern matching tool is a simple sound detection tool that only requires a single example of the target sound to get started.',
+    tag: 'Supervised',
+    iconUrl: pmIcon
+  },
+  {
+    id: 2,
+    title: 'Soundscape Analyses',
+    description: 'Our soundscape analytical approaches allow holistic comparisons of entire eco-acoustic communities across time and space.',
+    tag: '',
+    iconUrl: ssIcon
+  },
+  {
+    id: 3,
+    title: 'AI Detection Models',
+    description: 'Our deep-learning audio recognition models are trained to automate the detection of known species in raw soundscape recordings.',
+    tag: 'Supervised',
+    iconUrl: rfmIcon
+  },
+  {
+    id: 4,
+    title: 'Automated Event Detection & Clustering',
+    description: 'Our Audio Event Detection (AED) and clustering analyses aim to automatically detect and categorize sounds in large audio datasets.',
+    tag: 'Unsupervised',
+    iconUrl: aedIcon
+  },
+  {
+    id: 5,
+    title: 'Ecological Analyses',
+    description: 'We use ecological models that combine audio detections with environmental variables to estimate ecological trends and patterns from species to community levels.',
+    tag: '',
+    iconUrl: ''
+  }
+]
+</script>
---
+++ apps/website/src/landing/how-it-works/components/analyse-card.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="pt-6 pb-8 rounded-lg border-1 border-frequency">
+    <div
+      v-if="iconUrl || tag"
+      class="flex flex-row w-full justify-between items-center mb-4"
+    >
+      <img
+        v-if="iconUrl"
+        :src="iconUrl"
+        alt="icon"
+        class="ml-6"
+      >
+      <span
+        v-else
+        class="ml-6 not-sr-only invisible"
+      >
+        .
+      </span>
+      <span
+        v-if="tag"
+        class="border-y-1 border-l-1 border-spoonbill rounded-l-full px-3 uppercase font-header"
+      >
+        {{ tag }}
+      </span>
+    </div>
+    <div class="px-6">
+      <h4 class="mb-3 basis-2/3 dark:text-insight">
+        {{ title }}
+      </h4>
+      <p class="text-gray-700 dark:text-insight">
+        {{ description }}
+      </p>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+defineProps<{ iconUrl?: string, title: string, description: string, tag?: string }>()
+</script>
