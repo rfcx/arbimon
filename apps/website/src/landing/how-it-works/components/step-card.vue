8d200b8ad821a370c528fcf151ced0f4798f7d91
Somjintana Korbut <nuisomjin@gmail.com>
2023-07-27 06:40:20 +0100

    #1068 How it work - tidy up the spacing

--- apps/website/src/landing/how-it-works/components/step-card.vue
+++ apps/website/src/landing/how-it-works/components/step-card.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="rounded-xl px-8 py-12">
+  <div class="rounded-xl px-6 py-8">
     <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12">
       <img
         :src="iconUrl"
         alt="icon"
       >
     </div>
-    <h4 class="mb-2 dark:text-insight">
+    <h4 class="mb-4 dark:text-insight">
       {{ title }}
     </h4>
     <p class="text-gray-700 dark:text-insight">
