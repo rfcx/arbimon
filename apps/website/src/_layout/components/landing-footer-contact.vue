ac10135cf3240699ffeb27c28c729827c84365de
Somjintana Korbut <nuisomjin@gmail.com>
2023-07-30 23:52:56 +0100

    #1077 add border top for stay connected in the footer

--- apps/website/src/_layout/components/landing-footer-contact.vue
+++ apps/website/src/_layout/components/landing-footer-contact.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="dark:bg-moss px-4 sm:px-6 lg:px-20">
+  <section class="dark:bg-moss px-4 sm:px-6 lg:px-20 border-t border-t-frequency">
     <div class="max-w-screen-xl mx-auto py-10 gap-8 flex flex-col lg:(flex-row items-center) justify-between">
       <div class="basis-full lg:basis-1/3">
         <h2>Stay connected</h2>
