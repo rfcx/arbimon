3ceae75c4c7d34320de04ef02b1a8280f1e46fe7
Somjintana Korbut <nuisomjin@gmail.com>
2023-08-01 14:41:11 +0100

    #1087 FAQ - update layout and add expand collapse

---
+++ apps/website/src/landing/faq/components/faq-accordion.vue
@@ -0,0 +1,44 @@
+<template>
+  <h2 :id="'accordion-open-heading-' + itemId">
+    <button
+      type="button"
+      class="flex items-center justify-between w-full p-4 text-left text-insight border-b-1 border-util-gray-01 bg-transparent focus:bg-transparent dark:bg-transparent dark:hover:bg-transparent"
+      :data-accordion-target="'#accordion-open-body-' + itemId"
+      aria-expanded="true"
+      :aria-controls="'accordion-open-body-' + itemId"
+    >
+      <div class="text-lg lg:text-xl font-medium">
+        <slot name="question" />
+      </div>
+      <svg
+        data-accordion-icon
+        class="w-3 h-3 rotate-180 shrink-0"
+        aria-hidden="true"
+        width="14"
+        height="17"
+        viewBox="0 0 14 17"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M0 9.33789L1.1875 8.13477L6.14062 13.0879L6.14062 0.259766H7.85938L7.85938 13.0879L12.7969 8.13477L14 9.33789L7 16.3379L0 9.33789Z"
+          fill="#ADFF2C"
+        />
+      </svg>
+    </button>
+  </h2>
+  <div
+    :id="'accordion-open-body-' + itemId"
+    class="hidden font-base"
+    :aria-labelledby="'accordion-open-heading-' + itemId"
+  >
+    <div class="px-4 py-8">
+      <slot name="answer" />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+defineProps<{
+  itemId: string
+}>()
+</script>
--- apps/website/src/landing/faq/faq-page.vue
+++ apps/website/src/landing/faq/faq-page.vue
@@ -1,192 +1,47 @@
 <template>
   <section class="bg-white dark:bg-echo">
     <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-      <h2 class="mb-8 text-gray-900 dark:text-frequency">
-        Frequently asked questions
-      </h2>
-      <div class="grid pt-8 text-left border-t border-gray-200 md:gap-16 dark:border-gray-700 md:grid-cols-2">
+      <div class="grid pt-8 text-left gap-10 md:gap-16 dark:border-gray-700 md:grid-cols-2">
         <div>
-          <div class="mb-10">
-            <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
-              <svg
-                class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-frequency"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              ><path
-                fill-rule="evenodd"
-                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                clip-rule="evenodd"
-              /></svg>
-              What do you mean by "Figma assets"?
-            </h3>
-            <p class="text-gray-500 dark:text-insight">
-              You will have access to download the full Figma project including all of the pages, the components, responsive pages, and also the icons, illustrations, and images included in the screens.
-            </p>
-          </div>
-          <div class="mb-10">
-            <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
-              <svg
-                class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-frequency"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              ><path
-                fill-rule="evenodd"
-                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                clip-rule="evenodd"
-              /></svg>
-              What does "lifetime access" exactly mean?
-            </h3>
-            <p class="text-gray-500 dark:text-insight">
-              Once you have purchased either the design, code, or both packages, you will have access to all of the future updates based on the roadmap, free of charge.
-            </p>
-          </div>
-          <div class="mb-10">
-            <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
-              <svg
-                class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-frequency"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              ><path
-                fill-rule="evenodd"
-                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                clip-rule="evenodd"
-              /></svg>
-              How does support work?
-            </h3>
-            <p class="text-gray-500 dark:text-insight">
-              We're aware of the importance of well qualified support, that is why we decided that support will only be provided by the authors that actually worked on this project.
-            </p>
-            <p class="text-gray-500 dark:text-insight">
-              Feel free to <a
-                href="#"
-                class="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline"
-                target="_blank"
-                rel="noreferrer"
-              >contact us</a> and we'll help you out as soon as we can.
-            </p>
-          </div>
-          <div class="mb-10">
-            <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
-              <svg
-                class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-frequency"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              ><path
-                fill-rule="evenodd"
-                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                clip-rule="evenodd"
-              /></svg>
-              I want to build more than one project. Is that allowed?
-            </h3>
-            <p class="text-gray-500 dark:text-insight">
-              You can use Windster for an unlimited amount of projects, whether it's a personal website, a SaaS app, or a website for a client. As long as you don't build a product that will directly compete with Windster either as a UI kit, theme, or template, it's fine.
-            </p>
-            <p class="text-gray-500 dark:text-insight">
-              Find out more information by <a
-                href="#"
-                class="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline"
-              >reading the license</a>.
-            </p>
-          </div>
+          <h1 class="mb-8 text-gray-900 dark:text-frequency">
+            Frequently Asked Questions
+          </h1>
+          <p>Questions about Arbimon? Our FAQ page has you covered.</p>
         </div>
-        <div>
-          <div class="mb-10">
-            <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
-              <svg
-                class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-frequency"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              ><path
-                fill-rule="evenodd"
-                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                clip-rule="evenodd"
-              /></svg>
-              What does "free updates" include?
-            </h3>
-            <p class="text-gray-500 dark:text-insight">
-              The free updates that will be provided is based on the <a
-                href="#"
-                class="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline"
-              >roadmap</a> that we have laid out for this project. It is also possible that we will provide extra updates outside of the roadmap as well.
-            </p>
-          </div>
-          <div class="mb-10">
-            <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
-              <svg
-                class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-frequency"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              ><path
-                fill-rule="evenodd"
-                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                clip-rule="evenodd"
-              /></svg>
-              What does the free version include?
-            </h3>
-            <p class="text-gray-500 dark:text-insight">
-              The <a
-                href="#"
-                class="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline"
-              >free version</a> of Windster includes a minimal style guidelines, component variants, and a dashboard page with the mobile version alongside it.
-            </p>
-            <p class="text-gray-500 dark:text-insight">
-              You can use this version for any purposes, because it is open-source under the MIT license.
-            </p>
-          </div>
-          <div class="mb-10">
-            <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
-              <svg
-                class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-frequency"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              ><path
-                fill-rule="evenodd"
-                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                clip-rule="evenodd"
-              /></svg>
-              What is the difference between Windster and Tailwind UI?
-            </h3>
-            <p class="text-gray-500 dark:text-insight">
-              Although both Windster and Tailwind UI are built for integration with Tailwind CSS, the main difference is in the design, the pages, the extra components and UI elements that Windster includes.
-            </p>
-            <p class="text-gray-500 dark:text-insight">
-              Additionally, Windster is a project that is still in development, and later it will include both the application, marketing, and e-commerce UI interfaces.
-            </p>
-          </div>
-          <div class="mb-10">
-            <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
-              <svg
-                class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-frequency"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              ><path
-                fill-rule="evenodd"
-                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                clip-rule="evenodd"
-              /></svg>
-              Can I use Windster in open-source projects?
-            </h3>
-            <p class="text-gray-500 dark:text-insight">
-              Generally, it is accepted to use Windster in open-source projects, as long as it is not a UI library, a theme, a template, a page-builder that would be considered as an alternative to Windster itself.
-            </p>
-            <p class="text-gray-500 dark:text-insight">
-              With that being said, feel free to use this design kit for your open-source projects.
-            </p>
-            <p class="text-gray-500 dark:text-insight">
-              Find out more information by <a
-                href="#"
-                class="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline"
-              >reading the license</a>.
-            </p>
-          </div>
+        <div
+          id="accordion-open"
+          data-accordion="collapse"
+          data-active-classes="dark:bg-moss text-insight border-b-0 border-gray-200 dark:border-gray-700"
+          data-inactive-classes="text-insight"
+        >
+          <faq-accordion item-id="1">
+            <template #question>
+              Why is Arbimon free?
+            </template>
+            <template #answer>
+              // TODO: add answer
+            </template>
+          </faq-accordion>
+          <faq-accordion item-id="2">
+            <template #question>
+              Is it really unlimited storage and analysis?
+            </template>
+            <template #answer>
+              Yes!  With a free account, you can store as many files and run as many analyses as you want.  However, there are some technical limits in terms of file and job size. For storage, the platform will not accept single files over 200MB.
+
+              For some analyses (e.g. Pattern Matching, Random Forest), you can create jobs of any size, but others (e.g. Audio Event Detection and Clustering) have a cap on the number of recordings or data points that can be analyzed in a single job. You can see the limits in the table below. We have introduced a cap because it enables us to provide sufficient resources and cover the costs of running analyses for all projects
+              // TODO: add link
+              // TODO: add table
+            </template>
+          </faq-accordion>
+          <faq-accordion item-id="3">
+            <template #question>
+              How much does it cost to run a job?
+            </template>
+            <template #answer>
+              ...
+            </template>
+          </faq-accordion>
         </div>
       </div>
     </div>
@@ -197,4 +52,5 @@
 <script setup lang="ts">
 import FooterContact from '@/_layout/components/landing-footer-contact.vue'
 import faqCta from './blocks/faq-cta.vue'
+import faqAccordion from './components/faq-accordion.vue'
 </script>
