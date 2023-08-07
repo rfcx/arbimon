5f928beb54460d787a18064201a1c8e490136d61
Somjintana Korbut <nuisomjin@gmail.com>
2023-08-07 10:20:35 +0100

    #1089 implement team member section
    - add quote card
    - adjust card to support responsive
    - create team member list component
    - use new visual for team member

---
+++ apps/website/src/landing/team/components/quote-card.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="h-full w-full bg-chirp p-6 text-left text-echo flex flex-col items-baseline justify-center rounded-lg">
+    <h1>{{ title }}</h1>
+    <h2
+      v-if="subtitle"
+      class="md:text-2xl lg:text-3xl"
+    >
+      {{ subtitle }}
+    </h2>
+  </div>
+</template>
+<script setup lang="ts">
+defineProps<{
+  readonly title: string
+  readonly subtitle?: string
+}>()
+</script>
