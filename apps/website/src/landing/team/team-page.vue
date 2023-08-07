a1de33aee0386d8d10e59823353f8af09a026947
Somjintana Korbut <nuisomjin@gmail.com>
2023-08-07 12:23:07 +0100

    #1089 Team - add gallery section (with just header)

--- apps/website/src/landing/team/team-page.vue
+++ apps/website/src/landing/team/team-page.vue
@@ -2,10 +2,12 @@
   <team-hero />
   <team-purpose />
   <team-member-list />
+  <team-gallery />
   <team-cta />
 </template>
 <script setup lang="ts">
 import teamCta from './blocks/team-cta.vue'
+import teamGallery from './blocks/team-gallery.vue'
 import teamHero from './blocks/team-hero.vue'
 import teamMemberList from './blocks/team-member-list.vue'
 import teamPurpose from './blocks/team-purpose.vue'
