b5e756125a04ceb041c03e225d5683d6c9756ece
Somjintana Korbut <nuisomjin@gmail.com>
2023-08-07 10:49:30 +0100

    #1089 Team - add hero content

--- apps/website/src/landing/team/team-page.vue
+++ apps/website/src/landing/team/team-page.vue
@@ -1,4 +1,5 @@
 <template>
+  <team-hero />
   <section class="bg-white dark:bg-pitch">
     <div class="py-8 px-4 mx-auto max-w-screen-xl text-center sm:py-16 lg:px-6å">
       <team-member-list />
@@ -8,5 +9,6 @@
 </template>
 <script setup lang="ts">
 import teamCta from './blocks/team-cta.vue'
+import teamHero from './blocks/team-hero.vue'
 import teamMemberList from './blocks/team-member-list.vue'
 </script>
