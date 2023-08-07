5c38a7f4a5f5b40da852acd37a62a0617c1376dc
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-08-07 12:13:37 +0700

    #1085 restyle big view with finer grid spec

--- apps/website/src/landing/publications/blocks/publications-list.vue
+++ apps/website/src/landing/publications/blocks/publications-list.vue
@@ -1,42 +1,20 @@
 <template>
   <section class="bg-white dark:bg-pitch">
-    <div
-      id="publication-list"
-      class="max-w-screen-xl mx-auto grid gap-x-12"
-    >
-      <aside class="lg:w-90 w-full h-20" />
-      <main class="grow shrink-0 mt-17">
-        <div>
-          <div
-            v-for="(publicationYear, idx) in publicationYearsList"
-            :key="`${publicationYear}_${idx}`"
-          >
-            <h1 class="text-white text-4xl mb-12 lg:mx-0 mx-7 leading-10">
-              {{ publicationYear }}
-            </h1>
-            <div
-              v-for="publication in publicationsGroupedByYear[publicationYear]"
-              :key="publication.title"
-              class="mb-10 mx-5 lg:mx-0 px-10 pt-6 pb-8 border-1 rounded-2xl border-frequency bg-moss"
-            >
-              <span class="text-xs text-spoonbill font-eyebrow">
-                {{ publication.year }}| {{ publication.journal }}
-              </span>
-              <p class="text-xl mt-2 leading-7 text-white/80">
-                {{ publication.title }}
-              </p>
-              <p class="text-base text-white/80 mt-2">
-                {{ publication.author }}
-              </p>
-              <a
-                :href="publication.doiUrl"
-                rel="noreferrer nofollow"
-                class="inline-block mt-4 text-frequency"
-              >
-                Read the paper <icon-fas-arrow-right class="inline-block text-frequency" />
-              </a>
-            </div>
-          </div>
+    <div class="max-w-screen-xl mx-auto grid gap-x-12 md:grid-cols-12">
+      <aside class="md:col-span-4 md:(sticky h-screen top-4)" />
+      <main class="md:col-span-8 mt-17 mx-5">
+        <div
+          v-for="(publicationYear, idx) in publicationYearsList"
+          :key="`${publicationYear}_${idx}`"
+        >
+          <h1 class="text-white text-4xl mb-12 lg:mx-0 mx-7 leading-10">
+            {{ publicationYear }}
+          </h1>
+          <PublicationCard
+            v-for="publication in publicationsGroupedByYear[publicationYear]"
+            :key="publication.title"
+            :publication="publication"
+          />
         </div>
       </main>
     </div>
@@ -47,6 +25,7 @@
 import { groupBy } from 'lodash-es'
 import { computed } from 'vue'
 
+import PublicationCard from '../components/publication-card.vue'
 import { publications } from '../data'
 import type { Publication } from '../data/types'
 
@@ -61,15 +40,3 @@ const publicationYearsList = computed<number[]>(() => {
   return Object.keys(publicationsGroupedByYear.value).map(p => Number(p)).sort((a, b) => b - a)
 })
 </script>
-
-<style lang="scss">
-div#publication-list {
-  grid-template-rows: fit-content(10rem) 1fr;
-}
-
-@media (min-width: 1024px) {
-  div#publication-list {
-    grid-template-columns: 22.5rem 1fr;
-  }
-}
-</style>
