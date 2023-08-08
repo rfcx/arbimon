03f86763301a3e093fd5462e6447b14ca6d839fb
Somjintana Korbut <nuisomjin@gmail.com>
2023-08-08 14:43:44 +0100

    #1089 Team - host gallery pics in asset folder

--- apps/website/src/landing/team/blocks/team-gallery.vue
+++ apps/website/src/landing/team/blocks/team-gallery.vue
@@ -12,44 +12,54 @@
       <div class="grid grid-cols-2 md:grid-cols-4 gap-6">
         <imageCard
           class="order-first col-span-2"
-          src="https://firebasestorage.googleapis.com/v0/b/rfcx-demo.appspot.com/o/landing%2Fteam%2Fgallery%2F1.webp?alt=media&token=e6db8af4-77bf-466a-a23d-8cdce53aa838"
+          :src="image1"
         />
         <imageCard
           class="md:order-2 order-3"
-          src="https://firebasestorage.googleapis.com/v0/b/rfcx-demo.appspot.com/o/landing%2Fteam%2Fgallery%2F2.webp?alt=media&token=0f1a291b-1ce5-4161-8a8c-9dc88fd5dc30"
+          :src="image2"
         />
         <imageCard
           class="md:(order-3 col-span-1) order-8 col-span-2"
-          src="https://firebasestorage.googleapis.com/v0/b/rfcx-demo.appspot.com/o/landing%2Fteam%2Fgallery%2F3.gif?alt=media&token=5c9fdcd9-3975-410a-88fc-90b878c48ece"
+          :src="image3"
         />
         <imageCard
           class="md:order-4 order-2"
-          src="https://firebasestorage.googleapis.com/v0/b/rfcx-demo.appspot.com/o/landing%2Fteam%2Fgallery%2F5.gif?alt=media&token=00b2ac4f-a702-44e3-9f0e-d7a836411607"
+          :src="image4"
         />
         <imageCard
           class="md:(order-5 row-span-2 h-[440px]) order-6 object-top"
-          src="https://firebasestorage.googleapis.com/v0/b/rfcx-demo.appspot.com/o/landing%2Fteam%2Fgallery%2F5.webp?alt=media&token=0b11f20e-0e4e-4300-8c18-fa8fee26ae1e"
+          :src="image5"
         />
         <imageCard
           class="md:order-6 order-5 col-span-2"
-          src="https://firebasestorage.googleapis.com/v0/b/rfcx-demo.appspot.com/o/landing%2Fteam%2Fgallery%2F6.webp?alt=media&token=3020525c-7416-4e86-908d-dd2553995cef"
+          :src="image6"
         />
         <imageCard
           class="md:(order-7 col-span-1) order-4 col-span-2"
-          src="https://firebasestorage.googleapis.com/v0/b/rfcx-demo.appspot.com/o/landing%2Fteam%2Fgallery%2F7.gif?alt=media&token=f4f19843-d33e-43dc-977c-ffab7f4190f1"
+          :src="image7"
         />
         <imageCard
           class="md:order-8 order-6"
-          src="https://firebasestorage.googleapis.com/v0/b/rfcx-demo.appspot.com/o/landing%2Fteam%2Fgallery%2F8.gif?alt=media&token=c3ddb3ca-55f9-42d3-b4b5-676bdfa80bd7"
+          :src="image8"
         />
         <imageCard
           class="order-last md:col-span-1 col-span-2"
-          src="https://firebasestorage.googleapis.com/v0/b/rfcx-demo.appspot.com/o/landing%2Fteam%2Fgallery%2F9.webp?alt=media&token=78120895-826a-4d61-9609-4d59eb55aeb5"
+          :src="image9"
         />
       </div>
     </div>
   </section>
 </template>
 <script setup lang="ts">
+// TODO: png file might be replaced with gif in the future
+import image1 from '@/_assets/landing/team/gallery/1.webp'
+import image2 from '@/_assets/landing/team/gallery/2.webp'
+import image3 from '@/_assets/landing/team/gallery/3.png'
+import image4 from '@/_assets/landing/team/gallery/4.png'
+import image5 from '@/_assets/landing/team/gallery/5.webp'
+import image6 from '@/_assets/landing/team/gallery/6.webp'
+import image7 from '@/_assets/landing/team/gallery/7.png'
+import image8 from '@/_assets/landing/team/gallery/8.png'
+import image9 from '@/_assets/landing/team/gallery/9.webp'
 import imageCard from '../components/image-card.vue'
 </script>
