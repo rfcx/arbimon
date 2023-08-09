6a38fe925ea79c8497a60a3f3e440a2b4f6e22cc
Somjintana Korbut <nuisomjin@gmail.com>
2023-08-09 12:57:03 +0100

    #1111 Theme - add new colour scheme & font to nav bar & button

--- apps/website/windi.config.ts
+++ apps/website/windi.config.ts
@@ -17,11 +17,11 @@ export default defineConfig({
     plugin(({ addComponents }) => {
       const navbarItems = {
         '.navbar-menu-item.router-link-exact-active,.navbar-menu-item.router-link-active:not(.router-link-needs-exact)': {
-          'box-shadow': 'inset 0 -3px 0 #31984f'
+          'box-shadow': 'inset 0 -3px 0 #ADFF2C'
         },
         '@media (max-width: 699px)': {
           '.navbar-menu-item.router-link-exact-active,.navbar-menu-item.router-link-active:not(.router-link-needs-exact)': {
-            'box-shadow': 'inset 5px 0 0 #31984f'
+            'box-shadow': 'inset 5px 0 0 #ADFF2C'
           }
         },
         '.router-link-active > a > span': {
@@ -55,14 +55,38 @@ export default defineConfig({
         warning: '#FFCD00',
         danger: '#D46767'
       },
+      fontFamily: {
+        header: ['ABCFavoritExpanded'],
+        display: ['ABCFavoritExtended'],
+        eyebrow: ['Roboto Mono'],
+        sans: ['Poppins', 'ui-sans-serif', 'system-ui', '-apple-system', 'system-ui', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji']
+      },
       colors: {
+        // Primary
+        frequency: '#ADFF2C',
+        moss: '#1E1C13',
+        echo: '#14130D',
+        pitch: '#060508',
+        // Secondary
+        spoonbill: '#FFAFAF',
+        flamingo: '#FF6868',
+        palm: '#008059',
+        chirp: '#D2FF8A',
+        // Neutrals
+        insight: '#FFFEFC',
+        cloud: '#F9F6F2',
+        fog: '#E9E6E1',
+        'util-gray-01': '#7F7D78',
+        'util-gray-02': '#4B4B4B',
+        'util-gray-03': '#242424',
+        // Olds
         placeholder: '#999',
         faded: '#333',
-        subtle: '#ffffff90',
+        subtle: '#F9F6F2',
         'brand-primary': {
-          DEFAULT: '#31984F',
-          light: '#E6F3EA',
-          dark: '#25713b'
+          DEFAULT: '#ADFF2C',
+          light: '#bdff56',
+          dark: '#8acc23'
         },
         danger: {
           DEFAULT: '#D46767',
@@ -99,15 +123,16 @@ export default defineConfig({
     range(12).map(i => `md:grid-cols-${i}`)
   ],
   shortcuts: {
-    // button
-    btn: 'py-2 px-4 text-primary rounded-lg shadow-md cursor-pointer bg-box-grey hover:bg-box-grey-dark focus:(ring-0 outline-none) disabled:(bg-box-grey cursor-not-allowed opacity-50)',
-    'btn-primary': 'bg-brand-primary hover:bg-brand-primary-dark',
+    // buttons
+    btn: 'px-5 py-2 text-base text-center text-pitch rounded-full font-display',
+    'btn-primary': 'bg-frequency hover:bg-chirp focus:ring-4 focus:ring-chirp dark:bg-frequency dark:hover:bg-chirp dark:focus:ring-chirp',
+    'btn-secondary': 'rounded-full text-frequency border border-frequency hover:bg-chirp focus:ring-4 focus:ring-chirp dark:text-frequency dark:hover:bg-chirp dark:border-frequency dark:hover:text-pitch dark:hover:border-chirp dark:focus:ring-chirp',
     'btn-warning': 'bg-warning hover:bg-warning-dark',
     'btn-danger': 'bg-danger hover:bg-danger-dark',
-    'btn-icon': 'py-2 px-3',
-    // nav;
-    'navbar-container': 'box-content text-primary flex items-center justify-center h-13',
-    'navbar-item': 'box-content text-primary flex items-center justify-center h-13',
+    'btn-icon': 'py-2 px-3 rounded-full text-frequency border border-frequency hover:bg-chirp hover:text-pitch hover:border-chirp',
+    // nav
+    'navbar-container': 'box-content text-insight flex items-center justify-center h-13',
+    'navbar-item': 'box-content text-insight flex items-center justify-center h-13',
     'navbar-menu-item': 'px-2',
     // input
     'input-field': 'bg-box-grey border text-sm rounded-lg block w-full p-2.5 focus:border-brand-primary bg-mirage-grey text-white'
