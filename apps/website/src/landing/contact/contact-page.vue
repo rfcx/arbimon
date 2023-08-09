b076d01ed27a1ff2c2c2c37fc68a3dda00201d48
Somjintana Korbut <nuisomjin@gmail.com>
2023-08-09 11:42:47 +0100

    #1086 Contact - edit styling of submitted confirmation alert

--- apps/website/src/landing/contact/contact-page.vue
+++ apps/website/src/landing/contact/contact-page.vue
@@ -33,35 +33,37 @@
           </div>
         </div>
         <div class="basis-3/5 w-full">
-          <div v-if="isSubmitted">
-            <div
-              class="flex p-4 mb-4 text-green-800 rounded-xl bg-green-50 dark:bg-gray-800 dark:text-green-400"
-              role="alert"
-            >
-              <svg
-                aria-hidden="true"
-                class="flex-shrink-0 inline w-5 h-5 mr-3"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              ><path
-                fill-rule="evenodd"
-                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-                clip-rule="evenodd"
-              /></svg>
-              <span class="sr-only">Info</span>
-              <div>
-                <span class="font-medium">Submitted!</span> Thank you for contacting us.
-              </div>
-            </div>
-          </div>
           <form
-            v-else
             action="https://webto.salesforce.com/servlet/servlet.WebToCase?encoding=UTF-8"
             method="POST"
             class="space-y-8 p-10 bg-gradient-to-br from-bg-moss to-bg-transparent"
           >
-            <div class="uppercase col-span-2 font-eyebrow text-spoonbill">
+            <div v-if="!isSubmitted">
+              <div
+                class="flex text-chirp"
+                role="alert"
+              >
+                <svg
+                  aria-hidden="true"
+                  class="flex-shrink-0 inline w-5 h-5 mr-3"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                ><path
+                  fill-rule="evenodd"
+                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                  clip-rule="evenodd"
+                /></svg>
+                <span class="sr-only">Info</span>
+                <div>
+                  <span class="font-medium">Submitted!</span> Thank you for contacting us.
+                </div>
+              </div>
+            </div>
+            <div
+              v-else
+              class="uppercase col-span-2 font-eyebrow text-spoonbill"
+            >
               SEND US A MESSAGE
             </div>
             <div class="grid md:grid-cols-2 gap-8">
