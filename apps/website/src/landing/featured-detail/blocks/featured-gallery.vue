8aa182ab838cbf8b67a628ed8d7bf4153da1a7e5
Somjintana Korbut <nuisomjin@gmail.com>
2023-08-11 12:35:20 +0100

    #1100 Gallery - define max height

--- apps/website/src/landing/featured-detail/blocks/featured-gallery.vue
+++ apps/website/src/landing/featured-detail/blocks/featured-gallery.vue
@@ -1,12 +1,11 @@
 <template>
   <section class="bg-white dark:bg-moss">
-    <!-- TODO: bind images -->
-    <div class="py-16 mx-auto xl:gap-8 md:py-8 lg:grid lg:grid-cols-2">
+    <div class="py-16 mx-auto gap-2 xl:gap-8 md:py-8 lg:grid lg:grid-cols-2">
       <img
         v-for="(image, index) in images"
         :key="index"
         :src="image"
-        class="w-full h-full rounded-lg object-cover"
+        class="w-full h-max-[369px] rounded-lg object-cover"
       >
     </div>
   </section>
