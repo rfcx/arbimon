5ddc963c3c394cfffc8afcff42a807d8b2aca591
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-08-11 18:08:40 +0700

    #1101 margin fix for the feature-stat

--- apps/website/src/landing/featured-detail/components/featured-stat.vue
+++ apps/website/src/landing/featured-detail/components/featured-stat.vue
@@ -2,7 +2,7 @@
   <div class="mb-2 uppercase text-spoonbill font-eyebrow text-xs col-span-1">
     {{ title }}
   </div>
-  <p class="mb-4 text-gray-900 dark:text-insight col-span-2">
+  <p class="mb-6 text-gray-900 dark:text-insight col-span-2">
     <slot />
   </p>
 </template>
