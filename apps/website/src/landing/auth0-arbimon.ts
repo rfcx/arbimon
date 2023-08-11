5e09e9829f4caab92e1913aa54b6ccb5321782a8
Antony Harfield <antonyharfield@hotmail.com>
2023-08-11 21:01:18 +0100

    Ingress for login

--- apps/website/src/landing/auth0-arbimon.ts
+++ apps/website/src/landing/auth0-arbimon.ts
@@ -1,7 +1,7 @@
 const ARBIMON_BASE_URL = import.meta.env.VITE_ARBIMON_BASE_URL
 const arbimonClientId = 'eh2NHbG6hOVjHMGxkmCHGe307sLmKGKb'
 const audience = 'https://rfcx.org'
-const redirectUri = `${ARBIMON_BASE_URL}/login`
+const redirectUri = `${ARBIMON_BASE_URL}/auth0-login`
 const universalLoginUrl = `https://auth.rfcx.org/authorize?audience=${audience}&scope=openid%20email%20profile%20offline_access` +
     `&response_type=code&client_id=${arbimonClientId}&redirect_uri=${redirectUri}&theme=dark`
 
