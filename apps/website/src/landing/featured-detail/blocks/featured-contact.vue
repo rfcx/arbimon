17ab6598addf0f7cda5bbf4ff3b3554d5bac15c7
Somjintana Korbut <nuisomjin@gmail.com>
2023-08-11 12:24:23 +0100

    #1101 CTA - add a, an infront of project category name

--- apps/website/src/landing/featured-detail/blocks/featured-contact.vue
+++ apps/website/src/landing/featured-detail/blocks/featured-contact.vue
@@ -4,7 +4,7 @@
   <contact-card hero-background="hero-cta">
     <div class="flex flex-col gap-2">
       <h2 class="font-medium font-header">
-        Working on <span class="lowercase">{{ projectName }}</span> project?
+        Working on <span class="lowercase">{{ text }}</span> project?
       </h2>
       <p>
         <router-link
@@ -18,11 +18,18 @@
   </contact-card>
 </template>
 <script setup lang="ts">
+import { computed } from 'vue'
+
 import ContactCard from '@/_layout/components/landing-cta-card/contact-card-full-background.vue'
 import { ROUTE_NAMES } from '~/router'
 
-defineProps<{
+const props = defineProps<{
   readonly projectName: string
 }>()
+
+const text = computed(() => {
+  const isStartWithVowel = ['a', 'e', 'i', 'o', 'u'].some((e) => e === props.projectName.charAt(0).toLowerCase())
+  return isStartWithVowel ? `an ${props.projectName}` : `a ${props.projectName}`
+})
 
 </script>
