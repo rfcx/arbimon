7cb9bee522c34dccb18cdd50d24db1440b8a3c63
Somjintana Korbut <nuisomjin@gmail.com>
2023-08-11 13:43:27 +0100

    Mail - update form

--- apps/website/src/_services/mailchimp/index.ts
+++ apps/website/src/_services/mailchimp/index.ts
@@ -1,2 +1,2 @@
-export const formMainAudience = 'https://rfcx.us9.list-manage.com/subscribe/post?u=979004c94b83bcd925931d541&amp;id=26545eff0f&amp;f_id=86350aa3cf'
-export const tagWebsiteSubscriber = '12736205'
+export const formMainAudience = 'https://rfcx.us9.list-manage.com/subscribe/post?u=979004c94b83bcd925931d541&id=86350aa3cf'
+export const tagWebsiteSubscriber = '12736205' // TODO: need to confirm with marketing team
