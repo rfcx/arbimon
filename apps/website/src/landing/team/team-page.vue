10f5a4ccefcbdbeccfe0866fd880df7c0f750e47
Somjintana Korbut <nuisomjin@gmail.com>
2023-08-11 14:13:24 +0100

    Footer contact added to Contact and About us pages

--- apps/website/src/landing/team/team-page.vue
+++ apps/website/src/landing/team/team-page.vue
@@ -4,8 +4,10 @@
   <team-member-list />
   <team-gallery />
   <team-cta />
+  <footer-contact />
 </template>
 <script setup lang="ts">
+import FooterContact from '@/_layout/components/landing-footer-contact.vue'
 import teamCta from './blocks/team-cta.vue'
 import teamGallery from './blocks/team-gallery.vue'
 import teamHero from './blocks/team-hero.vue'
