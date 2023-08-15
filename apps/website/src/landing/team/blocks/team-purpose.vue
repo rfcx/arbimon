9a58da99ceeeadc19675faa32f4dc25b1096c866
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-08-15 18:24:50 +0700

    add paragraph spacing

--- apps/website/src/landing/team/blocks/team-purpose.vue
+++ apps/website/src/landing/team/blocks/team-purpose.vue
@@ -7,9 +7,15 @@
             Our purpose
           </h1>
           <div class="text-base lg:text-lg">
-            <p>We aim to safeguard and nurture Earth's ecosystems for every creature that calls it home. By harnessing the power of sound, we unlock a deeper understanding of biodiversity, vital for conservation efforts.</p>
-            <p>With cutting-edge AI technology and acoustic innovation, Arbimon empowers individuals and organizations to transform sound into actionable insights.</p>
-            <p>Collaboration is at the heart of our approach. Arbimon is open-source, and we actively engage with ecologists, foundations, governments, corporations, and local communities to extend the reach and impact of our initiatives.</p>
+            <p class="pb-3 last:pb-0">
+              We aim to safeguard and nurture Earth's ecosystems for every creature that calls it home. By harnessing the power of sound, we unlock a deeper understanding of biodiversity, vital for conservation efforts.
+            </p>
+            <p class="pb-3 last:pb-0">
+              With cutting-edge AI technology and acoustic innovation, Arbimon empowers individuals and organizations to transform sound into actionable insights.
+            </p>
+            <p class="pb-3 last:pb-0">
+              Collaboration is at the heart of our approach. Arbimon is open-source, and we actively engage with ecologists, foundations, governments, corporations, and local communities to extend the reach and impact of our initiatives.
+            </p>
           </div>
         </div>
         <div class="basis-7/12">
