c5e943e614320b2f6403f28ba76954f8e3fbefb1
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-08-22 14:39:02 +0700

    #1065 separate page to components

---
+++ apps/website/src/detect/cnn-job-detail-by-species/components/job-detail-header.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="job-detail-by-species-header-wrapper flex justify-between">
+    <el-breadcrumb
+      class="job-detail-breadcrumb"
+      separator="/"
+    >
+      <el-breadcrumb-item :to="{ name: ROUTE_NAMES.cnnJobList }">
+        CNN Jobs
+      </el-breadcrumb-item>
+      <el-breadcrumb-item :to="{ name: ROUTE_NAMES.cnnJobDetail, params: { jobId } }">
+        <span class="text-subtle">{{ jobId }}</span>
+      </el-breadcrumb-item>
+      <el-breadcrumb-item>
+        <span class="text-subtle">{{ speciesSlug }}</span>
+      </el-breadcrumb-item>
+    </el-breadcrumb>
+    <div class="job-btn-container">
+      <el-button
+        type="info"
+        size="large"
+        class="rounded-md uppercase font-semibold"
+      >
+        JOB DETAIL
+      </el-button>
+      <el-button
+        type="info"
+        size="large"
+        class="rounded-md"
+      >
+        <icon-fa-ellipsis-h />
+      </el-button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+import { ROUTE_NAMES } from '~/router'
+
+const route = useRoute()
+const jobId = computed(() => typeof route.params.jobId === 'string' ? parseInt(route.params.jobId) : -1)
+const speciesSlug = computed(() => typeof route.params.speciesSlug === 'string' ? route.params.speciesSlug : '')
+</script>
