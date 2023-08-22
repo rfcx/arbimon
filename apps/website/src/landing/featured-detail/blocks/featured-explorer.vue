7584e85bb372d1153ac006ed63e3d5d5174f39a1
Somjintana Korbut <nuisomjin@gmail.com>
2023-08-22 11:11:05 +0100

    #1126 Feature detail - change font and sizing for exploring other project cards

--- apps/website/src/landing/featured-detail/blocks/featured-explorer.vue
+++ apps/website/src/landing/featured-detail/blocks/featured-explorer.vue
@@ -1,14 +1,14 @@
 <template>
   <section class="bg-white dark:bg-echo">
-    <div class="py-30 px-4 mx-auto xl:gap-16 lg:px-6 lg:pb-60">
-      <h2 class="text-center mb-20">
+    <div class="py-30 mx-auto xl:gap-16 lg:pb-60">
+      <h2 class="text-center mb-20 spx-4 lg:px-6">
         Explore our featured projects
       </h2>
       <div class="grid grid-row gap-1 text-gray-500 dark:text-insight md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
         <div
           v-for="project in props.projects"
           :key="project.id"
-          class="flex justify-center items-center w-full h-32 relative"
+          class="flex justify-center items-center w-full h-45 relative"
         >
           <router-link
             :to="'/featured/' + project.category.id"
@@ -21,8 +21,8 @@
               :class="isSelctedProject(project.category.id) ? 'opacity-100' : 'opacity-50'"
             >
             <p
-              class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-display rounded-full px-4 py-2 text-center"
-              :class="isSelctedProject(project.category.id) ? 'text-frequency font-bold bg-pitch' : 'text-gray-500 dark:text-insight'"
+              class="min-w-2/3 max-w-5/6 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-header rounded-full p-4 text-center break-normal"
+              :class="isSelctedProject(project.category.id) ? 'text-frequency bg-pitch' : 'text-gray-500 dark:text-insight'"
             >
               {{ project.category.name }}
             </p>
