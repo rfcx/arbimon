7ea2e29a8a862c8bb89b020bfe8fce8fe9b40dac
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-08-22 14:34:32 +0700

    #1065 api changes to accept enabled status

--- apps/website/src/detect/_composables/use-classifier.ts
+++ apps/website/src/detect/_composables/use-classifier.ts
@@ -4,9 +4,12 @@ import { type UseQueryReturnType, useQuery } from 'vue-query'
 
 import { type ClassifierParams, type ClassifierQueryParams, type ClassifierResponse, apiBioGetClassifier } from '@rfcx-bio/common/api-bio/classifiers/classifier'
 
-export const useClassifier = (apiClient: AxiosInstance, params: ComputedRef<ClassifierParams>, query: ClassifierQueryParams): UseQueryReturnType<ClassifierResponse | undefined, unknown> => {
+export const useClassifier = (apiClient: AxiosInstance, params: ComputedRef<ClassifierParams>, query: ClassifierQueryParams, enabled: ComputedRef<boolean>): UseQueryReturnType<ClassifierResponse, unknown> => {
   return useQuery(
     ['fetch-classifier', params],
-    async () => await apiBioGetClassifier(apiClient, params.value, query)
+    async () => await apiBioGetClassifier(apiClient, params.value, query),
+    {
+      enabled
+    }
   )
 }
