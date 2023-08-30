ee9dca7bce69cf51c9aadf613677def1eac4c0ec
Somjintana Korbut <nuisomjin@gmail.com>
2023-08-30 22:54:23 +0100

    #1126 feature detail - change aspect ratio of image #2 and #3 to 2:1

--- apps/website/src/landing/featured-detail/blocks/featured-gallery.vue
+++ apps/website/src/landing/featured-detail/blocks/featured-gallery.vue
@@ -1,16 +1,20 @@
 <template>
   <section class="bg-white dark:bg-moss">
     <div class="py-16 mx-auto gap-2 xl:gap-8 md:py-8 grid lg:grid-cols-2">
-      <img
+      <div
         v-for="(image, index) in images"
         :key="index"
-        :src="image"
-        class="w-full object-cover"
-        :class="{
-          'lg:rounded-r-lg': index/2 === 0,
-          'lg:rounded-l-lg': index/2 !== 0
-        }"
+        class="relative pb-[50%] h-0"
       >
+        <img
+          :src="image"
+          class="absolute w-full h-full"
+          :class="{
+            'lg:rounded-r-lg': index/2 === 0,
+            'lg:rounded-l-lg': index/2 !== 0
+          }"
+        >
+      </div>
     </div>
   </section>
 </template>
