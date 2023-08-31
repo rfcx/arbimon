75b3bd0cf23766d327287588f656e094b8041112
Somjintana Korbut <nuisomjin@gmail.com>
2023-08-31 14:23:15 +0100

    Bring back APIs that accidentally got removed 🫣

--- apps/api/src/_services/api-core/api-core.ts
+++ apps/api/src/_services/api-core/api-core.ts
@@ -77,6 +77,27 @@ async function getMemberProjectCoreIdsFromApiPaged (token: string, limit: number
   }
 }
 
+export async function createProject (project: Pick<CoreProject, 'name' | 'is_public'>, token: string): Promise<string> {
+  const response = await axios.request<unknown>({
+    method: 'POST',
+    url: `${CORE_API_BASE_URL}/projects`,
+    headers: { authorization: token },
+    data: project
+  }).catch(unpackAxiosError)
+  const id = response.headers.location.split('/').pop()
+  if (id === undefined) throw new Error('Create project failed: no core id')
+  return id
+}
+
+export async function getProject (id: string, token: string): Promise<Pick<CoreProjectLight, 'external_id'>> {
+  const response = await axios.request<Pick<CoreProjectLight, 'external_id'>>({
+    method: 'GET',
+    url: `${CORE_API_BASE_URL}/projects/${id}?fields=external_id`,
+    headers: { authorization: token }
+  }).catch(unpackAxiosError)
+  return response.data
+}
+
 export async function updateDetectionReviewFromApi (token: string, classifierJobId: number, data: DetectReviewDetectionBody): Promise<DetectReviewDetectionResponse> {
   try {
     const resp = await axios.request<DetectReviewDetectionResponse>({
