a116cc498a6e09628a36d140d5a666bf91b50336
Somjintana Korbut <nuisomjin@gmail.com>
2023-09-12 10:40:56 +0100

    #1137 #1140 Insights - styling the insight header

--- apps/website/src/insights/insights-root.vue
+++ apps/website/src/insights/insights-root.vue
@@ -1,56 +1,48 @@
 <template>
-  <nav
-    class="bg-gray-50 dark:bg-steel-gray-light"
-  >
-    <div class="max-w-screen-xl mx-auto px-2 sm:px-8 text-gray-900 dark:text-white">
-      <h1 class="text-4xl pt-16 pb-1 font-bold">
-        {{ store.selectedProject?.name }}
-      </h1>
-      <h2 class="text-lg text-light-900 pb-4">
-        Project one-line summary
-      </h2>
-      <div class="pb-4">
-        <a
-          href="#citation"
-          class="text-sm text-light-900 hover:underline hover:text-light-300"
-        ><svg
-          aria-hidden="true"
-          focusable="false"
-          class="h-4 mr-2 inline"
-          role="img"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 448 512"
-        ><path
-          fill="currentColor"
-          d="M96 0C43 0 0 43 0 96V416c0 53 43 96 96 96H384h32c17.7 0 32-14.3 32-32s-14.3-32-32-32V384c17.7 0 32-14.3 32-32V32c0-17.7-14.3-32-32-32H384 96zm0 384H352v64H96c-17.7 0-32-14.3-32-32s14.3-32 32-32zm32-240c0-8.8 7.2-16 16-16H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16zm16 48H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16s7.2-16 16-16z"
-        /></svg>Cite this project</a>
-      </div>
-    </div>
-  </nav>
-  <nav class="sticky top-0 z-40 bg-gray-50 dark:bg-steel-gray-light">
-    <div class="max-w-screen-xl mx-auto px-2 sm:px-8">
-      <div class="font-medium text-center text-gray-500 dark:text-gray-300">
-        <ul class="flex flex-wrap -mb-px">
-          <li
-            v-for="(item, index) in items"
-            :key="item.title"
-            class="mr-2"
-          >
-            <router-link
-              :to="item.route"
-              :active-class="index > 0 ? 'text-primary border-primary dark:text-primary dark:border-primary hover:text-primary hover:border-primary dark:hover:text-primary' : ''"
-              :exact-active-class="index === 0 ? 'text-primary border-primary dark:text-primary dark:border-primary hover:text-primary hover:border-primary dark:hover:text-primary' : ''"
-              class="inline-block p-4 border-b-4 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-200 dark:hover:text-gray-200"
-            >
-              {{ item.title }}
-            </router-link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
   <div
-    class="max-w-screen-xl mx-auto px-2 sm:px-8 py-8"
+    class="bg-gray-50 dark:bg-hero-cta-frog-bg bg-cover border-b-1 border-fog"
+  >
+    <div class="max-w-screen-xl mx-auto px-8 md:px-10 pt-20 pb-10 text-gray-900 dark:text-insight flex flex-col md:flex-row justify-between">
+      <div class="">
+        <h1 class="pb-4 text-frequency font-header">
+          {{ store.selectedProject?.name }}
+        </h1>
+        <h6 class="text-insight text-sm pb-4 max-w-100">
+          One line summary about the project. Some context such as timeline, goals. Some context such as timeline, goals.
+        </h6>
+        <div>
+          Puerto Rico 🇵🇷
+        </div>
+      </div>
+      <div class="justify-self-end order-first md:order-last">
+        <span class="text-xxs text-fog">// TODO: menu</span>
+      </div>
+    </div>
+    <nav class="sticky top-0 z-40">
+      <div class="max-w-screen-xl mx-auto px-8">
+        <div class="text-center text-gray-500 dark:text-insight">
+          <ul class="flex flex-wrap -mb-px">
+            <li
+              v-for="(item, index) in items"
+              :key="item.title"
+              class="mr-3"
+            >
+              <router-link
+                :to="item.route"
+                :active-class="index > 0 ? 'text-insight border-frequency dark:text-frequency dark:border-frequency hover:text-insight hover:border-frequency dark:hover:text-insight' : ''"
+                :exact-active-class="index === 0 ? 'text-insight border-frequency dark:text-frequency dark:border-frequency hover:text-insight hover:border-frequency dark:hover:text-insight' : ''"
+                class="inline-block pt-3 px-3 pb-1.5 border-b-3 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-200 dark:hover:text-gray-200"
+              >
+                {{ item.title }}
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+  <div
+    class="max-w-screen-xl mx-auto px-6 md:px-10 py-10"
   >
     <router-view />
   </div>
