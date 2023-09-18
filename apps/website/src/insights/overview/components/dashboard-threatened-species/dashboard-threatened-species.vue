7bdd328171aeb55b1d7037a616d188a2d2296e50
Somjintana Korbut <nuisomjin@gmail.com>
2023-09-18 22:13:31 +0100

    #1136 fix build & remove unused file

--- apps/website/src/insights/overview/components/dashboard-threatened-species/dashboard-threatened-species.vue
+++ apps/website/src/insights/overview/components/dashboard-threatened-species/dashboard-threatened-species.vue
@@ -13,10 +13,19 @@
 
 <script setup lang="ts">
 
+import { type RiskRatingUi } from '~/risk-ratings'
 import { ROUTE_NAMES } from '~/router'
 import { useStore } from '~/store'
-import { type ThreatenedSpeciesRow } from '../types'
 import speciesCard from './components/species-card.vue'
+
+export interface ThreatenedSpeciesRow {
+  slug: string
+  taxonSlug: string
+  scientificName: string
+  commonName?: string
+  photoUrl: string
+  riskRating: RiskRatingUi
+}
 
 const store = useStore()
 
