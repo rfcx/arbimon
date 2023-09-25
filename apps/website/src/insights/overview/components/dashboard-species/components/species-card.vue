1507e724255f60795ebcf6e405917da9258a4c3b
Somjintana Korbut <nuisomjin@gmail.com>
2023-09-25 15:57:31 +0100

    #1151 fix species card style to support longer species name

--- apps/website/src/insights/overview/components/dashboard-species/components/species-card.vue
+++ apps/website/src/insights/overview/components/dashboard-species/components/species-card.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="border-1 border-frequency rounded-2xl flex h-100 flex-col bg-moss">
+  <div class="border-1 border-frequency rounded-2xl flex h-80 xl:h-116 flex-col bg-moss">
     <img
       :src="item.photoUrl"
       :alt="item.scientificName"
-      class="w-full h-2/3 bg-util-gray-03 rounded-t-2xl object-cover border-b-frequency border-b-1"
+      class="w-full h-1/2 xl:h-2/3 bg-util-gray-03 rounded-t-2xl object-cover border-b-frequency border-b-1"
     >
-    <div class="flex flex-col h-1/3 gap-y-4 p-4 justify-between">
+    <div class="flex flex-col h-1/2 xl:h-1/3 gap-y-4 p-4 justify-between">
       <div>
         <div class="text-spoonbill font-eyebrow text-sm uppercase">
           {{ item.commonName }}
