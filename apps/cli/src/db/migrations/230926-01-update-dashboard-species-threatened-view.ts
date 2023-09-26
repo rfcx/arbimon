891a670ee2386d65292b03576e744765b06a7047
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-09-26 16:21:11 +0700

    #1162 new view schema to not have duplicate species
    on a species that have multiple image sources

---
+++ apps/cli/src/db/migrations/230926-01-update-dashboard-species-threatened-view.ts
@@ -0,0 +1,33 @@
+/**
+ * WARNING: MIGRATIONS ARE IMMUTABLE
+ * Do not depend on imported code which may change
+ */
+
+import { type QueryInterface } from 'sequelize'
+import { type MigrationFn } from 'umzug'
+
+const VIEW_NAME = 'dashboard_species_threatened'
+
+export const up: MigrationFn<QueryInterface> = async (params): Promise<void> => {
+  await params.context.sequelize.query(
+    `
+    CREATE OR REPLACE VIEW ${VIEW_NAME} AS
+      SELECT
+        sip.taxon_species_id,
+        sip.taxon_class_slug,
+        sip.taxon_species_slug,
+        sip.scientific_name,
+        sip.risk_rating_id,
+        sip.common_name,
+        sip.photo_url
+      FROM species_in_project sip
+      JOIN risk_rating_iucn rri on sip.risk_rating_id = rri.id
+      WHERE rri.is_threatened
+      ORDER BY sip.risk_rating_id DESC, sip.scientific_name;
+    `
+  )
+}
+
+export const down: MigrationFn<QueryInterface> = async (params) => {
+  await params.context.sequelize.query(`DROP VIEW IF EXISTS ${VIEW_NAME};`)
+}
