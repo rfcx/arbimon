acdcb83a33b8e70b4b2e1b158e94af9b50caee05
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-09-26 14:34:09 +0700

    #1135 add comment to note that this is beta component

--- apps/website/src/insights/overview/components/dashboard-metrics/components/numeric-metric-error.vue
+++ apps/website/src/insights/overview/components/dashboard-metrics/components/numeric-metric-error.vue
@@ -1,4 +1,4 @@
-<!-- NOTE: This is just a temporary style while I am waiting on comments on how should I design this from Tiffany -->
+<!-- TODO: This is just a temporary style while I am waiting on comments on how should I design this from Tiffany -->
 <template>
   <div class="rounded-xl dark:bg-flamingo dark:text-insight p-6 border-1 border-insight">
     <p class="font-bold text-4xl <sm:text-2xl">
