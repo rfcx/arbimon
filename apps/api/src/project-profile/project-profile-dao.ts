7f8601e1f32ef0bdaa36594b4cad79ed7de25c85
Somjintana Korbut <nuisomjin@gmail.com>
2023-09-27 20:09:23 +0100

    #1154 API - api to update project profile (summary or readme)

--- apps/api/src/project-profile/project-profile-dao.ts
+++ apps/api/src/project-profile/project-profile-dao.ts
@@ -12,15 +12,10 @@ export const getProjectProfile = async (locationProjectId: number): Promise<Loca
       raw: true
     }) ?? { locationProjectId, summary: '', readme: '' }
 
-export const updateProjectProfile = async (locationProjectId: number, summary: string, readme: string): Promise<LocationProjectProfile | undefined> => {
-  let updated
-  const locationProjectProfile = await ModelRepository.getInstance(getSequelize())
-    .LocationProjectProfile
-    .findOne({ where: { locationProjectId } })
-
-  if (locationProjectProfile) {
-    updated = await locationProjectProfile.update({ summary, readme })
-  }
-
+export const updateProjectProfile = async (locationProjectId: number, summary: string | undefined, readme: string | undefined): Promise<LocationProjectProfile | undefined> => {
+  const locationProjectProfile = ModelRepository.getInstance(getSequelize()).LocationProjectProfile
+  const res = await locationProjectProfile.update({ summary, readme }, { where: { locationProjectId }, returning: true })
+  if (res[0] === 0) return undefined
+  const updated = res[1][0].get({ plain: true })
   return updated
 }
--- apps/api/src/project-profile/project-profile-update-handler.ts
+++ apps/api/src/project-profile/project-profile-update-handler.ts
@@ -1,16 +1,14 @@
-import { type ProjectProfileParams, type ProjectProfileResponse } from '@rfcx-bio/common/api-bio/project-profile/project-profile'
+import { type ProjectProfileParams, type ProjectProfileResponse, type ProjectProfileUpdateBody } from '@rfcx-bio/common/api-bio/project-profile/project-profile'
 
 import { type Handler } from '~/api-helpers/types'
 import { BioInvalidPathParamError } from '~/errors'
 import { assertPathParamsExist } from '~/validation'
-import { getProjectProfile } from './project-profile-dao'
+import { updateProjectProfile } from './project-profile-dao'
 
-export const projectProfileUpdateHandler: Handler<ProjectProfileResponse, ProjectProfileParams> = async (req) => {
-  // TODO: validate input
-  // TODO: validate user permissions
-  // TODO: update project profile
+export const projectProfileUpdateHandler: Handler<ProjectProfileResponse, ProjectProfileParams, unknown, ProjectProfileUpdateBody> = async (req) => {
   // Inputs & validation
   const { projectId } = req.params
+  const { summary, readme } = req.body
   assertPathParamsExist({ projectId })
 
   const projectIdInteger = Number(projectId)
@@ -18,6 +16,12 @@ export const projectProfileUpdateHandler: Handler<ProjectProfileResponse, Projec
     throw BioInvalidPathParamError({ projectId })
   }
 
-  const projectContent = await getProjectProfile(projectIdInteger)
-  return projectContent
+  if (summary === undefined && readme === undefined) {
+    throw new Error('Either summary or readme must be provided')
+  }
+
+  // TODO: validate user permissions with Core API
+
+  const projectContent = await updateProjectProfile(projectIdInteger, summary, readme)
+  return { summary: projectContent?.summary ?? '', readme: projectContent?.readme ?? '' }
 }
