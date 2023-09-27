89b051ad703038c8c956f393d177c70fc780d82b
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2023-09-28 00:12:26 +0300

    #1163 Add custom icons for the analysis section

--- apps/website/src/projects/components/dashboard-analyses.vue
+++ apps/website/src/projects/components/dashboard-analyses.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="grid grid-cols-1 gap-2 bg-stone-900 border-1 border-orange-100 rounded-2xl shadow p-6 dark:border-orange-100 h-46">
-    <div class="h-8">
+    <div class="h-8 flex items-center space-x-2 sm:space-x-3">
+      <icon-custom-fi-pm
+        v-if="analysis.iconName === 'fi-pm'"
+        class="text-white"
+      />
+      <icon-custom-fi-soundscape
+        v-if="analysis.iconName === 'fi-soundscape'"
+      />
+      <icon-custom-fi-rfm
+        v-if="analysis.iconName === 'fi-rfm'"
+      />
+      <icon-custom-fi-aed
+        v-if="analysis.iconName === 'fi-aed'"
+      />
       <a
         class="text-xl font-medium text-frequency font-header cursor-pointer border-b-1 border-frequency"
         :href="analysis.link"
