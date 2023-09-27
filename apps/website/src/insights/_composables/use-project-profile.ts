86c24a0659bf457fba51390ac795af23d9c19eab
Somjintana Korbut <nuisomjin@gmail.com>
2023-09-27 22:51:51 +0100

    #1154 UI - call update project profile API when the user click save

--- apps/website/src/insights/_composables/use-project-profile.ts
+++ apps/website/src/insights/_composables/use-project-profile.ts
@@ -1,12 +1,22 @@
 import { type AxiosInstance } from 'axios'
-import { type UseQueryReturnType, useQuery } from 'vue-query'
+import { type UseMutationReturnType, type UseQueryReturnType, useMutation, useQuery } from 'vue-query'
 
-import { type ProjectProfileResponse, apiBioGetProjectProfileData } from '@rfcx-bio/common/api-bio/project-profile/project-profile'
+import { type ProjectProfileResponse, type ProjectProfileUpdateBody, apiBioGetProjectProfileData, apiBioUpdateProjectProfileData } from '@rfcx-bio/common/api-bio/project-profile/project-profile'
 
 export const useGetProjectProfile = (apiClient: AxiosInstance, projectId: number): UseQueryReturnType<ProjectProfileResponse, unknown> => {
   return useQuery({
     queryKey: ['get-project-profile'],
     queryFn: async () => await apiBioGetProjectProfileData(apiClient, projectId),
     staleTime: 10 * 60 * 1000 // 10 minutes
+  })
+}
+
+export const useUpdateProjectProfile = (apiClient: AxiosInstance, projectId: number): UseMutationReturnType<ProjectProfileResponse | undefined, unknown, ProjectProfileUpdateBody, unknown, unknown> => {
+  return useMutation({
+    mutationKey: ['update-project-profile'],
+    mutationFn: async (projectProfile: ProjectProfileUpdateBody) => {
+      const { summary, readme } = projectProfile
+      return await apiBioUpdateProjectProfileData(apiClient, projectId, { summary, readme })
+    }
   })
 }
