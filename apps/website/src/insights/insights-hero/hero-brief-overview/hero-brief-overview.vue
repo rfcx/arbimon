e8ee7001623b975ad8f950a0a5e0fe43337b59c1
Somjintana Korbut <nuisomjin@gmail.com>
2023-09-27 22:51:51 +0100

    #1154 UI - call update project profile API when the user click save

--- apps/website/src/insights/insights-hero/hero-brief-overview/hero-brief-overview.vue
+++ apps/website/src/insights/insights-hero/hero-brief-overview/hero-brief-overview.vue
@@ -8,11 +8,18 @@
     @click="enterEditingMode"
   >
     <h6
-      class="text-insight text-sm pb-4 max-w-100"
+      v-if="text"
+      class="text-insight pb-4 max-w-100 min-h-10"
     >
-      {{ defaultText }}
+      {{ text }}
     </h6>
-    <button class="w-2 h-2">
+    <h6
+      v-else
+      class="text-fog pb-4 max-w-100 min-h-10"
+    >
+      {{ PLACEHOLDER_TEXT }}
+    </h6>
+    <button class="w-2 h-2 ml-2">
       <icon-custom-fi-edit
         v-if="isHovering && !isEditing && canEdit"
         @click="isEditing = true"
@@ -24,15 +31,20 @@
     class="flex flex-col lg:flex-row gap-2 items-start"
   >
     <textarea
-      v-model="text"
-      class="text-pitch rounded-md p-1 w-100 h-12 border-util-gray-03 text-sm"
+      v-model.lazy.trim="text"
+      :placeholder="PLACEHOLDER_TEXT"
+      class="text-pitch rounded-md p-1 w-100 h-12 border-util-gray-03"
     />
     <div class="flex gap-2">
       <button
-        class="btn btn-primary py-1 flex-0"
-        @click="exitEdittingMode"
+        class="btn btn-primary py-1 flex-0 inline-flex items-center"
+        @click="save"
       >
-        Save
+        <span v-if="isLoading">
+          <icon-fas-spinner class="animate-spin" />
+          <span class="sr-only">Loading...</span>
+        </span>
+        <span v-else>Save</span>
       </button>
       <button
         class="btn btn-secondary py-1 flex-0"
@@ -45,7 +57,20 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { type AxiosInstance } from 'axios'
+import { inject, ref, watch } from 'vue'
+
+import { apiClientBioKey } from '@/globals'
+import { useDashboardStore, useStore } from '~/store'
+import { useUpdateProjectProfile } from '../../_composables/use-project-profile'
+
+const PLACEHOLDER_TEXT = 'One line summary about the project. Some context such as timeline, goals. Some context such as timeline, goals.'
+
+const apiClientBio = inject(apiClientBioKey) as AxiosInstance
+const store = useStore()
+const dashboardStore = useDashboardStore()
+
+const { isLoading, mutate: mutateProjectProfile } = useUpdateProjectProfile(apiClientBio, store.selectedProject?.id ?? -1)
 
 const props = defineProps<{
   defaultText: string
@@ -57,6 +82,10 @@ const isHovering = ref(false)
 
 const text = ref(props.defaultText)
 
+watch(() => props.defaultText, (newVal) => {
+  text.value = newVal
+})
+
 const enterEditingMode = () => {
   if (!props.canEdit) { return }
   isEditing.value = true
@@ -67,4 +96,19 @@ const exitEdittingMode = () => {
   isEditing.value = false
   isHovering.value = false
 }
+
+const save = () => {
+  mutateProjectProfile({ summary: text.value }, {
+    onSuccess: (res) => {
+      if (res?.summary) {
+        dashboardStore.updateProjectSummary(res?.summary)
+      }
+      exitEdittingMode()
+    },
+    onError: () => {
+      // TODO: show error message
+    }
+  })
+}
+
 </script>
