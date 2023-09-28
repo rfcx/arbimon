668aacfce49ffd47bcc992adb819febb4296f51e
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2023-09-28 14:50:01 +0300

    #1147 Add backdrop styles for the cta popup

--- apps/website/src/insights/components/cta-card.vue
+++ apps/website/src/insights/components/cta-card.vue
@@ -1,38 +1,50 @@
 <template>
-  <div class="fixed top-0 left-0 right-0 z-50 w-full bg-opacity-70 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full justify-center items-center flex mx-auto">
+  <div class="fixed top-0 left-0 right-0 z-50 w-full custom-backdrop overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full justify-center items-center flex mx-auto">
     <div class="relative w-full max-w-sm max-h-full">
-      <div class="relative rounded-md shadow p-7 bg-moss border-1 border-frequency">
-        <h2 class="text-xl font-header text-insight text-center">
+      <div class="relative rounded-md shadow p-6 bg-moss border-1 border-frequency">
+        <h2 class="text-2xl font-header text-insight text-center">
           Uh oh!
         </h2>
-        <div class="text-left text-sm pt-8 text-insight">
+        <div class="text-left text-base pt-6 text-insight">
           Your Insight dashboard is not ready yet.
         </div>
-        <div class="text-left text-sm pt-1 text-insight">
+        <div class="text-left text-base pt-1 text-insight">
           Here's what you need to do:
         </div>
-        <ul class="list-inside list-circle text-sm">
+        <ul class="list-inside list-circle text-base">
           <li class="pt-2">
-            Ran an audio analysis
+            Run an audio analysis
           </li>
           <li>
             Validate the result
           </li>
         </ul>
         <button
-          class="btn btn-primary h-10 mr-0 py-0 grow-0 mt-8 w-full"
+          class="btn btn-primary h-8 mr-0 py-0 grow-0 mt-6 w-full"
           type="button"
-          title="'Return to Audio Analysis'"
+          title="Return to Audio Analysis"
         >
-          <router-link :to="{ name: ROUTE_NAMES.dashboard }">
+          <a
+            :href="analysisUrl"
+            target="_blank"
+          >
             Return to Audio Analysis
-          </router-link>
+          </a>
         </button>
       </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import { ROUTE_NAMES } from '~/router'
-// TODO: change to analysis page
+import { computed } from 'vue'
+
+import { useStore } from '~/store'
+
+const store = useStore()
+
+const selectedProject = computed(() => store.selectedProject)
+
+const BASE_URL = import.meta.env.VITE_ARBIMON_BASE_URL
+const analysisUrl = `${BASE_URL}/project/${selectedProject.value?.slug}/analysis`
+
 </script>
