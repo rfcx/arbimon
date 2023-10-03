f320d9414f53f61fd265a97d11a505201266d462
Somjintana Korbut <nuisomjin@gmail.com>
2023-10-03 14:48:07 +0100

    #1176 changing bar chart colour (species by risk and species by taxon)

--- apps/website/src/_services/risk-ratings/index.ts
+++ apps/website/src/_services/risk-ratings/index.ts
@@ -10,13 +10,13 @@ export interface RiskRatingUi {
 export const DEFAULT_RISK_RATING_ID = -1
 export const RISKS_BY_ID: Record<number, RiskRatingUi> = {
   [DEFAULT_RISK_RATING_ID]: { code: 'NL', label: 'Not Listed', color: '#AAAAAA' },
-  0: { code: 'NE', label: 'Not Evaluated', color: '#888888' },
-  100: { code: 'DD', label: 'Data Deficient', color: '#755F85' },
-  200: { code: 'LC', label: 'Least Concern', color: '#2F6E61' },
-  300: { code: 'NT', label: 'Near Threatened', color: '#5F9E61' },
-  400: { code: 'VU', label: 'Vulnerable', color: '#C49A33' },
-  500: { code: 'EN', label: 'Endangered', color: '#C06B3D' },
-  600: { code: 'CR', label: 'Critically Endangered', color: '#BD3F38' },
+  0: { code: 'NE', label: 'Not Evaluated', color: '#F9F6F2' },
+  100: { code: 'DD', label: 'Data Deficient', color: '#7F7D78' },
+  200: { code: 'LC', label: 'Least Concern', color: '#008059' },
+  300: { code: 'NT', label: 'Near Threatened', color: '#00AF90' },
+  400: { code: 'VU', label: 'Vulnerable', color: '#FFC946' },
+  500: { code: 'EN', label: 'Endangered', color: '#FF9457' },
+  600: { code: 'CR', label: 'Critically Endangered', color: '#CC1E3D' },
   700: { code: 'RE', label: 'Regionally Extinct', color: '#444444' },
   800: { code: 'EW', label: 'Extinct in the Wild', color: '#222222' },
   900: { code: 'EX', label: 'Extinct', color: '#000000' }
