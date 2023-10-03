25ab1b8d28a794d55f948c3fd619b87ba676e1fa
Somjintana Korbut <nuisomjin@gmail.com>
2023-10-03 14:48:07 +0100

    #1176 changing bar chart colour (species by risk and species by taxon)

--- apps/website/src/_services/taxon-classes/index.ts
+++ apps/website/src/_services/taxon-classes/index.ts
@@ -10,9 +10,9 @@ export interface TaxonClassUi {
 export const DEFAULT_TAXON_ID = 999
 export const TAXON_CLASSES_BY_ID: Record<number, TaxonClassUi> = {
   [DEFAULT_TAXON_ID]: { label: 'Others', symbol: '❔', color: '#B177FC' },
-  100: { label: 'Amphibians', symbol: '🐸', color: '#02A84F' },
-  200: { label: 'Mammals', symbol: '🐗', color: '#F5A700' }, // used to be bat, but got combine to mammals
-  300: { label: 'Birds', symbol: '🐦', color: '#E043A9' },
+  100: { label: 'Amphibians', symbol: '🐸', color: '#003F5C' },
+  200: { label: 'Mammals', symbol: '🐗', color: '#7D447D' }, // used to be bat, but got combine to mammals
+  300: { label: 'Birds', symbol: '🐦', color: '#444E86' },
   400: { label: 'Fish', symbol: '🐟', color: '#1259DE' },
   500: { label: 'Insects', symbol: '🦟', color: '#A80915' },
   600: { label: 'Mammals', symbol: '🐗', color: '#F5A700' }
