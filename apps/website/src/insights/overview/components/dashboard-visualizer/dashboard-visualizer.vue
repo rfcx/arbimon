872b23e4fe3efe7aa06dc3c4076db3117f720cfc
Somjintana Korbut <nuisomjin@gmail.com>
2023-10-05 13:28:27 +0100

    #1178 add API composable for dashboard visualizer content

--- apps/website/src/insights/overview/components/dashboard-visualizer/dashboard-visualizer.vue
+++ apps/website/src/insights/overview/components/dashboard-visualizer/dashboard-visualizer.vue
@@ -14,11 +14,29 @@
       </li>
     </ol>
   </div>
+  <div
+    v-if="!isLoading || !isError"
+    class="inline-grid w-full gap-2 mt-2 xl:grid-cols-2"
+  >
+    <!-- TODO: fix this to use map compomnent -->
+    <span class="hidden">{{ mapDataset.maxValues }}</span>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { type AxiosInstance } from 'axios'
+import { type ComputedRef, computed, inject, ref } from 'vue'
 
-import { ref } from 'vue'
+import { dayjs } from '@rfcx-bio/utils/dayjs-initialized'
+
+// import { MapBaseComponent } from '~/maps/map-base'
+import { apiClientBioKey } from '@/globals'
+import { type MapDataSet } from '~/maps/types'
+import { useStore } from '~/store'
+import { useGetDashboardDataBySite } from './_composables/use-get-visaulizer'
+
+const apiClientBio = inject(apiClientBioKey) as AxiosInstance
+const store = useStore()
 
 interface Tab {
   label: string
@@ -36,5 +54,32 @@ const tabs: Tab[] = [
 ]
 
 const selectedTab = ref(tabs[0].value)
+const { isLoading: isLoadingDataBySite, isError: isErrorDataBySite, data: dataBySite } = useGetDashboardDataBySite(apiClientBio, store.selectedProject?.id ?? -1)
+const richnessMapDataBySite = computed(() => dataBySite.value?.richnessBySite ?? [])
+// TODO: add detections data
+
+const isLoading = computed(() => selectedTab.value === TAB_VALUES.richness ? isLoadingDataBySite.value : false)
+const isError = computed(() => selectedTab.value === TAB_VALUES.richness ? isErrorDataBySite.value : false)
+const data = computed(() => selectedTab.value === TAB_VALUES.richness ? richnessMapDataBySite.value : dataBySite.value?.detectionBySite ?? [])
+
+const mapDataset: ComputedRef<MapDataSet> = computed(() => {
+  return {
+      startDate: dayjs(),
+      endDate: dayjs(),
+      sites: store.projectFilters?.locationSites ?? [],
+      data: data.value
+        .map(({ name: siteName, latitude, longitude, value }) => ({
+          siteName,
+          latitude,
+          longitude,
+          values: {
+            [selectedTab.value]: value
+          }
+        })),
+      maxValues: {
+        [selectedTab.value]: Math.max(...data.value.map(({ value }) => value))
+      }
+    }
+})
 
 </script>
