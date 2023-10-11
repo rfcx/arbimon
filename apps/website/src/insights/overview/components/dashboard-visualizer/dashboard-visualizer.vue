5bd702a081908bcd8cfa5047f2e8497365a2c6c8
Somjintana Korbut <nuisomjin@gmail.com>
2023-10-11 13:39:28 +0100

    Revert "#1190 refactor some type stuff"
    This reverts commit 492201ba2d0d065b59a115adffb4c21be6b76d30.

--- apps/website/src/insights/overview/components/dashboard-visualizer/dashboard-visualizer.vue
+++ apps/website/src/insights/overview/components/dashboard-visualizer/dashboard-visualizer.vue
@@ -98,7 +98,7 @@ import { type ComputedRef, computed, inject, ref } from 'vue'
 import { dayjs } from '@rfcx-bio/utils/dayjs-initialized'
 
 import { apiClientBioKey } from '@/globals'
-import { type MapboxStatisticsStyle, MAPBOX_STYLE_CIRCLE } from '~/maps'
+import { type MapboxStatisticsStyle, type MapboxStyle, MAPBOX_STYLE_CIRCLE } from '~/maps'
 import { DEFAULT_NON_ZERO_STYLE } from '~/maps/constants'
 import { MapBaseComponent } from '~/maps/map-base'
 import MapToolMenu from '~/maps/map-tool-menu/map-tool-menu.vue'
@@ -170,9 +170,7 @@ const mapLegendLabels = computed(() => {
   return [1, Math.ceil(maxValue / 2), maxValue]
 })
 
-const propagateMapStatisticsStyle = (style: MapboxStatisticsStyle) => {
-  mapStatisticsStyle.value = style
-}
+const propagateMapStatisticsStyle = (style: MapboxStyle) => { mapStatisticsStyle.value = style }
 
 const mapInitialBounds: ComputedRef<LngLatBoundsLike | null> = computed(() => {
   const project = store.selectedProject
