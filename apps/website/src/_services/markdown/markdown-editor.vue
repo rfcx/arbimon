b22c30231ec269f95933d062a4f3cd503d31a727
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-10-16 11:45:23 +0700

    #1153 add modal for adding link support

--- apps/website/src/_services/markdown/markdown-editor.vue
+++ apps/website/src/_services/markdown/markdown-editor.vue
@@ -126,7 +126,7 @@
         data-modal-target="markdown-editor-insert-link-modal"
         data-modal-toggle="markdown-editor-insert-link-modal"
         type="button"
-        @click="setLink"
+        @click="openModalToSetLink"
       >
         <icon-custom-ft-link />
       </button>
@@ -146,17 +146,16 @@
       class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
     >
       <div class="relative w-full max-w-2xl max-h-full">
-        <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-          <!-- Modal header -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-echo">
           <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
             <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-              Static modal
+              Link
             </h3>
             <button
               type="button"
-              class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+              class="text-white bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
               data-modal-hide="markdown-editor-insert-link-modal"
+              @click="closeModal"
             >
               <svg
                 class="w-3 h-3"
@@ -178,28 +177,32 @@
           </div>
           <!-- Modal body -->
           <div class="p-6 space-y-6">
-            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
+            <p class="text-base leading-relaxed text-gray-500 dark:text-white">
+              Input the link that you wanted to put.
             </p>
-            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-            </p>
+            <input
+              v-model="linkToSet"
+              class="text-black rounded-lg w-72"
+              type="text"
+            >
           </div>
           <!-- Modal footer -->
           <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
             <button
               data-modal-hide="markdown-editor-insert-link-modal"
               type="button"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              class="text-black bg-frequency font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              @click="confirmSetLink"
             >
-              I accept
+              Ok
             </button>
             <button
               data-modal-hide="markdown-editor-insert-link-modal"
               type="button"
-              class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+              class="text-white bg-echo rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5"
+              @click="closeModal"
             >
-              Decline
+              Cancel
             </button>
           </div>
         </div>
@@ -220,6 +223,7 @@ import { type Ref, onMounted, ref, watch } from 'vue'
 const props = defineProps<{ modelValue: string }>()
 const emit = defineEmits<{(e: 'update:modelValue', value: string): void}>()
 const modal = ref() as Ref<Modal>
+const linkToSet = ref('')
 
 watch(() => props.modelValue, (newValue) => {
   if (editor.value?.storage.markdown.getMarkdown() === newValue) {
@@ -251,20 +255,29 @@ onMounted(() => {
   })
 })
 
-const setLink = (): void => {
+const openModalToSetLink = (): void => {
   const previousUrl: string | undefined = editor.value?.getAttributes('link').href
-  const url = window.prompt('URL', previousUrl)
 
-  if (url == null) {
-    return
-  }
+  // sets to old link first
+  linkToSet.value = previousUrl ?? ''
+  modal.value.show()
+}
 
-  if (url === '') {
+const confirmSetLink = (): void => {
+  // get link from the modal. If empty string is passed. Unlink the given link
+  if (linkToSet.value === '') {
     editor.value?.chain().focus().extendMarkRange('link').unsetLink().run()
     return
   }
 
-  editor.value?.chain().focus().extendMarkRange('link').setLink({ href: url }).run()
+  // Otherwise set the highlighted text to the given link. Then reset the link.
+  editor.value?.chain().focus().extendMarkRange('link').setLink({ href: linkToSet.value }).run()
+  closeModal()
+}
+
+const closeModal = (): void => {
+  modal.value.hide()
+  linkToSet.value = ''
 }
 </script>
 
