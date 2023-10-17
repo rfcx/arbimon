4fe3634850bcedd8fb9439a76fc878188612c287
Somjintana Korbut <nuisomjin@gmail.com>
2023-10-17 13:17:55 +0100

    #1199 Insight overview map - update data when the selected project has changed

--- apps/website/src/insights/overview/components/dashboard-visualizer/_composables/use-get-visaulizer.ts
+++ apps/website/src/insights/overview/components/dashboard-visualizer/_composables/use-get-visaulizer.ts
@@ -1,19 +1,20 @@
 import { type AxiosInstance } from 'axios'
+import { type ComputedRef } from 'vue'
 import { type UseQueryReturnType, useQuery } from 'vue-query'
 
 import { type DashboardDataByHourResponse, apiBioGetDashboardDataByHour } from '@rfcx-bio/common/api-bio/dashboard/dashboard-data-by-hour'
 import { type DashboardDataBySiteResponse, apiBioGetDashboardDataBySite } from '@rfcx-bio/common/api-bio/dashboard/dashboard-data-by-site'
 
-export const useGetDashboardDataBySite = (apiClient: AxiosInstance, projectId: number): UseQueryReturnType<DashboardDataBySiteResponse, unknown> => {
+export const useGetDashboardDataBySite = (apiClient: AxiosInstance, projectId: ComputedRef<number | undefined>): UseQueryReturnType<DashboardDataBySiteResponse, unknown> => {
   return useQuery({
     queryKey: ['get-dashboard-data-by-site', projectId],
-    queryFn: async () => await apiBioGetDashboardDataBySite(apiClient, projectId)
+    queryFn: async () => await apiBioGetDashboardDataBySite(apiClient, projectId?.value ?? -1)
   })
 }
 
-export const useGetDashboardDataByTime = (apiClient: AxiosInstance, projectId: number): UseQueryReturnType<DashboardDataByHourResponse, unknown> => {
+export const useGetDashboardDataByTime = (apiClient: AxiosInstance, projectId: ComputedRef<number | undefined>): UseQueryReturnType<DashboardDataByHourResponse, unknown> => {
   return useQuery({
     queryKey: ['get-dashboard-data-by-time', projectId],
-    queryFn: async () => await apiBioGetDashboardDataByHour(apiClient, projectId)
+    queryFn: async () => await apiBioGetDashboardDataByHour(apiClient, projectId?.value ?? -1)
   })
 }
