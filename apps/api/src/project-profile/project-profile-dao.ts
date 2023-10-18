b649c831ea0716786bb3b197f7e77e6cbd8f3b61
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-10-18 11:35:06 +0700

    #1153 type mismatch

--- apps/api/src/project-profile/project-profile-dao.ts
+++ apps/api/src/project-profile/project-profile-dao.ts
@@ -1,18 +1,18 @@
-import { type DashboardContentResponse } from '@rfcx-bio/common/api-bio/dashboard/dashboard-content'
+import { type ProjectProfileResponse } from '@rfcx-bio/common/api-bio/project-profile/project-profile'
 import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
 
 import { getSequelize } from '~/db'
 
-export const getProjectProfile = async (locationProjectId: number): Promise<DashboardContentResponse> =>
+export const getProjectProfile = async (locationProjectId: number): Promise<ProjectProfileResponse> =>
   await ModelRepository.getInstance(getSequelize())
     .LocationProjectProfile
     .findOne({
       where: { locationProjectId },
       attributes: ['summary', 'readme'],
       raw: true
-    }) ?? { locationProjectId, summary: '', readme: '' }
+    }) ?? { summary: '', readme: '' }
 
-export const updateProjectProfile = async (locationProjectId: number, summary: string | undefined, readme: string | undefined): Promise<DashboardContentResponse | undefined> => {
+export const updateProjectProfile = async (locationProjectId: number, summary: string | undefined, readme: string | undefined): Promise<ProjectProfileResponse | undefined> => {
   const locationProjectProfile = ModelRepository.getInstance(getSequelize()).LocationProjectProfile
   const res = await locationProjectProfile.update({ summary, readme }, { where: { locationProjectId }, returning: true })
   if (res[0] === 0) return undefined
