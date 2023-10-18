42d90a5cb27a36c1cb8edfb20a7e7723ff0997a9
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-10-18 16:58:41 +0700

    #1153 new useMutation for updating the values

--- apps/website/src/insights/overview/composables/use-get-dashboard-content.ts
+++ apps/website/src/insights/overview/composables/use-get-dashboard-content.ts
@@ -1,11 +1,13 @@
 import { type AxiosInstance } from 'axios'
+import { type ComputedRef } from 'vue'
 import { type UseQueryReturnType, useQuery } from 'vue-query'
 
 import { type DashboardContentResponse, apiBioGetDashboardContent } from '@rfcx-bio/common/api-bio/dashboard/dashboard-content'
 
-export const useGetDashboardContent = (apiClient: AxiosInstance, projectId: number): UseQueryReturnType<DashboardContentResponse, unknown> => {
+export const useGetDashboardContent = (apiClient: AxiosInstance, projectId: number, enabled: ComputedRef<boolean>): UseQueryReturnType<DashboardContentResponse, unknown> => {
   return useQuery({
     queryKey: ['get-dashboard-content'],
-    queryFn: async () => await apiBioGetDashboardContent(apiClient, projectId)
+    queryFn: async () => await apiBioGetDashboardContent(apiClient, projectId),
+    enabled
   })
 }
---
+++ apps/website/src/insights/overview/composables/use-update-dashboard-key-result.ts
@@ -0,0 +1,13 @@
+import { type AxiosInstance } from 'axios'
+import { type UseMutationReturnType, useMutation } from 'vue-query'
+
+import { type UpdateDashboardContentResponse, apiBioUpdateDashboardContent } from '@rfcx-bio/common/api-bio/dashboard/dashboard-content'
+
+export const useUpdateDashboardKeyResult = (apiClient: AxiosInstance, projectId: number): UseMutationReturnType<UpdateDashboardContentResponse, unknown, string, unknown, unknown> => {
+  return useMutation({
+    mutationKey: ['update-dashboard-key-result'],
+    mutationFn: async (value: string) => {
+      return await apiBioUpdateDashboardContent(apiClient, projectId, 'keyResult', value)
+    }
+  })
+}
