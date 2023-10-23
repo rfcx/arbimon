7f98fade13a887373992518e8d0906b67d5255f9
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-10-23 15:56:31 +0700

    #1153 fix markdown link editor sometimes not showing

--- apps/website/src/insights/overview/components/dashboard-markdown-viewer-editor/dashboard-markdown-viewer-editor.vue
+++ apps/website/src/insights/overview/components/dashboard-markdown-viewer-editor/dashboard-markdown-viewer-editor.vue
@@ -1,24 +1,24 @@
 <template>
   <div
     v-show="!isViewMored"
-    id="markdown-viewer-mask"
+    :id="`${id}-markdown-viewer-mask`"
     class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-gradient-to-b from-transparent to-echo z-10"
   />
   <MarkdownViewer
     v-show="!isEditing"
-    id="markdown-viewer-component"
+    :id="`${id}-markdown-viewer-component`"
     :class="isViewMored === true ? 'z-0' : 'max-h-52 overflow-y-hidden z-0'"
     :markdown="editableMarkdownText"
   />
   <button
-    id="markdown-viewer-edit-button"
+    :id="`${id}-markdown-viewer-edit-button`"
     class="absolute lg:right-4 top-0 z-20 hover:block hidden"
     @click="editMarkdownContent"
   >
     <icon-custom-fi-edit />
   </button>
   <button
-    id="markdown-viewer-read-more"
+    :id="`${id}-markdown-viewer-read-more`"
     :class="isViewMored === true ? 'bg-transparent absolute left-12 bottom-4 text-frequency text-base font-normal leading-normal z-20 hidden' : 'bg-transparent absolute left-12 bottom-4 text-frequency text-base font-normal leading-normal z-20'"
     @click="emit('update:isViewMored', true)"
   >
@@ -28,6 +28,7 @@
   </button>
   <MarkdownEditor
     v-show="isEditing"
+    :id="id"
     v-model="editableMarkdownText"
     :character-limit="props.characterLimit"
     class="mx-auto"
@@ -41,7 +42,7 @@ import { toRef } from 'vue'
 import MarkdownEditor from '~/markdown/markdown-editor.vue'
 import MarkdownViewer from '~/markdown/markdown-viewer.vue'
 
-const props = withDefaults(defineProps<{ markdownText: string, isViewMored: boolean, isEditing: boolean, characterLimit?: number }>(), { characterLimit: 1000 })
+const props = withDefaults(defineProps<{ id: string, markdownText: string, isViewMored: boolean, isEditing: boolean, characterLimit?: number }>(), { characterLimit: 1000 })
 const emit = defineEmits<{(e: 'on-editor-close', value: string): void, (e: 'update:isViewMored', value: boolean): void, (e: 'update:isEditing', value: boolean): void}>()
 
 const editableMarkdownText = toRef(props.markdownText)
@@ -57,19 +58,19 @@ const closeEditor = (): void => {
 </script>
 
 <style lang="scss">
-div#markdown-viewer-mask:hover ~ button#markdown-viewer-edit-button {
+div[id$="markdown-viewer-mask"]:hover ~ button[id$="markdown-viewer-edit-button"] {
   display: block;
 }
 
-div#markdown-viewer-component:hover + button#markdown-viewer-edit-button {
+div[id$="markdowe-viewer-component"]:hover + button[id$="markdown-viewer-edit-button"] {
   display: block;
 }
 
-div#markdown-viewer-mask + button#markdown-viewer-edit-button {
+div[id$="markdown-viewer-mask"] + button[id$="markdown-viewer-edit-button"] {
   display: none;
 }
 
-button#markdown-viewer-edit-button:hover {
+button[id$="markdown-viewer-edit-button"]:hover {
   display: block !important;
 }
 </style>
