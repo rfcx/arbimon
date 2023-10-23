8dec222cd30bb0f15616d38bd9bc159f60c32e05
Somjintana Korbut <nuisomjin@gmail.com>
2023-10-23 08:19:34 +0100

    Fix migration on dev

--- apps/cli/src/db/migrations/231020-01-new-location-project-detection-metric-materialized-view.ts
+++ apps/cli/src/db/migrations/231020-01-new-location-project-detection-metric-materialized-view.ts
@@ -10,19 +10,19 @@ const MATERIALIZED_VIEW_NAME = 'location_project_detection_metric'
 
 export const up: MigrationFn<QueryInterface> = async (params): Promise<void> => {
   await params.context.sequelize.query(`
-    create or replace materialized view ${MATERIALIZED_VIEW_NAME} as
-      select
+    CREATE MATERIALIZED VIEW ${MATERIALIZED_VIEW_NAME} AS
+      SELECT
         location_project_id,
         sum(count) as detection_minutes_count,
-        count(distinct taxon_species_id) as species_count,
-        min(time_precision_hour_local) as min_date,
-        max(time_precision_hour_local) as max_date
-      from
+        count(distinct taxon_species_id) AS species_count,
+        min(time_precision_hour_local) AS min_date,
+        max(time_precision_hour_local) AS max_date
+      FROM
         detection_by_site_species_hour
-      group by
+      GROUP BY
         location_project_id;`)
 }
 
 export const down: MigrationFn<QueryInterface> = async (params): Promise<void> => {
-  await params.context.sequelize.query(`drop materialized view if exists ${MATERIALIZED_VIEW_NAME}`)
+  await params.context.sequelize.query(`DROP MATERIALIZED VIEW IF EXISTS ${MATERIALIZED_VIEW_NAME}`)
 }
