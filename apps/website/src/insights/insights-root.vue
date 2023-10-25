d6104012ed1cd6b87743d27698a34cf893e4ed60
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2023-10-25 14:54:07 +0400

    Fix insights header to fill hero-cta-frog-bg for all row. Fix padding on the cnn page

--- apps/website/src/insights/insights-root.vue
+++ apps/website/src/insights/insights-root.vue
@@ -2,97 +2,101 @@
   <div
     class="bg-gray-50 dark:bg-hero-cta-frog-bg bg-cover border-b-1 border-fog"
   >
-    <div class="max-w-screen-xl mx-auto px-8 md:px-10 pt-20 pb-10 text-gray-900 dark:text-insight flex flex-col md:flex-row justify-between">
-      <div class="">
-        <h1 class="pb-4 text-frequency font-header">
-          {{ selectedProject?.name }}
-        </h1>
-        <hero-brief-overview
-          :can-edit="store.selectedProject?.isMyProject ?? false"
-          :default-text="dashboardStore.projectSummary ?? ''"
-        />
-        <div class="mt-4 flex flex-row items-center justify-start">
-          <div
-            class="flex flex-row items-center font-display text-sm mr-2 border-r-2 border-gray-300 h-5"
-          >
-            <icon-fas-spinner
-              v-if="isLoadingProjectStreams"
-              class="animate-spin"
-              aria-label="Loading"
-            />
-            <span
-              class="text-insight text-sm mr-2"
-            >
-              {{ projectCountry }}
-            </span>
+    <div class="pl-64">
+      <div class="max-w-screen-xl mx-auto px-8 md:px-10 pt-20 pb-10 text-gray-900 dark:text-insight flex flex-col md:flex-row justify-between">
+        <div class="">
+          <h1 class="pb-4 text-frequency font-header">
+            {{ selectedProject?.name }}
+          </h1>
+          <hero-brief-overview
+            :can-edit="store.selectedProject?.isMyProject ?? false"
+            :default-text="dashboardStore.projectSummary ?? ''"
+          />
+          <div class="mt-4 flex flex-row items-center justify-start">
             <div
-              v-if="projectFlag"
-              class="mr-2 align-baseline"
+              class="flex flex-row items-center font-display text-sm mr-2 border-r-2 border-gray-300 h-5"
             >
-              <country-flag
-                :country="projectFlag"
-                size="small"
+              <icon-fas-spinner
+                v-if="isLoadingProjectStreams"
+                class="animate-spin"
+                aria-label="Loading"
+              />
+              <span
+                class="text-insight text-sm mr-2"
+              >
+                {{ projectCountry }}
+              </span>
+              <div
+                v-if="projectFlag"
+                class="mr-2 align-baseline"
+              >
+                <country-flag
+                  :country="projectFlag"
+                  size="small"
+                />
+              </div>
+              <icon-custom-fi-globe
+                v-else
+                class="self-start mr-2"
               />
             </div>
-            <icon-custom-fi-globe
-              v-else
-              class="self-start mr-2"
-            />
-          </div>
-          <div
-            class="font-display text-sm flex flex-row items-center justify-between mr-2 border-r-2 border-gray-300 h-5"
-          >
-            <span
-              class="text-insight text-sm mx-3"
+            <div
+              class="font-display text-sm flex flex-row items-center justify-between mr-2 border-r-2 border-gray-300 h-5"
             >
-              Project dates:
-            </span>
-            <span
-              class="text-insight uppercase mx-3"
-            >
-              {{ formatDateRange(metrics?.minDate) }}
-            </span>
-            <icon-custom-arrow-right-white class="self-start" />
-            <span
-              class="text-insight uppercase mx-3 my-2"
-            >
-              {{ formatDateRange(metrics?.maxDate) }}
-            </span>
-          </div>
-        </div>
-      </div>
-      <div class="justify-self-end order-first md:order-last">
-        <span class="text-xxs text-fog">// TODO: menu</span>
-      </div>
-    </div>
-    <insight-not-ready-card v-if="!metrics?.totalDetections && !isLoading" />
-    <nav class="sticky top-0 z-40">
-      <div class="max-w-screen-xl mx-auto px-8">
-        <div class="text-center text-gray-500 dark:text-insight">
-          <ul class="flex flex-wrap -mb-px">
-            <li
-              v-for="(item, index) in items"
-              :key="item.title"
-              class="mr-3"
-            >
-              <router-link
-                :to="item.route"
-                :active-class="index > 0 ? 'text-insight border-frequency dark:text-frequency dark:border-frequency hover:text-insight hover:border-frequency dark:hover:text-insight' : ''"
-                :exact-active-class="index === 0 ? 'text-insight border-frequency dark:text-frequency dark:border-frequency hover:text-insight hover:border-frequency dark:hover:text-insight' : ''"
-                class="inline-block pt-3 px-3 pb-1.5 border-b-3 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-200 dark:hover:text-gray-200"
+              <span
+                class="text-insight text-sm mx-3"
               >
-                {{ item.title }}
-              </router-link>
-            </li>
-          </ul>
+                Project dates:
+              </span>
+              <span
+                class="text-insight uppercase mx-3"
+              >
+                {{ formatDateRange(metrics?.minDate) }}
+              </span>
+              <icon-custom-arrow-right-white class="self-start" />
+              <span
+                class="text-insight uppercase mx-3 my-2"
+              >
+                {{ formatDateRange(metrics?.maxDate) }}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="justify-self-end order-first md:order-last">
+          <span class="text-xxs text-fog">// TODO: menu</span>
         </div>
       </div>
-    </nav>
+      <insight-not-ready-card v-if="!metrics?.totalDetections && !isLoading" />
+      <nav class="sticky top-0 z-40">
+        <div class="max-w-screen-xl mx-auto px-8">
+          <div class="text-center text-gray-500 dark:text-insight">
+            <ul class="flex flex-wrap -mb-px">
+              <li
+                v-for="(item, index) in items"
+                :key="item.title"
+                class="mr-3"
+              >
+                <router-link
+                  :to="item.route"
+                  :active-class="index > 0 ? 'text-insight border-frequency dark:text-frequency dark:border-frequency hover:text-insight hover:border-frequency dark:hover:text-insight' : ''"
+                  :exact-active-class="index === 0 ? 'text-insight border-frequency dark:text-frequency dark:border-frequency hover:text-insight hover:border-frequency dark:hover:text-insight' : ''"
+                  class="inline-block pt-3 px-3 pb-1.5 border-b-3 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-200 dark:hover:text-gray-200"
+                >
+                  {{ item.title }}
+                </router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
   </div>
-  <div
-    class="max-w-screen-xl mx-auto px-6 md:px-10 py-10"
-  >
-    <router-view />
+  <div class="pl-64">
+    <div
+      class="max-w-screen-xl mx-auto px-6 md:px-10 py-10"
+    >
+      <router-view />
+    </div>
   </div>
 </template>
 <script setup lang="ts">
