b66238b1be71e0d3a5b735983357198b4f2546a9
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-11-01 18:04:09 +0700

    #1243 add empty div for displaying empty state of markdown content

--- apps/website/src/insights/overview/components/dashboard-markdown-viewer-editor/dashboard-markdown-viewer-editor.vue
+++ apps/website/src/insights/overview/components/dashboard-markdown-viewer-editor/dashboard-markdown-viewer-editor.vue
@@ -1,40 +1,48 @@
 <template>
-  <div
-    v-show="!isViewMored"
-    :id="`${id}-markdown-viewer-mask`"
-    class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-gradient-to-b from-transparent to-echo z-10"
-  />
-  <MarkdownViewer
-    v-show="!isEditing"
-    :id="`${id}-markdown-viewer-component`"
-    :class="isViewMored === true ? 'z-0' : 'max-h-52 overflow-y-hidden z-0'"
-    :markdown="editableMarkdownText"
-  />
-  <button
-    v-if="editable"
-    :id="`${id}-markdown-viewer-edit-button`"
-    class="absolute lg:right-4 top-0 z-20 hover:block hidden"
-    @click="editMarkdownContent"
-  >
-    <icon-custom-fi-edit />
-  </button>
-  <button
-    :id="`${id}-markdown-viewer-read-more`"
-    :class="isViewMored === true ? 'bg-transparent absolute left-12 bottom-4 text-frequency text-base font-normal leading-normal z-20 hidden' : 'bg-transparent absolute left-12 bottom-4 text-frequency text-base font-normal leading-normal z-20'"
-    @click="emit('update:isViewMored', true)"
-  >
-    <span>
-      View More <icon-custom-arrow-right class="text-frequency inline-block" />
-    </span>
-  </button>
-  <MarkdownEditor
-    v-show="isEditing"
-    :id="id"
-    v-model="editableMarkdownText"
-    :character-limit="props.characterLimit"
-    class="mx-auto"
-    @on-editor-close="closeEditor"
-  />
+  <template v-if="(rawMarkdownText == null || rawMarkdownText === '') && !isViewMored">
+    <ProjectSummaryEmpty
+      @emit-add-content="editMarkdownContent"
+      @emit-close="expandMarkdownContentEmpty"
+    />
+  </template>
+  <template v-else>
+    <div
+      v-show="!isViewMored"
+      :id="`${id}-markdown-viewer-mask`"
+      class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-gradient-to-b from-transparent to-echo z-10"
+    />
+    <MarkdownViewer
+      v-show="!isEditing"
+      :id="`${id}-markdown-viewer-component`"
+      :class="isViewMored === true ? 'z-0' : 'max-h-52 overflow-y-hidden z-0'"
+      :markdown="editableMarkdownText"
+    />
+    <button
+      v-if="editable"
+      :id="`${id}-markdown-viewer-edit-button`"
+      class="absolute lg:right-4 top-0 z-20 hover:block hidden"
+      @click="editMarkdownContent"
+    >
+      <icon-custom-fi-edit />
+    </button>
+    <button
+      :id="`${id}-markdown-viewer-read-more`"
+      :class="isViewMored === true ? 'bg-transparent absolute left-12 bottom-4 text-frequency text-base font-normal leading-normal z-20 hidden' : 'bg-transparent absolute left-12 bottom-4 text-frequency text-base font-normal leading-normal z-20'"
+      @click="expandMarkdownContent"
+    >
+      <span>
+        View More <icon-custom-arrow-right class="text-frequency inline-block" />
+      </span>
+    </button>
+    <MarkdownEditor
+      v-show="isEditing"
+      :id="id"
+      v-model="editableMarkdownText"
+      :character-limit="props.characterLimit"
+      class="mx-auto"
+      @on-editor-close="closeEditor"
+    />
+  </template>
 </template>
 
 <script setup lang="ts">
@@ -42,15 +50,30 @@ import { toRef } from 'vue'
 
 import MarkdownEditor from '~/markdown/markdown-editor.vue'
 import MarkdownViewer from '~/markdown/markdown-viewer.vue'
+import ProjectSummaryEmpty from '../project-summary-empty/project-summary-empty.vue'
 
-const props = withDefaults(defineProps<{ id: string, editable: boolean, markdownText: string, isViewMored: boolean, isEditing: boolean, characterLimit?: number }>(), { characterLimit: 1000 })
+const DEFAULT_CHARACTER_LIMIT = 10000
+
+const props = withDefaults(defineProps<{ id: string, editable: boolean, rawMarkdownText: string | undefined, defaultMarkdownText: string, isViewMored: boolean, isEditing: boolean, characterLimit?: number }>(), { characterLimit: DEFAULT_CHARACTER_LIMIT })
 const emit = defineEmits<{(e: 'on-editor-close', value: string): void, (e: 'update:isViewMored', value: boolean): void, (e: 'update:isEditing', value: boolean): void}>()
 
-const editableMarkdownText = toRef(props.markdownText)
+const editableMarkdownText = toRef(props.rawMarkdownText == null || props.rawMarkdownText === '' ? props.defaultMarkdownText : props.rawMarkdownText)
+
+const expandMarkdownContent = (): void => {
+  emit('update:isViewMored', true)
+  emit('update:isEditing', false)
+}
+
+const expandMarkdownContentEmpty = (): void => {
+  emit('update:isViewMored', true)
+  emit('update:isEditing', false)
+  editableMarkdownText.value = ''
+}
 
 const editMarkdownContent = (): void => {
   emit('update:isEditing', true)
   emit('update:isViewMored', true)
+  editableMarkdownText.value = props.rawMarkdownText == null || props.rawMarkdownText === '' ? props.defaultMarkdownText : props.rawMarkdownText
 }
 
 const closeEditor = (): void => {
---
+++ apps/website/src/insights/overview/components/project-summary-empty/project-summary-empty.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="bg-neutral-600 border border-white rounded-md w-fit px-4 py-2 mx-auto">
+    <span class="text-white text-base font-medium">
+      Don't leave this space empty - make it meaningful by adding your content.
+      <span
+        class="text-frequency cursor-pointer"
+        @click="$emit('emit-add-content')"
+      >
+        Add content
+      </span>
+      <svg
+        width="16"
+        height="16"
+        viewBox="0 0 16 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        @click="$emit('emit-close')"
+      >
+        <path
+          d="M12 4L4 12"
+          stroke="#FFFEFC"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M4 4L12 12"
+          stroke="#FFFEFC"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineEmits<{(event: 'emit-add-content'): void, (event: 'emit-close'): void}>()
+</script>
