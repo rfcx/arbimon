4a8fce3d06f08372f108da0afb35ba19bc45cd79
Somjintana Korbut <nuisomjin@gmail.com>
2023-11-02 10:55:00 +0000

    #1224 #1229 exclude objectives from dashboard content

--- packages/common/src/api-bio/dashboard/dashboard-content.ts
+++ packages/common/src/api-bio/dashboard/dashboard-content.ts
@@ -11,7 +11,7 @@ import { type ProjectRouteParamsSerialized, PROJECT_SPECIFIC_ROUTE_PREFIX } from
 export type DashboardContentParams = ProjectRouteParamsSerialized
 
 // Response types
-export type DashboardContentResponse = Omit<LocationProjectProfile, 'summary'>
+export type DashboardContentResponse = Omit<LocationProjectProfile, 'summary' | 'objectives'>
 
 // Route (They both share the same route, different method)
 export const dashboardContentRoute = `${PROJECT_SPECIFIC_ROUTE_PREFIX}/dashboard-content`
