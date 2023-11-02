bcfeec2f75bb1a65b73eb9de739b88d952469044
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2023-11-03 00:41:03 +0400

    #1239 Can move recordings and detections if site moves to another project

--- apps/cli/src/ingest/outputs/sites.int.test.ts
+++ apps/cli/src/ingest/outputs/sites.int.test.ts
@@ -1,9 +1,14 @@
+import dayjs from 'dayjs'
 import { Op } from 'sequelize'
 import { beforeEach, describe, expect, test, vi } from 'vitest'
 
 import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
+import { UPDATE_ON_DUPLICATE_DETECTION_BY_SITE_SPECIES_HOUR } from '@rfcx-bio/common/dao/models/detection-by-site-species-hour-model'
+import { UPDATE_ON_DUPLICATE_RECORDING_BY_SITE_HOUR } from '@rfcx-bio/common/dao/models/recording-by-site-hour-model'
+import { type TaxonSpecies } from '@rfcx-bio/common/dao/types'
 
 import { getSequelize } from '@/db/connections'
+import { literalIntegerArray2D, reducedAndSortedPairs } from '@/db/seeders/_helpers/sequelize-literal-integer-array-2d'
 import { deleteOutputProjects } from '../_testing/helper'
 import type { SiteArbimon } from '../parsers/parse-site-arbimon-to-bio'
 import { writeSitesToBio } from './sites'
@@ -83,7 +88,201 @@ describe('ingest > outputs > sites', () => {
     const updatedSite = await ModelRepository.getInstance(biodiversitySequelize).LocationSite
       .findOne({ where: { idArbimon: updatedArbimonSite.idArbimon } })
     expect(updatedSite?.locationProjectId).toBe(newProject.id)
-    // TODO Check that recordings and detections are moved too
+  })
+
+  test('can move recordings and detections if site moves to another project', async () => {
+    // Arrange
+
+    // Insert project
+    const arbimonProject = { ...DEFAULT_PROJECT, id: 2, idCore: '807cuoi3cvwx', idArbimon: 1921, slug: 'my-project-2' }
+    await biodiversitySequelize.query(SQL_INSERT_PROJECT, { bind: arbimonProject })
+    const bioProject = await ModelRepository.getInstance(biodiversitySequelize).LocationProject
+      .findOne({ where: { idArbimon: arbimonProject.idArbimon } })
+    // Insert site
+    await writeSitesToBio([DEFAULT_ARB_SITE], biodiversitySequelize)
+    const site = await ModelRepository.getInstance(biodiversitySequelize).LocationSite
+      .findOne({ where: { idArbimon: DEFAULT_ARB_SITE.idArbimon } })
+    // Insert specie
+    const SPECIES_INPUT: Omit<TaxonSpecies, 'id'> = {
+      idArbimon: 1050,
+      slug: 'falco-amurensis',
+      taxonClassId: 300,
+      scientificName: 'Falco amurensis'
+    }
+    const specie = await ModelRepository.getInstance(biodiversitySequelize).TaxonSpecies
+      .findOne({ where: { idArbimon: SPECIES_INPUT.idArbimon } })
+    if (!specie) await ModelRepository.getInstance(biodiversitySequelize).TaxonSpecies.bulkCreate([SPECIES_INPUT])
+    // Insert recordings
+    const RECORDING_INPUT = [
+      {
+        locationSiteId: site?.id ?? -1,
+        timePrecisionHourLocal: dayjs('2022-07-06 07:00:00+00').toDate(),
+        locationProjectId: bioProject?.id ?? -1,
+        count: 1,
+        countsByMinute: [[0, 1]],
+        totalDurationInMinutes: 1
+      },
+      {
+        locationSiteId: site?.id ?? -1,
+        timePrecisionHourLocal: dayjs('2022-07-06 08:00:00+00').toDate(),
+        locationProjectId: bioProject?.id ?? -1,
+        count: 2,
+        countsByMinute: [[10, 1], [20, 1]],
+        totalDurationInMinutes: 2
+      }
+    ]
+    const recordings = RECORDING_INPUT.map(group => {
+      return { ...group, countsByMinute: literalIntegerArray2D(reducedAndSortedPairs(group.countsByMinute), biodiversitySequelize) }
+    })
+    // @ts-expect-error: countsByMinute in rows has incompatible type (but it seems to be accepted by sequelize)
+    await ModelRepository.getInstance(biodiversitySequelize).RecordingBySiteHour.bulkCreate(recordings, {
+      updateOnDuplicate: UPDATE_ON_DUPLICATE_RECORDING_BY_SITE_HOUR
+    })
+    // Insert detections
+    const DETECTION_INPUT = [
+      {
+        locationSiteId: site?.id ?? -1,
+        timePrecisionHourLocal: dayjs('2022-07-06 07:00:00+00').toDate(),
+        locationProjectId: bioProject?.id ?? -1,
+        count: 1,
+        countsByMinute: [[0, 1]],
+        taxonSpeciesId: specie?.id ?? -1,
+        taxonClassId: 300
+      },
+      {
+        locationSiteId: site?.id ?? -1,
+        timePrecisionHourLocal: dayjs('2022-07-06 08:00:00+00').toDate(),
+        locationProjectId: bioProject?.id ?? -1,
+        count: 2,
+        countsByMinute: [[10, 1], [20, 1]],
+        taxonSpeciesId: specie?.id ?? -1,
+        taxonClassId: 300
+      }
+    ]
+    const detections = DETECTION_INPUT.map(group => {
+      return { ...group, countsByMinute: literalIntegerArray2D(reducedAndSortedPairs(group.countsByMinute), biodiversitySequelize) }
+    })
+    // @ts-expect-error: countsByMinute in rows has incompatible type (but it seems to be accepted by sequelize)
+    await ModelRepository.getInstance(biodiversitySequelize).DetectionBySiteSpeciesHour.bulkCreate(detections, {
+      updateOnDuplicate: UPDATE_ON_DUPLICATE_DETECTION_BY_SITE_SPECIES_HOUR
+    })
+    // Update site
+    const updatedArbimonSite = {
+      ...DEFAULT_ARB_SITE,
+      projectIdArbimon: arbimonProject.idArbimon
+    }
+
+    // Act
+    await writeSitesToBio([updatedArbimonSite], biodiversitySequelize)
+
+    // Assert
+    const updatedSite = await ModelRepository.getInstance(biodiversitySequelize).LocationSite
+      .findOne({ where: { idArbimon: updatedArbimonSite.idArbimon } })
+    expect(updatedSite?.locationProjectId).toBe(bioProject?.id)
+    // Check moved recordings
+    const oldRecordings = await ModelRepository.getInstance(biodiversitySequelize).RecordingBySiteHour
+      .findAll({ where: { locationProjectId: site?.locationProjectId, locationSiteId: site?.id } })
+    const updatedRecordings = await ModelRepository.getInstance(biodiversitySequelize).RecordingBySiteHour
+      .findAll({ where: { locationProjectId: updatedSite?.locationProjectId, locationSiteId: updatedSite?.id } })
+    expect(oldRecordings).toHaveLength(0)
+    expect(updatedRecordings).toHaveLength(2)
+    updatedRecordings.forEach(rec => { expect(rec.locationSiteId).toBe(updatedSite?.id) })
+    // Check moved detections
+    const oldDetections = await ModelRepository.getInstance(biodiversitySequelize).DetectionBySiteSpeciesHour
+      .findAll({ where: { locationProjectId: site?.locationProjectId, locationSiteId: site?.id } })
+    const updatedDetections = await ModelRepository.getInstance(biodiversitySequelize).DetectionBySiteSpeciesHour
+      .findAll({ where: { locationProjectId: updatedSite?.locationProjectId, locationSiteId: updatedSite?.id } })
+    expect(oldDetections).toHaveLength(0)
+    expect(updatedDetections).toHaveLength(2)
+    updatedDetections.forEach(det => { expect(det.locationSiteId).toBe(updatedSite?.id) })
+  })
+
+  test('recordings and detections keep original project if site doesnt move to another project', async () => {
+    // Arrange
+
+    // Insert site
+    const bioProject = await ModelRepository.getInstance(biodiversitySequelize).LocationProject
+      .findOne({ where: { idArbimon: DEFAULT_PROJECT.idArbimon } })
+    await writeSitesToBio([DEFAULT_ARB_SITE], biodiversitySequelize)
+    const bioSite = await ModelRepository.getInstance(biodiversitySequelize).LocationSite
+      .findOne({ where: { idArbimon: DEFAULT_ARB_SITE.idArbimon } })
+    // Insert specie
+    const SPECIES_INPUT: Omit<TaxonSpecies, 'id'> = {
+      idArbimon: 1050,
+      slug: 'falco-amurensis',
+      taxonClassId: 300,
+      scientificName: 'Falco amurensis'
+    }
+    const specie = await ModelRepository.getInstance(biodiversitySequelize).TaxonSpecies
+      .findOne({ where: { idArbimon: SPECIES_INPUT.idArbimon } })
+    if (!specie) await ModelRepository.getInstance(biodiversitySequelize).TaxonSpecies.bulkCreate([SPECIES_INPUT])
+    // Insert recordings
+    const RECORDING_INPUT = [
+      {
+        locationSiteId: bioSite?.id ?? -1,
+        timePrecisionHourLocal: dayjs('2022-07-06 07:00:00+00').toDate(),
+        locationProjectId: bioProject?.id ?? -1,
+        count: 1,
+        countsByMinute: [[0, 1]],
+        totalDurationInMinutes: 1
+      },
+      {
+        locationSiteId: bioSite?.id ?? -1,
+        timePrecisionHourLocal: dayjs('2022-07-06 08:00:00+00').toDate(),
+        locationProjectId: bioProject?.id ?? -1,
+        count: 2,
+        countsByMinute: [[10, 1], [20, 1]],
+        totalDurationInMinutes: 2
+      }
+    ]
+    const recordings = RECORDING_INPUT.map(group => {
+      return { ...group, countsByMinute: literalIntegerArray2D(reducedAndSortedPairs(group.countsByMinute), biodiversitySequelize) }
+    })
+    // @ts-expect-error: countsByMinute in rows has incompatible type (but it seems to be accepted by sequelize)
+    await ModelRepository.getInstance(biodiversitySequelize).RecordingBySiteHour.bulkCreate(recordings, {
+      updateOnDuplicate: UPDATE_ON_DUPLICATE_RECORDING_BY_SITE_HOUR
+    })
+    // Insert detections
+    const DETECTION_INPUT = [
+      {
+        locationSiteId: bioSite?.id ?? -1,
+        timePrecisionHourLocal: dayjs('2022-07-06 07:00:00+00').toDate(),
+        locationProjectId: bioProject?.id ?? -1,
+        count: 1,
+        countsByMinute: [[0, 1]],
+        taxonSpeciesId: specie?.id ?? -1,
+        taxonClassId: 300
+      },
+      {
+        locationSiteId: bioSite?.id ?? -1,
+        timePrecisionHourLocal: dayjs('2022-07-06 08:00:00+00').toDate(),
+        locationProjectId: bioProject?.id ?? -1,
+        count: 2,
+        countsByMinute: [[10, 1], [20, 1]],
+        taxonSpeciesId: specie?.id ?? -1,
+        taxonClassId: 300
+      }
+    ]
+    const detections = DETECTION_INPUT.map(group => {
+      return { ...group, countsByMinute: literalIntegerArray2D(reducedAndSortedPairs(group.countsByMinute), biodiversitySequelize) }
+    })
+    // @ts-expect-error: countsByMinute in rows has incompatible type (but it seems to be accepted by sequelize)
+    await ModelRepository.getInstance(biodiversitySequelize).DetectionBySiteSpeciesHour.bulkCreate(detections, {
+      updateOnDuplicate: UPDATE_ON_DUPLICATE_DETECTION_BY_SITE_SPECIES_HOUR
+    })
+
+    // Assert
+    expect(bioSite?.locationProjectId).toBe(bioProject?.id)
+    // Check recordings
+    const recordingResult = await ModelRepository.getInstance(biodiversitySequelize).RecordingBySiteHour
+      .findAll({ where: { locationProjectId: bioSite?.locationProjectId, locationSiteId: bioSite?.id } })
+    expect(recordingResult).toHaveLength(2)
+    recordingResult.forEach(rec => { expect(rec.locationSiteId).toBe(bioSite?.id) })
+    // Check moved detections
+    const detectionResult = await ModelRepository.getInstance(biodiversitySequelize).DetectionBySiteSpeciesHour
+      .findAll({ where: { locationProjectId: bioSite?.locationProjectId, locationSiteId: bioSite?.id } })
+    expect(detectionResult).toHaveLength(2)
+    detectionResult.forEach(det => { expect(det.locationSiteId).toBe(bioSite?.id) })
   })
 
   test('log error when project does not exist', async () => {
