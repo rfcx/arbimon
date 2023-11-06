c3b7e0333bef50f0966568e9687c9ec49099abcc
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-11-06 16:46:47 +0700

    #1243 remove x icon

--- apps/website/src/insights/overview/components/project-summary-empty/project-summary-empty.vue
+++ apps/website/src/insights/overview/components/project-summary-empty/project-summary-empty.vue
@@ -8,29 +8,6 @@
       >
         Add content
       </span>
-      <svg
-        class="inline-block align-middle cursor-pointer"
-        width="16"
-        height="16"
-        viewBox="0 0 16 16"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M12 4L4 12"
-          stroke="#FFFEFC"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-        <path
-          d="M4 4L12 12"
-          stroke="#FFFEFC"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-      </svg>
     </span>
   </div>
 </template>
