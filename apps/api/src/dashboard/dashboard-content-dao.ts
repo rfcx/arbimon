628f3783feb14e221d341bafa1f2fdaeda42021c
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-11-06 17:21:21 +0700

    #1153 move sql-values creation to a different file

--- apps/api/src/dashboard/dashboard-content-dao.ts
+++ apps/api/src/dashboard/dashboard-content-dao.ts
@@ -5,6 +5,7 @@ import { type LocationProjectProfileContentType } from '@rfcx-bio/common/dao/typ
 import { checkUserPermissionForEditingDashboardContent } from '~/api-core/api-core'
 import { getSequelize } from '~/db'
 import { BioPublicError } from '~/errors'
+import { sqlValues } from './sql-values'
 
 export const getDashboardContent = async (locationProjectId: number): Promise<DashboardContentResponse> => {
   const sequelize = getSequelize()
@@ -51,32 +52,4 @@ export const updateDashboardContent = async (token: string, locationProjectId: n
       now(),
       ${values.values}
     ) on conflict (location_project_id) do update set ${values.updateClause}, updated_at = now();`)
-}
-
-/**
- * Accepts a key and value to add to the create table, this function is used so that when doing an insert on another column we can guarantee that there will be no panics
- * on the column that does not have default value set.
- */
-export const sqlValues = (contentType: LocationProjectProfileContentType, value: string): { keys: string, values: string, updateClause: string } => {
-  const columnNameMap: Record<LocationProjectProfileContentType, string> = {
-    summary: 'summary',
-    readme: 'readme',
-    keyResult: 'key_result',
-    resources: 'resources',
-    methods: 'methods'
-  }
-
-  // Columns without default value inside the database
-  const noDefaultColumnNames: Partial<Record<LocationProjectProfileContentType, string>> = {
-    summary: '',
-    readme: ''
-  }
-
-  noDefaultColumnNames[contentType] = value
-
-  return {
-    keys: Object.keys(noDefaultColumnNames).map(k => columnNameMap[k as LocationProjectProfileContentType]).join(', '),
-    values: Object.values(noDefaultColumnNames).map(v => `'${v}'`).join(', '),
-    updateClause: `${columnNameMap[contentType]} = '${value}'`
-  }
 }
---
+++ apps/api/src/dashboard/sql-values.ts
@@ -0,0 +1,29 @@
+import { type LocationProjectProfileContentType } from '@rfcx-bio/common/dao/types'
+
+/**
+ * Accepts a key and value to add to the create table, this function is used so that when doing an insert on another column we can guarantee that there will be no panics
+ * on the column that does not have default value set.
+ */
+export const sqlValues = (contentType: LocationProjectProfileContentType, value: string): { keys: string, values: string, updateClause: string } => {
+  const columnNameMap: Record<LocationProjectProfileContentType, string> = {
+    summary: 'summary',
+    readme: 'readme',
+    keyResult: 'key_result',
+    resources: 'resources',
+    methods: 'methods'
+  }
+
+  // Columns without default value inside the database
+  const noDefaultColumnNames: Partial<Record<LocationProjectProfileContentType, string>> = {
+    summary: '',
+    readme: ''
+  }
+
+  noDefaultColumnNames[contentType] = value
+
+  return {
+    keys: Object.keys(noDefaultColumnNames).map(k => columnNameMap[k as LocationProjectProfileContentType]).join(', '),
+    values: Object.values(noDefaultColumnNames).map(v => `'${v}'`).join(', '),
+    updateClause: `${columnNameMap[contentType]} = '${value}'`
+  }
+}
