9a192c242d5e00d4187dfaf023688230b851938f
Somjintana Korbut <nuisomjin@gmail.com>
2023-11-07 12:55:51 +0000

    #1229 Project blurb - add tooltips, change from 'summary' to 'blurb', remove placeholder

--- apps/website/src/projects/components/form/project-summary-form.vue
+++ apps/website/src/projects/components/form/project-summary-form.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="mt-4">
+  <div class="mt-4 flex flex-row justify-between items-center">
     <label
       for="summary"
       class="block mb-2 font-medium text-gray-900 dark:text-insight"
-    >Project summary</label>
+    >Project blurb</label>
+    <icon-custom-ic-info
+      data-tooltip-target="summary_tooltip"
+      data-tooltip-style="light"
+      class="inline-block basis-8 h-4 w-4 cursor-pointer text-insight"
+    />
+    <div
+      id="summary_tooltip"
+      role="tooltip"
+      class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-gray-900 transition-opacity duration-300 bg-white rounded-lg shadow-sm opacity-0 tooltip"
+    >
+      {{ PLACEHOLDER_TEXT }}
+      <div
+        class="tooltip-arrow"
+        data-popper-arrow
+      />
+    </div>
   </div>
   <textarea
     v-model.lazy.trim="summary"
-    :placeholder="PLACEHOLDER_TEXT"
     class="input-field w-full h-28"
   />
 </template>
 
 <script setup lang="ts">
+import { initTooltips } from 'flowbite'
 import { onMounted, ref, watch } from 'vue'
 
 const props = defineProps<{
@@ -26,6 +42,7 @@ const PLACEHOLDER_TEXT = 'One line summary about the project. Some context such 
 const summary = ref('')
 
 onMounted(() => {
+  initTooltips()
   if (props.existingSummary) {
     summary.value = props.existingSummary
   }
