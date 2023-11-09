7a0733cb83f4a93f2c7cef85dc3092e2b4b64512
Somjintana Korbut <nuisomjin@gmail.com>
2023-11-09 21:17:36 +0000

    #1253 separate species by taxon as component

---
+++ apps/website/src/insights/overview/components/dashboard-species/dashboard-species-by-taxon.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2 class="mt-6">
+    Taxonomic groups
+  </h2>
+  <h6>Number of species detected in each taxonomic group.</h6>
+  <div class="dashboard-taxon">
+    <StackDistribution
+      :dataset="dataset"
+      :known-total-count="knownTotalCount"
+      class="my-6"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import StackDistribution from './components/stack-distribution.vue'
+import { type HorizontalStack } from './components/stack-distribution.vue'
+
+defineProps<{
+  dataset: HorizontalStack[]
+  knownTotalCount: string
+}>()
+
+</script>
