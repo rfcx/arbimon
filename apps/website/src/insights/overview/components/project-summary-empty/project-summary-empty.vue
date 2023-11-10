f8651cabeef328152daa9c74e85b8a2172cdd87a
Somjintana Korbut <nuisomjin@gmail.com>
2023-11-10 22:58:49 +0000

    #1253 wrap project summary empty state (so it shows multiple lines of text in smaller screen size)

--- apps/website/src/insights/overview/components/project-summary-empty/project-summary-empty.vue
+++ apps/website/src/insights/overview/components/project-summary-empty/project-summary-empty.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-neutral-600 border border-white rounded-md w-fit px-4 py-2 mx-auto whitespace-nowrap">
+  <div class="bg-neutral-600 border border-white rounded-md w-fit px-4 py-2 mx-auto">
     <span class="text-white text-base font-medium">
       Don't leave this space empty - make it meaningful by adding your content.
       <span
