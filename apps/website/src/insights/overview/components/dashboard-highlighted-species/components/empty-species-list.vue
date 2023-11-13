2d49b67d8357ac3648cf51745499efd32f4faeeb
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2023-11-13 19:26:56 +0400

    #1257 Fix gap between boxes

--- apps/website/src/insights/overview/components/dashboard-highlighted-species/components/empty-species-list.vue
+++ apps/website/src/insights/overview/components/dashboard-highlighted-species/components/empty-species-list.vue
@@ -1,23 +1,25 @@
 <template>
-  <div
-    v-for="item in emptySpecies"
-    :key="item.title"
-    class="flex content-center mb-2"
-  >
-    <div
-      class="min-h-14 h-14 min-w-14 w-14 object-cover rounded bg-util-gray-02"
-    />
-    <div
-      class="ml-4 self-center hover:(text-subtle)"
+  <ul class="flex flex-col gap-y-2">
+    <li
+      v-for="item in emptySpecies"
+      :key="item.title"
+      class="flex items-center"
     >
-      <p class="italic">
-        {{ item.title }}
-      </p>
-      <p class="text-xs text-subtle">
-        {{ item.subtitle }}
-      </p>
-    </div>
-  </div>
+      <div
+        class="min-h-14 h-14 min-w-14 w-14 object-cover rounded bg-util-gray-02"
+      />
+      <div
+        class="ml-4 self-center hover:(text-subtle)"
+      >
+        <p class="italic">
+          {{ item.title }}
+        </p>
+        <p class="text-xs text-subtle">
+          {{ item.subtitle }}
+        </p>
+      </div>
+    </li>
+  </ul>
 </template>
 
 <script setup lang="ts">
