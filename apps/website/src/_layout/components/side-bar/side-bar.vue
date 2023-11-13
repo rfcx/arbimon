2b7a0e119600ff397cf8798fbbffdaf957712d64
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2023-11-13 10:53:18 +0400

    #1276 Update sidebar paddings

--- apps/website/src/_layout/components/side-bar/side-bar.vue
+++ apps/website/src/_layout/components/side-bar/side-bar.vue
@@ -1,7 +1,7 @@
 <template>
   <aside
     id="sidebar"
-    class="fixed z-50 top-0 left-0 w-14 h-screen transition-transform -translate-x-full bg-white sm:translate-x-0 dark:bg-echo group transition delay-500 duration-300 ease-in-out hover:(w-66 delay-300)"
+    class="fixed z-50 top-0 left-0 w-13 h-screen transition-transform -translate-x-full bg-white sm:translate-x-0 dark:bg-pitch group transition delay-500 duration-300 ease-in-out hover:(w-66 delay-300)"
     aria-label="Sidebar"
     data-drawer-backdrop="false"
     @mouseenter="showSidebar = true"
@@ -10,7 +10,7 @@
     <div class="h-full pb-4 overflow-y-auto">
       <div class="flex flex-col h-full justify-between">
         <div>
-          <div class="px-2 my-4 h-9 flex flex-row">
+          <div class="px-3 my-4 h-9 flex flex-row">
             <img
               src="/src/_services/assets/arbimon-logo.svg"
               class="h-8"
@@ -22,33 +22,34 @@
             >Arbimon</span>
           </div>
           <div class="my-2 border-t-1 border-color-util-gray-01" />
-          <ul class="px-2 border-gray-200 dark:border-gray-700">
+          <ul class="px-3 border-gray-200 dark:border-gray-700">
             <li
               v-for="item in items"
               :key="item.title"
+              class="my-3"
             >
               <router-link
                 v-if="item.route"
                 :to="item.route"
                 :title="item.title"
-                exact-active-class="bg-gray-100 rounded-lg text-moss"
-                class="flex items-center p-2 text-base hover:(bg-util-gray-02 rounded-lg transition duration-300)"
+                exact-active-class="bg-gray-100 rounded text-moss"
+                class="flex items-center text-base hover:(bg-util-gray-02 rounded transition duration-300)"
               >
                 <span
                   v-if="item.iconRaw === 'fi-grid'"
-                  class="py-0.5"
+                  class="p-0.5"
                 >
                   <icon-custom-fi-grid />
                 </span>
                 <span
                   v-if="item.iconRaw === 'pres-chart-bar' === true"
-                  class="py-0.5"
+                  class="p-0.5"
                 >
                   <icon-custom-pres-chart-bar />
                 </span>
                 <span
                   v-if="item.iconRaw === 'fi-settings'"
-                  class="py-0.5"
+                  class="p-0.5"
                 >
                   <icon-custom-fi-settings />
                 </span>
@@ -57,7 +58,7 @@
               <a
                 v-else-if="item.legacyPath"
                 :href="arbimonLink + item.legacyPath"
-                class="flex items-center p-2 text-base ease-in-out active:text-moss hover:(bg-util-gray-02 rounded-lg transition duration-300) active:(bg-gray-100 rounded-lg text-moss)"
+                class="flex items-center text-base ease-in-out active:text-moss hover:(bg-util-gray-02 rounded transition duration-300) active:(bg-gray-100 rounded text-moss)"
               >
                 <span class="ml-2 hidden group-hover:block">{{ item.title }}</span>
                 <icon-custom-linkout
@@ -68,58 +69,61 @@
               <button
                 v-else
                 type="button"
-                class="mainmenu flex items-center p-2 w-full text-base font-normal active:text-moss hover:(bg-util-gray-02 rounded-lg transition duration-300)"
+                class="mainmenu flex items-center w-full text-base font-normal active:text-moss hover:(bg-util-gray-02 rounded transition duration-300)"
                 :aria-controls="itemId(item.title)"
                 :data-collapse-toggle="itemId(item.title)"
               >
                 <span
                   v-if="item.iconRaw === 'cloud-upload'"
-                  class="py-0.5"
+                  class="p-0.5"
                 >
                   <icon-custom-cloud-upload />
                 </span>
                 <span
                   v-if="item.iconRaw === 'fa-search'"
-                  class="py-0.5"
+                  class="p-0.5"
                 >
                   <icon-fa-search class="h-5 w-5" />
                 </span>
                 <span
                   v-if="item.iconRaw === 'fi-aed'"
-                  class="py-0.5"
+                  class="p-0.5"
                 >
                   <icon-custom-fi-activity class="h-6 w-6" />
                 </span>
                 <span class="flex-1 ml-2 text-left whitespace-nowrap hidden group-hover:block">
                   {{ item.title }}
                 </span>
-                <icon-fa-chevron-down
-                  v-if="showSidebar"
-                  class="w-3 h-3"
-                />
+                <span class="p-0.5">
+                  <icon-fa-chevron-down
+                    v-if="showSidebar"
+                    class="w-3 h-3"
+                  />
+                </span>
               </button>
               <ul
                 v-if="item.children"
                 :id="itemId(item.title)"
-                class="submenu py-2 hidden"
+                class="submenu hidden"
                 :class="{ 'block': isParent(item) }"
               >
                 <li
                   v-for="childItem in item.children"
                   :key="childItem.title"
+                  class="my-3"
                 >
                   <router-link
                     v-if="childItem.route"
                     :to="childItem.route"
-                    exact-active-class="bg-gray-100 rounded-lg text-moss"
-                    class="flex items-center p-2 pl-12 w-full text-base font-normal hover:(bg-util-gray-02 rounded-lg transition duration-300)"
+                    exact-active-class="bg-gray-100 rounded text-moss"
+                    class="flex items-center pl-12 w-full text-base font-normal hover:(bg-util-gray-02 rounded transition duration-300)"
                   >
                     {{ childItem.title }}
                   </router-link>
                   <a
                     v-else-if="childItem.legacyPath"
                     :href="arbimonLink + childItem.legacyPath"
-                    class="flex items-center p-2 pl-12 w-full text-base font-normal hover:(bg-util-gray-02 rounded-lg transition duration-300) active:(bg-gray-100 rounded-lg text-moss)"
+                    class="flex items-center pl-12 w-full text-base font-normal hover:(bg-util-gray-02 rounded transition duration-300) active:(bg-gray-100 rounded text-moss)"
                   >
                     {{ childItem.title }}
                     <icon-custom-linkout
@@ -132,27 +136,27 @@
             </li>
           </ul>
           <div class="my-2 border-t-1 border-color-util-gray-01" />
-          <ul class="px-2">
-            <li>
+          <ul class="px-3">
+            <li class="my-3">
               <router-link
                 :to="{ name: ROUTE_NAMES.myProjects }"
-                exact-active-class="bg-gray-100 rounded-lg text-moss"
-                class="flex items-center p-2 text-base font-normal active:text-moss hover:(bg-util-gray-02 rounded-lg transition duration-300) active:(bg-gray-100 rounded-lg text-moss)"
+                exact-active-class="bg-gray-100 rounded text-moss"
+                class="flex items-center text-base font-normal active:text-moss hover:(bg-util-gray-02 rounded transition duration-300) active:(bg-gray-100 rounded text-moss)"
               >
                 <span
-                  class="py-0.5"
+                  class="p-0.5"
                 >
                   <icon-custom-fi-clipboard />
                 </span>
                 <span class="ml-2 hidden group-hover:block">My Projects</span>
               </router-link>
             </li>
-            <li>
+            <li class="my-3">
               <a
                 :title="'Arbimon Support'"
                 :href="supportLink"
-                exact-active-class="bg-gray-100 rounded-lg text-moss"
-                class="flex items-center p-2 text-base font-normal hover:(bg-util-gray-02 rounded-lg transition duration-300) active:(bg-gray-100 rounded-lg text-moss)"
+                exact-active-class="bg-gray-100 rounded text-moss"
+                class="flex items-center text-base font-normal hover:(bg-util-gray-02 rounded transition duration-300) active:(bg-gray-100 rounded text-moss)"
               >
                 <icon-custom-fi-book-open />
                 <span class="ml-2 hidden group-hover:block">Arbimon Support</span>
@@ -162,22 +166,22 @@
         </div>
         <div>
           <div class="my-2 border-t-1 border-color-util-gray-01" />
-          <ul class="px-2">
-            <li>
+          <ul class="px-3">
+            <li class="my-3">
               <a
                 :title="'Account Setting'"
                 :href="arbimonLink + 'settings/users'"
-                class="flex items-center p-2 text-base font-normal active:text-moss hover:(bg-util-gray-02 rounded-lg transition duration-300) active:(bg-gray-100 rounded-lg text-moss)"
+                class="flex items-center text-base font-normal active:text-moss hover:(bg-util-gray-02 rounded transition duration-300) active:(bg-gray-100 rounded text-moss)"
               >
                 <icon-custom-fi-user />
                 <span class="ml-2 hidden group-hover:block">Account Setting</span>
               </a>
             </li>
             <li
-              class="flex items-center cursor-pointer p-2 text-base font-normal active:text-moss hover:(bg-util-gray-02 rounded-lg transition duration-300) active:(bg-gray-100 rounded-lg text-moss)"
+              class="my-2 flex items-center cursor-pointer text-base font-normal active:text-moss hover:(bg-util-gray-02 rounded transition duration-300) active:(bg-gray-100 rounded text-moss)"
               @click="logout"
             >
-              <span class="py-0.5">
+              <span class="p-0.5">
                 <icon-custom-fi-log-out />
               </span>
               <span class="ml-2 hidden group-hover:block">Sign Out</span>
@@ -186,7 +190,7 @@
           <div class="my-2 border-t-1 border-color-util-gray-01" />
           <ul class="px-1 pt-2">
             <li
-              class="flex items-center cursor-pointer p-2 text-base font-normal h-10 hover:(bg-util-gray-02 rounded-lg transition duration-300) active:(bg-gray-100 rounded-lg text-moss)"
+              class="my-2 flex items-center cursor-pointer text-base font-normal h-10 hover:(bg-util-gray-02 rounded transition duration-300) active:(bg-gray-100 rounded text-moss)"
             >
               <img
                 class="h-8 w-8 rounded-full"
