43ac1f047dfcce8aa97b36d5139e278ee702578d
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2023-11-13 21:02:14 +0400

    #1235 User can see list of highlighted species

--- apps/website/src/insights/overview/components/dashboard-highlighted-species/components/highlighted-species-list.vue
+++ apps/website/src/insights/overview/components/dashboard-highlighted-species/components/highlighted-species-list.vue
@@ -1,44 +1,43 @@
 <template>
-  <div
-    v-for="item in species"
-    :key="'dashboard-highlighted-' + item.slug"
-    class="flex content-center mb-2"
-  >
-    <img
-      class="min-h-14 h-14 min-w-14 w-14 object-cover mr-2"
-      :src="item.photoUrl"
+  <ul class="flex flex-col gap-y-2">
+    <li
+      v-for="item in species"
+      :key="'dashboard-highlighted-' + item.slug"
+      class="flex flex-row justify-between"
     >
-    <router-link
-      class="mt-0.5 self-center hover:(text-subtle)"
-      :to="{ name: ROUTE_NAMES.activityPatterns, params: { projectSlug: store.selectedProject?.slug, speciesSlug: item.slug } }"
-    >
+      <div class="flex flex-row gap-x-4">
+        <img
+          :src="item.photoUrl"
+          class="min-h-16 h-16 min-w-16 w-16 object-cover rounded bg-util-gray-02"
+        >
+        <div
+          class="self-center hover:(text-subtle)"
+        >
+          <p class="italic">
+            {{ item.scientificName }}
+          </p>
+          <p class="text-xs text-subtle">
+            {{ item.commonName || 'unknown' }}
+          </p>
+        </div>
+      </div>
       <div class="flex items-center">
         <el-tag
-          class="species-highlights border-none text-xs"
+          class="species-highlights border-none text-md h-6"
           effect="dark"
-          size="small"
+          size="large"
           :color="item.riskRating.color"
           :title="item.riskRating.label"
         >
           {{ item.riskRating.code }}
         </el-tag>
-        <p class="ml-1 italic">
-          {{ item.scientificName }}
-        </p>
       </div>
-      <p class="text-xs text-subtle capitalize">
-        {{ item.commonName || 'unknown' }}
-      </p>
-    </router-link>
-  </div>
+    </li>
+  </ul>
 </template>
 
 <script setup lang="ts">
-import { ROUTE_NAMES } from '~/router'
-import { useStore } from '~/store'
 import { type HighlightedSpeciesRow } from '../../../types/highlighted-species'
-
-const store = useStore()
 
 defineProps<{ species: HighlightedSpeciesRow[] }>()
 
