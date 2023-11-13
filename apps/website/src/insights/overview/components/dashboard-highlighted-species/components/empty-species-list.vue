06a08741ecae82b05a922014a395a7f4f7925c15
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2023-11-13 16:55:11 +0400

    #1235 Add components for empty and highlighted species list

---
+++ apps/website/src/insights/overview/components/dashboard-highlighted-species/components/empty-species-list.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    v-for="item in emptySpecies"
+    :key="item.title"
+    class="flex content-center mb-2"
+  >
+    <div
+      class="min-h-14 h-14 min-w-14 w-14 object-cover rounded bg-util-gray-02"
+    />
+    <div
+      class="ml-4 self-center hover:(text-subtle)"
+    >
+      <p class="italic">
+        {{ item.title }}
+      </p>
+      <p class="text-xs text-subtle">
+        {{ item.subtitle }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const emptyRow = ref({
+  title: 'Highlight speices',
+  subtitle: 'Not selected'
+})
+
+const emptySpecies = ref(Array(5).fill(emptyRow.value))
+</script>
+
+<style lang="scss">
+.species-highlights {
+  height: 1rem;
+}
+</style>
---
+++ apps/website/src/insights/overview/components/dashboard-highlighted-species/components/highlighted-species-list.vue
@@ -0,0 +1,51 @@
+<template>
+  <div
+    v-for="item in species"
+    :key="'dashboard-highlighted-' + item.slug"
+    class="flex content-center mb-2"
+  >
+    <img
+      class="min-h-14 h-14 min-w-14 w-14 object-cover mr-2"
+      :src="item.photoUrl"
+    >
+    <router-link
+      class="mt-0.5 self-center hover:(text-subtle)"
+      :to="{ name: ROUTE_NAMES.activityPatterns, params: { projectSlug: store.selectedProject?.slug, speciesSlug: item.slug } }"
+    >
+      <div class="flex items-center">
+        <el-tag
+          class="species-highlights border-none text-xs"
+          effect="dark"
+          size="small"
+          :color="item.riskRating.color"
+          :title="item.riskRating.label"
+        >
+          {{ item.riskRating.code }}
+        </el-tag>
+        <p class="ml-1 italic">
+          {{ item.scientificName }}
+        </p>
+      </div>
+      <p class="text-xs text-subtle capitalize">
+        {{ item.commonName || 'unknown' }}
+      </p>
+    </router-link>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ROUTE_NAMES } from '~/router'
+import { useStore } from '~/store'
+import { type HighlightedSpeciesRow } from '../../../types/highlighted-species'
+
+const store = useStore()
+
+defineProps<{ species: HighlightedSpeciesRow[] }>()
+
+</script>
+
+<style lang="scss">
+.species-highlights {
+  height: 1rem;
+}
+</style>
