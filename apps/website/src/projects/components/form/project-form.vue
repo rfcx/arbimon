7d600d918135ece7847d5bbfb80c3b2d4421b680
ratree <ratree.ochn@gmail.com>
2023-11-15 11:29:01 +0700

    #1271 Add on-going project

--- apps/website/src/projects/components/form/project-form.vue
+++ apps/website/src/projects/components/form/project-form.vue
@@ -18,7 +18,7 @@
   <div
     id="dateRangePicker"
     date-rangepicker
-    class="flex mt-4 items-center gap-4 hidden"
+    class="flex mt-7 items-center gap-4"
   >
     <div class="flex-1">
       <label
@@ -76,6 +76,13 @@
         >
       </div>
     </div>
+  </div>
+  <div class="items-center mt-4">
+    <input
+      type="checkbox"
+      class="w-5 h-5 border mb-1 border-gray-300 rounded dark:bg-echo focus:ring-3 focus:ring-primary-300 dark:border-white-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+    >
+    <label class="font-light text-gray-500 dark:text-gray-300 ml-2">This is an on-going project</label>
   </div>
 </template>
 
