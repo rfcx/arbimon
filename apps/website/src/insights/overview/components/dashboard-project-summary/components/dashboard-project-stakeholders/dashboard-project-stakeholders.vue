5b6eba210245b56a48085e3ee35254ac2a139981
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-11-15 16:23:20 +0700

    #1275 new component for stakeholders

---
+++ apps/website/src/insights/overview/components/dashboard-project-summary/components/dashboard-project-stakeholders/dashboard-project-stakeholders.vue
@@ -0,0 +1,77 @@
+<template>
+  <template v-if="organizations.length === 0">
+    <ProjectSummaryEmpty @emit-add-content="$emit('update:isEditing', true)" />
+  </template>
+  <template v-else>
+    <h3 class="text-white text-xl font-medium font-sans mt-10">
+      Project members
+    </h3>
+    <div
+      class="grid"
+      style="grid-template-columns: repeat(auto-fit, minmax(17rem, 1fr))"
+    >
+      <StakeholderCard
+        name="Lori Tan"
+        :ranking="0"
+        email="loritan@tmu.com"
+      />
+      <StakeholderCard
+        name="Lori Tan"
+        :ranking="1"
+      />
+      <StakeholderCard
+        name="Lori Tan"
+        :ranking="1"
+      />
+      <StakeholderCard
+        name="Lori Tan"
+        :ranking="1"
+      />
+      <StakeholderCard
+        name="Lori Tan"
+        :ranking="1"
+      />
+      <StakeholderCard
+        name="Lori Tan"
+        :ranking="1"
+      />
+    </div>
+    <h3 class="text-white text-xl font-medium font-sans mt-6">
+      Organizations
+    </h3>
+    <div
+      class="grid"
+      style="grid-template-columns: repeat(auto-fit, minmax(17rem, 1fr))"
+    >
+      <StakeholderCard
+        v-for="org of organizations"
+        :key="org.id"
+        :name="org.name"
+        :description="ORGANIZATION_TYPE_NAME[org.type]"
+        :ranking="1"
+      />
+    </div>
+    <div class="flex w-full justify-end mt-6">
+      <button
+        v-show="editable"
+        class="btn btn-secondary"
+      >
+        Manage stakeholders
+      </button>
+    </div>
+  </template>
+</template>
+
+<script setup lang="ts">
+import { type OrganizationTypes, ORGANIZATION_TYPE_NAME } from '@rfcx-bio/common/dao/types/organization'
+
+import ProjectSummaryEmpty from '../project-summary-empty.vue'
+import StakeholderCard from './stakeholder-card.vue'
+
+const props = defineProps<{
+  isEditing: boolean,
+  editable: boolean,
+  organizations: Array<OrganizationTypes['light']>
+}>()
+const emit = defineEmits<{(event: 'update:isEditing', value: boolean): void}>()
+</script>
