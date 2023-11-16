29f19c336e2d34eddd4c2a33f5c8925fe50cc365
Somjintana Korbut <nuisomjin@gmail.com>
2023-11-16 20:12:43 +0000

    #1225 #1227 when the user click hide insight, show the 3 button popup (step 2) as a starting point

--- apps/website/src/insights/components/share-insight/share-insight.vue
+++ apps/website/src/insights/components/share-insight/share-insight.vue
@@ -29,8 +29,8 @@ import ShareInsightSuccessful from './share-insight-successful.vue'
 
 const store = useStore()
 
-const props = defineProps<{ modelValue: 'start-show' | 'start-hide' | 'idle' }>()
-const emit = defineEmits<{(event: 'update:modelValue', value: 'start-show' | 'start-hide' | 'idle'): void, (event: 'emit-share-insight-successful'): void, (event: 'emit-hide-insight-successful'): void}>()
+const props = defineProps<{ modelValue: 'start-show' | 'start-hide' | 'default-show' | 'idle' }>()
+const emit = defineEmits<{(event: 'update:modelValue', value: 'start-show' | 'start-hide'| 'default-show' | 'idle'): void, (event: 'emit-share-insight-successful'): void, (event: 'emit-hide-insight-successful'): void}>()
 
 const steps = ref<0 | 1 | 2 | 3>(0)
 
@@ -41,6 +41,10 @@ const { mutate: mutateInsightsPublishStatus } = useUpdateInsightsPublishStatus(a
 watch(() => props.modelValue, (newValue) => {
   if (newValue === 'start-show') {
     steps.value = 1
+  }
+
+  if (newValue === 'default-show') {
+    steps.value = 2
   }
 
   if (newValue === 'start-hide') {
