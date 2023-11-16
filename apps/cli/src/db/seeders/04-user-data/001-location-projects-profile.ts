bd3663bd14b96d8a48719d6c6cd82a5e25ab4dc3
Somjintana Korbut <nuisomjin@gmail.com>
2023-11-16 21:43:06 +0000

    #1271 add dateStart & dateEnd to seed data

--- apps/cli/src/db/seeders/04-user-data/001-location-projects-profile.ts
+++ apps/cli/src/db/seeders/04-user-data/001-location-projects-profile.ts
@@ -20,7 +20,7 @@ export const up: MigrationFn<QueryInterface> = async (params): Promise<void> => 
     .then(allProjects => Object.fromEntries(allProjects.map(s => [s.slug, s.id])))
 
   const projectsProfile: LocationProjectProfile[] = rawEnvToProjectAndProfile[BIO_ENVIRONMENT]
-    .map(({ slug, summary, readme, keyResult, resources, methods, objectives }) => {
+    .map(({ slug, summary, readme, keyResult, resources, methods, objectives, dateStart, dateEnd }) => {
       // Try to find project ID
       const locationProjectId = projectSlugToId[slug]
       if (!locationProjectId) return undefined
@@ -32,7 +32,9 @@ export const up: MigrationFn<QueryInterface> = async (params): Promise<void> => 
         keyResult,
         resources,
         methods,
-        objectives
+        objectives,
+        dateStart,
+        dateEnd
       }
     })
     .filter(isDefined)
