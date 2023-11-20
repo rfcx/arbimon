ce175082eba68b6bc7145c1af91f078d669c4d55
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-11-20 20:55:19 +0700

    #1275 add seed data for the stakeholders

---
+++ apps/cli/src/db/seeders/04-user-data/005-location-project-organizations.ts
@@ -0,0 +1,20 @@
+import { type QueryInterface } from 'sequelize'
+import { type MigrationFn } from 'umzug'
+
+import { LocationProjectOrganizationModel } from '@rfcx-bio/common/dao/models/location-project-organization-model'
+import { OrganizationModel } from '@rfcx-bio/common/dao/models/organization-model'
+
+import { requireEnv } from '~/env'
+import { rawEnvToLocationProjectOrganizations } from '../_data/location-project-organizations'
+
+const { BIO_ENVIRONMENT } = requireEnv('BIO_ENVIRONMENT')
+
+export const up: MigrationFn<QueryInterface> = async (params): Promise<void> => {
+  const sequelize = params.context.sequelize
+
+  // Lookups
+  const orgs = rawEnvToLocationProjectOrganizations[BIO_ENVIRONMENT]
+
+  await OrganizationModel(sequelize).bulkCreate(orgs)
+  await LocationProjectOrganizationModel(sequelize).bulkCreate([{ locationProjectId: 1, organizationId: 1 }])
+}
