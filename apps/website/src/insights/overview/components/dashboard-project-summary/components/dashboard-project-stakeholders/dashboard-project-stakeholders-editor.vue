6bc59b298aa8c9261d39266b9f859d355dbeb5e4
Antony Harfield <antonyharfield@hotmail.com>
2023-11-21 14:59:45 +0000

    #1233 Org search dropdown

--- apps/website/src/insights/overview/components/dashboard-project-summary/components/dashboard-project-stakeholders/dashboard-project-stakeholders-editor.vue
+++ apps/website/src/insights/overview/components/dashboard-project-summary/components/dashboard-project-stakeholders/dashboard-project-stakeholders-editor.vue
@@ -91,16 +91,41 @@
         <div
           v-else
         >
-          <input
-            ref="organizationSearchInput"
-            v-model="searchOrganizationValue"
-            class="px-3 py-2 w-[20.0rem] text-sm text-insight bg-echo outline-none focus:outline-none rounded-t-lg font-sans"
-            type="text"
-            placeholder="Type to search organizations"
-            data-dropdown-toggle="dropdown"
-            @input="refetchOrganizationsSearch"
-            @blur="isOrganizationSearchInputOpen = false"
-          >
+          <div class="relative">
+            <input
+              ref="organizationSearchInput"
+              v-model="searchOrganizationValue"
+              class="px-3 py-2 w-[20.0rem] text-sm text-insight bg-echo outline-none focus:outline-none rounded-t-lg font-sans"
+              type="text"
+              placeholder="Type to search organizations"
+              data-dropdown-toggle="dropdown"
+              @input="organizationSearchInputChanged"
+              @blur="isOrganizationSearchInputOpen = false"
+            >
+            <div
+              ref="organizationSearchLoading"
+              role="status"
+              :class="{ hidden: !isOrganizationSearchLoading }"
+            >
+              <svg
+                aria-hidden="true"
+                class="inline w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+              <span class="sr-only">Loading...</span>
+            </div>
+          </div>
           <div
             ref="organizationSearchResultContainer"
             class="z-10 hidden w-[20.0rem] text-insight bg-echo border-cloud border-b border-l border-r rounded-b-lg divide-y divide-gray-100 shadow"
@@ -154,7 +179,7 @@
         class="btn btn-secondary"
         @click="onFinishedEditing"
       >
-        Save displayed stakeholders
+        Save and view insights
       </button>
     </div>
   </div>
@@ -180,6 +205,7 @@ const emit = defineEmits<{(event: 'emit-finished-editing', orgIds: number[]): vo
 const selectedUsers = ref([1])
 
 const isOrganizationSearchInputOpen = ref(false)
+const isOrganizationSearchLoading = ref(false)
 const organizationSearchInput = ref<HTMLDivElement | null>(null)
 const organizationSearchResultContainer = ref<HTMLDivElement | null>(null)
 const editableOrganizations = ref([...props.organizations])
@@ -198,6 +224,13 @@ const openOrganizationSearch = async () => {
   new Dropdown(organizationSearchResultContainer.value, organizationSearchInput.value, dropdownOptions).show()
 }
 
+const organizationSearchInputChanged = async () => {
+  isOrganizationSearchLoading.value = true
+  await refetchOrganizationsSearch()
+  isOrganizationSearchLoading.value = false
+}
+
+// TODO
 const organizationSearchResults = [
   { id: 1, name: 'Company X' },
   { id: 2, name: 'Company Y' },
