015e36859a49c0c837ee889b4940e8cfc4ae85d1
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-11-22 12:39:09 +0700

    #1233 omit id field from seeders

--- apps/cli/src/db/seeders/_data/location-project-organizations.ts
+++ apps/cli/src/db/seeders/_data/location-project-organizations.ts
@@ -2,31 +2,28 @@ import { type Organization } from '@rfcx-bio/common/dao/types'
 
 import { type BioEnvironment } from '~/env/types'
 
-const rfcx: Organization = {
-  id: 1,
+const rfcx: Omit<Organization, 'id'> = {
   name: 'Rainforest Connection',
   type: 'non-profit-organization',
   url: 'rfcx.org',
   image: 'https://rfcx.org/wp-content/themes/rainforestconnection/assets/images/favicon/RFCx-Lockup-White.svg'
 }
 
-const nu: Organization = {
-  id: 2,
+const nu: Omit<Organization, 'id'> = {
   name: 'Naresuan University',
   type: 'research-institution',
   url: 'https://nu.ac.th',
   image: 'https://www.nu.ac.th/wp-content/uploads/2017/06/NULOGO-Download.png'
 }
 
-const chingHua: Organization = {
-  id: 3,
+const chingHua: Omit<Organization, 'id'> = {
   name: 'Ching Hua University',
   type: 'research-institution',
   url: 'https://www.tsinghua.edu.cn',
   image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Tsinghua_University_Logo.svg/800px-Tsinghua_University_Logo.svg.png'
 }
 
-export const rawEnvToLocationProjectOrganizations: Record<BioEnvironment, Organization[]> = {
+export const rawEnvToLocationProjectOrganizations: Record<BioEnvironment, Array<Omit<Organization, 'id'>>> = {
   default: [
     rfcx,
     nu,
