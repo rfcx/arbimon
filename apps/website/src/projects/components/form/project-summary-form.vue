1fb3f50bd0b322c5a90c41dc74d45dcc1e6a568d
ratree <ratree.ochn@gmail.com>
2023-11-22 17:42:55 +0700

    #1300 Use component in project-summary-form.vue

--- apps/website/src/projects/components/form/project-summary-form.vue
+++ apps/website/src/projects/components/form/project-summary-form.vue
@@ -4,35 +4,25 @@
       for="summary"
       class="block mb-2 font-medium text-gray-900 dark:text-insight"
     >Project blurb</label>
-    <icon-custom-ic-info
-      data-tooltip-target="summary_tooltip"
-      data-tooltip-style="light"
-      class="inline-block basis-8 h-4 w-4 cursor-pointer text-insight"
+    <icon-i-info
+      tooltip-id="summary_tooltip"
+      :tooltip-text="PLACEHOLDER_TEXT"
     />
-    <div
-      id="summary_tooltip"
-      role="tooltip"
-      class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-gray-900 transition-opacity duration-300 bg-white rounded-lg shadow-sm opacity-0 tooltip"
-    >
-      {{ PLACEHOLDER_TEXT }}
-      <div
-        class="tooltip-arrow"
-        data-popper-arrow
-      />
-    </div>
   </div>
   <textarea
     v-model.trim="summary"
     class="input-field w-full h-28"
     :placeholder="PLACEHOLDER_SUMMARY_TEXT"
-    maxlength="140"
+    :maxlength="MAX"
   />
-  <span class="flex flex-row-reverse items-end text-fog">{{ summary.length }}/{{ MAX }} Characters</span>
+  <span class="flex flex-row-reverse items-end text-fog text-xs">{{ summary.length }}/{{ MAX }} Characters</span>
 </template>
 
 <script setup lang="ts">
 import { initTooltips } from 'flowbite'
 import { onMounted, ref, watch } from 'vue'
+
+import IconIInfo from '../icon-i-info.vue'
 
 const props = defineProps<{
   existingSummary?: string
