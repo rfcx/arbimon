94b4c2294561be477a19f571b6b086f5dcb75c87
Somjintana Korbut <nuisomjin@gmail.com>
2023-11-22 13:14:41 +0000

    project objective - fix typo

--- apps/website/src/projects/types.ts
+++ apps/website/src/projects/types.ts
@@ -44,7 +44,7 @@ export interface ProjectObjective {
 }
 
 export const masterOjectiveTypes = {
-  BioBaseline: { id: 100, slug: 'bio-baseline', description: 'Establish baseline biodiversity' },
+  BioBaseline: { id: 100, slug: 'bio-baseline', description: 'Establish biodiversity baseline' },
   MonitorSpecies: { id: 200, slug: 'monitor-species', description: 'Detect / monitor endangered species' },
   MonitorIllegalAct: { id: 300, slug: 'monitor-illegal-act', description: 'Detect and monitor illegal activity' },
   ImpactHuman: { id: 400, slug: 'impact-human', description: 'Evaluate impact of human activities on biodiversity' },
