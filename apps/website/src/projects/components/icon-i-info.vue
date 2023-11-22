951b80a08c2312be11784d3c4355db50a8d4dd03
RatreeOchn <51106210+RatreeOchn@users.noreply.github.com>
2023-11-22 18:05:01 +0700

    Add description (i) icons (#1330)
    * #1300 Add description (i) icons on Project settings
    
    * #1300 Add placeholder and max length
    
    * #1290 change font weight
    
    * #1300 Add limit text count at 140 characters
    
    * #1300 Use component in project-summary-form.vue
    
    ---------
    
    Co-authored-by: Somjintana Korbut <nuisomjin@gmail.com>

---
+++ apps/website/src/projects/components/icon-i-info.vue
@@ -0,0 +1,34 @@
+<template>
+  <icon-custom-ic-info
+    v-if="tooltipText"
+    :data-tooltip-target="tooltipId"
+    data-tooltip-style="light"
+    class="inline-block basis-8 h-4 w-4 cursor-pointer text-insight"
+  />
+  <div
+    :id="tooltipId"
+    role="tooltip"
+    class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-gray-900 transition-opacity duration-300 bg-white rounded-lg shadow-sm opacity-0 tooltip"
+  >
+    {{ tooltipText }}
+    <div
+      class="tooltip-arrow"
+      data-popper-arrow
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { initTooltips } from 'flowbite'
+import { onMounted } from 'vue'
+
+defineProps<{
+  tooltipId: string
+  tooltipText?: string
+}>()
+
+onMounted(() => {
+  initTooltips()
+})
+
+</script>
