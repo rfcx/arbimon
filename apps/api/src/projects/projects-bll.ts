59c4e802f42489dceeb888abe1ed7538dfe4d6cb
Somjintana Korbut <nuisomjin@gmail.com>
2023-11-24 21:12:30 +0000

    #1320 rename imported function

--- apps/api/src/projects/projects-bll.ts
+++ apps/api/src/projects/projects-bll.ts
@@ -1,11 +1,11 @@
 import { type MyProjectsResponse, type ProjectsResponse } from '@rfcx-bio/common/api-bio/project/projects'
 
-import { getViewableProjects } from '@/projects/projects-dao'
+import { getMyProjectsWithInfo as getMyProjects, getViewableProjects } from '@/projects/projects-dao'
 
 export const getProjects = async (memberProjectCoreIds: string[]): Promise<ProjectsResponse> => {
   return await getViewableProjects(memberProjectCoreIds)
 }
 
 export const getMyProjectsWithInfo = async (memberProjectCoreIds: string[]): Promise<MyProjectsResponse> => {
-  return await getMyProjectsWithInfo(memberProjectCoreIds)
+  return await getMyProjects(memberProjectCoreIds)
 }
