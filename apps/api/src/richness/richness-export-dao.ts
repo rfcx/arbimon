bc6266872f4f085b7e7536006eb0be3095d9e193
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-11-27 18:44:27 +0700

    #1331 rfcx/engineering-support#479 update query for better performance

--- apps/api/src/richness/richness-export-dao.ts
+++ apps/api/src/richness/richness-export-dao.ts
@@ -14,28 +14,24 @@ export const getRichnessExportData = async (sequelize: Sequelize, filter: Filter
   const sql = `
     SELECT
       ts.id_arbimon as species_id,
-      sip.common_name as name,
-      sip.scientific_name as scientific_name,
+      ts.scientific_name as scientific_name,
       ls.id_arbimon as site_id,
-      ls.name as site,
+      ls.name as site_name,
       ls.latitude as latitude,
       ls.longitude as longitude,
       ls.altitude as altitude,
       EXTRACT(day FROM dbssh.time_precision_hour_local) as day,
       EXTRACT(month FROM dbssh.time_precision_hour_local) as month,
       EXTRACT(year FROM dbssh.time_precision_hour_local) as year,
-      dbssh.time_precision_hour_local as date,
       EXTRACT(hour from dbssh.time_precision_hour_local) as hour,
       dbssh.count
     FROM
       detection_by_site_species_hour dbssh
-      LEFT JOIN species_in_project sip ON dbssh.taxon_species_id = sip.taxon_species_id
-      LEFT JOIN location_site ls ON dbssh.location_site_id = ls.id
-      INNER JOIN taxon_species ts on sip.taxon_species_id = ts.id
-    WHERE ${conditions} 
-    GROUP BY
-      ls.id, ts.id_arbimon, sip.taxon_species_slug, sip.common_name, sip.scientific_name, sip.taxon_class_id, dbssh.taxon_species_id, dbssh.time_precision_hour_local, dbssh.count
-    ORDER BY sip.common_name, ls.name, dbssh.time_precision_hour_local
+      JOIN taxon_species ts ON dbssh.taxon_species_id = ts.id
+      JOIN location_site ls ON dbssh.location_site_id = ls.id
+    WHERE
+      ${conditions}
+    ORDER BY ts.scientific_name, dbssh.time_precision_hour_local
   `
 
   return await sequelize.query(sql, { type: QueryTypes.SELECT, bind, raw: true })
