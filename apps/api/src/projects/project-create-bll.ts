f9f654066c656be09f31fe106e5e414f74ee1d4d
Antony Harfield <antonyharfield@hotmail.com>
2023-11-28 11:06:49 +0000

    Refactor orgs and projs

--- apps/api/src/projects/project-create-bll.ts
+++ apps/api/src/projects/project-create-bll.ts
@@ -15,11 +15,9 @@ export const createProject = async (request: ProjectCreateRequest, auth0UserId: 
   // Pre-populate insights table with the same data (will get updated again after sync)
   const dateStart = request.dateStart ? new Date(request.dateStart) : null
   const dateEnd = request.dateEnd ? new Date(request.dateEnd) : null
-
   if (dateStart && dateEnd && dateStart > dateEnd) {
     throw new Error('Date start must be before date end')
   }
-
   const project = { idCore, idArbimon, name: request.name, objectives: request.objectives, dateStart, dateEnd }
   const slug = await createProjectLocal(project)
   return slug
