f90cc70198fc02395334ca1ca11ae8bcdafcd258
Somjintana Korbut <nuisomjin@gmail.com>
2023-11-30 10:59:40 +0000

    #1304 #1305 #1309 Project directory - add more mock data, and implement scroll to load more

--- apps/website/src/projects/project-directory/explore-page.vue
+++ apps/website/src/projects/project-directory/explore-page.vue
@@ -2,7 +2,7 @@
   <landing-navbar />
   <section class="static overflow-hidden">
     <project-list
-      :data="mockProjects"
+      :data="mockProjects.sort((a, b) => a ? -1 : 1)"
       class="absolute z-40 h-[calc(100vh_-_15rem)]"
     />
     <map-view
@@ -19,9 +19,16 @@ import ProjectList from './blocks/projects-list.vue'
 import { rawDirectoryProjectsData } from './data/rawDirectoryProjectsData'
 
 const mockProjects = rawDirectoryProjectsData
-const mockMapData: MapSiteDataLight[] = mockProjects.map(project => ({
-  siteName: project.name,
-  latitude: project.latitudeNorth + project.latitudeSouth / 2,
-  longitude: project.longitudeWest + project.longitudeEast / 2
-}))
+const mockMapData: MapSiteDataLight[] = mockProjects.map(project => {
+  const avgCoordinate = (x: number, y: number) => {
+    if (x === y) return x
+    return (x + y) / 2
+  }
+  return {
+    siteName: project.name,
+    latitude: avgCoordinate(project.latitudeNorth, project.latitudeSouth),
+    longitude: avgCoordinate(project.longitudeEast, project.longitudeWest)
+  }
+})
+
 </script>
