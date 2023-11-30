cbe4f120d78a167d3271c9bd8cd4c2852b56814f
ratree <ratree.ochn@gmail.com>
2023-11-30 13:09:08 +0700

    #1317 Call load more function when scroll

--- apps/website/src/projects/my-projects-page.vue
+++ apps/website/src/projects/my-projects-page.vue
@@ -1,31 +1,33 @@
 <template>
-  <landing-navbar />
-  <section class="pt-16 bg-white dark:bg-echo">
-    <div class="py-8 mx-auto max-w-screen-xl <lg:mx-8 lg:py-24">
-      <div class="mt-6 flex flex-row">
-        <h2 class="text-gray-900 dark:text-insight">
-          My Projects
-        </h2>
-        <router-link
-          :to="{ name: ROUTE_NAMES.createProject }"
-          class=" flex-row ml-6"
+  <div v-infinite-scroll="loadMore">
+    <landing-navbar />
+    <section class="pt-16 bg-white dark:bg-echo">
+      <div class="py-8 mx-auto max-w-screen-xl <lg:mx-8 lg:py-24">
+        <div class="mt-6 flex flex-row">
+          <h2 class="text-gray-900 dark:text-insight">
+            My Projects
+          </h2>
+          <router-link
+            :to="{ name: ROUTE_NAMES.createProject }"
+            class=" flex-row ml-6"
+          >
+            <a class="btn btn-primary">
+              Create a new project +
+            </a>
+          </router-link>
+        </div>
+        <div
+          class="grid grid-cols-2 gap-4 py-8 lg:gap-6 lg:py-16 lg:grid-cols-3"
         >
-          <a class="btn btn-primary">
-            Create a new project +
-          </a>
-        </router-link>
+          <ProjectCard
+            v-for="project in myProjectsInfo"
+            :key="project.id"
+            :project="project"
+          />
+        </div>
       </div>
-      <div
-        class="grid grid-cols-2 gap-4 py-8 lg:gap-6 lg:py-16 lg:grid-cols-3"
-      >
-        <ProjectCard
-          v-for="project in myProjectsInfo"
-          :key="project.id"
-          :project="project"
-        />
-      </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
 <script setup lang="ts">
 import { computed } from 'vue'
@@ -36,6 +38,10 @@ import { useStore } from '~/store'
 import ProjectCard from './components/project-card.vue'
 
 const store = useStore()
+
+const loadMore = () => {
+  console.info('hello')
+}
 
 const myProjectsInfo = computed(() => store.myProjects)
 </script>
