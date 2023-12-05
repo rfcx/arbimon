23cd20a2111577988fd3426552f6e721f5f22d23
Somjintana Korbut <nuisomjin@gmail.com>
2023-12-05 09:32:46 +0000

    #1319 try moving the type back

--- apps/website/src/projects/project-directory/components/project-list-item.vue
+++ apps/website/src/projects/project-directory/components/project-list-item.vue
@@ -30,8 +30,7 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 
-import { masterOjectiveTypes, objectiveTypes } from '@rfcx-bio/common/dao/master-data'
-
+import { masterOjectiveTypes, objectiveTypes } from '../../types'
 import type { ProjectProfileWithMetrics } from '../data/types'
 
 const props = defineProps<{ project: ProjectProfileWithMetrics, isSelected: boolean }>()
