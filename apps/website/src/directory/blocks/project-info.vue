d51c2ed672b81f89dd43b0150663456913eab7b6
Somjintana Korbut <nuisomjin@gmail.com>
2023-12-06 10:43:42 +0000

    #1304 get project light if the project data hasn't been loaded

--- apps/website/src/directory/blocks/project-info.vue
+++ apps/website/src/directory/blocks/project-info.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="flex flex-col gap-4 inset-1/4 left-100 w-98 bg-moss transition-transform -translate-x-full p-6 lg:p-10 rounded-lg">
     <span class="text-flamingo"> ⚠️ work in progress ⚠️ </span>
-    {{ project?.name }}
+    <span
+      v-if="project?.countries.length !== 0"
+      class="text-xxs text-spoonbill"
+    >{{ project?.countries }}</span>
+    <span class="text-lg">{{ project?.name }}</span>
     <router-link
       :to="`/p/${project?.slug}`"
       class="text-frequency"
@@ -30,5 +34,23 @@ import { type ProjectProfileWithMetrics } from '../data/types'
 const props = defineProps<{ projectId: number }>()
 
 const pdStore = useProjectDirectoryStore()
-const project = computed<ProjectProfileWithMetrics | undefined>(() => pdStore.getProjectWithMetricsById(props.projectId))
+const project = computed<ProjectProfileWithMetrics | undefined>(() => {
+  const project = pdStore.getProjectWithMetricsById(props.projectId)
+  if (!project) { // TODO: fetch from api if there is no metrics in store
+    const projectLight = pdStore.getProjectLightById(props.projectId)
+    if (!projectLight) { return undefined }
+    return {
+      ...projectLight,
+      summary: 'This is a test project!',
+      objectives: ['bio-baseline'],
+      noOfSpecies: 0,
+      noOfRecordings: 0,
+      countries: [],
+      isHighlighted: false,
+      isMock: true,
+      imageUrl: ''
+    }
+  }
+  return project
+})
 </script>
