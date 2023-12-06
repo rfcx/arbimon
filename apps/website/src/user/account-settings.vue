854f1f6cfa0b2987a51d0a870590d94bec01deb6
ratree <ratree.ochn@gmail.com>
2023-12-06 20:07:42 +0700

    #1310 Background Account settings and limit of get loadmore

--- apps/website/src/user/account-settings.vue
+++ apps/website/src/user/account-settings.vue
@@ -1,226 +1,228 @@
 <template>
   <landing-navbar />
-  <section class="pt-16 bg-white dark:bg-echo">
-    <div class="py-8 px-4 mx-auto max-w-screen-md lg:py-10 border-b-1 border-white/80">
-      <h1 class="tracking-tight font-medium text-gray-900 dark:text-white">
-        Account settings
-      </h1>
-      <span v-if="isLoadingProfileData">Loading</span>
-    </div>
-    <div class="py-8 px-4 mx-auto max-w-screen-md lg:py-10 border-b-1 border-white/80">
-      <div class="flex items-start gap-4 flex-col md:flex-row">
-        <img
-          :src="profilePhoto"
-          alt="Profile"
-          class="w-32 h-32 aspect-square object-cover rounded-full mr-4"
-        >
-        <div>
-          <h2>
-            Change profile photo
-          </h2>
-          <h4 class="mt-3">
-            JPG and PNG files less than 5 MB are supported.
-          </h4>
-          <input
-            id="fileUpload"
-            type="file"
-            accept="image/jpeg, image/png"
-            hidden
-            @change="uploadPhoto"
-          >
-          <button
-            class="btn btn-secondary group mt-5"
-            type="button"
-            @click="selectPhoto"
-          >
-            Upload photo <icon-custom-cloud-upload class="ml-2 group-hover:stroke-pitch inline-flex" />
-          </button>
-        </div>
+  <section class="pt-16 mb-16 bg-white dark:bg-echo">
+    <div class="block rounded-lg bg-moss py-3 px-4 mx-auto max-w-screen-md dark:bg-moss h-auto">
+      <div class="py-2 px-4 mx-auto max-w-screen-md border-b-1 border-white/80">
+        <h2 class="tracking-tight font-medium text-gray-900 dark:text-white">
+          Account settings
+        </h2>
+        <span v-if="isLoadingProfileData">Loading</span>
       </div>
-    </div>
-    <div class="py-8 px-4 mx-auto max-w-screen-md lg:py-10">
-      <p class="text-insight text-base font-medium font-sans">
-        First name*
-      </p>
-      <input
-        id="firstName"
-        v-model="firstName"
-        name="firstName"
-        type="text"
-        class="w-full mt-2 border border-cloud rounded-md dark:(bg-pitch text-fog placeholder:text-insight) focus:(border-frequency ring-frequency) disabled:opacity-70"
-        required
-      >
-      <p class="mt-5 text-insight text-base font-medium font-sans">
-        Last name*
-      </p>
-      <input
-        id="lastName"
-        v-model="lastName"
-        name="lastName"
-        type="text"
-        class="w-full mt-2 border border-cloud rounded-md dark:(bg-pitch text-fog placeholder:text-insight) focus:(border-frequency ring-frequency) disabled:opacity-70"
-        required
-      >
-      <p class="mt-5 opacity-70 text-insight text-base font-medium font-sans">
-        Email address
-      </p>
-      <input
-        id="email"
-        v-model="email"
-        name="email"
-        type="text"
-        class="w-full mt-2 border border-cloud rounded-md dark:(bg-pitch text-fog placeholder:text-insight) focus:(border-frequency ring-frequency) disabled:opacity-70"
-        disabled
-      >
-      <p class="mt-5 text-insight text-base font-medium font-sans">
-        Affiliated organization
-      </p>
-      <div class="flex items-center mt-2">
-        <button
-          v-if="dropdownStatus === 'idle'"
-          @click="openOrganizationSearch()"
-        >
-          <icon-custom-ft-search-lg
-            class="text-white w-5 h-5"
-          />
-        </button>
-        <div
-          v-else
-        >
-          <div class="relative">
+      <div class="py-8 px-4 mx-auto max-w-screen-md lg:py-10 border-b-1 border-white/80">
+        <div class="flex items-start gap-4 flex-col md:flex-row">
+          <img
+            :src="profilePhoto"
+            alt="Profile"
+            class="w-32 h-32 aspect-square object-cover rounded-full mr-4"
+          >
+          <div>
+            <h3>
+              Change profile photo
+            </h3>
+            <h5 class="mt-3">
+              JPG and PNG files less than 5 MB are supported.
+            </h5>
             <input
-              ref="organizationSearchInput"
-              v-model="searchOrganizationValue"
-              class="px-3 py-2 w-[20.0rem] text-sm text-insight bg-echo outline-none focus:outline-none rounded-t-lg font-sans"
-              :class="{ 'rounded-b-lg': orgsSearchResult.length === 0 || dropdownStatus !== 'search' }"
-              type="text"
-              placeholder="Type to search organizations"
-              data-dropdown-toggle="dropdown"
-              @input="organizationSearchInputChanged"
-              @blur="onBlur"
+              id="fileUpload"
+              type="file"
+              accept="image/jpeg, image/png"
+              hidden
+              @change="uploadPhoto"
             >
-            <div
-              ref="organizationSearchLoading"
-              role="status"
-              class="absolute z-index-10 absolute top-1 right-3"
-              :class="{ hidden: !isSearchOrganizationFetching }"
-            >
-              <svg
-                aria-hidden="true"
-                class="inline w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-                viewBox="0 0 100 101"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                  fill="currentColor"
-                />
-                <path
-                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                  fill="currentFill"
-                />
-              </svg>
-              <span class="sr-only">Loading...</span>
-            </div>
-          </div>
-
-          <div
-            ref="createNewOrganizationFormContainer"
-            class="z-10 hidden w-[20.0rem] text-insight bg-moss border-cloud border-b border-l border-r rounded-b-lg shadow"
-          >
-            <div class="max-w-sm mx-auto p-3">
-              <div class="mb-5">
-                <label
-                  for="dashboard-project-summary-stakeholders-select-partner-type"
-                  class="block mb-2 text-insight text-sm font-normal font-sans leading-normal"
-                >
-                  Select partner type
-                </label>
-                <select
-                  id="dashboard-project-summary-stakeholders-select-partner-type"
-                  v-model="newOrganizationType"
-                  class="bg-echo border border-cloud text-insight text-sm rounded-lg block w-full py-2 px-3 font-sans"
-                >
-                  <option
-                    v-for="orgType in ORGANIZATION_TYPE"
-                    :key="orgType"
-                    :value="orgType"
-                  >
-                    {{ ORGANIZATION_TYPE_NAME[orgType] }}
-                  </option>
-                </select>
-              </div>
-              <div class="mb-5">
-                <label
-                  for="dashboard-project-summary-stakeholders-input-organization-url"
-                  class="block mb-2 text-insight text-sm font-normal font-sans leading-normal"
-                >
-                  Website
-                </label>
-                <input
-                  id="dashboard-project-summary-stakeholders-input-organization-url"
-                  v-model="newOrganizationUrl"
-                  type="text"
-                  placeholder="www.darwinfoundation.org"
-                  class="bg-echo border-cloud py-2 px-3 text-insight placeholder:italic placeholder:opacity-75 placeholder:text-stone-300 text-sm rounded-lg block w-full font-sans"
-                  required
-                >
-              </div>
-              <div class="flex w-full flex-row justify-end">
-                <button
-                  type="submit"
-                  class="btn btn-primary"
-                  @click="createNewOrganization"
-                >
-                  Create organization
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <div
-            ref="organizationSearchResultNotFoundContainer"
-            class="z-10 hidden w-[20.0rem] text-insight bg-echo border-cloud border-b border-l border-r rounded-b-lg shadow flex flex-row justify-between p-3"
-          >
-            <p class="text-sm font-normal font-sans text-insight leading-tight">
-              We are unable to find this organization.
-            </p>
             <button
+              class="btn btn-secondary group mt-5"
               type="button"
-              class="text-frequency text-sm font-medium font-display leading-none"
-              @click="openCreateNewOrganizationForm"
+              @click="selectPhoto"
             >
-              Create
+              Upload photo <icon-custom-cloud-upload class="ml-2 group-hover:stroke-pitch inline-flex" />
             </button>
           </div>
-          <div
-            ref="organizationSearchResultContainer"
-            class="z-10 hidden w-[20.0rem] text-insight bg-echo border-cloud border-b border-l border-r rounded-b-lg divide-y divide-gray-100 shadow"
-          >
-            <OrganizationSearchResultCard
-              v-for="s in orgsSearchResult"
-              :id="s.id"
-              :key="`${s.id}-${s.name}-search`"
-              :name="s.name"
-              :description="s.description"
-              :image="s.image"
-              @emit-add-to-selected-organization="onAddNewOrganizationFromSearch"
-            />
-          </div>
         </div>
       </div>
-      <button
-        class="w-full btn btn-primary inline items-center group mt-7"
-        type="button"
-        @click="saveAccountSetting"
-      >
-        Save
-        <icon-fas-spinner
-          v-if="isUpdatingProfilePhoto || isUpdatingUserProfile"
-          class="animate-spin w-4 h-4 ml-2 inline"
-        />
-      </button>
+      <div class="py-8 px-4 mx-auto max-w-screen-md lg:py-10">
+        <p class="text-insight text-base font-medium font-sans">
+          First name*
+        </p>
+        <input
+          id="firstName"
+          v-model="firstName"
+          name="firstName"
+          type="text"
+          class="w-full mt-2 border border-cloud rounded-md dark:(bg-pitch text-fog placeholder:text-insight) focus:(border-frequency ring-frequency) disabled:opacity-70"
+          required
+        >
+        <p class="mt-5 text-insight text-base font-medium font-sans">
+          Last name*
+        </p>
+        <input
+          id="lastName"
+          v-model="lastName"
+          name="lastName"
+          type="text"
+          class="w-full mt-2 border border-cloud rounded-md dark:(bg-pitch text-fog placeholder:text-insight) focus:(border-frequency ring-frequency) disabled:opacity-70"
+          required
+        >
+        <p class="mt-5 opacity-70 text-insight text-base font-medium font-sans">
+          Email address
+        </p>
+        <input
+          id="email"
+          v-model="email"
+          name="email"
+          type="text"
+          class="w-full mt-2 border border-cloud rounded-md dark:(bg-pitch text-fog placeholder:text-insight) focus:(border-frequency ring-frequency) disabled:opacity-70"
+          disabled
+        >
+        <p class="mt-5 text-insight text-base font-medium font-sans">
+          Affiliated organization
+        </p>
+        <div class="flex items-center mt-2">
+          <button
+            v-if="dropdownStatus === 'idle'"
+            @click="openOrganizationSearch()"
+          >
+            <icon-custom-ft-search-lg
+              class="text-white w-5 h-5"
+            />
+          </button>
+          <div
+            v-else
+          >
+            <div class="relative">
+              <input
+                ref="organizationSearchInput"
+                v-model="searchOrganizationValue"
+                class="px-3 py-2 w-[20.0rem] text-sm text-insight bg-echo outline-none focus:outline-none rounded-t-lg font-sans"
+                :class="{ 'rounded-b-lg': orgsSearchResult.length === 0 || dropdownStatus !== 'search' }"
+                type="text"
+                placeholder="Type to search organizations"
+                data-dropdown-toggle="dropdown"
+                @input="organizationSearchInputChanged"
+                @blur="onBlur"
+              >
+              <div
+                ref="organizationSearchLoading"
+                role="status"
+                class="absolute z-index-10 absolute top-1 right-3"
+                :class="{ hidden: !isSearchOrganizationFetching }"
+              >
+                <svg
+                  aria-hidden="true"
+                  class="inline w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+                <span class="sr-only">Loading...</span>
+              </div>
+            </div>
+
+            <div
+              ref="createNewOrganizationFormContainer"
+              class="z-10 hidden w-[20.0rem] text-insight bg-moss border-cloud border-b border-l border-r rounded-b-lg shadow"
+            >
+              <div class="max-w-sm mx-auto p-3">
+                <div class="mb-5">
+                  <label
+                    for="dashboard-project-summary-stakeholders-select-partner-type"
+                    class="block mb-2 text-insight text-sm font-normal font-sans leading-normal"
+                  >
+                    Select partner type
+                  </label>
+                  <select
+                    id="dashboard-project-summary-stakeholders-select-partner-type"
+                    v-model="newOrganizationType"
+                    class="bg-echo border border-cloud text-insight text-sm rounded-lg block w-full py-2 px-3 font-sans"
+                  >
+                    <option
+                      v-for="orgType in ORGANIZATION_TYPE"
+                      :key="orgType"
+                      :value="orgType"
+                    >
+                      {{ ORGANIZATION_TYPE_NAME[orgType] }}
+                    </option>
+                  </select>
+                </div>
+                <div class="mb-5">
+                  <label
+                    for="dashboard-project-summary-stakeholders-input-organization-url"
+                    class="block mb-2 text-insight text-sm font-normal font-sans leading-normal"
+                  >
+                    Website
+                  </label>
+                  <input
+                    id="dashboard-project-summary-stakeholders-input-organization-url"
+                    v-model="newOrganizationUrl"
+                    type="text"
+                    placeholder="www.darwinfoundation.org"
+                    class="bg-echo border-cloud py-2 px-3 text-insight placeholder:italic placeholder:opacity-75 placeholder:text-stone-300 text-sm rounded-lg block w-full font-sans"
+                    required
+                  >
+                </div>
+                <div class="flex w-full flex-row justify-end">
+                  <button
+                    type="submit"
+                    class="btn btn-primary"
+                    @click="createNewOrganization"
+                  >
+                    Create organization
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div
+              ref="organizationSearchResultNotFoundContainer"
+              class="z-10 hidden w-[20.0rem] text-insight bg-echo border-cloud border-b border-l border-r rounded-b-lg shadow flex flex-row justify-between p-3"
+            >
+              <p class="text-sm font-normal font-sans text-insight leading-tight">
+                We are unable to find this organization.
+              </p>
+              <button
+                type="button"
+                class="text-frequency text-sm font-medium font-display leading-none"
+                @click="openCreateNewOrganizationForm"
+              >
+                Create
+              </button>
+            </div>
+            <div
+              ref="organizationSearchResultContainer"
+              class="z-10 hidden w-[20.0rem] text-insight bg-echo border-cloud border-b border-l border-r rounded-b-lg divide-y divide-gray-100 shadow"
+            >
+              <OrganizationSearchResultCard
+                v-for="s in orgsSearchResult"
+                :id="s.id"
+                :key="`${s.id}-${s.name}-search`"
+                :name="s.name"
+                :description="s.description"
+                :image="s.image"
+                @emit-add-to-selected-organization="onAddNewOrganizationFromSearch"
+              />
+            </div>
+          </div>
+        </div>
+        <button
+          class="w-full btn btn-primary inline items-center group mt-7"
+          type="button"
+          @click="saveAccountSetting"
+        >
+          Save
+          <icon-fas-spinner
+            v-if="isUpdatingProfilePhoto || isUpdatingUserProfile"
+            class="animate-spin w-4 h-4 ml-2 inline"
+          />
+        </button>
+      </div>
     </div>
   </section>
 </template>
