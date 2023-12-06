8eea1382d54107b6fad88f254091eda11fb11766
RatreeOchn <51106210+RatreeOchn@users.noreply.github.com>
2023-12-06 16:19:10 +0700

    1310 Account settings (#1358)
    * #1310 Added account setting page
    
    * #1310 Added change profile UI
    
    * #1310 Add first / last name, email, affiliated organization and save button (only UI)
    
    * #1310 Show the real photo
    
    * #1310 Disabled email
    
    * #1310 Update account setting link into the sidebar
    
    * #1312 Show selected profile photo into the form
    
    * #1312 Fix lint
    
    * #1312 Connect selected image with bio api
    
    * #1310 Show name and first name
    
    * #1310 Added call api for user can edit the first name and last name
    
    * #1310 Update Account settings text
    
    * #1310 Get user profile
    
    * #1310 Get profile new way
    
    ---------
    
    Co-authored-by: Evgeniia Rassokhina <ea.rassokhina@gmail.com>

--- apps/website/src/_services/router/route-names.ts
+++ apps/website/src/_services/router/route-names.ts
@@ -28,6 +28,7 @@ export const ROUTE_NAMES = {
   // User
   userCompleteProfile: 'user_complete_profile',
   userPreferences: 'user_preferences',
+  accountSettings: 'account_settings',
   // Other
   callback: 'callback',
   info: 'info',
