542b8eb5814493501289e813b23c3afdf9df000c
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-12-06 18:08:49 +0700

    #1306 add try catch to detect forbidden response
    Guest role cannot see project users.

--- apps/website/src/_services/store/use-project-user-permissions-store.ts
+++ apps/website/src/_services/store/use-project-user-permissions-store.ts
@@ -35,8 +35,13 @@ export const useProjectUserPermissionsStore = defineStore('project-user-permissi
     const authClient = await useAuth0Client()
     const apiClient = getApiClient(import.meta.env.VITE_API_BASE_URL, async () => await getIdToken(authClient))
 
-    const members = await apiClient.get<GetProjectMembersResponse>(`/projects/${id}/users`)
-    projectMembers.value = members.data
+    try {
+      const members = await apiClient.get<GetProjectMembersResponse>(`/projects/${id}/users`)
+      projectMembers.value = members.data
+    } catch (_e) {
+      // guaranteed Guest
+      projectMembers.value = []
+    }
   }
 
   const currentUserRoleOfCurrentProject = computed<CoreUser['role']>(() => {
