fd88e15d1339db05579e60b3b57f5641a47bf2a8
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-12-06 17:43:29 +0700

    #1336 abstract aws logic to different module

---
+++ apps/api/src/_services/storage/index.ts
@@ -0,0 +1,73 @@
+import { GetObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { type Readable } from 'node:stream'
+
+import { BioPublicError } from '~/errors'
+import { env } from '../env'
+
+const {
+  AWS_S3_ENDPOINT: endpoint,
+  AWS_S3_BUCKET_REGION: region,
+  AWS_S3_BUCKET_NAME: bucketName,
+  AWS_S3_ACCESS_KEY_ID: accessKeyId,
+  AWS_S3_SECRET_ACCESS_KEY: secretAccessKey
+} = env
+
+let s3: S3Client | null = null
+export const getS3Client = (): S3Client => {
+  if (s3 == null) {
+    if (
+      accessKeyId == null || accessKeyId === '' ||
+      secretAccessKey == null || secretAccessKey === '' ||
+      bucketName == null || bucketName === '' ||
+      region == null || region === ''
+    ) {
+      throw new BioPublicError('AWS S3 bucket credentials are not defined', 500)
+    }
+
+    s3 = new S3Client({
+      endpoint,
+      region,
+      credentials: {
+        accessKeyId,
+        secretAccessKey
+      }
+    })
+
+    return s3
+  }
+
+  return s3
+}
+
+export const getObject = async (key: string): Promise<ArrayBuffer> => {
+  const client = getS3Client()
+
+  const command = new GetObjectCommand({
+    Bucket: bucketName,
+    Key: key
+  })
+
+  const response = await client.send(command)
+  const stream = response.Body as Readable
+
+  return await new Promise<ArrayBuffer>((resolve, reject) => {
+    const chunks: Buffer[] = []
+
+    stream.on('data', chunk => chunks.push(chunk))
+    stream.once('error', () => { reject(new BioPublicError('Cannot parse image from AWS S3 into file correctly', 500)) })
+    stream.once('end', () => { resolve(Buffer.concat(chunks)) })
+  })
+}
+
+export const putObject = async (key: string, body: Buffer, mimetype: string): Promise<void> => {
+  const client = getS3Client()
+
+  const command = new PutObjectCommand({
+    Bucket: bucketName,
+    Key: key,
+    Body: body,
+    ContentType: mimetype
+  })
+
+  await client.send(command)
+}
--- apps/api/src/users/user-profile-bll.ts
+++ apps/api/src/users/user-profile-bll.ts
@@ -1,12 +1,11 @@
-import { GetObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import { type MultipartFile } from '@fastify/multipart'
 import { extname } from 'node:path'
-import { type Readable } from 'node:stream'
 
 import { type UserProfileResponse } from '@rfcx-bio/common/api-bio/users/profile'
 import { type OrganizationTypes, type UserProfile } from '@rfcx-bio/common/dao/types'
 
 import { BioNotFoundError, BioPublicError } from '~/errors'
+import { getObject, putObject } from '~/storage'
 import { env } from '../_services/env'
 import { getAllOrganizations as daoGetAllOrganizations, getUserProfile as daoGetUserProfile, patchUserProfile as daoPatchUserProfile, patchUserProfileImage as daoPatchUserProfileImage } from './user-profile-dao'
 
@@ -35,72 +34,31 @@ export const getUserProfileImage = async (userId: string): Promise<ArrayBuffer> 
     throw BioNotFoundError()
   }
 
-  if (
-    env.AWS_S3_BUCKET_REGION === undefined || env.AWS_S3_BUCKET_REGION === '' ||
-    env.AWS_S3_ENDPOINT === undefined || env.AWS_S3_ENDPOINT === '' ||
-    env.AWS_S3_ACCESS_KEY_ID === undefined || env.AWS_S3_ACCESS_KEY_ID === '' ||
-    env.AWS_S3_SECRET_ACCESS_KEY === undefined || env.AWS_S3_SECRET_ACCESS_KEY === ''
-  ) {
-    throw new BioPublicError('AWS S3 bucket region is not defined', 500)
-  }
-
-  // TODO: fix default bucket
-  const s3 = new S3Client({
-    region: env.AWS_S3_BUCKET_REGION,
-    endpoint: env.AWS_S3_ENDPOINT,
-    credentials: {
-      accessKeyId: env.AWS_S3_ACCESS_KEY_ID,
-      secretAccessKey: env.AWS_S3_SECRET_ACCESS_KEY
-    }
-  })
-
-  const command = new GetObjectCommand({
-    Bucket: env.AWS_S3_BUCKET_REGION,
-    Key: userProfile.image
-  })
-
-  const response = await s3.send(command)
-  const stream = response.Body as Readable
-
-  return await new Promise<ArrayBuffer>((resolve, reject) => {
-    const chunks: Buffer[] = []
-
-    stream.on('data', chunk => chunks.push(chunk))
-    stream.once('error', () => { reject(new BioPublicError('Cannot parse image from AWS S3 into file correctly', 500)) })
-    stream.once('end', () => { resolve(Buffer.concat(chunks)) })
-  })
+  return await getObject(userProfile.image)
 }
 
 export const patchUserProfileImage = async (userId: string, file: MultipartFile): Promise<void> => {
+    const {
+      AWS_S3_ENDPOINT: endpoint,
+      AWS_S3_BUCKET_REGION: region,
+      AWS_S3_BUCKET_NAME: bucketName,
+      AWS_S3_ACCESS_KEY_ID: accessKeyId,
+      AWS_S3_SECRET_ACCESS_KEY: secretAccessKey
+    } = env
+
   if (
-    env.AWS_S3_BUCKET_NAME === undefined || env.AWS_S3_BUCKET_NAME === '' ||
-    env.AWS_S3_BUCKET_REGION === undefined || env.AWS_S3_BUCKET_REGION === '' ||
-    env.AWS_S3_ENDPOINT === undefined || env.AWS_S3_ENDPOINT === '' ||
-    env.AWS_S3_ACCESS_KEY_ID === undefined || env.AWS_S3_ACCESS_KEY_ID === '' ||
-    env.AWS_S3_SECRET_ACCESS_KEY === undefined || env.AWS_S3_SECRET_ACCESS_KEY === ''
+    bucketName === undefined || bucketName === '' ||
+    region === undefined || region === '' ||
+    accessKeyId === undefined || accessKeyId === '' ||
+    secretAccessKey === undefined || secretAccessKey === ''
   ) {
-    throw new BioPublicError('AWS S3 bucket region is not defined', 500)
+    throw new BioPublicError('AWS S3 bucket credentials is not defined', 500)
   }
 
-  const s3 = new S3Client({
-    region: env.AWS_S3_BUCKET_REGION,
-    endpoint: env.AWS_S3_ENDPOINT,
-    credentials: {
-      accessKeyId: env.AWS_S3_ACCESS_KEY_ID,
-      secretAccessKey: env.AWS_S3_SECRET_ACCESS_KEY
-    }
-  })
+  const imagePath = `users/${userId}/profile-image${extname(file.filename)}`
+  await putObject(`/${imagePath}`, await file.toBuffer(), file.mimetype)
 
-  const imagePath = `${env.AWS_S3_BUCKET_NAME}/${userId}/profile-image${extname(file.filename)}`
-  const command = new PutObjectCommand({
-    Bucket: env.AWS_S3_BUCKET_NAME,
-    Key: imagePath,
-    Body: await file.toBuffer(),
-    ContentType: file.mimetype
-  })
-
-  await s3.send(command)
-  await daoPatchUserProfileImage(userId, env.AWS_S3_ENDPOINT == null || env.AWS_S3_ENDPOINT === '' ? `https://${env.AWS_S3_BUCKET_NAME}.s3.${env.AWS_S3_BUCKET_REGION}.amazonaws.com/${imagePath}` : `${env.AWS_S3_ENDPOINT}/${imagePath}`)
+  await daoPatchUserProfileImage(userId, endpoint == null || endpoint === '' ? `https://${bucketName}.s3.${region}.amazonaws.com/${imagePath}` : `${endpoint}/${bucketName}/${imagePath}`)
 }
 
 export const getAllOrganizations = async (): Promise<Array<OrganizationTypes['light']>> => {
