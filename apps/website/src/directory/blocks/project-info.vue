40fac93c4758e02d7167960b02cbc8e65a825b21
ratree <ratree.ochn@gmail.com>
2023-12-08 12:58:15 +0700

    #1360 Add image on project info

--- apps/website/src/directory/blocks/project-info.vue
+++ apps/website/src/directory/blocks/project-info.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="flex flex-col gap-4 inset-1/4 left-100 w-98 bg-pitch transition-transform -translate-x-full rounded-lg">
-    <div class="rounded-t-lg bg-moss p-4 text-spoonbill">
+  <div class="flex flex-col inset-1/4 left-100 w-98 bg-pitch transition-transform -translate-x-full rounded-lg">
+    <div class="rounded-t-lg bg-moss p-4">
       <div class="flex flex-row justify-between items-center">
         <span
           v-if="project?.countries.length === 0"
-          class="text-xs text-spoonbill flex-1"
+          class="text-xs text-spoonbill font-medium flex-1"
         >
           No site
         </span>
         <span
           v-if="project?.countries.length !== 0"
-          class="text-spoonbill"
+          class="text-spoonbill font-medium text-xs flex-1"
         >{{ project?.countries }}</span>
         <icon-fa-close
           class="text-fog m-auto self-end w-4 h-3.5"
@@ -18,27 +18,31 @@
         />
       </div>
     </div>
-    <span
-      v-if="project?.countries.length !== 0"
-      class="text-xxs text-spoonbill"
-    >{{ project?.countries }}</span>
-    <span class="text-lg">{{ project?.name }}</span>
-    <router-link
-      :to="`/p/${project?.slug}`"
-      class="text-frequency"
-    >
-      <button
-        class="btn btn-primary w-full"
-        :disabled="project?.isMock"
-        :class="{'opacity-50 cursor-not-allowed': project?.isMock}"
+    <div class="overflow-scroll">
+      <img
+        src="project?.imageUrl"
+        class="w-full h-full object-cover object-center h-52"
       >
-        View Insights
-      </button>
-    </router-link>
-    <span
-      v-if="project?.isMock"
-      class="text-sm text-gray-300 px-2"
-    >Only fake data. This project is not on dev environment.</span>
+      <div class=" p-4">
+        <span class="text-lg">{{ project?.name }}</span>
+        <router-link
+          :to="`/p/${project?.slug}`"
+          class="text-frequency"
+        >
+          <button
+            class="btn btn-primary w-full"
+            :disabled="project?.isMock"
+            :class="{'opacity-50 cursor-not-allowed': project?.isMock}"
+          >
+            View Insights
+          </button>
+        </router-link>
+        <span
+          v-if="project?.isMock"
+          class="text-sm text-gray-300 px-2"
+        >Only fake data. This project is not on dev environment. </span>
+      </div>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
@@ -71,7 +75,4 @@ const project = computed<ProjectProfileWithMetrics | undefined>(() => {
   }
   return project
 })
-console.info(project)
-console.info(project.value?.countries)
-
 </script>
