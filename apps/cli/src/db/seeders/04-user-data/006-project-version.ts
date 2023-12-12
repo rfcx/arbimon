f797319a5bf457ca8c79956d2fdab8ffd3f2870a
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2023-12-12 18:43:03 +0400

    Fix lint

--- apps/cli/src/db/seeders/04-user-data/006-project-version.ts
+++ apps/cli/src/db/seeders/04-user-data/006-project-version.ts
@@ -2,13 +2,12 @@ import { type QueryInterface } from 'sequelize'
 import { type MigrationFn } from 'umzug'
 
 import { LocationProjectModel } from '@rfcx-bio/common/dao/models/location-project-model'
-import { LocationProjectProfileModel } from '@rfcx-bio/common/dao/models/location-project-profile-model'
-import { type LocationProjectProfile, type ProjectVersion } from '@rfcx-bio/common/dao/types'
+import { ProjectVersionModel } from '@rfcx-bio/common/dao/models/project-version-model'
+import { type ProjectVersion } from '@rfcx-bio/common/dao/types'
 import { isDefined } from '@rfcx-bio/utils/predicates'
 
 import { requireEnv } from '~/env'
 import { rawEnvToProjectAndProfile } from '../_data/location-project-and-profile'
-import { ProjectVersionModel } from '@rfcx-bio/common/dao/models/project-version-model'
 
 const { BIO_ENVIRONMENT } = requireEnv('BIO_ENVIRONMENT')
 
@@ -20,7 +19,7 @@ export const up: MigrationFn<QueryInterface> = async (params): Promise<void> => 
     .findAll()
     .then(allProjects => Object.fromEntries(allProjects.map(s => [s.slug, s.id])))
 
-  const projectVersion: Omit<ProjectVersion, 'id'>[] = rawEnvToProjectAndProfile[BIO_ENVIRONMENT].map(({ slug, isPublic, isPublished }) => {
+  const projectVersion: Array<Omit<ProjectVersion, 'id'>> = rawEnvToProjectAndProfile[BIO_ENVIRONMENT].map(({ slug, isPublic, isPublished }) => {
     // Try to find project ID
     const locationProjectId = projectSlugToId[slug]
     if (!locationProjectId) return undefined
