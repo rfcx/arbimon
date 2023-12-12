2cd36fc3694acab9ffed232039c0b8019b485e9b
Somjintana Korbut <nuisomjin@gmail.com>
2023-12-12 09:00:38 +0000

    #1361 implement mock bll to get directory projects from json file

--- apps/api/src/projects/get-directory-projects-bll.ts
+++ apps/api/src/projects/get-directory-projects-bll.ts
@@ -1,6 +1,6 @@
 import { type DirectoryProjectsQuery, type DirectoryProjectsResponse } from '@rfcx-bio/common/api-bio/project/projects'
 
-import { queryDirectoryProjects } from './get-directory-projects-dao'
+import { queryDirectoryProjects, getDirectoryProjects as getMock } from './get-directory-projects-dao'
 
 export const getDirectoryProjects = async (req: DirectoryProjectsQuery): Promise<DirectoryProjectsResponse> => {
   const isFull = req.full ?? false
@@ -8,4 +8,12 @@ export const getDirectoryProjects = async (req: DirectoryProjectsQuery): Promise
   const keywords = req.keywords?.split(',') ?? []
   console.info('getDirectoryProjects', req, isFull, ids, keywords)
   return await queryDirectoryProjects(isFull, ids, keywords)
+}
+
+export const getDirectoryProjectsMock = async (req: DirectoryProjectsQuery): Promise<DirectoryProjectsResponse> => {
+  const isFull = req.full ?? false
+  const ids = (req.ids?.split(',') ?? []).map((id) => parseInt(id))
+  const keywords = req.keywords?.split(',') ?? []
+  console.info('getDirectoryProjectsMock', req, isFull, ids, keywords)
+  return getMock(isFull, ids, keywords)
 }
