870719b7e14c4b5eaab7f624c74d9560d6058b18
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2023-12-13 12:52:12 +0400

    #1372 stakeholders: fix jumping image/name

--- apps/website/src/insights/overview/components/dashboard-project-summary/components/dashboard-project-stakeholders/stakeholder-card-edit.vue
+++ apps/website/src/insights/overview/components/dashboard-project-summary/components/dashboard-project-stakeholders/stakeholder-card-edit.vue
@@ -1,7 +1,7 @@
 <template>
   <div
-    class="flex flex-row items-center justify-start space-x-3 border-1 border-insight rounded-lg py-4 px-[18px] row-span-3 group hover:(row-span-4 py-8 bg-moss)"
-    :class="{'row-span-4 py-8 bg-moss': isHovered, 'border-frequency relative': checked}"
+    class="flex flex-row items-center justify-between border-1 border-insight rounded-lg py-4 px-[18px] row-span-3 group hover:(row-span-4 py-8 bg-moss)"
+    :class="{'row-span-4 bg-moss': isHovered, 'border-frequency relative row-span-4 bg-moss': checked}"
     @click="toggleSelectMemberCard"
   >
     <icon-custom-fi-check-circle
@@ -9,11 +9,11 @@
       class="text-frequency bg-pitch w-6 h-6 absolute -translate-y-1/2 translate-x-1/2 left-auto -top-3 -right-3"
     />
     <img
-      class="w-12 h-12 rounded-full shadow"
+      class="w-12 h-12 mr-3 rounded-full shadow"
       :src="image || 'https://inaturalist-open-data.s3.amazonaws.com/photos/332643265/small.jpeg'"
       alt="user profile image"
     >
-    <div class="max-w-2/3">
+    <div class="w-2/4">
       <h3
         v-if="ranking === 0"
         class="text-danger text-xs font-medium font-eyebrow uppercase"
@@ -34,42 +34,44 @@
         </a>
       </div>
     </div>
-    <button
-      :id="`${email}EditStakeholderDropdownButton`"
-      :data-dropdown-toggle="`${email}dropdownBottom`"
-      data-dropdown-placement="bottom"
-      class="hidden relative group-hover:block !ml-auto"
-      type="button"
-      @click="toggleCard()"
-    >
-      <icon-custom-dots-vertical />
-      <div
-        :id="`${email}dropdownBottom`"
-        class="z-10 hidden list-none bg-moss divide-y divide-gray-100 rounded-lg shadow p-3 w-56"
-        @mouseleave="toggleCard()"
+    <div class="w-1/4">
+      <button
+        :id="`${email}EditStakeholderDropdownButton`"
+        :data-dropdown-toggle="`${email}dropdownBottom`"
+        data-dropdown-placement="bottom"
+        class="hidden relative group-hover:block !ml-auto"
+        type="button"
+        @click="toggleCard()"
       >
-        <ul
-          :aria-labelledby="`${email}EditStakeholderDropdownButton`"
-          class="flex flex-col gap-y-3"
+        <icon-custom-dots-vertical class="hidden relative group-hover:block !ml-auto"/>
+        <div
+          :id="`${email}dropdownBottom`"
+          class="z-10 hidden list-none bg-moss divide-y divide-gray-100 rounded-lg shadow p-3 w-56"
+          @mouseleave="toggleCard()"
         >
-          <li
-            class="flex flex-row items-center justify-start space-x-2"
-            @click="makePrimaryContact()"
+          <ul
+            :aria-labelledby="`${email}EditStakeholderDropdownButton`"
+            class="flex flex-col gap-y-3"
           >
-            <icon-fa-plus class="h-3 w-3 text-insight mr-2" />
-            <span class="text-left">Make primary contact</span>
-          </li>
-          <li
-            class="flex flex-row items-center justify-start space-x-2"
-            :class="{'text-util-gray-02': isHidden}"
-            @click="hideEmail()"
-          >
-            <icon-custom-fi-eye-off class="h-6 w-6" />
-            <span class="text-left">Hide email (display email only)</span>
-          </li>
-        </ul>
-      </div>
-    </button>
+            <li
+              class="flex flex-row items-center justify-start space-x-2"
+              @click="makePrimaryContact()"
+            >
+              <icon-fa-plus class="h-3 w-3 text-insight mr-2" />
+              <span class="text-left">Make primary contact</span>
+            </li>
+            <li
+              class="flex flex-row items-center justify-start space-x-2"
+              :class="{'text-util-gray-02': isHidden}"
+              @click="hideEmail()"
+            >
+              <icon-custom-fi-eye-off class="h-6 w-6" />
+              <span class="text-left">Hide email (display email only)</span>
+            </li>
+          </ul>
+        </div>
+      </button>
+    </div>
   </div>
 </template>
 
