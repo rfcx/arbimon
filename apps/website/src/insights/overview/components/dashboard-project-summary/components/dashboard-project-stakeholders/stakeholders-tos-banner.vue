adce6626586619b7e63b1924dd81b07433fa9c9a
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-12-14 16:27:03 +0700

    #1306 readonly banner for guest and logged out user

---
+++ apps/website/src/insights/overview/components/dashboard-project-summary/components/dashboard-project-stakeholders/stakeholders-tos-banner.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-row gap-x-3 py-5 px-6 border border-util-gray-01 rounded-lg bg-util-gray-03">
+    <icon-custom-alert-circle class="text-insight w-6 h-6" />
+    <span class="text-insight text-base font-normal font-sans leading-normal">
+      The stakeholder information is provided by the project owners and has not been verified by Arbimon. Our user
+      <a
+        href="https://rfcx.org/terms-of-service"
+        rel="noopener noreferrer"
+        class="hover:underline text-frequency"
+      >
+        terms and conditions
+      </a>
+      detail our community expectations.
+    </span>
+  </div>
+</template>
