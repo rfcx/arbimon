f2cda1e80c5c1fc592eed02994f164af174c59c3
Somjintana Korbut <nuisomjin@gmail.com>
2023-12-14 22:45:08 +0000

    #1378 set pre handler for project profile route

--- apps/api/src/project-profile/index.ts
+++ apps/api/src/project-profile/index.ts
@@ -1,5 +1,6 @@
 import { projectDataRoute } from '@rfcx-bio/common/api-bio/project-profile/project-settings'
 
+import { setMemberProjectCoreIds } from '@/_middleware/get-member-projects'
 import { type RouteRegistration, GET, PATCH } from '../_services/api-helpers/types'
 import { projectProfileHandler, projectProfileUpdateHandler } from './project-profile-handler'
 
@@ -7,11 +8,13 @@ export const routesProjectProfile: RouteRegistration[] = [
   {
     method: GET,
     url: projectDataRoute + '/profile',
+    preHandler: [setMemberProjectCoreIds],
     handler: projectProfileHandler
   },
   {
     method: PATCH,
     url: projectDataRoute + '/profile',
+    preHandler: [setMemberProjectCoreIds],
     handler: projectProfileUpdateHandler
   }
 ]
