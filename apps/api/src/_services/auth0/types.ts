5693d9a05c8141452591dd5e40ac064a7eee5fd6
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-12-18 12:27:43 +0700

    #1363 ignore users without names in the jwt. (tree's case)

--- apps/api/src/_services/auth0/types.ts
+++ apps/api/src/_services/auth0/types.ts
@@ -6,23 +6,31 @@ export interface Auth0UserInfo {
     }
     guid: string
     loginsNumber: number
+    accessibleSites?: string[]
+    defaultSite?: string
   }
   user_metadata: {
-    consentGiven: string | boolean
-    consentGivenDashboard: string | boolean
-    consentGivenAcousticsExplorer: string | boolean
-    consentGivenRangerApp: string | boolean
+    name?: string
+    given_name?: string
+    family_name?: string
+    consentGiven?: string | boolean
+    consentGivenDashboard?: string | boolean
+    consentGivenAcousticsExplorer?: string | boolean
+    consentGivenRangerApp?: string | boolean
   }
   'https://rfcx.org/user_metadata': {
-    consentGiven: string | boolean
-    consentGivenDashboard: string | boolean
-    consentGivenAcousticsExplorer: string | boolean
-    consentGivenRangerApp: string | boolean
+    name?: string
+    given_name?: string
+    family_name?: string
+    consentGiven?: string | boolean
+    consentGivenDashboard?: string | boolean
+    consentGivenAcousticsExplorer?: string | boolean
+    consentGivenRangerApp?: string | boolean
   }
   guid: string
   loginsNumber: string
-  given_name: string
-  family_name: string
+  given_name?: string
+  family_name?: string
   nickname: string
   name: string
   picture: string | null
