8e4e2b96c1344962308daf32c254f367a117ffe2
ratree <ratree.ochn@gmail.com>
2023-12-18 14:48:42 +0700

    #1345 use 'focus:ring-0'

--- apps/website/src/projects/components/form/project-objective-form.vue
+++ apps/website/src/projects/components/form/project-objective-form.vue
@@ -29,7 +29,7 @@
         v-if="obj.slug === 'others'"
         v-model="otherReason"
         type="text"
-        class="input-field ml-4 w-full disabled:cursor-not-allowed disabled:opacity-75"
+        class="input-field ml-4 w-full disabled:cursor-not-allowed disabled:opacity-75 focus:ring-0"
         :disabled="isDisabled"
         :class="{'border-1 border-frequency': isSelected(obj.id)}"
         @click="forceSelectOther"
@@ -117,9 +117,3 @@ watch(() => selectedSlugs.value, () => {
   emit('emitProjectObjectives', selectedSlugs.value)
 })
 </script>
-
-<style lang="scss">
-.input-field:focus {
-  --tw-border-opacity: 0 !important
-}
-</style>
