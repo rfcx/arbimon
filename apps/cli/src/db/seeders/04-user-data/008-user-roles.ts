7a3f4cfe0fd9e6468abd9447e4f029a8dfb24cbc
Somjintana Korbut <nuisomjin@gmail.com>
2023-12-21 15:32:23 +0000

    #1306 add users and roles to seeders data

---
+++ apps/cli/src/db/seeders/04-user-data/008-user-roles.ts
@@ -0,0 +1,31 @@
+import { type QueryInterface } from 'sequelize'
+import { type MigrationFn } from 'umzug'
+
+import { LocationProjectModel } from '@rfcx-bio/common/dao/models/location-project-model'
+import { LocationProjectUserRoleModel } from '@rfcx-bio/common/dao/models/location-project-user-role-model'
+
+import { rawUsersWithRolesToProjects } from '../_data/users'
+
+export const up: MigrationFn<QueryInterface> = async (params): Promise<void> => {
+  const sequelize = params.context.sequelize
+
+  // Lookups
+  const projectSlugToId: Record<string, number> = await LocationProjectModel(sequelize)
+    .findAll()
+    .then(allProjects => Object.fromEntries(allProjects.map(s => [s.slug, s.id])))
+
+  rawUsersWithRolesToProjects.forEach(({ slug, users }) => {
+    // Try to find project ID
+    const locationProjectId = projectSlugToId[slug]
+    if (!locationProjectId) return
+
+    users.flatMap(async ({ userId, roleId }) => {
+      return await LocationProjectUserRoleModel(sequelize).create({
+        locationProjectId,
+        userId,
+        roleId,
+        ranking: -1
+      })
+    })
+  })
+}
