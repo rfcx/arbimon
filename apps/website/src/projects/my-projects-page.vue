3851fd9dbe03ee166ed0fdd09d13dd7c4663dde8
RatreeOchn <51106210+RatreeOchn@users.noreply.github.com>
2023-12-22 15:49:24 +0700

    1367 My project adjustments (#1407)
    * #1367 Add link to support page
    
    * #1367 Update the error state - use a hyperlink for try again
    
    * #1367 Update loading state
    
    * #1367 Change text 'Help & support documents'

--- apps/website/src/projects/my-projects-page.vue
+++ apps/website/src/projects/my-projects-page.vue
@@ -21,12 +21,32 @@
           </router-link>
         </div>
         <div>
-          <h4
+          <div
             v-if="isLoading && projects.length === 0"
-            class="py-10"
+            class="fixed inset-0 flex flex-col justify-center items-center"
           >
-            Loading...
-          </h4>
+            <div class="animate-spin h-10 w-10">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  class="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  stroke-width="4"
+                />
+                <path
+                  class="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                />
+              </svg>
+            </div>
+          </div>
         </div>
         <div
           v-if="projects.length !== 0"
@@ -44,14 +64,14 @@
             class="mt-40 lg:mt-60"
           >
             <h3 class="mb-8 text-xl text-gray-900 dark:text-insight font-header">
-              Content not available. Try again.
+              Content not available.
+              <span
+                class="text-frequency cursor-pointer"
+                @click="fetchProjects(0, LIMIT)"
+              >
+                Try again.
+              </span>
             </h3>
-            <a
-              class="btn btn-secondary"
-              @click="fetchProjects(0, LIMIT)"
-            >
-              Try again
-            </a>
           </div>
         </div>
         <div class="mx-auto max-w-screen-md text-center mt-35 xl:mt-45">
@@ -73,7 +93,10 @@
               </a>
             </router-link>
             <h6 class="mt-8 text-base">
-              Not sure where to start? Check out our tutorial video.
+              Not sure where to start? Check out our <a
+                href="https://support.rfcx.org/"
+                class="text-frequency"
+              >help & support documents</a>.
             </h6>
           </div>
         </div>
