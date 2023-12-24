f36414b7a400e6f262fcf02cdcd399d711ae74cc
Antony Harfield <antonyharfield@hotmail.com>
2023-12-24 00:41:51 +0000

    Fix pinia tests

--- apps/website/src/_testing/default-render-options.ts
+++ apps/website/src/_testing/default-render-options.ts
@@ -2,7 +2,7 @@ import { createTestingPinia } from '@pinia/testing'
 import { type MountingOptions } from '@vue/test-utils'
 import { vi } from 'vitest'
 
-type VueTestUtilsRenderOptions = Omit<MountingOptions<Record<string, any>>, 'attachTo' | 'propsData' | 'shallow'>
+type VueTestUtilsRenderOptions = Omit<(MountingOptions<any> & Record<string, any>), 'attachTo' | 'propsData' | 'shallow'>
 
 export const DEFAULT_RENDER_OPTIONS: VueTestUtilsRenderOptions = {
   global: {
