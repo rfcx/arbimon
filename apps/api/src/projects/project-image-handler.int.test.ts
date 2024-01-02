84c31d7652d8b85147e03f246803b11090160238
Antony Harfield <antonyharfield@hotmail.com>
2024-01-02 12:11:01 +0700

    #1404 Basic test (failing)

---
+++ apps/api/src/projects/project-image-handler.int.test.ts
@@ -0,0 +1,58 @@
+import formAutoContent from 'form-auto-content'
+import { createReadStream } from 'fs'
+import { afterAll, beforeAll, expect, test, vi } from 'vitest'
+
+import { projectProfileImageRoute } from '@rfcx-bio/common/api-bio/project/project-image'
+import { modelRepositoryWithElevatedPermissions } from '@rfcx-bio/testing/dao'
+import { makeApp } from '@rfcx-bio/testing/handlers'
+
+import { PATCH } from '~/api-helpers/types'
+import { routesProject } from './index'
+
+vi.mock('~/api-core/api-core')
+
+const { LocationProject, LocationProjectProfile } = modelRepositoryWithElevatedPermissions
+
+const defaultProject = {
+  id: 50010,
+  idArbimon: 5010,
+  idCore: 's1O7h5',
+  name: 'Sloths hanging from branches',
+  slug: 'sloths-hanging',
+  latitudeNorth: 0,
+  latitudeSouth: 0,
+  longitudeEast: 0,
+  longitudeWest: 0
+}
+const defaultImageUrl = 'https://loremflickr.com/320/240/forest?lock=1'
+const localImageUrl = '../website/src/_assets/default-species-image.jpg'
+
+beforeAll(async () => {
+  if (await LocationProject.findByPk(defaultProject.id) === undefined) {
+    await LocationProject.create(defaultProject)
+  }
+})
+
+afterAll(async () => {
+  await LocationProjectProfile.destroy({ where: { locationProjectId: defaultProject.id } })
+  await LocationProject.destroy({ where: { id: defaultProject.id } })
+})
+
+test.only(`PATCH ${projectProfileImageRoute} uploads file to storage`, async () => {
+  // Arrange
+  const app = await makeApp(routesProject, { projectRole: 'admin' })
+  const url = projectProfileImageRoute.replace(':projectId', defaultProject.id.toString())
+  const form = formAutoContent({
+    file: createReadStream(localImageUrl) // createUrlReadStream(defaultImageUrl)
+  })
+
+  // Act
+  const response = await app.inject({ method: PATCH, url, ...form })
+
+  // Assert
+  expect(response.statusCode).toBe(204)
+  const profile = await LocationProjectProfile.findOne({ where: { locationProjectId: defaultProject.id } })
+  expect(profile).toBeDefined()
+  expect(profile?.image).toBeDefined()
+  // TODO: check storage
+})
