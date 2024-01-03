36a5dfa9766fcefb8fb7499282132c35b62118f7
Antony Harfield <antonyharfield@hotmail.com>
2024-01-03 17:08:40 +0700

    #1404 Refactor profile dao

--- apps/api/src/projects/project-create-handler.ts
+++ apps/api/src/projects/project-create-handler.ts
@@ -6,6 +6,11 @@ import { createProject } from './project-create-bll'
 export const projectCreateHandler: Handler<ProjectCreateResponse, unknown, unknown, ProjectCreateRequest> = async (req, res) => {
   // Inputs & validation
   const project = req.body
+  const dateStart = project.dateStart ? new Date(project.dateStart) : undefined
+  const dateEnd = project.dateEnd ? new Date(project.dateEnd) : undefined
+  if (dateStart && dateEnd && dateStart > dateEnd) {
+    throw new Error('Date start must be before date end')
+  }
 
   const [slug, id] = await createProject(project, req.userId as number, req.headers.authorization ?? '')
   res.statusCode = 201
