7148d7236a58643d15e821f29b808c48c179a857
ratree <ratree.ochn@gmail.com>
2024-01-09 21:36:21 +0700

    #1392 component for map control

---
+++ apps/website/src/insights/overview/components/dashboard-map/components/map-style-options.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="items-center inline-flex gap-2">
+    Map type:
+    <div>
+      <button
+        id="mapTypeDropdown"
+        data-dropdown-toggle="mapDropdown"
+        class="border-1 border-frequency rounded-full bg-moss text-frequency px-3 py-2 flex items-center gap-2"
+        type="button"
+      >
+        {{ mapStyleLabel }}
+        <span class="pl-3">
+          <icon-fa-chevron-down class="w-3 h-3 fa-chevron-down" />
+          <icon-fa-chevron-up class="w-3 h-3 fa-chevron-up hidden" />
+        </span>
+      </button>
+      <div
+        id="mapDropdown"
+        class="z-10 hidden bg-moss border-1 border-frequency rounded-lg"
+      >
+        <ul
+          aria-labelledby="mapTypeDropdown"
+          class="p-2 flex flex-col font-medium"
+        >
+          <li
+            v-for="mapStyle in mapStatisticsDisplayStyleOptions"
+            :key="mapStyle.name"
+            v-modal="mapStatisticsStyle"
+            class="bg-moss text-frequency px-3 py-2 flex items-center gap-2"
+            :class="mapStyleLabel === mapStyle.name ? 'border-1 border-frequency rounded-full' : ''"
+            @click="propagateMapStatisticsStyle(mapStyle.style)"
+          >
+            <div
+              class="species-highlights items-center border-none cursor-pointer text-md select-none h-6 bg-moss"
+              size="large"
+            >
+              {{ mapStyle.name }}
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { initDropdowns } from 'flowbite'
+import { onMounted, ref } from 'vue'
+
+import { type MapboxStatisticsStyle, type MapboxStyle, MAPBOX_STYLE_CIRCLE, MAPBOX_STYLE_HEATMAP } from '~/maps'
+import type { MapOptions } from '~/maps/map-tool-menu/types'
+
+const props = defineProps<{
+  dropdown?: Dropdown
+}>()
+
+const emit = defineEmits<{(e: 'emitMapStyle', style: MapboxStyle): void}>()
+const mapStatisticsDisplayStyleOptions: MapOptions[] = [
+  { style: MAPBOX_STYLE_HEATMAP, name: 'Heatmap', icon: new URL('./icons/heatmap.svg', import.meta.url).toString() },
+  { style: MAPBOX_STYLE_CIRCLE, name: 'Point map', icon: new URL('./icons/bubble.svg', import.meta.url).toString() }
+]
+const mapStatisticsStyle = ref<MapboxStatisticsStyle>(MAPBOX_STYLE_CIRCLE)
+const mapStyleLabel = ref<string>(mapStatisticsDisplayStyleOptions[1].name)
+
+onMounted(() => {
+  initDropdowns()
+})
+
+const propagateMapStatisticsStyle = (style: MapboxStyle) => {
+  mapStatisticsStyle.value = style as MapboxStatisticsStyle
+  if (style === MAPBOX_STYLE_CIRCLE) {
+    mapStyleLabel.value = mapStatisticsDisplayStyleOptions[1].name
+  } else {
+    mapStyleLabel.value = mapStatisticsDisplayStyleOptions[0].name
+  }
+  if (props?.dropdown !== null) {
+    props?.dropdown.hide()
+  }
+  emit('emitMapStyle', style)
+}
+
+</script>
--- apps/website/src/insights/overview/components/dashboard-map/dashboard-map.vue
+++ apps/website/src/insights/overview/components/dashboard-map/dashboard-map.vue
@@ -12,47 +12,11 @@
           @emit-taxon-class-filter="onEmitTaxonClassFilter"
         />
       </div>
-      <div class="items-center inline-flex gap-2">
-        Map type:
-        <div>
-          <button
-            id="mapTypeDropdown"
-            data-dropdown-toggle="mapDropdown"
-            class="border-1 border-frequency rounded-full bg-moss text-frequency px-3 py-2 flex items-center gap-2"
-            type="button"
-          >
-            {{ mapStyleLabel }}
-            <span class="pl-3">
-              <icon-fa-chevron-down class="w-3 h-3 fa-chevron-down" />
-              <icon-fa-chevron-up class="w-3 h-3 fa-chevron-up hidden" />
-            </span>
-          </button>
-          <div
-            id="mapDropdown"
-            class="z-10 hidden bg-moss border-1 border-frequency rounded-lg"
-          >
-            <ul
-              aria-labelledby="mapTypeDropdown"
-              class="p-2 flex flex-col font-medium"
-            >
-              <li
-                v-for="mapStyle in mapStatisticsDisplayStyleOptions"
-                :key="mapStyle.name"
-                v-modal="mapStatisticsStyle"
-                class="bg-moss text-frequency px-3 py-2 flex items-center gap-2"
-                :class="mapStyleLabel === mapStyle.name ? 'border-1 border-frequency rounded-full' : ''"
-                @click="propagateMapStatisticsStyle(mapStyle.style)"
-              >
-                <div
-                  class="species-highlights items-center border-none cursor-pointer text-md select-none h-6 bg-moss"
-                  size="large"
-                >
-                  {{ mapStyle.name }}
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
+      <div>
+        <map-style-options
+          :dropdown="dropdown"
+          @emit-map-style="onMapStyleChange"
+        />
       </div>
     </div>
   </div>
@@ -101,23 +65,23 @@ import { type ComputedRef, type Ref, computed, inject, onMounted, ref } from 'vu
 import { dayjs } from '@rfcx-bio/utils/dayjs-initialized'
 
 import { apiClientKey } from '@/globals'
-import { type MapboxStatisticsStyle, type MapboxStyle, MAPBOX_STYLE_CIRCLE, MAPBOX_STYLE_HEATMAP } from '~/maps'
+import { type MapboxStatisticsStyle, MAPBOX_STYLE_CIRCLE, MAPBOX_STYLE_HEATMAP } from '~/maps'
 import { DEFAULT_NON_ZERO_STYLE } from '~/maps/constants'
 import { MapBaseComponent } from '~/maps/map-base'
 import CircleLegend from '~/maps/map-legend/circle-legend.vue'
 import HeatmapLegend from '~/maps/map-legend/heatmap-legend.vue'
-import type { MapOptions } from '~/maps/map-tool-menu/types'
 import { type MapBaseFormatter, type MapDataSet, type MapSiteData } from '~/maps/types'
 import { CircleFormatterNormalizedWithMin } from '~/maps/utils/circle-formatter/circle-formatter-normalized-with-min'
 import { useStore } from '~/store'
 import { useGetDashboardDataBySite } from './_composables/use-get-visaulizer'
+import MapStyleOptions from './components/map-style-options.vue'
 import TaxonFilter from './components/taxon-filter.vue'
 
 const apiClientBio = inject(apiClientKey) as AxiosInstance
 const store = useStore()
 
 const MAP_KEY = 'speciesRichness'
-let dropdown: any = null
+let dropdown: Dropdown
 
 onMounted(() => {
   initDropdowns()
@@ -133,14 +97,11 @@ const { isLoading: isLoadingDataBySite, isError: isErrorDataBySite, data: dataBy
 
 // UI
 const selectedTaxons: Ref<number[] | null> = ref(null)
-
-// Map
-const mapStatisticsDisplayStyleOptions: MapOptions[] = [
-  { style: MAPBOX_STYLE_HEATMAP, name: 'Heatmap', icon: new URL('./icons/heatmap.svg', import.meta.url).toString() },
-  { style: MAPBOX_STYLE_CIRCLE, name: 'Point map', icon: new URL('./icons/bubble.svg', import.meta.url).toString() }
-]
 const mapStatisticsStyle = ref<MapboxStatisticsStyle>(MAPBOX_STYLE_CIRCLE)
-const mapStyleLabel = ref<string>(mapStatisticsDisplayStyleOptions[1].name)
+
+const onMapStyleChange = (style: MAPBOX_STYLE_CIRCLE) => {
+  mapStatisticsStyle.value = style
+}
 
 const filteredByTaxon = computed(() => {
   const data = dataBySite.value?.richnessBySite ?? []
@@ -182,16 +143,6 @@ const mapDataset: ComputedRef<MapDataSet> = computed(() => {
       }
     }
 })
-
-const propagateMapStatisticsStyle = (style: MapboxStyle) => {
-  mapStatisticsStyle.value = style as MapboxStatisticsStyle
-  if (style === MAPBOX_STYLE_CIRCLE) {
-    mapStyleLabel.value = mapStatisticsDisplayStyleOptions[1].name
-  } else {
-    mapStyleLabel.value = mapStatisticsDisplayStyleOptions[0].name
-  }
-  dropdown.hide()
-}
 
 const mapInitialBounds: ComputedRef<LngLatBoundsLike | null> = computed(() => {
   const project = store.selectedProject
