064e46e590b7bca0a1b34c85a1f6c693b22d631b
Bhattarapong Somwong <numbbutt34685@gmail.com>
2024-01-12 19:06:50 +0700

    #1333 full reindex test suite

--- apps/cli/src/search/full-reindex.ts
+++ apps/cli/src/search/full-reindex.ts
@@ -12,10 +12,25 @@ export const fullReindex = async (client: Client, sequelize: Sequelize): Promise
 
   // Index projects
   const sql = `
-    select p.id, p.name, p.slug, p.created_at, p.updated_at, p.latitude_north, p.latitude_south, p.longitude_east, p.longitude_west, pp.summary, pp.readme, pm.species_count, pm.min_date, pm.max_date, pm.detection_minutes_count 
+    select
+      p.id, 
+      p.name, 
+      p.slug, 
+      p.created_at, 
+      p.updated_at, 
+      p.latitude_north, 
+      p.latitude_south, 
+      p.longitude_east, 
+      p.longitude_west, 
+      pp.summary, 
+      pp.readme, 
+      pm.species_count, 
+      pm.min_date, 
+      pm.max_date, 
+      pm.detection_minutes_count 
     from location_project p 
       left join location_project_profile pp on p.id = pp.location_project_id
-      left join location_project_metric pm on p.id = pm.location_project_id;
+      left join location_project_metric pm on p.id = pm.location_project_id
   `
   const projects = await sequelize.query(sql, { raw: true }).then(([rows, _]) => rows) as Array<{ id: string }>
   for (const project of projects) {
