aba30aecb4c626fd1487434fbc233fd18c8982b0
Antony Harfield <antonyharfield@hotmail.com>
2024-01-14 14:50:10 +0700

    Fix missing values for location_project_metric

--- apps/cli/src/db/migrations/231212-01-add-site-count-to-location-project-recording-metric.ts
+++ apps/cli/src/db/migrations/231212-01-add-site-count-to-location-project-recording-metric.ts
@@ -28,19 +28,20 @@ export const up: MigrationFn<QueryInterface> = async (params): Promise<void> => 
   await params.context.sequelize.query(`
     CREATE VIEW ${VIEW_NAME} AS
       SELECT
-        rm.location_project_id,
-        dm.species_count,
-        rm.site_count,
-        rm.recording_minutes_count,
-        dm.detection_minutes_count,
+        p.id location_project_id,
+        coalesce(dm.species_count, 0) species_count,
+        coalesce(rm.site_count, 0) site_count,
+        coalesce(rm.recording_minutes_count, 0) recording_minutes_count,
+        coalesce(dm.detection_minutes_count, 0) detection_minutes_count,
         least(dm.min_date, rm.min_date) min_date,
         greatest(dm.max_date, rm.max_date) max_date,
-        rm.min_date as recording_min_date,
-        rm.max_date as recording_max_date,
-        dm.min_date as detection_min_date,
-        dm.max_date as detection_max_date
-      FROM ${MATERIALIZED_VIEW_NAME} rm
-        INNER JOIN location_project_detection_metric dm ON rm.location_project_id = dm.location_project_id;
+        rm.min_date recording_min_date,
+        rm.max_date recording_max_date,
+        dm.min_date detection_min_date,
+        dm.max_date detection_max_date
+      FROM location_project p
+        LEFT JOIN ${MATERIALIZED_VIEW_NAME} rm ON p.id = rm.location_project_id
+        LEFT JOIN location_project_detection_metric dm ON p.id = dm.location_project_id;
   `)
   await grant(params.context.sequelize, VIEW_NAME, [GrantPermission.SELECT], DatabaseUser.API)
 }
