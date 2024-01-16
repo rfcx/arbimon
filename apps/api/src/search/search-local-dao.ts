2a23af0a74201538de3b77a48492f2f1decfcff3
Bhattarapong Somwong <numbbutt34685@gmail.com>
2024-01-16 16:33:03 +0700

    #1333 trending projects endpoint

--- apps/api/src/search/search-local-dao.ts
+++ apps/api/src/search/search-local-dao.ts
@@ -5,6 +5,7 @@ import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
 
 import { getSequelize } from '~/db'
 import { getAverageCoordinate } from './helpers'
+import { fileUrl } from '~/format-helpers/file-url'
 
 export const getProjectsByQuery = async (query: string, limit: number, offset: number): Promise<{ total: number, data: SearchResponse }> => {
   const sequelize = getSequelize()
@@ -49,6 +50,59 @@ export const getProjectsByQuery = async (query: string, limit: number, offset: n
         slug: project.slug,
         status: project.status,
         image: profile?.image ?? '',
+        objectives: profile?.objectives ?? '',
+        summary: profile?.summary ?? '',
+        speciesCount: metric?.speciesCount ?? 0,
+        recordingMinutesCount: metric?.recordingMinutesCount ?? 0,
+        countryCodes: country?.countryCodes ?? []
+      }
+    })
+  }
+}
+
+export const getTrendingProjects = async (limit: number, offset: number): Promise<{ total: number, data: SearchResponse }> => {
+  const sequelize = getSequelize()
+  const { LocationProject, LocationProjectMetric, LocationProjectProfile, LocationProjectCountry } = ModelRepository.getInstance(sequelize)
+
+  const results = await LocationProject.findAndCountAll({
+    where: {
+      status: ['listed', 'published']
+    },
+    include: [
+      {
+        model: LocationProjectMetric,
+        as: 'LocationProjectMetric'
+      },
+      {
+        model: LocationProjectProfile,
+        as: 'LocationProjectProfile'
+      },
+      {
+        model: LocationProjectCountry,
+        as: 'LocationProjectCountry'
+      }
+    ],
+    order: [
+      [LocationProjectMetric, 'recordingMinutesCount', 'DESC']
+    ]
+  })
+
+  return {
+    total: results.count,
+    data: results.rows.map(result => {
+      // @ts-expect-error this sequelize won't know of these existence
+      const { LocationProjectMetric: metric, LocationProjectProfile: profile, LocationProjectCountry: country, ...project } = result.toJSON()
+      return {
+        type: 'project',
+        avgLatitude: getAverageCoordinate(project.latitudeNorth, project.latitudeSouth),
+        avgLongitude: getAverageCoordinate(project.longitudeEast, project.longitudeWest),
+        id: project.id,
+        idCore: project.idCore,
+        idArbimon: project.idArbimon,
+        name: project.name,
+        slug: project.slug,
+        status: project.status,
+        image: fileUrl(project?.image) ?? '',
         objectives: profile?.objectives ?? '',
         summary: profile?.summary ?? '',
         speciesCount: metric?.speciesCount ?? 0,
