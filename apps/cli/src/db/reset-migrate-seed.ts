9ff7ab13d52d01b62f8b24bdb3c6640352fce082
Bhattarapong Somwong <numbbutt34685@gmail.com>
2024-01-18 18:23:41 +0700

    #1333 utilize opensearch index during the dev environment

--- apps/cli/src/db/reset-migrate-seed.ts
+++ apps/cli/src/db/reset-migrate-seed.ts
@@ -1,7 +1,8 @@
 import { execSeeders } from '@/db/actions/exec-seeders'
 import { refreshMviews } from '@/db/actions/refresh-mviews'
+import { getOpenSearchClient } from '@/search/opensearch'
 import { updateMasterData } from './_helpers/update-master-data'
-import { dropTables, execMigrations } from './actions'
+import { dropTables, execMigrations, indexOpensearch } from './actions'
 import { getSequelize } from './connections'
 import { defaultSeederPaths } from './seeders/default-seeders'
 
@@ -13,6 +14,7 @@ const seederPaths = process.argv
 const main = async (): Promise<void> => {
   try {
     // Setup
+    const opensearch = getOpenSearchClient()
     const sequelize1 = getSequelize(verbose)
     const sequelize2 = getSequelize(verbose) // Seeders uses a different Umzug (which seems to require a fresh Sequelize instance)
 
@@ -24,10 +26,12 @@ const main = async (): Promise<void> => {
       await execSeeders(sequelize2, seederPath, verbose)
     }
     await refreshMviews(sequelize2)
+    await indexOpensearch(opensearch, sequelize1)
 
     // Teardown
     await sequelize1.close()
     await sequelize2.close()
+    await opensearch.close()
     console.info('RMS: finished')
   } catch (err: any) {
     console.error(err)
