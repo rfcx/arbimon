47d759b433b64dfdf3898763d5c5fb69446c7a00
ratree <ratree.ochn@gmail.com>
2024-01-19 12:53:19 +0700

    #1438 Should show "Unfortunately the project owner..." when you are the guest/not a project member

--- apps/website/src/insights/overview/components/dashboard-project-summary/components/dashboard-project-stakeholders/dashboard-project-stakeholders-viewer.vue
+++ apps/website/src/insights/overview/components/dashboard-project-summary/components/dashboard-project-stakeholders/dashboard-project-stakeholders-viewer.vue
@@ -59,8 +59,10 @@
       class="mt-4"
     >
       <dashboard-project-stakeholders-empty
+        v-if="editable && !projectUserPermissionsStore.isGuest"
         @emit-add-stakeholders="$emit('emit-is-updating')"
       />
+      <ProjectSummaryEmptyForNonProjectMember v-else />
     </div>
     <StakeholdersTosBanner
       v-if="store.user === null"
@@ -74,6 +76,7 @@ import { type DashboardStakeholdersUser } from '@rfcx-bio/common/api-bio/dashboa
 import { type OrganizationTypes, ORGANIZATION_TYPE_NAME } from '@rfcx-bio/common/dao/types/organization'
 
 import { useProjectUserPermissionsStore, useStore } from '~/store'
+import ProjectSummaryEmptyForNonProjectMember from '../project-summary-empty-for-non-project-member.vue'
 import DashboardProjectStakeholdersEmpty from './dashboard-project-stakeholders-empty.vue'
 import StakeholderCard from './stakeholder-card.vue'
 import StakeholdersTosBanner from './stakeholders-tos-banner.vue'
