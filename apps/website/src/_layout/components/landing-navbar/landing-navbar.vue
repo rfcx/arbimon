9a8c9f0783f7e3cd9965bbb7f1e8fd3bdc45d086
ratree <ratree.ochn@gmail.com>
2024-01-22 16:10:26 +0700

    #1430 Adjust nav bar highlighted line should be on top of the main line

--- apps/website/src/_layout/components/landing-navbar/landing-navbar.vue
+++ apps/website/src/_layout/components/landing-navbar/landing-navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <nav class="bg-white border-frequency border-b-1 px-4 lg:px-6 py-2.5 dark:bg-echo">
+    <nav class="bg-white border-frequency border-b-1 px-4 lg:px-6 py-1.7 dark:bg-echo">
       <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
         <div class="flex gap-10 items-center">
           <!-- Logo -->
