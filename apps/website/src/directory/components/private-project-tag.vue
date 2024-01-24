59f1a1837eeb963adb9d89e3e3fb07cbd5e0b9b6
Somjintana Korbut <nuisomjin@gmail.com>
2024-01-24 20:55:03 +0700

    #1429 set max zoom & fix icon is too small

--- apps/website/src/directory/components/private-project-tag.vue
+++ apps/website/src/directory/components/private-project-tag.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-row gap-4 border-t-1 border-util-gray-03 p-4 bg-echo">
-    <icon-custom-ic-warning class="text-insight" />
+    <icon-custom-ic-warning class="text-insight flex-shrink-0" />
     <div class="flex flex-col gap-2">
       <span>Private project</span>
       <span class="text-xs">Project details are hidden to non-project members. For any inquiries about this project, contact us and we’ll connect you with the owners.</span>
