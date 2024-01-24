ba503f32daa3ccf4b35d2b43d5b9a0a24227262e
Somjintana Korbut <nuisomjin@gmail.com>
2024-01-24 22:29:38 +0700

    remove require auth from getUserRoleForProject
    ref: https://rfcx.slack.com/archives/C02G4AR2GEL/p1706105812094139

--- apps/api/src/projects/index.ts
+++ apps/api/src/projects/index.ts
@@ -89,7 +89,6 @@ export const routesProject: RouteRegistration[] = [
   {
     method: GET,
     url: projectRoleRoute,
-    preHandler: [requireAuthorized],
     handler: getProjectRoleHandler
   },
   {
