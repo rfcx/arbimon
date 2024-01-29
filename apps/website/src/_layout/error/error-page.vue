abae92b57b114132f49f77e1543dee5756ff4e67
Antony Harfield <antonyharfield@hotmail.com>
2024-01-29 23:39:20 +0700

    #1463 require verify email

--- apps/website/src/_layout/error/error-page.vue
+++ apps/website/src/_layout/error/error-page.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <navbar />
-    <div class="text-subtle">
-      Page not found
+    <div class="text-fog p-10 lg:p-20">
+      <span v-if="route.query.error">{{ route.query.error }}</span>
+      <span v-else>Page not found</span>
     </div>
   </div>
 </template>
+
 <script setup lang="ts">
+import { useRoute } from 'vue-router'
+
 import Navbar from '@/_layout/components/landing-navbar/landing-navbar.vue'
+
+const route = useRoute()
 </script>
