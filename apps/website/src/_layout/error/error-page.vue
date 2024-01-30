035d6538162810319a107906d73281831acf75bc
Somjintana Korbut <nuisomjin@gmail.com>
2024-01-30 12:51:26 +0700

    #1463 update error style and add additional text for email verification error

--- apps/website/src/_layout/error/error-page.vue
+++ apps/website/src/_layout/error/error-page.vue
@@ -1,8 +1,21 @@
 <template>
   <div>
     <navbar />
-    <div class="text-fog p-10 lg:p-20">
-      <span v-if="route.query.error">{{ route.query.error }}</span>
+    <div class="text-fog p-10 lg:p-20 mx-auto text-center">
+      <div v-if="route.query.error && route.query.error.includes('Email verification')">
+        <p class="text-lg text-xl text-gray-900 dark:text-insight font-header">
+          {{ route.query.error }}
+        </p>
+        <p>
+          You should have received a verification email in your inbox. If you do not receive one, please contact <a
+            class="text-frequency text-underline"
+            href="mailto:support@rfcx.org"
+          >support@rfcx.org</a>.
+        </p>
+      </div>
+      <p v-else-if="route.query.error">
+        {{ route.query.error }}
+      </p>
       <span v-else>Page not found</span>
     </div>
   </div>
