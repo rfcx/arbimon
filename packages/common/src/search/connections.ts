708adb5d4e7616969c40c1b3706d754d20e3f21c
Antony Harfield <antonyharfield@hotmail.com>
2024-02-02 22:24:00 +0700

    Permanent logging opensearch

--- packages/common/src/search/connections.ts
+++ packages/common/src/search/connections.ts
@@ -4,12 +4,11 @@ import { AwsSigv4Signer } from '@opensearch-project/opensearch/aws'
 export const openSearchClient = (host: string, config: { port?: number, ssl?: boolean, httpAuth?: { user: string, password: string }, aws?: { region: string, accessKeyId: string, secretAccessKey: string } }): Client => {
   const httpAuth = config.httpAuth ? `${config.httpAuth.user}:${config.httpAuth.password}@` : ''
   const node = `${config.ssl === true ? 'https' : 'http'}://${httpAuth}${host}${config.port !== undefined ? ':' + config.port.toString() : ''}`
-
-  console.info('aoss node', node)
+  console.info('Instantiating OpenSearch client for node:', node)
 
   if (config.aws) {
     const { region, ...credentials } = config.aws
-    console.info('aoss aws region', region)
+    console.info('Instantiating OpenSearch client with AWS credentials, region:', region)
     const signer = AwsSigv4Signer({
       region,
       service: 'aoss',
