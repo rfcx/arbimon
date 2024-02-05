1006bf27b0fe9ef45af0d1f545ee7c8eff23f3ec
ratree <ratree.ochn@gmail.com>
2024-02-05 11:58:49 +0700

    #1494 When minimize the browser, Login button disappears.

--- apps/website/src/_layout/components/landing-navbar/landing-navbar.vue
+++ apps/website/src/_layout/components/landing-navbar/landing-navbar.vue
@@ -30,7 +30,7 @@
           >
             <a
               :href="universalLoginUrl"
-              class="text-gray-800 font-display dark:text-insight px-4 lg:px-5 py-1.5 lg:py-2 mr-2 dark:hover:text-frequency hidden md:block"
+              class="text-gray-800 font-display dark:text-insight px-4 lg:px-5 py-1.5 lg:py-2 mr-2 dark:hover:text-frequency md:block"
             >
               Log in
             </a>
