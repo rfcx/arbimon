e62294a6936505c9b3a29793529dab79b164fc6f
Antony Harfield <antonyharfield@hotmail.com>
2024-02-12 09:41:44 +0000

    Hide OS logging from tests

--- packages/common/src/search/connections.ts
+++ packages/common/src/search/connections.ts
@@ -1,14 +1,18 @@
 import { Client } from '@opensearch-project/opensearch'
 import { AwsSigv4Signer } from '@opensearch-project/opensearch/aws'
 
-export const openSearchClient = (host: string, config: { port?: number, ssl?: boolean, httpAuth?: { user: string, password: string }, aws?: { region: string, accessKeyId: string, secretAccessKey: string } }): Client => {
+export const openSearchClient = (host: string, config: { port?: number, ssl?: boolean, httpAuth?: { user: string, password: string }, aws?: { region: string, accessKeyId: string, secretAccessKey: string } }, verbose: boolean = true): Client => {
   const httpAuth = config.httpAuth ? `${config.httpAuth.user}:${config.httpAuth.password}@` : ''
   const node = `${config.ssl === true ? 'https' : 'http'}://${httpAuth}${host}${config.port !== undefined && config.port > 0 ? ':' + config.port.toString() : ''}`
-  console.info('Instantiating OpenSearch client for node:', node)
+  if (verbose) {
+    console.info('Instantiating OpenSearch client for node:', node)
+  }
 
   if (config.aws) {
     const { region, ...credentials } = config.aws
-    console.info('Instantiating OpenSearch client with AWS credentials, region:', region)
+    if (verbose) {
+      console.info('Instantiating OpenSearch client with AWS credentials, region:', region)
+    }
     const signer = AwsSigv4Signer({
       region,
       service: 'aoss',
