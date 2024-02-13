90ecdc77a530cd78eb5c12b03695493144ea2add
Somjintana Korbut <nuisomjin@gmail.com>
2024-02-13 12:28:51 +0000

    #1520 Router - change the logic to check for user permission to a project

--- apps/website/src/_services/router/guard-store-project.ts
+++ apps/website/src/_services/router/guard-store-project.ts
@@ -2,41 +2,21 @@ import { type NavigationGuardWithThis } from 'vue-router'
 
 import { type ProjectRole, rolesGreaterOrEqualTo } from '@rfcx-bio/common/roles'
 
-import { useAuth0Client } from '~/auth-client'
 import { useProjectUserPermissionsStore, useStore } from '~/store'
 
 export const storeProjectGuard: NavigationGuardWithThis<undefined> = async (to, _from, next) => {
   const store = useStore()
   const userPermissionsStore = useProjectUserPermissionsStore()
-  const currentProjectSlug = store.selectedProject?.slug
   const newProjectSlug = to.params.projectSlug // TODO 44: Extract `projectSlug` as a const?
 
-  // TODO: get project from API by slug
+  await store.updateSelectedProjectSlug(newProjectSlug as string)
+  const project = store.selectedProject
 
-  if (newProjectSlug !== currentProjectSlug) {
-    const project = store.projects?.find(p => p.slug === newProjectSlug)
-
-    if (project) {
-      // Project found
-      store.updateSelectedProject(project)
-      await store.updateProjectFilters()
-      await userPermissionsStore.getRole(project.id ?? -1)
-    } else {
-      const auth0Client = await useAuth0Client()
-      if (await auth0Client.isAuthenticated()) {
-        // Definitely no access to project
-        store.updateSelectedProject(undefined)
-        await store.updateProjectFilters()
-        userPermissionsStore.role = undefined
-      } else {
-        // Might need to login
-        await auth0Client.loginWithRedirect({ appState: { target: to.fullPath } })
-      }
-    }
-  } else {
-    // just need to update the filters when both of it is the same already.
+  if (project) {
     await store.updateProjectFilters()
-    await userPermissionsStore.getRole(store.selectedProject?.id ?? -1)
+    await userPermissionsStore.getRole(project.id ?? -1)
+  } else {
+    await resetSelectedProject()
   }
 
   next()
@@ -64,6 +44,8 @@ const hasRoleGreaterThan = (minimumRole: ProjectRole): boolean => {
 
 const resetSelectedProject = async (): Promise<void> => {
   const store = useStore()
+  const userPermissionsStore = useProjectUserPermissionsStore()
   store.updateSelectedProject(undefined)
   await store.updateProjectFilters()
+  userPermissionsStore.role = undefined
 }
