6c1e2680c1c71dcfc22fe3cf54676095ff1ba293
Somjintana Korbut <nuisomjin@gmail.com>
2024-02-15 13:40:29 +0000

    #1541 My projects - update loading indicator

--- apps/website/src/projects/my-projects-page.vue
+++ apps/website/src/projects/my-projects-page.vue
@@ -25,27 +25,7 @@
             v-if="(isLoading && projects.length === 0) || showLoading"
             class="fixed inset-0 flex flex-col justify-center items-center"
           >
-            <div class="animate-spin h-10 w-10">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  class="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  stroke-width="4"
-                />
-                <path
-                  class="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                />
-              </svg>
-            </div>
+            <icon-custom-ic-loading class="w-10 h-10" />
           </div>
         </div>
         <div
