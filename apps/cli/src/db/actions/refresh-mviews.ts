e48a9cce51f061ca4933059db1698c05a35de266
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2024-02-19 14:57:55 +0400

    remove location_project_metric from materialized views updates

--- apps/cli/src/db/actions/refresh-mviews.ts
+++ apps/cli/src/db/actions/refresh-mviews.ts
@@ -6,8 +6,7 @@ export const refreshMviews = async (sequelize: Sequelize): Promise<void> => {
     'species_in_project',
     'location_project_detection_metric',
     'location_project_recording_metric',
-    'location_project_country',
-    'location_project_metric'
+    'location_project_country'
   ]
 
   // Get a list of all other views
