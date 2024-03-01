c173ea935d28057ea3c1dda034dc33b6c1cde813
RatreeOchn <51106210+RatreeOchn@users.noreply.github.com>
2024-03-01 14:40:49 +0700

    CNN job create (#1623)
    * #1504 Update text size
    
    * #1504 Update color of the step number and line
    
    * #1504 Update Time of day UI
    
    * #1504 date picker updated
    
    * #1570 Update UI selected Classifier
    
    * #1570 add new search for site dropdown with search component
    
    * #1570 add cursor pointer to tag
    
    * #1570 Dropdown list isn't so long
    
    * #1570 Color scheme & padding are the same as design
    
    * #1570 Type to search for site begin with keyword & select tag should say "All sites start with ##" not "##*"
    
    * #1570 Dropdown sometimes doesn't show up when click on input box
    
    * #1570 Click anywhere in the <form> should show cursor for the user to input in the input box
    
    * #1570  Emit selected options back to parent view
    
    * #1570 When selected another site should not show 'All sites in the project'
    
    * #1570 Change dates and highlight default color on calendar as new design
    
    * #1570 Change color selected date
    
    * #1570 Remove site-picker.vue once you're sure we covered all the feature in the new UI
    
    * #1570 Hide test classifier picker
    
    * #1570 Set default of Time of day
    
    * #1570 Check is already selected site
    
    * #1570 Remove test file of element-plus (el-select)
    
    * #1570 Fix selected filter sites, then remove all sites
    
    * #1570 Correct mistakes
    
    * #1570 add placeholder to suggest the format
    
    * #1570 Fix text sizes are still not the same as design
    
    * #1570 move time picker to its own component
    
    * #1570 Remove type
    
    * #1570 Close icon that looks more similar to the design
    
    * #1570 Update PR
    
    * #1570 Update error text
    
    * #1570 When classifiers = undefined and Loading
    
    * #1570 Remove selectedTime
    
    ---------
    
    Co-authored-by: Somjintana Korbut <nuisomjin@gmail.com>

---
+++ apps/website/src/_services/picker/site-input.vue
@@ -0,0 +1,202 @@
+<template>
+  <div class="relative my-6">
+    <form
+      ref="siteResultForm"
+      class="relative w-full py-1 px-2 flex flex-row items-center gap-1 flex-wrap border-1 border-frequency rounded-md focus:border-frequency focus:outline-none focus:ring-0"
+      @click="openSiteTrigger()"
+    >
+      <!-- selected sites so far -->
+      <ul
+        class="flex flex-row flex-wrap gap-2"
+      >
+        <li
+          v-for="site in selectedOptions"
+          :key="site.label"
+          class="cursor-pointer rounded-sm px-2 py-1 flex flex-row grow items-center text-base bg-util-gray-01 text-pitch"
+        >
+          {{ site === ALL_SITES_OPTIONS ? ALL_SITES_OPTIONS.label : site.label }}
+          <button
+            type="button"
+            class="ml-2 p-0.5 hover:bg-util-gray-02 rounded-sm"
+            @click="unselectSite(site)"
+          >
+            <icon-custom-ic-close-black class="w-4 h-4 cursor-pointer" />
+          </button>
+        </li>
+      </ul>
+      <input
+        ref="siteResultInput"
+        v-model="inputFilter"
+        type="text"
+        class="border-transparent px-2 py-1 bg-transparent ring-0 focus:ring-0 focus:border-transparent"
+        @keydown.delete="onKeydownDeleteSiteInput"
+        @focus="hasFocusInput = true"
+        @blur="hasFocusInput = false"
+      >
+      <span class="absolute right-4 cursor-pointer pointer-events-none">
+        <icon-fa-chevron-down
+          class="w-3 h-3 fa-chevron-down text-util-gray-03"
+          :class="hasFocusInput ? 'transform rotate-180' : ''"
+        />
+      </span>
+    </form>
+    <div
+      ref="siteResultDropdownContainer"
+      class="absolute w-5/6 left-4 z-40 bg-white rounded-md shadow dark:bg-moss border-util-gray-03 border-1 hidden"
+    >
+      <ul
+        v-for="group in groupOptions"
+        :key="group.label"
+        class="overflow-y-auto max-h-80 border-cloud bg-moss rounded-md"
+      >
+        <li class="rounded-md px-4 py-2 text-base text-subtle font-medium">
+          {{ group.label }}
+        </li>
+        <li
+          v-for="item in group.options"
+          :key="item.value"
+          class="cursor-pointer rounded-md px-4 py-2 hover:bg-util-gray-03 text-base"
+          @click="selectSite(item)"
+        >
+          {{ item.label }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+/** Note: this UI needed to implement further.
+ *
+ * Reference:
+ * All requirements from old component can be fonud at `site-picker.vue` file
+ * Implementation detail for search + dropdown at `filter-site.vue` file
+ *
+ *  */
+
+import { Dropdown } from 'flowbite'
+import { type Ref, computed, nextTick, onMounted, ref, watch } from 'vue'
+
+import type { Site } from '@rfcx-bio/common/dao/types'
+
+const ALL_SITES_OPTIONS = {
+  value: '',
+  label: 'All sites in the project'
+}
+const props = defineProps<{
+  initialSites?: Site[]
+}>()
+const emit = defineEmits<{(e: 'emitSelectSites', value: string | null): void}>()
+const inputFilter = ref('')
+const hasFocusInput = ref(false)
+const hasDeletedSite = ref(false)
+const siteSearchDropdown = ref() as Ref<Dropdown>
+const siteResultForm = ref<HTMLDivElement | null>(null)
+const siteResultInput = ref<HTMLDivElement | null>(null)
+const siteResultDropdownContainer = ref<HTMLDivElement | null>(null)
+
+export interface SiteInputOptions {
+  value: string
+  label: string
+}
+
+onMounted(() => {
+  siteSearchDropdown.value = new Dropdown(siteResultDropdownContainer.value, siteResultForm.value, { placement: 'bottom', triggerType: 'none', offsetDistance: 1 })
+})
+
+const openSiteTrigger = async () => {
+  if (hasDeletedSite.value) {
+    siteSearchDropdown.value.hide()
+    hasDeletedSite.value = false
+    return
+  }
+
+  await nextTick()
+  siteResultInput.value?.focus()
+  siteSearchDropdown.value.show()
+}
+// TODO: change type to be Site[]
+const projectSiteOptions = computed(() => props.initialSites ?? [])
+
+// options
+const siteOptions = computed(() => {
+  const allSitesOptions = projectSiteOptions.value
+    .map(s => ({
+      value: s.name,
+      label: s.name
+    }))
+  if (!inputFilter.value) { return allSitesOptions }
+  // with filter
+  const filtered = allSitesOptions?.filter(s => {
+    return s.value.toLocaleLowerCase().startsWith(inputFilter.value.toLocaleLowerCase())
+  })
+  const special = {
+    value: `${inputFilter.value}*`,
+    label: `All sites start with ${inputFilter.value}`,
+    disabled: isAllSiteOptionSelected.value
+  }
+  if ((filtered?.length ?? 0) > 0) { filtered?.unshift(special) }
+  return filtered
+})
+
+const groupOptions = computed(() => {
+  const optionAll = {
+    value: 'Select all sites',
+    label: 'Select all sites',
+    options: [ALL_SITES_OPTIONS]
+  }
+  const optionFilter = {
+    value: 'Filter sites',
+    label: 'Filter sites',
+    options: siteOptions.value
+  }
+  return ((siteOptions.value?.length ?? 0) > 0) ? [optionAll, optionFilter] : []
+})
+
+// selected values
+const selectedOptions = ref([ALL_SITES_OPTIONS])
+const isAllSiteOptionSelected = computed(() => selectedOptions.value.includes(ALL_SITES_OPTIONS))
+
+const selectSite = (site: SiteInputOptions) => {
+  // if selected all sites, then remove all other sites
+  if (site.value === ALL_SITES_OPTIONS.value) {
+    selectedOptions.value = [ALL_SITES_OPTIONS]
+    return
+  } else {
+    // if selected filter sites, then remove all sites
+    selectedOptions.value = selectedOptions.value.filter(s => s.value !== ALL_SITES_OPTIONS.value)
+  }
+
+  // if already selected, then do nothing
+  if (selectedOptions.value.some(s => s.value === site.value)) { return }
+
+  selectedOptions.value = [...selectedOptions.value, site]
+}
+
+const unselectSite = (site: SiteInputOptions) => {
+  // force to default value = all sites
+  if (selectedOptions.value.length === 1) {
+    selectedOptions.value = [ALL_SITES_OPTIONS]
+  } else {
+    selectedOptions.value = selectedOptions.value.filter((s) => s.value !== site.value)
+  }
+  hasDeletedSite.value = true
+}
+
+const onKeydownDeleteSiteInput = () => {
+  if (inputFilter.value.length === 0 && selectedOptions.value.length > 0) {
+    unselectSite(selectedOptions.value[selectedOptions.value.length - 1])
+  }
+}
+
+const selectedQuerySites = computed(() => {
+  const listSite = selectedOptions.value.map(a => a.value)
+  return isAllSiteOptionSelected.value ? null : listSite.join(',')
+})
+
+watch(selectedQuerySites, () => {
+  emit('emitSelectSites', selectedQuerySites.value)
+})
+
+</script>
