7e41b091a716a2cfb8bb6798a3b78667c765bb05
Antony Harfield <antonyharfield@hotmail.com>
2024-03-04 16:58:23 +0000

    Fix incremental sync on staging/testing

--- apps/cli/src/ingest/sync/incremental.ts
+++
@@ -1,7 +0,0 @@
-import { getSequelize } from '@/db/connections'
-import { getArbimonSequelize } from '../_connections/arbimon'
-import { syncAllIncrementally } from './sync-all'
-
-const arbimonSequelize = getArbimonSequelize()
-const biodiversitySequelize = getSequelize()
-await syncAllIncrementally(arbimonSequelize, biodiversitySequelize)
