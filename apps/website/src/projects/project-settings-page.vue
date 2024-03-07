68c7efce7fc953ce433eb72e3cde4869179a6141
Somjintana Korbut <nuisomjin@gmail.com>
2024-03-07 15:03:44 +0000

    #1657 date picker - add new date picker into project form

--- apps/website/src/projects/project-settings-page.vue
+++ apps/website/src/projects/project-settings-page.vue
@@ -12,8 +12,8 @@
           </h5>
           <project-form
             :existing-name="selectedProject?.name"
-            :date-start="settings?.dateStart"
-            :date-end="settings?.dateEnd"
+            :date-start="settings?.dateStart ? new Date(settings?.dateStart) : undefined"
+            :date-end="settings?.dateEnd ? new Date(settings?.dateEnd) : undefined"
             :is-disabled="!isUserHasFullAccess"
             @emit-update-value="onEmitDefaultValue"
           />
