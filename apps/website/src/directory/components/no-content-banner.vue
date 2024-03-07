043f773ac22ba5c2958590d1d293b54005f09ef0
turk-t <turk@rfcx.org>
2024-03-07 18:44:36 +0700

    #1665 Change the message to No content

--- apps/website/src/directory/components/no-content-banner.vue
+++ apps/website/src/directory/components/no-content-banner.vue
@@ -2,6 +2,6 @@
   <p
     class="text-sm p-4 m-4 rounded-lg border-1 border-util-gray-03 bg-util-gray-04"
   >
-    Unfortunately, the project owner has not added content for this section.
+    No content
   </p>
 </template>
