78b63274482e722b5c0755232665dda02648f026
turk-t <turk@rfcx.org>
2024-03-07 18:44:36 +0700

    #1665 Change the message to No content

--- apps/website/src/insights/overview/components/dashboard-project-summary/components/project-summary-empty-for-non-project-member.vue
+++ apps/website/src/insights/overview/components/dashboard-project-summary/components/project-summary-empty-for-non-project-member.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col space-y-3 bg-util-gray-04 p-4 border border-util-gray-02 rounded-md px-4 py-4 mx-auto">
     <span class="text-insight text-base font-sans">
-      Unfortunately, the project owner has not added content for this section. <br> Contact project owner for more information about the project.
+      No content
     </span>
     <a
       v-show="false"
