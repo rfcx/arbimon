62bf1ddd67f55bc7e9ef826efe36926240356e7b
LucyDimitrova <lucy@riseart.com>
2024-03-08 12:39:50 +0200

    fix and refactor tests

--- apps/api/src/dashboard/dashboard-stakeholders-handler.int.test.ts
+++ apps/api/src/dashboard/dashboard-stakeholders-handler.int.test.ts
@@ -1,5 +1,5 @@
 import { Op } from 'sequelize'
-import { afterAll, beforeAll, expect, test } from 'vitest'
+import { afterAll, beforeAll, describe, expect, test, vi } from 'vitest'
 
 import { dashboardStakeholdersRoute } from '@rfcx-bio/common/api-bio/dashboard/dashboard-stakeholders'
 import { modelRepositoryWithElevatedPermissions } from '@rfcx-bio/testing/dao'
@@ -11,87 +11,125 @@ import { routesDashboard } from './index'
 
 const { LocationProject, LocationProjectProfile, LocationProjectUserRole, UserProfile } = modelRepositoryWithElevatedPermissions
 
-const currentUserId = 9001
-const fakeToken = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6ImE0NTBhMzFkMjEwYTY5N2ZmMDI3NjU0YmZhMWZmMTFlIn0.eyJhdXRoMF91c2VyX2lkIjoidGVzdCJ9.571qutLhQm4Wc6hdhsVCxKm_rh4szTg9Wygz2JVxIItf3M_hNI5ats5W-HoJJjmFsBJ_oOwI1uU_6e4bfaFcrg'
+vi.mock('~/api-core/api-core')
 
+const currentUserId = 9001
 const user1 = {
-    email: 'user1@test.org',
-    firstName: 'User1',
-    lastName: 'User1-LastName'
+    email: 'john@test.org',
+    firstName: 'John',
+    lastName: 'Smith'
 }
 
 const user2 = {
-    email: 'user2@test.org',
-    firstName: 'User2',
-    lastName: 'User2-LastName'
+    email: 'adam@test.org',
+    firstName: 'Adam',
+    lastName: 'Murphy'
 }
-beforeAll(async () => {
-    const project = await createProject({ name: 'Grey-blue humpback whales' }, currentUserId, '')
-    const locationProjectId = project[1]
-    const users = await UserProfile.bulkCreate([user1, user2])
-    const userIds = users.map(user => user.id)
-    const roles = userIds.map(userId => ({ locationProjectId, userId, roleId: 1, ranking: -1 }))
-    await LocationProjectUserRole.bulkCreate(roles)
-})
 
-afterAll(async () => {
-    const locationProjects = await LocationProject.findAll({ where: { slug: { [Op.like]: 'grey-blue-humpback%' } } }).then(projects => projects.map(project => project.id))
-    await LocationProjectProfile.destroy({ where: { locationProjectId: { [Op.in]: locationProjects } } })
-    await LocationProjectUserRole.destroy({ where: { locationProjectId: { [Op.in]: locationProjects } } })
-    await LocationProject.destroy({ where: { id: { [Op.in]: locationProjects } }, force: true })
-    const users = await UserProfile.findAll({ where: { email: { [Op.in]: ['user1@test.org', 'user2@test.org'] } } }).then(users => users.map(user => user.id))
-    await UserProfile.destroy({ where: { id: { [Op.in]: users } }, force: true })
-})
-
-test(`GET ${dashboardStakeholdersRoute} exists`, async () => {
-    // Arrange
-    const app = await makeApp(routesDashboard, { userId: currentUserId })
-
-    // Act
-    const routes = [...app.routes.keys()]
-
-    // Assert
-    expect(routes).toContain(dashboardStakeholdersRoute)
-}, 10000)
-
-test(`GET ${dashboardStakeholdersRoute} returns successfully`, async () => {
-    // Arrange
-    const app = await makeApp(routesDashboard, { userId: currentUserId })
-    const project = await LocationProject.findOne({ where: { slug: { [Op.like]: 'grey-blue-humpback%' } } })
-
-    // Act
-    const response = await app.inject({
-        method: GET,
-        url: dashboardStakeholdersRoute.replace(':projectId', project?.id.toString() ?? ''),
-        headers: { Authorization: fakeToken }
+describe(`GET ${dashboardStakeholdersRoute}`, async () => {
+    beforeAll(async () => {
+        const project = await createProject({ name: 'Grey-blue humpback whales' }, currentUserId, '')
+        const locationProjectId = project[1]
+        const users = await UserProfile.bulkCreate([user1, user2])
+        const userIds = users.map(user => user.id)
+        const roles = userIds.map(userId => ({ locationProjectId, userId, roleId: 2, ranking: -1 }))
+        await LocationProjectUserRole.bulkCreate(roles)
     })
 
-    // Assert
-    expect(response.statusCode).toBe(200)
-
-    const result = JSON.parse(response.body)
-    expect(result).toBeDefined()
-    expect(result).toBeTypeOf('object')
-    expect(result.users).toBeDefined()
-    expect(result.organizations).toBeDefined()
-})
-
-test(`GET ${dashboardStakeholdersRoute} returns all users for owner`, async () => {
-    // Arrange
-    const app = await makeApp(routesDashboard, { userId: currentUserId })
-    const project = await LocationProject.findOne({ where: { slug: { [Op.like]: 'grey-blue-humpback%' } } })
-    // Act
-    const response = await app.inject({
-        method: GET,
-        url: dashboardStakeholdersRoute.replace(':projectId', project?.id.toString() ?? ''),
-        headers: { Authorization: fakeToken }
+    afterAll(async () => {
+        const locationProjects = await LocationProject.findAll({ where: { slug: { [Op.like]: 'grey-blue-humpback%' } } }).then(projects => projects.map(project => project.id))
+        await LocationProjectProfile.destroy({ where: { locationProjectId: { [Op.in]: locationProjects } } })
+        await LocationProjectUserRole.destroy({ where: { locationProjectId: { [Op.in]: locationProjects } } })
+        await LocationProject.destroy({ where: { id: { [Op.in]: locationProjects } }, force: true })
+        const users = await UserProfile.findAll({ where: { email: { [Op.in]: ['john@test.org', 'adam@test.org'] } } }).then(users => users.map(user => user.id))
+        await UserProfile.destroy({ where: { id: { [Op.in]: users } }, force: true })
     })
 
-    // Assert
-    expect(response.statusCode).toBe(200)
+    test('exists', async () => {
+        // Arrange
+        const app = await makeApp(routesDashboard, { userId: currentUserId })
 
-    const result = JSON.parse(response.body)
-    expect(result).toBeDefined()
-    expect(result.users).toBeDefined()
-    expect(result.users).toHaveLength(3)
+        // Act
+        const routes = [...app.routes.keys()]
+
+        // Assert
+        expect(routes).toContain(dashboardStakeholdersRoute)
+    })
+
+    test('returns successfully', async () => {
+        // Arrange
+        const app = await makeApp(routesDashboard, { userId: currentUserId })
+        const project = await LocationProject.findOne({ where: { slug: { [Op.like]: 'grey-blue-humpback%' } } })
+
+        // Act
+        const response = await app.inject({
+            method: GET,
+            url: dashboardStakeholdersRoute.replace(':projectId', project?.id.toString() ?? '')
+        })
+
+        // Assert
+        expect(response.statusCode).toBe(200)
+        const result = JSON.parse(response.body)
+        expect(result).toBeDefined()
+        expect(result).toBeTypeOf('object')
+        expect(result.users).toBeDefined()
+        expect(result.organizations).toBeDefined()
+    })
+
+    test('returns all users for owner', async () => {
+        // Arrange
+        const app = await makeApp(routesDashboard, { projectRole: 'owner' })
+        const project = await LocationProject.findOne({ where: { slug: { [Op.like]: 'grey-blue-humpback%' } } })
+
+        // Act
+        const response = await app.inject({
+            method: GET,
+            url: dashboardStakeholdersRoute.replace(':projectId', project?.id.toString() ?? '')
+        })
+
+        // Assert
+        expect(response.statusCode).toBe(200)
+        const result = JSON.parse(response.body)
+        expect(result).toBeDefined()
+        expect(result.users).toBeDefined()
+        expect(result.users).toHaveLength(3)
+    })
+
+    test('returns all users for admin', async () => {
+        // Arrange
+        const app = await makeApp(routesDashboard, { projectRole: 'admin' })
+        const project = await LocationProject.findOne({ where: { slug: { [Op.like]: 'grey-blue-humpback%' } } })
+
+        // Act
+        const response = await app.inject({
+            method: GET,
+            url: dashboardStakeholdersRoute.replace(':projectId', project?.id.toString() ?? '')
+        })
+
+        // Assert
+        expect(response.statusCode).toBe(200)
+        const result = JSON.parse(response.body)
+        expect(result).toBeDefined()
+        expect(result.users).toBeDefined()
+        expect(result.users).toHaveLength(3)
+    })
+
+    test('returns only listed users for other roles', async () => {
+        // Arrange
+        const app = await makeApp(routesDashboard, { projectRole: 'user' })
+        const project = await LocationProject.findOne({ where: { slug: { [Op.like]: 'grey-blue-humpback%' } } })
+
+        // Act
+        const response = await app.inject({
+            method: GET,
+            url: dashboardStakeholdersRoute.replace(':projectId', project?.id.toString() ?? '')
+        })
+
+        // Assert
+        expect(response.statusCode).toBe(200)
+        const result = JSON.parse(response.body)
+        expect(result).toBeDefined()
+        expect(result.users).toBeDefined()
+        expect(result.users).toHaveLength(1)
+    })
 })
