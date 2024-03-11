ed036190237f062c5b240acdc1e66d2cc1201061
LucyDimitrova <lucy@riseart.com>
2024-03-11 17:32:04 +0200

    refactor /species route

--- packages/common/src/api-bio/species/project-species-all.ts
+++ packages/common/src/api-bio/species/project-species-all.ts
@@ -2,33 +2,39 @@ import { type AxiosInstance } from 'axios'
 
 import { apiGetOrUndefined } from '@rfcx-bio/utils/api'
 
-import { type SpeciesInProjectTypes } from '../../dao/types/species-in-project'
+import { SpeciesInProject } from '@/dao/types/species-in-project'
 import { type ProjectRouteParamsSerialized, PROJECT_SPECIFIC_ROUTE_PREFIX } from '../_helpers'
 import { type DashboardSpecies } from '../dashboard/common'
 
 // Request types
 export type ProjectSpeciesAllParams = ProjectRouteParamsSerialized
+export type ProjectSpeciesParams = ProjectRouteParamsSerialized
+
+type ProjectSpeciesFieldSet = 'light' | 'dashboard'
 
 export interface ProjectSpeciesQueryParams {
   limit?: number
   offset?: number
+  fields?: ProjectSpeciesFieldSet
 }
 
 // Response types
-export interface ProjectSpeciesLightResponse {
-  species: Array<SpeciesInProjectTypes['light']>
-}
+// TODO: delete
 export interface ProjectSpeciesAllResponse {
   species: DashboardSpecies[]
 }
 
+export interface ProjectSpeciesResponse {
+  species: Array<Partial<SpeciesInProject>>
+}
+
 // Route
-export const projectSpeciesLightRoute = `${PROJECT_SPECIFIC_ROUTE_PREFIX}/species`
-export const projectSpeciesAllRoute = `${PROJECT_SPECIFIC_ROUTE_PREFIX}/species-all`
+export const projectSpeciesRoute = `${PROJECT_SPECIFIC_ROUTE_PREFIX}/species`
 
 // Service
-export const apiBioGetProjectSpeciesLight = async (apiClient: AxiosInstance, projectId: number): Promise<ProjectSpeciesLightResponse | undefined> =>
-  await apiGetOrUndefined(apiClient, `/projects/${projectId}/species`)
-
+// TODO: delete
 export const apiBioGetProjectSpeciesAll = async (apiClient: AxiosInstance, projectId: number, params: ProjectSpeciesQueryParams = {}): Promise<ProjectSpeciesAllResponse | undefined> =>
   await apiGetOrUndefined(apiClient, `/projects/${projectId}/species-all`, { params })
+
+export const apiBioGetProjectSpecies = async (apiClient: AxiosInstance, projectId: number, params: ProjectSpeciesQueryParams = {}): Promise<ProjectSpeciesResponse | undefined> =>
+    await apiGetOrUndefined(apiClient, `/projects/${projectId}/species`, { params })
