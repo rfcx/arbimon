4321dd5b67aa44a0ae9bcdbf7340011b1104f1cd
turk-t <turk@rfcx.org>
2024-03-12 18:59:26 +0700

    #1632 add input limit length

--- apps/website/src/projects/components/form/project-slug.vue
+++ apps/website/src/projects/components/form/project-slug.vue
@@ -22,6 +22,7 @@
         :disabled="isDisabled"
         class="w-full border border-cloud rounded-md dark:(bg-pitch text-fog placeholder:text-insight) focus:(border-frequency ring-frequency) disabled:opacity-70 disabled:cursor-not-allowed"
         required
+        maxlength="50"
       >
     </div>
   </div>
