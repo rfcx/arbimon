a9b2b85da17d72a3a77a17e3d8b6ad2aa1350178
LucyDimitrova <lucy@riseart.com>
2024-03-12 18:19:44 +0200

    fix type on project species response

--- packages/common/src/api-bio/species/project-species-all.ts
+++ packages/common/src/api-bio/species/project-species-all.ts
@@ -2,7 +2,7 @@ import { type AxiosInstance } from 'axios'
 
 import { apiGetOrUndefined } from '@rfcx-bio/utils/api'
 
-import { type SpeciesInProject } from '@/dao/types/species-in-project'
+import { type SpeciesInProjectTypes } from '../../dao/types/species-in-project'
 import { type ProjectRouteParamsSerialized, PROJECT_SPECIFIC_ROUTE_PREFIX } from '../_helpers'
 import { type DashboardSpecies } from '../dashboard/common'
 
@@ -25,7 +25,7 @@ export interface ProjectSpeciesAllResponse {
 }
 
 export interface ProjectSpeciesResponse {
-  species: Array<Partial<SpeciesInProject>>
+  species: Array<SpeciesInProjectTypes['light'] | SpeciesInProjectTypes['dashboard']>
 }
 
 // Route
