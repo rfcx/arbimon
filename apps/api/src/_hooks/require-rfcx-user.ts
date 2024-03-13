e658c38fa8b39938307e2396501eaefc9e6572f0
Bhattarapong Somewong <numbbutt34685@gmail.com>
2024-03-13 19:00:22 +0700

    #1715 fix type guard

--- apps/api/src/_hooks/require-rfcx-user.ts
+++ apps/api/src/_hooks/require-rfcx-user.ts
@@ -2,7 +2,7 @@ import { type Middleware } from '~/api-helpers/types'
 import { BioForbiddenError } from '~/errors'
 
 export const requireRfcxEmail: Middleware<void> = async (req): Promise<void> => {
-  if (!req.userToken.email.includes('rfcx.org')) {
+  if (req.userToken === null || req.userToken === undefined || !req.userToken.email.includes('rfcx.org')) {
     throw BioForbiddenError()
   }
 }
