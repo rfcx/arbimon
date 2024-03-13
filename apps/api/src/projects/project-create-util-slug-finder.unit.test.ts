79f964544e25c8b373e15314c3091d82eabe5f2a
Antony Harfield <antonyharfield@hotmail.com>
2024-03-13 11:48:14 +0000

    #1627 Limit chars on slug generator

--- apps/api/src/projects/project-create-util-slug-finder.unit.test.ts
+++ apps/api/src/projects/project-create-util-slug-finder.unit.test.ts
@@ -18,6 +18,18 @@ test('trims whitespace', async () => {
   expect(await uniqueSlug('  My Project  ', async () => true)).toBe('my-project')
 })
 
+test('trims words when string over 50 characters', async () => {
+  expect(await uniqueSlug('Fire effects on bird communities from seasonally flooded forests along Amazon', async () => true)).toBe('fire-effects-on-bird-communities-from-seasonally')
+})
+
+test('trims characters when single word over 50 characters', async () => {
+  expect(await uniqueSlug('FireeffectsonbirdcommunitiesfromseasonallyfloodedforestsalongAmazon', async () => true)).toBe('fireeffectsonbirdcommunitiesfromseasonallyfloodedf')
+})
+
 test('adds suffix when not initially unique', async () => {
   expect(await uniqueSlug('test', async (slug) => slug !== 'test')).toMatch(/test-[a-z0-9]{4}/)
+})
+
+test('adds suffix when not initially unique and keeping within 50 char limit', async () => {
+  expect(await uniqueSlug('fireeffectsonbirdcommunitiesfromseasonallyflooded', async (slug) => slug !== 'fireeffectsonbirdcommunitiesfromseasonallyflooded')).toMatch(/fireeffectsonbirdcommunitiesfromseasonallyflo-[a-z0-9]{4}/)
 })
