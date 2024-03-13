026255a7bb499affad9fc2c642268ba8d610a621
Bhattarapong Somewong <numbbutt34685@gmail.com>
2024-03-14 00:19:53 +0700

    #1715 revert unintended frontend changes

--- apps/website/src/detect/_composables/use-classifier-jobs.ts
+++ apps/website/src/detect/_composables/use-classifier-jobs.ts
@@ -1,14 +1,13 @@
 import { type UseQueryReturnType, useQuery } from '@tanstack/vue-query'
 import { type AxiosInstance } from 'axios'
-import { type Ref } from 'vue'
 
-import { type GetClassifierJobsResponse, apiBioGetClassifierJobs } from '@rfcx-bio/common/api-bio/cnn/classifier-jobs'
+import { type ClassifierJobAll, type ClassifierJobAllParams, apiCoreGetClassifierJobAll } from '@rfcx-bio/common/api-core/classifier-job/classifier-job-all'
 
 export const FETCH_CLASSIFIER_JOBS_KEY = 'fetch-classifier-jobs'
 
-export const useClassifierJobs = (apiClient: AxiosInstance, projectId: string, createdBy: Ref<'me' | 'all'>): UseQueryReturnType<GetClassifierJobsResponse, unknown > => {
+export const useClassifierJobs = (apiClient: AxiosInstance, params?: ClassifierJobAllParams): UseQueryReturnType<ClassifierJobAll | undefined, unknown > => {
   return useQuery({
-    queryKey: [FETCH_CLASSIFIER_JOBS_KEY, projectId, createdBy],
-    queryFn: async () => await apiBioGetClassifierJobs(apiClient, projectId, createdBy.value)
+    queryKey: [FETCH_CLASSIFIER_JOBS_KEY, params],
+    queryFn: async () => await apiCoreGetClassifierJobAll(apiClient, params)
   })
 }
