86ef5911cffbb56088466271a78587852bcf58f1
turk-t <turk@rfcx.org>
2024-03-13 17:10:06 +0700

    #1583 create general component for dialog

--- apps/website/src/_components/dialog_member.vue
+++ apps/website/src/_components/dialog_member.vue
@@ -1,10 +1,16 @@
 <template>
   <!-- Dialog Success -->
-  <Transition>
+  <Transition
+    v-if="showAlert"
+    enter-active-class="transition opacity 400ms ease-out"
+    leave-active-class="transition opacity 400ms ease-out"
+    enter-from-class="opacity-0 transition opacity 700ms ease-out"
+    leave-to-class="opacity-0 transition opacity 700ms ease-out"
+  >
     <div
-      v-if="showAlertSuccess"
+      v-if="success"
       id="targetElement"
-      class="flex absolute bottom-4 justify-around items-center p-4 mb-4 rounded-lg text-white bg-white shadow dark:bg-moss border-1 border-util-gray-02 rounded text-sm w-fit "
+      class="flex absolute bottom-4 justify-around items-center p-4 mb-4 rounded-lg text-white bg-white shadow dark:bg-moss border-1 border-util-gray-02 rounded text-sm w-fit transition-opacity duration-700 ease-out"
       role="alert"
     >
       <icon-custom-ic-success />
@@ -13,14 +19,15 @@
       >
         <span
           class="font-medium p-2"
-        > Success </span><span
-          data-v-b90261d1=""
+        > {{ title }} </span>
+        <span
           class="p-1"
-        > New Project member added successfully</span>
+        > {{ message }}
+        </span>
       </div><button
-        id="triggerElement"
+        id="closeButton"
         type="button"
-        class="ms-auto -mx-1.5 -my-1.5 text-white rounded-lg inline-flex items-center justify-center h-8 w-8 closealertbutton"
+        class="ms-auto -mx-1.5 -my-1.5 text-white rounded-lg inline-flex items-center justify-center h-8 w-8 closeAlertButton"
         data-dismiss-target="#targetElement"
         aria-label="Close"
         closable
@@ -28,30 +35,17 @@
       >
         <span
           class="sr-only"
-        >Close</span><svg
-          id="triggerElement"
-          class="w-3 h-3"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 14 14"
-        ><path
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-        /></svg>
+        >Close</span>
+        <icon-custom-ic-close-black
+          class="fill-white"
+        />
       </button>
     </div>
-  </Transition>
 
-  <!-- Dialog Duplicate -->
-  <Transition>
     <div
-      v-if="showAlertDuplicate"
+      v-else
       id="targetElement"
-      class="flex absolute bottom-4 justify-around items-center p-4 mb-4 rounded-lg bg-rose-200 border-1 border-l-4 border-rose-600 rounded text-moss text-sm w-fit"
+      class="flex absolute bottom-4 justify-around items-center p-4 mb-4 rounded-lg bg-rose-200 border-1 border-l-4 border-ibis rounded text-moss text-sm w-fit"
       role="alert"
     >
       <icon-custom-alert-triangle
@@ -62,80 +56,24 @@
       >
         <span
           class="font-medium p-2"
-        > Duplicate  </span><span
+        > {{ title }} </span>
+        <span
           class="p-1"
-        > The use is already a project member</span>
+        > {{ message }}
+        </span>
       </div><button
-        id="triggerElement"
+        id="closeButton"
         type="button"
-        class="ms-auto -mx-1.5 -my-1.5 text-white rounded-lg inline-flex items-center justify-center h-8 w-8"
-        data-dismiss-target="targetElement"
+        class="ms-auto -mx-1.5 -my-1.5 text-white rounded-lg inline-flex items-center justify-center h-8 w-8 closeAlertButton"
+        data-dismiss-target="#targetElement"
         aria-label="Close"
+        closable
         @click="closeAlert"
       >
         <span
           class="sr-only"
-        >Close</span><svg
-          id="triggerElement"
-          class="w-3 h-3"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 14 14"
-        ><path
-          stroke="black"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-        /></svg>
-      </button>
-    </div>
-  </Transition>
-
-  <!-- Dialog Error -->
-  <Transition>
-    <div
-      v-if="showAlertError"
-      id="targetElement"
-      class="flex absolute bottom-4 justify-around items-center p-4 mb-4 rounded-lg bg-rose-200 border-1 border-l-4 border-rose-600 rounded text-moss text-sm w-fit"
-      role="alert"
-    >
-      <icon-custom-alert-triangle
-        class="h-6 w-6 cursor-pointer item-center"
-      />
-      <div
-        class="ms-3 text-sm px-2"
-      >
-        <span
-          class="font-medium p-2"
-        > Error  </span><span
-          class="p-1"
-        > failed to add project member</span>
-      </div><button
-        id="triggerElement"
-        type="button"
-        class="ms-auto -mx-1.5 -my-1.5 text-white rounded-lg inline-flex items-center justify-center h-8 w-8"
-        data-dismiss-target="targetElement"
-        aria-label="Close"
-        @click="closeAlert"
-      >
-        <span
-          class="sr-only"
-        >Close</span><svg
-          id="triggerElement"
-          class="w-3 h-3"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 14 14"
-        ><path
-          stroke="black"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-        /></svg>
+        >Close</span>
+        <icon-custom-ic-close-black />
       </button>
     </div>
   </Transition>
@@ -146,22 +84,17 @@
 import type { DismissInterface, DismissOptions, InstanceOptions } from 'flowbite'
 import { Dismiss } from 'flowbite'
 
-defineProps<{ showAlertSuccess?: boolean, showAlertDuplicate?: boolean, showAlertError?: boolean}>()
+defineProps<{showAlert: boolean, success: boolean, title: string, message: string}>()
 
 const closeAlert = (): void => {
   const $targetEl: HTMLElement | null = document.getElementById('targetElement')
 
-  const $triggerEl: HTMLElement | null = document.getElementById('triggerElement')
+  const $triggerEl: HTMLElement | null = document.getElementById('closeButton')
 
   const options: DismissOptions = {
     transition: 'transition-opacity',
     duration: 700,
-    timing: 'ease-out',
-
-    onHide: (context, targetEl) => {
-      console.info('element has been dismissed')
-      console.info(targetEl)
-    }
+    timing: 'ease-out'
   }
 
   const instanceOptions: InstanceOptions = {
@@ -174,16 +107,3 @@ const closeAlert = (): void => {
 }
 
 </script>
-
-<style lane="scss" scoped>
-.v-enter-active,
-.v.leave-active {
-  transition: opacity 400ms ease-out;
-}
-
-.v-enter-form,
-.v-leave-to {
-  opacity: 0;
-  transition: opacity 700ms ease-out;
-}
-</style>
