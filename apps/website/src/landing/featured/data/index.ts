527a2207bc512ba407530c9b738ea8d3492aae8a
Somjintana Korbut <nuisomjin@gmail.com>
2024-03-13 13:46:00 +0000

    #1738 remove feature work content

--- apps/website/src/landing/featured/data/index.ts
+++ apps/website/src/landing/featured/data/index.ts
@@ -1,16 +1,3 @@
-import { bioBaseline } from './biodiversity-baselines'
-import { carbonTransparency } from './carbon-transparency'
-import { climateChangeImpact } from './climate-change-impact'
-import { endangeredSpecies } from './endangered-species'
-import { restoration } from './restoration'
-import { supplyChain } from './supply-chain'
 import { type ProjectDetail } from './types'
 
-export const projects: ProjectDetail[] = [
-  bioBaseline,
-  endangeredSpecies,
-  restoration,
-  climateChangeImpact,
-  supplyChain,
-  carbonTransparency
-]
+export const projects: ProjectDetail[] = []
