ab9db6df871252c5619786932ab34d0d614fdb13
LucyDimitrova <lucy@riseart.com>
2024-03-13 19:52:58 +0200

    lint fix

--- apps/website/src/insights/overview/components/dashboard-highlighted-species/composables/use-species-in-project.ts
+++ apps/website/src/insights/overview/components/dashboard-highlighted-species/composables/use-species-in-project.ts
@@ -2,7 +2,7 @@ import { type UseQueryReturnType, useQuery } from '@tanstack/vue-query'
 import { type AxiosInstance } from 'axios'
 import { type ComputedRef } from 'vue'
 
-import { type ProjectSpeciesResponse, type ProjectSpeciesQueryParams, apiBioGetProjectSpecies } from '@rfcx-bio/common/api-bio/species/project-species-all'
+import { type ProjectSpeciesQueryParams, type ProjectSpeciesResponse, apiBioGetProjectSpecies } from '@rfcx-bio/common/api-bio/species/project-species-all'
 
 export const useSpeciesInProject = (apiClient: AxiosInstance, locationProjectId: ComputedRef<number | undefined>, params: ProjectSpeciesQueryParams = {}): UseQueryReturnType<ProjectSpeciesResponse, unknown> => {
   return useQuery({
