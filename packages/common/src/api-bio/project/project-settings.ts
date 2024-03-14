d75fd4c765026dd074ffb38e1f39ff6776cffb10
Antony Harfield <antonyharfield@hotmail.com>
2024-03-14 08:50:26 +0000

    #1671 API stops client updating hidden status when published

--- packages/common/src/api-bio/project/project-settings.ts
+++ packages/common/src/api-bio/project/project-settings.ts
@@ -60,6 +60,7 @@ export type ProjectInfoResponse = Pick<Project, 'name' | 'slug'>
 export type ProjectSettingsResponse = Omit<ProjectInfoResponse, 'readme' | 'keyResults' | 'metrics'>
 
 export const ERROR_MESSAGE_UPDATE_PROJECT_SLUG_NOT_UNIQUE = 'Slug is not unique'
+export const ERROR_MESSAGE_UPDATE_PROJECT_SET_HIDDEN_WHEN_PUBLISHED = 'Cannot set hidden when published'
 
 export const ATTRIBUTES_PROJECT_INFO_RESPONSE = attributes<ProjectInfoResponse>()({
 })
