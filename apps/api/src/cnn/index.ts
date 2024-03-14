6f822d13f6bf9bc10bb24e7d2bbadbb8888b9fde
Bhattarapong Somewong <numbbutt34685@gmail.com>
2024-03-14 14:16:06 +0700

    #1715 get classifier jobs route as constant

--- apps/api/src/cnn/index.ts
+++ apps/api/src/cnn/index.ts
@@ -1,3 +1,5 @@
+import { getClassifierJobsRoute } from '@rfcx-bio/common/api-bio/cnn/classifier-jobs'
+
 import { requireProjectPermission } from '@/_hooks/require-permission'
 import { requireRfcxEmail } from '@/_hooks/require-rfcx-user'
 import { type RouteRegistration, GET } from '~/api-helpers/types'
@@ -6,7 +8,7 @@ import { getClassifierJobsHandler } from './get-classifier-jobs-handler'
 export const routesCnn: RouteRegistration[] = [
   {
     method: GET,
-    url: '/jobs',
+    url: getClassifierJobsRoute,
     preHandler: [requireProjectPermission('read-cnn'), requireRfcxEmail],
     handler: getClassifierJobsHandler
   }
