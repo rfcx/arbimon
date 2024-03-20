2064735265e31a47d497ac167a3fb71220d2dc8a
turk-t <turk@rfcx.org>
2024-03-20 17:37:32 +0700

    #1568 Add skeleton UI

--- apps/website/src/_layout/components/landing-navbar/auth-navbar-item/auth-navbar-item.vue
+++ apps/website/src/_layout/components/landing-navbar/auth-navbar-item/auth-navbar-item.vue
@@ -16,58 +16,79 @@
       Sign up
     </button>
   </div>
-  <div
-    v-else
-    class="flex items-center space-x-4 font-medium"
-  >
-    <router-link
-      :to="{ name: ROUTE_NAMES.myProjects }"
-      exact-active-class="tab-active"
-      class="block tab relative"
+  <div v-else>
+    <div
+      v-if="isLoading"
+      class="flex items-center space-x-4 font-medium animate-pulse"
     >
-      My Projects
-    </router-link>
-    <div class="flex">
-      <button
-        id="user-menu-button"
-        type="button"
-        class="flex mr-3 text-sm rounded-full md:mr-0 focus:ring-4 dark:focus:ring-frequency/10"
-        aria-expanded="false"
-        aria-haspopup="true"
-        data-dropdown-trigger="hover"
-        data-dropdown-toggle="user-dropdown"
-        data-dropdown-placement="bottom"
-      >
-        <span class="sr-only">Open user menu</span>
-        <img
-          class="h-8 w-8 rounded-full"
-          :src="userImage"
-        >
-      </button>
       <div
-        id="user-dropdown"
-        class="z-50 my-4 hidden text-base w-50 list-none bg-moss rounded-lg text-insight divide-y divide-gray-100 shadow"
+        role="status"
+        class="flex items-center space-x-4 font-medium animate-pulse"
       >
-        <ul
-          class="py-2 font-medium"
-          aria-labelledby="user-menu-button"
-          aria-orientation="vertical"
-          role="menu"
-          tabindex="-1"
+        <div class="flex max-w-sm items-center space-x-4 font-medium">
+          <div class="tab bg-steel-gray relative block h-5 w-24 rounded-full dark:bg-steel-gray-500" />
+          <div class="flex">
+            <div class="dark:focus:ring-frequency/10 mr-3 flex rounded-full text-sm focus:ring-4 md:mr-0">
+              <div class="bg-steel-gray h-8 w-8 rounded-full dark:bg-steel-gray-500" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      v-else
+      class="flex items-center space-x-4 font-medium"
+    >
+      <router-link
+        :to="{ name: ROUTE_NAMES.myProjects }"
+        exact-active-class="tab-active"
+        class="block tab relative"
+      >
+        My Projects
+      </router-link>
+      <div class="flex">
+        <button
+          id="user-menu-button"
+          type="button"
+          class="flex mr-3 text-sm rounded-full md:mr-0 focus:ring-4 dark:focus:ring-frequency/10"
+          aria-expanded="false"
+          aria-haspopup="true"
+          data-dropdown-trigger="hover"
+          data-dropdown-toggle="user-dropdown"
+          data-dropdown-placement="bottom"
         >
-          <li
-            class="px-4 py-2 rounded-lg hover:(bg-moss cursor-pointer text-frequency)"
-            @click="openProfile"
+          <span class="sr-only">Open user menu</span>
+          <img
+            class="h-8 w-8 rounded-full"
+            :src="userImage"
           >
-            Account settings
-          </li>
-          <li
-            class="px-4 py-2 rounded-lg hover:(bg-moss cursor-pointer text-frequency)"
-            @click="logout"
+        </button>
+        <div
+          id="user-dropdown"
+          class="z-50 my-4 hidden text-base w-50 list-none bg-moss rounded-lg text-insight divide-y divide-gray-100 shadow"
+        >
+          <ul
+            class="py-2 font-medium"
+            aria-labelledby="user-menu-button"
+            aria-orientation="vertical"
+            role="menu"
+            tabindex="-1"
           >
-            Log out
-          </li>
-        </ul>
+            <li
+              class="px-4 py-2 rounded-lg hover:(bg-moss cursor-pointer text-frequency)"
+              @click="openProfile"
+            >
+              Account settings
+            </li>
+            <li
+              class="px-4 py-2 rounded-lg hover:(bg-moss cursor-pointer text-frequency)"
+              @click="logout"
+            >
+              Log out
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -76,7 +97,7 @@
 <script setup lang="ts">
 import { type Auth0Client } from '@auth0/auth0-spa-js'
 import { initDropdowns } from 'flowbite'
-import { computed, inject, onMounted } from 'vue'
+import { computed, inject, onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 import { authClientKey, storeKey } from '@/globals'
@@ -111,6 +132,12 @@ const logout = async (): Promise<void> => {
 const openProfile = async (): Promise<void> => {
   void router.replace({ name: ROUTE_NAMES.accountSettings })
 }
+
+const isLoading = ref(true)
+
+setTimeout(() => {
+    isLoading.value = false
+  }, 1000)
 
 onMounted(() => {
   initDropdowns()
