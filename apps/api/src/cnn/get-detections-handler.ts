08137027a0403c544714a73f7d7bd5dd364d6b24
Bhattarapong Somewong <numbbutt34685@gmail.com>
2024-03-25 22:59:53 +0700

    #1790 shorthand return without variable creation

--- apps/api/src/cnn/get-detections-handler.ts
+++ apps/api/src/cnn/get-detections-handler.ts
@@ -4,6 +4,5 @@ import { type Handler } from '~/api-helpers/types'
 import { getDetections } from './get-detections-bll'
 
 export const getDetectionsHandler: Handler<GetDetectionsResponse, unknown, GetDetectionsQueryParams> = async (req) => {
-  const detections = await getDetections(req.headers.authorization ?? '', req.query)
-  return detections
+  return await getDetections(req.headers.authorization ?? '', req.query)
 }
