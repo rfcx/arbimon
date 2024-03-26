86815d214ce628fd64c6da6618f39c8fbf48a1ba
ratree <ratree.ochn@gmail.com>
2024-03-26 19:42:37 +0700

    #1505 implement this cnn-job-species-detected.ts in a composition API way

--- apps/website/src/detect/cnn-job-detail/components/cnn-job-species-detected.vue
+++ apps/website/src/detect/cnn-job-detail/components/cnn-job-species-detected.vue
@@ -59,7 +59,7 @@
             <tr class="border-b-1 border-util-gray-01">
               <td class="py-2 pl-4 sticky left-0 z-10">
                 <router-link
-                  :to="{ name: ROUTE_NAMES.activityPatterns, params: { speciesSlug: getSpeciesSlug(row.title) }, query: $route.query }"
+                  :to="{ name: ROUTE_NAMES.cnnJobList }"
                   class="text-subtle hover:(underline text-white) flex"
                 >
                   <img
@@ -147,4 +147,128 @@
     </div>
   </div>
 </template>
-<script lang="ts" src="./cnn-job-species-detected.ts"></script>
+<script setup lang="ts">
+import { computed, ref, watch } from 'vue'
+
+import { ROUTE_NAMES } from '~/router'
+
+type SortableColumn = 'scientificName' | 'unvalidated' | 'present' | 'notPresent' | 'unknown'
+type SortDirection = 1 | -1
+
+interface Header {
+  title: string
+  key: SortableColumn
+}
+
+export interface ClassificationsSummaryDataset {
+  value: string
+  title: string
+  image: string | null
+  unvalidated: number
+  rejected: number
+  uncertain: number
+  confirmed: number
+}
+
+const SORT_ASC: SortDirection = 1
+const SORT_DESC: SortDirection = -1
+
+const SORTABLE_COLUMNS: Record<SortableColumn, { defaultDirection: SortDirection, sortFunction: (e1: ClassificationsSummaryDataset, e2: ClassificationsSummaryDataset) => number }> = {
+  scientificName: {
+    defaultDirection: SORT_ASC,
+    sortFunction: (e1, e2) => e1.title.localeCompare(e2.title)
+  },
+  unvalidated: {
+    defaultDirection: SORT_ASC,
+    sortFunction: (e1, e2) => e1.unvalidated - e2.unvalidated
+  },
+  present: {
+    defaultDirection: SORT_DESC,
+    sortFunction: (e1, e2) => e1.confirmed - e2.confirmed
+  },
+  notPresent: {
+    defaultDirection: SORT_DESC,
+    sortFunction: (e1, e2) => e1.rejected - e2.rejected
+  },
+  unknown: {
+    defaultDirection: SORT_DESC,
+    sortFunction: (e1, e2) => e1.uncertain - e2.uncertain
+  }
+}
+
+const props = withDefaults(defineProps<{ datasets: ClassificationsSummaryDataset[], loading: boolean }>(), {
+loading: false
+})
+
+const sortColumn = ref<SortableColumn>('scientificName')
+const sortDirection = ref<SortDirection>(SORTABLE_COLUMNS.scientificName.defaultDirection)
+
+const pageIndex = ref(1) // 1-based for humans
+
+const tableHeader: Header[] =
+  [
+    { title: 'Class', key: 'scientificName' },
+    { title: 'Unvalidated', key: 'unvalidated' },
+    { title: 'Present', key: 'present' },
+    { title: 'Not Present', key: 'notPresent' },
+    { title: 'Unknown', key: 'unknown' }
+  ]
+
+const hasTableData = ref(props.datasets !== undefined)
+const pageSize = ref(25)
+const maxPage = ref(0)
+
+const sortedTableData = computed((): ClassificationsSummaryDataset[] => {
+  // Sort by user-chosen sort, then our default sort
+  const list = props.datasets
+  return list.sort((a, b) =>
+    SORTABLE_COLUMNS[sortColumn.value].sortFunction(a, b) * sortDirection.value ||
+    SORTABLE_COLUMNS.scientificName.sortFunction(a, b) * SORTABLE_COLUMNS.scientificName.defaultDirection
+  )
+})
+
+const pageData = computed(() : ClassificationsSummaryDataset[] => {
+  if (props.datasets === undefined) return []
+  const start = (pageIndex.value - 1) * pageSize.value
+  console.info(start)
+  console.info(pageData)
+  return sortedTableData.value.slice(start, start + pageSize.value)
+})
+
+watch(() => props.datasets, () => {
+  pageSize.value = props.datasets === undefined ? 0 : 25
+  maxPage.value = Math.ceil(props.datasets.length / pageSize.value)
+
+  if (pageIndex.value > maxPage.value) pageIndex.value = 1
+  hasTableData.value = props.datasets !== undefined
+})
+
+const previousPage = () => setPage(pageIndex.value - 1)
+
+const nextPage = () => setPage(pageIndex.value + 1)
+
+const setPage = (page: number) => {
+  // Wrap-around
+  let newPage = page
+  if (page < 1) newPage = maxPage.value
+  if (page > maxPage.value) newPage = 1
+
+  pageIndex.value = newPage
+}
+
+const sort = (column?: SortableColumn) => {
+  if (!column) return
+
+  if (sortColumn.value === column) {
+    // Change direction
+    sortDirection.value = sortDirection.value === SORT_ASC
+      ? SORT_DESC
+      : SORT_ASC
+  } else {
+    // Change column
+    sortColumn.value = column
+    sortDirection.value = SORTABLE_COLUMNS[column].defaultDirection
+  }
+}
+
+</script>
