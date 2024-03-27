0c65c0dc7c89f37d19accaee535f2a1dd7c3514d
Bhattarapong Somewong <numbbutt34685@gmail.com>
2024-03-27 20:50:09 +0700

    #1574 new endpoint to get dates group by the day

---
+++ apps/api/src/cnn/get-recorded-minutes-per-day-dao.ts
@@ -0,0 +1,28 @@
+import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
+
+import { getSequelize } from '~/db'
+
+export const getRecordedMinutesPerDay = async (projectId: string): Promise<Record<string, number>> => {
+  const sequelize = getSequelize()
+  const { RecordingBySiteHour } = ModelRepository.getInstance(sequelize)
+
+  const recordings = await RecordingBySiteHour.findAll({
+    attributes: [
+      [sequelize.fn('date', sequelize.col('time_precision_hour_local')), 'date'],
+      [sequelize.fn('coalesce', sequelize.fn('sum', sequelize.col('total_duration_in_minutes')), 0), 'recordingMinutes']
+    ],
+    where: {
+      locationProjectId: Number(projectId)
+    },
+    order: [
+      [sequelize.fn('date', sequelize.col('time_precision_hour_local')), 'asc']
+    ],
+    group: [
+      sequelize.fn('date', sequelize.col('time_precision_hour_local'))
+    ]
+  })
+
+  return Object.fromEntries(recordings.map(r => {
+    return [r.get('date'), r.get('recordingMinutes')]
+  }))
+}
