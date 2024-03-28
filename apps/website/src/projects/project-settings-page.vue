173d7a8c466faa2ef7cba69090945454ed46eefd
ratree <ratree.ochn@gmail.com>
2024-03-28 10:39:37 +0700

    #1796 used component show status text on project setting

--- apps/website/src/projects/project-settings-page.vue
+++ apps/website/src/projects/project-settings-page.vue
@@ -94,29 +94,13 @@
             Saving...
           </span>
         </div>
-        <span
-          v-if="lastUpdated && !hasFailed && !isSaving"
-          class="p-4 text-sm inline-flex"
-          role="alert"
-        >
-          <svg
-            class="w-4 h-4 ml-1"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M 20.292969 5.2929688 L 9 16.585938 L 4.7070312 12.292969 L 3.2929688 13.707031 L 9 19.414062 L 21.707031 6.7070312 L 20.292969 5.2929688 z" />
-          </svg>
-          <span class="font-medium ml-2 mt-0.15rem">{{ lastUpdatedText }}</span>
-        </span>
-        <span
-          v-if="hasFailed"
-          class="p-4 text-sm text-red-800 dark:text-flamingo"
-          role="alert"
-        >
-          <span class="font-medium">{{ errorMessage }}</span>
-        </span>
+        <textSaveChangeStatue
+          v-if="(lastUpdated || hasFailed) && !isSaving"
+          class="flex p-4"
+          :has-failed="hasFailed"
+          :last-updated-message="lastUpdatedText"
+          :error-message="errorMessage"
+        />
       </div>
     </div>
   </section>
@@ -130,6 +114,7 @@ import { type ProjectProfileUpdateBody, ERROR_MESSAGE_UPDATE_PROJECT_SLUG_NOT_UN
 import { dayjs } from '@rfcx-bio/utils/dayjs-initialized'
 import { isValidSlug } from '@rfcx-bio/utils/string/slug'
 
+import textSaveChangeStatue from '@/_components/text_save_change_statue.vue'
 import GuestBanner from '@/_layout/components//guest-banner/guest-banner.vue'
 import { urlWrapper } from '@/_services/images/url-wrapper'
 import { apiClientKey } from '@/globals'
