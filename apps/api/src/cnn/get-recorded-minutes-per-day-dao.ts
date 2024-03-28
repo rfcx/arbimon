a57e00ff7b3d2b168b2020a5e345574c524cb0e4
Bhattarapong Somewong <numbbutt34685@gmail.com>
2024-03-28 14:18:51 +0700

    #1574 new test harness for removing the recordings

--- apps/api/src/cnn/get-recorded-minutes-per-day-dao.ts
+++ apps/api/src/cnn/get-recorded-minutes-per-day-dao.ts
@@ -2,17 +2,17 @@ import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
 
 import { getSequelize } from '~/db'
 
-export const getRecordedMinutesPerDay = async (projectId: string): Promise<Record<string, number>> => {
+export const getRecordedMinutesPerDay = async (projectId: number): Promise<Record<string, number>> => {
   const sequelize = getSequelize()
   const { RecordingBySiteHour } = ModelRepository.getInstance(sequelize)
 
   const recordings = await RecordingBySiteHour.findAll({
     attributes: [
       [sequelize.fn('date', sequelize.col('time_precision_hour_local')), 'date'],
-      [sequelize.fn('coalesce', sequelize.fn('sum', sequelize.col('total_duration_in_minutes')), 0), 'recordingMinutes']
+      [sequelize.fn('coalesce', sequelize.fn('sum', sequelize.col('total_duration_in_minutes')), 0), 'recordedMinutes']
     ],
     where: {
-      locationProjectId: Number(projectId)
+      locationProjectId: projectId
     },
     order: [
       [sequelize.fn('date', sequelize.col('time_precision_hour_local')), 'asc']
@@ -23,6 +23,6 @@ export const getRecordedMinutesPerDay = async (projectId: string): Promise<Recor
   })
 
   return Object.fromEntries(recordings.map(r => {
-    return [r.get('date'), r.get('recordingMinutes')]
+    return [r.get('date'), r.get('recordedMinutes')]
   }))
 }
