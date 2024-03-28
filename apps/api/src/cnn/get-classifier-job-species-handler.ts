033e44a46a3bcb5146eb6ec9900757bee29de5e4
Bhattarapong Somewong <numbbutt34685@gmail.com>
2024-03-28 18:50:58 +0700

    #1616 fix axios cannot catch the value from the header

--- apps/api/src/cnn/get-classifier-job-species-handler.ts
+++ apps/api/src/cnn/get-classifier-job-species-handler.ts
@@ -6,6 +6,7 @@ import { getClassifierJobSpecies } from './get-classifier-job-species-bll'
 export const getClassifierJobSpeciesHandler: Handler<GetClassifierJobSpeciesResponse, GetClassifierJobSpeciesParams, GetClassifierJobSpeciesQueryParams> = async (req, res) => {
   const species = await getClassifierJobSpecies(req.headers.authorization ?? '', req.params.jobId, req.query)
 
+  void res.header('access-control-expose-headers', xTotalSpeciesCountHeaderName)
   void res.header(xTotalSpeciesCountHeaderName, species.total)
   return species.data
 }
