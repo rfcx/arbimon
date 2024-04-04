278b0e91b68e0e2427609f61d3f3cb20716c07bb
Antony Harfield <antonyharfield@hotmail.com>
2024-04-04 22:43:42 +0100

    #1490 Make lat/lng nullable

--- apps/cli/src/ingest/parsers/parse-site-arbimon-to-bio.unit.test.ts
+++ apps/cli/src/ingest/parsers/parse-site-arbimon-to-bio.unit.test.ts
@@ -30,25 +30,18 @@ describe('ingest > parsers > parseSiteArbimonToBio', () => {
     const { idCore, ...missingIdCore } = VALID_SITE
     const { projectIdArbimon, ...missingProjectId } = VALID_SITE
     const { name, ...missingName } = VALID_SITE
-    const { latitude, ...missingLatiude } = VALID_SITE
-    const { longitude, ...missingLongitude } = VALID_SITE
-    const { altitude, ...missingAltitude } = VALID_SITE
-    const { hidden, ...missingHidden } = VALID_SITE
 
     // Act
-    const actualMissing = [
-      parseSiteArbimon(missingIdArbimon),
-      parseSiteArbimon(missingIdCore),
-      parseSiteArbimon(missingProjectId),
-      parseSiteArbimon(missingName),
-      parseSiteArbimon(missingLatiude),
-      parseSiteArbimon(missingLongitude),
-      parseSiteArbimon(missingAltitude),
-      parseSiteArbimon(missingHidden)
-    ]
+    const actualMissingIdArbimon = parseSiteArbimon(missingIdArbimon)
+    const actualMissingIdCore = parseSiteArbimon(missingIdCore)
+    const actualMissingProjectId = parseSiteArbimon(missingProjectId)
+    const actualMissingName = parseSiteArbimon(missingName)
 
     // Assert
-    actualMissing.forEach(actual => { expect(actual.success).toBe(false) })
+    expect(actualMissingIdArbimon.success).toBe(false)
+    expect(actualMissingIdCore.success).toBe(false)
+    expect(actualMissingProjectId.success).toBe(false)
+    expect(actualMissingName.success).toBe(false)
   })
 
   test('fails if non-nullish props are nullish', async () => {
@@ -57,21 +50,12 @@ describe('ingest > parsers > parseSiteArbimonToBio', () => {
     const nullIdCore = { ...VALID_SITE, idCore: null }
     const nullProjectId = { ...VALID_SITE, projectIdArbimon: null }
     const nullName = { ...VALID_SITE, name: null }
-    const nullLatitude = { ...VALID_SITE, latitude: null }
-    const nullLongitude = { ...VALID_SITE, longitude: null }
-    const nullAltitude = { ...VALID_SITE, altitude: null }
-    const nullCountryCode = { ...VALID_SITE, countryCode: null }
-    const nullHidden = { ...VALID_SITE, hidden: null }
 
     const undefinedIdArbimon = { ...VALID_SITE, idArbimon: undefined }
     const undefinedIdCore = { ...VALID_SITE, idCore: undefined }
     const undefinedProjectId = { ...VALID_SITE, projectIdArbimon: undefined }
     const undefinedName = { ...VALID_SITE, name: undefined }
-    const undefinedLatitude = { ...VALID_SITE, latitude: undefined }
-    const undefinedLongitude = { ...VALID_SITE, longitude: undefined }
-    const undefinedAltitude = { ...VALID_SITE, altitude: undefined }
     const undefinedCountryCode = { ...VALID_SITE, countryCode: undefined }
-    const undefinedHidden = { ...VALID_SITE, hidden: undefined }
 
     // Act
     const actualMissing = [
@@ -79,28 +63,15 @@ describe('ingest > parsers > parseSiteArbimonToBio', () => {
       parseSiteArbimon(nullIdCore),
       parseSiteArbimon(nullProjectId),
       parseSiteArbimon(nullName),
-      parseSiteArbimon(nullLatitude),
-      parseSiteArbimon(nullLongitude),
-      parseSiteArbimon(nullAltitude),
-      parseSiteArbimon(nullHidden),
       parseSiteArbimon(undefinedIdArbimon),
       parseSiteArbimon(undefinedIdCore),
       parseSiteArbimon(undefinedProjectId),
       parseSiteArbimon(undefinedName),
-      parseSiteArbimon(undefinedLatitude),
-      parseSiteArbimon(undefinedLongitude),
-      parseSiteArbimon(undefinedAltitude),
-      parseSiteArbimon(undefinedCountryCode),
-      parseSiteArbimon(undefinedHidden)
-    ]
-
-    const actualNullishProps = [
-      parseSiteArbimon(nullCountryCode)
+      parseSiteArbimon(undefinedCountryCode)
     ]
 
     // Assert
     actualMissing.forEach(actual => { expect(actual.success).toBe(false) })
-    actualNullishProps.forEach(actual => { expect(actual.success).toBe(true) })
   })
 
   test('fails if props are in wrong type (idCoreNumber, idArbimonString)', async () => {
