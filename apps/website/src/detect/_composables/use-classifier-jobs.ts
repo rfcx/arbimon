284f03ae681d87485db77bc7d0aa7726092f1789
Somjintana Korbut <nuisomjin@gmail.com>
2024-04-09 22:31:24 +0100

    #1769 CNN Job list - update UI to call API from wrapper endpoint

--- apps/website/src/detect/_composables/use-classifier-jobs.ts
+++ apps/website/src/detect/_composables/use-classifier-jobs.ts
@@ -1,13 +1,13 @@
 import { type UseQueryReturnType, useQuery } from '@tanstack/vue-query'
 import { type AxiosInstance } from 'axios'
 
-import { type ClassifierJobAll, type ClassifierJobAllParams, apiCoreGetClassifierJobAll } from '@rfcx-bio/common/api-core/classifier-job/classifier-job-all'
+import { type GetClassifierJobsResponse, apiBioGetClassifierJobs } from '@rfcx-bio/common/api-bio/cnn/classifier-jobs'
 
 export const FETCH_CLASSIFIER_JOBS_KEY = 'fetch-classifier-jobs'
 
-export const useClassifierJobs = (apiClient: AxiosInstance, params?: ClassifierJobAllParams): UseQueryReturnType<ClassifierJobAll | undefined, unknown > => {
+export const useClassifierJobs = (apiClient: AxiosInstance, params: { projectId: string, createdBy: 'me' | 'all' }): UseQueryReturnType<GetClassifierJobsResponse | undefined, unknown> => {
   return useQuery({
     queryKey: [FETCH_CLASSIFIER_JOBS_KEY, params],
-    queryFn: async () => await apiCoreGetClassifierJobAll(apiClient, params)
+    queryFn: async () => await apiBioGetClassifierJobs(apiClient, params.projectId, params.createdBy)
   })
 }
