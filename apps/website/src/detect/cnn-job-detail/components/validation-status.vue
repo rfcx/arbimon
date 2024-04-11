29fe102b8053ce519ace5d1b0238c491f13be007
turk-t <turk@rfcx.org>
2024-04-11 13:51:43 +0700

    #1835 update text when user select dropdown on CNN Validation

--- apps/website/src/detect/cnn-job-detail/components/validation-status.vue
+++ apps/website/src/detect/cnn-job-detail/components/validation-status.vue
@@ -2,19 +2,19 @@
   <div>
     <icon-custom-fi-present
       v-if="props.value === 'present'"
-      class="h-4 w-4"
+      class="h-5 w-5"
     />
     <icon-custom-fi-not-present
       v-if="props.value === 'notPresent'"
-      class="h-4 w-4"
+      class="h-5 w-5"
     />
     <icon-custom-fi-unknown
       v-if="props.value === 'unknown'"
-      class="h-4 w-4"
+      class="h-5 w-5"
     />
     <icon-custom-fi-unvalidated
       v-if="(props.value === 'unvalidated' && props.hideUnvalidated === false)"
-      class="h-4 w-4"
+      class="h-5 w-5"
     />
   </div>
 </template>
