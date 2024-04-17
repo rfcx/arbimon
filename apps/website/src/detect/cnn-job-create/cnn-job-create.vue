b32e7fe51acaa973642d8b7d69f4a1fc6e0f65fb
turk-t <turk@rfcx.org>
2024-04-17 11:12:01 +0700

    #1835 fix side space on CNN page

--- apps/website/src/detect/cnn-job-create/cnn-job-create.vue
+++ apps/website/src/detect/cnn-job-create/cnn-job-create.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="pt-22 pl-115px pr-40 4xl:(pr-16)">
+  <section class="pt-20 pl-6 pr-10 md:(pl-10) xl:(pl-24)">
     <div class="text-frequency">
       <page-title page-title="Create New CNN Job" />
     </div>
