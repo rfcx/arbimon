085e8cb8191d96489bd33659229e4c96f55934d9
turk-t <turk@rfcx.org>
2024-04-24 14:14:55 +0700

    #1851  remove apply filter, update dropdown on CNN Validation

--- apps/website/src/detect/cnn-job-detail/components/detection-validator.vue
+++ apps/website/src/detect/cnn-job-detail/components/detection-validator.vue
@@ -32,7 +32,7 @@
             v-for="option in props.filterOptions"
             :key="option.value"
             class="bg-moss hover:text-util-gray-01"
-            @click="selectedFilter = option.value"
+            @click="selectedFilter = option.value; validateDetections()"
           >
             <div
               class="border-1 rounded-full cursor-pointer bg-moss"
@@ -47,17 +47,16 @@
             </div>
           </li>
         </ul>
-        <button
-          class="btn btn-primary py-2 h-10 whitespace-nowrap"
-          @click="validateDetections()"
-        >
-          Apply
-        </button>
       </div>
     </div>
 
     <div class="text-base flex gap-x-5 items-center">
-      {{ props.detectionCount }} selected
+      <span v-if="props.detectionCount === 1">
+        {{ props.detectionCount }} recording selected
+      </span>
+      <span v-else>
+        {{ props.detectionCount }} recordings selected
+      </span>
       <button
         class="btn border-1 border-util-gray-03 text-spoonbill bg-spoonbill bg-opacity-10 rounded-lg py-1 px-2 flex items-center gap-x-3"
         @click="close()"
