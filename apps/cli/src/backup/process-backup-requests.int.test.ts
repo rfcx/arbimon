8e87cd85c7de7d1bd82812ea7500692721820381
LucyDimitrova <lucy@riseart.com>
2024-04-26 19:39:30 +0300

    revert test

--- apps/cli/src/backup/process-backup-requests.int.test.ts
+++ apps/cli/src/backup/process-backup-requests.int.test.ts
@@ -1,6 +1,6 @@
-import { describe, expect, test } from 'vitest'
+import { describe, test } from 'vitest'
 
-import {backupProjects} from '@/backup/projects'
+import { backupProjects } from '@/backup/projects'
 import { getSequelize } from '@/db/connections'
 import { getStorageClient } from '~/storage'
 
@@ -9,12 +9,15 @@ const storage = getStorageClient()
 
 describe('Projects backup', async () => {
     test('generates and zips files', async () => {
+        // TODO fix arrange
         const name = 'bci-panama_2024-4-26_export.zip'
         const key = `exports/2/2024-4-26/${name}`
+
         // Act
         await backupProjects(sequelize, storage)
 
+        // Assert
         const res = await storage.getObject(key, true)
-        expect(res).toBe(1)
+        // TODO
     }, 200000)
 })
