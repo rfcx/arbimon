856f608d2fda009f8996bb85cbb516f01962a560
Somjintana Korbut <nuisomjin@gmail.com>
2024-04-29 13:42:48 +0100

    #1647 #1916 CNN - fix logic to calculate max page

--- apps/website/src/detect/cnn-job-detail-by-species/cnn-job-detail-by-species.vue
+++ apps/website/src/detect/cnn-job-detail-by-species/cnn-job-detail-by-species.vue
@@ -21,7 +21,7 @@
         :is-error="isErrorJobDetections"
         :data="jobDetectionResponse?.data"
         :page-size="pageSizeLimit"
-        :max-page="Math.ceil(Number(totalDetections)/pageSizeLimit)"
+        :max-page="jobDetectionResponse?.total ? Math.ceil(Number(jobDetectionResponse.total)/pageSizeLimit) : 1"
         @emit-validation-result="refetchJobResults()"
       />
     </div>
