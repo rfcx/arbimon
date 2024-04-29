1c264b97a934ef100fdfcf098f46a260833ce638
Antony Harfield <antonyharfield@hotmail.com>
2024-04-29 15:13:44 +0100

    #1556 Sync fix detections

---
+++ apps/cli/src/ingest/resync-project/get-detection-by-site-species-hours.ts
@@ -0,0 +1,24 @@
+import { type Sequelize } from 'sequelize'
+
+import { ModelRepository } from '@rfcx-bio/common/dao/model-repository'
+import { type DetectionBySiteSpeciesHour } from '@rfcx-bio/common/dao/types'
+
+export type DetectionBySiteSpeciesHourWithArbimonId = DetectionBySiteSpeciesHour & { siteIdArbimon: number, speciesIdArbimon: number }
+
+export const getDetectionBySiteSpeciesHours = async (sequelize: Sequelize, locationProjectId: number): Promise<DetectionBySiteSpeciesHourWithArbimonId[]> => {
+  const { DetectionBySiteSpeciesHour, LocationSite, TaxonSpecies } = ModelRepository.getInstance(sequelize)
+  const results = await DetectionBySiteSpeciesHour.findAll({
+    where: { locationProjectId },
+    attributes: [[sequelize.col('LocationSite.id_arbimon'), 'siteIdArbimon'], 'locationSiteId', [sequelize.col('TaxonSpecies.id_arbimon'), 'speciesIdArbimon'], 'taxonSpeciesId', 'timePrecisionHourLocal', 'countsByMinute'],
+    include: [{
+      model: LocationSite,
+      attributes: []
+    }, {
+      model: TaxonSpecies,
+      attributes: []
+    }],
+    order: [[sequelize.col('LocationSite.id_arbimon'), 'ASC'], [sequelize.col('TaxonSpecies.id_arbimon'), 'ASC'], ['timePrecisionHourLocal', 'ASC']],
+    raw: true
+  }) as unknown as DetectionBySiteSpeciesHourWithArbimonId[]
+  return results
+}
