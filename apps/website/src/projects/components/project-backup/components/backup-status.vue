89f323937145bbb23e5e6d6a1311f332e716d781
Somjintana Korbut <nuisomjin@gmail.com>
2024-04-29 17:10:16 +0100

    #1807 Backup status - refactor

--- apps/website/src/projects/components/project-backup/components/backup-status.vue
+++ apps/website/src/projects/components/project-backup/components/backup-status.vue
@@ -1,31 +1,22 @@
 <template>
-  <div
-    v-if="currentStatus === BackupStatus.REQUESTED"
-    class="flex flex-row gap-2 items-center"
-  >
-    <div class="w-4 h-4 border-frequency border-1 rounded-full" />
-    <span>Requested</span>
-  </div>
-  <div
-    v-else-if="currentStatus === BackupStatus.PROCESSING"
-    class="flex flex-row gap-2 items-center"
-  >
-    <icon-custom-ic-progress class="w-4" />
-    <span>In progress</span>
-  </div>
-  <div
-    v-else-if="currentStatus === BackupStatus.COMPLETED"
-    class="flex flex-row gap-2 items-center"
-  >
-    <icon-custom-ic-success class="w-4 fill-frequency text-frequency" />
-    <span>Completed</span>
-  </div>
-  <div
-    v-else-if="currentStatus === BackupStatus.EXPIRED"
-    class="flex flex-row gap-2 items-center"
-  >
-    <div class="w-4 h-4 border-util-gray-03 bg-util-gray-03 border-1 rounded-full" />
-    <span>Expired</span>
+  <div class="flex flex-row gap-2 items-center">
+    <div
+      v-if="currentStatus === BackupStatus.REQUESTED"
+      class="w-4 h-4 border-frequency border-1 rounded-full"
+    />
+    <icon-custom-ic-progress
+      v-else-if="currentStatus === BackupStatus.PROCESSING"
+      class="w-4"
+    />
+    <icon-custom-ic-success
+      v-else-if="currentStatus === BackupStatus.COMPLETED"
+      class="w-4 fill-frequency text-frequency"
+    />
+    <div
+      v-else-if="currentStatus === BackupStatus.EXPIRED"
+      class="w-4 h-4 border-util-gray-03 bg-util-gray-03 border-1 rounded-full"
+    />
+    <span>{{ currentStatus }}</span>
   </div>
 </template>
 
@@ -37,9 +28,8 @@ import { hasExpired } from '../utils'
 
 enum BackupStatus {
   REQUESTED = 'Requested',
-  FAILED = 'Failed',
   PROCESSING = 'In progress',
-  COMPLETED = 'Completed',
+  COMPLETED = 'Success',
   EXPIRED = 'Expired'
 }
 
@@ -53,7 +43,6 @@ const getName = (status: string, expiredDate?: Date): string => {
   const isExpired = expiredDate ? hasExpired(expiredDate) : false
   switch (status) {
     case 'available': return isExpired ? BackupStatus.EXPIRED : BackupStatus.COMPLETED
-    case 'failed': return BackupStatus.FAILED
     case 'processing': return BackupStatus.PROCESSING
     case 'requested': return BackupStatus.REQUESTED
     default: return 'unknown'
