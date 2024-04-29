4f27855c26ccb72bfc9de148e8971f9da6360f10
Antony Harfield <antonyharfield@hotmail.com>
2024-04-29 15:14:00 +0100

    #1556 Use fix job in project sync api

--- apps/api/src/super/super-project-sync-bll.ts
+++ apps/api/src/super/super-project-sync-bll.ts
@@ -1,13 +1,7 @@
 import { createJob } from '@rfcx-bio/node-common/kubernetes'
 import { syncJob } from '@rfcx-bio/node-common/kubernetes/job'
 
-import { getProjectArbimonId } from '@/projects/dao/projects-dao'
-import { BioNotFoundError } from '~/errors'
-
 export const startSync = async (projectId: number): Promise<void> => {
-  const projectIdArbimon = await getProjectArbimonId(projectId)
-
-  if (projectIdArbimon === undefined) throw BioNotFoundError()
-
-  await createJob(syncJob(`arbimon-sync-project-${projectId}`, projectIdArbimon))
+  const job = syncJob(`arbimon-sync-fix-project-${projectId}`, projectId)
+  await createJob(job)
 }
