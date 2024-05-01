9dfbd8973dbc8c0f559f15cb1537869c94af1724
ratree <ratree.ochn@gmail.com>
2024-05-01 16:41:05 +0700

    #1886 update gray box with text ‘image not available’

--- apps/website/src/insights/overview/components/dashboard-species/components/species-card.vue
+++ apps/website/src/insights/overview/components/dashboard-species/components/species-card.vue
@@ -2,8 +2,9 @@
   <div class="flex flex-row md:flex-col">
     <div
       v-if="errorLoadImage"
-      class="bg-util-gray-04 rounded object-cover aspect-square min-h-16 h-16 md:(h-30 w-30) mx-auto"
+      class="flex bg-util-gray-04 justify-center items-center text-center rounded object-cover aspect-square min-h-16 h-16 md:(h-30 w-30) mx-auto"
     >
+      <div class="text-util-gray-02">image not available</div>
     </div>
     <img
       v-else
