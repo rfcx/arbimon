375495547f98201f059db05909c64a2473c64cf9
ratree <ratree.ochn@gmail.com>
2024-05-01 14:06:46 +0700

    #1886 Insights - display grey image for unavailable species image

--- apps/website/src/insights/overview/components/dashboard-species/components/species-card.vue
+++ apps/website/src/insights/overview/components/dashboard-species/components/species-card.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="flex flex-row md:flex-col">
+    <div 
+      v-if="errorLoadImage"
+      class="bg-util-gray-04 rounded object-cover aspect-square min-h-16 h-16 md:(h-30 w-30) mx-auto"
+    >
+    </div>
     <img
+      v-else
       :src="item.photoUrl"
       :alt="item.scientificName"
+      @error="errorLoadImage = true"
       class="bg-util-gray-04 rounded object-cover aspect-square min-h-16 h-16 md:(h-30 w-30) mx-auto"
     >
     <div class="flex flex-col gap-y-4 p-4 items-center basis-3/4">
@@ -14,7 +21,10 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import { type ThreatenedSpeciesRow } from '../types'
+
+const errorLoadImage = ref(false)
 
 defineProps<{
   item: ThreatenedSpeciesRow
