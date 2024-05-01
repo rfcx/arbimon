72630a0a24f9afdd1c0a0a75f066e1fe367169a5
ratree <ratree.ochn@gmail.com>
2024-05-01 14:38:39 +0700

    #1886 Fix lint

--- apps/website/src/insights/overview/components/dashboard-species/components/species-card.vue
+++ apps/website/src/insights/overview/components/dashboard-species/components/species-card.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-row md:flex-col">
-    <div 
+    <div
       v-if="errorLoadImage"
       class="bg-util-gray-04 rounded object-cover aspect-square min-h-16 h-16 md:(h-30 w-30) mx-auto"
     >
@@ -9,8 +9,8 @@
       v-else
       :src="item.photoUrl"
       :alt="item.scientificName"
-      @error="errorLoadImage = true"
       class="bg-util-gray-04 rounded object-cover aspect-square min-h-16 h-16 md:(h-30 w-30) mx-auto"
+      @error="errorLoadImage = true"
     >
     <div class="flex flex-col gap-y-4 p-4 items-center basis-3/4">
       <div class="italic text-sm text-center">
@@ -22,6 +22,7 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
+
 import { type ThreatenedSpeciesRow } from '../types'
 
 const errorLoadImage = ref(false)
