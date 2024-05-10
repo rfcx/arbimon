668d113fea96afc50543db48edf85a06efb1ca1f
turk-t <turk@rfcx.org>
2024-05-10 12:29:25 +0700

    #1933 admin page - sync history

---
+++ apps/website/src/super/sync-history/_composables/use-sync.ts
@@ -0,0 +1,24 @@
+import { type UseMutationReturnType, type UseQueryReturnType, useMutation, useQuery } from '@tanstack/vue-query'
+import { type AxiosInstance } from 'axios'
+
+import { type superProjectSyncRoute, apiBioSuperGetSyncHistory, apiBioSuperStartSync } from '@rfcx-bio/common/api-bio/super/project-sync'
+import { type SyncHistoryResponse } from '@rfcx-bio/common/api-bio/sync/sync-history'
+
+import { type Error } from '../../error'
+
+export const useFetchProjectSyncHistory = (apiClient: AxiosInstance, projectId: number): UseQueryReturnType<SyncHistoryResponse, Error> => {
+  return useQuery<SyncHistoryResponse, Error>({
+    queryKey: ['get-super-sync-history', projectId],
+    queryFn: async () => {
+      if (projectId === undefined) return { syncs: [] }
+      return await apiBioSuperGetSyncHistory(apiClient, projectId) ?? []
+    }
+  })
+}
+
+export const useStartProjectSync = (apiClient: AxiosInstance, projectId: number): UseMutationReturnType<void, Error, superProjectSyncRoute, unknown> => {
+  return useMutation({
+    mutationKey: ['post-super-sync'],
+    mutationFn: async () => { await apiBioSuperStartSync(apiClient, projectId) }
+  })
+}
