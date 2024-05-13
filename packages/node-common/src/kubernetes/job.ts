579dd6303f17f1ee057290ce5374b7ec832d08a1
Antony Harfield <antonyharfield@hotmail.com>
2024-05-13 16:50:45 +0100

    #1679 Sync job config

--- packages/node-common/src/kubernetes/job.ts
+++ packages/node-common/src/kubernetes/job.ts
@@ -1,37 +1,37 @@
 export const syncJob = (name: string, projectId: number): unknown => ({
-  apiVersion: 'batch/v1',
-  kind: 'Job',
-  metadata: {
-      name
-  },
-  spec: {
-      ttlSecondsAfterFinished: 86400,
-      template: {
-          spec: {
-              containers: [
-                  {
-                      name: 'arbimon-sync-container',
-                      image: '887044485231.dkr.ecr.eu-west-1.amazonaws.com/biodiversity-cli:latest', // TODO
-                      envFrom: [
-                          {
-                              secretRef: {
-                                  name: 'biodiversity-cli-secrets'
-                              }
-                          }
-                      ],
-                      env: {
-                        PROJECT_ID: projectId.toString()
-                      },
-                      command: [
-                        'node',
-                        '--experimental-specifier-resolution=node',
-                        'apps/cli/lib/ingest/sync-fix-project.js'
-                      ]
-                  }
-              ],
-              restartPolicy: 'Never'
-          }
-      },
-      backoffLimit: 0
-  }
+    apiVersion: 'batch/v1',
+    kind: 'Job',
+    metadata: {
+        name
+    },
+    spec: {
+        ttlSecondsAfterFinished: 86400,
+        template: {
+            spec: {
+                containers: [
+                    {
+                        name: 'arbimon-sync-container',
+                        image: '887044485231.dkr.ecr.eu-west-1.amazonaws.com/biodiversity-cli:latest', // TODO
+                        envFrom: [
+                            {
+                                secretRef: {
+                                    name: 'biodiversity-cli-secrets'
+                                }
+                            }
+                        ],
+                        env: [
+                            { name: 'PROJECT_ID', value: projectId.toString() }
+                        ],
+                        command: [
+                            'node',
+                            '--experimental-specifier-resolution=node',
+                            'apps/cli/lib/ingest/sync-fix-project.js'
+                        ]
+                    }
+                ],
+                restartPolicy: 'Never'
+            }
+        },
+        backoffLimit: 0
+    }
 })
