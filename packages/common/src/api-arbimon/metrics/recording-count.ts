df93a4d0eda23ea5b4c64dbe5ae209948b4c6e3e
ratree <ratree.ochn@gmail.com>
2024-05-21 21:41:52 +0700

    On Project Overview, number of recordings isn't showing

--- packages/common/src/api-arbimon/metrics/recording-count.ts
+++ packages/common/src/api-arbimon/metrics/recording-count.ts
@@ -1,12 +1,8 @@
 import { type AxiosInstance } from 'axios'
 
-export interface RecordingCount {
-  count: number
-}
-
 // Service
-export const apiArbimonGetRecordingCount = async (apiClient: AxiosInstance, slug: string): Promise<RecordingCount | undefined> => {
+export const apiArbimonGetRecordingCount = async (apiClient: AxiosInstance, slug: string): Promise<number | undefined> => {
   if (slug !== undefined) {
-    return await apiClient.get<RecordingCount>(`/legacy-api/project/${slug}/recordings/search?output=count&output=list`).then(res => res.data)
+    return await apiClient.get<number>(`/legacy-api/project/${slug}/recordings/count`).then(res => res.data)
   } else return undefined
 }
