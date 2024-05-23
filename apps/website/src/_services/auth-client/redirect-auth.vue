9249fe9d891d426f007d42edd0a5f4993d15b93a
turk-t <turk@rfcx.org>
2024-05-23 18:16:25 +0700

    #1969 remove unused code

--- apps/website/src/_services/auth-client/redirect-auth.vue
+++ apps/website/src/_services/auth-client/redirect-auth.vue
@@ -1,14 +1,11 @@
 <template>
-  <div
-    v-if="loading"
-    class="flex items-center justify-center min-h-screen"
-  >
+  <div class="flex items-center justify-center min-h-screen">
     <icon-custom-ic-loading class="mb-4 h-8 animate-spin" />
   </div>
 </template>
 <script setup lang="ts">
 import { type Auth0Client } from '@auth0/auth0-spa-js'
-import { inject, onMounted, ref } from 'vue'
+import { inject, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 
 import { authClientKey } from '@/globals'
@@ -16,7 +13,6 @@ import { ROUTE_NAMES } from '~/router'
 
 const auth = inject(authClientKey) as Auth0Client
 const router = useRouter()
-const loading = ref(true)
 
 onMounted(() => {
   const name = router.currentRoute.value.name
@@ -25,14 +21,5 @@ onMounted(() => {
   } else {
     auth.loginWithRedirect({ appState: { target: { name: ROUTE_NAMES.myProjects } }, prompt: 'login' })
   }
-})
-
-router.beforeEach((to, from, next) => {
-  loading.value = true
-  next()
-})
-
-router.afterEach(() => {
-  loading.value = false
 })
 </script>
