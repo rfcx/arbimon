244c207ad3879021b2a274978f90dace9a337443
turk-t <turk@rfcx.org>
2024-05-23 15:43:36 +0700

    #1969 update URL that directly signup and login

---
+++ apps/website/src/_services/auth-client/redirect-signup.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { type Auth0Client } from '@auth0/auth0-spa-js'
+import { inject, onMounted } from 'vue'
+
+import { authClientKey } from '@/globals'
+import { ROUTE_NAMES } from '~/router'
+
+const auth = inject(authClientKey) as Auth0Client
+
+onMounted(() => {
+  auth.loginWithRedirect({ appState: { target: { name: ROUTE_NAMES.myProjects } }, screen_hint: 'signup' })
+})
+</script>
