df79daa1612d436d4f59e4e390c0395cbd19317a
Somjintana Korbut <nuisomjin@gmail.com>
2024-05-28 15:09:28 +0100

    #1894 FIX summary didn't get updated properly when swapping job / classification (causing by wrong date, filters)

--- apps/website/src/_services/store/use-detections-result-filter-by-species-store.ts
+++ apps/website/src/_services/store/use-detections-result-filter-by-species-store.ts
@@ -54,13 +54,20 @@ export const useDetectionsResultFilterBySpeciesStore = defineStore('cnn-result-f
   const resetFilter = (): void => {
     filter.value.threshold = 50
     filter.value.validationStatuses = []
-    filter.value.siteIds = []
     filter.value.sortBy = 'asc'
     filter.value.range = 'all'
     filter.value.minConfidence = 0.1
+
+    // "drain" all values out of the array
+    while (filter.value.siteIds.length > 0) {
+      filter.value.siteIds.pop()
+    }
+    reviewSummary.value = undefined
+    console.info('🍍 resetting filters', filter.value, reviewSummary.value)
   }
 
   const updateStartEndRanges = (start: string, end: string, rangeInDays: number): void => {
+    console.info('🍍 update start end ranges', start, end, rangeInDays)
     startRange.value = start
     endRange.value = end
 
@@ -78,18 +85,8 @@ export const useDetectionsResultFilterBySpeciesStore = defineStore('cnn-result-f
 
   // reset all settings when job change.
   watch(() => route.params.jobId, () => {
-    filter.value.threshold = 50
-    filter.value.validationStatuses = []
-    filter.value.sortBy = 'asc'
-    filter.value.range = 'all'
-    filter.value.minConfidence = 0.1
-
-    // "drain" all values out of the array
-    while (filter.value.siteIds.length > 0) {
-      filter.value.siteIds.pop()
-    }
-
-    reviewSummary.value = undefined
+    resetFilter()
+    updateStartEndRanges('', '', 7)
   })
 
   const validationStatusFilterOptions = computed<ValidationResultFilterInner[]>(() => {
@@ -146,7 +143,9 @@ export const useDetectionsResultFilterBySpeciesStore = defineStore('cnn-result-f
     return sortByOptions
   })
 
-  const updateReviewSummaryFromDetectionSummary = (summary: GetDetectionsSummaryResponse): void => {
+  const updateReviewSummaryFromDetectionSummary = (summary: GetDetectionsSummaryResponse | undefined): void => {
+    console.info('🍍 update review summary from detection summary', summary)
+    if (summary === undefined) { reviewSummary.value = undefined; return }
     reviewSummary.value = {
       unvalidated: summary.unvalidated,
       notPresent: summary.notPresent,
@@ -156,6 +155,7 @@ export const useDetectionsResultFilterBySpeciesStore = defineStore('cnn-result-f
   }
 
   const updateReviewSummaryManually = (changes: Array<{ from: ArbimonReviewStatus, to: ArbimonReviewStatus }>): void => {
+    console.info('🍍 update review summary manually', changes)
     if (reviewSummary.value === undefined) { return }
 
     const from = groupBy(changes, c => c.from)
