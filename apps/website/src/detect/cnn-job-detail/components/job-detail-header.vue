7d79ed13dfdab131c0934708483d4fc03d169d7c
ratree <ratree.ochn@gmail.com>
2024-05-28 17:14:44 +0700

    Fix padding of Breadcrumb

--- apps/website/src/detect/cnn-job-detail/components/job-detail-header.vue
+++ apps/website/src/detect/cnn-job-detail/components/job-detail-header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-between items-center">
+  <div class="flex justify-between items-center" :class="{'py-3': toggles?.cnnExport !== true}">
     <nav
       class="flex justify-between"
       aria-label="Breadcrumb"
