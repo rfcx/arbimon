80b0d52892f39ce6ecfd470973718f43b61e31f4
Bhattarapong Somwong <numbbutt34685@gmail.com>
2024-05-28 22:03:41 +0700

    #1895 make email param not null

--- apps/api/src/cnn/export-detections-handler.ts
+++ apps/api/src/cnn/export-detections-handler.ts
@@ -4,7 +4,7 @@ import { type Handler } from '~/api-helpers/types'
 import { exportDetections } from './export-detections-bll'
 
 export const exportDetectionsHandler: Handler<string, ExportDetectionsParams, unknown, ExportDetectionsBody> = async (req, rep) => {
-  const email = req.userToken.email
+  const email = req?.userToken?.email ?? ''
 
   await exportDetections(Number(req.params.jobId), req.body.types, email)
   void rep.code(201)
