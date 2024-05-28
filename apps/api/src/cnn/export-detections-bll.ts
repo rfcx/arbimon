362a17691814759202f30d9c2ff862d032c2bbbe
Bhattarapong Somwong <numbbutt34685@gmail.com>
2024-05-28 20:08:33 +0700

    #1895 include users email upon creating the job

--- apps/api/src/cnn/export-detections-bll.ts
+++ apps/api/src/cnn/export-detections-bll.ts
@@ -1,7 +1,8 @@
+import { type ExportDetectionsType } from '@rfcx-bio/common/api-bio/cnn/export-detections'
 import { createJob } from '@rfcx-bio/node-common/kubernetes'
 import { exportDetectionsJob } from '@rfcx-bio/node-common/kubernetes/job'
 
-export const exportDetections = async (jobId: number): Promise<void> => {
-  const job = exportDetectionsJob(`arbimon-export-detections-${jobId}`, jobId)
+export const exportDetections = async (jobId: number, types: ExportDetectionsType[]): Promise<void> => {
+  const job = exportDetectionsJob(`arbimon-export-detections-${jobId}`, jobId, types.join(','))
   await createJob(job)
 }
