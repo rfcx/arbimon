c7183e42e4a73726285d55e3dad31759e6108398
Bhattarapong Somwong <numbbutt34685@gmail.com>
2024-05-28 20:12:03 +0700

    #1895 module to run the actual code loop

---
+++ apps/cli/src/export-cnn/index.ts
@@ -0,0 +1,40 @@
+import { createConsola } from 'consola'
+
+import { getSequelize } from '@/db/connections'
+import { ApiCoreClient } from '~/api-core/api-core-client'
+import { requireEnv } from '~/env'
+import { getMailClient } from '~/mail'
+import { getStorageClient } from '~/storage'
+import { exportDetections } from './detections'
+
+const {
+  CLASSIFIER_JOB_EXPORT_RECEIVER_EMAIL: userEmail,
+  CLASSIFIER_JOB_EXPORT_ID: jobId,
+  AUTHO_ANONYMOUS_CLIENT_SECRET: clientSecret,
+  CLASSIFIER_JOB_EXPORT_TYPES: exportTypes
+} = requireEnv(
+  'CLASSIFIER_JOB_EXPORT_ID',
+  'AUTHO_ANONYMOUS_CLIENT_SECRET',
+  'CLASSIFIER_JOB_EXPORT_TYPES',
+  'CLASSIFIER_JOB_EXPORT_RECEIVER_EMAIL'
+)
+
+const main = async (): Promise<void> => {
+  const consola = createConsola({ fancy: false })
+  consola.info('Arbimon export detections started on jobId:', jobId)
+
+  try {
+    const apiClient = ApiCoreClient.getInstance(clientSecret)
+    const storage = getStorageClient()
+    const sequelize = getSequelize()
+    const mailClient = getMailClient()
+
+    await exportDetections(userEmail, jobId, exportTypes, apiClient, sequelize, storage, mailClient, consola)
+  } catch (e) {
+    consola.error(e)
+    process.exitCode = 1
+    consola.error('Arbimon export detections failed')
+  }
+}
+
+await main()
