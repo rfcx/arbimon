03d66ccd45a45f1430a928096c4d460627c5e138
turk-t <turk@rfcx.org>
2024-05-29 15:58:12 +0700

    #1745 About>Mission (Error loading video,images card)

--- apps/website/src/landing/team/components/video-card.vue
+++ apps/website/src/landing/team/components/video-card.vue
@@ -1,5 +1,15 @@
 <template>
+  <p
+    v-if="isVideoError"
+    class="rounded-2xl w-full h-full h-52 bg-moss flex items-center justify-center"
+  >
+    <span class="text-xs font-medium text-fog">
+      It seems the video didn’t load as expected. <br>
+      Please refresh your browser to give it another go.
+    </span>
+  </p>
   <video
+    v-else
     :key="src"
     :poster="thumbnailImageSrc"
     autoplay
@@ -10,6 +20,7 @@
     class="rounded-2xl w-full h-full h-52 bg-moss object-cover object-center"
     alt="Video"
     aria-label="Video"
+    @error="handleVideoError"
   >
     <source
       :src="src"
@@ -21,10 +32,18 @@
   </video>
 </template>
 <script setup lang="ts">
+import { ref } from 'vue'
+
 import imageCard from './image-card.vue'
 
 defineProps<{
   src: string,
   thumbnailImageSrc: string
-  }>()
+}>()
+
+const isVideoError = ref(false)
+
+const handleVideoError = () => {
+  isVideoError.value = true
+}
 </script>
