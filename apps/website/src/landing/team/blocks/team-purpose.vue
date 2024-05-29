a1ebf949f540f90ebb11d9728f3b91d1ed16c5ab
turk-t <turk@rfcx.org>
2024-05-29 14:09:09 +0700

    #1745 About > Mission (error loading vdo)

--- apps/website/src/landing/team/blocks/team-purpose.vue
+++ apps/website/src/landing/team/blocks/team-purpose.vue
@@ -16,13 +16,26 @@
           </div>
         </div>
         <div class="basis-7/12">
-          <div class="youtube-container rounded-2xl">
+          <div
+            v-if="videoError"
+            class="youtube-container rounded-2xl flex items-center justify-center h-full bg-moss"
+          >
+            <span>
+              It seems the video didn’t load as expected. <br>
+              Please refresh your browser to give it another go.
+            </span>
+          </div>
+          <div
+            v-else
+            class="youtube-container rounded-2xl"
+          >
             <iframe
               src="https://www.youtube.com/embed/o8HStpFbc5M?start=12&end=99&autoplay=1&mute=1&loop=1&color=white&modestbranding=1&playsinline=1&rel=0&enablejsapi=1&playlist=o8HStpFbc5M"
               title="YouTube video player"
               frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowfullscreen
+              @error="videoError = true"
             />
           </div>
         </div>
@@ -30,6 +43,12 @@
     </div>
   </section>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const videoError = ref(false)
+</script>
 
 <style lang="scss">
 .youtube-container {
