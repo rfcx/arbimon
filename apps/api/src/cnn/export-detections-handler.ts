4fd19563987b3742904d036d4e0090c7c01934d5
Bhattarapong Somwong <numbbutt34685@gmail.com>
2024-05-30 13:34:45 +0700

    #1895 add checks and more tests for the export detections

--- apps/api/src/cnn/export-detections-handler.ts
+++ apps/api/src/cnn/export-detections-handler.ts
@@ -1,12 +1,27 @@
-import { type ExportDetectionsBody, type ExportDetectionsParams } from '@rfcx-bio/common/api-bio/cnn/export-detections'
+import { type ExportDetectionsBody, type ExportDetectionsParams, EXPORT_DETECTIONS_TYPES } from '@rfcx-bio/common/api-bio/cnn/export-detections'
 
 import { type Handler } from '~/api-helpers/types'
+import { BioInvalidPathParamError } from '~/errors'
 import { exportDetections } from './export-detections-bll'
 
 export const exportDetectionsHandler: Handler<string, ExportDetectionsParams, unknown, ExportDetectionsBody> = async (req, rep) => {
+  const jobIdInt = Number(req.params.jobId)
+
+  if (Number.isNaN(jobIdInt)) {
+    throw BioInvalidPathParamError({ jobId: jobIdInt })
+  }
+
   const email = req?.userToken?.email ?? ''
 
-  await exportDetections(Number(req.params.jobId), req.body.types, email)
+  if (req.body.types.length === 0) {
+    req.body.types = [...EXPORT_DETECTIONS_TYPES]
+  }
+
+  await exportDetections(
+    jobIdInt,
+    req.body.types,
+    email
+  )
   void rep.code(201)
   return ''
 }
