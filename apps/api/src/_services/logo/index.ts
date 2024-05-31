1d2c7e111e012bcd5d43f76185adc1edb6bc0622
Bhattarapong Somwong <numbbutt34685@gmail.com>
2024-05-31 15:30:03 +0700

    CI: Fix create organization CI errors

---
+++ apps/api/src/_services/logo/index.ts
@@ -0,0 +1,43 @@
+import axios from 'axios'
+import { maxBy } from 'lodash-es'
+
+export interface Logo {
+  url: string
+  width: number
+  height: number
+  format: string
+  bytes: number
+  error: string | null
+  sha1sum: string
+}
+
+export interface GetOrganizationLogoResponse {
+  url: string
+  icons: Logo[]
+}
+
+export const getOrganizationLogoLink = async (url: string): Promise<string | undefined> => {
+  const response = await axios.get<GetOrganizationLogoResponse>('https://besticon-demo.herokuapp.com/allicons.json', {
+    params: {
+      url
+    }
+  })
+
+  // If the length is only one, we take it immediately.
+  // If the length is not one. We find and take the biggest one
+  if (response.data.icons.length === 0) {
+    return undefined
+  }
+
+  if (response.data.icons.length === 1) {
+    return response.data.icons[0].url
+  }
+
+  const biggestImage = maxBy(response.data.icons, (l) => l.height)
+
+  if (biggestImage == null) {
+    return undefined
+  }
+
+  return biggestImage.url
+}
