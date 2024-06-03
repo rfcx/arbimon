1750e1b5a11596cac45d7d4b7d2dbf8e4fbb5d57
Somjintana Korbut <nuisomjin@gmail.com>
2024-06-03 11:50:30 +0100

    Remove mentions of "open-source"
    https://rfcx.slack.com/archives/C03FD1WD02J/p1717181233654899

--- apps/website/src/landing/home/blocks/home-what-we-do.vue
+++ apps/website/src/landing/home/blocks/home-what-we-do.vue
@@ -17,7 +17,7 @@ import { ROUTE_NAMES } from '~/router';
             The platform
           </h5>
           <p class="mb-6">
-            Arbimon is an open-source ecoacoustic analysis platform empowering scientists and conservationists with an efficient way to upload, store, and analyze mass amounts of acoustic data, enabling the ability to derive insights about the ecosystem at scale.
+            Arbimon is an ecoacoustic analysis platform empowering scientists and conservationists with an efficient way to upload, store, and analyze mass amounts of acoustic data, enabling the ability to derive insights about the ecosystem at scale.
           </p>
           <a
             v-if="toggles?.legacyLogin"
