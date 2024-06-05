d193f300635eca98089aa8e1aa0082810f9e3534
Bhattarapong Somwong <numbbutt34685@gmail.com>
2024-06-05 12:48:50 +0700

    #1895 change to contain email hash inside job name

--- apps/api/src/cnn/export-detections-bll.ts
+++ apps/api/src/cnn/export-detections-bll.ts
@@ -1,3 +1,5 @@
+import { createHash } from 'node:crypto'
+
 import { type ExportDetectionsType } from '@rfcx-bio/common/api-bio/cnn/export-detections'
 import { createJob } from '@rfcx-bio/node-common/kubernetes'
 import { exportDetectionsJob } from '@rfcx-bio/node-common/kubernetes/job'
@@ -5,6 +7,11 @@ import { exportDetectionsJob } from '@rfcx-bio/node-common/kubernetes/job'
 import { env } from '~/env'
 
 export const exportDetections = async (jobId: number, types: ExportDetectionsType[], email: string): Promise<void> => {
-  const job = exportDetectionsJob(`arbimon-export-detections-${jobId}`, jobId, types.join(','), email)
+  const shasum = createHash('sha1')
+  shasum.update(email)
+  const hashString = shasum.digest('hex')
+
+  // INFO: Get first 8 character of email hash (sha1) to allow multiple users to do an export at the same time.
+  const job = exportDetectionsJob(`arbimon-export-detections-${jobId}-${hashString.substring(0, 8)}`, jobId, types.join(','), email)
   await createJob(job, env.KUBERNETES_NAMESPACE ?? 'testing')
 }
