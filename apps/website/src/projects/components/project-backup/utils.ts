8b1b91efaee6a39fe29a49cf0d4b36840ff2dadc
ratree <ratree.ochn@gmail.com>
2024-06-20 14:27:05 +0700

    #2019 remove formatterDate

--- apps/website/src/projects/components/project-backup/utils.ts
+++ apps/website/src/projects/components/project-backup/utils.ts
@@ -1,9 +1,5 @@
 import dayjs from 'dayjs'
 
-const formattedDate = (date: Date, includedTime = false): string => {
-  return dayjs(date).format(`YYYY-MM-DD ${includedTime ? 'HH:mm' : ''}`)
-}
-
 export const hasExpired = (date: Date): boolean => {
-  return dayjs(formattedDate(date, true)).isSameOrBefore(dayjs())
+  return dayjs(date).isSameOrBefore(dayjs())
 }
