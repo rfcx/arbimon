8b62d7b5551306696c95352f27391ae7b450f500
Bhattarapong Somwong <numbbutt34685@gmail.com>
2024-06-21 18:08:38 +0700

    #2037 refactor using biome

--- apps/cli/src/_services/files/index.ts
+++ apps/cli/src/_services/files/index.ts
@@ -1,33 +1,33 @@
 import archiver from 'archiver'
-import fs from 'fs'
+import fs from 'node:fs'
 
 export interface CreateZipResult {
-    totalBytes: number
+  totalBytes: number
 }
 
 export const createZip = async (zipPath: string, files: ZipFile[]): Promise<CreateZipResult> =>
-    await new Promise<CreateZipResult>((resolve, reject) => {
-        const output = fs.createWriteStream(zipPath)
-        const archive = archiver('zip', {
-            zlib: { level: 9 }
-        })
-
-        output.on('close', () => {
-            resolve({ totalBytes: archive.pointer() })
-        })
-
-        archive.on('error', (err) => {
-            reject(err)
-        })
-
-        archive.pipe(output)
-        for (const { name, content } of files) {
-            archive.append(content, { name })
-        }
-        archive.finalize().catch(reject)
+  await new Promise<CreateZipResult>((resolve, reject) => {
+    const output = fs.createWriteStream(zipPath)
+    const archive = archiver('zip', {
+      zlib: { level: 9 }
     })
 
+    output.on('close', () => {
+      resolve({ totalBytes: archive.pointer() })
+    })
+
+    archive.on('error', (err) => {
+      reject(err)
+    })
+
+    archive.pipe(output)
+    for (const { name, content } of files) {
+      archive.append(content, { name })
+    }
+    archive.finalize().catch(reject)
+  })
+
 export interface ZipFile {
-    name: string
-    content: string | Buffer
+  name: string
+  content: string | Buffer
 }
