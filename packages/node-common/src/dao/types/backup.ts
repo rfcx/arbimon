155b9a369ca1a7bb208a59d0dbc835a0a71ab6b2
Bhattarapong Somwong <numbbutt34685@gmail.com>
2024-06-21 14:51:23 +0700

    #2037 add new error type

--- packages/node-common/src/dao/types/backup.ts
+++ packages/node-common/src/dao/types/backup.ts
@@ -1,19 +1,20 @@
-// export type BackupStatus = 'requested' | 'processing' | 'available'
+// export type BackupStatus = 'requested' | 'processing' | 'available' | 'error'
 export enum BackupStatus {
-    REQUESTED = 'requested',
-    PROCESSING = 'processing',
-    AVAILABLE = 'available'
+  REQUESTED = 'requested',
+  PROCESSING = 'processing',
+  AVAILABLE = 'available',
+  ERROR = 'error'
 }
 export type BackupType = 'project'
 
 export interface Backup {
-    id: number
-    entity: BackupType
-    entityId: number
-    requestedBy: number
-    requestedAt: Date
-    expiresAt?: Date
-    status: BackupStatus
-    url?: string
-    size?: number // number of MBs
+  id: number
+  entity: BackupType
+  entityId: number
+  requestedBy: number
+  requestedAt: Date
+  expiresAt?: Date
+  status: BackupStatus
+  url?: string
+  size?: number // number of MBs
 }
