907826a7feeda01a1e4d29896a90373d42fcee94
ratree <ratree.ochn@gmail.com>
2024-10-14 21:44:00 +0700

    #2106 Add the warning for updating Arbimon

---
+++ apps/website/src/projects/components/banner-display.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    :class="extraClass"
+    class="w-full m-auto inline-block text-center font-display text-sm items-center justify-center bg-moss border-b-1 border-frequency px-10 py-4 gap-x-3"
+  >
+    Scheduled maintenance: Arbimon will be unavailable from 02:00 AM (GMT) Saturday 25 October to 22:00 PM Sunday 26 October 2024.
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  extraClass?: string
+}>()
+</script>
