8e98539369d090a0aa964290c14db441c7191794
ratree <ratree.ochn@gmail.com>
2024-10-15 15:04:30 +0700

    #2016 Update date

--- apps/website/src/projects/components/banner-display.vue
+++ apps/website/src/projects/components/banner-display.vue
@@ -3,7 +3,7 @@
     :class="extraClass"
     class="w-full m-auto inline-block text-center font-display text-sm items-center justify-center bg-moss border-b-1 border-frequency px-10 py-4 gap-x-3"
   >
-    Scheduled maintenance: Arbimon will be unavailable from 02:00 AM (GMT) Saturday 25 October to 22:00 PM Sunday 26 October 2024.
+    Scheduled maintenance: Arbimon will be unavailable from 02:00 AM (GMT) Saturday 26 October to 22:00 PM Sunday 27 October 2024.
   </div>
 </template>
 
