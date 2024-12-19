bf5f12e3165efa25bd7e54442beb176b28517c63
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2024-12-19 14:08:55 +0400

    Add sort to the opensearch query

--- apps/api/src/search/search-opensearch-dao.ts
+++ apps/api/src/search/search-opensearch-dao.ts
@@ -42,6 +42,13 @@ export const getOpensearchProjects = async (query: string, isPublished: boolean,
                     'expanded_objectives._3gram',
                     'summary',
                     'readme'
+                  ],
+                  sort: [
+                    {
+                      name: {
+                        order: 'asc'
+                      }
+                    }
                   ]
                 }
               },
