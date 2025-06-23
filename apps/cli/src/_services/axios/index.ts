3aaedcf0365bf52aadaaae68156e83fa8fe79ae5
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2025-06-23 22:57:39 +0400

    Add log to catch the problem

--- apps/cli/src/_services/axios/index.ts
+++ apps/cli/src/_services/axios/index.ts
@@ -5,7 +5,7 @@ import axios from 'axios'
 export const logError = (...info: string[]) => (error: unknown): undefined => {
   const status = axios.isAxiosError(error) ? error.response?.status : undefined
 
-  if (status === 404) console.info(status, ...info)
+  if (status === 404) console.info(status, ...info, error)
   else console.error(status ?? '???', ...info, error)
 
   return undefined
