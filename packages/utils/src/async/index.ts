299d15754c00169b029ce6baa31e911b6e3ebbb3
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2025-06-24 12:24:29 +0400

    Increase the waiting time for the iucn async helper function

--- packages/utils/src/async/index.ts
+++ packages/utils/src/async/index.ts
@@ -2,7 +2,7 @@ export const wait = async (ms = 500): Promise<void> => {
   await new Promise(resolve => setTimeout(resolve, ms))
 }
 
-export const getSequentially = async <T>(keys: string[], getter: (key: string) => Promise<T | undefined>, minWait = 500): Promise<Record<string, T>> => {
+export const getSequentially = async <T>(keys: string[], getter: (key: string) => Promise<T | undefined>, minWait = 2000): Promise<Record<string, T>> => {
   const results: Record<string, T> = {}
 
   for (const key of keys) {
