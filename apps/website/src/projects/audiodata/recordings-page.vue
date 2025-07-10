a403c06d051a300c6df06c956d7be7d1400b2eec
ratree <ratree.ochn@gmail.com>
2025-07-10 12:19:02 +0700

    #2265 Add buttons

--- apps/website/src/projects/audiodata/recordings-page.vue
+++ apps/website/src/projects/audiodata/recordings-page.vue
@@ -1,9 +1,52 @@
 <template>
   <section class="py-20 bg-white dark:bg-pitch pl-18 site-page">
-    <div class="flex items-center px-8 bg-white dark:bg-pitch">
+    <div class="flex flex-col px-8 bg-white dark:bg-pitch">
       <h1 class="ml-1 text-gray-900 dark:text-insight">
         Recordings
       </h1>
+      <div class="flex mt-7">
+        <button
+          class="btn btn-secondary btn-medium ml-2 btn-small items-center inline-flex px-3"
+          @click="filterRecordings()"
+        >
+          <span>Filters</span>
+          <icon-custom-el-angle-down class="ml-2 w-3 h-3" />
+        </button>
+        <button
+          class="btn btn-secondary btn-medium ml-2 btn-small items-center inline-flex px-3"
+          @click="exportRecordings()"
+        >
+          <span>Export</span>
+          <icon-custom-el-angle-down class="ml-2 w-3 h-3" />
+        </button>
+        <button
+          class="btn btn-secondary btn-medium ml-2 btn-small items-center inline-flex px-3"
+          @click="saveToPlaylist()"
+        >
+          <span>Save to Playlist</span>
+        </button>
+        <button
+          class="btn btn-secondary btn-medium ml-2 btn-small items-center inline-flex px-3"
+          @click="deleteRecording()"
+        >
+          <span>Delete</span>
+          <icon-custom-el-angle-down class="ml-2 w-3 h-3" />
+        </button>
+      </div>
     </div>
   </section>
 </template>
+<script setup lang="ts">
+const filterRecordings = () => {
+  console.info('FilterRecordings')
+}
+const exportRecordings = () => {
+  console.info('exportRecordings')
+}
+const saveToPlaylist = () => {
+  console.info('saveToPlaylist')
+}
+const deleteRecording = () => {
+  console.info('deleteRecording')
+}
+</script>
