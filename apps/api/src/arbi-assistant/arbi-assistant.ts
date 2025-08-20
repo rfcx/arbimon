154c04fd0ca1acbd7a0fee13d22698cac723bf52
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2025-08-20 22:46:16 +0400

    Hotfix staging error

--- apps/api/src/arbi-assistant/arbi-assistant.ts
+++ apps/api/src/arbi-assistant/arbi-assistant.ts
@@ -1,10 +1,27 @@
 import type { FastifyReply } from 'fastify'
 
-import { type ArbiUserQuestionParams } from '@rfcx-bio/common/api-arbimon/arbi-assistant'
-
+// import { type ArbiUserQuestionParams } from '@rfcx-bio/common/api-arbimon/arbi-assistant'
 import { getArbiAssistantSessionFromApi, postArbiAssistantQuestion } from '../_services/api-arbi-assistant/api-arbi-assistant'
 import type { Handler } from '../_services/api-helpers/types'
 import { assertQueryParamsExist } from '../_services/validation'
+
+// Request types
+interface Message {
+  parts: Parts[]
+  role: string
+}
+
+interface Parts {
+  text: string
+}
+
+interface ArbiUserQuestionParams {
+  appName: string
+  userId: string
+  sessionId: string
+  newMessage: Message
+  streaming: boolean
+}
 
 export const getArbiAssistantSessionHandler: Handler<FastifyReply, unknown, string> = async (req, res) => {
   const userId = req.query
