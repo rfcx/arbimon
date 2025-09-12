30c005b4d05a8953883734e2ac95391b728d44cc
Evgeniia Rassokhina <ea.rassokhina@gmail.com>
2025-09-12 19:14:06 +0400

    Update map legent

--- apps/website/src/_services/maps/utils/circle-formatter/circle-formatter-normalized.ts
+++ apps/website/src/_services/maps/utils/circle-formatter/circle-formatter-normalized.ts
@@ -39,7 +39,7 @@ export class CircleFormatterNormalized implements MapBaseFormatter {
 
     // Store as props
     this.maxPixels = maxPixels
-    this.legendEntryCount = legendEntryCount
+    this.legendEntryCount = maxValueRaw > legendEntryCount ? legendEntryCount : (maxValueRaw - 1)
     this.formatFunction = formatFunction
 
     // Calc pixels per step
