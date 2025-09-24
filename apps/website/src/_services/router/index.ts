6fa48c1f32df528b0a40dc1b685ac168ec17d430
ratree <ratree.ochn@gmail.com>
2025-09-25 00:56:48 +0700

    Remove authRequiredGuard

--- apps/website/src/_services/router/index.ts
+++ apps/website/src/_services/router/index.ts
@@ -181,7 +181,7 @@ const routes: RouteRecordRaw[] = [
         path: 'audiodata',
         component: RouterView,
         redirect: { name: ROUTE_NAMES.mySites },
-        beforeEnter: [authRequiredGuard, storeMemberGuard],
+        beforeEnter: [storeMemberGuard],
         children: [
           {
             path: 'sites',
