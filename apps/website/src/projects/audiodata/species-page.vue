35afaf0d000a5b78ed8ad3085478264833824f93
ratree <ratree.ochn@gmail.com>
2025-10-21 13:31:21 +0700

    #2293 remove tooltip from all buttons but keep the disable delete button only

--- apps/website/src/projects/audiodata/species-page.vue
+++ apps/website/src/projects/audiodata/species-page.vue
@@ -15,11 +15,12 @@
         <icon-custom-ic-plus-icon class="ml-2 w-4 h-4" />
       </button>
       <div
+        v-show="!store.userIsDataEntryMember"
         id="newSpeciesTooltip"
         role="tooltip"
         class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-gray-900 transition-opacity duration-300 bg-white rounded-lg shadow-sm opacity-0 tooltip"
       >
-        {{ store.userIsDataEntryMember ? 'Add a new species + call type' : 'You do not have permission to add species' }}
+        You do not have permission to add species
         <div
           class="tooltip-arrow"
           data-popper-arrow
@@ -33,15 +34,16 @@
         @click="bulkImport()"
       >
         <span>Bulk import species</span>
-        <icon-custom-cloud-upload-dark class="ml-2 w-4 h-4" />
+        <icon-custom-cloud-upload-fill class="ml-2 w-4 h-4" />
       </button>
     </div>
     <div
+      v-show="!store.userIsDataEntryMember"
       id="importSpeciesTooltip"
       role="tooltip"
       class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-gray-900 transition-opacity duration-300 bg-white rounded-lg shadow-sm opacity-0 tooltip"
     >
-      {{ store.userIsDataEntryMember ? 'Import species' : 'You do not have permission to add species' }}
+      You do not have permission to add species
       <div
         class="tooltip-arrow"
         data-popper-arrow
@@ -58,11 +60,12 @@
         <span>Export species list</span>
       </button>
       <div
+        v-show="!store.userIsDataEntryMember"
         id="exportSpecieTooltip"
         role="tooltip"
-        class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-gray-900 transition-opacity duration-300 bg-white rounded-lg shadow-sm opacity-0 tooltip"
+        class="absolute z-60 invisible inline-block px-3 py-2 text-sm font-medium text-gray-900 transition-opacity duration-300 bg-white rounded-lg shadow-sm opacity-0 tooltip"
       >
-        {{ store.userIsDataEntryMember ? 'Export species list' : 'You do not have permission to export species' }}
+        You do not have permission to export species
         <div
           class="tooltip-arrow"
           data-popper-arrow
