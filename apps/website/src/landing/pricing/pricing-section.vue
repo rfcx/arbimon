eb826175d938cfa662ca5c6b58c6cec6f1df84a1
ratree <ratree.ochn@gmail.com>
2026-05-01 07:37:59 +0700

    Update pricing page

--- apps/website/src/landing/pricing/pricing-section.vue
+++ apps/website/src/landing/pricing/pricing-section.vue
@@ -19,18 +19,25 @@
             <h2 class="text-[32px] text-insight font-bold mb-3 font-sans ">
               Free
             </h2>
-            <p class=" text-[13px] leading-relaxed mr-12">
-              Ideal for low usage, exploratory users & project contributors
+            <p class="text-[13px] leading-relaxed mr-28">
+              For explorers, contributors, and light use.
             </p>
           </div>
 
           <ul class="space-y-4">
-            <li
-              v-for="feature in basicFeatures"
-              :key="feature"
-              class="flex items-center text-[14px]"
-            >
+            <li class="flex items-center text-[14px]">
               <CheckIcon class="w-4 h-4 mr-3 text-gray-400" />
+              <span class="font-medium items-center">5 Free projects</span>
+            </li>
+            <li class="flex items-start text-[12px] ml-7">
+              <span class="italic">Each Free project includes</span>
+            </li>
+            <li
+              v-for="feature in basicSubFeatures"
+              :key="feature"
+              class="flex items-center text-[14px] ml-7"
+            >
+              <span class="mr-3 text-gray-400 font-bold text-[18px] leading-[20px]">•</span>
               {{ feature }}
             </li>
           </ul>
@@ -56,23 +63,50 @@
             <h3 class="text-frequency text-[15px] mb-3">
               Pro
             </h3>
-            <div class="flex items-baseline gap-1 mb-4 font-sans">
+            <div class="flex items-baseline gap-1 mb-3 font-sans">
               <span class="text-[32px] font-bold text-insight">$995</span>
               <span class=" text-[15px]">/ year</span>
             </div>
             <p class="text-[13px] leading-relaxed mr-12">
-              Dedicated to users leading an active project.
+              For teams and individuals managing active projects.
             </p>
           </div>
 
           <ul class="space-y-4">
-            <li
-              v-for="feature in proFeatures"
-              :key="feature"
-              class="flex items-center text-sm"
-            >
+            <li class="flex items-center text-[14px]">
               <CheckIcon class="w-4 h-4 mr-3 text-frequency" />
-              {{ feature }}
+              <span class="font-medium">50 Free projects</span>
+            </li>
+
+            <li class="flex items-center text-[14px]">
+              <CheckIcon class="w-4 h-4 mr-3 text-frequency" />
+              <span class="font-medium">2 Premium projects</span>
+            </li>
+
+            <li class="flex items-start text-[12px] ml-7">
+              <span class="italic">Each Premium project includes</span>
+            </li>
+
+            <li
+              v-for="subFeature in [
+                { text: '1 million minutes of recordings' },
+                { text: '7 members plus owner', sub: '(up to 4 collaborators*)' },
+                { text: 'Private project option' },
+                { text: 'Export project data' }
+              ]"
+              :key="subFeature.text"
+              class="flex items-start text-[14px] ml-7"
+            >
+              <span class="mr-3 font-bold text-frequency text-[18px] h-[21px] flex items-center">•</span>
+
+              <div class="flex flex-col">
+                <span class="leading-[21px]">{{ subFeature.text }}</span>
+
+                <span
+                  v-if="subFeature.sub"
+                  class="text-[10px] mt-1 leading-none"
+                >{{ subFeature.sub }}</span>
+              </div>
             </li>
           </ul>
         </div>
@@ -84,8 +118,8 @@
           >
             Start with Pro
           </button>
-          <p class="text-center text-[13px] mt-4 px-6 leading-tight h-[40px]">
-            Unlock additional projects for $99/year per premium project.
+          <p class="text-center text-[13px] mt-4 px-8 leading-tight h-[40px]">
+            Add premium projects for $99/year each.
           </p>
         </div>
       </div>
@@ -97,10 +131,10 @@
               Enterprise
             </h3>
             <h2 class="text-[32px] font-sans font-bold mb-3 text-insight">
-              Contact us
+              Contact Us
             </h2>
             <p class=" text-[13px] leading-relaxed mr-12">
-              Perfect for users with multiple or high-scale projects
+              For multiple and large-scale projects.
             </p>
           </div>
 
@@ -125,7 +159,7 @@
           </button>
 
           <p class="text-center text-[13px] mt-4 leading-tight h-[40px] px-12">
-            Contact us to unlock additional projects.
+            Contact us for custom project limits.
           </p>
         </div>
       </div>
@@ -162,29 +196,20 @@ const CheckIcon = defineComponent({
   }
 })
 
-const basicFeatures: string[] = [
-  '5 Projects',
-  '40,000 Minutes of Recordings',
-  'Publicly Accessible Insights'
-]
-
-const proFeatures: string[] = [
-  '50 Projects',
-  '1 Million Minutes of Recordings',
-  '4 Collaborators',
-  '3 Guests',
-  'Data Privacy Toggle',
-  'Download / Export Projects'
+const basicSubFeatures = [
+  '40,000 minutes of recordings',
+  '1 member (owner only)',
+  'Public visibility for project insights',
+  'Export project data'
 ]
 
 const enterpriseFeatures: string[] = [
-  'Unlimited Projects',
-  'Unlimited Recording Minutes',
-  'Unlimited Collaborators',
-  'Unlimited Guests',
-  'Data Privacy Toggle',
-  'Download / Export Projects',
-  'Dedicated Enterprise Manager'
+  'Unlimited projects',
+  'Unlimited recordings',
+  'Unlimited members',
+  'Private project option',
+  'Export project data',
+  'Dedicated Account Support'
 ]
 
 const signup = async (): Promise<void> => {
